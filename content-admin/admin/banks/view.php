f0dd992994a88c62e447f1399574f9a8c4cb5c7b
baravak <itb.baravak@gmail.com>
2014-11-10 20:20:48 +0330

    get sql table in public funcion sql_lib::getTable(TableName)

--- content-admin/admin/banks/view.php
+++ content-admin/admin/banks/view.php
@@ -3,9 +3,11 @@ class view extends main_view
 {
     // options function call in default config function in main view of admin pannel
     // if you want to use config, add on options function, also in normal situations you don't need to add any code in this function.
-    function options()
-    {
-        
-    }
+	function options()
+	{
+
+		$x = sql_lib::getTable("banks");
+		var_dump($x);
+	}
 }
 ?>
