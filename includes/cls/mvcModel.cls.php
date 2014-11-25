33ae02608d8b70c9d42c731f74c946b2d3b016bc
Javad <J.Evazzadeh@gmail.com>
2014-11-25 22:17:30 +0330

    bugfix model

--- includes/cls/mvcModel.cls.php
+++ includes/cls/mvcModel.cls.php
@@ -27,6 +27,8 @@ class mvcModel_cls{
 			foreach ($array as $key => $value) {
 				if(!is_array($value)) continue;
 				foreach ($value as $k => $v) {
+					echo "<pre>";
+					print_r($value);
 					$err = isset($v['error']) ? $v['error'] : $v;
 					if(preg_match("/^\[\[(update|insert|delete|select)\s(.*)\s(true|false|successful|failed)\]\]$/", $err, $parm)){
 						$err = _($parm[1]).' '._($parm[2]).' '._($parm[3]);
