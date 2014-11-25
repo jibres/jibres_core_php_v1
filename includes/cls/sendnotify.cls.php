02709c3ba28454bb031ab2b10c7bec746a893a79
Javad <J.Evazzadeh@gmail.com>
2014-11-25 23:12:48 +0330

    disable send message

--- includes/cls/sendnotify.cls.php
+++ includes/cls/sendnotify.cls.php
@@ -44,7 +44,7 @@ class sendnotify_cls
 			$api = new KavenegarApi("332F776565494F4D736446712F6D30553061767879673D3D");
 			$result = $api->send("30006703323323","09357269759","جیبرس\n\n"."کد فعال سازی شما: ".$tmp_code);
 			
-			debug_lib::true($result);
+			// debug_lib::true($result);
 		// try
 		// {	
 		// }
