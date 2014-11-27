9ca144c71a2cb1ef8cf3580097727a513a2d3201
baravak <itb.baravak@gmail.com>
2014-11-27 14:12:16 +0330

    fk javad

--- content-account/home/home/controller.php
+++ content-account/home/home/controller.php
@@ -1,6 +1,20 @@
 <?php
-class controller extends main_controller
+class controller extends mvcController_cls
 {
-	
+	function config(){
+		// ----------------------------------------- logout
+		$this->listen(
+			array(
+				"url" => array("account", "logout")
+				),
+			function()
+			{
+				session_unset(); 
+				session_destroy();
+				header("location: "."/");
+				exit();
+			}
+			);
+	}
 }
 ?>
--- content-account/main/controller.php
+++ content-account/main/controller.php
@@ -3,37 +3,6 @@ class main_controller extends mvcController_cls
 {
 	public function config() 
 	{
-
-		// ----------------------------------------- login
-		$this->listen(
-		array(
-				"max" => 1,
-			),
-			function()
-			{
-				/**
-				@Javad: if user login to system redirect to home
-				 */
-				header("location: "."/login");
-				// header("location: http://".DOMAIN);
-				exit();
-			}
-		);
-
-		// ----------------------------------------- logout
-		$this->listen(
-			array(
-				"max" => 1,
-				"url" => array("logout")
-			),
-			function()
-			{
-				session_unset(); 
-				session_destroy();
-				header("location: "."/");
-				exit();
-			}
-		);
 	}
 
 	function options()
