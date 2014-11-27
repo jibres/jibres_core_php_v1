dae34d33831d9a3a49b1b9df1e5d59a000350c69
baravak <itb.baravak@gmail.com>
2014-11-27 19:00:38 +0330

    F

--- content-account/main/controller.php
+++ content-account/main/controller.php
@@ -3,7 +3,7 @@ class main_controller extends mvcController_cls{
 	function options(){
 		if(!$this->login()){
 			$this->listen(array(
-				"real_url" => array("/^(?!login)/")
+				"real_url" => array("/^(?!login|signup)/")
 					),
 				function(){
 					header("Location: /login");
