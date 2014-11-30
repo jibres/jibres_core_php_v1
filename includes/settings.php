c0a54688adee3e4ac919aa724d85084b0ef97869
Javad <J.Evazzadeh@gmail.com>
2014-11-30 15:51:50 +0330

    update for po

--- includes/settings.php
+++ includes/settings.php
@@ -42,15 +42,6 @@ if(!is_dir($repository))
 else
 	define("content"	, $repository);
 
-/**
- * Localized Language, defaults to English.
- *
- * Change this to localize Saloos. A corresponding MO file for the chosen
- * language must be installed to content/languages. For example, install
- * fa_IR.mo to content/languages and set LANGUAGE to 'fa_IR' to enable Persian
- * language support.
- */
-define('LANGUAGE', 'fa_IR');
 
 // in coming soon period show public_html/pages/coming/ folder
 // developer must set get parameter like site.com/dev=anyvalue
@@ -81,13 +72,40 @@ if (DEBUG)
 	error_reporting(E_ALL);
 }
 
-// set localization settings
-$locale = LANGUAGE;
-putenv( "LC_ALL={$locale}" );
-setlocale( LC_ALL				, $locale );
-$domain = $locale;
-bindtextdomain($domain			, includes_dir.'languages/');
-textdomain("*");
+/**
+ * Localized Language, defaults to English.
+ *
+ * Change this to localize Saloos. A corresponding MO file for the chosen
+ * language must be installed to content/languages. For example, install
+ * fa_IR.mo to content/languages and set LANGUAGE to 'fa_IR' to enable Persian
+ * language support.
+ */
+if(isset($_COOKIE["lang"]))
+	$language = $_COOKIE["lang"];
+
+elseif (isset($_GET["lang"]))
+    $language = $_GET["lang"];
+
+else if (isset($_SESSION["lang"]))
+    $language = $_SESSION["lang"];
+
+else 
+    $language = "en_US";
+
+// save language preference for future page requests
+$_SESSION["Language"]  = $language;
+setcookie('lang',$language,time() + 30*24*60*60,'/','.'.URL_RAW);
+
+$domain = "messages";
+putenv("LANG=" . $language);
+setlocale(LC_ALL, $language);
+bindtextdomain($domain , includes_dir.'languages/');
+bind_textdomain_codeset($domain , 'UTF-8');
+textdomain($domain);
+
+echo _("HELLO_WORLD");
+
+
 
 /** Define PATH as the site location */
 define("PATH", preg_replace("/index\.php$/", "", $_SERVER["SCRIPT_NAME"]));
