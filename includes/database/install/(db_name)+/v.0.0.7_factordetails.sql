017a031954a59f9ff34e567ea2b4b4de427755cc
Reza <rm.biqarar@gmail.com>
2017-10-23 19:15:09 +0330

    install database

--- includes/database/install/(db_name)+/v.0.0.7_factordetails.sql
+++ includes/database/install/(db_name)+/v.0.0.7_factordetails.sql
@@ -1,14 +1,10 @@
 CREATE TABLE `factordetails` (
 `factor_id` bigint(20) UNSIGNED NOT NULL,
 `product_id` int(10) UNSIGNED NOT NULL,
-`title` varchar(500) NOT NULL,
-`price` int(10) DEFAULT NULL,
-`count` smallint(5) DEFAULT NULL,
-`total` int(10) DEFAULT NULL,
-`discount` smallint(5) DEFAULT NULL,
-`status` enum('enable','disable','expire') NOT NULL DEFAULT 'enable',
-`datecreated` datetime DEFAULT CURRENT_TIMESTAMP,
-`datemodified` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-`desc` text CHARACTER SET utf8mb4,
-CONSTRAINT `factordetails_factor_id` FOREIGN KEY (`factor_id`) REFERENCES `factors` (`id`) ON UPDATE CASCADE
+`price` bigint(20) UNSIGNED DEFAULT NULL,
+`count` int(10) UNSIGNED DEFAULT NULL,
+`discount` int(10) DEFAULT NULL,
+`sum` bigint(20) UNSIGNED DEFAULT NULL,
+CONSTRAINT `factordetails_factor_id` FOREIGN KEY (`factor_id`) REFERENCES `factors` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `factordetails_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
