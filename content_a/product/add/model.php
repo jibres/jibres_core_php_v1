f4d95fa1fbf28ad50ef10af9a447bd1011c675ab
Reza <rm.biqarar@gmail.com>
2017-11-02 10:18:31 +0330

    add new product get all field

--- content_a/product/add/model.php
+++ content_a/product/add/model.php
@@ -6,6 +6,7 @@ use \lib\debug;
 class model extends \content_a\main\model
 {
 
+
 	/**
 	 * Gets the post.
 	 *
@@ -13,14 +14,38 @@ class model extends \content_a\main\model
 	 */
 	public static function getPost()
 	{
+
 		$args =
 		[
-			'title' => utility::post('title'),
-			'name'  => utility::post('name'),
+			'title'          => utility::post('title'),
+			'name'           => utility::post('name'),
+			'cat'            => utility::post('cat'),
+			'slug'           => utility::post('slug'),
+			'company'        => utility::post('company'),
+			'shortcode'      => utility::post('shortcode'),
+			'unit'           => utility::post('unit'),
+			'barcode'        => utility::post('barcode'),
+			'barcode2'       => utility::post('barcode2'),
+			'buyprice'       => utility::post('buyprice'),
+			'price'          => utility::post('price'),
+			'discount'       => utility::post('discount'),
+			'vat'            => utility::post('vat'),
+			'initialbalance' => utility::post('initialbalance'),
+			'minstock'       => utility::post('minstock'),
+			'maxstock'       => utility::post('maxstock'),
+			'status'         => utility::post('status'),
+			'sold'           => utility::post('sold'),
+			'stock'          => utility::post('stock'),
+			'service'        => utility::post('service') === 'on' ? 1 : 0,
+			'sellonline'     => utility::post('sellonline') === 'on' ? 1 : 0,
+			'sellstore'      => utility::post('sellstore') === 'on' ? 1 : 0,
+			'carton'         => utility::post('carton'),
 		];
 
 		return $args;
 	}
+
+
 
 
 	/**
