571482ab27a8fdada6907a7c063141191e467d15
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-11 15:13:35 +0330

    fix on c and block all main modules

--- content_c/main/model.php
+++ content_c/main/model.php
@@ -1,6 +1,7 @@
 <?php
 namespace content_c\main;
 
+
 class model extends \mvc\model
 {
 
--- includes/mvc/controller.php
+++ includes/mvc/controller.php
@@ -3,6 +3,13 @@ namespace mvc;
 
 class controller extends \lib\controller
 {
-
+	public function project()
+	{
+		if(\lib\router::get_url() === 'main')
+		{
+			$this->redirector($this->url('baseFull'))->redirect();
+			\lib\error::page();
+		}
+	}
 }
 ?>
