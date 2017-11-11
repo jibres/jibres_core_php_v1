deaa2a757e6cb4ae5651a9f41f765db91586ae64
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-11 16:46:16 +0330

    remove unused in add product

--- content_a/product/add/model.php
+++ content_a/product/add/model.php
@@ -10,28 +10,25 @@ class model extends \content_a\main\model
 		$args =
 		[
 			'title'          => \lib\utility::post('title'),
-			'name'           => \lib\utility::post('name'),
+
 			'cat'            => \lib\utility::post('cat'),
-			'slug'           => \lib\utility::post('slug'),
 			'company'        => \lib\utility::post('company'),
-			'shortcode'      => \lib\utility::post('shortcode'),
+
 			'unit'           => \lib\utility::post('unit'),
+			'carton'         => \lib\utility::post('carton'),
+
 			'barcode'        => \lib\utility::post('barcode'),
 			'barcode2'       => \lib\utility::post('barcode2'),
+
+			'minstock'       => \lib\utility::post('minstock'),
+			'maxstock'       => \lib\utility::post('maxstock'),
+
 			'buyprice'       => \lib\utility::post('buyprice'),
 			'price'          => \lib\utility::post('price'),
 			'discount'       => \lib\utility::post('discount'),
-			'vat'            => \lib\utility::post('vat'),
+
 			'initialbalance' => \lib\utility::post('initialbalance'),
-			'minstock'       => \lib\utility::post('minstock'),
-			'maxstock'       => \lib\utility::post('maxstock'),
 			'status'         => \lib\utility::post('status'),
-			'sold'           => \lib\utility::post('sold'),
-			'stock'          => \lib\utility::post('stock'),
-			'service'        => \lib\utility::post('service') === 'on' ? 1 : 0,
-			'sellonline'     => \lib\utility::post('sellonline') === 'on' ? 1 : 0,
-			'sellstore'      => \lib\utility::post('sellstore') === 'on' ? 1 : 0,
-			'carton'         => \lib\utility::post('carton'),
 		];
 
 		return $args;
