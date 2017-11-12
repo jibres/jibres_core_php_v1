d7797debce42c7f8448037cc45efa95475b55ace
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-12 18:20:36 +0330

    add summary design

--- content_a/product/summary/view.php
+++ content_a/product/summary/view.php
@@ -7,7 +7,7 @@ class view extends \content_a\main\view
 	public function config()
 	{
 		$this->data->page['title'] = T_('Products Summary');
-		$this->data->page['desc']  = T_('Some detail about this product!');
+		$this->data->page['desc']  = T_('Some detail about your product!');
 
 		$this->data->page['badge']['link'] = '/a/product/add';
 		$this->data->page['badge']['text'] = T_('Add new product');
