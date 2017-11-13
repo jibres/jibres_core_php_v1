944ff74f4ea922c9cf234c69d4572abda2c5b573
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-13 18:21:07 +0330

    fix url if not have query for search

--- content_a/product/home/controller.php
+++ content_a/product/home/controller.php
@@ -8,6 +8,14 @@ class controller extends \content_a\main\controller
 	{
 		$this->get()->ALL();
 		// $this->post('search')->ALL();
+
+		// redirect if query not exist
+		if(\lib\utility::get('q') === '' && \lib\router::get_url_property('page') === null)
+		{
+			$new_url = $this->url('full');
+			$new_url = strtok($new_url,'?');
+			$this->redirector($new_url)->redirect();
+		}
 	}
 }
 ?>
--- content_a/product/home/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace content_a\product\home;
-
-
-class model extends \content_a\main\model
-{
-
-}
-?>
