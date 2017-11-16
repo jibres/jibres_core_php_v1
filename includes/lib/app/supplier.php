9ca1a8a11b802421a2d06455b712a5dec939bbaa
Reza <rm.biqarar@gmail.com>
2017-11-16 16:17:36 +0330

    supplier add bug fix

--- includes/lib/app/supplier.php
+++ includes/lib/app/supplier.php
@@ -30,27 +30,6 @@ class supplier
 	 */
 	public static function check($_option = [])
 	{
-		if(\lib\app::isset_request('firstname') && !trim(\lib\app::request('firstname')))
-		{
-			if(\lib\app::isset_request('lastname') && !trim(\lib\app::request('lastname')))
-			{
-				\lib\app::log('app:supplier:firstname:cannot:null:'. self::$type , \lib\user::id());
-				\lib\debug::error(T_("Firstname or Lastname of supplier can not be null"), ['firstname', 'lastname']);
-				return false;
-			}
-		}
-
-		if(\lib\app::isset_request('birthday') && \lib\app::request('birthday'))
-		{
-			$birthday = \lib\utility\human::number(\lib\app::request('birthday'), 'en');
-			if(strtotime($birthday) === false)
-			{
-				\lib\app::log('app:supplier:invalid:birthday:'. self::$type , \lib\user::id());
-				\lib\debug::error(T_("birthday is incorrect"), 'birthday');
-				return false;
-			}
-		}
-
 		return \lib\app\user::check(...func_get_args());
 	}
 }
