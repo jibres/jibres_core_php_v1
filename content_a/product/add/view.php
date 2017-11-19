fd367f0620d9e6068fbb1feb38a2c3c210b6dd3a
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-19 10:13:50 +0330

    get some detail of product on add from parameter

--- content_a/product/add/view.php
+++ content_a/product/add/view.php
@@ -12,6 +12,32 @@ class view extends \content_a\main\view
 		$this->data->cat_list     = \lib\app\product::cat_list(true);
 		$this->data->company_list = \lib\app\product::company_list(true);
 		$this->data->unit_list    = \lib\app\product::unit_list(true);
+
+		// get some value from get
+		if(\lib\utility::get('barcode'))
+		{
+			$this->data->product['barcode'] = \lib\utility::get('barcode');
+		}
+		if(\lib\utility::get('barcode2'))
+		{
+			$this->data->product['barcode2'] = \lib\utility::get('barcode2');
+		}
+		if(\lib\utility::get('price'))
+		{
+			$this->data->product['price'] = \lib\utility::get('price');
+		}
+		if(\lib\utility::get('discount'))
+		{
+			$this->data->product['discount'] = \lib\utility::get('discount');
+		}
+		if(\lib\utility::get('buyprice'))
+		{
+			$this->data->product['buyprice'] = \lib\utility::get('buyprice');
+		}
+		if(\lib\utility::get('title'))
+		{
+			$this->data->product['title'] = \lib\utility::get('title');
+		}
 	}
 }
 ?>
