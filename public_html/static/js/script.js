4f6d5c5c1345033fc68474085800c486d704bdb4
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-19 13:27:21 +0330

    calc productList Prices

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -6,6 +6,7 @@ route('*', function()
 }).once(function()
 {
   runRunner();
+  calcFooterValues();
 });
 
 
@@ -97,18 +98,80 @@ function recalcProductListPrices(_this)
 {
   var elRow      = _this.parents('tr');
   var elTable    = _this.parents('table');
-  var elRowTotal = elRow.find('[data-total]');
+  var elRowTotal = elRow.find(':eq(5)');
 
-  var valCount    = elRow.find('[data-count] input').val().toEnglish();
-  var valPrice    = elRow.find('[data-price]').text().toEnglish();
-  var valDiscount = elRow.find('[data-discount] input').val().toEnglish();
+  var valCount    = elRow.find('.count').val().toEnglish();
+  var valPrice    = elRow.find(':eq(3)').text().toEnglish();
+  var valDiscount = elRow.find('.discount').val().toEnglish();
   var valTotal    = elRowTotal.text().toEnglish();
 
 
-  var calcTotal = valCount * (valPrice - valDiscount);
-  elRowTotal.text(fitNumber(calcTotal));
+  var calcRowTotal = valCount * (valPrice - valDiscount);
+  elRowTotal.text(fitNumber(calcRowTotal));
+
+  calcFooterValues(elTable);
+}
+
+
+function calcFooterValues(_table)
+{
+  if(!_table)
+  {
+    _table = $('.productList');
+    if(_table.length < 1)
+    {
+      return null;
+    }
+  }
+  var calcDtCountRow    = 0;
+  var calcDtSumCount    = 0;
+  var calcDtSumPrice    = 0;
+  var calcDtSumDiscount = 0;
+  var calcDtSumTotal    = 0;
+  // calc total of column
+  _table.find('tbody tr').each(function()
+  {
+    // variables
+    let tmpCount       = parseFloat($(this).find('.count').val().toEnglish());
+    let tmpPrice       = parseInt($(this).find('td:eq(3)').text().toEnglish());
+    let tmpDiscount    = parseInt($(this).find('.discount').val().toEnglish());
+    // check NaN values
+    if(isNaN(tmpCount))
+    {
+      tmpCount = 0;
+    }
+    if(isNaN(tmpPrice))
+    {
+      tmpPrice = 0;
+    }
+    if(isNaN(tmpDiscount))
+    {
+      tmpDiscount = 0;
+    }
+
+    let tmpPriceCol    = tmpCount * tmpPrice;
+    let tmpDiscountCol = tmpCount * tmpDiscount;
+    let tmpFinalCol    = tmpCount * (tmpPrice - tmpDiscount);
+    console.log(tmpFinalCol);
+
+    // count of row
+    calcDtCountRow += 1;
+    // sum of counts
+    calcDtSumCount += tmpCount;
+    calcDtSumPrice += tmpPriceCol;
+    calcDtSumDiscount += tmpDiscountCol;
+    calcDtSumTotal += tmpFinalCol;
+    $(this).find('td:eq(5)').text(fitNumber(calcDtSumTotal));
+
+  });
+  _table.find('tfoot tr th:eq(0)').text(fitNumber(calcDtCountRow));
+  _table.find('tfoot tr th:eq(2)').text(fitNumber(calcDtSumCount));
+  _table.find('tfoot tr th:eq(3)').text(fitNumber(calcDtSumPrice));
+  _table.find('tfoot tr th:eq(4)').text(fitNumber(calcDtSumDiscount));
+  _table.find('tfoot tr th:eq(5)').text(fitNumber(calcDtSumTotal));
 
 }
+
 
 
 
