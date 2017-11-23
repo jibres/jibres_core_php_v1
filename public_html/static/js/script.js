49f9947217d1fbb3ec87cd0a6eaa4e737c856081
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-11-23 16:54:01 +0330

    complete conditional formating for price of product and coloring them

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -78,6 +78,69 @@ function recalcPricePercents()
   {
     // elFinalPriceBox.slideUp();
   }
+
+  // conditional check
+  var elPrice      = $('#price').parents('.input');
+  var elDiscount   = $('#discount').parents('.input');
+  var elFinalPrice = $('#finalprice').parents('.input');
+
+  // all check for price
+  // if price is not normal and under buy price, impure under zero
+  if(impureIntrestRate < 0)
+  {
+    elPrice.addClass('warning');
+  }
+  else
+  {
+    elPrice.removeClass('warning');
+  }
+  // if discount is more than 100% of sell price
+  if(discount === 0)
+  {
+    elDiscount.removeClass('error');
+    elDiscount.removeClass('warning');
+  }
+  else if(discountRate > 100)
+  {
+    elDiscount.addClass('error');
+    elDiscount.removeClass('warning');
+  }
+  else if(sell - discount < buy)
+  {
+    elDiscount.removeClass('error');
+    elDiscount.addClass('warning');
+  }
+  else
+  {
+    elDiscount.removeClass('error');
+    elDiscount.removeClass('warning');
+  }
+
+  // all check for final price
+  if(finalPrice === 0)
+  {
+    elFinalPrice.removeClass('error');
+    elFinalPrice.removeClass('warning');
+    elFinalPrice.removeClass('ok');
+  }
+  else if(finalPrice < 0)
+  {
+    elFinalPrice.addClass('error');
+    elFinalPrice.removeClass('warning');
+    elFinalPrice.removeClass('ok');
+  }
+  else if(finalPrice < buy)
+  {
+    elFinalPrice.removeClass('error');
+    elFinalPrice.addClass('warning');
+    elFinalPrice.removeClass('ok');
+  }
+  else
+  {
+    elFinalPrice.removeClass('error');
+    elFinalPrice.removeClass('warning');
+    elFinalPrice.addClass('ok');
+  }
 }
 
 
