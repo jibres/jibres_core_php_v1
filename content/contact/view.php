459afafdce51fb972ce893d8b7c536731adfc186
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-12-07 16:21:06 +0330

    add map and some fix for vcard

--- content/contact/view.php
+++ content/contact/view.php
@@ -5,7 +5,7 @@ class view extends \mvc\view
 {
 	function config()
 	{
-		$this->data->page['title'] = T_("Contact");
+		$this->data->page['title'] = T_("Contact Us");
 		$this->data->page['desc'] = T_("Knowing your valuable comments about bugs and problems and more importantly your precious offers will help us in this way.");
 
 		$this->data->bodyclass = 'unselectable vflex';
