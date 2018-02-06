03b4d08a908b726d7bd9929deec1c49c29fe8d42
Reza <rm.biqarar@gmail.com>
2018-02-06 12:20:05 +0330

    save store factor syntax fix

--- content_a/setting/factor/model.php
+++ content_a/setting/factor/model.php
@@ -5,54 +5,65 @@ namespace content_a\setting\factor;
 class model extends \content_a\main\model
 {
 
-	public static function getPost($_old_pos_list = [])
+	public static function getPost($_old_meta = [])
 	{
-		if(!is_array($_old_pos_list))
-		{
-			$_old_pos_list = [];
-		}
+		$default_meta =
+		[
+			'print_status'  => false,
+			'print_size'    =>
+			[
+				'fishprint' => false,
+				'a4'        => false,
+				'a5'        => false,
+			],
+			'detault_print' => 'fishprint',
 
-		$meta                 = $_old_pos_list;
+			'pay_status' => false,
+			'pay'        =>
+			[
+				'cash'   => false,
+				'pos'    => false,
+				'cheque' => false,
+			],
+
+			'detault_pay' => 'cash',
+			'pos_list'    =>  [],
+			'default_pos' => null,
+		];
+
+		$meta                 = $default_meta;
 		$meta['print_status'] = \lib\utility::post('printStatus') ? true : false;
 
 		if(\lib\utility::post('print_size') && is_array(\lib\utility::post('print_size')))
 		{
-			$meta['print_size']   = array_values(\lib\utility::post('print_size'));
-		}
-		else
-		{
-			$meta['print_size']   = null;
+			$meta['print_size']['fishprint'] = in_array('fishprint', \lib\utility::post('print_size')) ? true : false;
+			$meta['print_size']['a4']        = in_array('a4', \lib\utility::post('print_size')) ? true : false;
+			$meta['print_size']['a5']        = in_array('a5', \lib\utility::post('print_size')) ? true : false;
 		}
 
 		if(\lib\utility::post('pay') && is_array(\lib\utility::post('pay')))
 		{
-			$meta['pay']          = array_values(\lib\utility::post('pay'));
+			$meta['pay']['cash']   = in_array('cash', \lib\utility::post('pay')) ? true : false;
+			$meta['pay']['pos']    = in_array('pos', \lib\utility::post('pay')) ? true : false;
+			$meta['pay']['cheque'] = in_array('cheque', \lib\utility::post('pay')) ? true : false;
+		}
+
+		$pos_name = \lib\utility::post('posList');
+
+		if(isset($_old_meta['pos_list']) && is_array($_old_meta['pos_list']))
+		{
+			array_push($_old_meta['pos_list'], $pos_name);
 		}
 		else
 		{
-			$meta['pay']          = null;
+			$_old_meta['pos_list'] = $default_meta['pos_list'];
 		}
 
-		$pos_name             = \lib\utility::post('posList');
-
-		if($pos_name)
-		{
-			if(isset($_old_pos_list['pos_list']))
-			{
-				if(!in_array($pos_name, $_old_pos_list['pos_list']))
-				{
-					$meta['pos_list'][] = $pos_name;
-				}
-			}
-			else
-			{
-				$meta['pos_list'] = [$pos_name];
-			}
-		}
-
+		$meta['pos_list'] = array_filter(array_unique($_old_meta['pos_list']));
 
 		return $meta;
 	}
+
 
 
 	public function post_factor()
