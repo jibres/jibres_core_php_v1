8ad6cfcda84daa7bf32e84051702438519654e80
Reza <rm.biqarar@gmail.com>
2018-02-07 18:31:57 +0330

    save thirdprty fix

--- content_a/thirdparty/add/model.php
+++ content_a/thirdparty/add/model.php
@@ -6,31 +6,36 @@ class model extends \content_a\main\model
 {
 	public static function getPostthirdparty()
 	{
+		$post         = [];
+		$post['type'] = \lib\utility::get('type');
+
 		if(\lib\utility::get('type') === 'supplier')
 		{
-			$post =
-			[
-				'type'          => \lib\utility::get('type'),
-				'visitorname'   => \lib\utility::post('visitorname'),
-				'visitormobile' => \lib\utility::post('visitormobile'),
-				'company'       => \lib\utility::post('company'),
-			];
+			$post['visitorname']   = \lib\utility::post('visitorname');
+			$post['visitormobile'] = \lib\utility::post('visitormobile');
+			$post['company']       = \lib\utility::post('company');
 		}
 		else
 		{
 
-			$post =
-			[
-				'mobile'       => \lib\utility\filter::mobile(\lib\utility::post('mobile')),
-				'type'         => \lib\utility::get('type'),
-				'firstname'    => \lib\utility::post('name'),
-				'lastname'     => \lib\utility::post('lastName'),
-				'nationalcode' => \lib\utility::post('nationalcode'),
-				'father'       => \lib\utility::post('father'),
-				'birthday'     => \lib\utility::post('birthday'),
-				'gender'       => \lib\utility::post('gender') === 'on' ? 'female' : 'male',
-			];
+			$post['mobile']       = \lib\utility\filter::mobile(\lib\utility::post('mobile'));
+			$post['type']         = \lib\utility::get('type');
+			$post['firstname']    = \lib\utility::post('name');
+			$post['lastname']     = \lib\utility::post('lastName');
+			$post['nationalcode'] = \lib\utility::post('nationalcode');
+			$post['gender']       = \lib\utility::post('gender') === 'on' ? 'female' : 'male';
 
+			if(\lib\utility::get('type') === 'staff')
+			{
+				$post['birthday']  = \lib\utility::post('birthday');
+			}
+			else
+			{
+				$post['code']    = \lib\utility::post('code');
+				$post['address'] = \lib\utility::post('address');
+				$post['phone']   = \lib\utility::post('phone');
+				$post['desc']    = \lib\utility::post('desc');
+			}
 		}
 
 		return $post;
@@ -42,13 +47,13 @@ class model extends \content_a\main\model
 		// ready request
 		$request = self::getPostthirdparty();
 
-		\lib\app\thirdparty::add($request);
+		$result = \lib\app\thirdparty::add($request);
 
 		if(\lib\debug::$status)
 		{
-			if(isset($result['user_id']))
+			if(isset($result['thirdparty_id']))
 			{
-				$this->redirector($this->url('base'). '/a/thirdparty/edit='. $result['user_id']);
+				$this->redirector($this->url('base'). '/a/thirdparty/edit?id='. $result['thirdparty_id']);
 			}
 			else
 			{
