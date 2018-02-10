590e87ba7a2abbf36f70eca65fdca55093b87636
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-02-10 15:39:56 +0330

    fix titles in home of factors

--- content_a/factor/home/view.php
+++ content_a/factor/home/view.php
@@ -6,15 +6,7 @@ class view extends \content_a\main\view
 {
 	public function config()
 	{
-		$this->data->page['title'] = T_('List of factors');
-		$this->data->page['desc']  = T_('You can search in list of factors, add new factor and edit existing.');
-
-		$this->data->page['badge']['link'] = $this->url('baseFull'). '/factor/add';
-		$this->data->page['badge']['text'] = T_('Add new factor');
-
-		// add back to product list link
-		$product_list_link =  '<a href="'. $this->url('baseFull') .'/factor" data-shortkey="121">'. T_('Back to factor list'). ' <kbd>f10</kbd></a>';
-		$this->data->page['desc']  .= ' '. $product_list_link;
+		self::set_best_title();
 
 		$args =
 		[
@@ -41,6 +33,33 @@ class view extends \content_a\main\view
 		if(isset($this->controller->pagnation))
 		{
 			$this->data->pagnation = $this->controller->pagnation_get();
+		}
+	}
+
+	private function set_best_title()
+	{
+		// set usable variable
+		$this->data->modulePath = $this->url('baseFull'). '/factor';
+		$this->data->moduleType = \lib\utility::get('type');
+
+		// set default title
+		$this->data->page['title'] = T_('List of factors');
+		$this->data->page['desc']  = T_('You can search in list of factors, add new factor or edit existing.');
+		// set badge
+		$this->data->page['badge']['link'] = $this->data->modulePath. '/summary';
+		$this->data->page['badge']['text'] = T_('Back to factors summary');
+
+
+		// // for special condition
+		if($this->data->moduleType)
+		{
+			$this->data->page['title'] = T_('List of :type', ['type' => $this->data->moduleType]);
+			$this->data->page['desc']  = T_('Search in list of :type factors, add or edit them.', ['type' => $this->data->moduleType]);
+			$this->data->page['desc']  .= ' <a href="'. $this->data->modulePath .'" data-shortkey="121">'. T_('List of all factors.'). '<kbd>f10</kbd></a>';
+
+
+			$this->data->page['badge']['link'] = $this->data->modulePath. '/add?type='. $this->data->moduleType;
+			$this->data->page['badge']['text'] = T_('Add new :type', ['type' => $this->data->moduleType]);
 		}
 	}
 }
