caf8c51661b70d9c1305bb0d5cf79962ada9d2d5
Reza <rm.biqarar@gmail.com>
2018-04-21 19:44:00 +0430

    fix option

--- includes/option/option.php
+++ includes/option/option.php
@@ -34,42 +34,12 @@ self::$language =
 /**
  * system default lanuage
  */
-// self::$config['default_language']             = 'fa';
-// self::$config['redirect_url']                 = 'https://jibres.com';
-// self::$config['multi_domain']                 = true;
-// self::$config['redirect_to_main']             = true;
-// self::$config['https']                        = true;
-// self::$config['default_tld']                  = 'com';
-self::$config['default_permission']           = null;
-self::$config['debug']                        = true;
-self::$config['coming']                       = false;
-self::$config['short_url']                    = null;
-self::$config['save_as_cookie']               = false;
-self::$config['log_visitors']                 = true;
-self::$config['passphrase']                   = null;
-self::$config['passkey']                      = null;
-self::$config['passvalue']                    = null;
-self::$config['default']                      = null;
+
+
 self::$config['redirect']                     = 'c';
-self::$config['register']                     = true;
-self::$config['recovery']                     = true;
-// self::$config['fake_sub']                     = null;
-// self::$config['real_sub']                     = true;
-self::$config['force_short_url']              = null;
-self::$config['sms']                          = true;
 
-// self::$config['account']                      = true;
-// self::$config['main_account']                 = null;
-// self::$config['account_status']               = true;
-// self::$config['use_main_account']             = false;
-
-// self::$config['domain_same']                  = true;
-// self::$config['domain_name']                  = 'jibres';
-// self::$config['main_site']                    = 'https://jibres.com';
 
 self::$config['favicon']['version']           = null;
-
-
 
 
 /**
@@ -79,13 +49,6 @@ self::$config['enter']['call']                = true;
 self::$config['enter']['call_template_fa'] = 'ermile-fa';
 self::$config['enter']['call_template_en'] = 'ermile-en';
 
-/**
- * telegram hook
- */
-self::$config['enter']['telegram_hook']   = '**Ermile**vHTnEoYth43MwBH7o6mPk807Jibresf0DUbXZ7k2Bju5n^^Telegram^^';
-// static token
-// get the user id by mobile in api header
-self::$config['enter']['static_token'][]  = '**Ermile**Azvir^^Jibres--Token__Static++6mPf0DUbXZ7kth43MwBH7o6mPk8';
 
 /**
  * first signup url
--- includes/option/sms.php
+++ includes/option/sms.php
@@ -3,7 +3,7 @@
 * sms kavenegar config
 */
 self::$sms['kavenegar']['status'] = true;
-self::$sms['kavenegar']['apikey'] = '783067644A597A41716F3734755A683152736F6673773D3D';
+// self::$sms['kavenegar']['apikey'] = '783067644A597A41716F3734755A683152736F6673773D3D';
 self::$sms['kavenegar']['debug']  = null;
 self::$sms['kavenegar']['line']   = '10006660066600';
 self::$sms['kavenegar']['iran']   = true;
