9e98f180e27a4d54e36e904ef66da40711707489
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-04-21 19:58:46 +0430

    fix sms token

--- includes/option/sms.php
+++ includes/option/sms.php
@@ -3,7 +3,7 @@
 * sms kavenegar config
 */
 self::$sms['kavenegar']['status'] = true;
-// self::$sms['kavenegar']['apikey'] = '783067644A597A41716F3734755A683152736F6673773D3D';
+self::$sms['kavenegar']['apikey'] = '5263694C4C426651434C6635686E463550333747363578636361446539383141';
 self::$sms['kavenegar']['debug']  = null;
 self::$sms['kavenegar']['line']   = '10006660066600';
 self::$sms['kavenegar']['iran']   = true;
