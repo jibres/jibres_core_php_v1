613714ae9d05259d1d666c35abca55ffcadc259c
Reza <rm.biqarar@gmail.com>
2018-04-29 19:36:42 +0430

    save transaction payment log (DB UPGRADE)

---
+++ includes/database/upgrade/(db_name)/v.2.1.1_payment_responce.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `transactions` ADD `payment_response1` text CHARACTER  SET utf8mb4 NULL;
+ALTER TABLE `transactions` ADD `payment_response2` text CHARACTER  SET utf8mb4 NULL;
+ALTER TABLE `transactions` ADD `payment_response3` text CHARACTER  SET utf8mb4 NULL;
+ALTER TABLE `transactions` ADD `payment_response4` text CHARACTER  SET utf8mb4 NULL;
