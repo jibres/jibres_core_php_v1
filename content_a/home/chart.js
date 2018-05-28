9a1e23f061a349eed7da570b7c355c0b8ae4c7e7
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-28 15:57:07 +0430

    add chartDrawe

--- content_a/home/chart.js
+++ content_a/home/chart.js
@@ -1,34 +1,37 @@
-am4core.useTheme(am4themes_animated);
+function chartDrawer()
+{
+  am4core.useTheme(am4themes_animated);
 
-var chart = am4core.create("chartdiv", am4charts.XYChart);
-chart.data = {{dashboardData.sale_time_chart   | raw}};
+  var chart = am4core.create("chartdiv", am4charts.XYChart);
+  chart.data = {{dashboardData.sale_time_chart   | raw}};
 
-var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.dataFields.category = "key";
-categoryAxis.renderer.minGridDistance = 60;
-
-
-var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.title.text = '{%trans "Count of factors"%}';
-
-var series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.categoryX = "key";
-series.dataFields.valueY = "value";
-series.tooltipText = "{valueY.value}"
-series.columns.template.strokeOpacity = 0;
+  var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.dataFields.category = "key";
+  categoryAxis.renderer.minGridDistance = 60;
 
 
-chart.cursor = new am4charts.XYCursor();
+  var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.title.text = '{%trans "Count of factors"%}';
 
-// as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-series.columns.template.adapter.add("fill", function (fill, target) {
-	return chart.colors.getIndex(target.dataItem.index);
-});
+  var series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.categoryX = "key";
+  series.dataFields.valueY = "value";
+  series.tooltipText = "{valueY.value}"
+  series.columns.template.strokeOpacity = 0;
 
-var label = chart.plotContainer.createChild(am4core.Label);
-label.text = '{%trans "Sales count group by hour"%}';
-label.x = 10;
-label.y = 10;
 
+  chart.cursor = new am4charts.XYCursor();
+
+  // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+  series.columns.template.adapter.add("fill", function (fill, target)
+  {
+    return chart.colors.getIndex(target.dataItem.index);
+  });
+
+  var label = chart.plotContainer.createChild(am4core.Label);
+  label.text = '{%trans "Sales count group by hour"%}';
+  label.x = 10;
+  label.y = 10;
+}
 
