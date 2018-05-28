482ee3df38d9c571e70d8468a667c0465e5493cc
Reza <rm.biqarar@gmail.com>
2018-05-28 15:39:11 +0430

    fix title of chart

--- content_a/home/chart.js
+++ content_a/home/chart.js
@@ -10,7 +10,9 @@ categoryAxis.renderer.grid.template.location = 0;
 categoryAxis.dataFields.category = "key";
 categoryAxis.renderer.minGridDistance = 60;
 
+
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.title.text = '{%trans "Cout of factors"%}';
 
 var series = chart.series.push(new am4charts.ColumnSeries());
 series.dataFields.categoryX = "key";
@@ -18,9 +20,14 @@ series.dataFields.valueY = "value";
 series.tooltipText = "{valueY.value}"
 series.columns.template.strokeOpacity = 0;
 
+
 chart.cursor = new am4charts.XYCursor();
 
 // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
 series.columns.template.adapter.add("fill", function (fill, target) {
 	return chart.colors.getIndex(target.dataItem.index);
 });
+
+
+
+
