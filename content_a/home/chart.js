05246f78812414462a17ef17fcda36153058de60
Reza <rm.biqarar@gmail.com>
2018-05-28 15:16:10 +0430

    add new chart in homepage

---
+++ content_a/home/chart.js
@@ -0,0 +1,26 @@
+am4core.useTheme(am4themes_animated);
+
+var chart = am4core.create("chartdiv", am4charts.XYChart);
+chart.data = {{dashboardData.sale_time_chart   | raw}};
+
+chart.padding(40, 40, 40, 40);
+
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.dataFields.category = "key";
+categoryAxis.renderer.minGridDistance = 60;
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+var series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.categoryX = "key";
+series.dataFields.valueY = "value";
+series.tooltipText = "{valueY.value}"
+series.columns.template.strokeOpacity = 0;
+
+chart.cursor = new am4charts.XYCursor();
+
+// as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+series.columns.template.adapter.add("fill", function (fill, target) {
+	return chart.colors.getIndex(target.dataItem.index);
+});
