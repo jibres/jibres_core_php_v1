0620d37ab333bb37985f263096141ed948ebda88
Reza <rm.biqarar@gmail.com>
2018-05-29 18:07:38 +0430

    show report page

---
+++ content_a/report/home/controller.php
@@ -0,0 +1,43 @@
+<?php
+namespace content_a\report\home;
+
+
+class controller
+{
+	public static function routing()
+	{
+		\dash\permission::access('aReportView');
+
+		$redirect_enter = false;
+		if(\dash\user::detail('logintime'))
+		{
+			if((time() - intval(\dash\user::detail('logintime')) ) > (60*10))
+			{
+				if(!isset($_SESSION['try_load_report']))
+				{
+					$_SESSION['try_load_report'] = 1;
+					\dash\notif::warn(T_("You must login again to view this page"));
+					\dash\header::status(403, ' ');
+				}
+				else
+				{
+					$redirect_enter = true;
+				}
+			}
+			else
+			{
+				$redirect_enter = false;
+			}
+		}
+		else
+		{
+			$redirect_enter = true;
+		}
+
+		if($redirect_enter)
+		{
+			\dash\redirect::to(\dash\url::base(). '/logout?mobile='. \dash\user::detail('mobile'). '&referer='. \dash\url::pwd());
+		}
+	}
+}
+?>
