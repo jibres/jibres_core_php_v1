bf5f0eccdeaa171dfb967d2333febb0b5122a246
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-19 17:46:44 +0430

    fix subdomain main page

--- content_subdomain/home/view.php
+++ content_subdomain/home/view.php
@@ -13,6 +13,14 @@ class view
 
 		\dash\data::bodyclass('unselectable flex align-center justify-center txtC');
 		\dash\data::include_js(false);
+
+		\dash\data::service_title(T_("Jibres"));
+		\dash\data::service_desc(T_("Jibres is not just an online accounting software;"). ' <br> '.  T_("We try to create the best financial platform that has everything you need to sale and manage your financial life."));
+		\dash\data::service_slogan(T_("Integrated Sales and Online Accounting"));
+		\dash\data::service_logo(\dash\url::site(). '/static/siftal/images/logo/jibres.png');
+		\dash\data::service_url('https://jibres.com');
+
+		\dash\data::store(\lib\store::detail());
 	}
 }
 ?>
