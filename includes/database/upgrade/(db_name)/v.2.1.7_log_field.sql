48b6b6ed4009495496d8d7d8f973e3c7b0f6ab09
Reza <rm.biqarar@gmail.com>
2018-07-17 15:07:59 +0430

    change log field

---
+++ includes/database/upgrade/(db_name)/v.2.1.7_log_field.sql
@@ -0,0 +1,14 @@
+ALTER TABLE `jibres_log`.`logs` CHANGE `caller` `caller` varchar(200) NULL DEFAULT NULL AFTER `id`;
+ALTER TABLE `jibres_log`.`logs` CHANGE `data` `data` varchar(200) NULL DEFAULT NULL AFTER `user_id`;
+ALTER TABLE `jibres_log`.`logs` ADD `datalink` varchar(100) NULL DEFAULT NULL AFTER `data`;
+ALTER TABLE `jibres_log`.`logs` ADD `subdomain` varchar(100) NULL DEFAULT NULL AFTER `caller`;
+ALTER TABLE `jibres_log`.`logs` DROP `before`;
+ALTER TABLE `jibres_log`.`logs` DROP `after`;
+ALTER TABLE `jibres_log`.`logs` DROP `desc`;
+ALTER TABLE `jibres_log`.`logs` DROP `vars`;
+ALTER TABLE `jibres_log`.`logs` DROP FOREIGN KEY `logs_logitems_id`;
+ALTER TABLE `jibres_log`.`logs` DROP INDEX `logs_logitems_id`;
+ALTER TABLE `jibres_log`.`logs` DROP `logitem_id`;
+ALTER TABLE `jibres_log`.`logs` DROP `createdate`;
+ALTER TABLE `jibres_log`.`logs` CHANGE `datecreated` `datecreated` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `status`;
+ALTER TABLE `jibres_log`.`logs` CHANGE `meta` `meta` MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL AFTER `visitor_id`;
