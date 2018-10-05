b1717d9ca0ed19220f617002e472e68a0f0a98d3
Reza <rm.biqarar@gmail.com>
2018-10-05 18:39:20 +0330

    add inventory table

---
+++ includes/database/upgrade/(db_name)/v.2.2.0_pruduct_review.sql
@@ -0,0 +1,90 @@
+
+ALTER TABLE `products` DROP `name`;
+ALTER TABLE `products` DROP `buylock`;
+
+ALTER TABLE `products` CHANGE `code` `quickcode`  varchar(100) NULL DEFAULT NULL;
+ALTER TABLE `products` CHANGE `checkstock` `oversale`  bit(1) NULL DEFAULT NULL;
+
+ALTER TABLE `products` ADD `taxable` bit(1) NULL DEFAULT NULL;
+
+ALTER TABLE `products` ADD `type` enum('product', 'file', 'service') NOT NULL DEFAULT 'product';
+
+ALTER TABLE `products` ADD `seotitle` varchar(300) NULL DEFAULT NULL;
+ALTER TABLE `products` ADD `seodesc` varchar(500) NULL DEFAULT NULL;
+ALTER TABLE `products` ADD `seourl` varchar(1000) NULL DEFAULT NULL;
+
+ALTER TABLE `products` ADD `gallery` text NULL DEFAULT NULL;
+
+ALTER TABLE `products` ADD `weight` float(10) NULL DEFAULT NULL;
+
+ALTER TABLE `products` CHANGE `sold` `sold` float(20) NULL DEFAULT NULL;
+ALTER TABLE `products` CHANGE `stock` `stock` float(20) NULL DEFAULT NULL;
+
+CREATE TABLE `inventory` (
+`id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`store_id` int(10) UNSIGNED NOT NULL,
+`name` varchar(200) NULL DEFAULT NULL,
+`address_id` bigint(20) UNSIGNED NULL,
+`default` bit(1) DEFAULT NULL,
+`online` bit(1) DEFAULT NULL,
+`sale` bit(1) DEFAULT NULL,
+`sort` smallint(3) DEFAULT NULL,
+`status` enum('enable', 'disable', 'deleted') NULL DEFAULT NULL,
+`datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`datemodified` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`),
+CONSTRAINT `inventory_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+INSERT INTO `inventory`
+(
+	`store_id`,
+	`name`,
+	`default`,
+	`sale`,
+	`online`,
+	`status`
+)
+SELECT
+	stores.id,
+	stores.name,
+	1,
+	1,
+	1,
+	'enable'
+FROM stores;
+
+
+
+CREATE TABLE `productinventory` (
+`id`           bigint(10) 	UNSIGNED NOT NULL AUTO_INCREMENT,
+`store_id`     int(10) 		UNSIGNED NOT NULL,
+`inventory_id` bigint(20) 	UNSIGNED NOT NULL,
+`product_id`   int(10) 		UNSIGNED NOT NULL,
+`stock`		   float(20) NULL DEFAULT NULL,
+`sold`		   float(20) NULL DEFAULT NULL,
+PRIMARY KEY (`id`),
+CONSTRAINT `productinventory_inventory_id` FOREIGN KEY (`inventory_id`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `productinventory_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `prudoctinventory_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `productinventory`
+(
+	`product_id`,
+	`store_id`,
+	`inventory_id`,
+	`stock`,
+	`sold`
+)
+SELECT
+	products.id,
+	products.store_id,
+	(SELECT inventory.id FROM inventory WHERE inventory.store_id = products.store_id LIMIT 1),
+	products.stock,
+	products.sold
+FROM products;
