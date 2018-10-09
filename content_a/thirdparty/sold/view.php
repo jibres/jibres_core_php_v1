8955fe16f7cdbfae0818bc8144f93adccdc0d80b
Reza <rm.biqarar@gmail.com>
2018-10-09 19:10:27 +0330

    add sample module to load and route it

---
+++ content_a/thirdparty/sold/view.php
@@ -0,0 +1,22 @@
+<?php
+namespace content_a\thirdparty\glance;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('aThirdPartyGlance');
+		\content_a\thirdparty\load::dataRow();
+
+		\dash\data::page_title(T_('Glance user detail'));
+		\dash\data::page_desc(T_('you can edit general detail of thirdparty'));
+
+		if(\dash\permission::check("aThirdPartyPermissionChange"))
+		{
+			$perm_list = \dash\permission::groups();
+			\dash\data::permGroup(array_keys($perm_list));
+		}
+	}
+}
+?>
