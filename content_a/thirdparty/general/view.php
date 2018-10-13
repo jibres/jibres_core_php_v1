1e276070928d30c8727e923aba9c1e6d26153794
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-13 19:44:03 +0330

    fix general detail

--- content_a/thirdparty/general/view.php
+++ content_a/thirdparty/general/view.php
@@ -9,8 +9,10 @@ class view
 		\content_a\thirdparty\load::dataRow();
 
 		\dash\data::page_title(T_('Edit general information'). \dash\data::page_title());
-		\dash\data::page_desc(T_('you can edit general detail of thirdparty'));
+		\dash\data::page_desc(T_('Edit general detail of this thirdparty.'));
+		\dash\data::page_pictogram('user');
 
+		\content_a\thirdparty\load::fixTitle();
 	}
 }
 ?>
