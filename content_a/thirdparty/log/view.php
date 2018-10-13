042aa2ccdc8e4bbf11bf5672c007a7d94ce3a86c
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-13 19:59:47 +0330

    fix log detai

--- content_a/thirdparty/log/view.php
+++ content_a/thirdparty/log/view.php
@@ -8,9 +8,12 @@ class view
 	{
 		\content_a\thirdparty\load::dataRow();
 
-		\dash\data::page_title(T_('Glance user detail'));
-		\dash\data::page_desc(T_('you can edit general detail of thirdparty'));
+		\dash\data::page_title(T_('Logs'));
+		\dash\data::page_desc(T_('Monitor user logs and actions in your store'));
+		\dash\data::page_pictogram('load-a');
 
+
+		\content_a\thirdparty\load::fixTitle();
 	}
 }
 ?>
