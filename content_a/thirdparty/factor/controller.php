a403b4e4cb62d8eb293eca37c381c47f4b15b931
Reza <rm.biqarar@gmail.com>
2018-10-13 10:04:22 +0330

    fix all thirdparty module

--- content_a/thirdparty/factor/controller.php
+++ content_a/thirdparty/factor/controller.php
@@ -1,82 +1,12 @@
 <?php
-namespace content_a\thirdparty\home;
+namespace content_a\thirdparty\factor;
 
 class controller
 {
 
 	public static function routing()
 	{
-		if(in_array(\dash\request::get('list'), ['mobile']))
-		{
-			$query_string = \dash\request::get('q');
-			if(!$query_string)
-			{
-				$query_string = null;
-			}
 
-			$args                    = [];
-			$args['order']           = 'desc';
-			$args['search_in_other'] = false;
-
-			$query_string = \dash\utility\convert::to_en_number($query_string);
-
-			if(substr($query_string, 0, 1) === '0')
-			{
-				$new_q = '98'. substr($query_string, 1);
-
-				$args['1.1'] = ["= 1.1 ", " AND (userstores.mobile LIKE '%$query_string%' OR userstores.mobile LIKE '$new_q%' )"];
-			}
-			else
-			{
-				$args['1.1'] = ["= 1.1 ", " AND userstores.mobile LIKE '%$query_string%' "];
-			}
-
-			$thirdpartyList       = \lib\app\thirdparty::list($query_string, $args);
-			$result            = [];
-			$result['success'] = true;
-			$result['result']  = [];
-
-			foreach ($thirdpartyList as $key => $value)
-			{
-				$myName = '<img class="ui avatar image" src="'.  $value['avatar'] .'">';
-				$myName .= '   '. $value['firstname']. ' <b>'. $value['lastname'];
-
-				if($value['mobile'])
-				{
-					$myName .= '<span class="description ">'. \dash\utility\human::fitNumber($value['mobile'], false). '</span>';
-				}
-
-				$result['result'][] =
-				[
-					'name'  => $myName,
-					'value' => $value['id'],
-				];
-			}
-
-			if(!$result['result'])
-			{
-				$result['result'][] =
-				[
-					'name'  => '<span class="badge warn">'. T_("Add"). '</span> <span class="badge mLa10 pain">' . $query_string. '</span>',
-					'value' => $query_string,
-				];
-			}
-
-			if(!\dash\utility\filter::mobile($query_string))
-			{
-				$result['result'] =
-				[
-					[
-						'name'  => '<span class="badge txtC danger2">'. T_("Invalid mobile"). '</span>',
-						'value' => 'error',
-					]
-				];
-			}
-
-			$result = json_encode($result, JSON_UNESCAPED_UNICODE);
-			echo $result;
-			\dash\code::boom();
-		}
 	}
 }
 ?>
--- content_a/thirdparty/factor/view.php
+++ content_a/thirdparty/factor/view.php
@@ -1,22 +1,16 @@
 <?php
-namespace content_a\thirdparty\glance;
+namespace content_a\thirdparty\factor;
 
 
 class view
 {
 	public static function config()
 	{
-		\dash\permission::access('aThirdPartyGlance');
 		\content_a\thirdparty\load::dataRow();
 
 		\dash\data::page_title(T_('Glance user detail'));
 		\dash\data::page_desc(T_('you can edit general detail of thirdparty'));
 
-		if(\dash\permission::check("aThirdPartyPermissionChange"))
-		{
-			$perm_list = \dash\permission::groups();
-			\dash\data::permGroup(array_keys($perm_list));
-		}
 	}
 }
 ?>
