06376643d2ac3ea7d29074da84218d43d6edfab7
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-14 17:30:19 +0330

    some fix on third party

--- content_a/thirdparty/comment/view.php
+++ content_a/thirdparty/comment/view.php
@@ -9,12 +9,16 @@ class view
 		\dash\permission::access('aThirdPartyGlance');
 		\content_a\thirdparty\load::dataRow();
 
-		\dash\data::page_title(T_('Glance user detail'));
-		\dash\data::page_desc(T_('you can edit general detail of thirdparty'));
+		\dash\data::page_title(T_('Notes'));
+		\dash\data::page_desc(T_('Add some note for user to archive them and read later.'));
+		\dash\data::page_pictogram('chat-alt-fill');
 
 		$log = \lib\app\thirdparty\comment::list(\dash\request::get('id'));
 		$log = array_reverse($log);
 		\dash\data::logDetail($log);
+
+
+		\content_a\thirdparty\load::fixTitle();
 	}
 }
 ?>
