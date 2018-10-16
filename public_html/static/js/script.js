5d3eeca6f2356659a75b8b8f537a52ef44b0abcc
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-16 11:25:48 +0330

    use js of product inside page

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -8,7 +8,6 @@ function pushState()
   // });
   runRunner();
   calcFooterValues();
-  recalcPricePercents();
   simplePrint();
   typeTitles();
 }
@@ -22,131 +21,6 @@ $(function()
 });
 
 
-
-
-
-function recalcPricePercents()
-{
-  if((window.location.pathname).indexOf('/a/product') < 0 || $('#price').length === 0)
-  {
-    return;
-  }
-
-  // declare variables
-  var elFinalPriceBox = $('#finalprice').parent().parent();
-  var buy             = parseInt($('#buyprice').val().toEnglish());
-  var sale            = parseInt($('#price').val().toEnglish());
-  var discount        = parseInt($('#discount').val().toEnglish());
-  var finalPrice      = 0;
-
-  var impureIntrestRate = 0;
-  var pureIntrestRate   = 0;
-  var discountRate      = 0;
-  // check and set NAN value
-  if(isNaN(buy))
-  {
-    buy = 0;
-  }
-  if(isNaN(sale))
-  {
-    sale = 0;
-  }
-  if(isNaN(discount))
-  {
-    discount = 0;
-  }
-
-  // impureIntrestRate
-  if(buy && sale)
-  {
-    impureIntrestRate = ((sale * 100 / buy) - 100).toFixed(2);
-    pureIntrestRate   = (((sale - discount) * 100 / buy) - 100).toFixed(2);
-  }
-  $('#price').parent().find('.addon').text(fitNumber(impureIntrestRate) + '%');
-
-  // Discount Rate
-  if(discount && sale)
-  {
-    discountRate = (discount * 100 / sale).toFixed(2);
-  }
-  $('#discount').parent().find('.addon').text(fitNumber(discountRate) + '%');
-
-  // final price
-  finalPrice = sale - discount;
-  $('#finalprice').val(finalPrice);
-  elFinalPriceBox.find('.addon').text(fitNumber(pureIntrestRate) + '%');
-  if(sale)
-  {
-    elFinalPriceBox.removeClass('hide').hide().slideDown();
-  }
-  else
-  {
-    // elFinalPriceBox.slideUp();
-  }
-
-  // conditional check
-  var elPrice      = $('#price').parents('.input');
-  var elDiscount   = $('#discount').parents('.input');
-  var elFinalPrice = $('#finalprice').parents('.input');
-
-  // all check for price
-  // if price is not normal and under buy price, impure under zero
-  if(impureIntrestRate < 0)
-  {
-    elPrice.addClass('warning');
-  }
-  else
-  {
-    elPrice.removeClass('warning');
-  }
-  // if discount is more than 100% of sale price
-  if(discount === 0)
-  {
-    elDiscount.removeClass('error');
-    elDiscount.removeClass('warning');
-  }
-  else if(discountRate > 100)
-  {
-    elDiscount.addClass('error');
-    elDiscount.removeClass('warning');
-  }
-  else if(sale - discount < buy)
-  {
-    elDiscount.removeClass('error');
-    elDiscount.addClass('warning');
-  }
-  else
-  {
-    elDiscount.removeClass('error');
-    elDiscount.removeClass('warning');
-  }
-
-  // all check for final price
-  if(finalPrice === 0)
-  {
-    elFinalPrice.removeClass('error');
-    elFinalPrice.removeClass('warning');
-    elFinalPrice.removeClass('ok');
-  }
-  else if(finalPrice < 0)
-  {
-    elFinalPrice.addClass('error');
-    elFinalPrice.removeClass('warning');
-    elFinalPrice.removeClass('ok');
-  }
-  else if(finalPrice < buy)
-  {
-    elFinalPrice.removeClass('error');
-    elFinalPrice.addClass('warning');
-    elFinalPrice.removeClass('ok');
-  }
-  else
-  {
-    elFinalPrice.removeClass('error');
-    elFinalPrice.removeClass('warning');
-    elFinalPrice.addClass('ok');
-  }
-}
 
 
 function runRunner()
@@ -860,7 +734,6 @@ function shortkey_toggleDiscount(_status)
   });
   // recalc values
   calcFooterValues();
-  recalcPricePercents();
 }
 
 
