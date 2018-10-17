ecc7aefa4d92976d23fb631cefdc51fce3ab278f
Reza <rm.biqarar@gmail.com>
2018-10-17 10:54:17 +0330

    add minus moduel

---
+++ content_a/thirdparty/minustransaction/view.php
@@ -0,0 +1,19 @@
+<?php
+namespace content_a\thirdparty\minustransaction;
+
+
+class view
+{
+	public static function config()
+	{
+		\content_a\thirdparty\load::dataRow();
+
+		\dash\data::page_title(T_('Transactions'));
+		\dash\data::page_desc(T_('Check all transactions include sales, purchases and money send and recieve.'));
+		\dash\data::page_pictogram('balance-scale');
+
+		\content_a\thirdparty\load::fixTitle();
+
+	}
+}
+?>
