4c020e1e15a770238bc34e1532608192024ca870
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 18:10:51 +0330

    fix warehouse

--- content_a/product/warehouse/view.php
+++ content_a/product/warehouse/view.php
@@ -7,7 +7,8 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Warehoust setting') . ' | '. \dash\data::dataRow_title());
-		\dash\data::page_desc(T_('Manage warehouse setting of product like name, category, price and etc.') .' '. T_('You can change another setting by choose another type of setting.'));
+		\dash\data::page_desc(T_('Manage warehouse setting of this product like count in each warehouse and some extra detail.'));
+		\dash\data::page_pictogram('internet');
 
 
 		\dash\data::badge_text(T_('Back to product list'));
