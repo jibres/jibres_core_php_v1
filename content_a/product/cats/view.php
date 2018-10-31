1fe73cd1931c66ed63340fda180debc4709176e4
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 19:37:37 +0330

    some fix on units

--- content_a/product/cats/view.php
+++ content_a/product/cats/view.php
@@ -5,7 +5,7 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_('Product categories'). ' | '. \dash\data::store_name());
+		\dash\data::page_title(T_('Product categories'));
 		\dash\data::page_desc(T_('You can manage your categories manually.'). ' '. T_("Don't worry! we are add categories automatically on add new product"));
 		\dash\data::page_pictogram('grid-1');
 
--- content_a/product/units/view.php
+++ content_a/product/units/view.php
@@ -5,9 +5,12 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_('Units Setting'). ' | '. \dash\data::store_name());
-		\dash\data::page_desc(T_('Check unit setting'));
+		\dash\data::page_title(T_('Unit of products'));
+		\dash\data::page_desc(T_('You can manage your units manually.'). ' '. T_("Don't worry! we are add units automatically on add new product"));
+		\dash\data::page_pictogram('eye-galsses');
 
+		\dash\data::badge_text(T_('Back to product list'));
+		\dash\data::badge_link(\dash\url::this());
 	}
 }
 ?>
