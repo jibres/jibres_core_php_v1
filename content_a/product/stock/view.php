040493a2f469d88ce3c019b4856278a1b3ea3779
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 18:20:28 +0330

    remove unused product module

--- content_a/product/stock/view.php
+++ content_a/product/stock/view.php
@@ -6,9 +6,9 @@ class view
 {
 	public static function config()
 	{
-
-		\dash\data::page_title(T_('Stock Product'). ' | '. \dash\data::dataRow_title());
-		\dash\data::page_desc(T_('You can check stock of product'));
+		\dash\data::page_title(T_('Stock settings'). ' | '. \dash\data::dataRow_title());
+		\dash\data::page_desc(T_('Manage warehouse setting of this product like count in each warehouse and some extra detail.'));
+		\dash\data::page_pictogram('internet');
 
 		\dash\data::badge_text(T_('Back to product list'));
 		\dash\data::badge_link(\dash\url::this());
