d21d8a46e7e024516e45aa323a7b0600cace3e25
Reza <rm.biqarar@gmail.com>
2018-10-31 18:08:37 +0330

    fix list of factor

--- content_a/chap/receipt/view.php
+++ content_a/chap/receipt/view.php
@@ -9,13 +9,34 @@ class view
 		\dash\data::page_title(T_('Print factor'));
 		\dash\data::page_desc(T_('You can search in list of factors, add new factor and edit existing.'));
 
-		\dash\data::badge_text(T_('Back to last sales'));
-		\dash\data::badge_link(\dash\url::this());
+		\dash\data::badge_text(T_('Back to dashboard'));
+		\dash\data::badge_link(\dash\url::here());
+
 
 
 		\dash\data::pageSize(\dash\request::get('size'));
 
-		\dash\data::factorDetail(\lib\app\factor::get(['id' => \dash\request::get('id')], []));
+		$id = \dash\request::get('id');
+		$factorDetail = \lib\app\factor::get(['id' => $id]);
+		\dash\data::factorDetail($factorDetail);
+
+		if(isset($factorDetail['factor']['type']))
+		{
+			switch ($factorDetail['factor']['type'])
+			{
+				case 'buy':
+					\dash\data::badge_text(T_('Back to last buy'));
+					\dash\data::badge_link(\dash\url::here(). '/factor?type=buy');
+				case 'sale':
+					\dash\data::badge_text(T_('Back to last sales'));
+					\dash\data::badge_link(\dash\url::here(). '/factor?type=sale');
+					break;
+
+				default:
+					# code...
+					break;
+			}
+		}
 	}
 }
 ?>
