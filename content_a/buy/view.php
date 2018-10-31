b8e3c079e05952b3faab1b3d265bc4463ae3c075
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 20:53:28 +0330

    fix on bug

--- content_a/buy/view.php
+++ content_a/buy/view.php
@@ -8,8 +8,10 @@ class view
 	{
 		\dash\permission::access('aFactorAdd');
 
-		\dash\data::page_title(T_('Sale invoicing'));
-		// \dash\data::page_desc(T_('Sale your product via Jibres and enjoy using integrated web base platform.'));
+		\dash\data::page_title(T_('Buy invoicing'));
+		\dash\data::page_desc(T_('Add buy from supplier will update stock and price of products automatically.'));
+		\dash\data::page_pictogram('buy-sign');
+
 		\dash\data::badge_text(T_('Back to last sales'));
 		\dash\data::badge_link(\dash\url::here(). '/factor?type=buy');
 	}
--- content_a/sale/view.php
+++ content_a/sale/view.php
@@ -10,6 +10,8 @@ class view
 
 		\dash\data::page_title(T_('Sale invoicing'));
 		// \dash\data::page_desc(T_('Sale your product via Jibres and enjoy using integrated web base platform.'));
+		\dash\data::page_pictogram('cart-shopping-1');
+
 		\dash\data::badge_text(T_('Back to last sales'));
 		\dash\data::badge_link(\dash\url::here(). '/factor?type=sale');
 	}
