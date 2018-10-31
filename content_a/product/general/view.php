b10b757058d50dd9c6b3f41d7e0c6dbca027fc67
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 16:58:16 +0330

    fix product

--- content_a/product/general/view.php
+++ content_a/product/general/view.php
@@ -8,6 +8,7 @@ class view
 	{
 		\dash\data::page_title(T_('General setting'). ' | ' . \dash\data::dataRow_title());
 		\dash\data::page_desc(T_('Manage general setting of product like name, category, price and etc.') .' '. T_('You can change another setting by choose another type of setting.'));
+		\dash\data::page_pictogram('tag-2');
 
 		\dash\data::badge_text(T_('Back to product list'));
 		\dash\data::badge_link(\dash\url::this());
