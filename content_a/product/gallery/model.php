700fd67e0c13f32a2a97e2f35e270e27d633b0dc
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 18:31:15 +0330

    fix gallery

--- content_a/product/gallery/model.php
+++ content_a/product/gallery/model.php
@@ -61,5 +61,34 @@ class model
 	}
 
 
+	// @reza
+	// check
+	public static function postThumb()
+	{
+		if(\dash\request::post('delete') === 'thumb')
+		{
+			\lib\app\product\file::thumb(null, \dash\request::get('id'));
+			\dash\notif::warn(T_("File removed"));
+		}
+		else
+		{
+			$file_url = \dash\app\file::upload_quick('thumb');
+			// we have an error in upload thumb
+			if(!$file_url)
+			{
+				\dash\notif::warn(T_("No file sended"));
+				return false;
+			}
+
+			\lib\app\product\file::thumb($file_url, \dash\request::get('id'));
+			\dash\notif::ok(T_("File successfully uploaded"));
+		}
+
+		if(\dash\engine\process::status())
+		{
+			\dash\redirect::pwd();
+		}
+	}
+
 }
 ?>
--- content_a/product/gallery/view.php
+++ content_a/product/gallery/view.php
@@ -7,6 +7,9 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Gallery product!'). ' | '. \dash\data::dataRow_title());
+		\dash\data::page_desc(T_('Set image of product to show as gallery also you can set main photo for this product as thumbnail.'));
+		\dash\data::page_pictogram('images');
+
 
 		\dash\data::badge_text(T_('Back to product list'));
 		\dash\data::badge_link(\dash\url::this());
