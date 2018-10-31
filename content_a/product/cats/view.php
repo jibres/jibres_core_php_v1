d855846b0ac417892e82cadb09d27f512d3a0c31
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-10-31 19:08:48 +0330

    fix product cats

--- content_a/product/cats/view.php
+++ content_a/product/cats/view.php
@@ -5,9 +5,12 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_('Cat Setting'). ' | '. \dash\data::store_name());
-		\dash\data::page_desc(T_('Check unit setting'));
+		\dash\data::page_title(T_('Product categories'). ' | '. \dash\data::store_name());
+		\dash\data::page_desc(T_('You can manage your categories manually.'). ' '. T_("Don't worry! we are add categories automatically on add new product"));
+		\dash\data::page_pictogram('grid-1');
 
+		\dash\data::badge_text(T_('Back to product list'));
+		\dash\data::badge_link(\dash\url::this());
 	}
 }
 ?>
