ea7b01a950ebd17df7b07f06de17d84b472c5831
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-03 20:15:36 +0330

    fix daily report

--- content_a/home/chart.js
+++ content_a/home/chart.js
@@ -8,117 +8,117 @@ function chartDrawer()
 function highChart()
 {
 
-Highcharts.chart('chartdiv',
-{
-  chart: {
-    zoomType: 'x',
-    style: {
-      fontFamily: 'IRANSans, Tahoma, sans-serif'
-    }
-  },
-  title: {
-    text: '{%trans "Sum factor price and count of it group by hours"%}'
-  },
-  xAxis: [{
-    categories: {{dashboardData.chart.categories | raw}},
-    crosshair: true
-  }],
-  yAxis: [{ // Primary yAxis
-    labels: {
-      format: '{value}',
+  Highcharts.chart('chartdiv',
+  {
+    chart: {
+      zoomType: 'x',
       style: {
-        color: Highcharts.getOptions().colors[0]
+        fontFamily: 'IRANSans, Tahoma, sans-serif'
       }
     },
     title: {
-      text: '{%trans "Sum price"%}',
-      useHTML: Highcharts.hasBidiBug,
-      style: {
-        color: Highcharts.getOptions().colors[0]
-      }
-    }
-  },
-  { // Secondary yAxis
-    title: {
-      text: '{%trans "Count"%}',
-      useHTML: Highcharts.hasBidiBug,
-      style: {
-        color: Highcharts.getOptions().colors[1]
-      }
+      text: '{%trans "Sum factor price and count of it group by hours"%}'
     },
-    labels: {
-      format: '{value}',
-      style: {
-        color: Highcharts.getOptions().colors[1]
-      }
-    },
-    opposite: true
-  }],
-  tooltip: {
-    useHTML: true,
-    borderWidth: 0,
-    shared: true
-  },
-  exporting:
-  {
-    buttons:
-    {
-      contextButton:
-      {
-        menuItems:
-        [
-         'printChart',
-         'separator',
-         'downloadPNG',
-         'downloadJPEG',
-         'downloadSVG'
-        ]
-      }
-    }
-  },
-  legend: {
-    layout: 'vertical',
-    align: 'left',
-    x: 120,
-    verticalAlign: 'top',
-    y: 100,
-    floating: true,
-    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
-  },
-  credits:
-  {
-      text: '{{service.title}}',
-      href: '{{service.url}}',
-      position:
-      {
-          x: -35,
-          y: -7
+    xAxis: [{
+      categories: {{dashboardData.chart.categories | raw}},
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       },
-      style: {
-          fontWeight: 'bold'
+      title: {
+        text: '{%trans "Sum price"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       }
-  },
-  series: [
-  {
-    name: '{%trans "Sum price"%}',
-    type: 'column',
-    data: {{dashboardData.chart.sum | raw}},
+    },
+    { // Secondary yAxis
+      title: {
+        text: '{%trans "Count"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      opposite: true
+    }],
     tooltip: {
-      valueSuffix: ' {%trans "Toman"%}'
-    }
+      useHTML: true,
+      borderWidth: 0,
+      shared: true
+    },
+    exporting:
+    {
+      buttons:
+      {
+        contextButton:
+        {
+          menuItems:
+          [
+           'printChart',
+           'separator',
+           'downloadPNG',
+           'downloadJPEG',
+           'downloadSVG'
+          ]
+        }
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 120,
+      verticalAlign: 'top',
+      y: 100,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+    },
+    credits:
+    {
+        text: '{{service.title}}',
+        href: '{{service.url}}',
+        position:
+        {
+            x: -35,
+            y: -7
+        },
+        style: {
+            fontWeight: 'bold'
+        }
+    },
+    series: [
+    {
+      name: '{%trans "Sum price"%}',
+      type: 'column',
+      data: {{dashboardData.chart.sum | raw}},
+      tooltip: {
+        valueSuffix: ' {%trans "Toman"%}'
+      }
 
-  },
-  {
-    name: '{%trans "Count"%}',
-    type: 'spline',
-    yAxis: 1,
-    data: {{dashboardData.chart.count | raw}},
-    tooltip: {
-      valueSuffix: ' {%trans "Count"%}'
+    },
+    {
+      name: '{%trans "Count"%}',
+      type: 'spline',
+      yAxis: 1,
+      data: {{dashboardData.chart.count | raw}},
+      tooltip: {
+        valueSuffix: ' {%trans "Count"%}'
+      }
     }
-  }
-  ]
-}, function(_chart)
+    ]
+  }, function(_chart)
   {
     _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
   });
--- content_a/report/daily/chart.js
+++ content_a/report/daily/chart.js
@@ -8,77 +8,93 @@ function chartDrawer()
 function highChart()
 {
 
-Highcharts.chart('chartdiv',
-{
-  chart: {
-    zoomType: 'x',
-    style: {
-      fontFamily: 'IRANSans, Tahoma, sans-serif'
-    }
-  },
-  title: {
-    text: '{%trans "Daily report"%}'
-  },
-  xAxis: [{
-    categories: {{ReportDailyChart.categories | raw}},
-    crosshair: true
-  }],
-  yAxis: [{ // Primary yAxis
-    labels: {
-      format: '{value}',
+  Highcharts.chart('chartdiv',
+  {
+    chart: {
+      zoomType: 'x',
       style: {
-        color: Highcharts.getOptions().colors[0]
+        fontFamily: 'IRANSans, Tahoma, sans-serif'
       }
     },
     title: {
-      text: '{%trans "Sum price"%}',
-      useHTML: Highcharts.hasBidiBug,
-      style: {
-        color: Highcharts.getOptions().colors[0]
+      text: '{%trans "Daily report"%}'
+    },
+    xAxis: [{
+      categories: {{ReportDailyChart.categories | raw}},
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
+      },
+      title: {
+        text: '{%trans "Sum price"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       }
-    }
-  }],
-  tooltip: {
-    useHTML: true,
-    borderWidth: 0,
-    shared: true
-  },
-  exporting:
-  {
-    buttons:
-    {
-      contextButton:
-      {
-        menuItems:
-        [
-         'printChart',
-         'separator',
-         'downloadPNG',
-         'downloadJPEG',
-         'downloadSVG'
-        ]
-      }
-    }
-  },
-  legend: {
-    layout: 'vertical',
-    align: 'left',
-    x: 120,
-    verticalAlign: 'top',
-    y: 100,
-    floating: true,
-    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
-  },
-  series: [
-  {
-    name: '{%trans "Sum price"%}',
-    type: 'column',
-    data: {{ReportDailyChart.value | raw}},
+    }],
     tooltip: {
-      valueSuffix: ' {%trans "Toman"%}'
-    }
+      useHTML: true,
+      borderWidth: 0,
+      shared: true
+    },
+    exporting:
+    {
+      buttons:
+      {
+        contextButton:
+        {
+          menuItems:
+          [
+           'printChart',
+           'separator',
+           'downloadPNG',
+           'downloadJPEG',
+           'downloadSVG'
+          ]
+        }
+      }
+    },
+    credits:
+    {
+        text: '{{service.title}}',
+        href: '{{service.url}}',
+        position:
+        {
+            x: -35,
+            y: -7
+        },
+        style: {
+            fontWeight: 'bold'
+        }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 120,
+      verticalAlign: 'top',
+      y: 100,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+    },
+    series: [
+    {
+      name: '{%trans "Sum price"%}',
+      type: 'column',
+      data: {{ReportDailyChart.value | raw}},
+      tooltip: {
+        valueSuffix: ' {%trans "Toman"%}'
+      }
 
-  }
-  ]
-});
+    }
+    ]
+  }, function(_chart)
+  {
+    _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
+  });
 }
