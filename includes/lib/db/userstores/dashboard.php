b58e2b3a554379f6b57034763415fdbf660e7d65
Reza <rm.biqarar@gmail.com>
2018-11-06 11:16:51 +0330

    add userstore dashboard function

---
+++ includes/lib/db/userstores/dashboard.php
@@ -0,0 +1,36 @@
+<?php
+namespace lib\db\userstores;
+
+class dashboard
+{
+	public static function lastactivity($_userstore_id)
+	{
+
+	}
+
+	public static function staff_sale($_staff_id, $_costomer_id, $_factor_id)
+	{
+
+	}
+
+	public static function staff_buy($_staff_id, $_supplier_id, $_factor_id)
+	{
+
+	}
+
+	public static function staff_buy_pay($_staff_id, $_factor_id)
+	{
+
+	}
+
+	public static function customer_pay($_costomer_id, $_factor_id)
+	{
+
+	}
+
+	public static function supplier_pay($_supplier_id, $_factor_id)
+	{
+
+	}
+}
+?>
