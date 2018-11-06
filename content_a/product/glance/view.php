fd3a18bc6623731e6c5c89db33fc6990f06aaac7
Reza <rm.biqarar@gmail.com>
2018-11-06 20:09:41 +0330

    fix price change history class

--- content_a/product/glance/view.php
+++ content_a/product/glance/view.php
@@ -12,7 +12,7 @@ class view
 
 		\content_a\product\load::fixTitle();
 
-		$dashboard = \lib\app\productprice\dashboard::glance(\dash\request::get('id'));
+		$dashboard = \lib\app\productprice::dashboard(\dash\request::get('id'));
 
 		\dash\data::dashboardData($dashboard);
 	}
