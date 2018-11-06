b393b96e3f758a883e3a7940f6f91596a5efdc78
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-06 17:34:06 +0330

    improve chart

--- content_a/product/pricechange/chart.js
+++ content_a/product/pricechange/chart.js
@@ -1,4 +1,7 @@
-if($("#chartdiv").length == 1){myChartProductPrice();}
+function chartDrawer()
+{
+  if($("#chartdiv").length == 1){myChartProductPrice();}
+}
 
 
 
@@ -8,42 +11,100 @@ if($("#chartdiv").length == 1){myChartProductPrice();}
 function myChartProductPrice()
 {
 
-Highcharts.chart('chartdiv', {
-
-  title: {
-    text: '{%trans "Price change in time line"%}'
-  },
-   xAxis: [{
-      categories: {{cahrtDetail.categories | raw}},
-      crosshair: true
+  Highcharts.chart('chartdiv',
+  {
+    chart:
+    {
+      zoomType: 'x',
+      style:
+      {
+        fontFamily: 'IRANSans, Tahoma, sans-serif'
+      }
+    },
+    title:
+    {
+      text: '{%trans "Price change in time line"%}'
+    },
+     xAxis:
+    [{
+        categories: {{cahrtDetail.categories | raw}},
+        crosshair: true
     }],
-  yAxis: {
-    title: {
-      text: '{%trans "Price"%}'
-    }
-  },
-  legend: {
-    layout: 'vertical',
-    align: 'right',
-    verticalAlign: 'middle'
-  },
-
-  series: {{cahrtDetail.data | raw}},
-
-  responsive: {
-    rules: [{
-      condition: {
-        maxWidth: 500
+    credits:
+    {
+        text: '{{service.title}}',
+        href: '{{service.url}}',
+        position:
+        {
+            x: -35,
+            y: -7
+        },
+        style: {
+            fontWeight: 'bold'
+        }
+    },
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
       },
-      chartOptions: {
-        legend: {
-          layout: 'horizontal',
-          align: 'center',
-          verticalAlign: 'bottom'
+      title: {
+        text: '{%trans "Price"%}',
+        useHTML: Highcharts.hasBidiBug,
+        style: {
+          color: Highcharts.getOptions().colors[0]
         }
       }
-    }]
-  }
+    }],
+    tooltip: {
+      useHTML: true,
+      borderWidth: 0,
+      shared: true
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
+    },
+    exporting:
+    {
+      buttons:
+      {
+        contextButton:
+        {
+          menuItems:
+          [
+           'printChart',
+           'separator',
+           'downloadPNG',
+           'downloadJPEG',
+           'downloadSVG'
+          ]
+        }
+      }
+    },
+    series: {{cahrtDetail.data | raw}},
 
-});
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          legend: {
+            layout: 'horizontal',
+            align: 'center',
+            verticalAlign: 'bottom'
+          }
+        }
+      }]
+    }
+
+  }, function(_chart)
+  {
+    _chart.renderer.image('{{service.logo}}', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
+  });
 }
