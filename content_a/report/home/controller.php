2197adee0daf7693da762fc76dc670c61941dcf3
Reza <rm.biqarar@gmail.com>
2018-11-07 16:03:28 +0330

    not redirect on report

--- content_a/report/home/controller.php
+++ content_a/report/home/controller.php
@@ -8,36 +8,36 @@ class controller
 	{
 		\dash\permission::access('reportView');
 
-		$redirect_enter = false;
-		if(\dash\user::detail('logintime'))
-		{
-			if((time() - intval(\dash\user::detail('logintime')) ) > (60*10))
-			{
-				if(!isset($_SESSION['try_load_report']))
-				{
-					$_SESSION['try_load_report'] = 1;
-					\dash\notif::warn(T_("You must login again to view this page"));
-					\dash\header::status(403, ' ');
-				}
-				else
-				{
-					$redirect_enter = true;
-				}
-			}
-			else
-			{
-				$redirect_enter = false;
-			}
-		}
-		else
-		{
-			$redirect_enter = true;
-		}
+		// $redirect_enter = false;
+		// if(\dash\user::detail('logintime'))
+		// {
+		// 	if((time() - intval(\dash\user::detail('logintime')) ) > (60*10))
+		// 	{
+		// 		if(!isset($_SESSION['try_load_report']))
+		// 		{
+		// 			$_SESSION['try_load_report'] = 1;
+		// 			\dash\notif::warn(T_("You must login again to view this page"));
+		// 			\dash\header::status(403, ' ');
+		// 		}
+		// 		else
+		// 		{
+		// 			$redirect_enter = true;
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		$redirect_enter = false;
+		// 	}
+		// }
+		// else
+		// {
+		// 	$redirect_enter = true;
+		// }
 
-		if($redirect_enter)
-		{
-			\dash\redirect::to(\dash\url::kingdom(). '/logout?mobile='. \dash\user::detail('mobile'). '&referer='. \dash\url::pwd());
-		}
+		// if($redirect_enter)
+		// {
+		// 	\dash\redirect::to(\dash\url::kingdom(). '/logout?mobile='. \dash\user::detail('mobile'). '&referer='. \dash\url::pwd());
+		// }
 	}
 }
 ?>
