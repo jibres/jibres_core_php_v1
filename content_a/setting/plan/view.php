f3aa56b681593b6b578c4d4a5ade42d598048cde
Reza <rm.biqarar@gmail.com>
2018-11-07 18:34:05 +0330

    fix plan

--- content_a/setting/plan/view.php
+++ content_a/setting/plan/view.php
@@ -5,7 +5,6 @@ class view
 {
 	public static function config()
 	{
-		// \dash\data::currentPlan(\lib\db\storeplans::current(\lib\store::id()));
 
 		\dash\data::page_title(T_('Setting | '). T_('Change Plan of :name', ['name'=>\dash\data::store_name()]));
 		\dash\data::page_desc(T_('By choose new plan, we generate your invoice until now and next invoice is created one month later exactly at this time and you can pay it from billing.'));
