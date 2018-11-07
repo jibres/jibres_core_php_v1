0353e6c9c649e752d19d229a064803ab072ea676
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-07 13:57:51 +0330

    fix product pagetitle

--- content_a/product/desc/view.php
+++ content_a/product/desc/view.php
@@ -7,6 +7,8 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Description product!'). ' | '. \dash\data::dataRow_title());
+		\dash\data::page_desc(T_('This description is show on the website page of this product'));
+		\dash\data::page_pictogram('file-text-o');
 
 		\dash\data::badge_text(T_('Back to product list'));
 		\dash\data::badge_link(\dash\url::this());
--- content_a/product/pricehistory/view.php
+++ content_a/product/pricehistory/view.php
@@ -7,6 +7,8 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Price change chart!'). ' | '. \dash\data::dataRow_title());
+		\dash\data::page_desc(T_('Check price change of this product like buy, sale and profit.'));
+		\dash\data::page_pictogram('chart-line');
 
 		\dash\data::badge_text(T_('Back to product list'));
 		\dash\data::badge_link(\dash\url::this());
