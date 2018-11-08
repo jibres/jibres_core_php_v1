58958ab8edbe72abd1c4ba19ed1b118952fbb72e
Reza <rm.biqarar@gmail.com>
2018-11-08 10:54:37 +0330

    check permission in buy and sale page

--- content_a/buy/view.php
+++ content_a/buy/view.php
@@ -6,7 +6,6 @@ class view
 {
 	public static function config()
 	{
-
 		\dash\data::page_title(T_('Buy invoicing'));
 		\dash\data::page_desc(T_('Add buy from supplier will update stock and price of products automatically.'));
 		\dash\data::page_pictogram('buy-sign');
