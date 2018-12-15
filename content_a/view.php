bc72d0a537ba442b51c87f39b61602a1991dd5c1
Reza <rm.biqarar@gmail.com>
2018-12-15 16:46:06 +0330

    fix use static

--- content_a/view.php
+++ content_a/view.php
@@ -13,7 +13,7 @@ class view
 		\dash\data::service_title(T_("Jibres"));
 		\dash\data::service_desc(T_("Jibres is not just an online accounting software;"). ' <br> '.  T_("We try to create the best financial platform that has everything you need to sale and manage your financial life."));
 		\dash\data::service_slogan(T_("Integrated Sales and Online Accounting"));
-		\dash\data::service_logo(\dash\url::site(). '/static/siftal/images/logo/jibres.png');
+		\dash\data::service_logo(\dash\url::static(). '/siftal/images/logo/jibres.png');
 		\dash\data::service_url('https://jibres.com');
 
 		// transfer to new location on root of content
