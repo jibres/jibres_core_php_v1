6f8c2debfc42d1ea8ba04cbbd398b373c0d26aa2
Reza <rm.biqarar@gmail.com>
2018-12-27 18:56:46 +0330

    user master ticket

--- content/contact/view.php
+++ content/contact/view.php
@@ -2,12 +2,13 @@
 namespace content\contact;
 
 
-class view
+class view extends \content_support\ticket\contact_ticket\view
 {
 	public static function config()
 	{
 		\dash\data::page_title(T_('Contact Us'));
 		\dash\data::page_desc(T_('Knowing your valuable comments about bugs and problems and more importantly your precious offers will help us in this way.'));
+		self::codeurl();
 	}
 }
 ?>
