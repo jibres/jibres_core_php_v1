2a00edaad46a05179bfb1e5e7f9be53f0b3c6b3c
Reza <rm.biqarar@gmail.com>
2019-01-16 16:39:40 +0330

    show plan history list

--- content_a/setting/plan/view.php
+++ content_a/setting/plan/view.php
@@ -7,6 +7,10 @@ class view
 	{
 		\dash\data::page_title(T_('Setting | '). T_('Change Plan of :name', ['name'=>\dash\data::store_name()]));
 		\dash\data::page_desc(T_('By choose new plan, we generate your invoice until now and next invoice is created one month later exactly at this time and you can pay it from billing.'));
+
+		$plan_history = \lib\app\planhistory::list();
+		\dash\data::dataTable($plan_history);
+
 	}
 }
 ?>
