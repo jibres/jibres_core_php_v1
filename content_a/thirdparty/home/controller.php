47f38253f7b8288dac6456313f62b61aa4114bc9
Reza <rm.biqarar@gmail.com>
2019-01-17 11:24:56 +0330

    fix load supplier in buy

--- content_a/thirdparty/home/controller.php
+++ content_a/thirdparty/home/controller.php
@@ -11,10 +11,10 @@ class controller
 			$notif_result = [];
 			$result       = [];
 
-			$meta                        = [];
-			$meta['userstores.supplier'] = ["IS", "NULL"];
-			$meta['sort_type']           = \dash\request::get('type');
-			$resultRaw                   = \lib\app\thirdparty::list(\dash\request::get('q'), $meta);
+			$meta              = [];
+			// $list              = \dash\request::get('list'); // staff, customer, supplier
+			$meta['sort_type'] = \dash\request::get('type'); // buy, sale
+			$resultRaw         = \lib\app\thirdparty::list(\dash\request::get('q'), $meta);
 
 			foreach ($resultRaw as $key => $value)
 			{
