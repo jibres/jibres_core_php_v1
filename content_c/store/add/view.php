7112c7a48e8d145b64cf2813bfd6f7a3611046aa
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-09 20:07:29 +0330

    add term link into create new account

--- content_c/store/add/view.php
+++ content_c/store/add/view.php
@@ -8,6 +8,9 @@ class view
 	{
 		\dash\data::page_title(T_("Add New Store"));
 		\dash\data::page_desc(T_("Add with simple detail and config more after adding new store."));
+
+		$termLink = '<a href="'. \dash\url::kingdom(). '/terms" target="_blank">'. T_('terms of service') .'</a>';
+		\dash\data::termOfService(T_("By clicking below to signup, you're agreeing to our :term.", ['term' => $termLink]));
 	}
 }
 ?>
