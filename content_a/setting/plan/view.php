29a126ae03b1ccf843e42354e3c9f2fe37937e28
Reza <rm.biqarar@gmail.com>
2019-02-11 13:57:23 +0330

    choose plan

--- content_a/setting/plan/view.php
+++ content_a/setting/plan/view.php
@@ -8,8 +8,27 @@ class view
 		\dash\data::page_title(T_('Setting | '). T_('Change Plan of :name', ['name'=>\dash\data::store_name()]));
 		\dash\data::page_desc(T_('By choose new plan, we generate your invoice until now and next invoice is created one month later exactly at this time and you can pay it from billing.'));
 
-		// $plan_history = \lib\app\planhistory::list();
-		// \dash\data::dataTable($plan_history);
+		$subchild = \dash\url::subchild();
+		switch ($subchild)
+		{
+			case 'choose':
+				\dash\data::myPlanDisplay('content_a/setting/plan/choosePlan.html');
+				break;
+
+			case 'start':
+			case 'standard':
+			case 'simple':
+				\dash\data::myPlanDisplay('content_a/setting/plan/choosePeriod.html');
+				break;
+
+
+			default:
+				\dash\data::myPlanDisplay('content_a/setting/plan/currentPlanDetail.html');
+				$plan_history = \lib\app\planhistory::list();
+				\dash\data::dataTable($plan_history);
+				break;
+		}
+
 
 	}
 }
