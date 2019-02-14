835d7b246e5df345380d83e0d3c7d57013d52ac8
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-14 13:41:06 +0330

    try another one

--- public_html/static/js/src/gulpfile.js
+++ public_html/static/js/src/gulpfile.js
@@ -1,3 +1,5 @@
+const { src, dest, parallel } = require('gulp');
+
 const gulp          = require('gulp');
 const gp_concat     = require('gulp-concat');
 const gp_rename     = require('gulp-rename');
@@ -5,16 +7,26 @@ const gp_uglify     = require('gulp-uglify');
 const gp_sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('js-do', function()
+function js()
 {
-  return gulp.src('src/*.js')
-    .pipe(gp_sourcemaps.init())
-    .pipe(gp_concat('concat.js'))
-    .pipe(gulp.dest('dist'))
+  return src('src/*.js', { sourcemaps: true })
+    .pipe(gp_concat('app.min.js'))
+    .pipe(dest('build/js', { sourcemaps: true }))
     .pipe(gp_rename('uglify.js'))
     .pipe(gp_uglify())
-    .pipe(gp_sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'))
 
-gulp.task('default', ['js-do'], function(){});
+    // .pipe(gp_sourcemaps.init())
+    // .pipe(gp_concat('concat.js'))
+    // .pipe(gulp.dest('dist'))
+    // .pipe(gp_rename('uglify.js'))
+    // .pipe(gp_uglify())
+    // .pipe(gp_sourcemaps.write('./'))
+    // .pipe(gulp.dest('dist'));
+}
+
+
+exports.js = js;
+
+exports.default = parallel(js);
+
