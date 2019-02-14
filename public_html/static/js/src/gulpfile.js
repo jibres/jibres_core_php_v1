f5855e9adbc3c5b3b7e094f7de06be978a1b3596
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-14 13:36:06 +0330

    try another

--- public_html/static/js/src/gulpfile.js
+++ public_html/static/js/src/gulpfile.js
@@ -1,23 +1,20 @@
-const { src, dest, parallel } = require('gulp');
+const gulp          = require('gulp');
+const gp_concat     = require('gulp-concat');
+const gp_rename     = require('gulp-rename');
+const gp_uglify     = require('gulp-uglify');
+const gp_sourcemaps = require('gulp-sourcemaps');
 
-// const less = require('gulp-less');
-// const minifyCSS = require('gulp-csso');
-const concat = require('gulp-concat');
 
-// function css() {
-//   return src('client/templates/*.less')
-//     .pipe(less())
-//     .pipe(minifyCSS())
-//     .pipe(dest('build/css'))
-// }
+gulp.task('js-do', function()
+{
+  return gulp.src('src/*.js')
+    .pipe(gp_sourcemaps.init())
+    .pipe(gp_concat('concat.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(gp_rename('uglify.js'))
+    .pipe(gp_uglify())
+    .pipe(gp_sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
 
-function js() {
-  return src('client/javascript/*.js', { sourcemaps: true })
-    .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
-}
-
-exports.js = js;
-// exports.css = css;
-
-exports.default = parallel(js);
+gulp.task('default', ['js-do'], function(){});
