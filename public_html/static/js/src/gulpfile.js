ef67b9fabea47659f7280d7dd67992f8f005b44f
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-14 13:29:24 +0330

    try more

--- public_html/static/js/src/gulpfile.js
+++ public_html/static/js/src/gulpfile.js
@@ -1,9 +1,23 @@
-function defaultTask(cb)
-{
-  // place code for your default task here
-  cb();
+const { src, dest, parallel } = require('gulp');
 
+// const less = require('gulp-less');
+// const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
 
+// function css() {
+//   return src('client/templates/*.less')
+//     .pipe(less())
+//     .pipe(minifyCSS())
+//     .pipe(dest('build/css'))
+// }
+
+function js() {
+  return src('client/javascript/*.js', { sourcemaps: true })
+    .pipe(concat('app.min.js'))
+    .pipe(dest('build/js', { sourcemaps: true }))
 }
 
-exports.default = defaultTask
+exports.js = js;
+// exports.css = css;
+
+exports.default = parallel(js);
