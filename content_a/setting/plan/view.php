f0528bb54e736b4f2026d9848b6273346e0fd01f
Javad Kakroudi <J.Evazzadeh@gmail.com>
2019-02-25 00:33:42 +0330

    add new plan choose table

--- content_a/setting/plan/view.php
+++ content_a/setting/plan/view.php
@@ -6,7 +6,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Setting | '). T_('Change Plan of :name', ['name'=>\dash\data::store_name()]));
-		\dash\data::page_desc(T_('By choose new plan, we generate your invoice until now and next invoice is created one month later exactly at this time and you can pay it from billing.'));
+		\dash\data::page_desc(T_('Choose or change your plan'));
 
 		$subchild = \dash\url::subchild();
 		switch ($subchild)
