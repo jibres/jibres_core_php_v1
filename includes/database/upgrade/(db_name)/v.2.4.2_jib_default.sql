6a7e2a5cbd0cf68a739c80e1bc1876348b08c475
Reza <rm.biqarar@gmail.com>
2019-03-20 18:48:06 +0330

    add one field to cheque table

--- includes/database/upgrade/(db_name)/v.2.4.2_jib_default.sql
+++ includes/database/upgrade/(db_name)/v.2.4.2_jib_default.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `i_jib` ADD  `isdefault` bit(1) NULL DEFAULT NULL;
 ALTER TABLE `i_chequebook` CHANGE  `number` `number` bigint(20) unsigned NULL DEFAULT NULL;
+ALTER TABLE `i_cheque` ADD  `chequebook_id` bigint(20) unsigned NULL DEFAULT NULL AFTER `bank_id`;
+
+ALTER TABLE `i_cheque` ADD CONSTRAINT `i_cheque_chequebook_id` FOREIGN KEY (`chequebook_id`) REFERENCES `i_chequebook` (`id`) ON UPDATE CASCADE;
