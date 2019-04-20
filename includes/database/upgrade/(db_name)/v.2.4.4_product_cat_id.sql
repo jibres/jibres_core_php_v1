a44f83559740b00c04af8638b4a8d5a0eea358ca
Reza <rm.biqarar@gmail.com>
2019-04-20 18:54:37 +0430

    add index to value

--- includes/database/upgrade/(db_name)/v.2.4.4_product_cat_id.sql
+++ includes/database/upgrade/(db_name)/v.2.4.4_product_cat_id.sql
@@ -3,5 +3,6 @@ ALTER TABLE `products` ADD CONSTRAINT `products_cat_id` FOREIGN KEY (`cat_id`) R
 ALTER TABLE `products` ADD INDEX `products_cat_id_search_index` (`cat_id`);
 ALTER TABLE `productproperties` ADD INDEX `productproperties_cat_search_index` (`cat`);
 ALTER TABLE `productproperties` ADD INDEX `productproperties_key_search_index` (`key`);
+ALTER TABLE `productproperties` ADD INDEX `productproperties_value_search_index` (`value`);
 ALTER TABLE `productproperties` ADD INDEX `productproperties_product_id_search_index` (`product_id`);
 ALTER TABLE `productproperties` ADD INDEX `productproperties_store_id_search_index` (`store_id`);
