1b0a86a21e9fed8b45ce340c406758f525873d4a
Reza <rm.biqarar@gmail.com>
2019-05-17 15:29:42 +0430

    add table of productunit

---
+++ includes/database/upgrade/(db_name)/v.2.4.7_productunit.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `productunit` (
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`store_id` int(10) UNSIGNED NULL DEFAULT NULL,
+`title` varchar(200)CHARACTER SET utf8mb4 NULL DEFAULT NULL,
+`desimal` bit(1) NULL DEFAULT NULL,
+`isdefault` bit(1) NULL DEFAULT NULL,
+`maxsale` int(10) UNSIGNED NULL DEFAULT NULL,
+PRIMARY KEY (`id`),
+CONSTRAINT `productunit_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
