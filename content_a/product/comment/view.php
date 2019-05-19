11f460b5b1e23adca60b0974eb574df0d99b1bb0
Reza <rm.biqarar@gmail.com>
2019-05-19 16:55:30 +0430

    fix comment view

--- content_a/product/comment/view.php
+++ content_a/product/comment/view.php
@@ -6,7 +6,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Company of products'));
-		\dash\data::page_desc(T_('You can manage your comment manually.'). ' '. T_("Don't worry! we are add comment automatically on add new product"));
+		\dash\data::page_desc(T_('You can manage your comment manually.'));
 		\dash\data::page_pictogram('eye-galsses');
 
 		\dash\data::badge_text(T_('Back to product list'));
@@ -15,19 +15,6 @@ class view
 		if(\dash\data::editMode())
 		{
 			\dash\data::page_title(T_('Edit product comment'));
-
-		}
-
-		if(\dash\data::removeMode())
-		{
-			$allCompany = \lib\app\product\comment::list();
-			\dash\data::allCompany($allCompany);
-		}
-
-		if(\dash\data::removeMode() || \dash\data::editMode())
-		{
-			\dash\data::badge_text(T_('Back to product comment list'));
-			\dash\data::badge_link(\dash\url::that());
 		}
 
 	}
