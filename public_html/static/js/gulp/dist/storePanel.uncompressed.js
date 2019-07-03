0b04f919df340ec6f52b04c290513559bd0b6311
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-03 17:21:29 +0430

    update condition of detect input change

--- public_html/static/js/gulp/dist/storePanel.uncompressed.js
+++ public_html/static/js/gulp/dist/storePanel.uncompressed.js
@@ -1050,7 +1050,18 @@ function bindBtnOnFactor()
     $(this).parents('tr').attr('data-selected', null);
   });
 
+
+  $(document).on('focus', '#productSearch', function()
+  {
+    calcFooterValues();
+  });
+
   $(document).on('input', 'input.count', function()
+  {
+    calcFooterValues();
+  });
+
+  $(document).on('blur', 'input.count', function()
   {
     calcFooterValues();
   });
@@ -1060,6 +1071,10 @@ function bindBtnOnFactor()
     calcFooterValues();
   });
 
+  $(document).on('blur', 'input.buy', function()
+  {
+    calcFooterValues();
+  });
 
   $(document).on('click', '.priceBox .discount', function()
   {
@@ -1067,6 +1082,11 @@ function bindBtnOnFactor()
   });
 
   $(document).on('input', 'input.discount', function()
+  {
+    calcFooterValues();
+  });
+
+  $(document).on('blur', 'input.discount', function()
   {
     calcFooterValues();
   });
