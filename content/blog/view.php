fbd865c9509c0edb0c7858df5e0fc5f96bf197a6
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-26 17:25:32 +0430

    fix blog and some minor fix

--- content/blog/view.php
+++ content/blog/view.php
@@ -6,6 +6,7 @@ class view
 {
 	public static function config()
 	{
+		\dash\data::page_desc(T_('Know about our stories and events'));
 		\dash\data::allPostList(\dash\app\posts::get_post_list(['pagenation' => true]));
 	}
 }
