dbc44c7fc7e1391dc57843e9f4c5cd8d607b98fc
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-28 20:49:31 +0430

    add link of dl catalog

---
+++ content/catalog/controller.php
@@ -0,0 +1,15 @@
+<?php
+namespace content\catalog;
+
+
+class controller
+{
+	public static function routing()
+	{
+		$lastVersion = '2.0';
+		$dlLink      = \dash\url::base().'/static/catalog/Jibres-Catalog-v'. $lastVersion. '-preview.pdf';
+
+		\dash\redirect::to($dlLink);
+	}
+}
+?>
