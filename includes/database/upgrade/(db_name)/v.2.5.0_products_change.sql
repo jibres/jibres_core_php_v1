e845a66810329d1beeb5a5ca398e66228caac42c
Reza <rm.biqarar@gmail.com>
2019-10-01 18:59:09 +0330

    add products2 db table

--- includes/database/upgrade/(db_name)/v.2.5.0_products_change.sql
+++ includes/database/upgrade/(db_name)/v.2.5.0_products_change.sql
@@ -1,87 +1,3 @@
-ALTER TABLE `products` DROP FOREIGN KEY `products_creator`;
-ALTER TABLE `products` DROP INDEX `products_creator`;
-ALTER TABLE `products` DROP `creator`;
-
-ALTER TABLE `products` DROP `cat`;
-ALTER TABLE `products` DROP `seourl`;
-ALTER TABLE `products` DROP `company`;
-
-
-ALTER TABLE `products` DROP FOREIGN KEY `products_guarantee_id`;
-ALTER TABLE `products` DROP INDEX `products_guarantee_id_search_index`;
-ALTER TABLE `products` DROP `guarantee_id`;
-ALTER TABLE `products` DROP `guarantee`;
-
-
-ALTER TABLE `products` ADD `code` int(10) UNSIGNED NULL DEFAULT NULL AFTER `store_id`;
-ALTER TABLE `products` ADD INDEX `products_code_search_index` (`code`);
-
-
-
-ALTER TABLE `products` DROP INDEX `index_search_quickcode`;
-ALTER TABLE `products` DROP `quickcode`;
-ALTER TABLE `products` DROP `shortcode`;
-
-
-ALTER TABLE `products` DROP `unit`;
-ALTER TABLE `products` DROP `taxable`;
-ALTER TABLE `products` DROP `initialbalance`;
-
-ALTER TABLE `products` DROP INDEX `index_search_sold`;
-ALTER TABLE `products` DROP INDEX `index_search_stock`;
-ALTER TABLE `products` DROP `sold`;
-ALTER TABLE `products` DROP `stock`;
-
-
-
-ALTER TABLE `products` DROP `service`;
-
-
-ALTER TABLE `products` DROP INDEX `products_salesite_search_index`;
-ALTER TABLE `products` DROP `salesite`;
-
-ALTER TABLE `products` DROP INDEX `products_salephysical_search_index`;
-ALTER TABLE `products` DROP `salephysical`;
-
-
-
-
-ALTER TABLE `productcompany` CHANGE `store_id` `store_id` int(10) UNSIGNED NULL DEFAULT NULL;
-
-ALTER TABLE `productunit` CHANGE `store_id` `store_id` int(10) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `productunit` DROP `isdefault`;
-ALTER TABLE `productunit` DROP `maxsale`;
-
-
-
-ALTER TABLE `products` DROP INDEX `index_search_buyprice`;
-ALTER TABLE `products` DROP INDEX `index_search_price`;
-ALTER TABLE `products` DROP INDEX `index_search_discount`;
-ALTER TABLE `products` DROP INDEX `products_saletelegram_search_index`;
-ALTER TABLE `products` DROP INDEX `products_saleapp_search_index`;
-
-ALTER TABLE `products` DROP FOREIGN KEY `products_cat_id`;
-ALTER TABLE `products` DROP FOREIGN KEY `products_company_id`;
-ALTER TABLE `products` DROP FOREIGN KEY `products_unit_id`;
-ALTER TABLE `products` DROP INDEX `products_cat_id_search_index`;
-ALTER TABLE `products` DROP INDEX `products_unit_id_search_index`;
-ALTER TABLE `products` DROP INDEX `products_company_id_search_index`;
-
-ALTER TABLE `products` ADD CONSTRAINT `products_company_id` FOREIGN KEY (`company_id`) REFERENCES `productcompany` (`id`) ON UPDATE CASCADE;
-ALTER TABLE `products` ADD CONSTRAINT `products_unit_id` FOREIGN KEY (`unit_id`) REFERENCES `productunit` (`id`) ON UPDATE CASCADE;
--- ALTER TABLE `products` ADD CONSTRAINT `products_cat_id` FOREIGN KEY (`cat_id`) REFERENCES `productcategory` (`id`) ON UPDATE CASCADE;
-
-
-ALTER TABLE `products` ADD `step` int(10) UNSIGNED NULL DEFAULT NULL AFTER `maxstock`;
-ALTER TABLE `products` ADD `minsale` int(10) UNSIGNED NULL DEFAULT NULL AFTER `step`;
-ALTER TABLE `products` ADD `maxsale` int(10) UNSIGNED NULL DEFAULT NULL AFTER `minsale`;
-ALTER TABLE `products` ADD `thumbid` int(10) UNSIGNED NULL DEFAULT NULL AFTER `thumb`;
-ALTER TABLE `products` ADD `weightunit` enum('lb', 'oz', 'kg', 'g') NULL DEFAULT NULL AFTER `weight`;
-
-
-
-
-
 
 CREATE TABLE `productcategory` (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -105,5 +21,90 @@ CONSTRAINT `productcategory_file_id` FOREIGN KEY (`file_id`) REFERENCES `files` 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `productcategory` ADD INDEX `productcategory_parent_search_index` (`parent`);
+
+
+
+
+CREATE TABLE `products2` (
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+
+`store_id` int(10) UNSIGNED NOT NULL,
+`code` int(10) UNSIGNED DEFAULT NULL,
+`sku` varchar(20) DEFAULT NULL,
+
+`title` varchar(500) DEFAULT NULL,
+`seotitle` varchar(300) DEFAULT NULL,
+`slug` varchar(200) DEFAULT NULL,
+`seodesc` varchar(500) DEFAULT NULL,
+`desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+
+`barcode` varchar(100) DEFAULT NULL,
+`barcode2` varchar(100) DEFAULT NULL,
+
+`cat_id` int(10) UNSIGNED DEFAULT NULL,
+`unit_id` int(10) UNSIGNED DEFAULT NULL,
+`company_id` int(10) UNSIGNED DEFAULT NULL,
+
+`salestep` int(10) UNSIGNED DEFAULT NULL,
+`minstock` int(10) UNSIGNED DEFAULT NULL,
+`maxstock` int(10) UNSIGNED DEFAULT NULL,
+`minsale` int(10) UNSIGNED DEFAULT NULL,
+`maxsale` int(10) UNSIGNED DEFAULT NULL,
+`carton` int(10) UNSIGNED DEFAULT NULL,
+`scalecode` int(10) DEFAULT NULL,
+
+`weight` int(10) DEFAULT NULL,
+`weightunit` enum('lb','oz','kg','g') DEFAULT NULL,
+
+`type` enum('product','file','service') NOT NULL DEFAULT 'product',
+`status` enum('unset','available','unavailable','soon','discountinued') DEFAULT NULL,
+
+`thumbid` int(10) UNSIGNED DEFAULT NULL,
+`gallery` text,
+
+`vat` enum('yes') DEFAULT NULL,
+`infinite` enum('yes') DEFAULT NULL,
+`oversale` enum('yes') DEFAULT NULL,
+
+`service` enum('yes') DEFAULT NULL,
+`locksaleonline` enum('yes') DEFAULT NULL,
+`locksaletelegram` enum('yes') DEFAULT NULL,
+`locksaleapp` enum('yes') DEFAULT NULL,
+
+
+`variants` mediumtext CHARACTER SET utf8mb4,
+`optionname1` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+`optionname2` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+`optionname3` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+`optionvalue1` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+`optionvalue2` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+`optionvalue3` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL,
+
+`parent` int(10) UNSIGNED DEFAULT NULL,
+
+`datecreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`datemodified` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`),
+CONSTRAINT `products_field_unit_id` FOREIGN KEY (`unit_id`) REFERENCES `productunit` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `products_field_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `products_field_company_id` FOREIGN KEY (`company_id`) REFERENCES `productcompany` (`id`) ON UPDATE CASCADE,
+KEY `index_search_products_sku` (`sku`),
+KEY `index_search_products_code` (`code`),
+KEY `index_search_products_title` (`title`),
+KEY `index_search_products_parent` (`parent`),
+KEY `index_search_products_status` (`status`),
+KEY `index_search_products_barcode` (`barcode`),
+KEY `index_search_products_barcode2` (`barcode2`),
+KEY `index_search_products_scalecode` (`scalecode`),
+KEY `index_search_products_locksaleapp` (`locksaleapp`),
+KEY `index_search_products_optionname1` (`optionname1`),
+KEY `index_search_products_optionname2` (`optionname2`),
+KEY `index_search_products_optionname3` (`optionname3`),
+KEY `index_search_products_optionvalue1` (`optionvalue1`),
+KEY `index_search_products_optionvalue2` (`optionvalue2`),
+KEY `index_search_products_optionvalue3` (`optionvalue3`),
+KEY `index_search_products_locksaleonline` (`locksaleonline`),
+KEY `index_search_products_locksaletelegram` (`locksaletelegram`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
