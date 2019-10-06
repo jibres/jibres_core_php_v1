cc176a0b3b15aa6afa0c4c8ae72cad23830036ad
Reza <rm.biqarar@gmail.com>
2019-10-06 17:17:33 +0330

    store db

--- includes/database/install/(db_name)+/v.0.1.2_add_userstores.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `contacts` ADD `store_id` INT(10) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `contacts` ADD CONSTRAINT `contacts_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON UPDATE CASCADE;
