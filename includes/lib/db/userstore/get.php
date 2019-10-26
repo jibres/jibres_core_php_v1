0233609dac42425d751b9bf09b010716f6c71374
Reza <rm.biqarar@gmail.com>
2019-10-26 15:39:33 +0330

    add userstore library

---
+++ includes/lib/db/userstore/get.php
@@ -0,0 +1,14 @@
+<?php
+namespace lib\db\userstore;
+
+
+class get
+{
+	public static function user_id_detail($_user_id)
+	{
+		$query = "SELECT * FROM userstore WHERE userstore.user_id = $_user_id LIMIT 1 ";
+		$result = \dash\db::get($query, null, true);
+		return $result;
+	}
+}
+?>
---
+++ includes/lib/db/userstore/insert.php
@@ -0,0 +1,30 @@
+<?php
+namespace lib\db\userstore;
+
+
+class insert
+{
+	public static function new_row($_args)
+	{
+		$set = \dash\db\config::make_set($_args, ['type' => 'insert']);
+		if($set)
+		{
+			$query = " INSERT INTO `userstore` SET $set ";
+
+			if(\dash\db::query($query))
+			{
+				$id = \dash\db::insert_id();
+				return $id;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+?>
