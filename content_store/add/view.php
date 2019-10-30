e7e05713590534188bf25b9a7ad4c245c8a3e1ae
Javad Adib <J.Evazzadeh@live.com>
2019-10-30 18:15:56 +0330

    add get started page

--- content_store/add/view.php
+++ content_store/add/view.php
@@ -7,7 +7,6 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("Add New Store"));
-		\dash\data::page_desc(T_("Add with simple detail and config more after adding new store."));
 
 		$termLink = '<a href="'. \dash\url::kingdom(). '/terms" target="_blank">'. T_('Terms of Service') .'</a>';
 		\dash\data::termOfService(T_("By press Create button, you're agreeing to our :term.", ['term' => $termLink]));
