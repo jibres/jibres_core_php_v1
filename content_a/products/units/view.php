23bc8e6c09ae4ef67ac181aa6f5cfd5303af1f2e
Reza <rm.biqarar@gmail.com>
2019-11-06 11:22:36 +0330

    add product unit module

---
+++ content_a/products/units/view.php
@@ -0,0 +1,35 @@
+<?php
+namespace content_a\product\units;
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Unit of products'));
+		\dash\data::page_desc(T_('You can manage your units manually.'). ' '. T_("Don't worry! we are add units automatically on add new product"));
+		\dash\data::page_pictogram('eye-galsses');
+
+		\dash\data::badge_text(T_('Back to product list'));
+		\dash\data::badge_link(\dash\url::this());
+
+		if(\dash\data::editMode())
+		{
+			\dash\data::page_title(T_('Edit product unit'));
+
+		}
+
+		if(\dash\data::removeMode())
+		{
+			$allUnit = \lib\app\product\unit::list(null, ['pagenation' => false]);
+			\dash\data::allUnit($allUnit);
+		}
+
+		if(\dash\data::removeMode() || \dash\data::editMode())
+		{
+			\dash\data::badge_text(T_('Back to product unit list'));
+			\dash\data::badge_link(\dash\url::that());
+		}
+
+	}
+}
+?>
