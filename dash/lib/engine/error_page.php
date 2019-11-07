0fc74b31f407d4bb6b720a3b96108123106f084b
Javad <Mr.Javad.Adib@gmail.com>
2019-11-07 16:40:19 +0330

    fix link to stiftal on install error

--- dash/lib/engine/error_page.php
+++ dash/lib/engine/error_page.php
@@ -6,7 +6,7 @@
  <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><![endif]-->
  <meta name ="viewport" content="width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0 maximum-scale=1.5, minimal-ui"/>
  <link rel="shortcut icon" href="<?php echo(\dash\url::static()); ?>/images/favicon-error.png"/>
- <link rel="stylesheet"  href="<?php echo(\dash\url::siftal()); ?>/css/siftal.css">
+ <link rel="stylesheet"  href="<?php echo(\dash\url::static()); ?>/css/siftal.min.css">
 </head>
 <body class='<?php echo (\dash\language::current('direction')); ?> errorPage s<?php echo(substr($_code,0,2));?>'>
  <div id="nodes"></div>
