af1bd4fa0963c1ad32fd07679eadab5c3818463b
Javad Adib <J.Evazzadeh@live.com>
2019-11-18 18:11:55 +0330

    fix support sidebar

--- content_support/view.php
+++ content_support/view.php
@@ -10,14 +10,9 @@ class view
 		\dash\data::include_js(false);
 		\dash\data::include_highcharts(true);
 
-
-
 		\dash\data::include_editor(true);
 		\dash\data::badge_shortkey(120);
 		\dash\data::badge2_shortkey(121);
-
-
-		\dash\data::display_admin('content_support/layout.html');
 
 		\dash\data::maxUploadSize(\dash\utility\upload::max_file_upload_size(true));
 
