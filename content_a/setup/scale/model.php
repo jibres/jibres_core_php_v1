2c1fd39f5aa4cc247bdc80e5974d599d54f3f94e
Reza <rm.biqarar@gmail.com>
2019-11-22 10:25:32 +0330

    show store detail

--- content_a/setup/scale/model.php
+++ content_a/setup/scale/model.php
@@ -7,6 +7,7 @@ class model
 	public static function post()
 	{
 		\lib\app\setting\setup::have_scale(\dash\request::post('scale'));
+		\lib\store::refresh();
 		$next_level = \lib\app\setting\setup::scale();
 		\dash\redirect::to($next_level);
 	}
--- content_a/setup/vat/model.php
+++ content_a/setup/vat/model.php
@@ -7,6 +7,7 @@ class model
 	public static function post()
 	{
 		\lib\app\setting\setup::have_vat(\dash\request::post('vat'));
+		\lib\store::refresh();
 		$next_level = \lib\app\setting\setup::vat();
 		\dash\redirect::to($next_level);
 	}
