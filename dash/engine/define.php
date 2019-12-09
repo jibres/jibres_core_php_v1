e81ae75aef9c62eff92cc7763927e1de59434b1e
Reza <rm.biqarar@gmail.com>
2019-12-09 14:37:45 +0330

    find $_SERVER from raw php

--- dash/engine/define.php
+++ dash/engine/define.php
@@ -3,6 +3,45 @@
  * default of all define
  */
 
+function cronjob_server()
+{
+	// cun cronjob by this code
+	// php /home/reza/projects/jibres/public_html/index.php '{"trust_token":"123","HTTP_HOST":"mohiti.jibres.local","SERVER_NAME":"jibres.local","SERVER_PORT":"80","SERVER_PROTOCOL":"HTTP/1.1","REQUEST_URI":"/a","REQUEST_METHOD":"POST","SCRIPT_FILENAME":"/home/reza/projects/jibres/public_html/index.php"}' &
+	if(!isset($GLOBALS['argv'][1]))
+	{
+		return false;
+	}
+
+	$newServer = $GLOBALS['argv'][1];
+	if(!is_string($newServer))
+	{
+		return false;
+	}
+
+	$newServer = json_decode($newServer, true);
+	if(!is_array($newServer))
+	{
+		return false;
+	}
+
+	$trust_addr = __DIR__. '/cronjob_server.me.token';
+	if(!is_file($trust_addr))
+	{
+		return false;
+	}
+
+	$load_token = file_get_contents($trust_addr);
+	if(isset($newServer['trust_token']) && $newServer['trust_token'] === $load_token)
+	{
+		$_SERVER = $newServer;
+	}
+}
+
+/**
+ * Find $_SERVER !
+ */
+cronjob_server();
+
 // Define Global variables ****************************************************
 // Core name
 define('core_name', 'dash');
