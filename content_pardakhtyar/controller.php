ae8fab51a54b12a10da53fa857da3c2d50ede6e3
Reza <rm.biqarar@gmail.com>
2019-12-10 21:02:31 +0330

    add pardakhtyar response v2

--- content_pardakhtyar/controller.php
+++ content_pardakhtyar/controller.php
@@ -6,7 +6,9 @@ class controller
 {
 	public static function routing()
 	{
-
+		// $addr = '/home/reza/projects/jibres/includes/docs/pardakhtyar/sample file/BMJSSHAP98091915012.CT';
+		// $xml = simplexml_load_string(file_get_contents($addr));
+		// j($xml);
 		// \dash\redirect::to_login();
 
 		// \dash\permission::access('contentA');
