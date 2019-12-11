64dd5eab61c5914031b9501b2e066753fc40100f
Reza <rm.biqarar@gmail.com>
2019-12-11 23:29:47 +0330

    fix remove product

--- lib/app/product/remove.php
+++ lib/app/product/remove.php
@@ -10,6 +10,20 @@ class remove
 		{
 			return false;
 		}
+
+		if(isset($product_detail['variant_child']) && $product_detail['variant_child'])
+		{
+			\dash\notif::error(T_("This product have some child and can not be remove!"));
+			return false;
+		}
+
+		$parent_id = null;
+
+		if(isset($product_detail['parent']) && $product_detail['parent'])
+		{
+			$parent_id = $product_detail['parent'];
+		}
+
 		// check have factor ?
 		$use_in_factor = false;
 		if($use_in_factor)
@@ -18,15 +32,27 @@ class remove
 		}
 		else
 		{
-			// remove tag
-			// remove fileusage
-			// remove product price
-			\lib\db\productprices\db::delete_by_product_id($_id);
-			// remove product
-			\lib\db\products\db::delete($_id);
+			self::hard_delete($_id);
 		}
+
+		if($parent_id)
+		{
+			\lib\db\products\update::variant_child_calc($parent_id);
+		}
+
 		\dash\notif::ok(T_("Product removed"));
 		return true;
+	}
+
+
+	private static function hard_delete($_id)
+	{
+		// remove tag
+		// remove fileusage
+		// remove product price
+		\lib\db\productprices\db::delete_by_product_id($_id);
+		// remove product
+		\lib\db\products\db::delete($_id);
 	}
 
 }
--- lib/db/products/update.php
+++ lib/db/products/update.php
@@ -21,5 +21,20 @@ class update
 	}
 
 
+	public static function variant_child_calc($_id)
+	{
+		$query  = "SELECT products.id AS `id` FROM products WHERE products.parent = $_id LIMIT 1";
+		$have_child = \dash\db::get($query, 'id', true);
+
+		$have_child = $have_child ? 1 : null;
+		if(!$have_child)
+		{
+			$query  = "UPDATE products SET products.variant_child = NULL WHERE products.id = $_id LIMIT 1";
+			$result = \dash\db::query($query);
+			return $result;
+		}
+	}
+
+
 }
 ?>
