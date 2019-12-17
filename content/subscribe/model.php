2f706fe5c5c1af1078526f874960946250bfe630
Javad Adib <Mr.Javad.Adib@gmail.com>
2019-12-17 12:10:53 +0330

    update sms message

--- content/subscribe/model.php
+++ content/subscribe/model.php
@@ -21,7 +21,11 @@ class model
 
 		if(isset($add['id']) || \dash\utility\filter::mobile($mobile) === '989109610612')
 		{
-			$sms = "سلام. به دنیای جیبرس خوش آمدید";
+			$sms = "سپاس از بازدیدتان از غرفه‌ی جیبرس". "\n";
+			$sms .= "به دنیای جیبرس خوش آمدید". "\n\n";
+			$sms .= "اپلیکیشن + وب‌سایت فروشگاهی + فروش حضوری + فروش در شبکه‌های اجتماعی". "\n";
+			$sms .= "در همه بسترهای ممکن  یکپارچه بفروشید و لذت ببرید". "\n\n";
+			$sms .= "Jibres.com";
 			if(\dash\utility\filter::ir_mobile($mobile))
 			{
 				\dash\utility\sms::send($mobile, $sms);
