5fe83998bcbdf4aaa5a14920744c5673a79a98ff
Reza <rm.biqarar@gmail.com>
2019-12-25 12:10:55 +0330

    transfer store plan

--- content_transfer/_store/db.php
+++ content_transfer/_store/db.php
@@ -5,10 +5,14 @@ class db
 {
 	public static function run()
 	{
-		\content_transfer\say::info('Start convert stores ...');
+		\content_transfer\say::info('Transfer stores ...');
 		self::convert_store();
 
+		\content_transfer\say::info('Transfer stores data ...');
 		self::convert_store_data();
+
+		\content_transfer\say::info('Transfer stores plan ...');
+		self::transfer_store_plan();
 	}
 
 
@@ -118,6 +122,59 @@ class db
 				\content_transfer\say::end('Can not add store data! '.  json_encode($new_store, JSON_UNESCAPED_UNICODE));
 			}
 		}
+	}
+
+
+
+
+
+	private static function transfer_store_plan()
+	{
+		$query =
+		"
+			INSERT INTO jibres.store_plan
+			(
+				`id`,
+				`store_id`,
+				`user_id`,
+				`plan`,
+				`start`,
+				`end`,
+				`type`,
+				`description`,
+				`status`,
+				`price`,
+				`discount`,
+				`promo`,
+				`period`,
+				`expireplan`,
+				`datecreated`,
+				`datemodified`
+			)
+			SELECT
+				jibres_transfer.planhistory.id,
+				(SELECT jibres_transfer.stores.new_id FROM jibres_transfer.stores WHERE jibres_transfer.stores.id = jibres_transfer.planhistory.store_id LIMIT 1),
+				jibres_transfer.planhistory.creator,
+				jibres_transfer.planhistory.plan,
+				jibres_transfer.planhistory.start,
+				jibres_transfer.planhistory.end,
+				jibres_transfer.planhistory.type,
+				jibres_transfer.planhistory.desc,
+				jibres_transfer.planhistory.status,
+				jibres_transfer.planhistory.price,
+				jibres_transfer.planhistory.discount,
+				jibres_transfer.planhistory.promo,
+				jibres_transfer.planhistory.period,
+				jibres_transfer.planhistory.expireplan,
+				jibres_transfer.planhistory.datecreated,
+				jibres_transfer.planhistory.datemodified
+			FROM
+				jibres_transfer.planhistory
+			WHERE 1
+		";
+
+		$result = \dash\db::query($query,'local', ['database' => 'jibres']);
+
 	}
 
 
