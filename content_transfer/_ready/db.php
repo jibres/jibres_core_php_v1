2d3c8c2b73d8d5ab0972a099741df78a9f8a6f3b
Reza <rm.biqarar@gmail.com>
2019-12-31 16:52:19 +0330

    delete fake stores

--- content_transfer/_ready/db.php
+++ content_transfer/_ready/db.php
@@ -5,6 +5,9 @@ class db
 {
 	public static function run()
 	{
+		\content_transfer\say::info('Remove fake stores ...');
+		self::remove_face_store();
+
 		\content_transfer\say::info('Alter table to add field stores.new_id To save store id in old db');
 		self::store_new_id();
 
@@ -35,6 +38,32 @@ class db
 		self::factors_new_id();
 	}
 
+	private static function remove_face_store()
+	{
+		$query = "SELECT * from stores where stores.id not in (select products.store_id from products);";
+		$result = \dash\db::get($query, null, false, 'local', ['database' => 'jibres_transfer']);
+		if($result)
+		{
+			$id = array_column($result, 'id');
+			$delete_id = implode(',', $id);
+			$query = "DELETE from planhistory where planhistory.store_id in ($delete_id);";
+			$result = \dash\db::query($query, 'local', ['database' => 'jibres_transfer']);
+			\content_transfer\say::info('Remove fake stores (planhistory)...');
+
+			$query = "DELETE from userstores where userstores.store_id in ($delete_id);";
+			$result = \dash\db::query($query, 'local', ['database' => 'jibres_transfer']);
+			\content_transfer\say::info('Remove fake stores (userstores)...');
+
+			$query = "DELETE from productterms where productterms.store_id in ($delete_id);";
+			$result = \dash\db::query($query, 'local', ['database' => 'jibres_transfer']);
+			\content_transfer\say::info('Remove fake stores (productterms)...');
+
+			$query = "DELETE from stores where stores.id in ($delete_id);";
+			$result = \dash\db::query($query, 'local', ['database' => 'jibres_transfer']);
+			\content_transfer\say::info(count($result). ' Store removed ...');
+		}
+
+	}
 
 	private static function store_new_id()
 	{
