558fa9b02eb421a34c53a49b826e6b899a07a81a
Reza <rm.biqarar@gmail.com>
2020-01-02 08:38:08 +0330

    add master jibres crontab

--- dash/engine/cronjob/cronjob.php
+++ dash/engine/cronjob/cronjob.php
@@ -4,10 +4,14 @@ class cronjob
 {
 	public static function run()
 	{
+		// this directory
 		$list_stores_addr = __DIR__;
 		$list_stores_addr = str_replace('dash/engine/cronjob', '', $list_stores_addr);
+
+		// master jibres directory
 		$jibres_addr = $list_stores_addr;
 
+		// store setting file directory
 		$list_stores_addr .= 'includes/stores/subdomain/';
 		if(!is_dir($list_stores_addr))
 		{
@@ -15,76 +19,77 @@ class cronjob
 			return;
 		}
 
-		$list_stores = glob($list_stores_addr. '*');
+		// generate trust token by random
+		$trust_token = self::generate_trust_token();
 
-		if(empty($list_stores))
-		{
-			self::save_log('stores subdomain folder is empty!');
-			return;
-		}
-
-		// cun cronjob by this code
-		// php /home/reza/projects/jibres/public_html/index.php '{"trust_token":"123","HTTP_HOST":"mohiti.jibres.local","SERVER_NAME":"jibres.local","SERVER_PORT":"80","SERVER_PROTOCOL":"HTTP/1.1","REQUEST_URI":"/a","REQUEST_METHOD":"POST","SCRIPT_FILENAME":"/home/reza/projects/jibres/public_html/index.php"}' &
-
-		$trust_token = self::trust_token();
-
+		// index.php directory
 		$index_php_addr = $jibres_addr. "public_html/index.php";
 
+		// the domain (this domain is safe and nobody can use this domain)
 		$SERVER_NAME = 'jibres.WorldSalesEngineeringSystem';
 
-
+		// fake $_SERVER
 		$server =
 		[
 			'trust_token'     => $trust_token,
-			'HTTP_HOST'       => null, // "mohiti.jibres.tld",
-			'SERVER_NAME'     => $SERVER_NAME, // "jibres.tld",
+			'HTTP_HOST'       => null, // "change by customer host",
+			'SERVER_NAME'     => $SERVER_NAME,
 			'SERVER_PORT'     => "443",
 			'SERVER_PROTOCOL' => "HTTP/1.1",
 			'REQUEST_URI'     => "/hook/crontab/". $trust_token,
 			'REQUEST_METHOD'  => "POST",
 			'SCRIPT_FILENAME' => $index_php_addr,
-
 		];
 
-		$exec = [];
-		foreach ($list_stores as $key => $value)
-		{
-			$subdomain           = str_replace($list_stores_addr, '', $value);
-			$HTTP_HOST           = $subdomain. '.'. $SERVER_NAME;
-			$server['HTTP_HOST'] = $HTTP_HOST;
-			$store_exec = 'php '. $index_php_addr. " '". json_encode($server, JSON_UNESCAPED_UNICODE). "' ";
-			$exec[] = $store_exec;
-		}
+		// execute list
+		$exec                = [];
 
-		if(empty($exec))
+		// run jibres master crontab
+		$HTTP_HOST           = $SERVER_NAME;
+		$server['HTTP_HOST'] = $HTTP_HOST;
+		$store_exec          = 'php '. $index_php_addr. " '". json_encode($server, JSON_UNESCAPED_UNICODE). "' ";
+		$exec[]              = $store_exec;
+
+		// load other store name
+		$list_stores = glob($list_stores_addr. '*');
+
+		if(empty($list_stores))
 		{
-			self::save_log('Exec list is empty!');
-			return false;
+			self::save_log('stores subdomain folder is empty!');
+		}
+		else
+		{
+			foreach ($list_stores as $key => $value)
+			{
+				$subdomain           = str_replace($list_stores_addr, '', $value);
+				$HTTP_HOST           = $subdomain. '.'. $SERVER_NAME;
+				$server['HTTP_HOST'] = $HTTP_HOST;
+				$store_exec = 'php '. $index_php_addr. " '". json_encode($server, JSON_UNESCAPED_UNICODE). "' ";
+				$exec[] = $store_exec;
+			}
 		}
 
 		$exec_addr = __DIR__. '/exec.me.php';
-		$exec = implode(" & ", $exec);
-		$exec_php = 'cd '. $jibres_addr. 'public_html && '. $exec;
 
-		file_put_contents($exec_addr, $exec_php);
+		$exec = implode(" & ", $exec);
+
+		file_put_contents($exec_addr, $exec);
+
+		$exec_php = 'cd '. $jibres_addr. 'public_html && sh '. $exec_addr;
 
 		shell_exec($exec_php);
 
 	}
 
 
-	private static function trust_token()
+	private static function generate_trust_token()
 	{
 		$engine_addr = __DIR__;
 		$engine_addr = str_replace('/cronjob', '', $engine_addr);
 		$engine_addr .= '/cronjob_server.me.token';
-
-		$rand = random_bytes(100). ' '. microtime(). ' '. rand();
-
-		$token = md5($rand);
-
+		$rand        = random_bytes(100). ' '. microtime(). ' '. rand(). ' '. rand();
+		$token       = md5($rand);
 		file_put_contents($engine_addr, $token);
-
 		return $token;
 	}
 
@@ -92,8 +97,9 @@ class cronjob
 	private static function save_log($_text)
 	{
 		// save text to file
+		$_text = date("Y-m-d H:i:s"). ' ----- '. $_text;
+		file_put_contents(__DIR__. '/cronjob.me.log', $_text);
 	}
-
 }
 cronjob::run();
 ?>
