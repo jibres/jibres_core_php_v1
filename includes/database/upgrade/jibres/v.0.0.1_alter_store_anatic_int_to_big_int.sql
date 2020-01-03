d871adae4842629f497fb17f98743a1654ae4038
Reza <rm.biqarar@gmail.com>
2020-01-03 16:27:14 +0330

    db upgrade to store_analytics

---
+++ includes/database/upgrade/jibres/v.0.0.1_alter_store_anatic_int_to_big_int.sql
@@ -0,0 +1,9 @@
+ALTER TABLE jibres.store_analytics CHANGE `dbtrafic` `dbtrafic` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `dbsize` `dbsize` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `sumplustransaction` `sumplustransaction` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `summinustransaction` `summinustransaction` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `factor` `factor` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `factorbuy` `factorbuy` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `factorsale` `factorsale` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `factordetail` `factordetail` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres.store_analytics CHANGE `sumfactor` `sumfactor` bigint(20) UNSIGNED NULL DEFAULT NULL;
