48c93ff48785bd5d2bee3f10c2c99a23fab6883a
Reza <rm.biqarar@gmail.com>
2020-01-03 18:35:15 +0330

    run all step by one btn

--- content_su/update/model.php
+++ content_su/update/model.php
@@ -31,6 +31,21 @@ class model
 				\dash\notif::ok(T_("System unlock successfully"));
 				break;
 
+			case 'all':
+				\dash\engine\lock::lock();
+				\dash\notif::warn(T_("System locked"));
+
+				if(\content_su\update\controller::gitUpdate('all', false))
+				{
+					\dash\notif::info(T_('System git pull successfully'));
+				}
+				\lib\app\upgradedb\upgrade::run();
+				\dash\notif::info(T_('System upgrade database successfully'));
+
+				\dash\engine\lock::unlock();
+				\dash\notif::ok(T_("System unlock successfully"));
+				break;
+
 			default:
 				\dash\notif::error(T_("Invalid type"));
 				return false;
