7f1c87454ee16846f52e1601a0192ea4b4e0f8aa
Reza <rm.biqarar@gmail.com>
2020-01-04 07:20:08 +0330

    download backup

--- content_su/backup/controller.php
+++ content_su/backup/controller.php
@@ -7,10 +7,21 @@ class controller
 	{
 
 		$download = \dash\request::get('download');
-		if($download)
+		if($download && is_file(database. 'backup/file/'. $download))
 		{
 			\dash\log::set('downloadBackup');
-			\dash\file::download(database. 'backup/files/'. $download);
+			\dash\file::download(database. 'backup/file/'. $download);
+			return;
+		}
+
+		$zipdownload = \dash\request::get('zipdownload');
+		if($zipdownload && is_dir(database. 'backup/file/'. $zipdownload))
+		{
+			$zip_addr  = __DIR__.'/';
+			$file_name = 'Backup_database.me.zip';
+			$zip       = \dash\utility\zip::folder($zip_addr. $file_name, database. 'backup/file/'. $zipdownload);
+			\dash\log::set('downloadBackupZip');
+			\dash\file::download($zip_addr. $file_name);
 			return;
 		}
 	}
--- content_su/backup/view.php
+++ content_su/backup/view.php
@@ -7,11 +7,6 @@ class view
 	public static function config()
 	{
 
-		if(\dash\request::get('show') === 'log')
-		{
-			\dash\data::autoBackupLog(@\dash\file::read(database. 'backup/log'));
-		}
-
 		$configBackup = @\dash\file::read(database. 'backup/schedule');
 		if($configBackup && is_string($configBackup))
 		{
@@ -22,7 +17,30 @@ class view
 
 		\dash\data::mysqlInfo(\dash\db::global_status());
 
-		$oldBackup = @glob(database .'backup/files/*');
+		$folder    = null;
+		$subfolder = null;
+
+		if(\dash\request::get('folder'))
+		{
+			$folder = \dash\request::get('folder');
+		}
+
+		if(\dash\request::get('subfolder'))
+		{
+			$subfolder = \dash\request::get('subfolder');
+		}
+
+		$addr = [];
+		$addr[] = 'file';
+		$addr[] = $folder;
+		$addr[] = $subfolder;
+		$addr[] = '*';
+		$addr = array_filter($addr);
+		$addr = implode('/', $addr);
+
+		$oldBackup = @glob(database .'backup/'. $addr);
+
+
 
 		$oldBackup_files = [];
 
@@ -30,14 +48,44 @@ class view
 		{
 			foreach ($oldBackup as $key => $value)
 			{
-				$oldBackup_files [] =
-				[
-					'name' => basename($value),
-					'time' => filemtime($value),
-					'size' => round(filesize($value) / 1024 / 1024, 1),
-					'date' => date("Y-m-d H:i:s", filemtime($value)),
-					'ago' => \dash\utility\human::timing(date("Y-m-d H:i:s", filemtime($value))),
-				];
+				if(is_dir($value))
+				{
+					$myFolder    = null;
+					$mySubfolder = null;
+
+					if(\dash\request::get('folder'))
+					{
+						$myFolder = \dash\request::get('folder');
+					}
+
+					if($myFolder)
+					{
+						$mySubfolder = basename($value);
+					}
+
+					$oldBackup_files [] =
+					[
+						'name'      => basename($value),
+						'type'      => 'folder',
+						'addr'      => str_replace(database .'backup/file/', '', $value),
+						'folder'    => $myFolder ? $myFolder : basename($value),
+						'subfolder' => $mySubfolder,
+					];
+				}
+				else
+				{
+					$oldBackup_files [] =
+					[
+						'type' => 'file',
+						'name' => basename($value),
+						'time' => filemtime($value),
+						'size' => round(filesize($value) / 1024 / 1024, 1),
+						'date' => date("Y-m-d H:i:s", filemtime($value)),
+						'addr' => str_replace(database .'backup/file/', '', $value),
+						'ago'  => \dash\utility\human::timing(date("Y-m-d H:i:s", filemtime($value))),
+					];
+
+				}
 			}
 			$oldBackup_files = array_reverse($oldBackup_files);
 			\dash\data::oldBackup($oldBackup_files);
