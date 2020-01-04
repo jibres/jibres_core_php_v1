01a772f9e716d749e80a2d0a84652c62f84ee197
Reza <rm.biqarar@gmail.com>
2020-01-04 16:23:43 +0330

    show real product count in store

--- lib/db/store/store_user.php
+++ lib/db/store/store_user.php
@@ -16,11 +16,16 @@ class store_user
 				store_data.plan,
 				store_data.startplan,
 				store_data.expireplan,
-				store_data.logo
+				store_data.logo,
+				store_analytics.product,
+				store_analytics.customer,
+				store_analytics.factor,
+				store_analytics.lastactivity
 			FROM
 				store_user
 			INNER JOIN store ON store.id = store_user.store_id
 			INNER JOIN store_data ON store_data.id = store_user.store_id
+			INNER JOIN store_analytics ON store_analytics.id = store_user.store_id
 			WHERE store_user.user_id = $_user_id
 		";
 		$result = \dash\db::get($query);
