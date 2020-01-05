8084d5424cc495e21ded3ea703d77519c4805d7c
Reza <rm.biqarar@gmail.com>
2020-01-05 13:06:18 +0330

    check enter change pass in controller

---
+++ content_enter/delete/controller.php
@@ -0,0 +1,14 @@
+<?php
+namespace content_enter\delete;
+
+class controller
+{
+	public static function routing()
+	{
+		if(!\dash\user::login())
+		{
+			\dash\header::status(403, T_("Login error"));
+		}
+	}
+}
+?>
