f29696214b558867e6a0538ab0e7d7fcd80e1319
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-01-06 19:14:45 +0330

    fix service worker register process

--- public_html/static/js/siftal/src/utility/serviceWorker.js
+++ public_html/static/js/siftal/src/utility/serviceWorker.js
@@ -1,45 +1,40 @@
 function registerServiceWorker()
 {
-	removeAllServiceWorkers();
-	return false;
-	// need more work
-	if ('serviceWorker' in navigator)
-	{
-		window.addEventListener('load', () =>
-		{
-			navigator.serviceWorker.register('/serviceWorker-v3?v=3').then(function(_reg)
-			{
-				// Registration was successful
-				logy('Service worker registered.');
-			}, function(_err)
-			{
-				// registration failed :(
-				logy('ServiceWorker registration failed.');
-			}).catch(function(_err)
-			{
-				logy(_err);
-			});
-		});
-	}
-	else
-	{
-		logy('service worker is not supported');
-	}
+  if ('serviceWorker' in navigator)
+  {
+    if (navigator.serviceWorker.controller)
+    {
+      console.log("Active service worker found, no need to register");
+    }
+    else
+    {
+      // Register the service worker
+      navigator.serviceWorker.register("/serviceWorker-v1?v=100", { scope: "./"})
+      .then(function (_reg)
+      {
+        console.log("Service worker has been registered for scope " + _reg.scope);
+      });
+    }
+  }
+  else
+  {
+    logy('service worker is not supported');
+  }
 }
 
 function removeAllServiceWorkers()
 {
-	if ('serviceWorker' in navigator) {
-	  navigator.serviceWorker.getRegistrations().then(function (registrations)
-	  {
-	    //returns installed service workers
-	    if (registrations.length)
-	    {
-	      for(let registration of registrations)
-	      {
-	        registration.unregister();
-	      }
-	    }
-	  });
-	}
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations)
+    {
+      //returns installed service workers
+      if (registrations.length)
+      {
+        for(let registration of registrations)
+        {
+          registration.unregister();
+        }
+      }
+    });
+  }
 }
