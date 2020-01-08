3d75a27286881e327a916f33e6869749d52b1c78
Reza <rm.biqarar@gmail.com>
2020-01-08 17:20:54 +0330

    only get last record of price history

--- lib/db/productprices/get.php
+++ lib/db/productprices/get.php
@@ -5,7 +5,8 @@ class get
 {
 	public static function last_active($_product_id)
 	{
-		$query  = "SELECT * FROM productprices WHERE `product_id` = $_product_id AND `last` = 'yes' AND `enddate` IS NULL ORDER BY `id` DESC LIMIT 1";
+		// $query  = "SELECT * FROM productprices WHERE `product_id` = $_product_id AND `last` = 'yes' AND `enddate` IS NULL ORDER BY `id` DESC LIMIT 1";
+		$query  = "SELECT * FROM productprices WHERE `product_id` = $_product_id ORDER BY `id` DESC LIMIT 1";
 		$result = \dash\db::get($query, null, true);
 		return $result;
 	}
