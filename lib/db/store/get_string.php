119ae39a7cc51e3876c698777d63650b319f6390
Reza <rm.biqarar@gmail.com>
2020-01-08 17:20:29 +0330

    add get string query for update db version

--- lib/db/store/get_string.php
+++ lib/db/store/get_string.php
@@ -19,5 +19,12 @@ class get_string
 		$query = "UPDATE store_data SET store_data.logo = '$_logo' WHERE store_data.id = $_store_id LIMIT 1";
 		return $query;
 	}
+
+
+	public static function update_db_version($_version, $_versiondate, $_store_id)
+	{
+		$query = "UPDATE jibres.store_data SET jibres.store_data.dbversion = '$_version', jibres.store_data.dbversiondate = '$_versiondate' WHERE jibres.store_data.id = $_store_id LIMIT 1";
+		return $query;
+	}
 }
 ?>
