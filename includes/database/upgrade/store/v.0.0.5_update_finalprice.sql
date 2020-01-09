37636216ecefb1a39a8b716485d04092e93a7601
Reza <rm.biqarar@gmail.com>
2020-01-09 19:48:46 +0330

    fix final price

--- includes/database/upgrade/store/v.0.0.5_update_finalprice.sql
+++ includes/database/upgrade/store/v.0.0.5_update_finalprice.sql
@@ -1,2 +1,3 @@
 SET sql_mode = 'NO_UNSIGNED_SUBTRACTION';
+UPDATE  `jibres_XXXXXXX`.`productprices` SET productprices.price = productprices.compareatprice;
 UPDATE  `jibres_XXXXXXX`.`productprices` SET productprices.finalprice = productprices.price - productprices.discount;
