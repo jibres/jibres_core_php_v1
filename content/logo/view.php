7ebd20eea6da5cc03a567dee3dd7bf27bd5d0de5
Javad Adib <J.Evazzadeh@live.com>
2020-01-14 13:57:27 +0330

    logo page

--- content/logo/view.php
+++ content/logo/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Jibres Logo'));
-		\dash\data::page_desc(T_('Need know more about Jibres Logo? We are not choose our final logo yet!'));
+		\dash\data::page_desc(T_('Know more about Jibres Logo'). ' '.  \dash\data::site_slogan());
 	}
 }
 ?>
