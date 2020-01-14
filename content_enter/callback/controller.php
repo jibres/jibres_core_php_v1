94a0d52500f0920a333fdea9b9feafd5c2b775b7
Javad Adib <J.Evazzadeh@live.com>
2020-01-14 11:28:17 +0330

    update enter uid

--- content_enter/callback/controller.php
+++ content_enter/callback/controller.php
@@ -7,7 +7,7 @@ class controller
 	public static function routing()
 	{
 		// 10002000200251
-		if(!\dash\request::get('service') || \dash\request::get('uid') != '201708111')
+		if(!\dash\request::get('service') || \dash\request::get('uid') != '20200114')
 		{
 			\dash\header::status(404, T_("Invalid url"));
 		}
