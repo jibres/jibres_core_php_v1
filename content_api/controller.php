7fa73f6038c4a7d7a5d2c0bd36a16e00a4f9cdb7
Javad Adib <J.Evazzadeh@live.com>
2020-01-20 18:58:33 +0330

    remove redirect api

--- content_api/controller.php
+++ content_api/controller.php
@@ -6,8 +6,6 @@ class controller
 {
 	public static function routing()
 	{
-		self::check_subdomain_and_content();
-
 		if(\dash\url::module() === 'v1')
 		{
 			\content_api\v1\check::basic_api_detail();
@@ -15,21 +13,6 @@ class controller
 		if(\dash\url::module() === 'v2')
 		{
 			\content_api\v2\check::basic_api_detail();
-		}
-	}
-
-
-	private static function check_subdomain_and_content()
-	{
-		// replace api form content to subdomain
-		if(\dash\url::subdomain() === null)
-		{
-			$newApiAddress = \dash\url::set_subdomain('api');
-			if(\dash\url::directory())
-			{
-				$newApiAddress .= '/'. \dash\url::directory();
-			}
-			\dash\redirect::to($newApiAddress);
 		}
 	}
 }
