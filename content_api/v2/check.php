4830f67f84c78b8cb4e55420381dad25bdcb5728
Javad Adib <J.Evazzadeh@live.com>
2020-01-20 16:10:21 +0330

    on api redirect content to subdomain

---
+++ content_api/v2/check.php
@@ -0,0 +1,15 @@
+<?php
+namespace content_api\v2;
+
+
+class check
+{
+	public static function basic_api_detail()
+	{
+		// if(!in_array(\dash\url::subdomain(), ['developers', 'api', 'core', null]))
+		// {
+		// 	\dash\redirect::remove_subdomain();
+		// }
+	}
+}
+?>
