a5abcc712eb5d093c3cd98da877e95856e08a814
Javad Adib <J.Evazzadeh@live.com>
2020-01-20 14:51:41 +0330

    transfer check inside v1

--- content_api/controller.php
+++ content_api/controller.php
@@ -6,45 +6,10 @@ class controller
 {
 	public static function routing()
 	{
-
-		if(!in_array(\dash\url::subdomain(), ['developers', null]))
+		if(\dash\url::module() === 'v1')
 		{
-			\dash\redirect::remove_subdomain();
+			\content_api\v1\check::basic_api_detail();
 		}
-
-		$module = \dash\url::module();
-
-		if(!$module || ($module === 'doc' && !\dash\url::child()) || (in_array($module, ['v1', 'v2'])))
-		{
-			// nothing
-		}
-		else
-		{
-			\dash\header::status(404, T_("Invalid url"));
-		}
-
-		// redirect api/doc to api/{last version of api}/doc
-		if($module === 'doc' && !\dash\url::child())
-		{
-			\dash\redirect::to(\dash\url::here(). '/v1/doc');
-		}
-
-
-		if($module === 'v1')
-		{
-			\content_api\v1\tools::master_check();
-		}
-		elseif($module === 'v2')
-		{
-			// \content_api\v1\tools::master_check();
-		}
-		elseif($module)
-		{
-			\dash\header::status(404);
-		}
-
-		// save api log
-		\dash\app\apilog::start();
 	}
 }
 ?>
