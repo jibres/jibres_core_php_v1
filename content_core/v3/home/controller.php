b32adb40f26fe5fed69d316d177563511e03a88c
Javad Adib <J.Evazzadeh@live.com>
2020-01-20 17:49:40 +0330

    add location module

--- content_core/v3/home/controller.php
+++ content_core/v3/home/controller.php
@@ -13,12 +13,18 @@ class controller
 				'website'  => 'https://jibres.com',
 				'endpoint' => 'https://core.jibres.com/v3',
 				'doc'      => 'https://core.jibres.com/v3/doc',
+				'direction' => 'ltr',
+				'name'    => 'English',
+				'localname' => 'English',
 			],
 			'fa' =>
 			[
 				'website'  => 'https://jibres.ir',
 				'endpoint' => 'https://core.jibres.ir/v3',
 				'doc'      => 'https://core.jibres.ir/v3/doc',
+				'direction' => 'rtl',
+				'name'    => 'Persian',
+				'localname' => 'فارسی',
 			],
 		];
 
