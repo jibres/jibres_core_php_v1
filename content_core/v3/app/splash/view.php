70e4f8909fd8628a046b9433215719e1af6b2bd9
Javad Adib <J.Evazzadeh@live.com>
2020-01-24 13:29:17 +0330

    add splash data

---
+++ content_core/v3/app/splash/view.php
@@ -0,0 +1,33 @@
+<?php
+namespace content_core\v3\app\splash;
+
+
+class view
+{
+	public static function config()
+	{
+		$result =
+		[
+			'logo'       => \dash\url::icon(),
+			'style'      => 1,
+			'title'      => T_('Jibres'),
+			'desc'       => T_('Sell and Enjoy'),
+			'meta'       => 'Powered by Ermile',
+
+			'bg' =>
+			[
+				'from' => '#4173cc',
+				'to'   => '#1da1f3',
+			],
+
+			'color' =>
+			[
+				'primary' => '#fff',
+				'master'  => '#eee',
+			],
+		];
+
+		\dash\notif::api($result);
+	}
+}
+?>
