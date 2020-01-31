538904964641cdaf5066f7fd188004bb30bb3bc1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-01-31 21:10:47 +0330

    update jibres catalog to v6

--- content/catalog/controller.php
+++ content/catalog/controller.php
@@ -6,7 +6,7 @@ class controller
 {
 	public static function routing()
 	{
-		$lastVersion = '2.0';
+		$lastVersion = '6.0';
 		$dlLink      = \dash\url::base().'/static/catalog/Jibres-Catalog-v'. $lastVersion. '-preview.pdf';
 
 		\dash\redirect::to($dlLink);
