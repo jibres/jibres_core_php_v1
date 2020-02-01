942ee2067a0fcda651d923beccf39608d0366a0c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-01 15:10:41 +0330

    add new vcard

---
+++ content/vcard/reza/controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace content\vcard\reza;
+
+
+class controller
+{
+	public static function routing()
+	{
+		\dash\redirect::to(\dash\url::static(). '/vcard/Jibres-vCard-Reza-Mohiti.vcf');
+	}
+}
+?>
