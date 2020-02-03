a01a5934393a862c3fb11fa2038829ea4424611e
Reza <rm.biqarar@gmail.com>
2020-02-03 15:46:30 +0330

    add dash setting payment library

---
+++ dash/setting/mellat.php
@@ -0,0 +1,50 @@
+<?php
+namespace dash\setting;
+
+
+class mellat
+{
+
+	private static $load = null;
+
+
+	private static function load()
+	{
+		if(self::$load === null)
+		{
+			$json = \dash\file::read(__DIR__. '/secret/payment_mellat.secret.json');
+			if($json && is_string($json))
+			{
+				$json = json_decode($json, true);
+			}
+
+			if(!is_array($json))
+			{
+				$json = [];
+			}
+			self::$load = $json;
+
+		}
+
+	}
+
+
+	public static function get($_key)
+	{
+
+		self::load();
+
+		if($_key === 'all')
+		{
+			return self::$load;
+		}
+
+		if(array_key_exists($_key, self::$load))
+		{
+			return self::$load[$_key];
+		}
+
+		return null;
+	}
+}
+?>
