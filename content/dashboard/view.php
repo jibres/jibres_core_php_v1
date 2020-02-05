c1bf22415e6e0b5f0af5af451c54562d62d9927e
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-05 15:52:20 +0330

    add simple dashboard

---
+++ content/dashboard/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\dashboard;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Jibres Dashboard'));
+		\dash\data::page_desc(T_('Sell and Enjoy'));
+	}
+}
+?>
