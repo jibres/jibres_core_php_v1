ae2ce53b9c90389d7601dd437fef733b5f7ee1f1
Reza <rm.biqarar@gmail.com>
2020-02-06 08:45:24 +0330

    send nic curl to broker

--- lib/nic/exec/run.php
+++ lib/nic/exec/run.php
@@ -6,8 +6,6 @@ class run
 {
 	public static function send($_xml)
 	{
-		$certFolder = root. 'dash/setting/secret/pem/nic/'. 'test1/';
-
 
 		$_xml = trim($_xml);
 
@@ -17,35 +15,26 @@ class run
 		//FALSE to stop cURL from verifying the peer's certificate
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 
-		curl_setopt($ch, CURLOPT_SSLCERT, $certFolder.'file2.pem');
-
-		// curl_setopt($ch, CURLOPT_SSLCERT, $certFolder.'file1.crt.pem');
-		// curl_setopt($ch, CURLOPT_SSLKEY, $certFolder.'file1.key.pem');
-		// curl_setopt($ch, CURLOPT_SSLCERTPASSWD, self::token());
-		// curl_setopt($ch, CURLOPT_SSLKEYPASSWD, self::token());
-
 		//TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-		//The contents of the "User-Agent: "
-		curl_setopt($ch, CURLOPT_USERAGENT, "IRNIC_EPP_Client_Sample");
-		// curl_setopt($ch, CURLOPT_USERAGENT, "User-Agent: JIbres");
-		//TRUE to do a regular HTTP POST.This POST is the normal application/x-www-form-urlencoded kind, most commonly used by HTML forms.
+
 		curl_setopt($ch, CURLOPT_POST, false);
 		//The URL to fetch.
-		curl_setopt($ch, CURLOPT_URL,"https://epp.nic.ir/submit");
+		curl_setopt($ch, CURLOPT_URL,"http://7.7.7.25/nic/");
 		//The full data to post in a HTTP "POST" operation.
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(['xmlStr' => $_xml]));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(['xml' => $_xml]));
 
 		// grab URL and pass it to the browser
 		$response = curl_exec($ch);
-		var_dump(curl_error($ch));
-		var_dump($response);exit();
+
 		if($response === false)	{
 			// echo Errors
 			echo 'Curl error: ' . curl_error($ch);
 		}
 		// close cURL resource, and free up system resources
 		curl_close ($ch);
+
+		return $response;
 
 
 	}
