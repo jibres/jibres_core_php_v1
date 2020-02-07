06fe5a1b43dc3b22c9fe0a9594d59ed3561d7a20
Reza <rm.biqarar@gmail.com>
2020-02-07 17:47:55 +0330

    add buy page

--- content_domain/buy/model.php
+++ content_domain/buy/model.php
@@ -6,23 +6,22 @@ class model
 {
 	public static function post()
 	{
-		$domain = \dash\request::post('domain');
-		if(!$domain)
+		$post =
+		[
+			'domain' => \dash\data::myDomain(),
+			'irnic'  => \dash\request::post('irnic'),
+			'period' => \dash\request::post('period'),
+			'ns1'    => \dash\request::post('ns1'),
+			'ns2'    => \dash\request::post('ns2'),
+			'ns3'    => \dash\request::post('ns3'),
+			'ns4'    => \dash\request::post('ns4'),
+		];
+
+		if(!\dash\request::post('agree'))
 		{
-			\dash\notif::error(T_("Please fill domain"), 'domain');
+			\dash\notif::warn(T_("Please view the privacy polici and check 'I agree' check box"), 'agree');
 			return false;
 		}
-
-		if(!\lib\app\nic_domain\check::syntax($domain))
-		{
-			\dash\notif::error(T_("Please enter a valid domain"), 'domain');
-			return false;
-		}
-
-		$url = \dash\url::this(). '/'. $domain;
-
-		\dash\redirect::to($url);
-
 
 	}
 }
