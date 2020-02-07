fd5915b65504883e8e8df3497f6c312850c4edbf
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-07 17:59:57 +0330

    add press and brand page

---
+++ content/brand/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\brand;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Jibres Brand'));
+		\dash\data::page_desc(T_('Need HELP? Be patient...'));
+	}
+}
+?>
---
+++ content/press/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\press;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Press'));
+		\dash\data::page_desc(T_('Need HELP? Be patient...'));
+	}
+}
+?>
