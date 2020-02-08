b70a3adc0ab297fe03f1d868e68a6997bea2a090
Reza <rm.biqarar@gmail.com>
2020-02-08 14:14:00 +0330

    bug fix domain

--- lib/nic/exec/run.php
+++ lib/nic/exec/run.php
@@ -27,7 +27,7 @@ class run
 
 		curl_setopt($ch, CURLOPT_POST, false);
 		//The URL to fetch.
-		curl_setopt($ch, CURLOPT_URL,"https://tunnel.jibres.ir/nic-broker");
+		curl_setopt($ch, CURLOPT_URL,"https://tunnel.jibres.ir/nic-broker/");
 
 		//The full data to post in a HTTP "POST" operation.
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
