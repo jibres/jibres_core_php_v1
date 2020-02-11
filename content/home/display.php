845b7eb5cc1bdf08b684ac76ed968a514bbad21b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-11 18:55:42 +0330

    fit top of homepage

--- content/home/display.php
+++ content/home/display.php
@@ -4,20 +4,20 @@
   <div class="h2"><span class="typed"></span></div>
 
   <div id="typed-strings" class="hide">
-    <h3>{%trans "Invoice Software"%}</h3>
-    <h4>{%trans "Easy Invoicing Software"%}</h4>
-    <h3>{%trans "Online Invoicing Software"%}</h3>
-    <h2>{%trans "Free Invoicing Software"%}</h2>
+    <h3><?php echo T_('Invoice Software'); ?></h3>
+    <h4><?php echo T_('Easy Invoicing Software'); ?></h4>
+    <h3><?php echo T_('Online Invoicing Software'); ?></h3>
+    <h2><?php echo T_('Free Invoicing Software'); ?></h2>
 
-    <h3>{%trans "Accounting Software"%}</h3>
-    <h2>{%trans "Online Accounting Software"%}</h2>
+    <h3><?php echo T_('Accounting Software'); ?></h3>
+    <h2><?php echo T_('Online Accounting Software'); ?></h2>
 
-    <h3>{%trans "Sales"%}</h3>
-    <h3>{%trans "Sales Software"%}</h3>
-    <h4>{%trans "Integrated Sales"%}</h4>
-    <h2 class="bold">{%trans "Integrated Ecommerce Platform"%}</h2>
+    <h3><?php echo T_('Sales'); ?></h3>
+    <h3><?php echo T_('Sales Software'); ?></h3>
+    <h4><?php echo T_('Integrated Sales'); ?></h4>
+    <h2 class="bold"><?php echo T_('Integrated Ecommerce Platform'); ?></h2>
   </div>
-  <h3>{{page.desc2}}</h3>
+  <h3><?php echo \dash\data::page_desc2(); ?></h3>
  </div>
 
 </section>
