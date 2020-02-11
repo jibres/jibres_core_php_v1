6d5eb316baac0f31fe948a7117ef7ba07b7e84b7
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-11 19:33:32 +0330

    add channels

--- content/home/display.php
+++ content/home/display.php
@@ -18,6 +18,51 @@
     <h2 class="bold"><?php echo T_('Integrated Ecommerce Platform'); ?></h2>
   </div>
   <h3><?php echo \dash\data::page_desc2(); ?></h3>
- </div>
 
+ </div>
+</section>
+
+
+<section id='saleChannels'>
+ <div class="cn">
+  <div class="title">
+   <h2><?php echo T_('Jibres Sale Channels'); ?></h2>
+   <p><?php echo T_('Sales channels represent the different marketplaces where you sell your products.'); ?><br><?php echo T_('By use each sales channel on Jibres, you can keep track of your products, orders, and customers in one place.'); ?> <a href="{{url.kingdom}}/about"><?php echo T_('Read more'); ?></a></p>
+  </div>
+
+  <div class="f">
+    <div class="c3 m6 s12">
+      <div class="item">
+        <img src="<?php echo \dash\url::icon(); ?>" alt="<?php echo T_('Jibres'); ?>">
+        <h3><?php echo T_('Point of Sale Software'); ?></h3>
+        <p><?php echo T_('Barcode reader'). T_(','). T_('Receipt printer'). T_(','). T_('PC POS'). T_(','). T_('Label Printing Scale'). T_(','). T_('Invoice Software'); ?></p>
+      </div>
+    </div>
+
+    <div class="c3 m6 s12">
+      <div class="item">
+        <img src="<?php echo \dash\url::icon(); ?>" alt='<?php echo T_('Jibres'); ?>'>
+        <h3><?php echo T_('Mobile Online Store'); ?></h3>
+        <p><?php echo T_('Create mobile app for your online store.'); ?> <?php echo T_('Free'). T_(','). T_('Fully Customizable'); ?></p>
+      </div>
+    </div>
+
+    <div class="c3 m6 s12">
+      <div class="item">
+        <img src="<?php echo \dash\url::icon(); ?>" alt='<?php echo T_('Jibres'); ?>'>
+        <h3><?php echo T_('Social Marketing'); ?></h3>
+        <p><?php echo T_('Easily add ecommerce to any website and social networks by embedding a single buy button.'); ?></p>
+      </div>
+    </div>
+
+    <div class="c3 m6 s12">
+      <div class="item">
+        <img src="<?php echo \dash\url::icon(); ?>" alt='<?php echo T_('Jibres'); ?>'>
+        <h3><?php echo T_('Online Store Website'); ?></h3>
+        <p><?php echo T_('Online store builder allow you robust your business in a faster way, simpler way!'); ?></p>
+      </div>
+    </div>
+
+  </div>
+ </div>
 </section>
