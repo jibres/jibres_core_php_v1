ae9d5533b6d1a6d77783bc1ad25b03943bc970e3
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-12 17:19:27 +0330

    fix header topNav

--- content/home/layout/header.php
+++ content/home/layout/header.php
@@ -1,35 +1,61 @@
- <header class='site-header' data-scroll>
-  <div class="cn f">
-
-   <h1 class="cauto logo">
-    <a class="flex" href='<?php echo \dash\url::kingdom() ?>/' tabindex='1'>
-     <img src='<?php echo \dash\url::icon();?>' alt='<?php echo \dash\data::site_title(). ' | '. \dash\data::site_desc(); ?>'>
-     <span><?php echo \dash\data::site_title(); ?></span> <small class="fs05 pLa5 s0"> <?php echo T_('Beta');?></small>
-    </a>
-   </h1>
-   <div class="c"></div>
-   <nav class="cauto os">
-    <a class="s0" href="<?php echo \dash\url::kingdom() ?>/pricing"><?php echo T_("Pricing"); ?></a>
-    <a class="s0" href="<?php echo \dash\url::kingdom() ?>/contact"><?php echo T_("Contact"); ?></a>
+ <div id="topBar">
+  <div class="cn">
+   <div class="f">
+    <nav class="cauto os">
 <?php
 if (\dash\user::id())
 {
-  if (\dash\url::subdomain())
-  {
-    echo '<a href="'. \dash\url::sitelang(). '/a" data-direct class="btn">'. T_("Store Panel"). '</a>';
-  }
-  else
-  {
-    echo '<a href="'. \dash\url::sitelang(). '/store" data-direct class="btn">'. T_("Admin Panel"). '</a>';
-  }
+ if (\dash\url::subdomain())
+ {
+  echo '     <a href="'. \dash\url::sitelang(). '/a" data-direct>'. T_("Store Panel"). '</a>'. "\n";
+ }
+ else
+ {
+  echo '     <a href="'. \dash\url::sitelang(). '/store" data-direct>'. T_("Admin Panel"). '</a>'. "\n";
+ }
 
 }
 else
 {
-  echo '<a href="'. \dash\url::sitelang(). '/enter" data-direct class="btn">'. T_("Enter"). '</a>';
+ echo '     <a href="'. \dash\url::sitelang(). '/enter" data-direct>'. T_("Enter"). '</a>'. "\n";
+ echo '     <a href="'. \dash\url::sitelang(). '/enter/signup" data-direct>'. T_("SIGN UP"). '</a>'. "\n";
 }
 ?>
+    </nav>
+   </div>
+  </div>
+ </div>
+
+  <div class="cn f">
+
+   <h1 class="cauto logo">
+   <a class="flex" href='<?php echo \dash\url::kingdom() ?>/' tabindex='1'>
+    <img src='<?php echo \dash\url::icon();?>' alt='<?php echo \dash\data::site_title(). ' | '. \dash\data::site_desc(); ?>'>
+    <span><?php echo \dash\data::site_title(); ?></span> <small class="fs05 pLa5 s0"> <?php echo T_('Beta');?></small>
+   </a>
+   </h1>
+   <div class="c"></div>
+   <nav class="cauto os">
+   <a class="s0" href="<?php echo \dash\url::kingdom() ?>/pricing"><?php echo T_("Pricing"); ?></a>
+   <a class="s0" href="<?php echo \dash\url::kingdom() ?>/contact"><?php echo T_("Contact"); ?></a>
+ <?php
+ if (\dash\user::id())
+ {
+  if (\dash\url::subdomain())
+  {
+   echo '<a href="'. \dash\url::sitelang(). '/a" data-direct class="btn">'. T_("Store Panel"). '</a>';
+  }
+  else
+  {
+   echo '<a href="'. \dash\url::sitelang(). '/store" data-direct class="btn">'. T_("Admin Panel"). '</a>';
+  }
+
+ }
+ else
+ {
+  echo '<a href="'. \dash\url::sitelang(). '/enter" data-direct class="btn">'. T_("Enter"). '</a>';
+ }
+ ?>
    </nav>
   </div>
- </header>
 
