ba78dfbc87a3eccc7f96a9f3a69a71b2301fa97f
Reza <rm.biqarar@gmail.com>
2020-02-13 09:43:34 +0330

    convert socialresponsibility to php

---
+++ content/socialresponsibility/display.php
@@ -0,0 +1,22 @@
+
+
+  <p class="msg success txtC"><?php echo T_("Social responsibility refers to our role in maintaining, caring about and helping our society, while having set as its goal a responsibility-centered enterprise along with wealth production."); ?></p>
+
+  <p><?php echo T_("The issue of the social responsibility of organizations and corporations towards the society is from among the important issues being focused on in recent years."); ?> <?php echo T_("The social responsibility of organizations relates to the organization's responsibility towards society, human beings and the environment in which they are active."); ?></p>
+
+  <p><?php echo T_("Based on the mentioned definitions of social responsibility, Jibres considers itself committed to society. Accordingly, besides attempting to offer creative and effective services, Jibres has invariably had social responsibility as one of its most important missions and, God willing, will continue to do so."); ?></p>
+
+  <h2 id="environmentalist"><?php echo T_("Environmentalist"); ?></h2>
+  <p><?php echo T_("In spite of technological advancements, paper usage is unfortunately preferred to modern ways. One of Jibres's objectives is trying to change this habit and contribute to the elimination of paper from the routine life and, therefore, to save the environment."); ?></p>
+
+  <h2 id="philanthropist-activities"><?php echo T_("Philanthropist Activities"); ?></h2>
+  <p><?php echo T_("One of the most valuable resources of charity organizations is the voluntary and active participation of the individuals who, without any expectations, provide them with their skillfulness and capital."); ?></p>
+  <p><?php echo T_("Jibres as well, with regard to its contribution to philanthropist activities, will honorably provide them with its services free of charge. To make use of these services, send your identity documents along with your request to Jibres's support center."); ?></p>
+
+  <h2 id="recruiting-motivated-staff"><?php echo T_("Recruiting Motivated Staff"); ?></h2>
+  <p><?php echo T_("An outstanding characteristic of any organization is its specialized and highly motivated staff, playing a vital role in growth and development."); ?> <?php echo T_("By the same token, we are always looking for creative and motivated Iranian youths in order to make the best and be effective in cooperation with them."); ?> <?php echo T_("Join us and grow in a different location."); ?></p>
+
+
+  <h2 id="customer-care-and-complete-satisfaction"><?php echo T_("Customer Care and Complete Satisfaction"); ?></h2>
+  <p><?php echo T_("Attracting people's participation as well as having their valuable presence has always been a great honor for Jibres and to appreciate your support, in return, Jibres presents services to promote customer satisfaction."); ?> <?php echo T_("These services are presented in different time intervals and are aimed at costomer care mission and appreciation of your valuable presence."); ?></p>
+
