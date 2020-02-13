949e668f9339b2f4bfdd6d1f06fb1474f9da6dfd
Reza <rm.biqarar@gmail.com>
2020-02-13 09:53:23 +0330

    conver whois to php

---
+++ content/whois/display.php
@@ -0,0 +1,43 @@
+
+<div class="f justify-center">
+	<div class="c6 s12">
+		<form method="post" autocomplete="off">
+			<div class="input ltr">
+				<input type="text" name="domain" placeholder='<?php echo T_("Domain"); ?>' value="<?php echo \dash\data::myDomain(); ?>">
+				<button class="btn addon success"><?php echo T_("Check domain"); ?></button>
+			</div>
+		</form>
+		<?php
+		if(\dash\data::domainError())
+		{
+			echo '<div class="msg danger mT20">'. \dash\data::domainError().'</div>';
+		}
+		?>
+	</div>
+</div>
+<?php
+if(\dash\data::whoisResult())
+{
+?>
+
+
+<div class="f justify-center mT20">
+
+	<div class="c8 s12 mT10">
+
+		<?php
+		if(\dash\data::whoisResult_available())
+		{
+			echo '<div class="msg success">'. T_("Domain is available"). '</div>';
+		}
+		?>
+
+		<pre>
+			<?php echo \dash\data::whoisResult_answer(); ?>
+		</pre>
+	</div>
+</div>
+
+<?php
+} // endif
+?>
