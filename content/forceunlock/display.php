d30eda30db9ed6e359f62d5f0f9300dbd752faae
Reza <rm.biqarar@gmail.com>
2020-02-13 08:44:33 +0330

    convert unlock page to php

---
+++ content/forceunlock/display.php
@@ -0,0 +1,24 @@
+
+
+<div class="f justify-center txtC mB50">
+	<div class="c6 s12 fs18">
+		<div class="mB10"><?php echo T_("Start lock"); ?></div>
+		<br>
+		<div class="mB10"><?php echo \dash\fit::date(\dash\data::startLock()); ?></div>
+		<br>
+		<div class="mB10"><?php echo \dash\fit::date(\dash\data::startLock()); ?></div>
+
+	</div>
+</div>
+
+<div class="f justify-center txtC">
+
+	<div class="c6 s12">
+		<div data-confirm data-data='{"unlock" : "unlock"}' class="btn success block xl">
+			<?php echo T_("Force unlock"); ?>
+		</div>
+	</div>
+
+
+</div>
+
