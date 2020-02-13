b24c78ee4151590fae9514439b89e689a468f270
Reza <rm.biqarar@gmail.com>
2020-02-13 09:47:16 +0330

    convert terms to php

---
+++ content/terms/display.php
@@ -0,0 +1,20 @@
+
+
+   <p><?php echo T_("Utilizing Jibres's services means the acceptance of and commitment to observing all the tenors of this agreement."); ?></p>
+
+   <ul class="list">
+    <li><?php echo T_("It is worth mentioning that due to the insecurity of cyberspace, you should never post your vital information on either Jibres or any other services!"); ?></li>
+    <li><?php echo T_("User's personal information is strictly confidential in our service. Jibres preserves the information as encoded and will not under any conditions transfer it to another person."); ?></li>
+    <li><?php echo T_("In case there is a request for receiving user's information by the competent authorities, according to the country the user lives in, Jibres will cooperate with them only upon receiving the judicial order issued by the country submitting the request."); ?></li>
+    <li><?php echo T_("Jibres will not offer any guarantee as to the quality of the products or services presented by the stores and will not accept any responsibility in this regard!"); ?></li>
+    <li><?php echo T_("Users should assume responsibility for any harm, both material and immaterial, caused by direct, indirect or penal factors and due to their utilization of this service; and, Jibres bears no responsibility accordingly."); ?></li>
+    <li><?php echo T_("Jibres accepts no responsibility, under any conditions, for the harms caused by users' mutual trust in each other or by the disclosure of information by users."); ?></li>
+    <li><?php echo T_("Jibres will do its best to protect both the users' information and the service; however, regarding the lack of certainty within the cyberspace, it will not assume any responsibility for the loss of the information entered on the service."); ?></li>
+    <li><?php echo T_("We will publicize our information and news only via Jibres's formal website. Accordingly, those who publicize any attributed news and claims, within social media and networks, must assume the responsibility and Jibres will not assume any responsibility."); ?></li>
+    <li><?php echo T_("Jibres will bear no responsibility for whatsoever messages sent to users, including those indicating winning a lottery, and all our information dissemination will be through the ways of Jibres's contact with users."); ?></li>
+    <li><?php echo T_("Any misuse of Jibres's trade name is prohibited and will be suable by making a complaint to the legal competent authorities."); ?></li>
+    <li><?php echo T_("Furthermore, this agreement, under the name Terms and Conditions, will be available and observable, in all pages of the website. Users will be responsible for a lack of information on the most recent changes."); ?></li>
+   </ul>
+
+   <p class="msg primary txtC"><?php echo T_("It should be mentioned that in case there would be any modifications in the above-mentioned tenors, an updated version will be available for the public, and as well, all the users will be informed."); ?></p>
+
