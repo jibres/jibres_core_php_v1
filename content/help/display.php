ab05bda61c801dd632266e9af8910817c108f120
Reza <rm.biqarar@gmail.com>
2020-02-13 08:57:37 +0330

    convert help page to php

---
+++ content/help/display.php
@@ -0,0 +1,3 @@
+<?php
+echo T_("Soon");
+?>
---
+++ content/help/faq/display.php
@@ -0,0 +1,55 @@
+
+  <div class="faq">
+
+    <h3><?php echo T_("General questions"); ?></h3>
+    <ul>
+     <li>
+      <h4><?php echo T_("How does Jibres work?"); ?></h4>
+      <p><?php echo T_("The easiest way to learn how to use Jibres is enter to it, which takes less than 3 minutes to setup your team."); ?></p>
+     </li>
+     <li>
+      <h4><?php echo T_("What is your privacy and security policy?"); ?></h4>
+      <p><?php echo T_("View Jibres's privacy and security policy at"); ?> <a href='{{url.here}}/privacy'>{{url.here}}/privacy</a></p>
+     </li>
+     <li>
+      <h4><?php echo T_("Where can I find your Terms of Service (TOS)?"); ?></h4>
+      <p><?php echo T_("You can find them at"); ?>  <a href='{{url.here}}/terms'>{{url.here}}/terms</a></p>
+     </li>
+     <li>
+      <h4><?php echo T_("What are your bandwidth fees?"); ?></h4>
+      <p><?php echo T_("There are none. All Jibres plans include unlimited bandwidth for free."); ?></p>
+     </li>
+     <li>
+      <h4><?php echo T_("Do I need a web host?"); ?></h4>
+      <p><?php echo T_("No! Jibres includes secure, unlimited hosting on all plans with free bandwith."); ?></p>
+     </li>
+    </ul>
+
+
+    <h3><?php echo T_("Billing & Invoicing"); ?></h3>
+     <ul>
+      <li>
+       <h4><?php echo T_("Is there a setup fee?"); ?></h4>
+       <p><?php echo T_("No. There are no setup fees on any of our plans!"); ?></p>
+      </li>
+
+      <li>
+       <h4><?php echo T_("Can I cancel my account at any time?"); ?></h4>
+       <p><?php echo T_("Yes. If you ever decide that Jibres isn’t the best platform for your business, simply cancel your account."); ?></p>
+      </li>
+
+      <li>
+       <h4><?php echo T_("How long are your contracts?"); ?></h4>
+       <p><?php echo T_("All Jibres plans are month to month. simple."); ?></p>
+      </li>
+      <li>
+       <h4><?php echo T_("Can I change my plan later on?"); ?></h4>
+       <p><?php echo T_("Absolutely! You can upgrade or downgrade your plan at any time."); ?></p>
+
+      <li>
+       <h4><?php echo T_("When is my billing date?"); ?></h4>
+       <p><?php echo T_("The date you first select a paid plan will be the recurring billing date. For example: If you sign up for the first time on July 15, all future charges will be billed on the 15th of every month."); ?></p>
+      </li>
+    </ul>
+
+  </div>
