b9e4ad7a42ca289cca9f3f8c8b1c0c7b0954730b
Reza <rm.biqarar@gmail.com>
2020-02-13 08:42:08 +0330

    convert enterprise to php

---
+++ content/enterprise/display.php
@@ -0,0 +1,3 @@
+<p class="msg warn2 txtC"><?php echo T_("Big companies simply don’t work like small companies, and they don’t use Jibres the same way either. That’s why there’s Enterprise."); ?></p>
+
+
