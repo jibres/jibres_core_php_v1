6e351f8fc67d50a8248297691f63231ad1a2137f
Reza <rm.biqarar@gmail.com>
2020-02-13 08:23:00 +0330

    remove useless code

--- content/careers/controller.php
+++ content/careers/controller.php
@@ -6,10 +6,6 @@ class controller
 	public static function routing()
 	{
 		\dash\redirect::to('https://sarshomar.com/fa/s/3L2fP', false);
-		if(\dash\url::child() === 'get' && \dash\url::subchild() === null)
-		{
-			\dash\open::get();
-		}
 	}
 }
 ?>
