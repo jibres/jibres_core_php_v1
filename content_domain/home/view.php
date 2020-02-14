a75188545f07146c72c3861095a16a7af5c4d9c4
Reza <rm.biqarar@gmail.com>
2020-02-14 17:07:48 +0330

    search in domain

--- content_domain/home/view.php
+++ content_domain/home/view.php
@@ -11,8 +11,32 @@ class view
 		\dash\data::page_special(true);
 
 
-		$list = \lib\app\nic_domain\search::my_list();
+
+		$args =
+		[
+			'order'  => \dash\request::get('order'),
+			'sort'   => \dash\request::get('sort'),
+			// 'admin'  => \dash\request::get('admin'),
+			// 'holder' => \dash\request::get('holder'),
+			// 'tech'   => \dash\request::get('tech'),
+			// 'bill'   => \dash\request::get('bill'),
+		];
+
+		$search_string = \dash\request::get('q');
+
+		$list = \lib\app\nic_domain\search::list($search_string, $args);
 		\dash\data::dataTable($list);
+
+		\dash\data::filterBox(\lib\app\nic_domain\search::filter_message());
+
+		$isFiltered = \lib\app\nic_domain\search::is_filtered();
+
+		\dash\data::isFiltered($isFiltered);
+
+		if($isFiltered)
+		{
+			\dash\data::page_title(\dash\data::page_title() . '  '. T_('Filtered'));
+		}
 	}
 }
 ?>
