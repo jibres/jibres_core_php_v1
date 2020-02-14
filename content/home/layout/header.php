a253dfc621549b9f6efe76c76f883a1c0f01bbb2
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-14 15:57:34 +0330

    clean header

--- content/home/layout/header.php
+++ content/home/layout/header.php
@@ -2,8 +2,7 @@
    <div class="fit">
     <div class="f">
      <div class="cauto s12">
-      <a class="logo" href='<?php echo \dash\url::kingdom() ?>/'>
-       <img <?php
+      <a class="logo" href='<?php echo \dash\url::kingdom() ?>/'><img <?php
         if (\dash\language::current() === 'fa')
         {
          echo "src='". \dash\url::static(). "/logo/fa/svg/Jibres-Logo-fa.svg". "' alt='". T_('Jibres Logo'). "'";
@@ -12,17 +11,14 @@
         {
          echo "src='". \dash\url::static(). "/logo/en/svg/Jibres-Logo-en.svg". "' alt='". T_('Jibres Logo'). "'";
         }
-       ?>>
-       <h1><?php echo T_("Jibres"); ?></h1>
-      </a>
+       ?>><h1><?php echo T_("Jibres"); ?></h1></a>
      </div>
      <nav class="c s0">
        <a href="<?php echo \dash\url::kingdom() ?>/pricing"><?php echo T_("Pricing"); ?></a>
        <a href="<?php echo \dash\url::kingdom() ?>/domain"><?php echo T_("Domains"); ?></a>
        <a href="<?php echo \dash\url::kingdom() ?>/support"><?php echo T_("Help Center"); ?></a>
      </nav>
-     <div class="cauto s12">
-      <?php
+     <div class="cauto s12"><?php
 if (\dash\user::id())
 {
  if (\dash\url::subdomain())
@@ -40,16 +36,11 @@ else
  echo '<a class="slave" href="'. \dash\url::sitelang(). '/enter" data-direct>'. T_("Enter"). '</a>';
  echo '<a class="master" href="'. \dash\url::sitelang(). '/enter/signup" data-direct>'. T_("SIGN UP"). '</a>';
 }
-?>
-     </div>
+?></div>
     </div>
    </div>
   </div>
-
-
-  <section id='jibresPageTitle'>
-    <div class="typing"><span class="typed"></span></div>
-
+  <section id='jibresPageTitle'><div class="typing"><span class="typed"></span></div>
 <?php
 if (\dash\url::module())
 {
@@ -68,10 +59,8 @@ else
      <h4><?php echo T_('Easy Invoicing Software'); ?></h4>
      <h3><?php echo T_('Online Invoicing Software'); ?></h3>
      <h2><?php echo T_('Free Invoicing Software'); ?></h2>
-
      <h3><?php echo T_('Accounting Software'); ?></h3>
      <h2><?php echo T_('Online Accounting Software'); ?></h2>
-
      <h3><?php echo T_('Sales'); ?></h3>
      <h3><?php echo T_('Sales Software'); ?></h3>
      <h4><?php echo T_('Integrated Sales'); ?></h4>
