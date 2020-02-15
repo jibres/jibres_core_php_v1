fb4e9358f3f8324d89513f3565c716b85050899b
Reza <rm.biqarar@gmail.com>
2020-02-15 20:24:29 +0330

    fix sidebar of account

--- dash/engine/layout/admin/sidebar/sidebar-account.php
+++ dash/engine/layout/admin/sidebar/sidebar-account.php
@@ -1,13 +1,13 @@
 
     <li>
      <ul>
-      <li><a href="{{url.here}}/billing"><i class="sf2-credit-card"></i>{%trans "Billing"%}</a></li>
-      <li><a href="{{url.here}}/my"><i class="sf2-user-close-settings"></i>{%trans "Personal info"%}</a></li>
-      <li><a href="{{url.here}}/my/profile"><i class="sf2-user-profile"></i>{%trans "Edit Profile"%}</a></li>
-      <li><a href="{{url.here}}/my/avatar"><i class="sf2-odnoklassniki"></i>{%trans "Avatar"%}</a></li>
-      <li><a href="{{url.here}}/my/social"><i class="sf2-internet"></i>{%trans "Social Networks"%}</a></li>
-      <li><a href="{{url.here}}/my/address"><i class="sf2-pin"></i>{%trans "Address"%}</a></li>
-      <li><a href="{{url.here}}/security"><i class="sf2-lock"></i>{%trans "Security"%}</a></li>
-      <li><a href="{{url.here}}/security/sessions"><i class="sf2-campfire"></i>{%trans "Active Sessions"%}</a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/billing"><i class="sf2-credit-card"></i><?php echo T_("Billing"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/my"><i class="sf2-user-close-settings"></i><?php echo T_("Personal info"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/my/profile"><i class="sf2-user-profile"></i><?php echo T_("Edit Profile"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/my/avatar"><i class="sf2-odnoklassniki"></i><?php echo T_("Avatar"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/my/social"><i class="sf2-internet"></i><?php echo T_("Social Networks"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/my/address"><i class="sf2-pin"></i><?php echo T_("Address"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/security"><i class="sf2-lock"></i><?php echo T_("Security"); ?></a></li>
+      <li><a href="<?php echo \dash\url::here(); ?>/security/sessions"><i class="sf2-campfire"></i><?php echo T_("Active Sessions"); ?></a></li>
      </ul>
     </li>
