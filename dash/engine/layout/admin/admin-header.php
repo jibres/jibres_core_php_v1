ac2e1ae7f5b791b8cd096f0c264b7a37132e09f5
Reza <rm.biqarar@gmail.com>
2020-02-15 13:36:07 +0330

    fix admin header

--- dash/engine/layout/admin/admin-header.php
+++ dash/engine/layout/admin/admin-header.php
@@ -1,37 +1,41 @@
    <div class="sidenavHandler c0 sauto mauto"><i class="sf-ellipsis-v"></i></div>
 
-   {%block headerTitle%}
-   <a class="cauto logo" href="{{url.kingdom}}" data-direct ><img src="{{site.logo}}" alt='{{site.title}}'></a>
-   <h1 class='c'><a href="{{url.kingdom}}" data-direct>{{site.title}}</a></h1>
-   {%endblock%}
 
-{%block headerMenu%}{%endblock%}
+   <a class="cauto logo" href="<?php echo \dash\url::kingdom(); ?>" data-direct ><img src="<?php echo \dash\data::site_logo(); ?>" alt='<?php echo \dash\data::site_title(); ?>'></a>
+   <h1 class='c'><a href="<?php echo \dash\url::kingdom(); ?>" data-direct><?php echo \dash\data::site_title(); ?></a></h1>
+
+
+
+
    <div class='hm right flex cauto os'>
-{%block headerRight%}
-    <a href="{{url.kingdom}}/support" data-tippy-animation="perspective" title='{%trans "Help Center"%}' class="support s0"><i class="sf-life-ring"></i></a>
 
-{%if user.id%}
-    <a href="{{url.kingdom}}/account/notification" data-tippy-animation="perspective" title='{%trans "Notifications"%}' class="notification"><i class="sf-bell"></i></a>
+    <a href="<?php echo \dash\url::kingdom(); ?>/support" data-tippy-animation="perspective" title='<?php echo T_("Help Center"); ?>' class="support s0"><i class="sf-life-ring"></i></a>
 
-    <div class="profileShow" data-tippy-animation="perspective" title='{{site.title}}<br>{{userBadge.desc | raw}}'
-    data-desc='{{userBadge.desc | raw}}'
-    data-footer='{{userBadge.footer | raw}}'
-    data-confirmTxt='{%trans "Account"%} <i class="sf-user"></i>' data-confirmLink='{{url.kingdom}}/account'
-    data-cancelTxt='{%trans "Logout"%} <i class="sf-out mLa10"></i>'
-    data-logoutConfirmTxt='{%trans "You really want to go?"%}'
-    data-logoutTxt='{%trans "We are waiting for you to come back:)"%} ☺️'
-    data-logoutUrl='{{url.kingdom}}/logout'>
-{%if login.avatar%}
-     <img src="{{login.avatar}}" alt='{%trans "Avatar of you"%} {{login.displayname}}'>
-{%elseif login%}
-     <img src="{{url.static}}/siftal/images/default/avatar.png" alt='{%trans "Default Avatar"%}'>
-{%else%}
-     <img src="{{url.static}}/siftal/images/avatar/guest.png" alt='{%trans "Default Avatar"%}'>
-{%endif%}
+<?php if(\dash\user::id()) { ?>
+    <a href="<?php echo \dash\url::kingdom(); ?>/account/notification" data-tippy-animation="perspective" title='<?php echo T_("Notifications"); ?>' class="notification"><i class="sf-bell"></i></a>
+
+    <div class="profileShow" data-tippy-animation="perspective" title='<?php echo \dash\data::site_title(); ?><br><?php echo \dash\data::userBadge_desc(); ?>'
+    data-desc='<?php echo \dash\data::userBadge_desc(); ?>'
+    data-footer='<?php echo \dash\data::userBadge_footer(); ?>'
+    data-confirmTxt='<?php echo T_("Account"); ?> <i class="sf-user"></i>' data-confirmLink='<?php echo \dash\url::kingdom(); ?>/account'
+    data-cancelTxt='<?php echo T_("Logout"); ?> <i class="sf-out mLa10"></i>'
+    data-logoutConfirmTxt='<?php echo T_("You really want to go?"); ?>'
+    data-logoutTxt='<?php echo T_("We are waiting for you to come back:)"); ?> ☺️'
+    data-logoutUrl='<?php echo \dash\url::kingdom(); ?>/logout'>
+<?php if(\dash\user::detail('avatar')) { ?>
+     <img src="<?php echo \dash\user::detail('avatar'); ?>" alt='<?php echo T_("Avatar of you"); ?> <?php echo \dash\user::detail('displayname'); ?>'>
+<?php }elseif(\dash\user::id()){ ?>
+     <img src="<?php echo \dash\url::static(); ?>/siftal/images/default/avatar.png" alt='<?php echo T_("Default Avatar"); ?>'>
+<?php }else{ ?>
+
+     <img src="<?php echo \dash\url::static(); ?>/siftal/images/avatar/guest.png" alt='<?php echo T_("Default Avatar"); ?>'>
+
+<?php } //endif ?>
     </div>
-{%else%}
-    <a href="{{url.kingdom}}/enter?referer={{url.pwd}}" data-direct data-tippy-animation="perspective" title='{%trans "Enter to have better experience"%}'><i class="sf-hand-stop"></i></a>
+<?php }else{ ?>
+    <a href="<?php echo \dash\url::kingdom(); ?>/enter?referer={{url.pwd}}" data-direct data-tippy-animation="perspective" title='<?php echo T_("Enter to have better experience"); ?>'><i class="sf-hand-stop"></i></a>
 
-{%endif%}
-{%endblock%}
+<?php } //endif ?>
+
+
    </div>
