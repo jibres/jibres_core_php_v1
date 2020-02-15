79608f8ab7be53a9bf848ba50e454d2378aef99c
Reza <rm.biqarar@gmail.com>
2020-02-15 11:57:31 +0330

    convert enter google display

---
+++ content_enter/google/display.php
@@ -0,0 +1,13 @@
+
+<div class="text">
+ <p><?php echo T_("Hi"); ?>. <?php echo T_("You must not be here!"); ?></p>
+</div>
+
+
+
+ <div class="f" id='eMethods'>
+	 <a href="<?php echo \dash\url::kingdom(); ?>/enter/signup" id='ebusername' class="c" title='<?php echo T_("No account?"); ?> <?php echo T_("Create for yourself"); ?>'>
+	    <i class="sf-user-plus"></i>
+	    <span><?php echo T_("Create Account"); ?></span>
+	 </a>
+  </div>
