07b3dbd5a2f60388f3c3fb245eb379de748eeb6b
Reza <rm.biqarar@gmail.com>
2020-02-15 15:32:23 +0330

    convert master sidebar to php

--- dash/engine/layout/admin/admin-sidebar.php
+++ dash/engine/layout/admin/admin-sidebar.php
@@ -1,45 +1,71 @@
 
 
 
-{%if url.subdomain%}
-      <li><a href="{{url.kingdom}}/a" {%if url.content == "a"%} class="activeContent"{%endif%}><i class='sf-align-left'></i> {% trans "Store admin panel" %}</a></li>
-  {%if url.content == "a"%}
-    {%embed "includes/html/sidebar/sidebar-a.html"%}{%endembed%}
-  {%endif%}
-{%endif%}
+<?php if(\dash\url::subdomain()) {?>
+  <li><a href="<?php echo \dash\url::kingdom(); ?>/a" <?php if(\dash\url::content() === 'a') {?> class="activeContent"<?php }//endif ?>><i class='sf-align-left'></i> <?php echo T_("Store admin panel"); ?></a></li>
+  <?php
+  if(\dash\url::content() === 'a')
+  {
+    require_once ('sidebar/sidebar-a.php');
+  }
+  ?>
+<?php }//endif ?>
 
-      <li><a href="{{url.kingdom}}/store" {%if url.content == "store"%} class="activeContent"{%endif%}><i class='sf-atom'></i> {% trans "Jibres Panel" %}</a></li>
+  <li><a href="<?php echo \dash\url::kingdom(); ?>/store" <?php if(\dash\url::content() === 'store') {?> class="activeContent"<?php }//endif ?>><i class='sf-atom'></i> <?php echo T_("Jibres Panel"); ?></a></li>
 
-{%if perm('contentCp')%}
-      <li><a href="{{url.kingdom}}/cms" {%if url.content == "cms"%} class="activeContent"{%endif%} data-shortkey="67+77" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-align-left'></i> {% trans "CMS" %}</a></li>
-  {%if url.content == "cms"%}
-    {%embed "includes/html/sidebar/sidebar-cms.html"%}{%endembed%}
-  {%endif%}
-{%endif%}
+<?php if(\dash\permission::check('contentCp')) {?>
+      <li><a href="<?php echo \dash\url::kingdom(); ?>/cms" <?php if(\dash\url::content() === 'cms') {?> class="activeContent"<?php }//endif ?> data-shortkey="67+77" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-align-left'></i> <?php echo T_("CMS"); ?></a></li>
+  <?php
+  if(\dash\url::content() === 'cms')
+  {
+    require_once ('sidebar/sidebar-cms.php');
+  }
+  ?>
 
-{%if perm('contentCrm')%}
-      <li><a href="{{url.kingdom}}/crm" {%if url.content == "crm"%} class="activeContent"{%endif%} data-shortkey="77+85" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-group-full'></i> {% trans "CRM Panel" %}</a></li>
-  {%if url.content == "crm"%}
-    {%embed "includes/html/sidebar/sidebar-crm.html"%}{%endembed%}
-  {%endif%}
-{%endif%}
+<?php }//endif ?>
 
-{%if perm_su()%}
-        <li><a href="{{url.kingdom}}/su" {%if url.content == "su"%} class="activeContent"{%endif%} data-shortkey="83+85" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-heartbeat'></i> <span>{% trans "Supervisor Panel" %}</span></a></li>
-  {%if url.content == "su"%}
-    {%embed "includes/html/sidebar/sidebar-su.html"%}{%endembed%}
-  {%endif%}
-{%endif%}
+<?php if(\dash\permission::check('contentCrm')) {?>
+   <li><a href="<?php echo \dash\url::kingdom(); ?>/crm" <?php if(\dash\url::content() === 'crm') {?> class="activeContent"<?php }//endif ?> data-shortkey="77+85" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-group-full'></i> <?php echo T_("CRM Panel"); ?></a></li>
+
+    <?php
+    if(\dash\url::content() === 'crm')
+    {
+      require_once ('sidebar/sidebar-crm.php'); ?>
+    }
+    ?>
+<?php }//endif ?>
 
 
-{%if login%}
-    <li><a href="{{url.kingdom}}/account" {%if url.content == "account"%} class="activeContent"{%endif%} data-shortkey="77+69" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-user'></i> <span>{% trans "My Account" %}</span></a></li>
-  {%if url.content == "account"%}
-    {%embed "includes/html/sidebar/sidebar-account.html"%}{%endembed%}
-  {%endif%}
-{%endif%}
+<?php if(\dash\permission::supervisor()) {?>
 
-      <li><a href="{{url.kingdom}}/support" {%if url.content == "support"%} class="activeContent"{%endif%} data-shortkey="112" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-life-ring'></i> <span>{% trans "Help Center" %}</span></a></li>
-{%if url.content == "support"%}
-{%embed "includes/html/sidebar/sidebar-support.html"%}{%endembed%}
-{%endif%}
+  <li><a href="<?php echo \dash\url::kingdom(); ?>/su" <?php if(\dash\url::content() === 'su') {?> class="activeContent"<?php }//endif ?> data-shortkey="83+85" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-heartbeat'></i> <span><?php echo T_("Supervisor Panel"); ?></span></a></li>
+  <?php
+  if(\dash\url::content() === 'su')
+  {
+    require_once ('sidebar/sidebar-su.php');
+  }
+  ?>
+
+<?php }//endif ?>
+
+
+<?php if(\dash\user::id()) {?>
+    <li><a href="<?php echo \dash\url::kingdom(); ?>/account" <?php if(\dash\url::content() === 'account') {?> class="activeContent"<?php }//endif ?> data-shortkey="77+69" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-user'></i> <span><?php echo T_("My Account"); ?></span></a></li>
+  <?php
+  if(\dash\url::content() === 'account')
+  {
+    require_once ('sidebar/sidebar-account.php');
+  }
+  ?>
+
+<?php }//endif ?>
+
+  <li><a href="<?php echo \dash\url::kingdom(); ?>/support" <?php if(\dash\url::content() === 'support') {?> class="activeContent"<?php }//endif ?> data-shortkey="112" data-shortkey-prevent data-shortkey-timeout='1000'><i class='sf-life-ring'></i> <span><?php echo T_("Help Center"); ?></span></a></li>
+<?php
+if(\dash\url::content() === 'support')
+{
+  require_once ('sidebar/sidebar-support.php');
+}
+?>
+
+<?php }//endif ?>
