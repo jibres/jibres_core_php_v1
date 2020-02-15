37627b4e0ab68758fe96166a5461bf7f6df2304d
Reza <rm.biqarar@gmail.com>
2020-02-15 11:49:08 +0330

    add enter block.php

---
+++ content_enter/block/display.php
@@ -0,0 +1,7 @@
+
+<div class="text">
+ <p><?php echo T_("You are blocked becauese of some reason!"); ?></p>
+ <p><?php echo T_("If have problem"); ?> <a href='<?php echo \dash\url::kingdom(); ?>/contact' data-direct><?php echo T_("Contact us"); ?></a></p>
+</div>
+
+
