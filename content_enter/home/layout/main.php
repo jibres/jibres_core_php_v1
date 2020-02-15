1b91d1f967fff797c539baede4b7bc58c9b71fc6
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-15 11:08:32 +0330

    fix main

--- content_enter/home/layout/main.php
+++ content_enter/home/layout/main.php
@@ -1,10 +1,27 @@
 <div id='enter'>
 
-  <h1 class='logo'><a href='{%if runPWA%}{{url.kingdom}}/enter{%else%}{{url.kingdom}}{%endif%}' tabindex='1' data-direct>
-    <img src='{{url.icon}}' alt='{{site.title}}'>
-    <span>{{site.title}}</span>
-  </a></h1>
-
+ <h1 class='logo'><a href="<?php
+if (\dash\detect\device::detectPWA())
+{
+ echo \dash\url::kingdom(). '/enter';
+}
+else
+{
+ echo \dash\url::kingdom();
+}
+?>" tabindex='1' data-direct>
+  <img src='<?php echo \dash\url::icon(); ?>' alt='<?php echo T_("Jibres") ?>'>
+  <span><?php echo T_("Jibres") ?></span>
+ </a></h1>
+ <div data-xhr='enter'>
+{%if url.module == ""%}
+   <h2 class='flex justify-center align-center'>{{page.title}}</h2>
+{%else%}
+   <h2 class='flex justify-center align-center'>{{page.desc|raw}}</h2>
+{%endif%}
+  <form method="post" autocomplete="off">
+  </form>
+ </div>
 <?php require_once \dash\engine\layout\fn::display(); ?>
 
 </div>
