022b2cfe77e0e2b532e6b3e3d7b5e29852f06814
Reza <rm.biqarar@gmail.com>
2020-02-15 11:47:40 +0330

    add ban display.php

---
+++ content_enter/ban/display.php
@@ -0,0 +1,8 @@
+
+  <div class="text">
+   <p><?php echo T_("What are you doing!"); ?></p>
+   <p><?php echo T_("If you are bot, be easy and call your mom"); ?> 😭</p>
+   <p><?php echo T_("If you are human being, what are you doing here! be personable..."); ?></p>
+  </div>
+
+
