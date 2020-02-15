268f4f445307660834f511170119ae41ede719e5
Reza <rm.biqarar@gmail.com>
2020-02-15 12:24:01 +0330

    convert twostep to php

---
+++ content_enter/twostep/display.php
@@ -0,0 +1,26 @@
+<?php
+if(\dash\user::detail('twostep'))
+{
+?>
+
+
+  <div class='text'>
+   <p><?php echo T_("Two-step login is now active for you."); ?></p>
+  </div>
+  <button data-ajaxify data-method='post' data-data='{"action": "deactive"}' class="btn pain block"><?php echo T_("Deactive two-step"); ?></button>
+
+<?php
+}
+else
+{
+?>
+
+  <div class='text'>
+   <p><?php echo T_("Two-step login is now deactive for you."); ?></p>
+  </div>
+  <button data-ajaxify data-method='post' data-data='{"action": "active"}' class="btn success block"><?php echo T_("Active two-step"); ?></button>
+
+
+<?php
+} // endif
+?>
