192cdfb869bdefcaf2ce8f5c3a181b5b1717f745
Reza <rm.biqarar@gmail.com>
2020-02-15 11:50:00 +0330

    add byebye page

---
+++ content_enter/byebye/display.php
@@ -0,0 +1,7 @@
+
+<div class="text">
+ <p><?php echo T_("We are so sorry for losing you!"); ?></p>
+ <p><?php echo T_("if you want you can come back to"); ?> <a href="{{url.here}}"><?php echo T_("join us"); ?></a></p>
+ <p><?php echo T_("Bye Bye :("); ?></p>
+</div>
+
