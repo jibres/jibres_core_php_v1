f6a2428661b41433e0d7c8c44b140cb473836e5f
Reza <rm.biqarar@gmail.com>
2020-02-16 09:47:54 +0330

    convert add ticket to php

---
+++ content_support/ticket/add/display.php
@@ -0,0 +1,15 @@
+
+<div class="f justify-center">
+	<div class="c8 s12">
+		<?php if(!\dash\user::id()) {?>
+			<div class="msg fs18 txtC warn">
+			  <a href="<?php echo \dash\url::kingdom(); ?>/enter?referer=<?php echo \dash\url::pwd(); ?>" data-direct><?php echo T_("Please log in if you need to store this ticket and track it in the future"); ?></a>
+			</div>
+		<?php } //endif ?>
+
+		<?php require_once( __DIR__. '/../addForm.php'); ?>
+
+	</div>
+</div>
+
+
---
+++ content_support/ticket/addForm.php
@@ -0,0 +1,83 @@
+
+<div class="cbox">
+  <form method="post" enctype="multipart/form-data" autocomplete="off" action="<?php echo \dash\url::pwd(); ?>">
+
+    <?php \dash\utility\hive::html(); ?>
+
+    <?php if(\dash\url::child() === 'add') {?>
+
+    <label for="icontent"><?php echo T_("Please write your message"); ?> <small class="fc-red"><?php echo T_("Require"); ?> *</small></label>
+
+    <?php }else{ ?>
+
+    <label for="icontent"><?php echo T_("Description"); ?> <small class="fc-red"><?php echo T_("Require"); ?> *</small></label>
+
+    <?php } //endif ?>
+
+    <?php if(\dash\permission::check('supportTicketSignature')) {?>
+
+      <textarea class="txt mB10" data-editor id='icontent' name="content"  maxlength='100000' ><?php echo \dash\user::detail('signature'); ?></textarea>
+
+    <?php }else{ ?>
+
+      <textarea class="txt mB10" id='icontent' name="content" placeholder='' maxlength='1000' rows="5"><?php echo \dash\data::dataRow_desc(); ?></textarea>
+
+    <?php } //endif ?>
+
+    <label for="file1"><?php echo T_("Attachment"); ?> <small class="fc-mute"><?php echo T_("Max size 5 MB"); ?></small></label>
+
+    <div class="input">
+     <input type="file" name="file" id="file1" data-max="5000">
+    </div>
+
+    <?php if(\dash\permission::check('supportTicketAnswer') && \dash\url::child() !== 'add') {?>
+
+      <?php if(\dash\data::masterTicketDetail_user_id()) {?>
+
+      <div class="check1 pLa5">
+       <input type="checkbox" name="sendmessage" id="sendmessage" checked>
+       <label for="sendmessage"><?php echo T_("Send notify about your answer to creator of ticket"); ?>
+        <?php if(\dash\data::masterTicketDetail_mobile()) {?>
+
+          <i title='<?php echo T_("Via sms to"). ' '. \dash\fit::mobile(\dash\data::masterTicketDetail_mobile()); ?>' class="sf-mobile fs14"></i>
+        <?php } //endif ?>
+
+        <?php if(\dash\data::masterTicketDetail_chatid()) {?>
+
+          <i title='<?php echo T_("Via telegram"); ?>' class="sf-paper-plane fs12"></i>
+
+        <?php } //endif ?>
+
+       </label>
+      </div>
+
+      <?php } //endif ?>
+
+    <?php } //endif ?>
+
+    <?php if(\dash\url::child() === 'add') {?>
+
+    <button class="btn primary block mT20"><?php echo T_("Submit a ticket"); ?></button>
+
+    <?php }else{ ?>
+
+    <?php if(\dash\permission::check('supportTicketAddNote')) {?>
+
+      <div class="f">
+        <div class="c9 s12">
+          <button class="btn primary block mT20"><?php echo T_("Send new message"); ?></button>
+        </div>
+        <div class="c s12">
+          <button class="btn secondary outline mLa5 block mT20" name="addnote" value="note"><?php echo T_("Add note"); ?></button>
+        </div>
+      </div>
+
+    <?php }else{ ?>
+
+    <button class="btn primary block mT20"><?php echo T_("Send new message"); ?></button>
+    <?php } //endif ?>
+
+   <?php } //endif ?>
+
+  </form>
+</div>
