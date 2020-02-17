4e1d861231398261b1afc98148249acd24c143d3
Reza <rm.biqarar@gmail.com>
2020-02-17 09:21:30 +0330

    convert all store step html to php

---
+++ content_store/error/display.php
@@ -0,0 +1,22 @@
+
+    <div id="get_started_card">
+      <div class="body">
+        <div class="pad">
+          <h1><?php echo T_("Oops!"); ?></h1>
+          <p> <?php echo T_("We can not build your store!"); ?> <?php echo T_("Please contact us to solve this problem."); ?></p>
+
+          <?php if(\dash\data::StoreCreateErrorCode()) {?>
+
+            <div class="msg ltr">
+              <code>Error <?php echo \dash\data::StoreCreateErrorCode(); ?></code>
+            </div>
+          <?php } //endif ?>
+          <a data-direct href='<?php echo \dash\url::here(); ?>' class="btn mTB20 block success2 outline"><?php echo T_("Try again"); ?></a>
+
+          <a data-direct href='<?php echo \dash\url::support(); ?>/setup/error' class="btn block secondary"><?php echo T_("Read about this problem"); ?></a>
+
+        </div>
+
+        <img class="internalError" src="<?php echo \dash\url::static(); ?>/img/store/error500.jpg" alt='<?php echo T_("Loading Jibres"); ?>'>
+      </div>
+    </div>
---
+++ content_store/subdomain/display.php
@@ -0,0 +1,24 @@
+
+    <div id="get_started_card">
+      <div class="body">
+        <div class="pad">
+          <h1><?php echo T_("Choose your online address"); ?></h1>
+          <p><?php echo T_("Your online store has online address on Jibres domain."); ?> <?php echo T_("Although you can connect your domain into store but to finish setup we need to set it."); ?></p>
+          <p class="msg fs09"><?php echo T_("Set it carefully, you can not change it."); ?></p>
+
+          <form method="post" autocomplete="off">
+            <div class="input fix mB10">
+              <input type="text" name="sd" id="sd" placeholder='<?php echo T_("Your subdomain"); ?>' maxlength="40" class="ltr" autofocus required>
+              <label class="addon ltr" for="sd">.Jibres.<?php echo \dash\url::tld(); ?></label>
+            </div>
+
+            <button class="btn success block"><?php echo T_("Build my online store"); ?></button>
+          </form>
+
+        </div>
+        <img src="<?php echo \dash\url::static(); ?>/img/store/choose-subdomain.svg" alt='<?php echo T_("choose subdomain on Jibres"); ?>'>
+      </div>
+      <div class="f align-center">
+        <div class="cauto os"><a href="<?php echo \dash\url::here(); ?>" class="btn" data-direct><?php echo T_("Cancel"); ?></a></div>
+      </div>
+    </div>
