4408c6565eeed5c18f2f16548457d406be7cfe51
Reza <rm.biqarar@gmail.com>
2020-02-17 18:04:26 +0330

    convert post add to php

---
+++ content_cms/posts/add/display.php
@@ -0,0 +1,21 @@
+
+<form class="f justify-center" method="post" autocomplete="off">
+ <div class="c9 x6 s12 pRa20">
+  <div class="cbox mB10">
+
+   <div class="input mB10">
+	  <input type="text" name="title" id="title" placeholder='<?php echo T_("Enter title here"); ?> *'  autofocus required maxlength='200' minlength="1" pattern=".{1,200}">
+
+	</div>
+
+
+   <textarea class="txt mB10" data-editor id='descInput' name="content" placeholder='<?php echo T_("Write post "); ?>' maxlength='100000' rows="15"></textarea>
+
+   <div class="msg"><?php echo T_("First type main text and save as draft, then complete and publish it."); ?></div>
+   <button class="btn success block mT20"><?php echo T_("Save as draft"); ?></button>
+  </div>
+
+ </div>
+</form>
+
+
