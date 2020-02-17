a17b55fa7df34468cf9dcf80985ec0fba3186df8
Reza <rm.biqarar@gmail.com>
2020-02-17 08:27:26 +0330

    convert remember me to php

---
+++ content_account/security/rememberme/display.php
@@ -0,0 +1,18 @@
+
+<form method="post" enctype="multipart/form-data" autocomplete="off">
+  <div class="f justify-center">
+    <div class="c8 s12">
+      <div class="cbox">
+          <div class="switch1 mT20">
+           <input type="checkbox" name="forceremember" id="forceremember" <?php if(\dash\data::dataRow_forceremember()) { echo 'checked'; } ?>>
+           <label for="forceremember"></label>
+           <label for="forceremember"><?php echo T_("Save remember session"); ?></label>
+          </div>
+
+          <div class="txtRa mT20">
+            <button class="btn success"><?php echo T_("Save"); ?></button>
+          </div>
+      </div>
+    </div>
+  </div>
+</form>
