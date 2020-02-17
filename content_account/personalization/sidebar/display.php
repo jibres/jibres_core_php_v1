d320c47f08b8d04a46676f8028b721206608a2d6
Reza <rm.biqarar@gmail.com>
2020-02-17 08:05:55 +0330

    convert sidebar to php

---
+++ content_account/personalization/sidebar/display.php
@@ -0,0 +1,29 @@
+
+<div class="f justify-center">
+  <div class="c6 m8 x5 s12">
+    <div class="cbox">
+      <div class="msg f">
+       <div class="cauto">
+        <span class="<?php if(\dash\user::detail('sidebar')) { echo 'sf-monitor'; }else{ echo 'sf-display'; } ?> fs40 pA10 pRa25 vlbottom"></span>
+       </div>
+        <div class="c">
+         <h3><?php echo T_("Toggle side bar"); ?></h3>
+         <p><?php echo T_("You can save side bar status"); ?></p>
+       </div>
+      </div>
+      <form method="post" autocomplete="off">
+
+          <div class="switch1 mT20">
+           <input type="checkbox" name="sidebar" id="isidebar" <?php if(\dash\data::dataRow_sidebar()) { echo "checked"; }?>>
+           <label for="isidebar"></label>
+           <label for="isidebar"><?php echo T_("Are your need side bar?"); ?></label>
+          </div>
+
+        <div class="txtRa">
+          <button class="btn success"><?php echo T_("Save"); ?></button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
