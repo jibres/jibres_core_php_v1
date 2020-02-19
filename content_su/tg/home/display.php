106a7e9e57a3f6739757b6edaa6ff7bef755fd4c
Reza <rm.biqarar@gmail.com>
2020-02-19 08:27:50 +0330

    conver su tg to php

---
+++ content_su/tg/home/display.php
@@ -0,0 +1,34 @@
+<?php
+$tg = \dash\data::tg();
+?>
+<div class="f mB25">
+  <div class="c3 s12">
+    <div class="dcard x4 mB10 ">
+      <h2><a href="https://t.me/<?php echo @$tg['info']['bot']; ?>" target="_blank"><?php echo @$tg['info']['bot']; ?></a></h2>
+      <pre class="mT25"><?php echo @$tg['info']['token']; ?></pre>
+      <a class="btn dark outline mT25" href="<?php echo \dash\url::this(); ?>/log"><?php echo T_("Logs"); ?></a>
+      <a class="btn dark outline mT25" href="<?php echo \dash\url::this(); ?>/users"><?php echo T_("Users"); ?></a>
+      <a class="btn dark outline mT25" href="<?php echo \dash\url::this(); ?>/system"><?php echo T_("System"); ?></a>
+    <?php if(\dash\user::id() === 1) {?>
+      <a class="btn danger outline mT10" href="<?php echo \dash\url::this(); ?>/webhook"><?php echo T_("Webhook"); ?></a>
+    <?php } ?>
+    </div>
+  </div>
+  <div class="c s12">
+    <div class="dcard x2 mB10">
+      <a class="btn primary" href="<?php echo \dash\url::this(); ?>/sendmessage"><?php echo T_("Send message"); ?></a>
+    </div>
+    <div class="dcard x2 mB10">
+      salam
+    </div>
+
+  </div>
+  <div class="c s12">
+    <div class="dcard x2 mB10">
+      <a class="btn" href="<?php echo \dash\url::this(); ?>/sendphoto"><?php echo T_("Send photo"); ?></a>
+    </div>
+    <div class="dcard x2 mB10">
+      salam
+    </div>
+  </div>
+</div>
---
+++ content_su/tg/sendmessage/display.php
@@ -0,0 +1,35 @@
+<?php
+$tg = \dash\data::tg();
+?>
+
+<form class="cbox" method="post" autocomplete="off">
+  <div class="input ltr mB10">
+    <label for="chatid"><?php echo T_("Chat id or username"); ?></label>
+    <input type="text" name="chatid" id="chatid" value="46898544" placeholder='<?php echo T_("Unique identifier for the target chat or username of the target channel (in the format @channelusername)"); ?>' required>
+  </div>
+
+  <label for="text"><?php echo T_("Message"); ?></label>
+  <textarea class="txt mB10" name="text" id="text" rows="7" placeholder='<?php echo T_("Text of the message to be sent"); ?>' required>
+    <?php echo \dash\data::site_title(); ?>
+
+    <?php echo \dash\fit::date(date("Y-m-d")); ?>
+    <?php echo \dash\fit::text(date("H:i:s")); ?>
+
+</textarea>
+
+  <button class="btn primary block"><?php echo T_("Send"); ?></button>
+</form>
+
+<?php if(isset($tg['send'])) {?>
+
+
+<div class="cbox">
+  <h2><?php echo T_("Last request"); ?></h2>
+  <div class="msg info2">
+    <pre><?php echo @$tg['send']; ?></pre>
+  </div>
+  <div class="msg">
+    <pre><?php echo @$tg['response']; ?></pre>
+  </div>
+</div>
+<?php } ?>
