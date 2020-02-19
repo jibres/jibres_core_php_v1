0aabcfda87baa6828672aedc5a9c1f284c4909ef
Reza <rm.biqarar@gmail.com>
2020-02-19 09:28:45 +0330

    convert su sidebar to php

--- dash/engine/layout/admin/sidebar/sidebar-su.php
+++ dash/engine/layout/admin/sidebar/sidebar-su.php
@@ -1,26 +1,29 @@
 
 <li>
   <ul>
-    <li><a href="{{url.here}}/backup">{% trans "Backup" %}</a></li>
-    <li><a href="{{url.here}}/dbtables">{% trans "Raw table" %}</a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/backup"><?php echo T_("Backup"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/dbtables"><?php echo T_("Raw table"); ?></a></li>
 
-    <li><a href="{{url.here}}/time">{% trans "Date and time" %}</a></li>
-    <li><a href="{{url.here}}/info">{% trans "Server information" %}</a></li>
-    <li><a href="{{url.here}}/cronjob">{% trans "Cronjob" %}</a></li>
-    <li><a href="{{url.here}}/ip">{% trans "IP" %}</a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/time"><?php echo T_("Date and time"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/info"><?php echo T_("Server information"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/cronjob"><?php echo T_("Cronjob"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/ip"><?php echo T_("IP"); ?></a></li>
 
-    <li><a href="{{url.here}}/gitstatus">{% trans "Git status" %}</a></li>
-    <li><a href="{{url.here}}/nano">{% trans "Nano" %}</a></li>
-    <li><a href="{{url.here}}/update" data-shortkey="85+80" data-shortkey-timeout='500'>{% trans "Update" %}<kbd class="floatLa mRa10 fs08">up</kbd></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/gitstatus"><?php echo T_("Git status"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/nano"><?php echo T_("Nano"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/update" data-shortkey="85+80" data-shortkey-timeout='500'><?php echo T_("Update"); ?><kbd class="floatLa mRa10 fs08">up</kbd></a></li>
 
-    <li><a href="{{url.here}}/log">{% trans "Log" %}</a></li>
-    <li><a href="{{url.here}}/apilog">{% trans "Api Log" %}</a></li>
-    <li><a href="{{url.here}}/smsclient">{% trans "Sms client" %}</a></li>
-    {%if url.tld == 'local'%}
-    <li><a href="{{url.here}}/permission">{% trans "Permission" %}</a></li>
-    <li><a href="{{url.here}}/translation">{% trans "Translation tools" %}</a></li>
-    {%endif%}
-    <li><a href="{{url.here}}/tg" data-shortkey="84+71" data-shortkey-timeout='500'>{% trans "Telegram" %}<kbd class="floatLa mRa10 fs08">tg</kbd></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/log"><?php echo T_("Log"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/apilog"><?php echo T_("Api Log"); ?></a></li>
+    <li><a href="<?php echo \dash\url::here(); ?>/smsclient"><?php echo T_("Sms client"); ?></a></li>
+
+    <?php if(\dash\url::isLocal()) {?>
+
+    <li><a href="<?php echo \dash\url::here(); ?>/permission"><?php echo T_("Permission"); ?></a></li>
+
+    <?php } //endif ?>
+
+    <li><a href="<?php echo \dash\url::here(); ?>/tg" data-shortkey="84+71" data-shortkey-timeout='500'><?php echo T_("Telegram"); ?><kbd class="floatLa mRa10 fs08">tg</kbd></a></li>
 
   </ul>
 </li>
