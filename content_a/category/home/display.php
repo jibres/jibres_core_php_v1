d4a4c59a70a29f1fb40e7773db8ba077a1fce141
Reza <rm.biqarar@gmail.com>
2020-02-19 10:17:07 +0330

    convert category to php

---
+++ content_a/category/home/display.php
@@ -0,0 +1,147 @@
+
+<?php
+
+
+if(\dash\data::dataTable())
+{
+  if(\dash\data::dataFilter())
+  {
+
+    htmlSearchBox();
+    htmlTable();
+    htmlFilter();
+
+  }
+  else
+  {
+    htmlSearchBox();
+    htmlTable();
+  }
+}
+else
+{
+  if(\dash\data::dataFilter())
+  {
+
+    htmlSearchBox();
+    htmlFilterNoResult();
+
+
+  }
+  else
+  {
+    htmlStartAddNew();
+
+  }
+
+}
+?>
+
+
+
+
+
+
+<?php function htmlSearchBox() {?>
+<div class="cbox fs12">
+  <form method="get" action='<?php echo \dash\url::this(); ?>' data-action>
+    <div class="input">
+      <input type="search" name="q" placeholder='<?php echo T_("Search"); ?>' id="q" value="<?php echo \dash\request::get('q'); ?>" autofocus autocomplete='off'>
+
+      <?php if(\dash\request::get('type')) {?>
+
+      <input type="hidden" name="type" value="<?php echo \dash\request::get('type'); ?>">
+
+      <?php } // endif ?>
+
+      <button class="addon btn "><?php echo T_("Search"); ?></button>
+    </div>
+  </form>
+</div>
+<?php } //endfunction ?>
+
+
+
+
+
+<?php function htmlTable() {?>
+
+<?php
+$sortLink = \dash\data::sortLink();
+$dataTable = \dash\data::dataTable();
+if(!is_array($dataTable))
+{
+  $dataTable = [];
+}
+
+?>
+
+  <table class="tbl1 v1 cbox fs12">
+    <thead>
+      <tr class="fs08">
+        <th class="collapsing">&nbsp;</th>
+        <th><?php echo T_("Title"); ?></th>
+        <th class="s0"><?php echo T_("Slug"); ?></th>
+        <th class="collapsing s0"><?php echo T_("Count product"); ?></th>
+
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php foreach ($dataTable as $key => $value) {?>
+
+      <tr  {%if value.isdefault%} class="positive" title='<?php echo T_("Is a default category"); ?>' {%endif%}>
+        <td class="collapsing">
+          <?php if(isset($value['file']) && $value['file']) {?><img src="<?php echo @$value['file']; ?>" class="avatar"><?php } //endif ?>
+        </td>
+        <td ><a class="txtB" href="<?php echo \dash\url::here(); ?>/category/edit?id=<?php echo @$value['id']; ?>"><i class="sf-edit-write mRa10"></i><?php echo @$value['title']; ?></a>
+          <br>
+          <span class="fc-mute fs09"><?php echo @$value['parent_title']; ?></span>
+        </td>
+        <td class="s0"><?php echo @$value['full_slug']; ?></td>
+        <td class="s0 collapsing">
+          <a href="<?php echo \dash\url::here(); ?>/products?catid=<?php echo @$value['id']; ?>"><?php echo \dash\fit::number(@$value['count']); ?> <small class="fc-mute"><?php echo T_("Product"); ?></small></a>
+        </td>
+
+      </tr>
+      <?php } //endfor ?>
+    </tbody>
+  </table>
+
+
+<?php \dash\utility\pagination::html(); ?>
+
+<?php } //endif ?>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php function htmlFilter() {?>
+<p class="f fs14 msg info2">
+  <span class="c"><?php echo \dash\data::dataFilter(); ?></span>
+  <a class="cauto" href="<?php echo \dash\url::here(); ?>/<?php echo \dash\url::module(); ?>"><?php echo T_("Clear filters"); ?></a>
+</p>
+<?php } //endif ?>
+
+
+<?php function htmlFilterNoResult() {?>
+<p class="f fs14 msg warn2">
+  <span class="c"><?php echo T_("Result not found!"); ?> <?php echo T_("Search with new keywords."); ?></span>
+  <a class="cauto" href="<?php echo \dash\url::here(); ?>/<?php echo \dash\url::module(); ?>"><?php echo T_("Clear filters"); ?></a>
+</p>
+<?php } //endif ?>
+
+
+<?php function htmlStartAddNew() {?>
+<p class="fs14 msg success2 pTB20"><?php echo T_("No record exist!"); ?></p>
+<?php } //endif ?>
+
+
