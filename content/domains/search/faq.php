1c01efa7382b530125316f73fc7ed16f015a138c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-20 18:04:04 +0330

    seperate search faq

---
+++ content/domains/search/faq.php
@@ -0,0 +1,50 @@
+
+ <div class="fit">
+  <section class="faq">
+   <h3><?php echo T_('Frequently Asked Questions'); ?></h3>
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_('What TLD types exist?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("There are currently two types of TLDs. Generic domain names (gTLDs) and Country code domains names (ccTLDs). gTLDs are the largest group of domains and account for most of the newly available domains, whereas ccTLDs are generally assigned to specific geographic locations (for example, .IR, .DE, and .US).");
+?></p>
+    </div>
+
+    <h4 data-kerkere="[data-faq2]" data-kerkere-icon='close'><?php echo T_('How do I renew a domain?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq2>
+     <p><?php
+echo T_("The domain renewal process is easy and can be completed <a href=':val'>by following these 4 steps.</a>", ['val' => \dash\url::kingdom(). '/domains/renew']);
+?></p>
+    </div>
+
+    <h4 data-kerkere="[data-faq3]" data-kerkere-icon='close'><?php echo T_('Can I renew an expired domain?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq3>
+     <p><?php
+echo T_("You can only 'renew' domains that are still active, but you can 'reactivate' an expired domain for exactly the same price as a renewal.");
+?></p>
+    </div>
+
+    <h4 data-kerkere="[data-faq4]" data-kerkere-icon='close'><?php echo T_('Why buy a domain name from Jibres?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq4>
+     <p><?php
+echo T_("Above all else, we strive to deliver outstanding customer experiences. When you buy a domain name from Jibres, we guarantee it will be handed over to you with superior standards of service and support. Our primary goal is to build a customer-focused atmosphere filled with the happiest customers in the galaxy. The Jibres guarantee is our mark of excellence.");
+?></p>
+    </div>
+
+    <h4 data-kerkere="[data-faq5]" data-kerkere-icon='close'><?php echo T_('Why is domain name registration required?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq5>
+     <p><?php
+echo T_("When you build a website, you want visitors to come and see what you've done. To get them there, you need a unique domain name that connects to your sites servers. Domain name registration is required to ensure that no one else in the world can claim ownership of your web site's address and to make finding your website simple.");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq6]" data-kerkere-icon='close'><?php echo T_('How does domain registration work?'); ?></h4>
+    <div data-kerkere-content='hide' data-faq6>
+     <p><?php
+echo T_("Think of the name you want to register. The answer is typically your company or website name. It is best to keep your domain name short and easy to understand. Say it out loud, and make sure it sounds great. Next, search to see if it is available. If the name you desire is taken with the .com top-level domain, there are hundreds of others available. Finally, add the top choices to your cart and complete the domain registration.");
+?></p>
+    </div>
+
+  </section>
+ </div>
