48d4e19ab466e9d2d0c4a066989b4c1366dceae5
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-20 17:03:03 +0330

    fix faq

--- content/domains/home/display.php
+++ content/domains/home/display.php
@@ -4,7 +4,7 @@
   <form class="domainSearchBox" action='<?php echo \dash\url::kindgom() ?>/domains/search' method='get' autocomplete='off'>
    <h2 class="txtC"><?php echo T_('Search for your dream domain'); ?></h2>
   <div class="input ltr">
-   <input type="text" name="q" id='domainFirstSearch' maxlength='63' autocomplete='off' <?php if (!\dash\detect\device::detectPWA()) echo 'autofocus'?>>
+   <input type="text" name="q" id='domainFirstSearch' maxlength='63' autocomplete='off' <?php if (!\dash\detect\device::detectPWA() && 0) echo 'autofocus'?>>
    <button class="addon btn primary"><?php echo T_('Search'); ?></button>
   </div>
  </form>
@@ -65,105 +65,93 @@
   <section class="faq">
    <h3><?php echo T_('Frequently Asked Questions'); ?></h3>
 
-    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'>What is a domain? What is a TLD?</h4>
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_('What is a domain? What is a TLD?'); ?></h4>
     <div data-kerkere-content='hide' data-faq1>
-   <p><?php
-echo T_("Think of your domain name as a street address. If people search for your domain, they'll be able to find your website.
-Every website on the internet has a unique IP address assigned to it, made up of a series of numbers. These numbers tell the domain name system (DNS) to locate the corresponding website. As we are humans and not computers, IP addresses are difficult to remember and so words are used instead. These words are known as the domain or URL. The DNS looks at the domain name and translates it into an IP address.");
+     <p><?php
+echo T_("Think of your domain name as a street address. If people search for your domain, they'll be able to find your website. Every website on the internet has a unique IP address assigned to it, made up of a series of numbers. These numbers tell the domain name system (DNS) to locate the corresponding website. As we are humans and not computers, IP addresses are difficult to remember and so words are used instead. These words are known as the domain or URL. The DNS looks at the domain name and translates it into an IP address.");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("Top-level domains were introduced to help organize the locations in the domain name system. All domains include a top-level domain (TLD) and a second-level domain (SLD). Imagine you own a business in the USA. In 'yourbusiness.us', the TLD is .us and the SLD is 'yourbusiness'.");
 ?></p>
     </div>
 
-
     <h4 data-kerkere="[data-faq10]" data-kerkere-icon='close'><?php echo T_("What is a TLD?"); ?></h4>
     <div data-kerkere-content='hide' data-faq10>
-   <p><?php
-echo T_('A Top-level Domain (TLD) is the part of a domain name to the right of the dot (e.g., the "com" in jibres.com).
-TLDs are part of the text-based interface assigned to numerical IP addresses that allows humans to more easily navigate the web without having to memorize long strings of numbers.');
+     <p><?php
+echo T_('A Top-level Domain (TLD) is the part of a domain name to the right of the dot (e.g., the "com" in jibres.com). TLDs are part of the text-based interface assigned to numerical IP addresses that allows humans to more easily navigate the web without having to memorize long strings of numbers.');
 ?></p>
     </div>
 
-
-      <h4 data-kerkere="[data-faq2]" data-kerkere-icon='close'><?php echo T_("What is the difference between various TLDs? Which ones are better?"); ?></h4>
-  <div data-kerkere-content='hide' data-faq2>
-   <p><?php
+    <h4 data-kerkere="[data-faq2]" data-kerkere-icon='close'><?php echo T_("What is the difference between various TLDs? Which ones are better?"); ?></h4>
+    <div data-kerkere-content='hide' data-faq2>
+     <p><?php
 echo T_("When it comes to picking a top-level domain for your website, there are plenty of options. Yes, .com domain extensions are popular, but due to their popularity, you may find that your dream domain has already been taken. Why not consider choosing a generic (gTLD) like .club for your tennis club, or a country-code (ccTLDs) like .co.uk for your UK office?");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("If you're a new business it may be important to choose a familiar domain extension like .com, a localized domain or a well-known gTLD. As they are popular, they are perceived as more trustworthy. Don't panic if you haven't managed to get one. The web is changing all the time, and as the web grows, so will the familiarity of new TLDs.");
 ?></p>
     </div>
 
-
-      <h4 data-kerkere="[data-faq3]" data-kerkere-icon='close'><?php echo T_("How do I check domain name availability status?"); ?></h4>
-  <div data-kerkere-content='hide' data-faq3>
-   <p><?php
+    <h4 data-kerkere="[data-faq3]" data-kerkere-icon='close'><?php echo T_("How do I check domain name availability status?"); ?></h4>
+    <div data-kerkere-content='hide' data-faq3>
+     <p><?php
 echo T_("To verify domain availability, use the search bar at the top of the page. Your website name will either be available or taken. If your domain is taken, this means that it is either reserved by the Registry or registered by someone else. At this point, you can choose to make an offer for that domain name.");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("If you are looking for a fast and safe domain name checker, use Jibres. We never have and never will sell any information on our clients.");
 ?></p>
     </div>
 
-
-      <h4 data-kerkere="[data-faq4]" data-kerkere-icon='close'><?php echo T_("Are .com domains better for search engine optimization (SEO)?"); ?></h4>
-  <div data-kerkere-content='hide' data-faq4>
-   <p><?php
-echo T_("No. All domain extensions are considered equal in the eyes of Google and there is no automatic preference given to a .com domain.
-When checking to see if a domain exists, it's important to note whether a large and recognizable company has the .com if it does, this can hurt your search results. It can also open you up to a potential trademark battle.");
+    <h4 data-kerkere="[data-faq4]" data-kerkere-icon='close'><?php echo T_("Are .com domains better for search engine optimization (SEO)?"); ?></h4>
+    <div data-kerkere-content='hide' data-faq4>
+     <p><?php
+echo T_("No. All domain extensions are considered equal in the eyes of Google and there is no automatic preference given to a .com domain. When checking to see if a domain exists, it's important to note whether a large and recognizable company has the .com if it does, this can hurt your search results. It can also open you up to a potential trademark battle.");
 ?></p>
     </div>
 
-
     <h4 data-kerkere="[data-faq5]" data-kerkere-icon='close'><?php echo T_("What is a premium domain?"); ?></h4>
     <div data-kerkere-content='hide' data-faq5>
-   <p><?php
+     <p><?php
 echo T_("Premium domains are short domains, often made up of just one word or 3-5 letters. They are also known as 'aftermarket' or 'pre-registered' domains. Most premium domains have a .com extension, but many end with .org, .net, and .biz.");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("As premium domains include common words they are often the most memorable. Additionally, companies value short domains that match their company name or products, meaning these domains are typically the most desirable. Sometimes certain domains sold by different registries are considered premium and can have a higher price point. With registry premium domains, both registration and renewal prices are set by the registry and are usually high.");
 ?></p>
     </div>
 
-
     <h4 data-kerkere="[data-faq6]" data-kerkere-icon='close'><?php echo T_("What is a new TLD?"); ?></h4>
     <div data-kerkere-content='hide' data-faq6>
-   <p><?php
+     <p><?php
 echo T_("If you're looking for something a little different for your business or personal website, why not consider a new TLD? There are hundreds of new fresh and exciting domain endings that can help bring your dream alive. Want a personal website? Share photos of your international trip with .travel, write your thoughts on the latest catwalk trends with .blog or get creative with a .is.");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("Looking to conquer your industry? Set up a web developer startup with a .dev and get a .inc to show you mean business.");
 ?></p>
     </div>
 
-
-      <h4 data-kerkere="[data-faq7]" data-kerkere-icon='close'><?php echo T_("Should I register multiple extensions for the same domain?"); ?></h4>
-  <div data-kerkere-content='hide' data-faq7>
-   <p><?php
+    <h4 data-kerkere="[data-faq7]" data-kerkere-icon='close'><?php echo T_("Should I register multiple extensions for the same domain?"); ?></h4>
+    <div data-kerkere-content='hide' data-faq7>
+     <p><?php
 echo T_("Yes. When you register a domain extension, no one else can use it. So, if you've already got mydomain.com, it's a good idea to register mydomain.net and mydomain.org and redirect them back to your original site. This helps to avoid confusion and makes capturing visitors easier.");
 ?></p>
-   <p><?php
+    <p><?php
 echo T_("");
 ?></p>
     </div>
 
-
-      <h4 data-kerkere="[data-faq8]" data-kerkere-icon='close'><?php echo T_("Is there a limit on how long or short my domain can be?"); ?></h4>
-  <div data-kerkere-content='hide' data-faq8>
-   <p><?php
+    <h4 data-kerkere="[data-faq8]" data-kerkere-icon='close'><?php echo T_("Is there a limit on how long or short my domain can be?"); ?></h4>
+    <div data-kerkere-content='hide' data-faq8>
+     <p><?php
 echo T_("The maximum length a domain can be is 63 characters and the minimum is 1 character. The shorter your domain is, the easier it is to type and remember.");
 ?></p>
-   <p><?php
+     <p><?php
 echo T_("When choosing a domain name, keep it simple - a word that is difficult to spell will be problematic for your visitors. Pick a name that reflects your brand, industry, or you if it's a personal project. Once you have a list of words, ask your friends and family for feedback. Have fun choosing!");
 ?></p>
     </div>
 
-
     <h4 data-kerkere="[data-faq9]" data-kerkere-icon='close'><?php echo T_("What can I do with my domain name?"); ?></h4>
     <div data-kerkere-content='hide' data-faq9>
-   <p><?php
+     <p><?php
 echo T_("Once you have registered your domain name it's time to use it. Maybe you want to funnel visitors to a landing page built especially for sales, or forward them to your personal Twitter page - it's completely up to you how you use it. You can also create personalized email addresses based on your domain name. This helps to build trust in your brand and promote your company.");
 ?></p>
     </div>
@@ -172,140 +160,3 @@ echo T_("Once you have registered your domain name it's time to use it. Maybe yo
 
  </div>
 </div>
-
-
-
-
-
-
-
-
-
-<hr>
-    <h2>Buy a domain name and create your website today.</h2>
-<p>
-
-Every website starts with a great domain name.
-Register Your Dream Domain Today.
-
-
-
-
-
-
-That's where Jibres comes in.
-Welcome to the domain registrar that has everything you need to get the right domain name for your personal or business website, including answers to questions like:
-
-
-You can choose from the most in-demand and recognizable domain names at great prices, with new choices added regularly.
-So you can register a domain that helps your online presence either take off, or hit new heights.
-
-
-Register
-Been dreaming of a .com or .ir that says exactly what you want to say about your business?
-Find that perfect domain name and get it registered today!
-
-
-Why register a domain name with Jibres?
-Our customers are our top priority
-
-*ICANN (the Internet Corporation for Assigned Names and Numbers) charges a mandatory annual fee of $0.18 for each domain registration, renewal or transfer.
-This will be added to the listed price for some domains, at the time of purchase.
-
-
-</p>
-
-
-
-<p>
-
-Find your dream domain.
-
-
-
-
-
-Remember to come up with words that are easy to spell and reflect the purpose of your website. Ask friends and family for their opinions - the more the merrier in your search for the perfect website domain name.
-
-Once you've come up with an exciting idea, it's time to see if your domain name has been taken.
-
-
-
-Search for your dream domain
-Discover the perfect domain now
-
-</p>
-
-<p>
-
-
-
-
-
-</p>
-
-
-
-
-
-<p>
-FAQ
-Frequently Asked Questions
-
-
-
-
-
-What TLD types exist?
-There are currently two types of TLDs.
-Generic domain names (gTLDs) and Country code domains names (ccTLDs).
-gTLDs are the largest group of domains and account for most of the newly available domains, whereas ccTLDs are generally assigned to specific geographic locations (for example, .IR, .DE, and .US).
-
-
-How do I renew a domain?
-The domain renewal process is easy and can be completed <a>by following these 4 steps.</a>
-
-
-Can I renew an expired domain?
-You can only 'renew' domains that are still active, but you can 'reactivate' an expired domain for exactly the same price as a renewal.
-
-
-Why buy a domain name from Jibres?
-Above all else, we strive to deliver outstanding customer experiences.
-When you buy a domain name from Jibres, we guarantee it will be handed over to you with superior standards of service and support.
-Our primary goal is to build a customer-focused atmosphere filled with the happiest customers in the galaxy.
-The Jibres guarantee is our mark of excellence.
-
-
-Why is domain name registration required?
-When you build a website, you want visitors to come and see what you've done.
-To get them there, you need a unique domain name that connects to your sites servers.
-Domain name registration is required to ensure that no one else in the world can claim ownership of your web site's address and to make finding your website simple.
-
-
-How does domain registration work?
-Think of the name you want to register.
-The answer is typically your company or website name.
-It is best to keep your domain name short and easy to understand.
-Say it out loud, and make sure it sounds great.
-Next, search to see if it is available.
-If the name you desire is taken with the .com top-level domain, there are hundreds of others available.
-Finally, add the top choices to your cart and complete the domain registration.
-
-
-
-
-
-</p>
-
-
- <section class="f fix align-center">
-   <div class="cauto">
-    <img class="dreamRider" src='<?php echo \dash\url::static() ?>/img/domain/jibres-domain-monitoring.gif'>
-
-   </div>
-   <div class="c">
-   </div>
- </section>
-
-
--- content/domains/search/display.php
+++ content/domains/search/display.php
@@ -4,4 +4,222 @@
  	Search
 
  </div>
+
+
+
+
+
+
+ <div class="fit">
+
+  <section class="faq">
+   <h3><?php echo T_('Frequently Asked Questions'); ?></h3>
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+
+    <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'><?php echo T_(''); ?></h4>
+    <div data-kerkere-content='hide' data-faq1>
+     <p><?php
+echo T_("");
+?></p>
+     <p><?php
+echo T_("");
+?></p>
+    </div>
+
+  </section>
+
+
+
+
+
+
+
+
+
+
+<hr>
+    <h2>Buy a domain name and create your website today.</h2>
+<p>
+
+Every website starts with a great domain name.
+Register Your Dream Domain Today.
+
+
+
+
+
+
+That's where Jibres comes in.
+Welcome to the domain registrar that has everything you need to get the right domain name for your personal or business website, including answers to questions like:
+
+
+You can choose from the most in-demand and recognizable domain names at great prices, with new choices added regularly.
+So you can register a domain that helps your online presence either take off, or hit new heights.
+
+
+Register
+Been dreaming of a .com or .ir that says exactly what you want to say about your business?
+Find that perfect domain name and get it registered today!
+
+
+Why register a domain name with Jibres?
+Our customers are our top priority
+
+*ICANN (the Internet Corporation for Assigned Names and Numbers) charges a mandatory annual fee of $0.18 for each domain registration, renewal or transfer.
+This will be added to the listed price for some domains, at the time of purchase.
+
+
+</p>
+
+
+
+<p>
+
+Find your dream domain.
+
+
+
+
+
+Remember to come up with words that are easy to spell and reflect the purpose of your website. Ask friends and family for their opinions - the more the merrier in your search for the perfect website domain name.
+
+Once you've come up with an exciting idea, it's time to see if your domain name has been taken.
+
+
+
+Search for your dream domain
+Discover the perfect domain now
+
+</p>
+
+<p>
+
+
+
+
+
+</p>
+
+
+
+
+
+<p>
+FAQ
+Frequently Asked Questions
+
+
+
+
+
+What TLD types exist?
+There are currently two types of TLDs.
+Generic domain names (gTLDs) and Country code domains names (ccTLDs).
+gTLDs are the largest group of domains and account for most of the newly available domains, whereas ccTLDs are generally assigned to specific geographic locations (for example, .IR, .DE, and .US).
+
+
+How do I renew a domain?
+The domain renewal process is easy and can be completed <a>by following these 4 steps.</a>
+
+
+Can I renew an expired domain?
+You can only 'renew' domains that are still active, but you can 'reactivate' an expired domain for exactly the same price as a renewal.
+
+
+Why buy a domain name from Jibres?
+Above all else, we strive to deliver outstanding customer experiences.
+When you buy a domain name from Jibres, we guarantee it will be handed over to you with superior standards of service and support.
+Our primary goal is to build a customer-focused atmosphere filled with the happiest customers in the galaxy.
+The Jibres guarantee is our mark of excellence.
+
+
+Why is domain name registration required?
+When you build a website, you want visitors to come and see what you've done.
+To get them there, you need a unique domain name that connects to your sites servers.
+Domain name registration is required to ensure that no one else in the world can claim ownership of your web site's address and to make finding your website simple.
+
+
+How does domain registration work?
+Think of the name you want to register.
+The answer is typically your company or website name.
+It is best to keep your domain name short and easy to understand.
+Say it out loud, and make sure it sounds great.
+Next, search to see if it is available.
+If the name you desire is taken with the .com top-level domain, there are hundreds of others available.
+Finally, add the top choices to your cart and complete the domain registration.
+
+
+
+
+
+</p>
+
+
+ <section class="f fix align-center">
+   <div class="cauto">
+    <img class="dreamRider" src='<?php echo \dash\url::static() ?>/img/domain/jibres-domain-monitoring.gif'>
+
+   </div>
+   <div class="c">
+   </div>
+ </section>
+
+
+
+
+ </div>
 </div>
