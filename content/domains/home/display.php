301aec8603e2e473e663e25ed2cc0e0ed2452974
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-20 16:39:53 +0330

    add developer box

--- content/domains/home/display.php
+++ content/domains/home/display.php
@@ -68,11 +68,141 @@
  </section>
 
 
-
-
-<section>
+</div>
+<section class="forDevelopers">
  <h3><?php echo T_('Built By Developers, For Developers.'); ?></h3>
+ <img class="dreamRider" src='<?php echo \dash\url::static() ?>/img/domain/jibres-dream-rider.gif'>
 </section>
+<div class="fit mT20">
+
+
+
+
+<section class="faq">
+ <h3><?php echo T_('Frequently Asked Questions'); ?></h3>
+
+  <h4 data-kerkere="[data-faq1]" data-kerkere-icon='close'>What is a domain? What is a TLD?</h4>
+  <div data-kerkere-content='hide' data-faq1>
+   <p><?php
+echo T_("Think of your domain name as a street address. If people search for your domain, they'll be able to find your website.
+Every website on the internet has a unique IP address assigned to it, made up of a series of numbers. These numbers tell the domain name system (DNS) to locate the corresponding website. As we are humans and not computers, IP addresses are difficult to remember and so words are used instead. These words are known as the domain or URL. The DNS looks at the domain name and translates it into an IP address.");
+?></p>
+   <p><?php
+echo T_("Top-level domains were introduced to help organize the locations in the domain name system. All domains include a top-level domain (TLD) and a second-level domain (SLD). Imagine you own a business in the USA. In 'yourbusiness.us', the TLD is .us and the SLD is 'yourbusiness'.");
+?></p>
+  </div>
+
+
+  <h4 data-kerkere="[data-faq2]" data-kerkere-icon='close'><?php echo T_("What is the difference between various TLDs? Which ones are better?"); ?></h4>
+  <div data-kerkere-content='hide' data-faq2>
+   <p><?php
+echo T_("When it comes to picking a top-level domain for your website, there are plenty of options. Yes, .com domain extensions are popular, but due to their popularity, you may find that your dream domain has already been taken. Why not consider choosing a generic (gTLD) like .club for your tennis club, or a country-code (ccTLDs) like .co.uk for your UK office?");
+?></p>
+   <p><?php
+echo T_("If you’re a new business it may be important to choose a familiar domain extension like .com, a localized domain or a well-known gTLD. As they are popular, they are perceived as more trustworthy. Don’t panic if you haven’t managed to get one. The web is changing all the time, and as the web grows, so will the familiarity of new TLDs.");
+?></p>
+  </div>
+
+
+  <h4 data-kerkere="[data-faq3]" data-kerkere-icon='close'><?php echo T_("How do I check domain name availability status?"); ?></h4>
+  <div data-kerkere-content='hide' data-faq3>
+   <p><?php
+echo T_("To verify domain availability, use the search bar at the top of the page. Your website name will either be available or taken. If your domain is taken, this means that it is either reserved by the Registry or registered by someone else. At this point, you can choose to make an offer for that domain name.");
+?></p>
+   <p><?php
+echo T_("If you are looking for a fast and safe domain name checker, use Jibres. We never have and never will sell any information on our clients.");
+?></p>
+  </div>
+
+
+  <h4 data-kerkere="[data-faq4]" data-kerkere-icon='close'><?php echo T_(""); ?></h4>
+  <div data-kerkere-content='hide' data-faq4>
+   <p><?php
+echo T_("");
+?></p>
+   <p><?php
+echo T_("");
+?></p>
+  </div>
+
+
+  <h4 data-kerkere="[data-faq5]" data-kerkere-icon='close'><?php echo T_(""); ?></h4>
+  <div data-kerkere-content='hide' data-faq5>
+   <p><?php
+echo T_("");
+?></p>
+   <p><?php
+echo T_("");
+?></p>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Are .com domains better for search engine optimization (SEO)?
+No. All domain extensions are considered equal in the eyes of Google and there is no automatic preference given to a .com domain.
+When checking to see if a domain exists, it’s important to note whether a large and recognizable company has the .com - if it does, this can hurt your search results. It can also open you up to a potential trademark battle.
+
+
+What is a premium domain?
+Premium domains are short domains, often made up of just one word or 3-5 letters. They are also known as ‘aftermarket’ or ‘pre-registered’ domains. Most premium domains have a .com extension, but many end with .org, .net, and .biz.
+As premium domains include common words they are often the most memorable. Additionally, companies value short domains that match their company name or products, meaning these domains are typically the most desirable.
+Sometimes certain domains sold by different registries are considered premium and can have a higher price point. With registry premium domains, both registration and renewal prices are set by the registry and are usually high.
+
+
+What is a new TLD?
+If you’re looking for something a little different for your business or personal website, why not consider a new TLD? There are hundreds of new fresh and exciting domain endings that can help bring your dream alive.
+Want a personal website? Share photos of your international trip with .travel, write your thoughts on the latest catwalk trends with .blog or get creative with a .is.
+Looking to conquer your industry? Set up a web developer startup with a .dev and get a .inc to show you mean business.
+
+
+Should I register multiple extensions for the same domain?
+Yes. When you register a domain extension, no one else can use it. So, if you’ve already got mydomain.com, it’s a good idea to register mydomain.net and mydomain.org and redirect them back to your original site. This helps to avoid confusion and makes capturing visitors easier.
+
+
+Is there a limit on how long or short my domain can be?
+The maximum length a domain can be is 63 characters and the minimum is 1 character. The shorter your domain is, the easier it is to type and remember.
+When choosing a domain name, keep it simple - a word that is difficult to spell will be problematic for your visitors. Pick a name that reflects your brand, industry, or you if it’s a personal project. Once you have a list of words, ask your friends and family for feedback. Have fun choosing!
+
+
+What can I do with my domain name?
+Once you have registered your domain name it’s time to use it. Maybe you want to funnel visitors to a landing page built especially for sales, or forward them to your personal Twitter page - it’s completely up to you how you use it. You can also create personalized email addresses based on your domain name. This helps to build trust in your brand and promote your company.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
 
 
 <hr>
@@ -131,65 +261,11 @@ Discover the perfect domain now
 </p>
 
 <p>
-What is a domain? What is a TLD?
-
-Think of your domain name as a street address.
-If people search for your domain, they’ll be able to find your website.
-Every website on the internet has a unique IP address assigned to it, made up of a series of numbers.
-These numbers tell the domain name system (DNS) to locate the corresponding website.
-As we are humans and not computers, IP addresses are difficult to remember and so words are used instead.
-These words are known as the domain or URL.
-The DNS looks at the domain name and translates it into an IP address.
-
-Top-level domains were introduced to help organize the locations in the domain name system.
-All domains include a top-level domain (TLD) and a second-level domain (SLD).
-Imagine you own a business in the USA. In ‘yourbusiness.us’, the TLD is .us and the SLD is ‘yourbusiness’.
 
 
 
 
-What is the difference between various TLDs? Which ones are better?
-When it comes to picking a top-level domain for your website, there are plenty of options. Yes, .com domain extensions are popular, but due to their popularity, you may find that your dream domain has already been taken.
-Why not consider choosing a generic (gTLD) like .club for your tennis club, or a country-code (ccTLDs) like .co.uk for your UK office?
-If you’re a new business it may be important to choose a familiar domain extension like .com, a localized domain or a well-known gTLD. As they are popular, they are perceived as more trustworthy. Don’t panic if you haven’t managed to get one. The web is changing all the time, and as the web grows, so will the familiarity of new TLDs.
-Have you tried Beast Mode? It’s an all-in-one tool that allows users to generate domain names, register, and buy in bulk.
-The Business Name Generator helps you discover the perfect name for your business or startup. Search thousands of domains fast, get inspiration, and collaborate with your friends and colleagues.
 
-
-How do I check domain name availability status?
-To verify domain availability, use the search bar at the top of the page. Your website name will either be available or taken. If your domain is taken, this means that it is either reserved by the Registry or registered by someone else. At this point, you can choose to make an offer for that domain name.
-If you are looking for a fast and safe domain name checker, use Jibres. We never have and never will sell any information on our clients.
-
-
-
-Are .com domains better for search engine optimization (SEO)?
-No. All domain extensions are considered equal in the eyes of Google and there is no automatic preference given to a .com domain.
-When checking to see if a domain exists, it’s important to note whether a large and recognizable company has the .com - if it does, this can hurt your search results. It can also open you up to a potential trademark battle.
-
-
-What is a premium domain?
-Premium domains are short domains, often made up of just one word or 3-5 letters. They are also known as ‘aftermarket’ or ‘pre-registered’ domains. Most premium domains have a .com extension, but many end with .org, .net, and .biz.
-As premium domains include common words they are often the most memorable. Additionally, companies value short domains that match their company name or products, meaning these domains are typically the most desirable.
-Sometimes certain domains sold by different registries are considered premium and can have a higher price point. With registry premium domains, both registration and renewal prices are set by the registry and are usually high.
-
-
-What is a new TLD?
-If you’re looking for something a little different for your business or personal website, why not consider a new TLD? There are hundreds of new fresh and exciting domain endings that can help bring your dream alive.
-Want a personal website? Share photos of your international trip with .travel, write your thoughts on the latest catwalk trends with .blog or get creative with a .is.
-Looking to conquer your industry? Set up a web developer startup with a .dev and get a .inc to show you mean business.
-
-
-Should I register multiple extensions for the same domain?
-Yes. When you register a domain extension, no one else can use it. So, if you’ve already got mydomain.com, it’s a good idea to register mydomain.net and mydomain.org and redirect them back to your original site. This helps to avoid confusion and makes capturing visitors easier.
-
-
-Is there a limit on how long or short my domain can be?
-The maximum length a domain can be is 63 characters and the minimum is 1 character. The shorter your domain is, the easier it is to type and remember.
-When choosing a domain name, keep it simple - a word that is difficult to spell will be problematic for your visitors. Pick a name that reflects your brand, industry, or you if it’s a personal project. Once you have a list of words, ask your friends and family for feedback. Have fun choosing!
-
-
-What can I do with my domain name?
-Once you have registered your domain name it’s time to use it. Maybe you want to funnel visitors to a landing page built especially for sales, or forward them to your personal Twitter page - it’s completely up to you how you use it. You can also create personalized email addresses based on your domain name. This helps to build trust in your brand and promote your company.
 </p>
 
 
