2c1ab23998869007cfcbb9bd8843ebb962ac90fe
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-20 18:46:55 +0330

    complete text of domain page

--- content/domains/home/definition.php
+++ content/domains/home/definition.php
@@ -29,22 +29,44 @@
     <figure class="c6 s12 pRa5">
      <img src='<?php echo \dash\url::static() ?>/img/domain/second-level-domain.png' alt=''>
      <figcaption><?php echo T_("To the left of the dot is the second level domain, which might be your name, the name of your business, blog, or store. It's the 'identity' of your site. In our case, the second level domain is 'jibres'."); ?></figcaption>
-
     </figure>
     <figure class="c6 s12 pLa5">
      <img src='<?php echo \dash\url::static() ?>/img/domain/top-level-domain.png' alt=''>
      <figcaption><?php echo T_("To the right of the dot is the top-level domain (or TLD), in this case the TLD is '.com', but you may have also seen TLDs like .net, or .org, or country identifiers like .ir or .us."); ?></figcaption>
-
     </figure>
    </div>
+
+   <p><?php echo T_("While the top level domain is essential for a domain to function, it's also less exciting than the second level domain. Which is probably why it's sometimes referred to as the 'parent' domain."); ?></p>
+   <p><?php echo T_("The exciting thing about domains is that you can choose almost any name you want as your second level domain. And sure, you can go with .com for your top level domain, but who doesn't want a cool 'parent'? Options like .club, .store, even .pizza are just a few ways you can boost your top level domain game."); ?></p>
+  </section>
+  <hr class="mB50">
+
+
+  <section>
+   <h3><?php echo T_("What's the purpose of a domain?"); ?></h3>
+   <p><?php echo T_("At some point someone probably said, 'Hey, you ought to register a domain.'' And you might have replied, 'Okay... but why?'"); ?></p>
+   <p><?php echo T_('The truth is that if you have a website, it needs to have an address, and that address needs to have a name.'); ?></p>
+
+   <h4><?php echo T_('But there are other good reasons to register a domain too.'); ?></h4>
+   <p><?php echo T_("Registering a domain reserves it so no one else can register it. So it might be smart to snatch up a domain now—your personal name, company name, or other things you're involved with, like a book title, band name, or hobby—just to take it 'off the market'."); ?></p>
+   <p><?php echo T_("In my case, I might not need the domain CosmoHedgehog.rocks today, but I'll definitely want to own it once I become super famous."); ?></p>
+
+   <p class="txtB fs11 mB5"><?php echo T_("There's also custom email."); ?></p>
+   <p><?php echo T_("Want an email address like me@cosmohedgehog.rocks? To get such a slick, personalized address, you'll need to reserve the domain first, then attach it to email hosting. This is a smart move especially if you're running a business site. That way, your customers can easily remember info@mybusiness as easily as they remember your domain itself."); ?></p>
+  </section>
+  <hr class="mB50">
+
+
+  <section>
+   <h3><?php echo T_("Don't have a website?"); ?> <?php echo T_("No problem!"); ?></h3>
+   <p><?php echo T_("You can point your domain to your Instagram, Facebook, Twitter, Telegram or other social media account. I don't know about you, but I'd much rather tell people to friend me at CosmoHedgehog.rocks than give out some long, convoluted URL. That reminds me, I should really register that domain now."); ?></p>
   </section>
 
 
-
-   <p><?php echo T_(''); ?></p>
-   <p><?php echo T_(''); ?></p>
-   <p><?php echo T_(''); ?></p>
-
-
+  <section>
+   <h3><?php echo T_("What's the difference between a domain and hosting?"); ?></h3>
+   <p><?php echo T_("To have a website, you need both a domain and hosting. Your domain gives people a way to find your website, and your hosting is space on a server (a remote computer) where all of your website content resides."); ?></p>
+   <p><?php echo T_("Think of a domain like it's your website's street address, and hosting is like the website's house. You can have your address before you build your house, but to show off all your photos on your website, you'll need a place to put them."); ?></p>
+  </section>
 
  </blockquote>
