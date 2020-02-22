e26aa745804d314fb0c9c39fbc2268f13841be7a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-22 14:12:38 +0330

    css bugfix on sale

--- content_my/domain/view.php
+++ content_my/domain/view.php
@@ -10,8 +10,6 @@ class view
 		\dash\data::site_desc(T_("Jibres is not just an online accounting software;"). ' '.  T_("We try to create the best financial platform that has everything you need to sale and manage your financial life."));
 		\dash\data::site_slogan(T_("Integrated Sales and Online Accounting"));
 
-
-		\dash\data::include_adminPanel(true);
 		\dash\data::include_highcharts(true);
 
 		// enable title box
