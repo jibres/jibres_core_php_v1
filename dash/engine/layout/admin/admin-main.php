0c2b8182370355316563781f9a5f4e87bfbbf608
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-22 14:06:45 +0330

    fix admin xhr

--- dash/engine/layout/admin/admin-main.php
+++ dash/engine/layout/admin/admin-main.php
@@ -1,4 +1,4 @@
-<div id='content' class='scr' data-xhr='content'>
+<div id='content' class='scr'>
 <?php require_once ('admin-titlebox.php');?>
 <?php require_once \dash\engine\layout\fn::display();?>
 </div>
