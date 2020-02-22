64df2e133497b7476ad0991a51cdb07fd5d8aab6
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-22 17:43:19 +0330

    fix search result

--- content/domains/search/display.php
+++ content/domains/search/display.php
@@ -9,41 +9,42 @@
   </div>
  </form>
 
+  <div class="result ltr">
  	<?php if(\dash\data::infoResult()) {?>
  		<?php foreach (\dash\data::infoResult() as $key => $value) {?>
- 			<?php if(@$value['soon']) {?>
- 				<div class="msg success2">
-		 			<div class="f">
-		 				<div class="c"><?php echo $key; ?> </div>
-		 				<div class="c"><?php echo T_("Domain ready to register"); ?></div>
+ 			<?php if(isset($value['soon']) && $value['soon']) {?>
+ 				<div class="msg minimal mB5">
+		 			<div class="f align-center pL10">
+		 				<div class="c pR10"><?php echo $key; ?> </div>
+		 				<div class="cauto pR20"></div>
 		 				<div class="cauto">
-		 					<a class="btn"><?php echo T_("Soon"); ?></a>
+		 					<a class="btn"><?php echo T_("Coming Soon"); ?></a>
 		 				</div>
 		 			</div>
 		 		</div>
  			<?php continue; ?>
  			<?php }// endif ?>
 
- 			<?php if(@$value['available']) {?>
+ 			<?php if(isset($value['available']) && $value['available']) {?>
 
-		 		<div class="msg success2">
-		 			<div class="f">
-		 				<div class="c"><?php echo $key; ?> </div>
-		 				<div class="c"><?php echo T_("Domain ready to register"); ?></div>
+		 		<div class="msg minimal mB5 success2">
+		 			<div class="f align-center pL10">
+		 				<div class="c pR10 txtB"><?php echo $key; ?> </div>
+		 				<div class="cauto pR20"><span class="compact"><?php echo T_('Toman'). ' '. \dash\fit::number('2000') ?></span> / <del class="compact fc-mute"><?php echo \dash\fit::number('5000'); ?></del></div>
 		 				<div class="cauto">
-		 					<a class="btn primary" href="<?php echo \dash\url::kingdom(); ?>/my/domain/buy/<?php echo $key; ?>"><?php echo T_("Add to cart"); ?></a>
+		 					<a class="btn success" href="<?php echo \dash\url::kingdom(); ?>/my/domain/buy/<?php echo $key; ?>"><?php echo T_("Buy"); ?></a>
 		 				</div>
 		 			</div>
 		 		</div>
 
  			<?php }else{ ?>
 
- 				<div class="msg">
-		 			<div class="f">
-		 				<div class="c"><?php echo $key; ?> </div>
-		 				<div class="c"><?php echo T_("Can not register this domain"); ?></div>
+ 				<div class="msg minimal mB5 danger2">
+		 			<div class="f align-center pL10">
+		 				<div class="c pR10"><?php echo $key; ?> </div>
+		 				<div class="cauto pR20"><?php echo T_("Taken"); ?></div>
 		 				<div class="cauto">
-		 					<a class="btn" href="<?php echo \dash\url::kingdom(); ?>/whois/<?php echo $key; ?>"><?php echo T_("Who is?"); ?></a>
+		 					<a class="btn" href="<?php echo \dash\url::kingdom(); ?>/whois/<?php echo $key; ?>"><?php echo T_("Whois taken?"); ?></a>
 		 				</div>
 		 			</div>
 		 		</div>
@@ -71,7 +72,7 @@
  		</div>
 
  	<?php } //endif ?>
-
+  </div>
 
  </div>
  <div class="fit">
