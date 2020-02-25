63bb3d1fb59494dadfb0ca608d8cf5c3d9cb1eb7
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-25 18:24:25 +0330

    fix change holder page

--- content_my/domain/setting/holder/display.php
+++ content_my/domain/setting/holder/display.php
@@ -2,33 +2,34 @@
 <div class="f justify-center">
 	<div class="c6 m8 s12">
 		<div class="cbox">
+			<div class="msg minimal pLR20-f fs16 txtB txtL success"><?php echo \dash\data::domainDetail_name() ?></div>
 
-				<form method="post" autocomplete="off" class="mB20" >
-				    <label for="iholder"><?php echo T_("Domain Holder"); ?></label>
-					<div class="input ltr">
-						<input type="text" name="holder" id="iholder" maxlength="50" disabled value="<?php echo \dash\data::domainDetail_holder(); ?>" >
-					</div>
+			<p class="msg info2"><?php echo T_('You can change domain technical and billing holder to another one to allow them to do some action.'); ?></p>
+			<form method="post" autocomplete="off" class="mB20" >
+			    <label for="iholder"><?php echo T_("Domain Holder"); ?></label>
+				<div class="input ltr">
+					<input type="text" name="holder" id="iholder" maxlength="50" disabled value="<?php echo \dash\data::domainDetail_holder(); ?>" >
+				</div>
 
-					<label for="iadmin"><?php echo T_("Domain Admin"); ?></label>
-					<div class="input ltr">
-						<input type="text" name="admin" id="iadmin" maxlength="50" disabled value="<?php echo \dash\data::domainDetail_admin(); ?>" >
-					</div>
+				<label for="iadmin"><?php echo T_("Domain Admin"); ?></label>
+				<div class="input ltr">
+					<input type="text" name="admin" id="iadmin" maxlength="50" disabled value="<?php echo \dash\data::domainDetail_admin(); ?>" >
+				</div>
 
-					<label for="itech"><?php echo T_("Domain Technical"); ?></label>
-					<div class="input ltr">
-						<input type="text" name="tech" id="itech" maxlength="50" value="<?php echo \dash\data::domainDetail_tech(); ?>" >
-					</div>
+				<label for="itech"><?php echo T_("Domain Technical"); ?></label>
+				<div class="input ltr">
+					<input type="text" name="tech" id="itech" maxlength="50" value="<?php echo \dash\data::domainDetail_tech(); ?>" >
+				</div>
 
-					<label for="ibill"><?php echo T_("Domain Billing"); ?></label>
-					<div class="input ltr">
-						<input type="text" name="bill" id="ibill" maxlength="50" value="<?php echo \dash\data::domainDetail_bill(); ?>" >
-					</div>
+				<label for="ibill"><?php echo T_("Domain Billing"); ?></label>
+				<div class="input ltr">
+					<input type="text" name="bill" id="ibill" maxlength="50" value="<?php echo \dash\data::domainDetail_bill(); ?>" >
+				</div>
 
-					<div class="txtRa">
-						<button class="btn success"><?php echo T_("Update"); ?></button>
-					</div>
-				</form>
-			</div>
+				<div class="txtRa mT25">
+					<button class="btn success"><?php echo T_("Update"); ?></button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
