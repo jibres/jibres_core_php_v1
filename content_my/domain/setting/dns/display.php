f44462013ae8ea285ec2ffe8fbad632d0c4e73d7
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-25 18:44:29 +0330

    some fix on dns

--- content_my/domain/setting/dns/display.php
+++ content_my/domain/setting/dns/display.php
@@ -2,69 +2,70 @@
 <div class="f justify-center">
 	<div class="c6 m8 s12">
 		<div class="cbox">
+			<div class="msg minimal pLR20-f fs16 txtB ltr txtL success"><?php echo \dash\data::domainDetail_name() ?></div>
 
-				<form method="post" autocomplete="off" class="mB20" >
-					<p><?php echo T_("You can edit your dns record") ?></p>
+			<p class="msg warn2"><?php echo T_('Filling the server IP address records (glue records) are required when you wish to set the name servers of a domain name to a hostname under the domain name itself.'); ?></p>
+			<form method="post" autocomplete="off" class="mB20" >
+				<p><?php echo T_("You can edit your dns record") ?></p>
 
-					  <div class="f">
+				  <div class="f">
 
 
-				    	<div class="c6 s12">
-						    <label for="ns1"><?php echo T_("DNS #1"); ?></label>
+			    	<div class="c6 s12">
+					    <label for="ns1"><?php echo T_("DNS #1"); ?></label>
+						<div class="input ltr">
+							<input type="text" name="ns1" id="ns1" maxlength="50" value="<?php echo \dash\data::domainDetail_ns1(); ?>" >
+						</div>
+						<label for="ns2"><?php echo T_("DNS #2"); ?></label>
+						<div class="input ltr">
+							<input type="text" name="ns2" id="ns2" maxlength="50" value="<?php echo \dash\data::domainDetail_ns2(); ?>" >
+						</div>
+			    	</div>
+
+			    	<div class="c6 s12">
+			    		<div class="mLa5">
+							<label for="ip1"><?php echo T_("Server IP #1"); ?></label>
 							<div class="input ltr">
-								<input type="text" name="ns1" id="ns1" maxlength="50" value="<?php echo \dash\data::domainDetail_ns1(); ?>" >
+								<input type="text" name="ip1" id="ip1" maxlength="50" value="<?php echo \dash\data::domainDetail_ip1(); ?>" >
 							</div>
-							<label for="ns2"><?php echo T_("DNS #2"); ?></label>
+							<label for="ip2"><?php echo T_("Server IP #2"); ?></label>
 							<div class="input ltr">
-								<input type="text" name="ns2" id="ns2" maxlength="50" value="<?php echo \dash\data::domainDetail_ns2(); ?>" >
+								<input type="text" name="ip2" id="ip2" maxlength="50" value="<?php echo \dash\data::domainDetail_ip2(); ?>" >
 							</div>
-				    	</div>
+			    		</div>
+			    	</div>
 
-				    	<div class="c6 s12">
-				    		<div class="mLa5">
-								<label for="ip1"><?php echo T_("IP #1"); ?></label>
-								<div class="input ltr">
-									<input type="text" name="ip1" id="ip1" maxlength="50" value="<?php echo \dash\data::domainDetail_ip1(); ?>" >
-								</div>
-								<label for="ip2"><?php echo T_("IP #2"); ?></label>
-								<div class="input ltr">
-									<input type="text" name="ip2" id="ip2" maxlength="50" value="<?php echo \dash\data::domainDetail_ip2(); ?>" >
-								</div>
-				    		</div>
-				    	</div>
+			    	<div class="c6 s12">
+						<label for="ns3"><?php echo T_("DNS #3"); ?></label>
+						<div class="input ltr">
+							<input type="text" name="ns3" id="ns3" maxlength="50" value="<?php echo \dash\data::domainDetail_ns3(); ?>" >
+						</div>
+						<label for="ns4"><?php echo T_("DNS #4"); ?></label>
+						<div class="input ltr">
+							<input type="text" name="ns4" id="ns4" maxlength="50" value="<?php echo \dash\data::domainDetail_ns4(); ?>" >
+						</div>
+			    	</div>
 
-				    	<div class="c6 s12">
-							<label for="ns3"><?php echo T_("DNS #3"); ?></label>
+			    	<div class="c6 s12">
+			    		<div class="mLa5">
+							<label for="ip3"><?php echo T_("Server IP #3"); ?></label>
 							<div class="input ltr">
-								<input type="text" name="ns3" id="ns3" maxlength="50" value="<?php echo \dash\data::domainDetail_ns3(); ?>" >
+								<input type="text" name="ip3" id="ip3" maxlength="50" value="<?php echo \dash\data::domainDetail_ip3(); ?>" >
 							</div>
-							<label for="ns4"><?php echo T_("DNS #4"); ?></label>
+
+							<label for="ip4"><?php echo T_("Server IP #4"); ?></label>
 							<div class="input ltr">
-								<input type="text" name="ns4" id="ns4" maxlength="50" value="<?php echo \dash\data::domainDetail_ns4(); ?>" >
+								<input type="text" name="ip4" id="ip4" maxlength="50" value="<?php echo \dash\data::domainDetail_ip4(); ?>" >
 							</div>
-				    	</div>
-
-				    	<div class="c6 s12">
-				    		<div class="mLa5">
-								<label for="ip3"><?php echo T_("IP #3"); ?></label>
-								<div class="input ltr">
-									<input type="text" name="ip3" id="ip3" maxlength="50" value="<?php echo \dash\data::domainDetail_ip3(); ?>" >
-								</div>
-
-								<label for="ip4"><?php echo T_("IP #4"); ?></label>
-								<div class="input ltr">
-									<input type="text" name="ip4" id="ip4" maxlength="50" value="<?php echo \dash\data::domainDetail_ip4(); ?>" >
-								</div>
-				    		</div>
-				    	</div>
-				    </div>
+			    		</div>
+			    	</div>
+			    </div>
 
 
-					<div class="txtRa">
-						<button class="btn success"><?php echo T_("Update"); ?></button>
-					</div>
-				</form>
-			</div>
+				<div class="txtRa">
+					<button class="btn success"><?php echo T_("Update"); ?></button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
