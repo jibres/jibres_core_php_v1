e1a20e1e73f2be31d1979dc856fc22d7e6232472
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-26 13:35:21 +0330

    some fix

--- content_my/domain/setting/display.php
+++ content_my/domain/setting/display.php
@@ -1,21 +1,118 @@
 <?php require_once (root. 'content_my/domain/setting/pageStep.php'); ?>
+
+<div class="f fs14 mT10 mB20">
+ <div class="c6 pRa5">
+  <div class="panel">
+    <table class="tbl1 v4 mB0">
+     <tr>
+      <th><?php echo T_('Domain') ?></th>
+      <td class="ltr txtL txtB"><?php echo \dash\data::domainDetail_name(); ?></td>
+     </tr>
+     <tr>
+      <th><?php echo T_('Registered on') ?></th>
+      <td class="ltr txtL"><?php echo \dash\fit::date(\dash\data::domainDetail_dateregister()); ?></td>
+     </tr>
+     <tr>
+      <th><?php echo T_('Expired on') ?></th>
+      <td class="ltr txtL"><?php echo \dash\fit::date(\dash\data::domainDetail_dateexpire()); ?></td>
+     </tr>
+     <tr>
+      <th><?php echo T_('Transfer lock') ?></th>
+      <td class="txtL"><a href="<?php echo \dash\url::that(). '/transfer?domain='. \dash\request::get('domain'); ?>" class="ibtn wide"><?php
+if(\dash\data::domainDetail_lock())
+{
+ echo '<div class="fc-green"><span>'.T_("Locked"). '</span>'. '<i class="sf-lock"></i></div>';
+}
+else
+{
+ echo '<div class="fc-red"><span>'.T_("Unlocked"). '</span>'. '<i class="sf-unlock"></i></div>';
+}?></a></td>
+     </tr>
+     <tr>
+      <th><?php echo T_('Auto Renew') ?></th>
+      <td class="txtL"><?php
+if(\dash\data::domainDetail_autorenew())
+{
+ echo "<div class='ibtn wide fc-blue' data-confirm data-data='{\"myaction\" : \"autorenew\", \"op\" :\"unset\"}'><span>". T_('Automatic'). "</span><i class='sf-refresh'></i></div>";
+}
+else
+{
+ echo "<div class='ibtn wide fc-red' data-confirm data-data='{\"myaction\" : \"autorenew\", \"op\" :\"set\"}'><span>". T_('Disabled'). "</span><i class='sf-times'></i></div>";
+}
+?></td>
+     </tr>
+    </table>
+  </div>
+ </div>
+
+ <div class="c6 pLa5">
+  <div class="panel">
+    <table class="tbl1 v4 mB0">
+     <tr>
+      <td><?php echo T_('Name Servers'). ' - '. T_("DNS #1") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns1() ?></td>
+     </tr>
+     <tr>
+      <td><?php echo T_('Name Servers'). ' - '. T_("DNS #2") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns2() ?></td>
+     </tr>
+
+     <?php if(\dash\data::domainDetail_ns3()) {?>
+     <tr>
+      <td><?php echo T_('Name Servers'). ' - '. T_("DNS #3") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns3() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+     <?php if(\dash\data::domainDetail_ns4()) {?>
+     <tr>
+      <td><?php echo T_('Name Servers'). ' - '. T_("DNS #4") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns4() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+     <?php if(\dash\data::domainDetail_ip1()) {?>
+     <tr>
+      <td><?php echo T_("IP #1") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip1() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+     <?php if(\dash\data::domainDetail_ip2()) {?>
+     <tr>
+      <td><?php echo T_("IP #2") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip2() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+     <?php if(\dash\data::domainDetail_ip3()) {?>
+     <tr>
+      <td><?php echo T_("IP #3") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip3() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+     <?php if(\dash\data::domainDetail_ip4()) {?>
+     <tr>
+      <td><?php echo T_("IP #4") ?></td>
+      <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip4() ?></td>
+     </tr>
+     <?php } //endif ?>
+
+    </table>
+  </div>
+ </div>
+</div>
+
+
+
+
 <div class="f justify-center">
  <div class="c6 m8 s12">
   <div class="cbox">
-      <div class="msg minimal pLR20-f fs16 txtB ltr txtL success"><?php echo \dash\data::domainDetail_name() ?></div>
-
 
    <table class="tbl1 v4">
 
-    <tr class="positive">
-     <td><?php echo T_("Name") ?></td>
-     <td class="txtB txtL ltr"><?php echo \dash\data::domainDetail_name() ?></td>
-    </tr>
-
-    <tr>
-     <td><?php echo T_("Expire date") ?></td>
-     <td class="txtL ltr"><?php echo \dash\fit::date_time(\dash\data::domainDetail_dateexpire()); ?></td>
-    </tr>
 
     <tr>
      <td><?php echo T_("IRNIC holder") ?></td>
@@ -36,75 +133,6 @@
      <td><?php echo T_("IRNIC technical") ?></td>
      <td class="txtL ltr"><?php echo \dash\data::domainDetail_tech(); ?></td>
     </tr>
-
-    <tr>
-     <td><div class="ibtn wide"><?php echo '<span>'.T_("Autorenew"). '</span>'; if(\dash\data::domainDetail_autorenew()) { echo '<i class="sf-refresh fc-blue"></i>'; } else{ echo '<i class="sf-times fc-red"></i>'; }?></div></td>
-     <td class="txtL ltr">
-      <?php  if(\dash\data::domainDetail_autorenew()) {?>
-       <div class="btn" data-confirm data-data='{"myaction" : "autorenew", "op" :"unset"}'><?php echo T_("Click To disable autorenew"); ?></div>
-      <?php }else{ ?>
-       <div class="btn primary2" data-confirm data-data='{"myaction" : "autorenew", "op" :"set"}'><?php echo T_("Click To enable autorenew"); ?></div>
-      <?php }// endif ?>
-     </td>
-    </tr>
-
-
-    <tr>
-     <td><?php echo T_("Domain Lock Status") ?></td>
-     <td class="txtL ltr"><div class="ibtn wide"><?php echo '<span>'.T_("Lock"). '</span>'; if(\dash\data::domainDetail_lock()) { echo '<i class="sf-lock fc-green"></i>'; } else{ echo '<i class="sf-unlock fc-red"></i>'; }?></div></td>
-    </tr>
-
-
-    <tr>
-     <td><?php echo T_("DNS #1") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns1() ?></td>
-    </tr>
-    <tr>
-     <td><?php echo T_("DNS #2") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns2() ?></td>
-    </tr>
-
-    <?php if(\dash\data::domainDetail_ns3()) {?>
-    <tr>
-     <td><?php echo T_("DNS #3") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns3() ?></td>
-    </tr>
-    <?php } //endif ?>
-
-    <?php if(\dash\data::domainDetail_ns4()) {?>
-    <tr>
-     <td><?php echo T_("DNS #4") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ns4() ?></td>
-    </tr>
-    <?php } //endif ?>
-
-    <?php if(\dash\data::domainDetail_ip1()) {?>
-    <tr>
-     <td><?php echo T_("IP #1") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip1() ?></td>
-    </tr>
-    <?php } //endif ?>
-
-    <?php if(\dash\data::domainDetail_ip2()) {?>
-    <tr>
-     <td><?php echo T_("IP #2") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip2() ?></td>
-    </tr>
-    <?php } //endif ?>
-
-    <?php if(\dash\data::domainDetail_ip3()) {?>
-    <tr>
-     <td><?php echo T_("IP #3") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip3() ?></td>
-    </tr>
-    <?php } //endif ?>
-
-    <?php if(\dash\data::domainDetail_ip4()) {?>
-    <tr>
-     <td><?php echo T_("IP #4") ?></td>
-     <td class="txtL ltr"><?php echo \dash\data::domainDetail_ip4() ?></td>
-    </tr>
-    <?php } //endif ?>
 
    </table>
 
