0584a8f2387adaa16853c5d5ca06af0abe10a1e5
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-26 14:37:13 +0330

    fix ltr design

--- content_my/domain/setting/display.php
+++ content_my/domain/setting/display.php
@@ -7,29 +7,29 @@
     <table class="tbl1 v4 mB0">
      <tr>
       <th><?php echo T_('Domain') ?> <a class="link mLa5" target="_blank" rel="nofollow" href="http://<?php echo \dash\data::domainDetail_name(); ?>"><i class=" mRa5 sf-link"></i></a></th>
-      <td class="ltr txtL txtB"><?php echo \dash\data::domainDetail_name(); ?></td>
+      <td class="ltr txtRa txtB"><?php echo \dash\data::domainDetail_name(); ?></td>
      </tr>
      <tr>
       <th><?php echo T_('Registrar') ?></th>
-      <td class="ltr txtL"><?php echo T_(\dash\data::domainDetail_registrar()); ?></td>
+      <td class="ltr txtRa"><?php echo T_(\dash\data::domainDetail_registrar()); ?></td>
      </tr>
      <tr>
       <th><?php echo T_('Registered on') ?></th>
-      <td class="ltr txtL"><?php echo \dash\fit::date(\dash\data::domainDetail_dateregister()); ?></td>
+      <td class="ltr txtRa"><?php echo \dash\fit::date(\dash\data::domainDetail_dateregister()); ?></td>
      </tr>
      <tr>
       <th><?php echo T_('Expired on') ?> <a class="link mLa5" href="<?php echo \dash\url::this(). '/renew?domain='. \dash\request::get('domain'); ?>"><?php echo T_('Renew'); ?></a></th>
-      <td class="ltr txtL"><?php echo \dash\fit::date(\dash\data::domainDetail_dateexpire()); ?></td>
+      <td class="ltr txtRa"><?php echo \dash\fit::date(\dash\data::domainDetail_dateexpire()); ?></td>
      </tr>
 <?php if(\dash\data::domainDetail_datemodified()) {?>
      <tr>
       <th><?php echo T_('Last activity') ?></th>
-      <td class="ltr txtL"><?php echo \dash\fit::date(\dash\data::domainDetail_datemodified()); ?></td>
+      <td class="ltr txtRa"><?php echo \dash\fit::date(\dash\data::domainDetail_datemodified()); ?></td>
      </tr>
 <?php }?>
      <tr>
       <th><?php echo T_('Transfer lock') ?> <a class="mLa5" href="<?php echo \dash\url::that(). '/transfer?domain='. \dash\request::get('domain'); ?>"><?php echo T_('Manage'); ?></a></th>
-      <td class="txtL"><div class="ibtn wide"><?php
+      <td class="txtRa"><div class="ibtn wide"><?php
 if(\dash\data::domainDetail_lock())
 {
  echo '<div class="fc-green"><span>'.T_("Locked"). '</span>'. '<i class="sf-lock"></i></div>';
@@ -50,7 +50,7 @@ else
  echo "<span class='link mLa5' data-confirm data-data='{\"myaction\" : \"autorenew\", \"op\" :\"set\"}'>". T_('Enable'). "</span>";
 }
       ?></th>
-      <td class="txtL"><?php
+      <td class="txtRa"><?php
 if(\dash\data::domainDetail_autorenew())
 {
  echo "<div class='ibtn wide fc-blue'><span>". T_('Automatic'). "</span><i class='sf-refresh'></i></div>";
@@ -70,27 +70,27 @@ else
     <table class="tbl1 v4 mB0">
      <tr>
       <td><a href="<?php echo \dash\url::that(). '/dns?domain='. \dash\request::get('domain'); ?>"><?php echo T_('Name Servers'). ' - DNS' ?></a></td>
-      <td class="txtL"><a rel="nofollow" target="_blank" class="btn secondary sm outline" href="https://intodns.com/<?php echo \dash\data::domainDetail_name(); ?>"><?php echo T_("check DNS server and mail server health"); ?></a></td>
+      <td class="txtRa"><a rel="nofollow" target="_blank" class="btn secondary sm outline" href="https://intodns.com/<?php echo \dash\data::domainDetail_name(); ?>"><?php echo T_("check DNS server and mail server health"); ?></a></td>
      </tr>
      <tr>
-      <td class="ltr txtL"><?php echo \dash\data::domainDetail_ns1() ?></td>
+      <td class="ltr txtRa"><?php echo \dash\data::domainDetail_ns1() ?></td>
       <td class="ltr txtR"><?php echo \dash\data::domainDetail_ip1() ?></td>
      </tr>
      <tr>
-       <td class="ltr txtL"><?php echo \dash\data::domainDetail_ns2() ?></td>
+       <td class="ltr txtRa"><?php echo \dash\data::domainDetail_ns2() ?></td>
        <td class="ltr txtR"><?php echo \dash\data::domainDetail_ip2() ?></td>
      </tr>
 
      <?php if(\dash\data::domainDetail_ns3()) {?>
      <tr>
-      <td class="ltr txtL"><?php echo \dash\data::domainDetail_ns3() ?></td>
+      <td class="ltr txtRa"><?php echo \dash\data::domainDetail_ns3() ?></td>
       <td class="ltr txtR"><?php echo \dash\data::domainDetail_ip3() ?></td>
      </tr>
      <?php } //endif ?>
 
      <?php if(\dash\data::domainDetail_ns4()) {?>
      <tr>
-      <td class="ltr txtL"><?php echo \dash\data::domainDetail_ns4() ?></td>
+      <td class="ltr txtRa"><?php echo \dash\data::domainDetail_ns4() ?></td>
       <td class="ltr txtR"><?php echo \dash\data::domainDetail_ip4() ?></td>
      </tr>
      <?php } //endif ?>
@@ -101,19 +101,19 @@ else
     <table class="tbl1 v4 mB0">
      <tr>
       <td><a href="<?php echo \dash\url::that(). '/dns?domain='. \dash\request::get('domain'); ?>"><?php echo T_("IRNIC holder") ?></td>
-      <td class="txtL ltr"><?php echo \dash\data::domainDetail_holder(); ?></td>
+      <td class="txtRa ltr"><?php echo \dash\data::domainDetail_holder(); ?></td>
      </tr>
      <tr>
       <td><a href="<?php echo \dash\url::that(). '/dns?domain='. \dash\request::get('domain'); ?>"><?php echo T_("IRNIC admin") ?></td>
-      <td class="txtL ltr"><?php echo \dash\data::domainDetail_admin(); ?></td>
+      <td class="txtRa ltr"><?php echo \dash\data::domainDetail_admin(); ?></td>
      </tr>
      <tr>
       <td><a href="<?php echo \dash\url::that(). '/dns?domain='. \dash\request::get('domain'); ?>"><?php echo T_("IRNIC billing") ?></td>
-      <td class="txtL ltr"><?php echo \dash\data::domainDetail_bill(); ?></td>
+      <td class="txtRa ltr"><?php echo \dash\data::domainDetail_bill(); ?></td>
      </tr>
      <tr>
       <td><a href="<?php echo \dash\url::that(). '/dns?domain='. \dash\request::get('domain'); ?>"><?php echo T_("IRNIC technical") ?></td>
-      <td class="txtL ltr"><?php echo \dash\data::domainDetail_tech(); ?></td>
+      <td class="txtRa ltr"><?php echo \dash\data::domainDetail_tech(); ?></td>
      </tr>
     </table>
   </div>
