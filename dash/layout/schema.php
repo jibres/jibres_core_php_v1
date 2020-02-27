eecd27cce099e5dc3223e661968a355d14b1ae2c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-27 19:16:08 +0330

    add simple schema

---
+++ dash/layout/schema.php
@@ -0,0 +1,16 @@
+<script type="application/ld+json">
+{
+ "@context" : "http://schema.org",
+ "@type" : "Organization",
+ "name" : "<?php echo T_("Jibres") ?>",
+ "url" : "<?php echo \dash\url::kingdom(); ?>",
+ "sameAs" : [
+   "https://twitter.com/jibres_com",
+   "https://www.facebook.com/jibres",
+   "https://instagram.com/jibres_com"
+   "https://github.com/jibres",
+   "https://linkedin.com/jibres_com",
+   "https://t.me/jibres",
+   ]
+}
+</script>
