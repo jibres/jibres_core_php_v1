f9fd1e2b45cad5006b676953fd6a54ba2cd3f99d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-27 15:19:18 +0330

    fix catalog link

--- content/catalog/controller.php
+++ content/catalog/controller.php
@@ -7,7 +7,7 @@ class controller
 	public static function routing()
 	{
 		$lastVersion = '7.0';
-		$dlLink      = \dash\url::base().'/static/catalog/Jibres-Catalog-v'. $lastVersion. '-preview.pdf';
+		$dlLink      = \dash\url::cdn().'/catalog/Jibres-Catalog-v'. $lastVersion. '-preview.pdf';
 
 		\dash\redirect::to($dlLink, false, T_('Jibres Catalog'));
 	}
