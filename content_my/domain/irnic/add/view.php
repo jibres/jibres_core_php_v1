4525567ebf50453e3146f13278a0e39d42a12513
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-29 13:19:17 +0330

    fix item3

--- content_my/domain/irnic/add/view.php
+++ content_my/domain/irnic/add/view.php
@@ -8,6 +8,10 @@ class view
 	{
 		\dash\data::page_title(T_("Add IRNIC handle"));
 
+		if(\dash\request::get('type') === 'new')
+		{
+			\dash\data::page_title(T_("Create IRNIC handle"));
+		}
 		// btn
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::that());
