bac7f9d902539235842e21f44ed09d844c1e773e
Reza Mohiti <rm.biqarar@gmail.com>
2020-03-01 09:18:23 +0330

    disable store cronjob

--- dash/engine/cronjob/cronjob.php
+++ dash/engine/cronjob/cronjob.php
@@ -52,23 +52,23 @@ class cronjob
 		$exec[]              = $store_exec;
 
 		// load other store name
-		$list_stores = glob($list_stores_addr. '*');
+		// $list_stores = glob($list_stores_addr. '*');
 
-		if(empty($list_stores))
-		{
-			self::save_log('stores subdomain folder is empty!');
-		}
-		else
-		{
-			foreach ($list_stores as $key => $value)
-			{
-				$subdomain           = str_replace($list_stores_addr, '', $value);
-				$HTTP_HOST           = $subdomain. '.'. $SERVER_NAME;
-				$server['HTTP_HOST'] = $HTTP_HOST;
-				$store_exec = 'php '. $index_php_addr. " '". json_encode($server, JSON_UNESCAPED_UNICODE). "' ";
-				$exec[] = $store_exec;
-			}
-		}
+		// if(empty($list_stores))
+		// {
+		// 	self::save_log('stores subdomain folder is empty!');
+		// }
+		// else
+		// {
+		// 	foreach ($list_stores as $key => $value)
+		// 	{
+		// 		$subdomain           = str_replace($list_stores_addr, '', $value);
+		// 		$HTTP_HOST           = $subdomain. '.'. $SERVER_NAME;
+		// 		$server['HTTP_HOST'] = $HTTP_HOST;
+		// 		$store_exec = 'php '. $index_php_addr. " '". json_encode($server, JSON_UNESCAPED_UNICODE). "' ";
+		// 		$exec[] = $store_exec;
+		// 	}
+		// }
 
 		$exec_addr = __DIR__. '/exec.me.php';
 
