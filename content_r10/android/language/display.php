43d19f4d610467c068414efe084e49115418c413
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-04 22:14:52 +0330

    fix choose language page

--- content_r10/android/language/display.php
+++ content_r10/android/language/display.php
@@ -1,31 +1,6 @@
-<div class="cn" id="android_chooseLang">
- <header>
-  <h1><?php echo T_('Choose'); ?></h1>
-  <h2><?php echo T_('your language'); ?></h2>
- </header>
-
- <div class="f">
-  <div class="c6 pRa5">
-   <a href="jibres://language/en" class="ltr langItem{%if requestGET.device == 'en'%} device{%endif%}">
-    <p class="hello">Hello!</p>
-    <p  class="iam">I'm Jibres</p>
-    <div class="lang">English</div>
-   </a>
-  </div>
-
-  <div class="c6 pLa5">
-   <a href="jibres://language/fa" class="rtl langItem{%if requestGET.device == 'fa'%} device{%endif%}">
-    <p class="hello">سلام!</p>
-    <p class="iam">من جیبرس هستم</p>
-    <div class="lang">فارسی</div>
-   </a>
-  </div>
- </div>
-</div>
-
 
 <style>
- body{background-color: #f9e7e0;webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-tap-highlight-color:transparent;}
+ body{background-color: #f9e7e0;}
  #android_chooseLang.cn{width:82%!important;max-width: 400px!important;min-width: 260px!important;}
  #android_chooseLang header{padding: 20vh 0 30px;}
  #android_chooseLang h1 {font-weight: 900;color:#000;font-size:20px;}
@@ -40,3 +15,28 @@
  #android_chooseLang .langItem p.iam{margin-bottom:10px;line-height: 25px;height:50px;font-size:15px;}
  #android_chooseLang .langItem .lang{color:#f7ab8d;font-size:14px;line-height: 20px;}
 </style>
+
+<div class="cn" id="android_chooseLang">
+ <header>
+  <h1><?php echo T_('Choose'); ?></h1>
+  <h2><?php echo T_('your language'); ?></h2>
+ </header>
+
+ <div class="f">
+  <div class="c6 pRa5">
+   <a href="jibres://language/en" class="ltr langItem<?php if(\dash\request::get('device') === 'en') echo ' device'; ?>">
+    <p class="hello">Hello!</p>
+    <p  class="iam">I'm Jibres</p>
+    <div class="lang">English</div>
+   </a>
+  </div>
+
+  <div class="c6 pLa5">
+   <a href="jibres://language/fa" class="rtl langItem<?php if(\dash\request::get('device') === 'fa') echo ' device'; ?>">
+    <p class="hello">سلام!</p>
+    <p class="iam">من جیبرس هستم</p>
+    <div class="lang">فارسی</div>
+   </a>
+  </div>
+ </div>
+</div>
