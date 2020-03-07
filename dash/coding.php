6e3e727d1e8408d04a1d2d9e10d0330d2af884ca
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-07 15:59:28 +0330

    add store encoding

--- dash/coding.php
+++ dash/coding.php
@@ -21,6 +21,7 @@ class coding
 	public static $ALPHABET_NUMBER = '1358962407';
 	// public static $ALPHABET     = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ';
 	public static $ALPHABET        = 'jBRS2kJ3yP4dLxN5tH6zC7vG8rD9bFhVmqWgKsQnYpTfZcMwX';
+	public static $ALPHABET_STORE  = '123456789abcdefghjkmnpqrstvwxyz';
 	public static $ALPHABET_ALL    = 'kjhOYUrxRDFIgZC2bKQ4Ww1JezVB890vE5opl3cLPtfNMdsASXnmiTyu67qaGH';
 
 
