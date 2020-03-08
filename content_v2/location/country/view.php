672ae0ad724c2801f858c7324be8197418b9125b
Reza <rm.biqarar@gmail.com>
2020-03-08 20:48:44 +0330

    bug fix load country flag

--- content_v2/location/country/view.php
+++ content_v2/location/country/view.php
@@ -15,7 +15,7 @@ class view
 				$temp         = $value;
 				$temp['id']   = $key;
 				$temp['text'] = $value['localname'] ? $value['localname'] : $value['name'];
-				$temp['flag'] = \dash\url::site(). '/static/img/flags/png100px/'. mb_strtolower($key). '.png';
+				$temp['flag'] = \dash\url::cdn(). '/img/flags/png100px/'. mb_strtolower($key). '.png';
 
 				if(empty($new_data))
 				{
