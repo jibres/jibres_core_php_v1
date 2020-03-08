6738d114967aef10a8055987361c444241b72b74
Reza <rm.biqarar@gmail.com>
2020-03-08 20:41:59 +0330

    add location home api to work js

---
+++ content_v2/location/controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace content_v2\location;
+
+
+class controller
+{
+	public static function routing()
+	{
+
+	}
+}
+?>
---
+++ content_v2/location/view.php
@@ -0,0 +1,12 @@
+<?php
+namespace content_v2\location;
+
+
+class view
+{
+	public static function config()
+	{
+		\content_v2\location\country\view::config();
+	}
+}
+?>
