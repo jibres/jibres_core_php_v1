ad65627e1c15a2f59e06a2f037a232d5370ec8c0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-08 20:38:45 +0330

    fix api location

--- dash/layout/foundation.php
+++ dash/layout/foundation.php
@@ -52,7 +52,7 @@ if(\dash\url::store())
  <meta content="yes" name="apple-mobile-web-app-capable"/>
  <meta content="index, follow" name ="robots"/>
  <meta content="<?php echo \dash\url::site();?>" name="jibres:site"/>
- <meta content="<?php echo \dash\url::sitelang();?>/api/" name="jibres:api"/>
+ <meta content="<?php echo \dash\url::set_subdomain('api');?>/" name="jibres:api"/>
  <meta content="<?php echo \dash\url::cdn();?>/" name="jibres:cdn"/>
  <link href="<?php echo \dash\url::cdn();?>/favicons/apple-touch-icon.png?v=1" sizes="180x180" rel="apple-touch-icon"/>
  <link href="<?php echo \dash\url::cdn();?>/favicons/favicon-64x64.png?v=1" sizes="64x64" type="image/png" rel="icon"/>
