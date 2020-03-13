c5af0817bc5865d4ed6adcafe69dc5c39af337ac
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-13 18:26:43 +0330

    complete team page

--- content/team/display.php
+++ content/team/display.php
@@ -1,10 +1,11 @@
 
 <div class="jibresBanner">
- <div class="fit">
-
-   <div class="browserBox" data-board='https://tejarak.com/ermile'><span></span>
-    <iframe src="https://tejarak.com/fa/ermile?ex=1" frameborder="0" style="border:0"></iframe>
-   </div>
-
+ <div class="fit txtC">
+ 	<p class="mB25"><?php echo \dash\data::page_desc(); ?></p>
+ 	<p><a class="btn pain" target="_blank" href="<?php echo \dash\url::kingdom(). '/careers' ?>"><?php echo T_("Join Jibres Team"); ?></a></p>
  </div>
+</div>
+
+<div class="browserBox" data-board='https://tejarak.com/ermile'><span></span>
+<iframe src="https://tejarak.com/fa/ermile?ex=1" frameborder="0" style="border:0"></iframe>
 </div>
--- content/team/view.php
+++ content/team/view.php
@@ -7,11 +7,14 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Jibres Team'));
-		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::page_desc("We are here to build something different. Something new from scratch, that's really cool.");
 		// btn
 		\dash\data::back_text(T_('Home'));
 		\dash\data::back_link(\dash\url::kingdom());
 
+		// btn
+		\dash\data::action_text(T_('Join us'));
+		\dash\data::action_link(\dash\url::kingdom(). '/careers');
 	}
 }
 ?>
