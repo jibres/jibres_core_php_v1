1116060a8130b4522c270e12040b8f836549f04f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-13 22:16:09 +0330

    add some detail to team page

--- content/team/display.php
+++ content/team/display.php
@@ -1,12 +1,19 @@
 
 <div class="jibresBanner">
  <div class="fit txtC">
- 	<p class=""><?php echo \dash\data::page_desc(); ?></p>
- 	<h2 class="mB25"><?php echo T_('Meet the people behind Jibres'); ?></h2>
- 	<p><a class="btn pain" target="_blank" href="<?php echo \dash\url::kingdom(). '/careers' ?>"><?php echo T_("Join Jibres Team"); ?></a></p>
+ 	<p><?php echo \dash\data::page_desc(); ?></p>
+ 	<h2 class="txtB"><?php echo T_('The best thing about Jibres is our people.'); ?></h2>
+ 	<hr>
+
+ 	<p><?php echo T_("We are curious. Our team is made up of pioneering innovators that approach some of the Internet’s toughest challenges with interest and a desire to learn. We are committed to providing exceptional customer service and solve customer problems in clever and technically astute ways."); ?></p>
+
+ 	<p class="mT20"><a class="btn pain" target="_blank" href="<?php echo \dash\url::kingdom(). '/careers' ?>"><?php echo T_("Join Jibres Team"); ?></a></p>
  </div>
 </div>
 
+ <div class="fit txtC">
+ 	<h2><?php echo T_('Meet the people behind Jibres'); ?></h2>
+ </div>
 <div class="browserBox" data-board='https://tejarak.com/ermile'><span></span>
 <iframe src="https://tejarak.com/fa/ermile?ex=1" frameborder="0" style="border:0"></iframe>
 </div>
