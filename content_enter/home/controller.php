43de55a9821fc767c03369f00d8ddd37b50b3b35
Reza <rm.biqarar@gmail.com>
2020-03-13 10:57:31 +0330

    clean save enter param

--- content_enter/home/controller.php
+++ content_enter/home/controller.php
@@ -38,20 +38,39 @@ class controller
 	 */
 	public static function save_param()
 	{
-		$param = $_REQUEST;
+		// get safe request
+		$post = \dash\request::post();
+		$get  = \dash\request::get();
 
-		if(!is_array($param))
+		$param = array_merge($get, $post);
+
+		// no param to save
+		if(!$param)
 		{
-			$param = [];
+			return;
 		}
 
 		$save_param = [];
 
+		// check count of input
+		$count      = 0;
+
 		foreach ($param as $key => $value)
 		{
-			if(substr($key, 0, 5) === 'param')
+			$count++;
+
+			// check just string value can save in session
+			if(is_string($key) && is_string($value) && mb_strlen($key) < 50 && mb_strlen($value) < 100)
 			{
-				$save_param[substr($key, 6)] = $value;
+				if(substr($key, 0, 5) === 'param')
+				{
+					$save_param[substr($key, 6)] = $value;
+				}
+			}
+
+			if($count > 10)
+			{
+				break;
 			}
 		}
 
