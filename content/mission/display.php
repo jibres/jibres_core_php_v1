c5c12ea025b0779443b381466f8c214f8d928cb4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-13 23:23:18 +0330

    add mission page

---
+++ content/mission/display.php
@@ -0,0 +1,31 @@
+
+<div class="jibresBanner">
+ <div class="fit zero">
+ 	<img class="block" src="<?php echo \dash\url::cdn(); ?>/img/pages/jibres-mission-earth.jpg">
+ </div>
+ <div class="fit">
+ 	<p class="mB25"><?php echo \dash\data::page_desc(); ?></p>
+
+
+
+ 	<p><?php echo T_("We are on a mission to simplify business system and their teams can spend more time creating money."); ?></p>
+
+
+ 	<p><?php echo T_("Jibres is a values-driven organization. Here is what we believe in."); ?></p>
+
+ 	<h3><?php echo T_("Simplicity in All We DO"); ?></h3>
+ 	<p><?php echo T_("We make decisions that eliminate complexity and obstacles to our moving fast so we can enable our Customers to focus on testing their Ideas, building their businesses and realizing their dreams."); ?></p>
+
+ 	<h3><?php echo T_("We Speak Up When We Have Something To Say and Listen When Others DO"); ?></h3>
+ 	<p><?php echo T_("We create an environment where we are transparent and respectful so we are confident in bringing our best selves on behalf of our customers and each other, every day."); ?></p>
+
+
+ 	<h3><?php echo T_("We Are Accountable To Deliver On Our Commitments"); ?></h3>
+ 	<p><?php echo T_("Earning trust every day is our core value proposition to our customers and to each other. We must be trustworthy, because the opportunity to serve our customers is larger than any one of us."); ?></p>
+
+
+ 	<h3><?php echo T_("Love is At Our Core"); ?></h3>
+ 	<p><?php echo T_("Underlying everything we do, love for our customers and our DO is essential to realizing our fullest potential."); ?></p>
+
+ </div>
+</div>
