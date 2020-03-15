c824e8eb2a0648e537a7ef06cec252da876e2c06
Reza <rm.biqarar@gmail.com>
2020-03-15 18:15:02 +0330

    add script.js into store creating

--- content_my/store/creating/display.php
+++ content_my/store/creating/display.php
@@ -14,39 +14,3 @@
     </div>
 
 
-<script type="text/javascript">
-
-document.addEventListener('DOMContentLoaded', function() {
-
-
-  $.ajax(
-    {
-     url : "<?php echo \dash\url::that(); ?>",
-     type : 'post',
-     data : {"create":"store"},
-     dataType: 'json',
-     success : function()
-     {
-      Navigate({ url: "<?php echo \dash\url::this(); ?>/opening" });
-     },
-     done : function()
-     {
-      Navigate({ url: "<?php echo \dash\url::this(); ?>/opening" });
-     },
-     fail : function()
-     {
-      Navigate({ url: "<?php echo \dash\url::this(); ?>/error" });
-     },
-     statusCode: {
-      501: function() {
-        Navigate({ url: "<?php echo \dash\url::this(); ?>/error" });
-      }
-     }
-   });
-
-  $("#linkmhe").delay( (60 * 1000) ).fadeIn( 400 );
-
-}, false);
-
-
-</script>
---
+++ content_my/store/creating/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+  $.ajax(
+    {
+     url : "<?php echo \dash\url::that(); ?>",
+     type : 'post',
+     data : {"create":"store"},
+     dataType: 'json',
+     success : function()
+     {
+      Navigate({ url: "<?php echo \dash\url::this(); ?>/opening" });
+     },
+     done : function()
+     {
+      Navigate({ url: "<?php echo \dash\url::this(); ?>/opening" });
+     },
+     fail : function()
+     {
+      Navigate({ url: "<?php echo \dash\url::this(); ?>/error" });
+     },
+     statusCode: {
+      501: function() {
+        Navigate({ url: "<?php echo \dash\url::this(); ?>/error" });
+      }
+     }
+   });
+
+  $("#linkmhe").delay( (60 * 1000) ).fadeIn( 400 );
+
+}, false);
