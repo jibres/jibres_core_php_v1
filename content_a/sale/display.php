620143b6e8ff5a875af74f6ca200fb68a051c0c2
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-16 18:38:29 +0330

    add search in product id

--- content_a/sale/display.php
+++ content_a/sale/display.php
@@ -3,7 +3,7 @@
 <form method="post" autocomplete="off" class="f" id='factorAdd' data-disallowEnter data-msgNewError='<?php echo T_("You can add new empty tab if current tab is filled!"); ?>' <?php if(\dash\request::get('extra') == 'true') {?> data-autoClose=2000 <?php } //endif ?>>
   <div class="c9 s12 pRa10">
 
-    <div class="cbox p0 selectBox">
+    <div class="cbox p0" id="searchInProducts">
       <select name="product" class="select22 barCode" id="productSearch" data-model='html' multiple2="multiple" data-selection='clean' autofocus data-default data-ajax--url='<?php echo \dash\url::this(); ?>?json=true' data-shortkey-search data-placeholder='<?php echo T_("Search in list to add product"); ?> +'>
       </select>
     </div>
