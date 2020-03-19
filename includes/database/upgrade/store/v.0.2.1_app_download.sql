6e1a92c897d0369310777ffab2c0fcfa6ccbae9c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-19 17:09:17 +0330

    app download table update

--- includes/database/upgrade/store/v.0.2.1_app_download.sql
+++ includes/database/upgrade/store/v.0.2.1_app_download.sql
@@ -1,15 +1,14 @@
 CREATE TABLE `jibres_XXXXXXX`.`app_download` (
 `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-`platform` enum('android','ios','telegram','website') DEFAULT NULL,
-`store_app_id` bigint(20) UNSIGNED  NULL,
+`os` enum('android','ios','windows','linux','bot','other') DEFAULT NULL,
 `version` smallint(5) UNSIGNED  NULL,
 `build` smallint(5) UNSIGNED  NULL,
 `user_id` int(10) UNSIGNED  NULL,
-`date` timestamp NULL DEFAULT NULL,
+`downloaddate` timestamp NULL DEFAULT NULL,
 PRIMARY KEY (`id`),
-KEY `app_download_index_search_platform` (`platform`),
+KEY `app_download_index_search_os` (`os`),
 KEY `app_download_index_search_version` (`version`),
 KEY `app_download_index_search_build` (`build`),
-KEY `app_download_index_search_date` (`date`),
+KEY `app_download_index_search_downloaddate` (`downloaddate`),
 CONSTRAINT `app_download_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
