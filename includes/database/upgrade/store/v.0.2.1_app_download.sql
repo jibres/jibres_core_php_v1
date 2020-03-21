b7b657e0be2a08c6285db720107069a680f1bc8e
Reza <rm.biqarar@gmail.com>
2020-03-21 12:20:57 +0430

    fix database field name

--- includes/database/upgrade/store/v.0.2.1_app_download.sql
+++ includes/database/upgrade/store/v.0.2.1_app_download.sql
@@ -4,11 +4,11 @@ CREATE TABLE `jibres_XXXXXXX`.`app_download` (
 `version` smallint(5) UNSIGNED  NULL,
 `build` smallint(5) UNSIGNED  NULL,
 `user_id` int(10) UNSIGNED  NULL,
-`downloaddate` timestamp NULL DEFAULT NULL,
+`datedownload` timestamp NULL DEFAULT NULL,
 PRIMARY KEY (`id`),
 KEY `app_download_index_search_os` (`os`),
 KEY `app_download_index_search_version` (`version`),
 KEY `app_download_index_search_build` (`build`),
-KEY `app_download_index_search_downloaddate` (`downloaddate`),
+KEY `app_download_index_search_datedownload` (`datedownload`),
 CONSTRAINT `app_download_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
