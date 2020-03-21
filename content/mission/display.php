365d9b87557f0b9c12d542fabd68a477ee5a5e58
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 02:53:56 +0430

    fix mission text

--- content/mission/display.php
+++ content/mission/display.php
@@ -11,21 +11,21 @@
 
  <div class="fit">
 
- 	<p><?php echo T_("Jibres is a values-driven organization. Here is what we believe in."); ?></p>
+ 	<p><?php echo T_("Jibres is a values driven organization. Here is what we believe in."); ?></p>
 
- 	<h3><?php echo T_("Simplicity in All We DO"); ?></h3>
- 	<p><?php echo T_("We make decisions that eliminate complexity and obstacles to our moving fast so we can enable our Customers to focus on testing their Ideas, building their businesses and realizing their dreams."); ?></p>
+ 	<h3><?php echo T_("Simplicity"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("We make decisions that eliminate complexity and obstacles to our moving fast so we can enable our customers to focus on testing their Ideas, building their businesses and realizing their dreams."); ?></p>
 
- 	<h3><?php echo T_("We Speak Up When We Have Something To Say and Listen When Others DO"); ?></h3>
- 	<p><?php echo T_("We create an environment where we are transparent and respectful so we are confident in bringing our best selves on behalf of our customers and each other, every day."); ?></p>
-
-
- 	<h3><?php echo T_("We Are Accountable To Deliver On Our Commitments"); ?></h3>
- 	<p><?php echo T_("Earning trust every day is our core value proposition to our customers and to each other. We must be trustworthy, because the opportunity to serve our customers is larger than any one of us."); ?></p>
+ 	<h3><?php echo T_("Transparency"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("We create an environment where we are transparent and respectful so we are confident in bringing our best selves on behalf of our customers and each other, every day."); ?></p>
 
 
- 	<h3><?php echo T_("Love is At Our Core"); ?></h3>
- 	<p><?php echo T_("Underlying everything we do, love for our customers and our DO is essential to realizing our fullest potential."); ?></p>
+ 	<h3><?php echo T_("Responsibility"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("Earning trust every day is our core value proposition to our customers and to each other. We must be trustworthy, because the opportunity to serve our customers is larger than any one of us."); ?></p>
+
+
+ 	<h3><?php echo T_("Love"); ?></h3>
+ 	<p class="msg"><?php echo T_("Underlying everything, love for our service and customers is essential to realizing our fullest potential."); ?></p>
 
  </div>
 
