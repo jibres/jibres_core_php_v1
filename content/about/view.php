ae8c1f40541755fa7f75942d3b65646d2da8df12
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 03:43:13 +0430

    fix about desc

--- content/about/view.php
+++ content/about/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('About Jibres'));
-		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::page_desc(T_("Advancement of technology and development of Web-based business Cause Need new tools to resolve the daily needs and that’s the goal of making Jibres."));
 		// btn
 		\dash\data::back_text(T_('Home'));
 		\dash\data::back_link(\dash\url::kingdom());
