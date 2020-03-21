e79a8b183a607616c690b9fafa58e3f49240f02c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 03:21:03 +0430

    add values page

---
+++ content/values/display.php
@@ -0,0 +1,31 @@
+<div class="jibresBanner">
+ <div class="fit zero">
+ 	<img class="block" src="<?php echo \dash\url::cdn(); ?>/img/bg/jibres-values-1.jpg" alt='<?php echo \dash\data::page_title();?>'>
+ </div>
+
+
+ <div class="fit">
+
+ 	<p><?php echo T_("Jibres is a values driven organization. Here is what we believe in."); ?></p>
+
+ 	<h3><?php echo T_("Simplicity"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("We make decisions that eliminate complexity and obstacles to our moving fast so we can enable our customers to focus on testing their Ideas, building their businesses and realizing their dreams."); ?></p>
+
+ 	<h3><?php echo T_("Transparency"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("We create an environment where we are transparent and respectful so we are confident in bringing our best selves on behalf of our customers and each other, every day."); ?></p>
+
+
+ 	<h3><?php echo T_("Responsibility"); ?></h3>
+ 	<p class="msg mB50"><?php echo T_("Earning trust every day is our core value proposition to our customers and to each other. We must be trustworthy, because the opportunity to serve our customers is larger than any one of us."); ?></p>
+
+
+ 	<h3><?php echo T_("Love"); ?></h3>
+ 	<p class="msg"><?php echo T_("Underlying everything, love for our service and customers is essential to realizing our fullest potential."); ?></p>
+
+ </div>
+
+ <div class="fit zero">
+ 	<img class="block" src="<?php echo \dash\url::cdn(); ?>/img/bg/jibres-mission-3.jpg" alt='<?php echo \dash\data::page_title();?>'>
+ </div>
+
+</div>
---
+++ content/values/view.php
@@ -0,0 +1,20 @@
+<?php
+namespace content\values;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_('Jibres Values'));
+		\dash\data::page_desc("We are on a mission to simplify business system and their teams can spend more time creating money.");
+		// btn
+		\dash\data::back_text(T_('Home'));
+		\dash\data::back_link(\dash\url::kingdom());
+
+		// btn
+		\dash\data::action_text(T_('Our Mission'));
+		\dash\data::action_link(\dash\url::kingdom(). '/mission');
+	}
+}
+?>
