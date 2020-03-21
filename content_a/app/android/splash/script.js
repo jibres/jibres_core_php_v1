b33219df388797bf7cb8a6e675c3efbe5b5765f1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-21 19:51:41 +0430

    get color

--- content_a/app/android/splash/script.js
+++ content_a/app/android/splash/script.js
@@ -1,4 +1,29 @@
 
-$('input[type=radio][name=bedStatus]').change(function() {
 
+$('[data-page="app_android_splash"] input[type=radio][name=theme]').change(function() {
+  console.log(this.value);
+
+  var from      = '#6DE195';
+  var to        = '#C4E759';
+  var colortext = '#000000';
+  var colordesc = '#333333';
+
+
+  $('[data-page="app_android_splash"] #from').val(from);
+  $('[data-page="app_android_splash"] #to').val(to);
+  $('[data-page="app_android_splash"] #colortext').val(colortext);
+  $('[data-page="app_android_splash"] #colordesc').val(colordesc);
+
+
+  applySplashColors();
 });
+
+
+function applySplashColors()
+{
+  $('[data-page="app_android_splash"] #from').val()
+  $('[data-page="app_android_splash"] #to').val()
+  $('[data-page="app_android_splash"] #colortext').val()
+  $('[data-page="app_android_splash"] #colordesc').val()
+}
+
