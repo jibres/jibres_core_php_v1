726477250f642a15a690f2173aaddbda7f29cf27
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 03:44:49 +0430

    fix desc of some pages

--- content/domains/search/view.php
+++ content/domains/search/view.php
@@ -7,12 +7,10 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_('Domain Name Search'));
-		\dash\data::page_desc(\dash\data::site_desc());
 		\dash\data::page_desc(T_('Every website starts with a great domain name.'). ' '. T_('Find your dream domain.'));
-
 		// btn
-		\dash\data::back_text(T_('Home'));
-		\dash\data::back_link(\dash\url::kingdom());
+		\dash\data::back_text(T_('Domains'));
+		\dash\data::back_link(\dash\url::kingdom(). '/domains');
 
 		$q = \dash\request::get('q');
 		$q = urldecode($q);
