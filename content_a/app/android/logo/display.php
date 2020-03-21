895ac44959333b38ee623eeac0c0f23c11078657
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-21 17:56:11 +0430

    fix logo page

--- content_a/app/android/logo/display.php
+++ content_a/app/android/logo/display.php
@@ -1,15 +1,15 @@
 <?php require_once(core. 'layout/tools/stepGuide.php'); ?>
 <div class="f justify-center">
   <div class="c8 m12 s12">
-    <div class="cbox">
-      <?php if(\dash\data::appQueue()) {?>
-        <div class="msg warn2"><?php echo T_("Changing these values ​​will need to be rebuilt") ?></div>
-      <?php } //endif ?>
-      <h4><?php echo T_("Set your application title logo"); ?></h4>
+    <form method="post" autocomplete="off" class="box">
+      <header><h4><?php echo T_("Set your application title logo");?></h4></header>
+      <div class="body zeroPad">
+<?php if(\dash\data::appQueue()) {?>
+        <div class="msg warn2 mB0"><?php echo T_("Changing these values ​​will need to be rebuilt") ?></div>
+<?php } //endif ?>
+      </div>
 
-      <form method="post" autocomplete="off">
-
-
+      <div class="body">
         <label for="logo"><?php echo T_("Logo"); ?> <small><?php echo T_("Use a square logo in png format"); ?></small></label>
         <div class="input">
           <input type="file" name="logo" id="logo">
@@ -17,18 +17,17 @@
 
 
         <?php if(\dash\get::index(\dash\data::appDetail(), 'logo')) {?>
-        <div class="mediaBox mB20">
+        <div class="mediaBox">
           <img src="<?php echo \dash\get::index(\dash\data::appDetail(), 'logo'); ?>" alt="<?php echo \dash\data::appDetail_title(); ?>" id="logoPreview">
         </div>
         <?php } ?>
+      </div>
 
 
-
-        <div class="txtRa">
-          <button class="btn success"><?php echo T_("Save"); ?></button>
-        </div>
-      </form>
-    </div>
+      <footer class="txtRa">
+        <button class="btn success"><?php echo T_("Save"); ?></button>
+      </footer>
+    </form>
   </div>
 </div>
 
--- content_a/app/android/setting/display.php
+++ content_a/app/android/setting/display.php
@@ -1,14 +1,14 @@
 <?php require_once(core. 'layout/tools/stepGuide.php'); ?>
 <div class="f justify-center">
   <div class="c6 m8 s12">
-    <section class="box">
+    <form method="post" autocomplete="off" class="box">
       <header><h2><?php echo T_("Set your application title and logo"); ?></h2></header>
-      <div class="body">
+      <div class="body zeroPad">
         <?php if(\dash\data::appQueue()) {?>
-          <div class="msg warn2"><?php echo T_("Changing these values ​​will need to be rebuilt") ?></div>
+          <div class="msg warn2 mB0"><?php echo T_("Changing these values ​​will need to be rebuilt") ?></div>
         <?php } //endif ?>
-
-        <form method="post" autocomplete="off">
+      </div>
+      <div class="body">
 
           <label for="title"><?php echo T_("Application title"); ?></label>
           <div class="input">
@@ -23,10 +23,9 @@
           <label for="desc"><?php echo T_("Application desc"); ?></label>
           <textarea class="txt mB10" name="desc" maxlength="150" rows="3" id="desc" ><?php echo \dash\data::appDetail_desc(); ?></textarea>
 
-        </form>
       </div>
       <footer class="txtRa"><button class="btn success"><?php echo T_("Save"); ?></button></footer>
-    </section>
+    </form>
   </div>
 </div>
 
