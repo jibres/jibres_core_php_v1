5425d2d297d8eafe766685d025877b18bca12f68
Reza <rm.biqarar@gmail.com>
2020-03-21 19:40:33 +0430

    add splash theme color range

--- lib/app/application/splash.php
+++ lib/app/application/splash.php
@@ -66,5 +66,28 @@ class splash
 	}
 
 
+	private static function theme_color()
+	{
+		$theme_color =
+		[
+			"gradient0"  => ["start" => "#6DE195",   "end" => "#C4E759"],
+			"gradient1"  => ["start" => "#41C7AF",   "end" => "#54E38E"],
+			"gradient2"  => ["start" => "#99E5A2",   "end" => "#D4FC78"],
+			"gradient3"  => ["start" => "#ABC7FF",   "end" => "#C1E3FF"],
+			"gradient4"  => ["start" => "#6CACFF",   "end" => "#8DEBFF"],
+			"gradient5"  => ["start" => "#5583EE",   "end" => "#41D8DD"],
+			"gradient6"  => ["start" => "#A16BFE",   "end" => "#DEB0DF"],
+			"gradient7"  => ["start" => "#D279EE",   "end" => "#F8C390"],
+			"gradient8"  => ["start" => "#F78FAD",   "end" => "#FDEB82"],
+			"gradient9"  => ["start" => "#BC3D2F",   "end" => "#A16BFE"],
+			"gradient10" => ["start" => "#A43AB2",   "end" => "#E13680"],
+			"gradient11" => ["start" => "#9D2E7D",   "end" => "#E16E93"],
+			"gradient12" => ["start" => "#F5CCF6",   "end" => "#F1EEF9"],
+			"gradient13" => ["start" => "#F0EFF0",   "end" => "#FAF8F9"],
+			"gradient14" => ["start" => "#121317",   "end" => "#323B42"],
+		];
+	}
+
+
 }
 ?>
