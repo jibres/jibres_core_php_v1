6751477009c4daedbac06dfca6760345bc645eb3
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 17:53:05 +0430

    fix app

--- content_a/app/android/appPreview.php
+++ content_a/app/android/appPreview.php
@@ -1,8 +1,8 @@
     <section class="mobileFrame" data-splash style="background: linear-gradient(0deg, #5583EE, #41D8DD);">
       <div class="screen">
         <img src="<?php echo \dash\data::appDetail_logo(); ?>" alt='<?php echo \dash\data::appDetail_title(); ?>'>
-        <h1 style="color:#000"><?php echo \dash\data::appDetail_title(); ?></h1>
-        <h2 style="color:#333"><?php echo \dash\data::appDetail_slogan(); ?></h2>
+        <h2 style="color:#000"><?php echo \dash\data::appDetail_title(); ?></h2>
+        <h3 style="color:#333"><?php echo \dash\data::appDetail_slogan(); ?></h3>
         <div class="desc" style="color:#333"><?php echo \dash\data::appDetail_desc(); ?></div>
       </div>
     </section>
--- content_a/app/android/intro/display.php
+++ content_a/app/android/intro/display.php
@@ -37,9 +37,20 @@
     <div class="c6 s12">
        <section class="mobileFrame" data-intro>
         <div class="screen">
-          <img src="<?php echo \dash\get::index($introSaved, $i, 'image'); ?>" alt='<?php echo \dash\get::index($introSaved, $i, 'title'); ?>'>
-          <h1><?php echo \dash\get::index($introSaved, $i, 'title'); ?></h1>
-          <h2><?php echo \dash\get::index($introSaved, $i, 'desc'); ?></h2>
+          <div class="imgBox">
+            <img src="<?php echo \dash\get::index($introSaved, $i, 'image'); ?>" alt='<?php echo \dash\get::index($introSaved, $i, 'title'); ?>'>
+          </div>
+          <h2><?php echo \dash\get::index($introSaved, $i, 'title'); ?></h2>
+          <p><?php echo \dash\get::index($introSaved, $i, 'desc'); ?></p>
+          <nav class="f">
+            <?php if ($i === 3) {?>
+            <div class="c12 next"><?php echo "Get Started"; ?></div>
+            <?php } else{?>
+            <div class="c4 prev txtLa"><?php echo "Skip"; ?></div>
+            <div class="c4 step"></div>
+            <div class="c4 next txtRa"><?php echo "Next"; ?></div>
+            <?php }?>
+          </nav>
         </div>
        </section>
     </div>
