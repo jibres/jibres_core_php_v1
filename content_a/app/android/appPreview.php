5a38590c05d50a8a627e3675250ef1542309df6f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 17:04:40 +0430

    fix mobileFrame

--- content_a/app/android/appPreview.php
+++ content_a/app/android/appPreview.php
@@ -1,4 +1,4 @@
-    <div data-frame='iphone-x' data-activity='splash' style="background: linear-gradient(0deg, #eee, #999);">
+    <div class="mobileFrame" data-activity='splash' style="background: linear-gradient(0deg, #eee, #999);">
       <img src="<?php echo \dash\data::appDetail_logo(); ?>" alt='<?php echo \dash\data::appDetail_title(); ?>'>
       <h1 style="color:#000"><?php echo \dash\data::appDetail_title(); ?></h1>
       <h2 style="color:#333"><?php echo \dash\data::appDetail_slogan(); ?></h2>
