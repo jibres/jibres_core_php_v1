e1bb613e6b8b3af1b799090ae306c2f083821821
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-22 05:19:55 +0430

    add some text to certificates

--- content/certificates/display.php
+++ content/certificates/display.php
@@ -3,27 +3,60 @@
   <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/bg/jibres-certificates-1.jpg" alt='<?php echo \dash\data::page_title();?>'>
  </div>
 
+<?php
+if(\dash\language::current() === 'fa')
+{
+?>
 <section id="jibresCertificates">
   <div class="fit-md">
     <h2><?php echo T_('Jibres Certificates'); ?></h2>
 
-			<div class="f align-center">
+			<div class="f align-center mB20">
 	      <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://pub.daneshbonyan.ir/"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-daneshbonyan.png" alt='DaneshBonyan Jibres'></a></div>
-	      <div class="c9 s12"><p></p></div>
+	      <div class="c9 s12"><p><?php echo T_("From the first day of starting this company we are stay ahead of the tech curve and create cutting edge technology."); ?></p><p><?php echo T_("In spring of 2019 after finish analyse and evaluation process of our services, Iran Vice-Presidency for science and technology is confirmed that our works is knowledge based."); ?></p></div>
 			</div>
 
-			<div class="f align-center">
+			<div class="f align-center mB20">
+      	<!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bank-markazi.png" alt=''></a></div> -->
+			</div>
 
-    <div class="f">
-      <!-- <div class="c3 s6"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bank-markazi.png" alt=''></a></div> -->
-      <!-- <div class="c3 s6"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-shaparak.png" alt=''></a></div> -->
-      <div class="c3 s6"><a tabindex='-1' target="_blank" href="https://jibres.ir"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bmn.png" alt=''></a></div>
-      <div class="c3 s6"><a tabindex='-1' target="_blank" rel='nofollow' href="https://trustseal.enamad.ir/?id=118387&Code=2iL8ULp5lVA5oSXMRiLp"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-enamad.png" alt=''></a></div>
-      <div class="c3 s6"><a tabindex='-1' target="_blank" rel='nofollow' href="http://qom.irannsr.org/index.php?module=cdk&func=loadmodule&system=cdk&sismodule=user/content_view.php&cnt_id=225820&ctp_id=61&id=17394&sisOp=view"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-nsr.png" alt=''></a></div>
-      <!-- <div class="c3 s6"><a tabindex='-1' target="_blank" rel='nofollow' href=""><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-brand.png" alt=''></a></div> -->
-      <div class="c3 s6"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-samandehi.png" alt='' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=162977&p=rfthgvkauiwkpfvljyoejyoe", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=460, height=600, top=30")'></div>
-    </div>
+
+			<div class="f align-center mB20">
+      	<!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-shaparak.png" alt=''></a></div> -->
+			</div>
+
+
+			<div class="f align-center mB20">
+	      <div class="c9 s12"><p>جیبرس توسط تیمی از نخبگان و مهندسان و متخصصان ایرانی ساخته شده است. ما جیبرس را ساخته‌ایم تا به‌سهم خود ایران را بسازیم. مهم‌ترین ویژگی اعضای تیم جیبرس، مهارت آن‌هاست. </p></div>
+      	<div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" href="https://jibres.ir/certificates"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bmn.png" alt=''></a></div>
+			</div>
+
+
+			<div class="f align-center mB20">
+      	<div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://trustseal.enamad.ir/?id=118387&Code=2iL8ULp5lVA5oSXMRiLp"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-enamad.png" alt=''></a></div>
+	      <div class="c9 s12"><p>نماد اعتماد الکترونیکی که توسط مرکز توسعه تجارت الکترونیکی در وزارت صنعت، معدن و تجارت صادر می‌شود از تاریخ ۱۸ اسفندماه ۱۳۹۷ توسط جیبرس اخذ شده است تا علاوه‌بر مجوز فعالیت، منجر به افزایش اعتماد مخاطبان جیبرس برای پرداخت‌های اینترنتی گردد.</p></div>
+			</div>
+
+
+			<div class="f align-center mB20">
+	      <div class="c9 s12"><p>فعالیت جیبرس تحت عنوان شرکت روز اندیش کویر پیما با مسئولیت محدود در ایران انجام می‌شود. شرکت روزاندیش کویرپیما از تاریخ ۱۷ تیرماه ۱۳۹۶ به عضویت سازمان نظام صنفی رایانه ای کشور درآمده است. مجوز فعالیت شرکت با شماره نظام صنفی رایانه ای ۲۵۰۱۰۱۳۹ طبق قانون حمایت از پدید آوردندگان نرم‌افزارهای رایانه‌ای مصوب ۴/۱۰/۱۳۷۹ مجلس شورای اسلامی و آئین‌نامه اجرایی شماره ۲۱۱۸۵ ت ۲۶۰۸۹ هـ مورخه ۲۴/۴/۱۳۸۳ مصوب دولت جمهوری اسلامی ایران در زمینه فناوری اطلاعات و ارتباطات بوده است.</p></div>
+      	<div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="http://qom.irannsr.org/index.php?module=cdk&func=loadmodule&system=cdk&sismodule=user/content_view.php&cnt_id=225820&ctp_id=61&id=17394&sisOp=view"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-nsr.png" alt=''></a></div>
+			</div>
+
+
+			<div class="f align-center mB20">
+      	<!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href=""><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-brand.png" alt=''></a></div> -->
+			</div>
+
+
+			<div class="f align-center">
+      	<div class="c3 s12 pRa10"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-samandehi.png" alt='' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=162977&p=rfthgvkauiwkpfvljyoejyoe", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=460, height=600, top=30")'></div>
+	      <div class="c9 s12"><p>صلاحیت فعالیت جیبرس از تاریخ ۹ آذرماه ۱۳۹۸ توسط مرکز فناوری اطلاعات و رسانه‌های دیجیتال وزارت فرهنگ و ارشاد اسلامی ایران احراز شده است. این مجوز فعالیت با کد شامد 1-2-709257-65-0-6 برای جیبرس به ثبت رسیده است و بدین ترتیب لوگوی طلایی رسانه برخط به جیبرس اعطا شده است.</p></div>
+
+			</div>
+
   </div>
 </section>
+<?php } ?>
 
 </div>
