b47175a45582a29340e0c5c4ea01e599835f4f9a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-24 18:11:02 +0430

    improve dashboard

--- content_a/android/home/display.php
+++ content_a/android/home/display.php
@@ -1,22 +1,19 @@
 <?php require_once(root. 'content_a/android/setupGuide.php'); ?>
 
-	<div class="box">
-		<head></head>
-		<div class="body">
-			<div class="f">
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/logo'; ?>" class="ibtn block"><span><?php echo T_("Logo") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/setting'; ?>" class="ibtn block"><span><?php echo T_("Title") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/splash'; ?>" class="ibtn block"><span><?php echo T_("Splash") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/intro'; ?>" class="ibtn block"><span><?php echo T_("Intro") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/review'; ?>" class="ibtn block"><span><?php echo T_("Review") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/download'; ?>" class="ibtn block"><span><?php echo T_("Download") ?></span></a></div>
-				<div class="c pLa5"><a href="<?php echo \dash\url::this(). '/apk'; ?>" class="ibtn block"><span><?php echo T_("apk") ?></span></a></div>
-
-			</div>
-		</div>
-	</div>
-
-
-  <div class="dcard x3 pA0">
-   <div id="chartdiv" class="chart" ></div>
+<div class="f">
+  <div class="c8 x9 s12">
+    <div class="dcard x3 pA0">
+     <div id="chartdiv" class="chart" ></div>
+    </div>
   </div>
+
+  <div class="c4 x3 s12">
+    <a class="mobileFrame" data-apk href="<?php echo \dash\url::this();?>/apk">
+      <div class="screen">
+          <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/gif/jibres-app-queue.gif" alt='<?php echo \dash\data::page_title(); ?>'>
+      </div>
+    </a>
+  </div>
+</div>
+
+
--- content_a/android/setupGuide.php
+++ content_a/android/setupGuide.php
@@ -1,8 +1,6 @@
-<?php
-if(!\dash\data::appQueue())
-{
-?>
+<?php if(!\dash\data::appQueue()) { ?>
 <div class="msg info2 fs16"><?php echo T_("Please set general detail about your app, then let us create your android application."); ?></div>
+<?php } //endif ?>
 
 <div class="setupGuide">
  <header><?php echo T_("Setup Progress"); ?></header>
@@ -13,10 +11,12 @@ if(!\dash\data::appQueue())
    <div class="c"><a class="item <?php if(\dash\data::setupGuide_splash()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/splash"><?php echo T_('App Splash');?></a></div>
    <div class="c"><a class="item <?php if(\dash\data::setupGuide_intro()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/intro"><?php echo T_('App Intro');?></a></div>
    <div class="c"><a class="item <?php if(\dash\data::setupGuide_apk()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/review"><?php echo T_('Review Your App');?></a></div>
+   <div class="c"><a class="item <?php if(\dash\data::setupGuide_download()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/download"><?php echo T_('App Download Page');?></a></div>
   </div>
  </section>
 </div>
 
+<?php if(!\dash\data::appQueue()) { ?>
 <div class="welcome">
   <p><?php echo T_("Easily Create your store application"); ?></p>
   <h2><?php echo T_("Create a custom app for your store"); ?></h2>
@@ -25,7 +25,4 @@ if(!\dash\data::appQueue())
     <a class="btn xl master" href="<?php echo \dash\url::this(); ?>/logo?setup=wizard"><?php echo T_("Build it now"); ?></a>
   </div>
 </div>
-<?php
-}
-//endif
-?>
+<?php } //endif ?>
