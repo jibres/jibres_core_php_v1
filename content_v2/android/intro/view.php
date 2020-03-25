66150d44c184760aeefb5c2c1cd92b627c7a5862
Reza <rm.biqarar@gmail.com>
2020-03-25 12:09:11 +0430

    fix android api

--- content_v2/android/intro/view.php
+++ content_v2/android/intro/view.php
@@ -8,28 +8,13 @@ class view
 	{
 		$page = \lib\app\application\intro::get();
 
-		$theme = isset($page['theme']) ? $page['theme'] : null;
-		$page = array_values($page);
+		unset($page['theme']);
+		unset($page['usersaved']);
 
+		$page = array_values($page);
 
 		$from = '#c80a5a';
 		$to   = '#c80a5a';
-
-		switch ($theme)
-		{
-			case 'theme1':
-			case 'theme2':
-			case 'theme3':
-			case 'theme4':
-			case 'theme5':
-				$from = '#6'. rand(10, 99).'a0a';
-				$to   = '#6'. rand(10, 99).'a0a';
-				break;
-
-			default:
-				// nothing
-				break;
-		}
 
 		$result =
 		[
@@ -57,28 +42,6 @@ class view
 
 		\dash\notif::api($result);
 
-		// $result =
-		// [
-		// 	'logo'  => \dash\url::cdn(). '/logo/icon-white/png/Jibres-Logo-icon-white-1024.png',
-		// 	'theme' => 'Jibres',
-		// 	'title' => T_('Jibres'),
-		// 	'desc'  => T_('Sell and Enjoy'),
-		// 	'meta'  => 'Powered by Ermile',
-
-		// 	'bg' =>
-		// 	[
-		// 		'from' => '#4173cc',
-		// 		'to'   => '#1da1f3',
-		// 	],
-
-		// 	'color' =>
-		// 	[
-		// 		'primary'   => '#fff',
-		// 		'secondary' => '#eee',
-		// 	],
-		// ];
-
-		// \dash\notif::api($result);
 	}
 }
 ?>
--- content_v2/android/splash/view.php
+++ content_v2/android/splash/view.php
@@ -7,17 +7,24 @@ class view
 	public static function config()
 	{
 
-		$detail = \lib\app\application\detail::get_android();
-		$logo   = isset($detail['logo']) ? $detail['logo'] : \dash\url::cdn(). '/logo/icon-white/png/Jibres-Logo-icon-white-1024.png';
-		$theme  = isset($detail['splash_theme']) ? $detail['splash_theme'] : 'store1';
-		$title  = isset($detail['title']) ? $detail['title'] : T_('Jibres');
-		$desc   = isset($detail['desc']) ? $detail['desc'] : T_('Sell and Enjoy');
-		$slogan = isset($detail['slogan']) ? $detail['slogan'] : T_('Powered by Jibres');
+		$detail             = \lib\app\application\detail::get_android();
+
+		$logo               = isset($detail['logo']) ? $detail['logo'] : \dash\url::cdn(). '/logo/icon-white/png/Jibres-Logo-icon-white-1024.png';
+		$theme              = isset($detail['splash_theme']) ? $detail['splash_theme'] : 'store1';
+
+		$start_splash_color = (isset($detail['splash_theme']['start'])) ? $detail['splash_theme']['start'] : '#121317';
+		$end_splash_color   = (isset($detail['splash_theme']['end'])) ? $detail['splash_theme']['end'] : '#323B42';
+		$text_splash_color  = (isset($detail['splash_theme']['text_color'])) ? $detail['splash_theme']['text_color'] : '#ffffff';
+		$meta_splash_color  = (isset($detail['splash_theme']['meta_color'])) ? $detail['splash_theme']['meta_color'] : '#eeeeee';
+
+		$title              = isset($detail['title']) ? $detail['title'] : T_('Jibres');
+		$desc               = isset($detail['desc']) ? $detail['desc'] : T_('Sell and Enjoy');
+		$slogan             = isset($detail['slogan']) ? $detail['slogan'] : T_('Powered by Jibres');
 
 		$result =
 		[
 			'logo'  => $logo,
-			'theme' => $theme,
+			'theme' => 'store1',
 			'title' => $title,
 			'desc'  => $slogan,
 			// 'meta'  => $desc,
@@ -25,14 +32,14 @@ class view
 
 			'bg' =>
 			[
-				'from' => '#36D1DC',
-				'to'   => '#5B86E5',
+				'from' => $start_splash_color,
+				'to'   => $end_splash_color,
 			],
 
 			'color' =>
 			[
-				'primary'   => '#ffffff',
-				'secondary' => '#eeeeee',
+				'primary'   => $text_splash_color,
+				'secondary' => $meta_splash_color,
 			],
 		];
 
