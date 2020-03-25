c0fff94a789e36d1b055356d159725b8296c2a13
Reza <rm.biqarar@gmail.com>
2020-03-25 11:21:08 +0430

    fix chart dashbaord

--- content_a/android/home/script.js
+++ content_a/android/home/script.js
@@ -9,16 +9,14 @@ $dashboardData = \dash\data::dashboardData();
 ?>
 
 
+
 function highChart()
 {
 
   Highcharts.chart('chartdiv',
   {
     chart: {
-      zoomType: 'x',
-      style: {
-        fontFamily: 'IRANSans, Tahoma, sans-serif'
-      }
+      zoomType: 'x'
     },
     title: {
       text: '<?php echo T_("Total Download application per day"); ?>'
@@ -27,95 +25,29 @@ function highChart()
       categories: <?php echo \dash\get::index($dashboardData, 'categories'); ?>,
       crosshair: true
     }],
-    yAxis: [{ // Primary yAxis
-      labels: {
-        format: '{value}',
-        style: {
-          color: Highcharts.getOptions().colors[0]
-        }
-      },
-      title: {
-        text: '<?php echo T_("Count"); ?>',
-        useHTML: Highcharts.hasBidiBug,
-        style: {
-          color: Highcharts.getOptions().colors[0]
-        }
-      }
-    },
-    { // Secondary yAxis
-      title: {
-        text: '<?php echo T_("Count"); ?>',
-        useHTML: Highcharts.hasBidiBug,
-        style: {
-          color: Highcharts.getOptions().colors[1]
-        }
-      },
-      labels: {
-        format: '{value}',
-        style: {
-          color: Highcharts.getOptions().colors[1]
-        }
-      },
-      opposite: true
-    }],
-    tooltip: {
-      useHTML: true,
-      borderWidth: 0,
-      shared: true
-    },
-    exporting:
+    yAxis:
     {
-      buttons:
-      {
-        contextButton:
-        {
-          menuItems:
-          [
-           'printChart',
-           'separator',
-           'downloadPNG',
-           'downloadJPEG',
-           'downloadSVG'
-          ]
-        }
-      }
+      title: false
     },
     legend: {
       layout: 'vertical',
       align: 'left',
       x: 120,
       verticalAlign: 'top',
-      y: 100,
+      y: 50,
       floating: true,
       backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
     },
-    credits:
-    {
-        text: '<?php echo T_('Jibres'); ?>',
-        href: '<?php echo 'https://jibres.com'; ?>',
-        position:
-        {
-            x: -35,
-            y: -7
-        },
-        style: {
-            fontWeight: 'bold'
+    series:
+    [
+      {
+        name: '<?php echo T_("Count"); ?>',
+        data: <?php echo \dash\get::index($dashboardData, 'count'); ?>,
+        tooltip: {
+          valueSuffix: ' <?php echo T_("Count"); ?>'
         }
-    },
-    series: [
-    {
-      name: '<?php echo T_("Count"); ?>',
-      type: 'spline',
-      yAxis: 1,
-      data: <?php echo \dash\get::index($dashboardData, 'count'); ?>,
-      tooltip: {
-        valueSuffix: ' <?php echo T_("Count"); ?>'
       }
-    }
     ]
-  }, function(_chart)
-  {
-    _chart.renderer.image('<?php echo \dash\url::icon(); ?>', 10, 5, 30, 30).attr({class: 'chartServiceLogo'}).add();
   });
 }
 
