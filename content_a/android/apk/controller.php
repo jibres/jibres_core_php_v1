b727e3b318487f8db98b860332426cf33d4390f2
Reza <rm.biqarar@gmail.com>
2020-03-25 12:54:46 +0430

    fix load apk module

--- content_a/android/apk/controller.php
+++ content_a/android/apk/controller.php
@@ -6,11 +6,6 @@ class controller
 {
 	public static function routing()
 	{
-		$isReadyToCreate = \lib\app\application\detail::is_ready_to_create();
-		if(!$isReadyToCreate['ok'])
-		{
-			\dash\redirect::to(\dash\url::this(). '/review');
-		}
 
 	}
 }
