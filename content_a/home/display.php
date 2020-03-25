2ac8ee5e4ebdf95f40f008d1630331952f8a1def
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-25 14:45:59 +0430

    change store dashboard

--- content_a/home/display.php
+++ content_a/home/display.php
@@ -1,87 +1,98 @@
-<?php
-$dashboardData = \dash\data::dashboardData();
+<?php $dashboardData = \dash\data::dashboardData(); ?>
 
-?>
+
 <div class="f">
- <?php if(\dash\permission::check('factorSaleAdd')) {?>
-  <div class="c6 s6">
-    <a class="dcard x2" href='<?php echo \dash\url::here(); ?>/sale'>
-     <div class="statistic teal">
-      <div class="value"><i class="sf-basket"></i></div>
-      <div class="label"><?php echo T_("Sale Invoicing"); ?></div>
-     </div>
-    </a>
-  </div>
+  <div class="c8 x9 s12 pRa10">
+<?php if(\dash\permission::check('staffAccess')) {?>
+   <div id="chartdiv" class="box chart x400"></div>
 <?php } //endif ?>
-
-
- <?php if(\dash\permission::check('factorBuyAdd')) {?>
-  <div class="c6 s6">
-    <a class="dcard x2 disabled">
-     <div class="statistic gray">
-      <div class="value"><i class="sf-basket"></i></div>
-      <div class="label"><?php echo T_("Buy Invocing"); ?></div>
-     </div>
-    </a>
   </div>
+  <div class="c4 x3 s12">
+   <nav class="items">
+     <ul>
+<?php if(\dash\permission::check('productList')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/products">
+          <div class="key"><?php echo T_('Products');?></div>
+          <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'product_count')); ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
 <?php } //endif ?>
-
+<?php if(\dash\permission::check('productAdd')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/products/add">
+          <div class="key"><?php echo T_('Add new Product');?></div>
+          <div class="go plus"></div>
+        </a>
+      </li>
+<?php } //endif ?>
+     </ul>
+   </nav>
+   <nav class="items">
+     <ul>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/factor">
+          <div class="key"><?php echo T_('Orders');?></div>
+          <div class="go"></div>
+        </a>
+       </li>
+<?php if(\dash\permission::check('factorSaleAdd')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/sale">
+          <div class="key"><?php echo T_('Sale Invoicing');?></div>
+          <div class="go plus"></div>
+        </a>
+       </li>
+<?php } //endif ?>
+<?php if(\dash\permission::check('factorBuyAdd')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/buy">
+          <div class="key"><?php echo T_('Buy Invocing');?></div>
+          <div class="go plus"></div>
+        </a>
+       </li>
+<?php } //endif ?>
+     </ul>
+   </nav>
+   <nav class="items">
+     <ul>
+<?php if(\dash\permission::check('customerAccess')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::kingdom();?>/crm">
+          <div class="key"><?php echo T_('Customer');?></div>
+          <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'customer_count')); ?></div>
+          <div class="go"></div>
+        </a>
+       </li>
+<?php } //endif ?>
+<?php if(\dash\permission::check('staffAccess')) {?>
+       <li>
+        <a class="f" href="<?php echo \dash\url::kingdom();?>/crm">
+          <div class="key"><?php echo T_('Staff');?></div>
+          <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'staff_count')); ?></div>
+          <div class="go"></div>
+        </a>
+       </li>
+<?php } //endif ?>
+     </ul>
+   </nav>
+   <nav class="items">
+     <ul>
+       <li>
+        <a class="f" href="<?php echo \dash\url::here();?>/setting">
+          <div class="key"><?php echo T_('Setting');?></div>
+          <div class="go"></div>
+        </a>
+       </li>
+     </ul>
+   </nav>
+  </div>
 
 </div>
 
-<div class="f">
- <?php if(\dash\permission::check('productList')) {?>
-  <div class="c6 s6">
-    <a class="dcard x1" href='<?php echo \dash\url::here(); ?>/products'>
-     <div class="statistic">
-      <div class="value"><span><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'product_count')); ?></span></div>
-      <div class="label"><?php echo T_("Product"); ?></div>
-     </div>
-    </a>
-  </div>
-<?php } //endif ?>
 
-
- <?php if(\dash\permission::check('productAdd')) {?>
-  <div class="c6 s6">
-    <a class="dcard x1" href='<?php echo \dash\url::here(); ?>/products/add'>
-     <div class="statistic green">
-      <div class="value"><i class="sf-plus"></i></div>
-      <div class="label"><?php echo T_("Add new Product"); ?></div>
-     </div>
-    </a>
-  </div>
-<?php } //endif ?>
-
-
-</div>
-
-<div class="f">
- <?php if(\dash\permission::check('customerAccess')) {?>
-  <div class="c4 s6">
-    <a class="dcard" href='<?php echo \dash\url::kingdom(); ?>/crm'>
-     <div class="statistic gray">
-      <div class="value"><i class="sf-user"></i><span><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'customer_count')); ?></span></div>
-      <div class="label"><?php echo T_("Customer"); ?></div>
-     </div>
-    </a>
-  </div>
-<?php } //endif ?>
-
-
- <?php if(\dash\permission::check('staffAccess')) {?>
-  <div class="c4 s6">
-    <a class="dcard" href='<?php echo \dash\url::kingdom(); ?>/crm'>
-     <div class="statistic orange">
-      <div class="value"><i class="sf-user-4"></i><span><?php echo \dash\fit::number(\dash\get::index($dashboardData, 'staff_count')); ?></span></div>
-      <div class="label"><?php echo T_("Staff"); ?></div>
-     </div>
-    </a>
-  </div>
-<?php } //endif ?>
-
-
-  <div class="c4 s">
+  <div class="c4 s hide">
    <div class="dcard">
      <div class="block">
       <div class="f">
@@ -97,21 +108,4 @@ $dashboardData = \dash\data::dashboardData();
     </div>
   </div>
 </div>
-
-
-<?php if(\dash\permission::check('staffAccess')) {?>
-  <div class="dcard x3 pA0">
-   <div id="chartdiv" class="chart" ></div>
-  </div>
-
-<?php } //endif ?>
-
-
-
-
-
-
-
-
-
 
