85bfaa7d9ae20c0afbf80c288d72a11b8f0228e0
Reza <rm.biqarar@gmail.com>
2020-03-25 18:21:00 +0430

    set upgrade query in install new store query

--- includes/database/customer/v.1.0.6_factordetails.sql
+++ includes/database/customer/v.1.0.6_factordetails.sql
@@ -7,7 +7,9 @@ CREATE TABLE `jibres_XXXXXXX`.`factordetails` (
 `finalprice` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
 `count` int(10) UNSIGNED DEFAULT NULL,
 `sum` bigint(20) UNSIGNED DEFAULT NULL,
+`productprice_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL,
 PRIMARY KEY (`factor_id`,`product_id`),
 CONSTRAINT `factordetails_factor_id` FOREIGN KEY (`factor_id`) REFERENCES `factors` (`id`) ON UPDATE CASCADE,
-CONSTRAINT `factordetails_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE
+CONSTRAINT `factordetails_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `factordetails_productprice_id` FOREIGN KEY (`productprice_id`) REFERENCES `productprices` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
