0501e45416fdbbed77cb75c0e1a6b694453cb294
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-25 18:07:23 +0430

    bugfix show footer

--- content/home/layout/main.php
+++ content/home/layout/main.php
@@ -1,6 +1,6 @@
 <?php
 require_once \dash\layout\func::display();
-if(\dash\detect\device::detectPWA())
+if(!\dash\url::subdomain() && \dash\url::content() === null && \dash\detect\device::detectPWA())
 {
 	require_once root.'content/home/layout/pwa-footer.php';
 }
