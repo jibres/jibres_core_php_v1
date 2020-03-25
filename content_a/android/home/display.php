178e6a3ec218d9457177c5d98486b1687ef30b72
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-25 14:28:01 +0430

    clean html

--- content_a/android/home/display.php
+++ content_a/android/home/display.php
@@ -29,13 +29,41 @@
   <div class="c4 x3 s12">
    <nav class="items">
      <ul>
-       <li><a class="f <?php if(\dash\data::setupGuide_apk()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/apk"><div class="key"><?php echo T_('Download your App');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_download()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/download"><div class="key"><?php echo T_('App Download Page');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_logo()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/logo"><div class="key"><?php echo T_('App Logo');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_title()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/title"><div class="key"><?php echo T_('App Title');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_splash()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/splash"><div class="key"><?php echo T_('App Splash');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_intro()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/intro"><div class="key"><?php echo T_('App Intro');?></div><div class="go"></div></a></li>
-       <li><a class="f <?php if(\dash\data::setupGuide_review()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/review"><div class="key"><?php echo T_('Review Your App');?></div><div class="go"></div></a></li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_apk()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/apk">
+          <div class="key"><?php echo T_('Download your App');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_download()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/download">
+          <div class="key"><?php echo T_('App Download Page');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_logo()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/logo">
+          <div class="key"><?php echo T_('App Logo');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_title()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/title">
+          <div class="key"><?php echo T_('App Title');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_splash()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/splash">
+          <div class="key"><?php echo T_('App Splash');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_intro()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/intro">
+          <div class="key"><?php echo T_('App Intro');?></div>
+          <div class="go"></div></a>
+       </li>
+       <li>
+        <a class="f <?php if(\dash\data::setupGuide_review()) { echo 'complete'; } ?>" href="<?php echo \dash\url::this();?>/review">
+          <div class="key"><?php echo T_('Review Your App');?></div>
+          <div class="go"></div></a>
+       </li>
      </ul>
    </nav>
   </div>
