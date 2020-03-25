1dad071b89e84eab6519135812d9d459a01e0e66
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-25 15:55:22 +0430

    show log

--- content_su/apilog/view/display.php
+++ content_su/apilog/view/display.php
@@ -1,4 +1,31 @@
 
-<div class="cbox">
-  <pre><?php print_r(\dash\data::dataRow()); ?></pre>
+<div class="box">
+	<table class="tbl1 v3">
+
+<?php
+foreach (\dash\data::dataRow() as $key => $value)
+{
+	echo "<tr>";
+	echo "<th>". $key. "</th>";
+	if(substr($value,0 , 1) === '[')
+	{
+		$myVal = json_decode($value, true);
+		echo "<td>";
+		foreach ($myVal as $ikey => $ivalue)
+		{
+			echo "<div class=''>";
+			echo json_encode($ivalue);
+			echo "</div>";
+			# code...
+		}
+		echo "</td>";
+	}
+	else
+	{
+		echo "<td>". $value. "</td>";
+	}
+	echo "</tr>";
+}
+ ?>
+	</table>
 </div>
