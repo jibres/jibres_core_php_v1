6c134477bf22c3eb8c04e60fc442002a2c8301a6
Reza <rm.biqarar@gmail.com>
2020-03-25 12:38:30 +0430

    round number

--- lib/price.php
+++ lib/price.php
@@ -8,12 +8,12 @@ class price
 
 	public static function up($_number)
 	{
-		return intval(floatval($_number) * self::$rate);
+		return round(floatval($_number) * self::$rate);
 	}
 
 	public static function down($_number)
 	{
-		return intval($_number) / self::$rate;
+		return floatval($_number) / self::$rate;
 	}
 }
 ?>
