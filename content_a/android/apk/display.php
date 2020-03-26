91e249a22456daafb7716da6c321e76cae8f1ca4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-03-26 21:23:39 +0430

    fix link

--- content_a/android/apk/display.php
+++ content_a/android/apk/display.php
@@ -39,7 +39,7 @@ elseif(\dash\data::appQueue_status() === 'done' || \dash\data::appQueue_status()
         <div class="body">
           <div class="msg success2 f">
             <div class="c"><?php echo T_("Your application is ready to use"); ?></div>
-            <div class="cauto os"><a class="link" href="<?php echo \dash\data::downoadAPK();?>"><?php echo T_("Download");?></a></div>
+            <div class="cauto os"><a target="_blank" class="link" href="<?php echo \dash\data::downoadAPK();?>"><?php echo T_("Download");?></a></div>
           </div>
 
           <?php if(\dash\data::downoadAPK()) {?>
