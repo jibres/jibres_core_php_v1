f046136fca542bd9ae9df421610f8541319dd9a4
Reza <rm.biqarar@gmail.com>
2020-03-29 19:14:46 +0430

    save website header logo

--- dash/upload/store_logo.php
+++ dash/upload/store_logo.php
@@ -149,5 +149,51 @@ class store_logo
 
 		return $file_detail['path'];
 	}
+
+
+	public static function website_logo()
+	{
+		$meta =
+		[
+			'allow_size' => \dash\upload\size::MB(1),
+			'ext' =>
+			[
+				'png', 'jpg'
+			],
+			'notif_msg_ext'    => T_("You must upload only png or jpg file format for website logo"),
+		];
+
+		$file_detail = \dash\upload\file::upload('logo', $meta);
+
+		if(!$file_detail)
+		{
+			return false;
+		}
+
+		$fileusage =
+		[
+			'file_id'     => $file_detail['id'],
+			'user_id'     => \dash\user::id(),
+			'title'       => null,
+			'alt'         => null,
+			'desc'        => null,
+			'related'     => 'file_site_header_logo',
+			'related_id'  => \lib\store::id(),
+			'datecreated' => date("Y-m-d H:i:s"),
+		];
+
+		$check_duplicate_usage = \dash\db\fileusage::duplicate('file_site_header_logo', \lib\store::id());
+
+		if(isset($check_duplicate_usage['id']))
+		{
+			\dash\db\fileusage::update_file_id($check_duplicate_usage['id'], $file_detail['id']);
+		}
+		else
+		{
+			\dash\db\fileusage::insert($fileusage);
+		}
+
+		return $file_detail['path'];
+	}
 }
 ?>
--- lib/app/website_header/set.php
+++ lib/app/website_header/set.php
@@ -69,6 +69,60 @@ class set
 			}
 		}
 
+
+		$logo = \dash\upload\store_logo::website_logo();
+
+		if($logo)
+		{
+			$get_logo = \lib\db\setting\get::platform_cat_key('website', 'header_logo', 'file');
+
+			$have_change = false;
+
+			$logo_setting_id = null;
+			if(isset($get_logo['id']))
+			{
+				$logo_setting_id = $get_logo['id'];
+
+				if(isset($get_logo['value']) && $get_logo['value'] === $logo)
+				{
+					\dash\notif::info(T_("You website logo saved without change"));
+				}
+				else
+				{
+					$have_change = true;
+					\lib\db\setting\update::value($logo, $get_logo['id']);
+				}
+			}
+			else
+			{
+				$insert =
+				[
+					'platform' => 'website',
+					'cat'      => 'header_logo',
+					'key'      => 'file',
+					'value'    => $logo,
+				];
+
+				$have_change = true;
+				$logo_setting_id = \lib\db\setting\insert::new_record($insert);
+			}
+
+
+			if($have_change)
+			{
+				// \dash\notif::ok(T_("Your website logo was saved"));
+			}
+
+			if($logo_setting_id)
+			{
+				$current_detail['website_header_logo_setting_id'] = $logo_setting_id;
+			}
+
+		}
+
+
+
+
 		$current_detail = json_encode($current_detail, JSON_UNESCAPED_UNICODE);
 		\lib\db\setting\update::overwirte_platform_cat_key($current_detail, 'website', $save_header['key'], 'customized');
 
