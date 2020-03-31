8b33d754872691ffc9d7a0d7f571120934b4a177
Reza <rm.biqarar@gmail.com>
2020-03-31 16:01:08 +0430

    generate menu

--- content_subdomain/home/layout/footer/footer_2.php
+++ content_subdomain/home/layout/footer/footer_2.php
@@ -1,19 +1,7 @@
 <footer>
   <div class="copyright"><a href="<?php echo \dash\url::kingdom(); ?>"><?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?></a></div>
-<?php if(isset($website['footer_customized']['footer_menu_1']) && isset($website['menu'][$website['footer_customized']['footer_menu_1']]['list'])) {?>
-  <nav>
-    <?php foreach ($website['menu'][$website['footer_customized']['footer_menu_1']]['list'] as $menuValue) {?>
-      <a <?php if(\dash\get::index($menuValue, 'target')) {echo 'target="_blank" data-direct ';} ?> href="<?php echo \dash\get::index($menuValue, 'url'); ?>"><?php echo \dash\get::index($menuValue, 'title'); ?></a>
-    <?php }// endfor ?>
-  </nav>
-<?php }// endif ?>
+  <?php \lib\app\website_menu\generate::menu('footer_menu_1'); ?>
+  <?php \lib\app\website_menu\generate::menu('footer_menu_2'); ?>
 
-<?php if(isset($website['footer_customized']['footer_menu_2']) && isset($website['menu'][$website['footer_customized']['footer_menu_2']]['list'])) {?>
-  <nav>
-    <?php foreach ($website['menu'][$website['footer_customized']['footer_menu_2']]['list'] as $menuValue) {?>
-      <a <?php if(\dash\get::index($menuValue, 'target')) {echo 'target="_blank" data-direct ';} ?> href="<?php echo \dash\get::index($menuValue, 'url'); ?>"><?php echo \dash\get::index($menuValue, 'title'); ?></a>
-    <?php }// endfor ?>
-  </nav>
-<?php }// endif ?>
 
 </footer>
---
+++ lib/app/website_menu/generate.php
@@ -0,0 +1,32 @@
+<?php
+namespace lib\app\website_menu;
+
+class generate
+{
+
+	public static function menu($_key)
+	{
+		$customized_key = '';
+		if(in_array(substr($_key, 0, 6), ['header', 'footer']))
+		{
+			$customized_key = substr($_key, 0, 6) . '_customized';
+		}
+
+		$website = \dash\data::website();
+		if(isset($website[$customized_key][$_key]) && isset($website['menu'][$website[$customized_key][$_key]]['list']) && is_array($website['menu'][$website[$customized_key][$_key]]['list']))
+		{
+  			echo '<nav>';
+     		foreach ($website['menu'][$website[$customized_key][$_key]]['list'] as $menuValue)
+     		{
+      			echo '<a ';
+      			if(\dash\get::index($menuValue, 'target'))
+      			{
+      				echo 'target="_blank" data-direct ';
+      			}
+      			echo ' href="'. \dash\get::index($menuValue, 'url'). '">'. \dash\get::index($menuValue, 'title'). '</a>';
+     		}
+  			echo '</nav>';
+		}
+	}
+}
+?>
