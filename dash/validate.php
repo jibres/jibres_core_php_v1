21a108b68d341a22030e6be077d442bb290804e3
Reza <rm.biqarar@gmail.com>
2020-04-04 20:00:33 +0430

    add is_equal function to validate

--- dash/validate.php
+++ dash/validate.php
@@ -10,5 +10,34 @@ class validate
 	{
 		return \dash\cleanse::data($_function, ...$_args);
 	}
+
+
+	/**
+	 * check is Equal 2 string
+	 *
+	 * @param      string   $_a     { parameter_description }
+	 * @param      string   $_b     { parameter_description }
+	 *
+	 * @return     boolean  True if equal, False otherwise.
+	 */
+	public static function is_equal($_a, $_b)
+	{
+		if($_a === $_b)
+		{
+			return true;
+		}
+
+		if((string) $_a == (string) $_b)
+		{
+			return true;
+		}
+
+		if(($_a == '' || is_null($_a) || $_a == null) && ($_b == '' || is_null($_b) || $_b == null))
+		{
+			return true;
+		}
+
+		return false;
+	}
 }
 ?>
