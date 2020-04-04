73272f5b00e3d80199d6641fe178a2398a98ae70
Reza <rm.biqarar@gmail.com>
2020-04-04 14:40:11 +0430

    remove dash option

--- includes/option/option.php
+++
@@ -1,5 +0,0 @@
-<?php
-self::$config['site']['title']  = "Jibres";
-self::$config['site']['desc']   = "Jibres is not just an online accounting software; We try to create the best financial platform that has everything you need to sale and manage your financial life.";
-self::$config['site']['slogan'] = "Integrated Sales and Online Accounting";
-?>
