40a113cad777772567564c3dc7daec39be317cbb
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-05 18:19:47 +0430

    update price

--- lib/app/nic_domain/price.php
+++ lib/app/nic_domain/price.php
@@ -9,11 +9,11 @@ class price
 	{
 		if($_period === '5year')
 		{
-			return 13000;
+			return 15000;
 		}
 		else
 		{
-			return 4000;
+			return 5000;
 		}
 	}
 
@@ -22,18 +22,18 @@ class price
 	{
 		if($_period === '5year')
 		{
-			return 13000;
+			return 15000;
 		}
 		else
 		{
-			return 4000;
+			return 5000;
 		}
 	}
 
 
 	public static function transfer()
 	{
-		return 4000;
+		return 5000;
 	}
 
 
@@ -41,11 +41,11 @@ class price
 	{
 		if($_period === '5year')
 		{
-			return 15000 + 2000;
+			return 30000;
 		}
 		else
 		{
-			return 5000 + 2000;
+			return 6000;
 		}
 	}
 
