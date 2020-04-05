f611e788f36a09d316996df2ba649e515c9cb34b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-05 10:37:19 +0430

    add billboard module

---
+++ content/billboard/display.php
@@ -0,0 +1,7 @@
+<div class="jibresBanner">
+
+ <div class="fit zero">
+  <iframe class="block" src="https://status.jibres.com" height="1800"></iframe>
+ </div>
+
+</div>
---
+++ content/billboard/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content\billboard;
+
+
+class view
+{
+	public static function config()
+	{
+		require_once core.'layout/html/iframeMode.html';
+		\dash\code::boom();
+	}
+}
+?>
