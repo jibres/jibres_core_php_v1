b09b359723321c3f8747d4dda984dc5dd3e81371
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-06 19:44:19 +0430

    fix api page

--- content_r10/irnic/doc/010-contact/010-app-detail.php
+++ content_r10/irnic/doc/010-contact/010-app-detail.php
@@ -1,22 +1,25 @@
-<h2 class="f" data-kerkere='#contact-detail' data-kerkere-icon='open'>
-  <span class="cauto pRa10"><span class="badge success">GET</span></span>
-  <span class="c"><?php echo T_("Get IRNIC contact list"); ?></span>
-</h2>
-<div id="contact-detail">
-  <div class="cbox" id='contact'>
+<div class="box">
+  <header>
+    <h2 class="f" data-kerkere='#contact-detail' data-kerkere-icon='open'>
+      <span class="cauto pRa10"><span class="badge success">GET</span></span>
+      <span class="c"><?php echo T_("Get IRNIC contact list"); ?></span>
+    </h2>
+  </header>
+  <div class="body" id="contact-detail">
+      <div id='contact'>
 
-    <div class="msg url ltr txtL">
-      <i class="method">GET</i>
-      <span><?php echo \dash\data::IRNICApiURL(); ?><b>contact_list</b></span>
-    </div>
+        <div class="msg url ltr txtL">
+          <i class="method">GET</i>
+          <span><?php echo \dash\data::IRNICApiURL(); ?><b>contact_list</b></span>
+        </div>
 
-    <?php require(root. 'content_r10/irnic/doc/000-public/header-appkey-apikey.php'); ?>
+        <?php require(root. 'content_r10/irnic/doc/000-public/header-appkey-apikey.php'); ?>
 
-    <h3><?php echo T_("cURL"); ?> <small><?php echo T_("example"); ?></small></h3>
-    <pre>curl -X GET <?php echo \dash\data::IRNICApiURL(); ?>contact_list -H 'appkey: <?php echo \dash\data::myAppKey(); ?>'</pre>
+        <h3><?php echo T_("cURL"); ?> <small><?php echo T_("example"); ?></small></h3>
+        <pre>curl -X GET <?php echo \dash\data::IRNICApiURL(); ?>contact_list -H 'appkey: <?php echo \dash\data::myAppKey(); ?>'</pre>
 
-    <h3><?php echo T_("Response"); ?> <small><?php echo T_("example"); ?></small></h3>
-<pre>
+        <h3><?php echo T_("Response"); ?> <small><?php echo T_("example"); ?></small></h3>
+<samp>
 {
   "ok": true,
   "result": {
@@ -42,8 +45,7 @@
       }
     }
   }
-}
-</pre>
-
+}</samp>
   </div>
 </div>
+
