3761d2b14d89692c905d7a0876d72d3713193306
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-06 20:03:22 +0430

    add header

---
+++ content_subdomain/home/layout/header.php
@@ -0,0 +1,11 @@
+<?php
+$website = \dash\data::website();
+if(isset($website['header']['active']))
+{
+  $addr = root. 'content_subdomain/home/layout/header/'. $website['header']['active']. '.php';
+  if(is_file($addr))
+  {
+    require_once($addr);
+  }
+}
+?>
