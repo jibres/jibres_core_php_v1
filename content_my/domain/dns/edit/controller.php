514ab0c5f8eebf0095f36d5045bcfef5e825c475
Reza <rm.biqarar@gmail.com>
2020-04-11 17:03:09 +0430

    fix load dns in api

--- content_my/domain/dns/edit/controller.php
+++ content_my/domain/dns/edit/controller.php
@@ -12,13 +12,13 @@ class controller
 		{
 			$get_api    = new \lib\nic\api();
 			$detail     = $get_api->dns_load(\dash\request::get('id'));
+
 		}
 		else
 		{
 			$detail = \lib\app\nic_contact\get::load();
 		}
 
-		$detail = \lib\app\nic_dns\get::load();
 		if(!$detail)
 		{
 			\dash\header::status(403, T_("Invalid id"));
