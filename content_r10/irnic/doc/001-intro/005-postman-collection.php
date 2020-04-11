03cbf250c792bff8af8fa356041fd8a2a3d1f284
Reza <rm.biqarar@gmail.com>
2020-04-11 09:49:00 +0430

    add postman link and php sdk

--- content_r10/irnic/doc/001-intro/005-postman-collection.php
+++ content_r10/irnic/doc/001-intro/005-postman-collection.php
@@ -1,12 +1,21 @@
 <div class="box">
   <header>
     <h2 class="f" data-kerkere='#postman-collection' data-kerkere-icon='open'>
-     	<?php echo T_("Postman Collection JSON"); ?>
+     	<?php echo T_("Postman Collection And PHP-SDK"); ?>
     </h2>
   </header>
   <div class="body" id="postman-collection">
-    <p><?php echo T_("If you use from postman for api, can import this collection to get all request in postman"); ?></p>
-    <a class="btn success" data-direct target="_blank" href="<?php echo \dash\url::cdn(). '/api/Jibres_Domain_API_r10.postman_collection.json'; ?>" download><?php echo T_("Download Postman Collection"); ?></a>
+    <p>
+    	<?php echo T_("If you use from postman for api, can import this collection to get all request in postman"); ?>
+    	<br>
+    	<a class="btn success" data-direct target="_blank" href="https://explore.postman.com/team/jibres" ><?php echo T_("Postman Collection"); ?></a>
+    </p>
+
+    <p class="mT20"><?php echo T_("Get PHP-SDK"); ?>
+    	<br>
+    	<a class="btn" data-direct target="_blank" href="https://github.com/jibres/jibres-domain-php-sdk" ><?php echo T_("PHP-SDK"); ?></a>
+    </p>
+
   </div>
 </div>
 
