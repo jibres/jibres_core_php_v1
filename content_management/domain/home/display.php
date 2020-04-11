cc5f46e791ff50aedf5a5a9f34d608fe6444db8d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-11 19:35:54 +0430

    add dashboard

--- content_management/domain/home/display.php
+++ content_management/domain/home/display.php
@@ -1,1 +1,76 @@
-Salam
+<div class="f">
+  <div class="c3 s6 pRa10">
+    <a href="" class="stat">
+      <h3><?php echo T_("Total Domain Buy");?></h3>
+      <div class="val"><?php echo \dash\fit::number(2070);?></div>
+    </a>
+  </div>
+  <div class="c3 s6 pRa10">
+    <a href="" class="stat">
+      <h3><?php echo T_("Total Domain Renew");?></h3>
+      <div class="val"><?php echo \dash\fit::number(306);?></div>
+    </a>
+  </div>
+  <div class="c3 s6 pRa10">
+    <a href="" class="stat">
+      <h3><?php echo T_("Total Domain Transfer");?></h3>
+      <div class="val"><?php echo \dash\fit::number(129);?></div>
+    </a>
+  </div>
+  <div class="c3 s6">
+    <a href="" class="stat">
+      <h3><?php echo T_("Total Whois");?></h3>
+      <div class="val counter"><?php echo \dash\fit::number(49350);?></div>
+    </a>
+  </div>
+</div>
+
+<div id="chartdiv" class="box chart x200"></div>
+
+<div class="f">
+  <div class="c8 pRa10">
+    <div id="chartdiv" class="box chart x200"></div>
+
+  </div>
+  <div class="c4">
+   <nav class="items">
+     <ul>
+       <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/apk">
+            <div class="key"><?php echo T_('Download your App');?></div>
+            <div class="value"><?php echo \dash\fit::number(49350);?></div>
+            <div class="go"></div>
+          </a>
+       </li>
+
+       <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/apk">
+            <div class="key"><?php echo T_('Download your App');?></div>
+            <div class="value"><?php echo \dash\fit::number(49350);?></div>
+            <div class="go"></div>
+          </a>
+       </li>
+
+       <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/apk">
+            <div class="key"><?php echo T_('Download your App');?></div>
+            <div class="value"><?php echo \dash\fit::number(49350);?></div>
+            <div class="go"></div>
+          </a>
+       </li>
+
+       <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/apk">
+            <div class="key"><?php echo T_('Download your App');?></div>
+            <div class="value"><?php echo \dash\fit::number(49350);?></div>
+            <div class="go"></div>
+          </a>
+       </li>
+
+
+
+     </ul>
+   </nav>
+  </div>
+</div>
+
