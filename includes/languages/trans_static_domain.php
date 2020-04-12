e7398656266b6e0311d7f8691c46b524c134f8a8
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-12 16:30:59 +0430

    add static trans

---
+++ includes/languages/trans_static_domain.php
@@ -0,0 +1,20 @@
+<?php
+class TransStaticDomain
+{
+ private function trans_static()
+ {
+	echo T_("Jibres");
+	echo T_("contact_check");
+	echo T_("contact_info");
+	echo T_("domain_check");
+	echo T_("domain_info");
+	echo T_("domain_create");
+	echo T_("domain_update");
+	echo T_("domain_renew");
+	echo T_("domain_transfer");
+	echo T_("poll_request");
+	echo T_("poll_acknowledge");
+
+ }
+}
+?>
