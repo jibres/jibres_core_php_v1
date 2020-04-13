e146c022b6bd2bc53d2443270a1c2f858735c3db
Reza <rm.biqarar@gmail.com>
2020-04-13 19:08:26 +0430

    show new status

--- lib/app/nic_domainaction/search.php
+++ lib/app/nic_domainaction/search.php
@@ -120,17 +120,19 @@ class search
 							$list[$key]['icon']    = '<i class="sf-exchange fc-green"></i>';
 							break;
 
-						case 'unlock':
+
+						case 'domain_unlock':
 							$list[$key]['taction'] = T_("Unlock domain");
 							$list[$key]['class']   = '';
 							$list[$key]['icon']    = '<i class="sf-unlock fc-red"></i>';
 							break;
 
-						case 'lock':
+						case 'domain_lock':
 							$list[$key]['taction'] = T_("Lock domain");
 							$list[$key]['class']   = '';
 							$list[$key]['icon']    = '<i class="sf-lock fc-green"></i>';
 							break;
+
 
 						case 'changedns':
 							$list[$key]['taction'] = T_("Change domain dns");
@@ -142,6 +144,78 @@ class search
 							$list[$key]['taction'] = T_("Update domain holder");
 							$list[$key]['class']   = '';
 							$list[$key]['icon']    = '<i class="sf-cogs fc-blue"></i>';
+							break;
+
+						case 'domain_buy_ready':
+							$list[$key]['taction'] = T_("Ready to buy domain");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-cart-plus fc-green"></i>';
+							break;
+
+						case 'domain_buy_pay_link':
+							$list[$key]['taction'] = T_("Domain register pay link created");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-cart fc-green"></i>';
+							break;
+
+						case 'register_failed':
+							$list[$key]['taction'] = T_("Register domain failed");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-red"></i>';
+							break;
+
+						case 'domain_enable_autorenew':
+							$list[$key]['taction'] = T_("Enable domain auto renew");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-green"></i>';
+							break;
+
+						case 'domain_disable_autorenew':
+							$list[$key]['taction'] = T_("Disable domain auto renew");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-red"></i>';
+							break;
+
+						case 'domain_tranfer_pay_link':
+							$list[$key]['taction'] = T_("Domain transfer pay link created");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-cart fc-green"></i>';
+							break;
+
+						case 'domain_renew_ready':
+							$list[$key]['taction'] = T_("Ready to renew domain");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-green"></i>';
+							break;
+
+						case 'domain_renew_pay_link':
+							$list[$key]['taction'] = T_("Domain renew pay link created");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-cart fc-green"></i>';
+							break;
+
+						case 'renew_faled':
+							$list[$key]['taction'] = T_("Renew domain failed");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-red"></i>';
+							break;
+
+						case 'domain_transfer_ready':
+							$list[$key]['taction'] = T_("Ready transfer domain");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-green"></i>';
+							break;
+
+						case 'domain_tranfer_pay_link':
+							$list[$key]['taction'] = T_("Domain transfer pay link created");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-cart fc-green"></i>';
+							break;
+
+						case 'transfer_faled':
+							$list[$key]['taction'] = T_("Transfer domain failed");
+							$list[$key]['class']   = '';
+							$list[$key]['icon']    = '<i class="sf-info fc-red"></i>';
 							break;
 
 						case 'delete':
