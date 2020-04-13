5fb47a2f2de226c43e1e005439b7b4f8fd91f503
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-13 14:18:52 +0430

    change links to new mode

--- content_my/domain/home/display.php
+++ content_my/domain/home/display.php
@@ -1,3 +1,76 @@
+
+<div class="f">
+  <div class="c9">
+
+  </div>
+  <div class="c3">
+    <nav class="items">
+      <ul>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/buy">
+            <div class="key"><?php echo T_('Buy domain');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/renew">
+            <div class="key"><?php echo T_('Renew domain');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/transfer">
+            <div class="key"><?php echo T_('Transfer domain');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <nav class="items">
+      <ul>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/irnic">
+            <div class="key"><?php echo T_('IRNIC Handle');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/dns">
+            <div class="key"><?php echo T_('DNS');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php if(\dash\data::dataRow()) {?>
 
     <?php if(\dash\data::dataRow_status() === 'enable') {?>
@@ -34,195 +107,4 @@
 <?php } //endif ?>
 
 
-
-<div class="f">
-
-
-  <div class="c s12">
-    <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/buy'>
-     <div class="statistic blue">
-      <div class="value"><i class="sf-cart-plus"></i></div>
-      <div class="label"><?php echo T_("Buy domain"); ?></div>
-     </div>
-    </a>
-  </div>
-
-
-  <div class="c s6">
-    <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/renew'>
-     <div class="statistic blue">
-      <div class="value"><i class="sf-retweet"></i></div>
-      <div class="label"><?php echo T_("Renew domain"); ?></div>
-     </div>
-    </a>
-  </div>
-
-
-  <div class="c s6">
-    <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/transfer'>
-     <div class="statistic green">
-      <div class="value"><i class="sf-exchange"></i></div>
-      <div class="label"><?php echo T_("Transfer domain"); ?></div>
-     </div>
-    </a>
-  </div>
-
-
-   <div class="cauto s6">
-    <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/irnic'>
-     <div class="statistic blue">
-      <div class="value"><i class="sf-vcard"></i></div>
-      <div class="label"><?php echo T_("IRNIC Handle"); ?></div>
-     </div>
-    </a>
-  </div>
-
-    <div class="cauto s6">
-     <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/dns'>
-      <div class="statistic blue">
-       <div class="value"><i class="sf-internet"></i></div>
-       <div class="label"><?php echo T_("DNS"); ?></div>
-      </div>
-     </a>
-    </div>
-
-
-</div>
-
-
-<?php
-if(\dash\data::dataTable())
-{
-    if(\dash\data::isFiltered())
-    {
-        htmlSearchBox();
-        htmlTable();
-        htmlFilter();
-    }
-    else
-    {
-        htmlSearchBox();
-        htmlTable();
-    }
-
-}
-else
-{
-    if(\dash\data::isFiltered())
-    {
-        htmlSearchBox();
-
-        htmlFilter();
-    }
-    else
-    {
-        htmlStartAddNew();
-    }
-}
-?>
-
-
-
-
-
-
-
-<?php function htmlSearchBox() {?>
-<div class="fs12">
-    <form method="get" autocomplete="off" class="mB20" action="<?php echo \dash\url::that(); ?>">
-        <div class="input search">
-            <input type="text" name="q" placeholder='<?php echo T_("Search"); ?>' value="<?php echo \dash\request::get('q'); ?>">
-            <button class="btn addon success"><?php echo T_("Search"); ?></button>
-        </div>
-    </form>
-</div>
-<?php } //endfunction ?>
-
-
-<?php function htmlTable() {?>
-<?php $sortLink = \dash\data::sortLink(); ?>
-
-<div class="fs12">
-    <table class="tbl1 v1 responsive">
-        <thead>
-            <tr class="fs09">
-                <th colspan="2" data-sort="<?php echo \dash\get::index($sortLink, 'name', 'order'); ?>" ><a href="<?php echo \dash\get::index($sortLink, 'name', 'link'); ?>"><?php echo T_("Domain"); ?></a></th>
-                <th class="txtC"><?php echo T_("Status"); ?></th>
-                <th class="txtL" data-sort="<?php echo \dash\get::index($sortLink, 'dateexpire', 'order'); ?>"><a href="<?php echo \dash\get::index($sortLink, 'dateexpire', 'link'); ?>"><?php echo T_("Expire date"); ?></a></th>
-                <th class="txtL" data-sort="<?php echo \dash\get::index($sortLink, 'dateupdate', 'order'); ?>">
-                  <a href="<?php echo \dash\get::index($sortLink, 'dateupdate', 'link'); ?>">
-                    <?php echo T_("Create date"); ?><br><?php echo T_("Date modified"); ?>
-                  </a>
-                </th>
-                <th class="txtL"><?php echo T_("DNS"); ?></th>
-            </tr>
-        </thead>
-        <tbody class="fs12">
-
-            <?php foreach (\dash\data::dataTable() as $key => $value) {?>
-
-            <tr <?php if(\dash\request::get('resultid') && \dash\get::index($value, 'id') == \dash\request::get('resultid')) { echo "class='positive'";} if(\dash\get::index($value, 'status') === 'disable') { echo 'class="negative"'; }?> >
-                <td>
-
-                    <!-- <a target="_blank" href="http://<?php echo \dash\get::index($value, 'name'); ?>"><i class="sf-link"></i></a> -->
-                    <a href="<?php echo \dash\url::that(); ?>/setting?domain=<?php echo \dash\get::index($value, 'name'); ?>" class="link flex"> <i class="sf-edit"></i> <code><?php echo \dash\get::index($value, 'name'); ?></code></a>
-                </td>
-                <td class="collapsing">
-                  <?php if(\dash\get::index($value, 'verify')) {?>
-                    <a href="<?php echo \dash\url::this(). '/setting/transfer?domain='. \dash\get::index($value, 'name'); ?>">
-
-                    <div class="ibtn x30 wide"><?php echo '<span>'.T_("Lock"). '</span>'; if(isset($value['lock']) && $value['lock']) { echo '<i class="sf-lock fc-green"></i>'; } else{ echo '<i class="sf-unlock fc-red"></i>'; }?></div>
-                    </a>
-                  <?php } //endif ?>
-                    <a href="<?php echo \dash\url::this(). '/setting?domain='. \dash\get::index($value, 'name'); ?>">
-                    <div class="ibtn x30 wide"><?php echo '<span>'.T_("Autorenew"). '</span>'; if(isset($value['autorenew']) && $value['autorenew']) { echo '<i class="sf-refresh fc-blue"></i>'; } else{ echo '<i class="sf-times fc-red"></i>'; }?></div>
-                    </a>
-                </td>
-                <td class="txtC">
-                  <?php echo \dash\get::index($value, 'status_html'); ?>
-                </td>
-                <td class="collapsing txtL fs09"><?php echo \dash\fit::date(\dash\get::index($value, 'dateexpire')); ?></td>
-                <td class="collapsing txtL fs09">
-                  <div><?php echo \dash\fit::date(\dash\get::index($value, 'dateregister')); ?></div>
-                  <div><?php echo \dash\fit::date(\dash\get::index($value, 'dateupdate')); ?></div>
-                </td>
-                <td class="collapsing ltr txtL">
-                    <code><?php echo \dash\get::index($value, 'ns1'); ?></code>
-                    <br>
-                    <code><?php echo \dash\get::index($value, 'ns2'); ?></code>
-                </td>
-
-            </tr>
-            <?php } //endfor ?>
-        </tbody>
-    </table>
-</div>
-<?php \dash\utility\pagination::html(); ?>
-
-<?php } //endfunction ?>
-
-
-
-
-<?php function htmlFilter() {?>
-<p class="f fs14 msg warn2">
-  <span class="c"><?php echo \dash\data::filterBox(); ?></span>
-  <a class="cauto" href="<?php echo \dash\url::this(); ?>"><?php echo T_("Clear filters"); ?></a>
-</p>
-
-<?php } //endfunction ?>
-
-
-
-
-
-<?php function htmlStartAddNew() {?>
-
-<div class="fs14 msg info2 pTB20">
-  <p><?php echo T_("Hi!"); ?></p>
-  <p><a href="<?php echo \dash\url::this(); ?>/buy"><?php echo T_("Buy your first winning domain!"); ?></a></p>
-
-</div>
-
-<?php } //endfunction ?>
 
--- content_my/domain/home/view.php
+++ content_my/domain/home/view.php
@@ -18,61 +18,6 @@ class view
 		\dash\face::btnImport(\dash\url::this().'/import');
 		// \dash\face::btnExport(\dash\url::this().'/export');
 		\dash\face::help(\dash\url::support().'/domain');
-
-		$id = \dash\request::get('resultid');
-		$id = \dash\coding::decode($id);
-		if($id)
-		{
-			$detail = \lib\app\nic_domain\get::by_id($id);
-			\dash\data::dataRow($detail);
-		}
-
-
-		$args =
-		[
-			'order'  => \dash\request::get('order'),
-			'sort'   => \dash\request::get('sort'),
-			'dns'  => \dash\request::get('dns'),
-			// 'holder' => \dash\request::get('holder'),
-			// 'tech'   => \dash\request::get('tech'),
-			// 'bill'   => \dash\request::get('bill'),
-		];
-
-		$search_string = \dash\request::get('q');
-
-		if(\lib\nic\mode::api())
-		{
-			$args['q'] = $search_string;
-
-			$get_api    = new \lib\nic\api();
-			$list       = $get_api->domain_fetch($args);
-			$filterBox  = $get_api->meta('filter_message');
-			$isFiltered = $get_api->meta('is_filtered');
-		}
-		else
-		{
-
-			$list          = \lib\app\nic_domain\search::list($search_string, $args);
-			$filterBox     = \lib\app\nic_domain\search::filter_message();
-			$isFiltered    = \lib\app\nic_domain\search::is_filtered();
-
-		}
-
-		\dash\data::filterBox($filterBox);
-
-		\dash\data::dataTable($list);
-
-		\dash\data::isFiltered($isFiltered);
-
-
-		if($isFiltered)
-		{
-			\dash\face::title(\dash\face::title() . '  '. T_('Filtered'));
-		}
-
-		$sortLink = \dash\app\sort::make_sortLink(['name', 'dateexpire', 'dateregister', 'dateupdate'], \dash\url::this());
-		\dash\data::sortLink($sortLink);
-
 	}
 }
 ?>
