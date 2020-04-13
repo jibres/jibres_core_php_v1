b1526f8c764160da19fd7ed1c284d98e80a714a9
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-13 12:54:48 +0430

    fix back on domain management

--- content_management/domain/home/view.php
+++ content_management/domain/home/view.php
@@ -10,7 +10,7 @@ class view
 
 		// btn
 		\dash\data::back_text(T_('Dashboard'));
-		\dash\data::back_link(\dash\url::here());
+		\dash\data::back_link(\dash\url::this());
 
 		\dash\data::action_text(T_('Log'));
 		\dash\data::action_link(\dash\url::this(). '/log');
--- content_management/domain/log/view.php
+++ content_management/domain/log/view.php
@@ -10,7 +10,7 @@ class view
 
 		// btn
 		\dash\data::back_text(T_('Dashboard'));
-		\dash\data::back_link(\dash\url::here());
+		\dash\data::back_link(\dash\url::this());
 
 
 		$args =
