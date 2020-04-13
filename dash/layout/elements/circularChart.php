31b6c4a5a403054f6cae28ab1ad5487ba24caf9d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-13 20:03:35 +0430

    add chart autocolor

--- dash/layout/elements/circularChart.php
+++ dash/layout/elements/circularChart.php
@@ -1,1 +1,42 @@
-<div class="circularChart" data-color='<?php echo $myColor;?>'><svg viewBox="0 0 36 36"><path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke-dasharray="<?php if(!$myPercent) $myPercent=0; echo $myPercent;?>, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text class="percentage" x="18" y="20.35"><?php echo $myPercent;?>%</text></svg></div>
+<?php
+if(!isset($myPercent))
+{
+	$myPercent = 0;
+}
+if(!$myPercent || $myPercent < 0)
+{
+	$myPercent = 0;
+}
+if($myPercent > 100)
+{
+	$myPercent = 100;
+}
+if(!isset($myColor))
+{
+	$myColor = null;
+}
+if(!$myColor)
+{
+	if($myPercent == 100)
+	{
+		$myColor = 'green';
+	}
+	elseif($myPercent >= 70)
+	{
+		$myColor = 'blue';
+	}
+	elseif($myPercent >= 50)
+	{
+		$myColor = 'yellow';
+	}
+	elseif($myPercent >= 30)
+	{
+		$myColor = 'orange';
+	}
+	elseif($myPercent >= 10)
+	{
+		$myColor = 'red';
+	}
+}
+
+?><div class="circularChart" data-color='<?php echo $myColor;?>'><svg viewBox="0 0 36 36"><path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke-dasharray="<?php echo $myPercent;?>, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text class="percentage" x="18" y="20.35"><?php echo $myPercent;?>%</text></svg></div>
