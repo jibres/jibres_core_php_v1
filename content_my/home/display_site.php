a815f96dc02ffc0c982b26a0af25349d0fc0f7b0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-13 19:15:04 +0430

    clean jibres dashboard

--- content_my/home/display_site.php
+++ content_my/home/display_site.php
@@ -1,13 +1,81 @@
 
+	<div class="f">
+		<div class="c9">
+			<div class="f">
+				<div class="c6">
+<?php
+$listStore_owner = \dash\data::listStore_owner();
+if($listStore_owner && is_array($listStore_owner))
+{
+?>
+					<nav class="items">
+					 <ul>
+					  <li class="pA10"><?php echo T_("Bussiness that you are owner"); ?></li>
+<?php foreach ($listStore_owner as $key => $value) {?>
+					  <li>
+					   <a class="f" href="<?php echo \dash\get::index($value, 'url'); ?>/a">
+					   	<img src="<?php echo \dash\get::index($value, 'logo'); ?>" alt="<?php echo \dash\get::index($value, 'title'); ?>">
+					    <div class="key"><?php echo \dash\get::index($value, 'title'); ?></div>
+					    <div class="value"><?php echo \dash\get::index($value, 'subdomain'); ?></div>
+					    <div class="go"></div>
+					   </a>
+					  </li>
+<?php }//endfor ?>
+					  <li>
+					   <a class="f" href="<?php echo \dash\url::here(); ?>/store/start">
+					   	<img src="<?php echo \dash\url::icon();?>" alt="<?php echo T_("Add New Business");?>">
+					    <div class="key"><?php echo T_("Add New Business");?></div>
+					    <div class="go plus"></div>
+					   </a>
+					  </li>
+					 </ul>
+					</nav>
+<?php }//endif ?>
+				</div>
+				<div class="c6">
+<?php
+$listStore_staff = \dash\data::listStore_staff();
+if($listStore_staff && is_array($listStore_staff))
+{
+?>
+					<nav class="items">
+					 <ul>
+					 	<li class="pA10"><?php echo T_("Bussiness that you are staff"); ?></li>
+<?php foreach ($listStore_staff as $key => $value) {?>
+					  <li>
+					   <a class="f" href="<?php echo \dash\get::index($value, 'url'); ?>/a">
+					   	<img src="<?php echo \dash\get::index($value, 'logo'); ?>" alt="<?php echo \dash\get::index($value, 'title'); ?>">
+					    <div class="key"><?php echo \dash\get::index($value, 'title'); ?></div>
+					    <div class="value"><?php echo \dash\get::index($value, 'subdomain'); ?></div>
+					    <div class="go"></div>
+					   </a>
+					  </li>
+<?php }//endfor ?>
+					 </ul>
+					</nav>
+<?php }//endif ?>
+				</div>
+			</div>
 
-<?php require_once(root. 'content_my/store/home/display.php'); ?>
+		</div>
 
-<div class="welcome">
-  <p><?php echo T_("Easily register your business brand"); ?></p>
-  <h2><?php echo T_("Jibres; NIC's official representative in sales and domain management"); ?></h2>
 
-  <div class="buildBtn">
-    <a class="btn xl master" href="<?php echo \dash\url::this(); ?>/domain"><?php echo T_("Domain Center"); ?></a>
-  </div>
-
-</div>
+		<div class="c3">
+			<nav class="items">
+			 <ul>
+			  <li>
+			   <a class="f" href="<?php echo \dash\url::here();?>/domain">
+			    <div class="key"><?php echo T_('Domain Center');?></div>
+			    <div class="go"></div>
+			   </a>
+			  </li>
+			  <li>
+			   <a class="f" href="<?php echo \dash\url::here();?>/domain/buy">
+			    <div class="key"><?php echo T_('Buy New Domain');?></div>
+			    <div class="go"></div>
+			   </a>
+			  </li>
+			 </ul>
+			</nav>
+		</div>
+	</div>
--- content_my/home/view.php
+++ content_my/home/view.php
@@ -6,16 +6,14 @@ class view
 {
 	public static function config()
 	{
-		\dash\face::title(T_('Jibres my'));
-		\dash\face::boxTitle(false);
+		\dash\face::title(T_('Jibres Dashboard'));
+		\dash\face::help(\dash\url::support().'/dashboard');
 
 		if(!\dash\detect\device::detectPWA())
 		{
-			\dash\face::title(' ');
-
-			\dash\face::boxTitle(true);
-			\dash\data::action_text(T_('Stores'));
-			\dash\data::action_link(\dash\url::here() . '/store');
+			\dash\data::action_text(T_('Add Your Business'));
+			\dash\data::action_icon('plus');
+			\dash\data::action_link(\dash\url::this(). '/store/start');
 		}
 	}
 }
