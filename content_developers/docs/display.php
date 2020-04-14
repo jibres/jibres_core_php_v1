758c9258bc268b69fd6380b61c5db860217713ca
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 18:20:33 +0430

    add link of all api

--- content_developers/docs/display.php
+++ content_developers/docs/display.php
@@ -5,10 +5,37 @@
 
 
 	<div class="fit pT50">
-		<div class="box">
-			<div class="body">
-				<p class="msg mB0"><?php echo T_("We will write it soon :)"); ?></p>
+		<div class="f">
+
+			<div class="c6 s12 pA10">
+				<div class="box">
+					<header><h3><?php echo T_("API");?></h3></header>
+					<div class="body">
+						<p><?php echo T_("Our RESTful API allows you to perform actions enabled by Jibres control panel programmatically."); ?></p>
+						<a class="btn2 blue" href=""><?php echo T_("View API v2 Documentations"); ?></a>
+					</div>
+				</div>
 			</div>
+
+			<div class="c6 s12 pA10">
+				<div class="box">
+					<header><h3><?php echo T_("Core");?></h3></header>
+					<div class="body">
+						<p><?php echo T_("Our RESTful API allows you to perform actions enabled by Jibres control panel programmatically."); ?></p>
+						<a class="btn2 red" href=""><?php echo T_("View Core v10 Documentations"); ?></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="c6 s12 pA10">
+				<div class="box">
+					<header><h3><?php echo T_("Domains");?></h3></header>
+					<div class="body">
+						<p><?php echo T_("Our RESTful API allows you to perform actions enabled by Jibres control panel programmatically."); ?></p>
+						<a class="btn2 green" href=""><?php echo T_("View Domain v2 Documentations"); ?></a>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
-
