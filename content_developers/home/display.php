8cb7a8e11268110d58a7b899bbec428cbf098790
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 17:20:00 +0430

    add developers page

--- content_developers/home/display.php
+++ content_developers/home/display.php
@@ -1,1 +1,38 @@
-Developers
+
+	<div class="fit">
+		<div class="f txtC pT50 pB100">
+
+			<div class="c4 s12 pRa20">
+				<div class="box">
+					<div class="body">
+						<h2><?php echo T_("Guides"); ?></h2>
+						<p class="mB50"><?php echo T_("Learn the ins and outs of our<br>API and get started."); ?></p>
+				  <a class="btn2 block blue" href="<?php echo \dash\url::kingdom() ?>/guides"><?php echo T_("View Guides"); ?></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="c4 s12 pRa20">
+				<div class="box">
+					<div class="body">
+						<h2><?php echo T_("Libraries"); ?></h2>
+						<p class="mB50"><?php echo T_("Find our API translated to<br>your preferred language."); ?></p>
+				  <a class="btn2 block green" href="<?php echo \dash\url::kingdom() ?>/libraries"><?php echo T_("View Libraries"); ?></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="c4 s12">
+				<div class="box">
+					<div class="body">
+						<h2><?php echo T_("Documentation"); ?></h2>
+						<p class="mB50"><?php echo T_("Get everything you need to<br>build on top of our API."); ?></p>
+				  <a class="btn2 block red" href="<?php echo \dash\url::kingdom() ?>/docs"><?php echo T_("View Docs"); ?></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
