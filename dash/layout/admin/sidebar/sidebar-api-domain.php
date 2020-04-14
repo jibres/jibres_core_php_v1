d3ffb9114d9b0ec4c65df54ea5e3ef9161a2e3da
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 20:59:04 +0430

    fix sidebar of api

--- dash/layout/admin/sidebar/sidebar-api-domain.php
+++ dash/layout/admin/sidebar/sidebar-api-domain.php
@@ -4,7 +4,7 @@
      <li><a href="<?php echo \dash\url::current(); ?>#requests"><i class='sf-angle-double-left'></i> <span><?php echo T_("Requests"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current(); ?>#responses"><i class='sf-angle-double-right'></i> <span><?php echo T_("Responses"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current(); ?>#postman-collection"><i class='sf-terminal'></i> <span><?php echo T_("Postman collection"); ?></span></a></li>
-     <li><hr></li>
+     <li class="hr"></li>
      <!--   DOMAIN   -->
      <li><a href="<?php echo \dash\url::current() ?>#domain-check"><i class="sf-download"></i><span><?php echo T_("Choose domain"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#domain-available"><i class="sf-download"></i><span><?php echo T_("Check domain available"); ?></span></a></li>
@@ -20,7 +20,7 @@
      <li><a href="<?php echo \dash\url::current(); ?>#domain-renew"><i class="sf-shop"></i><span><?php echo T_("Renew domain"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current(); ?>#domain-transfer"><i class="sf-shop"></i><span><?php echo T_("Transfer domain"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current(); ?>#domain-history"><i class="sf-list"></i><span><?php echo T_("Domain history"); ?></span></a></li>
-     <li><hr></li>
+     <li class="hr"></li>
      <!--   CONTACT   -->
      <li><a href="<?php echo \dash\url::current() ?>#contact-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch contacts"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#contact-load"><i class="sf-download"></i><span><?php echo T_("Get contact detail"); ?></span></a></li>
@@ -28,13 +28,13 @@
      <li><a href="<?php echo \dash\url::current() ?>#contact-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update contact"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#contact-add"><i class="sf-plus-circle"></i><span><?php echo T_("Add contact"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#contact-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new contact"); ?></span></a></li>
-     <li><hr></li>
+     <li class="hr"></li>
      <!--   DNS   -->
      <li><a href="<?php echo \dash\url::current() ?>#dns-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch dns"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#dns-load"><i class="sf-download"></i><span><?php echo T_("Get dns detail"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#dns-delete"><i class="sf-eraser"></i><span><?php echo T_("Delete dns"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#dns-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update dns"); ?></span></a></li>
      <li><a href="<?php echo \dash\url::current() ?>#dns-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new dns"); ?></span></a></li>
-     <li><hr></li>
+     <li class="hr"></li>
      <!--   POLL   -->
      <li><a href="<?php echo \dash\url::current() ?>#poll-request"><i class="sf-th"></i><span><?php echo T_("Get poll"); ?></span></a></li>
--- dash/layout/admin/sidebar/sidebar-api-v2.php
+++ dash/layout/admin/sidebar/sidebar-api-v2.php
@@ -1,29 +1,25 @@
-
-  <li>
-    <a href="<?php echo \dash\url::current(); ?>"><i class='sf-campfire'></i> <span><?php echo \dash\face::site(); ?> <?php echo T_("API"); ?></span></a>
-    <ul>
-      <?php // ------------------------------------------------------------------------------------------------------------------ INTRO  ?>
+      <?php // -------------------------------------------------------------- INTRO  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#intro"><i class='sf-tree-1'></i> <span><?php echo T_("Introduction"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#endpoints"><i class='sf-terminal'></i> <span><?php echo T_("Endpoints"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#requests"><i class='sf-angle-double-left'></i> <span><?php echo T_("Requests"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#responses"><i class='sf-angle-double-right'></i> <span><?php echo T_("Responses"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ APPLICATION  ?>
+      <?php // -------------------------------------------------------------- APPLICATION  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#app-detail"><i class='sf-download'></i> <span><?php echo T_("Get app detail"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#token"><i class='sf-unlock-alt'></i> <span><?php echo T_("Get Access Token"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#user-add"><i class='sf-plus-circle'></i> <span><?php echo T_("Sign Up User"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#language"><i class='sf-download'></i> <span><?php echo T_("Get language list"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ LOCATION  ?>
+      <?php // -------------------------------------------------------------- LOCATION  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#location-country"><i class='sf-earth'></i> <span><?php echo T_("Country list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#location-province"><i class='sf-earth'></i> <span><?php echo T_("Province list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#location-city"><i class='sf-earth'></i> <span><?php echo T_("City list"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ PROFILE  ?>
+      <?php // -------------------------------------------------------------- PROFILE  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#get-profile"><i class='sf-download'></i> <span><?php echo T_("Get profile detail"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#upload-avatar"><i class='sf-cloud-upload'></i> <span><?php echo T_("Upload avatar"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#update-profile"><i class='sf-edit-write'></i> <span><?php echo T_("Update profile"); ?></span></a></li>
@@ -31,9 +27,9 @@
       <li><a href="<?php echo \dash\url::current(); ?>#profile-address-list"><i class='sf-th'></i> <span><?php echo T_("Get list of your address"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#profile-address-edit"><i class='sf-edit-write'></i> <span><?php echo T_("Edit address"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#profile-address-remove"><i class='sf-eraser'></i> <span><?php echo T_("Remove address"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ USER MANAGMENT  ?>
+      <?php // -------------------------------------------------------------- USER MANAGMENT  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#user-list"><i class='sf-th'></i> <span><?php echo T_("Users list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#user-add-crm"><i class='sf-plus-circle'></i> <span><?php echo T_("Add new user"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#get-user-detail"><i class='sf-download'></i> <span><?php echo T_("Get one user detail"); ?></span></a></li>
@@ -43,27 +39,27 @@
       <li><a href="<?php echo \dash\url::current(); ?>#user-address-list"><i class='sf-th'></i> <span><?php echo T_("Get list of user address"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#user-address-edit"><i class='sf-edit-write'></i> <span><?php echo T_("Edit user address"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#user-address-remove"><i class='sf-eraser'></i> <span><?php echo T_("Remove user address"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ BUSINESS  ?>
+      <?php // -------------------------------------------------------------- BUSINESS  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#about"><i class='sf-download'></i> <span><?php echo T_("Get about"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#mission"><i class='sf-download'></i> <span><?php echo T_("Get mission"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#vision"><i class='sf-download'></i> <span><?php echo T_("Get vision"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#contact"><i class='sf-download'></i> <span><?php echo T_("Get contact"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#posts"><i class='sf-th'></i> <span><?php echo T_("Get list of posts"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#get-post"><i class='sf-download'></i> <span><?php echo T_("Get post detail"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ ACCOUNT  ?>
+      <?php // -------------------------------------------------------------- ACCOUNT  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#user-login"><i class='sf-log-in'></i> <span><?php echo T_("Login user"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#user-login-verify"><i class='sf-check'></i> <span><?php echo T_("Verify user"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#smile"><i class='sf-download'></i> <span><?php echo T_("Check new notification"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#notif"><i class='sf-th'></i> <span><?php echo T_("Get notification list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#session-list"><i class='sf-th'></i> <span><?php echo T_("Session list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#session-terminate"><i class='sf-eraser'></i> <span><?php echo T_("Terminate session"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ TICKET MANAGEMENT  ?>
+      <?php // -------------------------------------------------------------- TICKET MANAGEMENT  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#ticket-add"><i class='sf-plus-circle'></i> <span><?php echo T_("Add new ticket"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#ticket-list"><i class='sf-th'></i> <span><?php echo T_("Ticket list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#ticket-load"><i class='sf-download'></i> <span><?php echo T_("Load a ticket"); ?></span></a></li>
@@ -71,10 +67,10 @@
       <li><a href="<?php echo \dash\url::current(); ?>#ticket-status"><i class='sf-cog'></i> <span><?php echo T_("Set ticket status"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#ticket-solved"><i class='sf-cog'></i> <span><?php echo T_("Set ticket solved"); ?></span></a></li>
 
-      <li><hr></li>
+      <li class="hr"></li>
 
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ PRODUCT  ?>
+      <?php // -------------------------------------------------------------- PRODUCT  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#product-add"><i class='sf-plus-circle'></i> <span><?php echo T_("Add new product"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#product-edit"><i class='sf-edit-write'></i> <span><?php echo T_("Edit product"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#product-gallery"><i class='sf-cloud-upload'></i> <span><?php echo T_("Upload product gallery"); ?></span></a></li>
@@ -89,30 +85,24 @@
 
 
       <li><a href="<?php echo \dash\url::current(); ?>#product-remove"><i class='sf-eraser'></i> <span><?php echo T_("Remove product"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ PRODUCT COMMENT  ?>
+      <?php // -------------------------------------------------------------- PRODUCT COMMENT  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#product-comment-add"><i class='sf-plus-circle'></i> <span><?php echo T_("Add product comment"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ PRODUCT META  ?>
+      <?php // -------------------------------------------------------------- PRODUCT META  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#category-list"><i class='sf-th'></i> <span><?php echo T_("Category list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#category-child"><i class='sf-th'></i> <span><?php echo T_("Category child"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#category-property"><i class='sf-th'></i> <span><?php echo T_("Category property"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#company-list"><i class='sf-th'></i> <span><?php echo T_("Company list"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#collection-list"><i class='sf-th'></i> <span><?php echo T_("Collection list"); ?></span></a></li>
-      <li><hr></li>
+      <li class="hr"></li>
 
 
-      <?php // ------------------------------------------------------------------------------------------------------------------ CART  ?>
+      <?php // -------------------------------------------------------------- CART  ?>
       <li><a href="<?php echo \dash\url::current(); ?>#add-to-cart"><i class='sf-plus-circle'></i> <span><?php echo T_("Add to cart"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#show-cart"><i class='sf-th'></i> <span><?php echo T_("Get cart detail"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#delete-from-cart"><i class='sf-eraser'></i> <span><?php echo T_("Delete from cart"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#edit-cart"><i class='sf-edit-write'></i> <span><?php echo T_("Edit cart"); ?></span></a></li>
       <li><a href="<?php echo \dash\url::current(); ?>#checkout-cart"><i class='sf-edit-write'></i> <span><?php echo T_("Checkout cart"); ?></span></a></li>
-
-
-    </ul>
-  </li>
-
-
