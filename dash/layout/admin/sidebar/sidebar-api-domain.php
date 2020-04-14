a8ba4cee880387d802c88dc3461066e2e62a8c74
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 20:43:42 +0430

    clean sidebar

--- dash/layout/admin/sidebar/sidebar-api-domain.php
+++ dash/layout/admin/sidebar/sidebar-api-domain.php
@@ -1,49 +1,40 @@
-
-
-      <!-- INTRO -->
-      <li><a href="<?php echo \dash\url::current(); ?>#intro"><i class='sf-tree-1'></i> <span><?php echo T_("Introduction"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#endpoints"><i class='sf-terminal'></i> <span><?php echo T_("Endpoints"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#requests"><i class='sf-angle-double-left'></i> <span><?php echo T_("Requests"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#responses"><i class='sf-angle-double-right'></i> <span><?php echo T_("Responses"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#postman-collection"><i class='sf-terminal'></i> <span><?php echo T_("Postman collection"); ?></span></a></li>
-      <li><hr></li>
-
-      <!--   DOMAIN   -->
-      <li><a href="<?php echo \dash\url::current() ?>#domain-check"><i class="sf-download"></i><span><?php echo T_("Choose domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#domain-available"><i class="sf-download"></i><span><?php echo T_("Check domain available"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch domain list"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-detail"><i class="sf-download"></i><span><?php echo T_("Get domain detail"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-autorenew"><i class="sf-edit-write"></i><span><?php echo T_("Set autorenew setting"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-holder"><i class="sf-edit-write"></i><span><?php echo T_("Change domain holder"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-dns"><i class="sf-edit-write"></i><span><?php echo T_("Change domain dns"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-remove"><i class="sf-eraser"></i><span><?php echo T_("Remove domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-lock"><i class="sf-edit-write"></i><span><?php echo T_("Lock / Unlock domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-action"><i class="sf-download"></i><span><?php echo T_("Get domain action"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-buy"><i class="sf-shop"></i><span><?php echo T_("Buy new domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-renew"><i class="sf-shop"></i><span><?php echo T_("Renew domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-transfer"><i class="sf-shop"></i><span><?php echo T_("Transfer domain"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current(); ?>#domain-history"><i class="sf-list"></i><span><?php echo T_("Domain history"); ?></span></a></li>
-      <li><hr></li>
-
-      <!--   CONTACT   -->
-      <li><a href="<?php echo \dash\url::current() ?>#contact-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch contacts"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#contact-load"><i class="sf-download"></i><span><?php echo T_("Get contact detail"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#contact-delete"><i class="sf-eraser"></i><span><?php echo T_("Delete contact"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#contact-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update contact"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#contact-add"><i class="sf-plus-circle"></i><span><?php echo T_("Add contact"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#contact-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new contact"); ?></span></a></li>
-      <li><hr></li>
-
-      <!--   DNS   -->
-      <li><a href="<?php echo \dash\url::current() ?>#dns-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch dns"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#dns-load"><i class="sf-download"></i><span><?php echo T_("Get dns detail"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#dns-delete"><i class="sf-eraser"></i><span><?php echo T_("Delete dns"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#dns-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update dns"); ?></span></a></li>
-      <li><a href="<?php echo \dash\url::current() ?>#dns-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new dns"); ?></span></a></li>
-      <li><hr></li>
-
-      <!--   POLL   -->
-      <li><a href="<?php echo \dash\url::current() ?>#poll-request"><i class="sf-th"></i><span><?php echo T_("Get poll"); ?></span></a></li>
-
-
-
+     <!-- INTRO -->
+     <li><a href="<?php echo \dash\url::current(); ?>#intro"><i class='sf-tree-1'></i> <span><?php echo T_("Introduction"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#endpoints"><i class='sf-terminal'></i> <span><?php echo T_("Endpoints"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#requests"><i class='sf-angle-double-left'></i> <span><?php echo T_("Requests"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#responses"><i class='sf-angle-double-right'></i> <span><?php echo T_("Responses"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#postman-collection"><i class='sf-terminal'></i> <span><?php echo T_("Postman collection"); ?></span></a></li>
+     <li><hr></li>
+     <!--   DOMAIN   -->
+     <li><a href="<?php echo \dash\url::current() ?>#domain-check"><i class="sf-download"></i><span><?php echo T_("Choose domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#domain-available"><i class="sf-download"></i><span><?php echo T_("Check domain available"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch domain list"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-detail"><i class="sf-download"></i><span><?php echo T_("Get domain detail"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-autorenew"><i class="sf-edit-write"></i><span><?php echo T_("Set autorenew setting"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-holder"><i class="sf-edit-write"></i><span><?php echo T_("Change domain holder"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-dns"><i class="sf-edit-write"></i><span><?php echo T_("Change domain dns"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-remove"><i class="sf-eraser"></i><span><?php echo T_("Remove domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-lock"><i class="sf-edit-write"></i><span><?php echo T_("Lock / Unlock domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-action"><i class="sf-download"></i><span><?php echo T_("Get domain action"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-buy"><i class="sf-shop"></i><span><?php echo T_("Buy new domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-renew"><i class="sf-shop"></i><span><?php echo T_("Renew domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-transfer"><i class="sf-shop"></i><span><?php echo T_("Transfer domain"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current(); ?>#domain-history"><i class="sf-list"></i><span><?php echo T_("Domain history"); ?></span></a></li>
+     <li><hr></li>
+     <!--   CONTACT   -->
+     <li><a href="<?php echo \dash\url::current() ?>#contact-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch contacts"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#contact-load"><i class="sf-download"></i><span><?php echo T_("Get contact detail"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#contact-delete"><i class="sf-eraser"></i><span><?php echo T_("Delete contact"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#contact-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update contact"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#contact-add"><i class="sf-plus-circle"></i><span><?php echo T_("Add contact"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#contact-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new contact"); ?></span></a></li>
+     <li><hr></li>
+     <!--   DNS   -->
+     <li><a href="<?php echo \dash\url::current() ?>#dns-fetch"><i class="sf-th"></i><span><?php echo T_("Fetch dns"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#dns-load"><i class="sf-download"></i><span><?php echo T_("Get dns detail"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#dns-delete"><i class="sf-eraser"></i><span><?php echo T_("Delete dns"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#dns-patch"><i class="sf-edit-write"></i><span><?php echo T_("Update dns"); ?></span></a></li>
+     <li><a href="<?php echo \dash\url::current() ?>#dns-create-new"><i class="sf-plus-circle"></i><span><?php echo T_("Create new dns"); ?></span></a></li>
+     <li><hr></li>
+     <!--   POLL   -->
+     <li><a href="<?php echo \dash\url::current() ?>#poll-request"><i class="sf-th"></i><span><?php echo T_("Get poll"); ?></span></a></li>
