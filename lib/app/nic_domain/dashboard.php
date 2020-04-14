5bb4e1d291305b8e2c1d7bc54b4faf6973762b29
Reza <rm.biqarar@gmail.com>
2020-04-14 11:37:22 +0430

    get result from billing

--- lib/app/nic_domain/dashboard.php
+++ lib/app/nic_domain/dashboard.php
@@ -15,17 +15,17 @@ class dashboard
 
 
 		$result                          = [];
-		$result['sale_count_today']      = \lib\db\nic_domainaction\get::sale_count_date($today);
-		$result['sale_count_yesterday']  = \lib\db\nic_domainaction\get::sale_count_date($yesterday);
-		$result['sale_count_last_week']  = \lib\db\nic_domainaction\get::sale_count_date($last_week);
-		$result['sale_count_last_month'] = \lib\db\nic_domainaction\get::sale_count_date($last_month);
-		$result['sale_count_total']      = \lib\db\nic_domainaction\get::sale_count_date();
+		$result['sale_count_today']      = \lib\db\nic_domainbilling\get::sale_count_date($today);
+		$result['sale_count_yesterday']  = \lib\db\nic_domainbilling\get::sale_count_date($yesterday);
+		$result['sale_count_last_week']  = \lib\db\nic_domainbilling\get::sale_count_date($last_week);
+		$result['sale_count_last_month'] = \lib\db\nic_domainbilling\get::sale_count_date($last_month);
+		$result['sale_count_total']      = \lib\db\nic_domainbilling\get::sale_count_date();
 
 
 		$result['total_buyers']          = \lib\db\nic_domainaction\get::total_buyers();
 		$result['total_log']             = \lib\db\nic_log\get::count_all();
 
-		$group_by_action                 = \lib\db\nic_domainaction\get::count_group_by_action();
+		$group_by_action                 = \lib\db\nic_domainbilling\get::count_group_by_action();
 
 		$result['total_domain_buy']      = isset($group_by_action['register']) ? $group_by_action['register'] : null;
 		$result['total_domain_renew']    = isset($group_by_action['renew']) ? $group_by_action['renew'] : null;;
--- lib/db/nic_domainbilling/get.php
+++ lib/db/nic_domainbilling/get.php
@@ -9,10 +9,10 @@ class get
 		$date = null;
 		if($_date)
 		{
-			$date = " AND DATE(domainbilling.date) > DATE('$_date') ";
+			$date = " AND DATE(domainbilling.datecreated) > DATE('$_date') ";
 		}
 
-		$query  = "SELECT SUM(domainbilling.price) AS `price` FROM domainbilling WHERE domainbilling.user_id = $_user_id $date";
+		$query  = "SELECT SUM(domainbilling.price) AS `price` FROM domainbilling WHERE domainbilling.user_id = $_user_id WHERE 1 $date";
 
 		$result = \dash\db::get($query, 'price', true, 'nic');
 
@@ -34,6 +34,53 @@ class get
 				domainbilling.user_id = $_user_id
 			GROUP BY
 				`month`,`year`
+		";
+
+		$result = \dash\db::get($query, null, false, 'nic');
+		return $result;
+	}
+
+
+		public static function sale_count_date($_date = null)
+	{
+		$date = null;
+		if($_date)
+		{
+			$date = " AND DATE(domainbilling.date) > DATE('$_date') ";
+		}
+
+		$query  = "SELECT COUNT(*) AS `count` FROM domainbilling WHERE 1 $date";
+
+		$result = \dash\db::get($query, 'count', true, 'nic');
+
+		return $result;
+	}
+
+
+
+	public static function count_group_by_action()
+	{
+		$query  = "SELECT COUNT(*) AS `count`, domainbilling.action FROM domainbilling  GROUP BY domainbilling.action";
+		$result = \dash\db::get($query, ['action', 'count'], false, 'nic');
+		return $result;
+	}
+
+
+	public static function chart_domain_action($_date)
+	{
+		$query  =
+		"
+			SELECT
+				COUNT(*) AS `count`,
+				domainbilling.action,
+				DATE(domainbilling.datecreated) AS `date`
+			FROM
+				domainbilling
+			WHERE
+				DATE(domainbilling.datecreated) > DATE('$_date')
+			GROUP BY
+				domainbilling.action,
+				DATE(domainbilling.datecreated)
 		";
 
 		$result = \dash\db::get($query, null, false, 'nic');
