d75983442a8cec7ad459f6ec030f70c445f3f75f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 15:04:56 +0430

    fix redirect all invalid url into developers subdomain

--- content_developers/controller.php
+++ content_developers/controller.php
@@ -5,23 +5,18 @@ class controller
 {
 	public static function routing()
 	{
-		if(\dash\url::subdomain() === 'core')
+		if(!\dash\url::subdomain())
 		{
-			if(\dash\url::module())
-			{
-				\dash\header::status(404);
-			}
-
-			\dash\redirect::to(\dash\url::api('developers'));
+			\dash\redirect::to(\dash\url::api('developers'), false);
 		}
-		elseif(\dash\url::subdomain() === 'api')
+		elseif(\dash\url::subdomain() !== 'developers')
 		{
 			if(\dash\url::module())
 			{
 				\dash\header::status(404);
 			}
 
-			\dash\redirect::to(\dash\url::api('developers'));
+			\dash\redirect::to(\dash\url::api('developers'), false);
 		}
 	}
 }
