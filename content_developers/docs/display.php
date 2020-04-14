de7ffd1b25ed10737b6cebb81d2461fc107254f6
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 18:11:38 +0430

    add docs

---
+++ content_developers/docs/display.php
@@ -0,0 +1,14 @@
+  <section id='jibresPageTitle' class="color2">
+   <h2><?php echo T_('Access all the information you need to build on our API.') ?></h2>
+   <p><?php echo T_('Our documentation for API, OAuth, and metadata is clearly organized and easy to use.') ?></p>
+  </section>
+
+
+	<div class="fit pT50">
+		<div class="box">
+			<div class="body">
+				<p class="msg mB0"><?php echo T_("We will write it soon :)"); ?></p>
+			</div>
+		</div>
+	</div>
+
---
+++ content_developers/docs/view.php
@@ -0,0 +1,13 @@
+<?php
+namespace content_developers\docs;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\face::title(T_('Jibres API Documentations'));
+		\dash\face::desc(T_('Our documentation for API, OAuth, and metadata is clearly organized and easy to use.'));
+	}
+}
+?>
