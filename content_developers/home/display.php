49571ee17d4b670f9840d32ec7ff3b032cc8c2b0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-14 17:41:31 +0430

    add image

--- content_developers/home/display.php
+++ content_developers/home/display.php
@@ -2,8 +2,9 @@
 	<div class="fit">
 		<div class="f txtC pT50 pB100">
 
-			<div class="c4 s12 pRa20">
+			<div class="c4 s12 pA10">
 				<div class="box">
+					<img src="<?php echo \dash\url::cdn();?>/img/developers/jibres-guides-1.jpg" alt='<?php echo T_("Jibres Guides"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Guides"); ?></h2>
 						<p class="mB50"><?php echo T_("Learn the ins and outs of our<br>API and get started."); ?></p>
@@ -12,8 +13,9 @@
 				</div>
 			</div>
 
-			<div class="c4 s12 pRa20">
+			<div class="c4 s12 pA10">
 				<div class="box">
+					<img src="<?php echo \dash\url::cdn();?>/img/developers/jibres-libraries-1.jpg" alt='<?php echo T_("Jibres Libraries"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Libraries"); ?></h2>
 						<p class="mB50"><?php echo T_("Find our API translated to<br>your preferred language."); ?></p>
@@ -22,8 +24,9 @@
 				</div>
 			</div>
 
-			<div class="c4 s12">
+			<div class="c4 s12 pA10">
 				<div class="box">
+					<img src="<?php echo \dash\url::cdn();?>/img/developers/jibres-docs-1.jpg" alt='<?php echo T_("Jibres Docs"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Documentation"); ?></h2>
 						<p class="mB50"><?php echo T_("Get everything you need to<br>build on top of our API."); ?></p>
