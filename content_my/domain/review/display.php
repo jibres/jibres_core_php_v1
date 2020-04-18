b16876d9db88d96ec3290557194958393893b752
Reza <rm.biqarar@gmail.com>
2020-04-18 16:03:38 +0430

    try to fix review page

--- content_my/domain/review/display.php
+++ content_my/domain/review/display.php
@@ -1,10 +1,10 @@
 <div class="f justify-center">
   <div class="c6 m8 s12">
 
-    <form method="post" autocomplete="off" class="box impact">
+    <div class="box impact">
       <header><h2><?php echo T_("Your domain ready to register"); ?></h2></header>
 
-      <div class="body zeroPad">
+      <div class="body">
         <table class="tbl1 v4">
           <tbody>
             <tr>
@@ -69,17 +69,44 @@
               </tr>
             <?php } // endif ?>
 
-
-
-
           </tbody>
         </table>
+
+        <?php if($period && \dash\data::userBudget() && intval(\dash\data::userBudget()) > intval(\lib\app\nic_domain\price::register($period))) {?>
+        <?php } // endif ?>
+        <?php if (true) {?>
+        <div class="f mB10">
+          <div class="c pB10 pRa5">
+            <div class="radio3">
+              <input type="radio" name="paytype" value="budget" id="budget" checked>
+              <label for="budget"><?php echo T_("Pay from your budget"); ?> <small><?php echo \dash\fit::number(\dash\data::userBudget()); ?> <?php echo T_("Toman") ?></small></label>
+            </div>
+          </div>
+          <div class="c pB10">
+            <div class="radio3">
+              <input type="radio" name="paytype" value="bypayment" id="bypayment">
+              <label for="bypayment"><?php echo T_("Pay by IGP"); ?></label>
+            </div>
+          </div>
+        </div>
+      <?php } //endif ?>
+
+
+        <label for="gift"><?php echo T_("Gift code") ?></label>
+        <div class="input ltr">
+          <button class="btn primary addon"><?php echo T_("Check"); ?></button>
+          <input type="text" name="gift"  id="gift" >
+        </div>
+
 
       </div>
 
       <footer class="txtRa">
+        <form method="post" autocomplete="off">
+          <input type="hidden" name="register" value="1">
         <button class="btn success"><?php echo T_("Register domain"); ?></button>
+        </form>
       </footer>
-    </form>
+    </div>
   </div>
 </div>
