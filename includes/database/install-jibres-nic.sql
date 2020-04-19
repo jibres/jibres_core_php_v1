eb7db50b6a1d53cb72610992b46df0abd064cb47
Reza <rm.biqarar@gmail.com>
2020-04-19 16:11:34 +0430

    archive db query

--- includes/database/install-jibres-nic.sql
+++ includes/database/install-jibres-nic.sql
@@ -424,3 +424,11 @@ CREATE TABLE IF NOT EXISTS jibres_nic.usersetting (
 PRIMARY KEY (`id`),
 KEY `usersetting_index_search_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE jibres_nic.domainbilling ADD `finalprice` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres_nic.domainbilling ADD `giftusage_id` bigint(20) UNSIGNED NULL DEFAULT NULL;
+
+
+ALTER TABLE jibres_nic.domainaction ADD `finalprice` bigint(20) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE jibres_nic.domainaction ADD `giftusage_id` bigint(20) UNSIGNED NULL DEFAULT NULL;
--- includes/database/upgrade/jibres/v.0.2.7_gift_usage_id.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE jibres_nic.domainbilling ADD `finalprice` bigint(20) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE jibres_nic.domainbilling ADD `giftusage_id` bigint(20) UNSIGNED NULL DEFAULT NULL;
-
-
-ALTER TABLE jibres_nic.domainaction ADD `finalprice` bigint(20) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE jibres_nic.domainaction ADD `giftusage_id` bigint(20) UNSIGNED NULL DEFAULT NULL;
