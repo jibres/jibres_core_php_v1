1f01338aa02082ae6aae60e2233203b17aa3c48f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-23 19:41:44 +0430

    some fix

--- content_love/gift/home/giftcard.php
+++ content_love/gift/home/giftcard.php
@@ -8,19 +8,34 @@ if(!isset($gPercent))
 {
 	$gPercent = "0";
 }
- ?>
+if(!isset($gExpireDate))
+{
+	$gExpireDate = "2020-02-12";
+}
+if(!isset($gMaxGift))
+{
+	$gMaxGift = "10000";
+}
+?>
 <div class="giftBox">
 	<div class="giftcard">
 		<header class="f align-center">
 			<div class="cauto pRa20">
 				<img class="logo" src="<?php echo \dash\url::icon();?>" alt="<?php echo T_("Jibres");?>">
 			</div>
-			<div class="c pRa20">
+			<div class="c">
 				<h2 class="txtB mB0"><?php echo T_("Jibres"); ?></h2>
 				<h3><?php echo T_("Sell & Enjoy"); ?></h3>
 			</div>
-			<div class="c5">
-
+			<div class="cauto os pLa20">
+				<div class="f">
+					<abbr class="c6"><?php echo T_("Expire Date");?></abbr>
+					<span class="c6"><?php echo \dash\fit::date($gExpireDate); ?></span>
+				</div>
+				<div class="f">
+					<abbr class="c6"><?php echo T_("Max Gift");?></abbr>
+					<span class="c6"><?php echo \dash\fit::number($gMaxGift). ' '. T_("Toman");?></span>
+				</div>
 			</div>
 		</header>
 		<div class="body">
