bf96f8a35ce8ce618415d7d55d7ade72591426ea
Reza <rm.biqarar@gmail.com>
2020-04-25 13:20:23 +0430

    fix title

--- content_love/gift/edit/display.php
+++ content_love/gift/edit/display.php
@@ -4,7 +4,7 @@
   <div class="c6 m8 s12">
 
     <form method="post" autocomplete="off" class="box impact">
-      <header><h2><?php echo T_("Create new gift card"); ?></h2></header>
+      <header><h2><?php echo T_("Edit gift card"); ?></h2></header>
 
       <div class="body">
         <label for="code"><?php echo T_("Gift code"); ?></label>
