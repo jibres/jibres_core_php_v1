94cbf3bb224ed950143305a09492b113cb52d7be
Reza <rm.biqarar@gmail.com>
2020-04-27 14:48:58 +0430

    NIC: change position of domain add and domain rem

--- lib/nic/exec/domain_update.php
+++ lib/nic/exec/domain_update.php
@@ -51,128 +51,6 @@ class domain_update
 // <domain:add><domain:ns><domain:hostAttr><domain:hostName>ns2.example.net</domain:hostName><domain:hostAddr ip="v4">192.0.2.2</domain:hostAddr></domain:hostAttr></domain:ns></domain:add>
 // <domain:rem><domain:ns><domain:hostAttr><domain:hostName>ns1.JIBRES-SAMPLE-DOMAIN.IR</domain:hostName></domain:hostAttr></domain:ns></domain:rem>
 
-		if(
-			isset($_args['new_ns1']) && $_args['new_ns1'] ||
-			isset($_args['new_ns2']) && $_args['new_ns2'] ||
-			isset($_args['new_ns3']) && $_args['new_ns3'] ||
-			isset($_args['new_ns4']) && $_args['new_ns4'] ||
-
-			isset($_args['new_ip1']) && $_args['new_ip1'] ||
-			isset($_args['new_ip2']) && $_args['new_ip2'] ||
-			isset($_args['new_ip3']) && $_args['new_ip3'] ||
-			isset($_args['new_ip4']) && $_args['new_ip4']
-		  )
-		{
-
-			$add_ns_xml = '';
-
-			// -------------------------------------------------------- DNS 1 + IP 1
-			if(isset($_args['new_ns1']) && $_args['new_ns1'])
-			{
-				if(isset($_args['new_ip1']) && $_args['new_ip1'])
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns1']. '</domain:hostName>THIS-POSITION-ADD-IP1</domain:hostAttr>';
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns1']. '</domain:hostName></domain:hostAttr>';
-				}
-			}
-
-			if(isset($_args['new_ip1']) && $_args['new_ip1'])
-			{
-				if(isset($_args['new_ns1']) && $_args['new_ns1'])
-				{
-					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP1', '<domain:hostAddr ip="v4">'. $_args['new_ip1']. '</domain:hostAddr>', $add_ns_xml);
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip1']. '</domain:hostAddr>';
-				}
-			}
-
-			// -------------------------------------------------------- DNS 2 + IP 2
-			if(isset($_args['new_ns2']) && $_args['new_ns2'])
-			{
-				if(isset($_args['new_ip2']) && $_args['new_ip2'])
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns2']. '</domain:hostName>THIS-POSITION-ADD-IP2</domain:hostAttr>';
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns2']. '</domain:hostName></domain:hostAttr>';
-				}
-			}
-
-			if(isset($_args['new_ip2']) && $_args['new_ip2'])
-			{
-				if(isset($_args['new_ns2']) && $_args['new_ns2'])
-				{
-					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP2', '<domain:hostAddr ip="v4">'. $_args['new_ip2']. '</domain:hostAddr>', $add_ns_xml);
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip2']. '</domain:hostAddr>';
-				}
-			}
-
-			// -------------------------------------------------------- DNS 3 + IP 3
-			if(isset($_args['new_ns3']) && $_args['new_ns3'])
-			{
-				if(isset($_args['new_ip3']) && $_args['new_ip3'])
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns3']. '</domain:hostName>THIS-POSITION-ADD-IP3</domain:hostAttr>';
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns3']. '</domain:hostName></domain:hostAttr>';
-				}
-			}
-
-			if(isset($_args['new_ip3']) && $_args['new_ip3'])
-			{
-				if(isset($_args['new_ns3']) && $_args['new_ns3'])
-				{
-					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP3', '<domain:hostAddr ip="v4">'. $_args['new_ip3']. '</domain:hostAddr>', $add_ns_xml);
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip3']. '</domain:hostAddr>';
-				}
-			}
-
-			// -------------------------------------------------------- DNS 4 + IP 4
-			if(isset($_args['new_ns4']) && $_args['new_ns4'])
-			{
-				if(isset($_args['new_ip4']) && $_args['new_ip4'])
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns4']. '</domain:hostName>THIS-POSITION-ADD-IP4</domain:hostAttr>';
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns4']. '</domain:hostName></domain:hostAttr>';
-				}
-			}
-
-			if(isset($_args['new_ip4']) && $_args['new_ip4'])
-			{
-				if(isset($_args['new_ns4']) && $_args['new_ns4'])
-				{
-					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP4', '<domain:hostAddr ip="v4">'. $_args['new_ip4']. '</domain:hostAddr>', $add_ns_xml);
-				}
-				else
-				{
-					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip4']. '</domain:hostAddr>';
-				}
-			}
-
-			if($add_ns_xml)
-			{
-				$xml_domain_add = '<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name><domain:add><domain:ns>'. $add_ns_xml.'</domain:ns></domain:add>';
-				$xml = str_replace('<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name>', $xml_domain_add, $xml);
-			}
-		}
-
 
 		if(
 			isset($_args['old_ns1']) && $_args['old_ns1'] ||
@@ -294,6 +172,129 @@ class domain_update
 			{
 				$xml_domain_rem = '<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name><domain:rem><domain:ns>'. $rem_ns_xml.'</domain:ns></domain:rem>';
 				$xml = str_replace('<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name>', $xml_domain_rem, $xml);
+			}
+		}
+
+
+		if(
+			isset($_args['new_ns1']) && $_args['new_ns1'] ||
+			isset($_args['new_ns2']) && $_args['new_ns2'] ||
+			isset($_args['new_ns3']) && $_args['new_ns3'] ||
+			isset($_args['new_ns4']) && $_args['new_ns4'] ||
+
+			isset($_args['new_ip1']) && $_args['new_ip1'] ||
+			isset($_args['new_ip2']) && $_args['new_ip2'] ||
+			isset($_args['new_ip3']) && $_args['new_ip3'] ||
+			isset($_args['new_ip4']) && $_args['new_ip4']
+		  )
+		{
+
+			$add_ns_xml = '';
+
+			// -------------------------------------------------------- DNS 1 + IP 1
+			if(isset($_args['new_ns1']) && $_args['new_ns1'])
+			{
+				if(isset($_args['new_ip1']) && $_args['new_ip1'])
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns1']. '</domain:hostName>THIS-POSITION-ADD-IP1</domain:hostAttr>';
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns1']. '</domain:hostName></domain:hostAttr>';
+				}
+			}
+
+			if(isset($_args['new_ip1']) && $_args['new_ip1'])
+			{
+				if(isset($_args['new_ns1']) && $_args['new_ns1'])
+				{
+					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP1', '<domain:hostAddr ip="v4">'. $_args['new_ip1']. '</domain:hostAddr>', $add_ns_xml);
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip1']. '</domain:hostAddr>';
+				}
+			}
+
+			// -------------------------------------------------------- DNS 2 + IP 2
+			if(isset($_args['new_ns2']) && $_args['new_ns2'])
+			{
+				if(isset($_args['new_ip2']) && $_args['new_ip2'])
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns2']. '</domain:hostName>THIS-POSITION-ADD-IP2</domain:hostAttr>';
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns2']. '</domain:hostName></domain:hostAttr>';
+				}
+			}
+
+			if(isset($_args['new_ip2']) && $_args['new_ip2'])
+			{
+				if(isset($_args['new_ns2']) && $_args['new_ns2'])
+				{
+					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP2', '<domain:hostAddr ip="v4">'. $_args['new_ip2']. '</domain:hostAddr>', $add_ns_xml);
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip2']. '</domain:hostAddr>';
+				}
+			}
+
+			// -------------------------------------------------------- DNS 3 + IP 3
+			if(isset($_args['new_ns3']) && $_args['new_ns3'])
+			{
+				if(isset($_args['new_ip3']) && $_args['new_ip3'])
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns3']. '</domain:hostName>THIS-POSITION-ADD-IP3</domain:hostAttr>';
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns3']. '</domain:hostName></domain:hostAttr>';
+				}
+			}
+
+			if(isset($_args['new_ip3']) && $_args['new_ip3'])
+			{
+				if(isset($_args['new_ns3']) && $_args['new_ns3'])
+				{
+					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP3', '<domain:hostAddr ip="v4">'. $_args['new_ip3']. '</domain:hostAddr>', $add_ns_xml);
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip3']. '</domain:hostAddr>';
+				}
+			}
+
+			// -------------------------------------------------------- DNS 4 + IP 4
+			if(isset($_args['new_ns4']) && $_args['new_ns4'])
+			{
+				if(isset($_args['new_ip4']) && $_args['new_ip4'])
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns4']. '</domain:hostName>THIS-POSITION-ADD-IP4</domain:hostAttr>';
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAttr><domain:hostName>'. $_args['new_ns4']. '</domain:hostName></domain:hostAttr>';
+				}
+			}
+
+			if(isset($_args['new_ip4']) && $_args['new_ip4'])
+			{
+				if(isset($_args['new_ns4']) && $_args['new_ns4'])
+				{
+					$add_ns_xml = str_replace('THIS-POSITION-ADD-IP4', '<domain:hostAddr ip="v4">'. $_args['new_ip4']. '</domain:hostAddr>', $add_ns_xml);
+				}
+				else
+				{
+					$add_ns_xml .= '<domain:hostAddr ip="v4">'. $_args['new_ip4']. '</domain:hostAddr>';
+				}
+			}
+
+			if($add_ns_xml)
+			{
+				$xml_domain_add = '<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name><domain:add><domain:ns>'. $add_ns_xml.'</domain:ns></domain:add>';
+				$xml = str_replace('<domain:name>JIBRES-SAMPLE-DOMAIN.IR</domain:name>', $xml_domain_add, $xml);
 			}
 		}
 
