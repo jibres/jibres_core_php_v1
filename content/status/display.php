383904638a08812f36d68985920d6a3067a8ca65
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-02 03:50:30 +0430

    show status page based on domain

--- content/status/display.php
+++ content/status/display.php
@@ -1,7 +1,7 @@
 <div class="jibresBanner">
 
  <div class="fit zero">
-  <iframe class="block" src="https://status.jibres.com" height="1800"></iframe>
+  <iframe class="block" src="https://status.jibres.<?php if(\dash\url::tld() === 'fa') {echo 'ir';} else {echo 'com';} ?>" height="1800"></iframe>
  </div>
 
 </div>
