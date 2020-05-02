fe07150995ac6aaa9e5192c3e04ce99640ae475e
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-02 17:14:50 +0430

    add chambertrust certificate

--- content/certificates/display.php
+++ content/certificates/display.php
@@ -12,47 +12,55 @@ if(\dash\language::current() === 'fa')
    <h2><?php echo T_('Jibres Certificates'); ?></h2>
 
    <div class="f align-center mB20">
-    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://pub.daneshbonyan.ir/"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-daneshbonyan.png" alt='DaneshBonyan Jibres'></a></div>
+    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://pub.daneshbonyan.ir/"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-daneshbonyan.png" alt='جیبرس شرکت دانش بنیان'></a></div>
     <div class="c9 s12"><p>از نخستین روزهای شروع کار، ما بر روی لبه فناوری ایستاده‌ایم و مطابق آخرین روش‌های علمی تکنولوژی‌های پیشگام خلق می‌کنیم. پس از بررسی محصولات و سرویس‌های ما توسط کارگروه ارزیابی و تشخیص صلاحیت شرکت‌ها و موسسات دانش بنیان، در نهایت معاونت علمی و فناوری ریاست جمهوری اسلامی ایران، دانش بنیان بودن شرکت روزاندیش کویرپیما را به‌صورت رسمی از تاریخ ۱۴ اردیبهشت ۱۳۹۸ تایید کرد.</p></div>
    </div>
 
    <div class="f align-center mB20">
-    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bank-markazi.png" alt=''></a></div> -->
+    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bank-markazi.png" alt='جیبرس پرداخت یار رسمی بانک مرکزی'></a></div> -->
    </div>
 
    <div class="f align-center mB20">
-    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-shaparak.png" alt=''></a></div> -->
+    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://shaparak.com/tips/%D8%B4%D8%B1%DA%A9%D8%AA-%D9%87%D8%A7%DB%8C-%D9%BE%D8%B1%D8%AF%D8%A7%D8%AE%D8%AA-%DB%8C%D8%A7%D8%B1"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-shaparak.png" alt='جیبرس دارای مجوز ارائه درگاه پرداخت اینترنتی از شاپرک'></a></div> -->
    </div>
 
    <div class="f align-center mB20 fix">
-    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" href="https://jibres.ir/certificates"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bmn.png" alt=''></a></div>
+    <div class="c3 s12 pRa10"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-bmn.png" alt='جیبرس ساخت نخبگان و متخصصان ایرانی'></div>
     <div class="c9 s12"><p>جیبرس توسط تیمی از نخبگان و مهندسان و متخصصان ایرانی ساخته شده است. ما جیبرس را ساخته‌ایم تا به‌سهم خود ایران را بسازیم. مهم‌ترین ویژگی اعضای تیم جیبرس، مهارت آن‌هاست. </p></div>
    </div>
 
    <div class="f align-center mB20">
-    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="http://qom.irannsr.org/index.php?module=cdk&func=loadmodule&system=cdk&sismodule=user/content_view.php&cnt_id=225820&ctp_id=61&id=17394&sisOp=view"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-nsr.png" alt=''></a></div>
+    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="http://qom.irannsr.org/index.php?module=cdk&func=loadmodule&system=cdk&sismodule=user/content_view.php&cnt_id=225820&ctp_id=61&id=17394&sisOp=view"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-nsr.png" alt='جیبرس عضو نظام صنفی رایانه'></a></div>
     <div class="c9 s12"><p>فعالیت جیبرس تحت عنوان شرکت روز اندیش کویر پیما با مسئولیت محدود در ایران انجام می‌شود. شرکت روزاندیش کویرپیما از تاریخ ۱۷ تیرماه ۱۳۹۶ به عضویت سازمان نظام صنفی رایانه ای کشور درآمده است. مجوز فعالیت شرکت با شماره نظام صنفی رایانه ای ۲۵۰۱۰۱۳۹ طبق قانون حمایت از پدید آوردندگان نرم‌افزارهای رایانه‌ای مصوب ۴/۱۰/۱۳۷۹ مجلس شورای اسلامی و آئین‌نامه اجرایی شماره ۲۱۱۸۵ ت ۲۶۰۸۹ هـ مورخه ۲۴/۴/۱۳۸۳ مصوب دولت جمهوری اسلامی ایران در زمینه فناوری اطلاعات و ارتباطات بوده است.</p></div>
    </div>
 
    <div class="f align-center mB20 fix">
-    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://trustseal.enamad.ir/?id=118387&Code=2iL8ULp5lVA5oSXMRiLp"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-enamad.png" alt=''></a></div>
+    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://trustseal.enamad.ir/?id=118387&Code=2iL8ULp5lVA5oSXMRiLp"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-enamad.png" alt='جیبرس دارای نماد اعتماد الکترونیکی'></a></div>
     <div class="c9 s12"><p>نماد اعتماد الکترونیکی که توسط مرکز توسعه تجارت الکترونیکی در وزارت صنعت، معدن و تجارت صادر می‌شود از تاریخ ۱۸ اسفندماه ۱۳۹۷ توسط جیبرس اخذ شده است تا علاوه‌بر مجوز فعالیت، منجر به افزایش اعتماد مخاطبان جیبرس برای پرداخت‌های اینترنتی گردد.</p></div>
    </div>
 
    <div class="f align-center mB20">
-    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href=""><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-brand.png" alt=''></a></div> -->
+    <!-- <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href=""><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-brand.png" alt='جیبرس برند ثبت شده'></a></div> -->
    </div>
 
    <div class="f align-center">
-    <div class="c3 s12 pRa10"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-samandehi.png" alt='' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=162977&p=rfthgvkauiwkpfvljyoejyoe", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=460, height=600, top=30")'></div>
+    <div class="c3 s12 pRa10"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-samandehi.png" alt='جیبرس دارای نماد ساماندهی' onclick='window.open("https://logo.samandehi.ir/Verify.aspx?id=162977&p=rfthgvkauiwkpfvljyoejyoe", "Popup","toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=460, height=600, top=30")'></div>
     <div class="c9 s12"><p>صلاحیت فعالیت جیبرس از تاریخ ۹ آذرماه ۱۳۹۸ توسط مرکز فناوری اطلاعات و رسانه‌های دیجیتال وزارت فرهنگ و ارشاد اسلامی ایران احراز شده است. این مجوز فعالیت با کد شامد 1-2-709257-65-0-6 برای جیبرس به ثبت رسیده است و بدین ترتیب لوگوی طلایی رسانه برخط به جیبرس اعطا شده است.</p></div>
    </div>
 
 
    <div class="f align-center mB20 fix">
-    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://www.nic.ir/List_of_Resellers"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-irnic.png" alt=''></a></div>
+    <div class="c3 s12 pRa10"><a tabindex='-1' target="_blank" rel='nofollow' href="https://www.nic.ir/List_of_Resellers"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-irnic.png" alt='جیبرس نماینده رسمی ایرنیک'></a></div>
     <div class="c9 s12"><p>جیبرس از تاریخ  ۱۷ فروردین ۱۳۹۹ پس  از عقد قرارداد رسمی به شماره دی۹۹۷ با مرکز ثبت دامنه کشوری ایران،  به  نماینده رسمی  این مرکز تبدیل شد. این مرکز زیر نظر  پژوهشگاه دانش‌های بنیادی فعالیت می‌کند. بدین ترتیب مرکز دامنه‌های جیرس راه‌اندازی شده و قابلیت  خرید دامنه  ایرانی برای عموم  از طریق جیبرس ممکن گشت. هدف ما کمک به بیزینس‌هاست  و  در این راستا تلاش داریم تا تمام نیازهای آن‌ها را مثل دامنه برطرف کنیم. بخش دامنه‌های جیبرس به‌صورت تمام  ای‌پی‌ای راه‌اندازی شده و قابلیت استفاده توسط سایر کسب و کارها را نیز داراست.</p></div>
    </div>
+
+
+
+   <div class="f align-center mB20">
+    <div class="c3 s12 pRa10"><img src="<?php echo \dash\url::cdn(); ?>/img/certificates/jibres-certificate-chambertrust.png" alt='جیبرس دارای کارت بازرگانی'></div>
+    <div class="c9 s12"><p>شرکت روزاندیش کویرپیما با شماره ثبت ۱۳۵۵۲ از تاریخ ۲۴ فروردین ۱۳۹۹ به عضویت حقوقی اتاق  بازرکانی، صنایع، معادن و کشاورزی ایران درآمده است و با شماره کارت  و دارای کارت بازرگانی  هوشمند می‌باشد. بدین ترتیب برخی از موانع پیش رو برای صادرات محصول به خارج از کشور  برای ما برطرف گردیده است.</p></div>
+   </div>
+
 
   </div>
 </section>
