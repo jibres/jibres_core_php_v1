dff969d9ecbe6aa9ba266e8e9a02be2d3d5bddce
Reza <rm.biqarar@gmail.com>
2020-05-02 16:47:38 +0430

    DB: fix import backup local

--- content_su/backup/import/controller.php
+++ content_su/backup/import/controller.php
@@ -51,11 +51,25 @@ class controller
 				\dash\file::append(__DIR__. '/exec.me.sql','mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS \`jibres_nic_log\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"'. "\n");
 				\dash\file::append(__DIR__. '/exec.me.sql','bunzip2 < '.$db_backup_file.' | mysql -uroot -proot jibres_nic_log'. "\n");
 			}
+			elseif(preg_match("/(jibres_shaparak_log_2020)/", $db_backup_file, $split))
+			{
+				$i++;
+				\dash\file::append(__DIR__. '/exec.me.sql','mysql -uroot -proot -e "DROP DATABASE \`jibres_shaparak_log\`"'. "\n");
+				\dash\file::append(__DIR__. '/exec.me.sql','mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS \`jibres_shaparak_log\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"'. "\n");
+				\dash\file::append(__DIR__. '/exec.me.sql','bunzip2 < '.$db_backup_file.' | mysql -uroot -proot jibres_shaparak_log'. "\n");
+			}
+			elseif(preg_match("/(jibres_shaparak_2020)/", $db_backup_file, $split))
+			{
+				$i++;
+				\dash\file::append(__DIR__. '/exec.me.sql','mysql -uroot -proot -e "DROP DATABASE \`jibres_shaparak\`"'. "\n");
+				\dash\file::append(__DIR__. '/exec.me.sql','mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS \`jibres_shaparak\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"'. "\n");
+				\dash\file::append(__DIR__. '/exec.me.sql','bunzip2 < '.$db_backup_file.' | mysql -uroot -proot jibres_shaparak'. "\n");
+			}
 		}
 
 		foreach ($list as $key => $db_backup_file)
 		{
-			if(preg_match("/(jibres\_\d{7})/", $db_backup_file, $split))
+			if(preg_match("/(business\_\d{7})/", $db_backup_file, $split))
 			{
 				$i++;
 				$db_name = $split[1];
