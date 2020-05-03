5db89fd9926abfa6708f40d8907b8c6aad3806ad
Reza <rm.biqarar@gmail.com>
2020-05-03 16:37:02 +0430

    check website setting in app website generator

--- dash/layout/business.php
+++ dash/layout/business.php
@@ -35,7 +35,7 @@ class business
 			return false;
 		}
 
-		$load_website_setting = self::load_website_setting($store_id);
+		$load_website_setting = \lib\app\website\generator::load_website_setting($store_id);
 
 		if(!$load_website_setting)
 		{
@@ -115,63 +115,5 @@ class business
 		return $addr;
 	}
 
-
-
-	/**
-	 * Loads a website setting from file and database
-	 *
-	 * @param      <type>         $_store_id  The store identifier
-	 *
-	 * @return     array|boolean  ( description_of_the_return_value )
-	 */
-	private static function load_website_setting($_store_id)
-	{
-		$addr = \dash\engine\store::website_addr();
-
-		if(!is_dir($addr))
-		{
-			\dash\file::makeDir($addr, null, true);
-		}
-
-		$addr .= $_store_id;
-
-		$website_setting = [];
-
-		if(is_file($addr))
-		{
-			$load = \dash\file::read($addr);
-			$load = json_decode($load, true);
-			if(!is_array($load))
-			{
-				$load = [];
-			}
-
-			$website_setting = $load;
-		}
-
-		if(empty($website_setting))
-		{
-			$load_query = \lib\app\website\template::get();
-			if(!is_array($load_query))
-			{
-				$load_query = [];
-			}
-
-			$load_query['update_time'] = time();
-
-			$website_setting = $load_query;
-
-			\dash\file::write($addr, json_encode($website_setting, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-
-		}
-
-		if(isset($website_setting['template']))
-		{
-			return $website_setting;
-		}
-
-		return false;
-
-	}
 }
 ?>
