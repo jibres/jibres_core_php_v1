3688a8c91e8c36efda7f89a583e372f69bc0288c
Reza <rm.biqarar@gmail.com>
2020-05-05 14:47:44 +0430

    try to load news in subdomain

--- content_subdomain/home/display.php
+++ content_subdomain/home/display.php
@@ -1,20 +1,27 @@
 <?php
-
-if(\dash\data::website_template())
+if(\dash\data::postIsLoaded())
 {
-	$addr = root. 'content_subdomain/home/layout/'. \dash\data::website_template(). '.php';
-	if(is_file($addr))
+	// require_once ('layout/loadpost.php');
+}
+else
+{
+	if(\dash\data::website_template())
 	{
+		$addr = root. 'content_subdomain/home/layout/'. \dash\data::website_template(). '.php';
+		if(is_file($addr))
+		{
 
-		require_once($addr);
+			require_once($addr);
+		}
+		else
+		{
+			require_once ('layout/visitcard.php');
+		}
 	}
 	else
 	{
 		require_once ('layout/visitcard.php');
 	}
-}
-else
-{
-	require_once ('layout/visitcard.php');
+
 }
 ?>
