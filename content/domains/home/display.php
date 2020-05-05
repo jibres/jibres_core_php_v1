47338cc59b8c0ed9398e91400e60db4f8e3ad049
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-06 01:48:48 +0430

    add domain pricing

--- content/domains/home/display.php
+++ content/domains/home/display.php
@@ -52,8 +52,65 @@
    </div>
   </section>
 
-
  </div>
+ <div class="fit">
+  <h3><?php echo T_("Jibres Domain Pricing"); ?></h3>
+  <table class="tbl1 v5">
+    <thead>
+      <tr>
+        <td><?php echo ""; ?></td>
+        <td><?php echo T_(".ir 1 year"); ?></td>
+        <td><?php echo T_(".ir 5 Year"); ?></td>
+        <td><?php echo T_(".com 1 year"); ?></td>
+        <td><?php echo T_("Auto Renew"); ?></td>
+        <td><?php echo T_("Beautiful Panel"); ?></td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?php echo T_("Jibres"); ?></td>
+        <td><?php echo \dash\fit::number("5000"); ?></td>
+        <td><?php echo \dash\fit::number("15000"); ?></td>
+        <td><?php echo T_("Soon"); ?></td>
+        <td>✔️</td>
+        <td>✔️</td>
+      </tr>
+      <tr>
+        <td>ایران سرور</td>
+        <td><?php echo \dash\fit::number("6000"); ?></td>
+        <td><?php echo \dash\fit::number("15000"); ?></td>
+        <td><?php echo \dash\fit::number("149000"); ?></td>
+        <td>❌</td>
+        <td>❌</td>
+      </tr>
+      <tr>
+        <td>پارس پک</td>
+        <td><?php echo \dash\fit::number("7000"); ?></td>
+        <td><?php echo \dash\fit::number("20000"); ?></td>
+        <td><?php echo \dash\fit::number("159000"); ?></td>
+        <td>❌</td>
+        <td>❌</td>
+      </tr>
+      <tr>
+        <td>آذر آنلاین</td>
+        <td><?php echo \dash\fit::number("7900"); ?></td>
+        <td><?php echo \dash\fit::number("29000"); ?></td>
+        <td><?php echo \dash\fit::number("140000"); ?></td>
+        <td>❌</td>
+        <td>❌</td>
+      </tr>
+      <tr>
+        <td>هاست ایران</td>
+        <td><?php echo \dash\fit::number("12000"); ?></td>
+        <td><?php echo \dash\fit::number("36000"); ?></td>
+        <td><?php echo \dash\fit::number("198000"); ?></td>
+        <td>❌</td>
+        <td>❌</td>
+      </tr>
+    </tbody>
+  </table>
+ </div>
+
  <section class="forDevelopers">
   <h3><?php echo T_('Built By Developers, For Developers.'); ?></h3>
   <img class="dreamRider" src='<?php echo \dash\url::cdn() ?>/img/domain/jibres-dream-rider.gif' alt='Jibres Dream Rider'>
