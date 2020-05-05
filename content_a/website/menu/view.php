3129807eefbec8f7528613d438806b89ce5818d2
Reza <rm.biqarar@gmail.com>
2020-05-05 17:52:36 +0430

    add menu link

--- content_a/website/menu/view.php
+++ content_a/website/menu/view.php
@@ -13,6 +13,10 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::this());
 
+		\dash\data::action_text(T_('Add new menu'));
+		\dash\data::action_icon('plus');
+		\dash\data::action_link(\dash\url::this(). '/menu/add');
+
 		$menu_list = \lib\app\website\menu\get::list_all_menu();
 		\dash\data::menuList($menu_list);
 
