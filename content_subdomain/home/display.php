8ff17ce25b32fe87c6a6a36ae72c4aa0e2882ce4
Reza <rm.biqarar@gmail.com>
2020-05-05 15:21:03 +0430

    website: subdomain display.php is not run

--- content_subdomain/home/display.php
+++ content_subdomain/home/display.php
@@ -1,27 +1,5 @@
 <?php
-if(\dash\data::postIsLoaded())
-{
-	// require_once ('layout/loadpost.php');
-}
-else
-{
-	if(\dash\data::website_template())
-	{
-		$addr = root. 'content_subdomain/home/layout/'. \dash\data::website_template(). '.php';
-		if(is_file($addr))
-		{
-
-			require_once($addr);
-		}
-		else
-		{
-			require_once ('layout/visitcard.php');
-		}
-	}
-	else
-	{
-		require_once ('layout/visitcard.php');
-	}
-
-}
+// ------------------------------- //
+// ---- THIS FILE IS NOT RUN! ---- //
+// ------------------------------- //
 ?>
