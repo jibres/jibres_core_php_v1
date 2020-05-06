4d95ed6ebbbd2459f79e91ed4b586107a3a20bb4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-07 02:48:15 +0430

    add wave to keep it simple on homepage

--- content/home/display.php
+++ content/home/display.php
@@ -1,7 +1,4 @@
 
-
-
-
 <section class="jibresBanner" id='saleChannels'>
  <div class="fit">
   <div class="f">
@@ -55,6 +52,13 @@
 </section>
 
 
+<section id="jibresDomain">
+  <div class="cn">
+    <h2><?php echo T_("Jibres Domain Center"); ?></h2>
+
+  </div>
+</section>
+
 
 <section id='keepitsimple'>
  <div class="title">
@@ -62,6 +66,16 @@
   <h3><?php echo T_('Simplicity is the ultimate sophistication'); ?></h3>
   <h3><?php echo T_('No one can fullfill your e-commerce needs like us'); ?> <span>💪</span></h3>
  </div>
+
+ <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+  <defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" /></defs>
+  <g class="parallax">
+   <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.5)" />
+   <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.3)" />
+   <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.1)" />
+   <use xlink:href="#gentle-wave" x="48" y="7" fill="#f7f7f9" />
+  </g>
+ </svg>
 </section>
 
 
