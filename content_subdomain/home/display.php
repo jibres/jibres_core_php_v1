683aa73d63783f4687c1da198bc575aa25a1dc9c
Reza <rm.biqarar@gmail.com>
2020-05-06 12:40:41 +0430

    load default website

--- content_subdomain/home/display.php
+++ content_subdomain/home/display.php
@@ -1,5 +1,19 @@
 <?php
-// ------------------------------- //
-// ---- THIS FILE IS NOT RUN! ---- //
-// ------------------------------- //
+if(\dash\data::website_template())
+{
+	$addr = root. 'content_subdomain/home/layout/'. \dash\data::website_template(). '.php';
+	if(is_file($addr))
+	{
+
+		require_once($addr);
+	}
+	else
+	{
+		require_once ('layout/visitcard.php');
+	}
+}
+else
+{
+	require_once ('layout/visitcard.php');
+}
 ?>
