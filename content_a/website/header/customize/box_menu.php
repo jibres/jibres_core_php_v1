c7a6e673463e73831ef8eac84a68a4c62ccb2526
Reza <rm.biqarar@gmail.com>
2020-05-07 13:18:53 +0430

    Website: some fix on customize header

--- content_a/website/header/customize/box_menu.php
+++ content_a/website/header/customize/box_menu.php
@@ -1,5 +1,3 @@
-
-
 <?php foreach ($box_detail as $key => $boxValue) {?>
   <section class="f" data-option='website-header-upload-logo-<?php echo $key; ?>'>
     <div class="c8 s12">
@@ -12,22 +10,20 @@
     </div>
     <form class="c4 s12" method="post" data-patch>
       <div class="action">
-        <div class="f">
-            <div class="c12">
-              <label for="idmenu<?php echo \dash\get::index($boxValue, 'name'); ?>"><?php echo \dash\get::index($boxValue, 'title'); ?></label>
-              <select name="<?php echo \dash\get::index($boxValue, 'name'); ?>" id="idmenu<?php echo \dash\get::index($boxValue, 'name'); ?>" class="select22">
-                <?php if(\dash\get::index($header_detail, 'saved', \dash\get::index($boxValue, 'name'))) {?>
-                  <option value="0"><?php echo T_("Without menu"); ?></option>
-                <?php }else{ ?>
-                  <option></option>
-                <?php } //endif ?>
-                <?php foreach (\dash\data::allMenu() as $key => $value) {?>
-                  <option value="<?php echo \dash\get::index($value, 'key'); ?>" <?php if(\dash\get::index($header_detail, 'saved', \dash\get::index($boxValue, 'name')) == \dash\get::index($value, 'key')) { echo 'selected';} ?>><?php echo \dash\get::index($value, 'title'); ?></option>
-                <?php } //endfor ?>
-              </select>
-            </div>
+        <div>
+          <label for="idmenu<?php echo \dash\get::index($boxValue, 'name'); ?>"><?php echo \dash\get::index($boxValue, 'title'); ?></label>
+          <select name="<?php echo \dash\get::index($boxValue, 'name'); ?>" id="idmenu<?php echo \dash\get::index($boxValue, 'name'); ?>" class="select22">
+            <?php if(\dash\get::index($header_detail, 'saved', \dash\get::index($boxValue, 'name'))) {?>
+              <option value="0"><?php echo T_("Without menu"); ?></option>
+            <?php }else{ ?>
+              <option></option>
+            <?php } //endif ?>
+            <?php foreach (\dash\data::allMenu() as $key => $value) {?>
+              <option value="<?php echo \dash\get::index($value, 'key'); ?>" <?php if(\dash\get::index($header_detail, 'saved', \dash\get::index($boxValue, 'name')) == \dash\get::index($value, 'key')) { echo 'selected';} ?>><?php echo \dash\get::index($value, 'title'); ?></option>
+            <?php } //endfor ?>
+          </select>
         </div>
       </div>
     </form>
   </section>
-<?php } //endfor ?>
+  <?php } //endfor ?>
