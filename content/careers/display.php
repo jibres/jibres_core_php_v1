82ccc7fddd32039d407fe73f2211c487ad51dc8a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-07 16:44:11 +0430

    bugfix status and careers

--- content/careers/display.php
+++ content/careers/display.php
@@ -1,6 +1,6 @@
 <div class="jibresBanner">
 
- <div class="avand-lg zero">
+ <div class="avand zero">
   <iframe class="block" src="https://sarshomar.com/fa/s/3L2fP" height="600"></iframe>
  </div>
 
--- content/status/display.php
+++ content/status/display.php
@@ -1,6 +1,6 @@
 <div class="jibresBanner">
 
- <div class="avand-lg zero">
+ <div class="avand zero">
   <iframe class="block" src="https://status.jibres.<?php if(\dash\url::tld() === 'fa') {echo 'ir';} else {echo 'com';} ?>" height="1800"></iframe>
  </div>
 
