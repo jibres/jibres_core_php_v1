76196a214b78730e6d3623a26a53fdf6e13f1957
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-08 17:22:27 +0430

    fix header

--- content_subdomain/home/layout/header/header_1.php
+++ content_subdomain/home/layout/header/header_1.php
@@ -1,21 +1,25 @@
 <?php require_once('before_header.php'); ?>
-<div class="jHeader100 circleEffect avand f align-center">
-  <div class="cauto pRa10">
-    <a class="logo" href="">
-      <img src="<?php echo \lib\filepath::fix(\dash\get::index(\dash\data::website(), 'header_customized', 'header_logo')) ?>" alt="">
-    </a>
+<div id='jHeader100' class="avand" data-circleEffect>
+  <div class="shine f align-center">
+    <div class="cauto pRa10">
+      <a class="logo" href="">
+        <img src="<?php echo \lib\filepath::fix(\dash\get::index(\dash\data::website(), 'header_customized', 'header_logo')) ?>" alt="">
+      </a>
+    </div>
+    <div class="c">
+    </div>
+    <div class="cauto pRa20">
+      <a class="search" href=""></a>
+    </div>
+    <div class="cauto pRa20">
+      <a class="cart" href="" data-count="۲">سبد خرید</a>
+    </div>
+    <div class="cauto">
+      <a class="enter" href="">ورود به حساب کاربری</a>
+    </div>
   </div>
-  <div class="c">
-  </div>
-  <div class="cauto pRa20">
-    <a class="search" href=""></a>
-  </div>
-  <div class="cauto pRa20">
-    <a class="cart" href="" data-count="۲">سبد خرید</a>
-  </div>
-  <div class="cauto">
-    <a class="enter" href="">ورود به حساب کاربری</a>
-  </div>
-</div>
 
+  <nav class="shine f align-center">
 <?php \lib\app\website\menu\generate::menu('header_menu_1'); ?>
+  </nav>
+</div>
