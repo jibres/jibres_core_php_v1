c929afd437be8b20970708d8b15739ef85c02b48
Reza <rm.biqarar@gmail.com>
2020-05-08 17:29:07 +0430

    Website: load toplink

--- content_subdomain/home/layout/header/before_header.php
+++ content_subdomain/home/layout/header/before_header.php
@@ -1,9 +1,33 @@
 <?php
-if(\dash\user::id())
+$header = \dash\data::website_header();
+
+if(\dash\get::index($header, 'topline', 'text'))
 {
-	if(\dash\permission::check('websiteManager'))
+	echo '<div class="topLine">';
+	if(\dash\get::index($header, 'topline', 'url'))
 	{
-		echo '<div class="topLine"><a target="_blank" href="'.\dash\url::sitelang(). '/'.\lib\store::code().'/a/website">'. T_("Jibres").'</a></div>';
+		echo '<a ';
+		if(\dash\get::index($header, 'topline', 'target'))
+		{
+			echo 'target="_blank" ';
+		}
+		echo 'href="'. \dash\get::index($header, 'topline', 'url'). '" >';
 	}
+	echo \dash\get::index($header, 'topline', 'text');
+
+	if(\dash\get::index($header, 'topline', 'url'))
+	{
+		echo '</a>';
+	}
+
+	echo '</div>';
 }
+
+// if(\dash\user::id())
+// {
+// 	if(\dash\permission::check('websiteManager'))
+// 	{
+// 		echo '<div class="topLine"><a target="_blank" href="'.\dash\url::sitelang(). '/'.\lib\store::code().'/a/website">'. T_("Jibres").'</a></div>';
+// 	}
+// }
 ?>
