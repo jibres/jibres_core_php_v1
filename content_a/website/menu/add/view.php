e4bf554737f2cd2659c89c02e92977cf409dc8a5
Reza <rm.biqarar@gmail.com>
2020-05-09 16:03:33 +0430

    Website: Bug fix link of back in menu add

--- content_a/website/menu/add/view.php
+++ content_a/website/menu/add/view.php
@@ -13,6 +13,13 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::this(). '/menu');
 
+		// if have not any menu must back to dashboard
+		$menu_list = \lib\app\website\menu\get::list_all_menu();
+		if(!$menu_list)
+		{
+			\dash\data::back_link(\dash\url::this());
+		}
+
 	}
 }
 ?>
