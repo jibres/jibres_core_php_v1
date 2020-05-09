86d0fdecf38545e6084ec7f9475113c40472001c
Reza <rm.biqarar@gmail.com>
2020-05-09 17:26:44 +0430

    Website: show line title

--- content_a/website/body/display.php
+++ content_a/website/body/display.php
@@ -9,7 +9,22 @@
         <a href="<?php echo \dash\url::that(). '/'. \dash\get::index($value,'key') .'?key='. \dash\get::index($value, 'saved_detail', 'line_key'); ?>" class="f">
           <div class="key">
             <div class="f">
-              <?php echo \dash\get::index($value, 'title')?>
+              <div class="c2">
+                <?php echo \dash\get::index($value, 'title')?>
+              </div>
+              <div class="c1">
+                <?php if(\dash\get::index($value, 'saved_detail', 'publish')) {?>
+                  <i title="<?php echo T_("Published") ?>" class="sf-check fc-green"></i>
+                <?php }else{ ?>
+                  <i title="<?php echo T_("Draft") ?>" class="sf-refresh fc-red"></i>
+                <?php } //endif ?>
+              </div>
+              <div class="c">
+                <?php if(\dash\get::index($value, 'saved_detail', 'title')) {?>
+                  <?php echo \dash\get::index($value, 'saved_detail', 'title') ?>
+                <?php } //endif ?>
+              </div>
+
             </div>
           </div>
           <div class="go"></div>
