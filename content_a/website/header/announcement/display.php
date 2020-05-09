27c502677c130227e4db89581901d50d4398c5a2
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-09 17:34:08 +0430

    use avand

--- content_a/website/header/announcement/display.php
+++ content_a/website/header/announcement/display.php
@@ -1,42 +1,39 @@
 
-<div class="f justify-center">
-  <div class="c6 m8 s12 x4">
-    <form method="post" class="box" autocomplete="off">
-      <header><h2><?php echo T_("Set website top line"); ?></h2></header>
+<div class="avand-sm">
+  <form method="post" class="box" autocomplete="off">
+    <div class="body">
 
-      <div class="body">
+        <div class="switch1 mB5">
+          <input type="checkbox" name="status" id="status" <?php if(\dash\data::toplineSaved_status()) {echo 'checked';} ?>>
+          <label for="status"></label>
+          <label for="status"><?php echo T_("Enable website topline"); ?><small></small></label>
+        </div>
+
+        <div data-response='status' data-response-effect='slide' <?php if(!\dash\data::toplineSaved_status()) {echo 'data-response-hide';} ?>>
+
+
+          <label for="text"><?php echo T_("Text"); ?></label>
+          <div class="input">
+            <input type="text" name="text" id="text" value="<?php echo \dash\data::toplineSaved_text() ?>" maxlength="100" required>
+          </div>
+
+          <label for="url"><?php echo T_("Url"); ?></label>
+          <div class="input">
+            <input type="url" name="url" id="url" value="<?php echo \dash\data::toplineSaved_url() ?>"  >
+          </div>
 
           <div class="switch1 mB5">
-            <input type="checkbox" name="status" id="status" <?php if(\dash\data::toplineSaved_status()) {echo 'checked';} ?>>
-            <label for="status"></label>
-            <label for="status"><?php echo T_("Enable website topline"); ?><small></small></label>
+            <input type="checkbox" name="target" id="target" <?php if(\dash\data::toplineSaved_target()) {echo 'checked';} ?>>
+            <label for="target"></label>
+            <label for="target"><?php echo T_("Open in New tab"); ?><small></small></label>
           </div>
+        </div>
 
-          <div data-response='status' data-response-effect='slide' <?php if(!\dash\data::toplineSaved_status()) {echo 'data-response-hide';} ?>>
+    </div>
 
+    <footer class="txtRa">
+      <button class="btn primary"><?php echo T_("Save"); ?></button>
+    </footer>
+  </form>
 
-            <label for="text"><?php echo T_("Text"); ?></label>
-            <div class="input">
-              <input type="text" name="text" id="text" value="<?php echo \dash\data::toplineSaved_text() ?>" maxlength="100" required>
-            </div>
-
-            <label for="url"><?php echo T_("Url"); ?></label>
-            <div class="input">
-              <input type="url" name="url" id="url" value="<?php echo \dash\data::toplineSaved_url() ?>"  >
-            </div>
-
-            <div class="switch1 mB5">
-              <input type="checkbox" name="target" id="target" <?php if(\dash\data::toplineSaved_target()) {echo 'checked';} ?>>
-              <label for="target"></label>
-              <label for="target"><?php echo T_("Open in New tab"); ?><small></small></label>
-            </div>
-          </div>
-
-      </div>
-
-      <footer class="txtRa">
-        <button class="btn primary"><?php echo T_("Save"); ?></button>
-      </footer>
-    </form>
-  </div>
 </div>
