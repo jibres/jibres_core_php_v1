b60e72a4e0753a0042232ea66ea201a1edd985ad
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-10 18:34:56 +0430

    add some other block

--- content_a/website/body/add/display.php
+++ content_a/website/body/add/display.php
@@ -12,7 +12,74 @@
   </div>
   <div class="c4 s12">
     <div class="action">
-        <a class="btn primary" href="<?php echo \dash\url::that();?>/slider"><?php echo T_("Add Slider") ?></a>
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/slider"><?php echo T_("Add Slider Block") ?></a>
+    </div>
+  </div>
+</section>
+
+
+<section class="f" data-option='website-block-image'>
+  <div class="c8 s12">
+    <div class="data">
+      <h3><?php echo T_("Image Block");?></h3>
+      <div class="body">
+        <p><?php echo T_("Add an image block with a link to somewhere. You can use a beautiful image to engage your customers. for example a special offer."); ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="c4 s12">
+    <div class="action">
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/image1"><?php echo T_("Add One Image Block") ?></a>
+    </div>
+  </div>
+</section>
+
+<section class="f" data-option='website-block-productline'>
+  <div class="c8 s12">
+    <div class="data">
+      <h3><?php echo T_("Product Line");?></h3>
+      <div class="body">
+        <p><?php echo T_("A block to show products like random products, products of special category, popular products, etc."); ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="c4 s12">
+    <div class="action">
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/latestbews"><?php echo T_("Add Products Block") ?></a>
+    </div>
+  </div>
+</section>
+
+
+<section class="f" data-option='website-block-quote'>
+  <div class="c8 s12">
+    <div class="data">
+      <h3><?php echo T_("Quote");?></h3>
+      <div class="body">
+        <p><?php echo T_("Show quote of some customers to engage your audience with beautiful design."); ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="c4 s12">
+    <div class="action">
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/quote"><?php echo T_("Add Quote Block") ?></a>
+    </div>
+  </div>
+</section>
+
+
+<section class="f" data-option='website-block-text'>
+  <div class="c8 s12">
+    <div class="data">
+      <h3><?php echo T_("Text");?></h3>
+      <div class="body">
+        <p><?php echo T_("Add simple text!"); ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="c4 s12">
+    <div class="action">
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/text"><?php echo T_("Add Text Block") ?></a>
     </div>
   </div>
 </section>
@@ -29,7 +96,7 @@
   </div>
   <div class="c4 s12">
     <div class="action">
-        <a class="btn primary" href="<?php echo \dash\url::that();?>/latestbews"><?php echo T_("Add Latest News") ?></a>
+        <a class="btn primary block" href="<?php echo \dash\url::that();?>/latestbews"><?php echo T_("Add Latest News Block") ?></a>
     </div>
   </div>
 </section>
