8d0aa0a476dafee9d8df7e09955d8e32eb6bb0ed
Reza <rm.biqarar@gmail.com>
2020-05-13 14:33:37 +0430

    Website: bug fix back link

--- content_a/website/body/quote/view.php
+++ content_a/website/body/quote/view.php
@@ -9,7 +9,7 @@ class view
 		\dash\face::title(T_('Manage quote'));
 
 		// back
-		\dash\data::back_quote(T_('Back'));
+		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::this(). '/body');
 
 	}
