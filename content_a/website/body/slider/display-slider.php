ed6ab0e96563203e59c9a57a13a954d1fc1fb80e
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-19 13:24:39 +0430

    use new grid row

--- content_a/website/body/slider/display-slider.php
+++ content_a/website/body/slider/display-slider.php
@@ -1,9 +1,10 @@
-<div class="avand">
 
-  <div class="large f">
+  <div class="row">
     <?php if(\dash\data::lineSetting_slider() && is_array(\dash\data::lineSetting_slider())) {?>
       <?php foreach (\dash\data::lineSetting_slider() as $key => $value) {?>
-        <div class="vcard mA10 <?php if(\dash\request::get('index') == $key) {echo 'active';} ?>">
+      <div class="c-3 c-xs-12">
+
+        <div class="vcard">
           <img  src="<?php echo \dash\get::index($value, 'image') ?>" alt="<?php echo \dash\get::index($value, 'alt') ?>" >
           <div class="content">
             <div class="header"><?php echo \dash\get::index($value, 'url'); ?></div>
@@ -13,17 +14,19 @@
             <a href="<?php echo \dash\url::that(). '/slider/edit?id='. \dash\request::get('id'). '&index='. $key; ?>" class="btn primary outline block"><?php echo T_("Edit") ?></a>
           </div>
         </div>
+      </div>
       <?php } // endfor ?>
     <?php } //endif ?>
-    <div class="vcard mA10">
-      <img  src="<?php echo \dash\url::icon() ?>" alt="<?php echo T_("Add slider page") ?>" >
-      <div class="content">
-        <div class="header"><?php echo T_("Add new slider page"); ?></div>
-      </div>
-      <div class="footer">
-        <a href="<?php echo \dash\url::that(). '/slider/add?id='. \dash\request::get('id'); ?>" class="btn primary outline block"><?php echo T_("Add") ?></a>
+    <div class="c-3 c-xs-12">
+      <div class="vcard">
+        <img  src="<?php echo \dash\url::icon() ?>" alt="<?php echo T_("Add slider page") ?>" >
+        <div class="content">
+          <div class="header"><?php echo T_("Add new slider page"); ?></div>
+        </div>
+        <div class="footer">
+          <a href="<?php echo \dash\url::that(). '/slider/add?id='. \dash\request::get('id'); ?>" class="btn primary outline block"><?php echo T_("Add") ?></a>
+        </div>
       </div>
     </div>
 
   </div>
-</div>
