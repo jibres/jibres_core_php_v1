ba40baf1ad78500c86f5cf4a093abbc58f960de1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-19 10:39:49 +0430

    fix dashboard title

--- content_a/home/view.php
+++ content_a/home/view.php
@@ -6,7 +6,8 @@ class view
 {
 	public static function config()
 	{
-		\dash\face::title(T_("Dashboard of your store"));
+		\dash\face::title(T_("Dashboard of :store", ['store' => \dash\face::site()]));
+
 		\dash\face::desc(T_('Glance at your store summary and compare some important data together and enjoy Jibres!'). ' '. T_('Have a good day;)'));
 
 		\dash\data::dashboardData(\lib\app\cache\get::admin_dashboard());
