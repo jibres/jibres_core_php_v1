b05c5fbbbe5a341898f9a72a7dbe21bfb1115b2f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-19 14:16:35 +0430

    use card

--- content_a/website/body/slider/display-slider.php
+++ content_a/website/body/slider/display-slider.php
@@ -4,15 +4,15 @@
       <?php foreach (\dash\data::lineSetting_slider() as $key => $value) {?>
       <div class="c-3 c-xs-12">
 
-        <div class="vcard">
+        <div class="card">
           <img  src="<?php echo \dash\get::index($value, 'image') ?>" alt="<?php echo \dash\get::index($value, 'alt') ?>" >
-          <div class="content">
-            <div class="header"><?php echo \dash\get::index($value, 'url'); ?></div>
+          <div class="body">
+            <header><?php echo \dash\get::index($value, 'url'); ?></header>
             <div class="meta"><span><?php if(\dash\get::index($value, 'target')) {?><i class="sf-external-link"></i><?php }// endif ?></span></div>
           </div>
-          <div class="footer">
+          <footer>
             <a href="<?php echo \dash\get::index($value, 'edit_link'); ?>" class="btn primary outline block"><?php if(\dash\get::index($value, 'mod') === 'add') { echo T_("Add new page"); }else{ echo T_("Edit"); } ?></a>
-          </div>
+          </footer>
         </div>
       </div>
       <?php } // endfor ?>
