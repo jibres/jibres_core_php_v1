7ab447377c9661f5c0b334b91562ab310f56019b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-21 09:19:48 +0430

    allow blob img

--- dash/engine/guard.php
+++ dash/engine/guard.php
@@ -74,7 +74,7 @@ class guard
 		$csp .= "style-src ". self::csp_domain('cdn'). " 'unsafe-inline'; ";
 		// $csp .= "style-src ". self::csp_domain('cdn'). "; ";
 		// img-src
-		$csp .= "img-src ". self::csp_domain(). " *.google-analytics.com *.doubleclick.net data:; ";
+		$csp .= "img-src ". self::csp_domain(). " *.google-analytics.com *.doubleclick.net blob: data:; ";
 		// font-src
 		$csp .= "font-src ". self::csp_domain('cdn'). " data:; ";
 		// media-src
