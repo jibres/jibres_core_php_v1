0488e8e892d4235af338956d12b31da1ce23bfbd
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-21 11:00:38 +0430

    add multiple

--- content_a/website/slider/add/display.php
+++ content_a/website/slider/add/display.php
@@ -5,7 +5,7 @@
     <form method="post" class="box" autocomplete="off" >
 
       <div data-uploader class="min-y120">
-        <input type="file" data-cropper-file name='image' accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2 >
+        <input type="file" data-cropper-file name='image' accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2 multiple data-multiple-caption="{count} files selected">
         <label for="image1"><?php echo T_('Drag &amp; Drop your files or <span class="txtB">Browse</span>'); ?></label>
       </div>
 
@@ -56,7 +56,7 @@
 
 
 <div class="mB50" style="display: block;max-width: 100%;">
-  <img id="image" data-cropper src="<?php echo \dash\data::dataRow_image() ?>" alt='<?php echo \dash\data::dataRow_alt() ?>'>
+  <img id="image" data-cropper2 src="<?php echo \dash\data::dataRow_image() ?>" alt='<?php echo \dash\data::dataRow_alt() ?>'>
 </div>
 
 </div>
