30121b240c1144caa3a3997e790b43ca19fcaef1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-21 18:04:34 +0430

    save

--- content_a/website/slider/add/display.php
+++ content_a/website/slider/add/display.php
@@ -4,8 +4,8 @@
 
   <form method="post" autocomplete="off" >
 
-    <div data-uploader class="box min-y120" <?php echo \dash\data::ratioHtml(); ?>>
-      <input type="file" data-cropper-file name='image' accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2 multiple data-multiple-caption="{count} files selected">
+    <div class="box min-y120" data-uploader data-name='image123' <?php echo \dash\data::ratioHtml(); ?>>
+      <input type="file" data-cropper-file accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2 multiple data-multiple-caption="{count} files selected">
       <label for="image1"><?php echo T_('Drag &amp; Drop your files or <span class="txtB">Browse</span>'); ?></label>
     </div>
 
