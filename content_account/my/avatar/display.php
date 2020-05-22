96c438f2ff3417c032ba044d146cba71d7358ffd
Reza <rm.biqarar@gmail.com>
2020-05-22 16:27:40 +0430

    Account: try to convert avatar uploader

--- content_account/my/avatar/display.php
+++ content_account/my/avatar/display.php
@@ -1,38 +1,15 @@
 
-<form method="post" enctype="multipart/form-data" autocomplete="off">
-  <div class="f justify-center txtC">
-    <div class="c6 m8 x5 s12">
+<div class="avand-sm">
 
+  <img id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
 
+  <form method="post" autocomplete="off" enctype="multipart/form-data">
 
-      <div class="cbox">
-        <h3 class="txtC mB20"><?php echo T_("Avatar"); ?></h3>
-
-        <?php if(\dash\data::dataRow_avatar()) {?>
-          <img src="<?php echo \dash\data::dataRow_avatar(); ?>" class="box200">
-        <?php }//endif ?>
-
-        <div class="input mT10">
-         <input type="file" accept="image/gif, image/jpeg, image/png" name="avatar" id="avatar1"  data-max="500">
-         <label for="avatar1" content_account\my>
-         </label>
-        </div>
-
-        <div class="f align-center">
-          <?php if(\dash\data::dataRow_avatar()) {?>
-          <div class="cauto">
-            <div class="btn danger xs"  data-confirm data-method='post' data-data='{"remove" : "avatar"}'><?php echo T_("Remove"); ?> <?php echo T_("Your avatar"); ?></div>
-          </div>
-          <?php } //endif ?>
-          <div class="c"></div>
-          <div class="cauto">
-            <button class="btn success"><?php echo T_("Save"); ?></button>
-          </div>
-        </div>
-
-      </div>
-
-
+    <div class="box min-y120" data-uploader data-name='avatar' >
+      <input type="file" data-cropper-file accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2 multiple data-multiple-caption="{count} files selected">
+      <label for="image1"><?php echo T_('Drag &amp; Drop your files or <span class="txtB">Browse</span>'); ?></label>
     </div>
-  </div>
-</form>
+
+  </form>
+
+</div>
