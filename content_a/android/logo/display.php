ba69e915c31e1f13efa5f5bc84ef78e14d8748ef
Reza <rm.biqarar@gmail.com>
2020-05-22 17:23:39 +0430

    Android logo auto send

--- content_a/android/logo/display.php
+++ content_a/android/logo/display.php
@@ -5,22 +5,21 @@
       <header><h2><?php echo T_("Set your application logo");?></h2></header>
       <div class="body">
 
-        <div class="box min-y120" data-uploader data-name='logo' data-ratio="1" data-final='#finalImage'>
+        <div class="box min-y120" data-uploader data-name='logo' data-ratio="1" data-final='#finalImage' data-autoSend>
           <input type="file" accept="image/jpeg, image/png" id="image1">
           <label for="image1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?> (<?php echo T_("Use a square logo"); ?>)</label>
         </div>
 
       </div>
-      <footer class="f">
-        <?php if(\dash\data::canUseStoreLogo()) {?>
+
+      <?php if(\dash\data::canUseStoreLogo()) {?>
+        <footer class="f">
           <div class="c">
             <div class="btn" data-ajaxify data-data='{"usestorelogo": 1}' data-method='post' ><span><?php echo T_("Use from store logo"); ?></span></div>
           </div>
-        <?php } //endif ?>
-        <div class="cauto os"><button class="btn success"><?php echo \dash\data::nextBtn(); ?></button></div>
+        </footer>
+      <?php } //endif ?>
 
-
-      </footer>
     </form>
   </div>
   <div class="c6 s12">
