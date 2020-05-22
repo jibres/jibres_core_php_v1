e7bf2b0d9f3fbd7846104e59a34759cd9dfb604f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-22 17:14:28 +0430

    add data-autosend

--- content_account/my/avatar/display.php
+++ content_account/my/avatar/display.php
@@ -4,13 +4,13 @@
 
   <form method="post" autocomplete="off" enctype="multipart/form-data">
 
-  	<div class="box min-y120" data-uploader data-name='avatar' data-ratio="1" data-final='#finalImage' data-preview-circle>
+  	<div class="box min-y120" data-uploader data-name='avatar' data-ratio="1" data-final='#finalImage' data-preview-circle data-autoSend>
       <input type="file" accept="image/jpeg, image/png" id="image1">
       <label for="image1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
     </div>
   </form>
 
   <div class="box">
-  	<img id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
+  	<img class="block" id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
   </div>
 </div>
