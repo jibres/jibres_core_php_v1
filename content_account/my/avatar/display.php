07d4dde9bc4b9c7e5c69f5cc9ab2c2541b3b7f06
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-22 17:29:07 +0430

    fix avatar

--- content_account/my/avatar/display.php
+++ content_account/my/avatar/display.php
@@ -1,16 +1,16 @@
 
 <div class="avand-sm">
 
+  <div class="mB50">
+  	<img class="profileAvatar" id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
+  </div>
 
   <form method="post" autocomplete="off" enctype="multipart/form-data">
-
   	<div class="box min-y120" data-uploader data-name='avatar' data-ratio="1" data-final='#finalImage' data-preview-circle data-autoSend>
       <input type="file" accept="image/jpeg, image/png" id="image1">
       <label for="image1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
     </div>
   </form>
 
-  <div class="mTB25">
-  	<img class="circleAvatar" id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
-  </div>
 </div>
+
