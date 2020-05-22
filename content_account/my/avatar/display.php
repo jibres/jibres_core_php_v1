a1b8bdcbfa42f8a8f8a18b74381728a791ab6a5d
Reza <rm.biqarar@gmail.com>
2020-05-22 16:54:27 +0430

    fix avatar

--- content_account/my/avatar/display.php
+++ content_account/my/avatar/display.php
@@ -5,10 +5,11 @@
 
   <form method="post" autocomplete="off" enctype="multipart/form-data">
 
-    <div class="box min-y120" data-uploader data-name='avatar' >
-      <input type="file" data-cropper-file accept="image/gif, image/jpeg, image/png" id="image1" data-max-files data-max-size=2>
-      <label for="image1"><?php echo T_('Drag &amp; Drop your files or <span class="txtB">Browse</span>'); ?></label>
+  	<div class="box min-y120" data-uploader data-name='avatar' data-ratio="1">
+      <input type="file" data-cropper-file accept="image/jpeg, image/png" id="image1" data-max-size=1 >
+      <label for="image1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
     </div>
+
 
   </form>
 
