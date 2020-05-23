ac0d6dbcea2456de0087562ec76b84e843ad2986
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-23 11:21:21 +0430

    show image inside avatar box

--- content_account/my/avatar/display.php
+++ content_account/my/avatar/display.php
@@ -3,14 +3,14 @@
 
   <form method="post" autocomplete="off" enctype="multipart/form-data">
 
-  	<?php if(\dash\data::dataRow_avatar()) {?>
-      <img class="profileAvatar" id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
-  	<?php }?>
 
   	<div data-uploader data-name='avatar' data-ratio="1" data-final='#finalImage' data-preview-circle data-autoSend data-uploader-circle>
       <input type="file" accept="image/jpeg, image/png" id="image1">
       <label for="image1"><?php echo T_('Drag &amp; Drop your picture or Browse'); ?></label>
 
+  	<?php if(\dash\data::dataRow_avatar()) {?>
+      <img id='finalImage' src="<?php echo \dash\data::dataRow_avatar() ?>" alt='<?php echo T_("Your avatar") ?>'>
+  	<?php }?>
     </div>
   </form>
 
