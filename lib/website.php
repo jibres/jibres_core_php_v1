09a394993c0a9a63210b512d029d51af38701a0b
Reza <rm.biqarar@gmail.com>
2020-05-23 17:03:08 +0430

    Website: fix load logo on header

--- lib/website.php
+++ lib/website.php
@@ -7,7 +7,7 @@ class website
 
 	public static function logo()
 	{
-		$logo = \dash\get::index(\dash\data::website(), 'header_customized', 'header_logo');
+		$logo = \dash\get::index(\dash\data::website(), 'header', 'header_logo');
 		if($logo)
 		{
 			return \lib\filepath::fix($logo);
