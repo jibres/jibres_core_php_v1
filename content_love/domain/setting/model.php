25dca1ced7310311bb2215416ae37b698117c2da
Reza <rm.biqarar@gmail.com>
2020-05-23 14:24:18 +0430

    Domain: remove domain fetch

--- content_love/domain/setting/model.php
+++ content_love/domain/setting/model.php
@@ -1,5 +1,5 @@
 <?php
-namespace content_love\domain\all\detail;
+namespace content_love\domain\setting;
 
 
 class model
@@ -14,46 +14,48 @@ class model
 			return;
 		}
 
+		\dash\notif::warn("This action needs to work!");
+		return;
 
-		if(\dash\request::post('myaction') == 'autorenew')
-		{
-			$autorenew = \dash\request::post('op') == 'set' ? 1 : 0;
+		// if(\dash\request::post('myaction') == 'autorenew')
+		// {
+		// 	$autorenew = \dash\request::post('op') == 'set' ? 1 : 0;
 
-			if(\lib\nic\mode::api())
-			{
-				$get_api     = new \lib\nic\api();
-				$load_domain = $get_api->domain_autorenew(\dash\data::domainDetail_id(), $autorenew);
-			}
-			else
-			{
-				$result = \lib\app\nic_domain\edit::edit(['autorenew' => $autorenew], \dash\data::domainDetail_id());
-			}
+		// 	if(\lib\nic\mode::api())
+		// 	{
+		// 		$get_api     = new \lib\nic\api();
+		// 		$load_domain = $get_api->domain_autorenew(\dash\data::domainDetail_id(), $autorenew);
+		// 	}
+		// 	else
+		// 	{
+		// 		$result = \lib\app\nic_domain\edit::edit(['autorenew' => $autorenew], \dash\data::domainDetail_id());
+		// 	}
 
-			if(\dash\engine\process::status())
-			{
-				\dash\redirect::pwd();
-			}
-			return;
-		}
+		// 	if(\dash\engine\process::status())
+		// 	{
+		// 		\dash\redirect::pwd();
+		// 	}
+		// 	return;
+		// }
 
-		if(\dash\request::post('status') == 'remove')
-		{
-			if(\lib\nic\mode::api())
-			{
-				$get_api     = new \lib\nic\api();
-				$load_domain = $get_api->domain_delete(\dash\data::domainDetail_id());
-			}
-			else
-			{
-				$result = \lib\app\nic_domain\remove::remove(\dash\data::domainDetail_id());
-			}
+		// if(\dash\request::post('status') == 'remove')
+		// {
+		// 	if(\lib\nic\mode::api())
+		// 	{
+		// 		$get_api     = new \lib\nic\api();
+		// 		$load_domain = $get_api->domain_delete(\dash\data::domainDetail_id());
+		// 	}
+		// 	else
+		// 	{
+		// 		$result = \lib\app\nic_domain\remove::remove(\dash\data::domainDetail_id());
+		// 	}
 
-			if(\dash\engine\process::status())
-			{
-				\dash\redirect::to(\dash\url::this());
-			}
-			return;
-		}
+		// 	if(\dash\engine\process::status())
+		// 	{
+		// 		\dash\redirect::to(\dash\url::this());
+		// 	}
+		// 	return;
+		// }
 	}
 }
 ?>
