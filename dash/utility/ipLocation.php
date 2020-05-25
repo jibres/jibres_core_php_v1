3b20d046140f234021499b7b7d6488deabec2e5f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-05-25 13:22:12 +0430

    fix connection problem

--- dash/utility/ipLocation.php
+++ dash/utility/ipLocation.php
@@ -50,7 +50,7 @@ class ipLocation
 		// api url
 		$url = 'http://api.db-ip.com/v2/free/'. $_ip;
 		// get content
-		$result = @file_get_contents($url);
+		$result = self::curl_get_contents($url);
 		// try to decode
 		$jsonResult = json_decode($result, true);
 		// return
@@ -64,13 +64,35 @@ class ipLocation
 		// api url
 		$url = 'https://api.ipgeolocation.io/ipgeo?apiKey='. $apiKey.'&ip='. $_ip;
 		// get content
-		$result = @file_get_contents($url);
+		$result = self::curl_get_contents($url);
 		// try to decode
+		var_dump($result);
 		$jsonResult = json_decode($result, true);
+		var_dump($jsonResult);
+		// remove flag url!
+		if(isset($jsonResult['country_flag']))
+		{
+			unset($jsonResult['country_flag']);
+		}
 		// return
 		return $jsonResult;
 	}
 
+
+
+	private static function curl_get_contents($url)
+	{
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+		$data = curl_exec($curl);
+		curl_close($curl);
+		return $data;
+	}
 
 }
 ?>
