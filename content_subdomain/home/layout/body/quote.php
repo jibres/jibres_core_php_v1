a25f81350507abd9488283d6d40633a41dc6f76c
Reza <rm.biqarar@gmail.com>
2020-05-25 08:02:31 +0430

    Website: quote, load quote in website

--- content_subdomain/home/layout/body/quote.php
+++ content_subdomain/home/layout/body/quote.php
@@ -6,13 +6,19 @@ if(isset($line_detail['value']['quote']) && is_array($line_detail['value']['quot
 {
 	$quote = $line_detail['value']['quote'];
 }
-
+// @TODO @Reza . if count of quote > 3 get random
 ?>
 
 <?php if($quote) {?>
 <div class="fit">
-	<a href="<?php echo \dash\get::index($quote, 'url') ?>" <?php if(\dash\get::index($quote, 'target')) { echo 'target="_blank"'; } ?>>
-		<p><?php echo \dash\get::index($quote, 'quote') ?></p>
-	</a>
+
+	<?php foreach ($quote as $key => $value) {?>
+    		<img class="avatar" src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'displayname'); ?>">
+		 	<b><?php echo \dash\get::index($value, 'displayname') ?> <small><?php echo \dash\get::index($value, 'job') ?></small></b>
+		 	<div class="ltr"><?php if(\dash\get::index($value, 'star')) { echo str_repeat('⭐️', \dash\get::index($value, 'star')); } ?></div>
+    		<p><?php echo \dash\get::index($value, 'text') ?></p>
+
+    <?php } //endif ?>
+
 </div>
 <?php } //endif ?>
