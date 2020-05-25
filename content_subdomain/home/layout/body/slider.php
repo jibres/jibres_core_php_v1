d2c6de73b75539c14e91d52cef901362915da26a
Reza <rm.biqarar@gmail.com>
2020-05-25 06:17:03 +0430

    Website: slider, fix show slider in website

--- content_subdomain/home/layout/body/slider.php
+++ content_subdomain/home/layout/body/slider.php
@@ -13,7 +13,7 @@ if(isset($line_detail['value']['slider']) && is_array($line_detail['value']['sli
 <div class="fit">
   <div class="jSlider1 mB10" data-slick>
 	<?php foreach ($slider as $key => $value) {?>
-		<a href="<?php echo \dash\get::index($value, 'url') ?>" <?php if(\dash\get::index($value, 'target')) { echo 'target="_blank"'; } ?>>
+		 <a<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
     		<img src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'alt'); ?>">
 		</a>
     <?php } //endif ?>
