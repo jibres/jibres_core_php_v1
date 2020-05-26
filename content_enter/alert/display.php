eee7e93ff6710f96a76ca6271c28bb8d9ce2700d
Reza <rm.biqarar@gmail.com>
2020-05-26 13:54:07 +0430

    remove alert

--- content_enter/alert/display.php
+++ content_enter/alert/display.php
@@ -2,9 +2,3 @@
 <p class="txtC "><?php echo \dash\data::alertMsg(); ?></p>
 <a class="link txtC " id="directLink"  href='<?php echo \dash\data::alertLink(); ?>'><?php echo \dash\data::alertButton(); ?></a>
 
-<script>
-setTimeout(function()
-{
-  document.getElementById('directLink').click();
-}, 2000);
-</script>
