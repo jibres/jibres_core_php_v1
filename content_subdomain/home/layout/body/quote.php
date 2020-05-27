016139aa0ba39de5cac72a9a68580e4e5d3f9bc1
Reza <rm.biqarar@gmail.com>
2020-05-27 13:26:14 +0430

    Website: fix quote design

--- content_subdomain/home/layout/body/quote.php
+++ content_subdomain/home/layout/body/quote.php
@@ -10,15 +10,30 @@ if(isset($line_detail['value']['quote']) && is_array($line_detail['value']['quot
 ?>
 
 <?php if($quote) {?>
-<div class="fit">
+
+
+<div class="avand">
+	<div class="f">
 
 	<?php foreach ($quote as $key => $value) {?>
-    		<img class="avatar" src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'displayname'); ?>">
-		 	<b><?php echo \dash\get::index($value, 'displayname') ?> <small><?php echo \dash\get::index($value, 'job') ?></small></b>
-		 	<div class="ltr"><?php if(\dash\get::index($value, 'star')) { echo str_repeat('⭐️', \dash\get::index($value, 'star')); } ?></div>
-    		<p><?php echo \dash\get::index($value, 'text') ?></p>
+      <div class="c4 m6 s12 pA15">
+        <div class="item f f-column justify-between">
+          <p class="flex-1"><?php echo \dash\get::index($value, 'text') ?></p>
+          <div class="fiveStar"><?php if(\dash\get::index($value, 'star')) { echo str_repeat('<span></span>', \dash\get::index($value, 'star')); } ?></div>
+
+          <footer class="f align-center from">
+            <div class="cauto"><img class="avatar" src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'displayname'); ?>"></div>
+            <div class="cauto pLa10">
+              <div class="name"><?php echo \dash\get::index($value, 'displayname') ?></div>
+              <div class="position"><?php echo \dash\get::index($value, 'job') ?></div>
+            </div>
+          </footer>
+
+    	</div>
+      </div>
 
     <?php } //endif ?>
 
+	</div>
 </div>
 <?php } //endif ?>
