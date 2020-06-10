ff70def6d84b99a0f13e374a0fe1eb360401306a
Reza Mohiti <rm.biqarar@gmail.com>
2020-06-10 12:38:10 +0430

    add cat btn

--- content_a/cart/home/view.php
+++ content_a/cart/home/view.php
@@ -12,6 +12,11 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
 
+		// action
+		\dash\data::action_text(T_('Add new cart'));
+		\dash\data::action_icon('plus');
+		\dash\data::action_link(\dash\url::this(). '/add');
+
 		$dataTable = \lib\app\cart\search::list();
 
 		\dash\data::dataTable($dataTable);
