f3751d97a9dc9e5a1508f5ed635ef76878dd2383
Reza <rm.biqarar@gmail.com>
2020-06-11 13:46:16 +0430

    SU: bug fix download backup

--- content_su/backup/controller.php
+++ content_su/backup/controller.php
@@ -7,17 +7,20 @@ class controller
 	{
 
 		$download = \dash\request::get('download');
-		if($download && is_file(database. 'backup/file/'. $download))
+
+		if($download && is_file(YARD. 'jibres_backup/database/'. $download))
 		{
 			\dash\log::set('downloadBackup');
-			\dash\file::download(database. 'backup/file/'. $download);
+			\dash\file::download(YARD. 'jibres_backup/database/'. $download);
 			return;
 		}
 
 		$zipdownload = \dash\request::get('zipdownload');
-		if($zipdownload && is_dir(database. 'backup/file/'. $zipdownload))
+
+		if($zipdownload && is_dir(YARD. 'jibres_backup/database/'. $zipdownload))
 		{
 			$zip_dir = YARD . 'jibres_backup/download';
+
 			if(!is_dir($zip_dir))
 			{
 				\dash\file::makeDir($zip_dir, null, true);
@@ -26,8 +29,11 @@ class controller
 			$zip_addr  = $zip_dir. '/Backup_download_now.zip';
 
 			\dash\file::delete($zip_addr);
-			$zip       = \dash\utility\zip::folder($zip_addr, database. 'backup/file/'. $zipdownload. '/');
+
+			$zip       = \dash\utility\zip::folder($zip_addr, YARD. 'jibres_backup/database/'. $zipdownload. '/');
+
 			\dash\log::set('downloadBackupZip');
+
 			\dash\file::download($zip_addr);
 			return;
 		}
