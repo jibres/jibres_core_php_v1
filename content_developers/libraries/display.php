e70b8e756564e2713a00ce7c4f411e6ebed8d3cd
Reza <rm.biqarar@gmail.com>
2020-06-12 18:52:09 +0430

    API: add library of domain

--- content_developers/libraries/display.php
+++ content_developers/libraries/display.php
@@ -6,10 +6,13 @@
 
 
 	<div class="avand pT50">
-		<div class="box">
-			<div class="body">
-				<p class="msg mB0"><?php echo T_("We will write it soon :)"); ?></p>
+		<div class="c6 s12 pA10">
+				<div class="box">
+					<div class="body f">
+						<p class="c"><?php echo T_("jibres domain SDK for php"); ?></p>
+						<a class="btn2 blue cauto" href="https://github.com/jibres/jibres-domain-php-sdk" target="_blank" data-direct><?php echo T_("Visit on Github"); ?></a>
+					</div>
+				</div>
 			</div>
-		</div>
 	</div>
 
