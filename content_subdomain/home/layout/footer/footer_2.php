7ca2563c272a4de2d879fd8cf0049901eea1dc62
Reza <rm.biqarar@gmail.com>
2020-06-19 18:42:12 +0430

    Website: fix load footer

--- content_subdomain/home/layout/footer/footer_2.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <div class="copyright"><a href="<?php echo \dash\url::kingdom(); ?>"><?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?></a></div>
-  <?php \lib\app\website\menu\generate::menu('footer_menu_1'); ?>
-  <?php \lib\app\website\menu\generate::menu('footer_menu_2'); ?>
-
-
-</footer>
---
+++ content_subdomain/home/layout/footer/footer_300.php
@@ -0,0 +1,7 @@
+<div class="jFooter1 txtC">
+	<div class="avand">
+		<div class="f">
+			<div class="c"><?php echo \dash\get::index($website, 'footer', 'maintext', 'text') ?> <a href="<?php echo \dash\url::kingdom(); ?>" target="_blank"><?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?></a></div>
+		</div>
+	</div>
+</div>
