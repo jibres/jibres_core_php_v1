13d0e3e57e24ae2c41d49099697a6ba768e4b5eb
Reza <rm.biqarar@gmail.com>
2020-06-22 09:41:56 +0430

    Fix rout product

--- content_subdomain/p/home/display.php
+++ content_subdomain/p/home/display.php
@@ -1,4 +1,53 @@
-<div class="jibresBanner">
+<div class="avand productPage">
+  <div class="box">
+
+
+
+
+      <div class="row">
+        <div class="c-xs-12 c-5">
+          <img src="http://cloud.talambar.local/jb2jw/202006/28-31a008b2a1ad34af5e43232acd36e76a.jpg">
+        </div>
+        <div class="c-xs-12 c-7">
+          <h1>Xiaomi Redmi Note 8 6.3" 64GB 4GB RAM (GSM Only, No CDMA) Internationa Version - No Warranty (Space Black)</h1>
+
+          <div class="priceLine">
+          <div class="row">
+            <div class="c">
+              <span><?php echo T_("Price"); ?></span>
+              <div class="priceShow">
+                <span class="price"><?php echo \dash\fit::price(2950000); ?></span>
+                <span class="unit"><?php echo \lib\currency::unit(); ?></span>
+              </div>
+            </div>
+            <div class="c-auto">
+              <a href="/cart/add/12345/1" class="addToCart"><?php echo T_("Add to Cart"); ?></a>
+            </div>
+          </div>
+          </div>
+
+          <div class="featureBullets">
+            <ul>
+              <li>HSDPA 850 / 900 / 2100 - LTE B1 (2100) B2 (1900 PCS) B3 (1800 +) B4 (1700/2100 AWS 1) B5 (850) B7 (2600) B8 (900) B20 (800 DD) B28 (700 APT) B38 (TD 2600) B40 (TD 2300) Dual SIM (Nano-SIM, dual stand-by)> (ensure to check compatibility with your carrier before purchase)</li>
+              <li>6. 3 inches 1080 x 2340 pixels, 19. 5: 9 ratio (~409 ppi density) - Fingerprint (rear-mounted)</li>
+              <li>64GB + 4GB RAM - microSD, up to 256 GB - Qualcomm SDM665 Snapdragon Octa-core - Non-removable Li-Po 4000 mAh battery</li>
+              <li>Rear (Main) Camera: Quad 48MP(wide) + 8MP(ultrawide) + 2MP(dedicated macro camera) + 2MP - Front Camera: 13 MP, f/2. 0 - Video: 2160p@30fps, 1080p@30/60/120fps, 720p@960fps</li>
+              <li>Unlocked cellphones are compatible with most of the GSM carriers ( Like T-Mobile or AT&T ) but please be aware that are not compatible with CDMA carriers ( Like Sprint or Verizon Wireless for example ) - FCC ID: 2AFZZC3JG</li>
+            </ul>
+
+          </div>
+
+
+        </div>
+
+      </div>
+  </div>
+</div>
+
+
+
+
+<div class=" hide jibresBanner">
  <div class="avand">
 <?php $dataRow = \dash\data::dataRow(); ?>
 <div class="blogEx">
