8e4fe08655a3763cd729630bb41d802fba3d1b31
Reza <rm.biqarar@gmail.com>
2020-06-22 17:23:19 +0430

    fix load bullet

--- content_subdomain/p/home/display.php
+++ content_subdomain/p/home/display.php
@@ -45,11 +45,9 @@
 
           <div class="featureBullets ltr">
             <ul>
-              <li>HSDPA 850 / 900 / 2100 - LTE B1 (2100) B2 (1900 PCS) B3 (1800 +) B4 (1700/2100 AWS 1) B5 (850) B7 (2600) B8 (900) B20 (800 DD) B28 (700 APT) B38 (TD 2600) B40 (TD 2300) Dual SIM (Nano-SIM, dual stand-by)> (ensure to check compatibility with your carrier before purchase)</li>
-              <li>6. 3 inches 1080 x 2340 pixels, 19. 5: 9 ratio (~409 ppi density) - Fingerprint (rear-mounted)</li>
-              <li>64GB + 4GB RAM - microSD, up to 256 GB - Qualcomm SDM665 Snapdragon Octa-core - Non-removable Li-Po 4000 mAh battery</li>
-              <li>Rear (Main) Camera: Quad 48MP(wide) + 8MP(ultrawide) + 2MP(dedicated macro camera) + 2MP - Front Camera: 13 MP, f/2. 0 - Video: 2160p@30fps, 1080p@30/60/120fps, 720p@960fps</li>
-              <li>Unlocked cellphones are compatible with most of the GSM carriers ( Like T-Mobile or AT&T ) but please be aware that are not compatible with CDMA carriers ( Like Sprint or Verizon Wireless for example ) - FCC ID: 2AFZZC3JG</li>
+              <?php foreach (\dash\data::dataRow_bullet() as $key => $value) {?>
+                <li><?php echo \dash\get::index($value, 'text'); ?></li>
+              <?php } // endif ?>
             </ul>
 
           </div>
