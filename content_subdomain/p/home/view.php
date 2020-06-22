798e7ad87fca31a1943cd2d4318f985bda9f2c25
Reza <rm.biqarar@gmail.com>
2020-06-22 14:52:53 +0430

    Add product property

--- content_subdomain/p/home/view.php
+++ content_subdomain/p/home/view.php
@@ -6,6 +6,9 @@ class view
 	public static function config()
 	{
 		\dash\face::title(\lib\store::title(). ' | '. \dash\data::dataRow_title());
+
+		$property_list = \lib\app\product\property::get(\dash\data::dataRow_id());
+		\dash\data::propertyList($property_list);
 	}
 }
 ?>
