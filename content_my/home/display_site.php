088bc05c7fab0c383d3c110cbd4daf5049cd52a1
Reza Mohiti <rm.biqarar@gmail.com>
2020-07-08 12:51:24 +0430

    fix show user budget in dashboard

--- content_my/home/display_site.php
+++ content_my/home/display_site.php
@@ -19,7 +19,7 @@
      <div class="c">
       <a href="<?php echo \dash\url::kingdom() ?>/account/billing" class="stat <?php if(\dash\get::index($myData, 'budget')>0) echo " green"; ?>">
        <h3><?php echo T_("Account Balance");?></h3>
-       <div class="val"><?php echo \dash\fit::stats(\dash\get::index($myData, 'budget'));?></div>
+       <div class="val"><?php echo \dash\fit::number(\dash\get::index($myData, 'budget'));?></div>
       </a>
      </div>
     </section>
