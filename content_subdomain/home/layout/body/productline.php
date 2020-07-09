e8b66e8972b2090960c6ff858461860a3ccfea11
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-09 14:14:21 +0430

    fix productLine for responsive

--- content_subdomain/home/layout/body/productline.php
+++ content_subdomain/home/layout/body/productline.php
@@ -3,21 +3,16 @@
 $productline = \lib\app\product\get::website_last_product();
 
 
-if($productline && is_array($productline))
-{
-?>
-
+if($productline && is_array($productline)) { ?>
+<section class="productLine">
+  <div class="avand">
 <?php if(isset($line_detail['value']['title'])) {?>
-<div class="fit">
-  <h2 class="jTitle1"><?php echo $line_detail['value']['title']; ?></h2>
-</div>
+    <h2 class="jTitle1"><?php echo $line_detail['value']['title']; ?></h2>
 <?php } //endif ?>
 
-  <div class="avand">
-    <div class="row padLess">
-
+    <div class="row padLess <?php if(\dash\detect\device::detectPWA()) {echo "horizontalScroll nowrap";}?>">
       <?php foreach ($productline as $key => $value) { ?>
-        <div class="c-xs-6 c-sm-6 c-md-4 c-lg-3 c-xl-2">
+        <div class="c-xs-6 c-sm-6 c-md-4 c-lg-3 c-xl-2 productBox">
           <a class="jProduct1" href="<?php echo \dash\get::index($value, 'url'); ?>">
             <img src="<?php echo \dash\get::index($value, 'thumb') ?>" alt="<?php echo \dash\get::index($value, 'title') ?>">
             <footer>
@@ -29,5 +24,6 @@ if($productline && is_array($productline))
       <?php } //endfor ?>
     </div>
   </div>
+</section>
 
 <?php } //endif ?>
