13c1c5ede2ce51273ec19dd586708b59e0ece63d
Reza <rm.biqarar@gmail.com>
2020-07-09 16:18:21 +0430

    Set face title

--- content_subdomain/home/view.php
+++ content_subdomain/home/view.php
@@ -6,13 +6,6 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::store(\lib\store::detail());
-
-		\dash\face::site(\lib\store::detail('title'));
-		\dash\face::title(\lib\store::detail('title'));
-
-		\dash\face::desc(\lib\store::detail('desc'));
-		\dash\face::intro(\lib\store::detail('desc'));
 
 	}
 }
--- content_subdomain/view.php
+++ content_subdomain/view.php
@@ -6,6 +6,13 @@ class view
 {
 	public static function config()
 	{
+		\dash\data::store(\lib\store::detail());
+
+		\dash\face::site(\lib\store::detail('title'));
+		\dash\face::title(\lib\store::detail('title'));
+
+		\dash\face::desc(\lib\store::detail('desc'));
+		\dash\face::intro(\lib\store::detail('desc'));
 
 	}
 }
