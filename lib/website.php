a3c5bf40c4a4026a2f9ee6b8cadb60992a2a3b3b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-10 18:00:52 +0430

    show discount of product on productlist

--- lib/website.php
+++ lib/website.php
@@ -48,41 +48,66 @@ class website
 
 
 
-	public static function product_list($_data)
+	public static function product_list($_productList)
 	{
-		if(!$_data || !is_array($_data))
+		if(!$_productList || !is_array($_productList))
 		{
 			return;
 		}
-		echo '<div class="row padLess ';
+
+		echo '<div class="row padLess';
 		if(\dash\detect\device::detectPWA())
 		{
-			echo "horizontalScroll nowrap";
+			echo " horizontalScroll nowrap";
 		}
 		echo '">';
 
-	     foreach ($_data as $key => $value)
-	     {
-	        echo '<div class="c-xs-6 c-sm-6 c-md-4 c-lg-3 c-xl-2 productBox">';
-	          echo '<a class="jProduct1" href="'. \dash\get::index($value, 'url'). '">';
-	            echo '<img src="'. \dash\get::index($value, 'thumb'). '" alt="'. \dash\get::index($value, 'title'). '">';
-	            echo '<footer>';
-	              echo '<div class="title">'. \dash\get::index($value, 'title'). '</div>';
-	              if(\dash\permission::supervisor())
-	              {
-	                if(\dash\get::index($value, 'discount'))
-	                {
-	                    echo '<u>'. \dash\fit::price(\dash\get::index($value, 'price') + \dash\get::index($value, 'discount')). '</u>';
-	                    echo '<br>';
-	                    echo '<i>'. \dash\fit::text(\dash\get::index($value, 'discountpercent')) . ' '. T_("%"). '</i>';
-	                }
-	              }
-	              echo '<div class="price"><span>'. \dash\fit::number(\dash\get::index($value, 'price')). '</span> <span class="unit">'. \dash\get::index($value, 'unit'). '</span></div>';
-	            echo '</footer>';
-	          echo '</a>';
-	        echo '</div>';
-	      }
-	    echo '</div>';
+		foreach ($_productList as $key => $myProduct)
+		{
+			echo '<div class="c-xs-6 c-sm-6 c-md-4 c-lg-3 c-xl-2 productBox">';
+			{
+				self::product_element_create($myProduct);
+			}
+			echo '</div>';
+		}
+
+		echo '</div>';
 	}
+
+
+	private static function product_element_create($_item)
+	{
+		echo '<a class="jProduct1" href="'. \dash\get::index($_item, 'url'). '">';
+		{
+			echo '<img src="'. \dash\get::index($_item, 'thumb'). '" alt="'. \dash\get::index($_item, 'title'). '">';
+			if(\dash\get::index($_item, 'discountpercent'))
+			{
+				echo '<span class="discount">';
+				echo '-7';
+				echo \dash\fit::price(\dash\get::index($_item, 'discountpercent'));
+				echo '%';
+				echo '</span>';
+			}
+			echo '<footer>';
+			{
+				echo '<div class="title">'. \dash\get::index($_item, 'title'). '</div>';
+				if(\dash\permission::supervisor())
+				{
+					if(\dash\get::index($_item, 'discount'))
+					{
+						echo '<u>'. \dash\fit::price(\dash\get::index($_item, 'price') + \dash\get::index($_item, 'discount')). '</u>';
+						echo '<br>';
+						echo '<i>'. \dash\fit::text(\dash\get::index($_item, 'discountpercent')) . ' '. T_("%"). '</i>';
+					}
+				}
+				echo '<div class="price"><span>'. \dash\fit::number(\dash\get::index($_item, 'price')). '</span> <span class="unit">'. \dash\get::index($_item, 'unit'). '</span></div>';
+			}
+			echo '</footer>';
+
+		}
+		echo '</a>';
+	}
+
+
 }
 ?>
