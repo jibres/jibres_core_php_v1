13db5dca19ec579bead581b5407f0740bdad011d
Reza <rm.biqarar@gmail.com>
2020-07-12 17:11:51 +0430

    fix irvat

--- content_a/irvat/edit/view.php
+++ content_a/irvat/edit/view.php
@@ -12,13 +12,12 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::this(). '/all');
 
-						// btn
-		\dash\data::action_text(T_('Add new factor'));
-		\dash\data::action_link(\dash\url::this(). '/add');
 
 		\dash\data::maxUploadSize(\dash\upload\size::MB(1, true));
 
 		\dash\data::titleList(\lib\app\irvat\get::title_list());
+
+		\content_crm\member\main\view::static_var();
 
 	}
 }
