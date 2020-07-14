360daba62aed774b3b6678d7ef91ebcfa789b18f
Reza <rm.biqarar@gmail.com>
2020-07-14 19:05:06 +0430

    Check telegram bot api key

--- content_a/products/share/display.php
+++ content_a/products/share/display.php
@@ -12,7 +12,12 @@ $propertyList = \dash\data::propertyList();
           <header><h2><?php echo T_("Share with telegram"); ?></h2></header>
           <div class="pad jboxProperty">
 
-
+            <?php if(!\dash\data::telegramSetting_apikey()) { // neet to set telegram setting first?>
+              <div class="msg warn2">
+                <?php echo T_("You must set Telegram bot setting first") ?>
+                <a class="btn link" href="<?php echo \dash\url::here(). '/setting/telegram' ?>"><?php echo T_("Click here to setup telegram setting") ?></a>
+              </div>
+            <?php }else{ ?>
             <form method="post" autocomplete="off">
               <img class="avatar fs40" src="<?php echo \dash\data::productDataRow_thumb() ?>">
               <h2><?php echo \dash\data::productDataRow_title() ?></h2>
@@ -24,7 +29,7 @@ $propertyList = \dash\data::propertyList();
                 <button class="btn master mT10" ><?php echo T_("Send"); ?></button>
               </div>
             </form>
-
+          <?php } //endif ?>
           </div>
       </section>
 
