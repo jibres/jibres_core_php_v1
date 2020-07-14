4635ed01cfbb0b1f389acd2114c68c68e8a0000b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-15 01:56:18 +0430

    send test message

--- content_a/products/share/model.php
+++ content_a/products/share/model.php
@@ -12,6 +12,12 @@ class model
 		$post         = [];
 		$post['sharetext'] = isset($_POST['sharetext']) ? $_POST['sharetext'] : null;
 
+
+		$chatid   = 46898544;
+		$text     = 'salam';
+		$myData   = ['chat_id' => $chatid, 'text' => $text];
+		$myResult = \dash\social\telegram\tg::json_sendMessage($myData);
+
 		$result = \lib\app\product\edit::edit($post, $id);
 	}
 
