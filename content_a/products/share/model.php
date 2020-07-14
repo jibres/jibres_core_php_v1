0e6e08798d931b31d69fd0a12f44d7af55eb6209
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-15 02:05:54 +0430

    fill message

--- content_a/products/share/model.php
+++ content_a/products/share/model.php
@@ -13,8 +13,27 @@ class model
 		$post['sharetext'] = isset($_POST['sharetext']) ? $_POST['sharetext'] : null;
 
 
+		$telegram_setting = \lib\app\setting\get::telegram_setting();
+		\dash\data::telegramSetting($telegram_setting);
+
+	    // @javad
+	    // $telegram_setting contain:
+		// 'apikey' => string 'apikey'
+		// 'channel' => string 'channel id'
+		// 'share_text' => string 'default share text'
+
+		$apikey   = $telegram_setting['apikey'];
+		$chatid   = $telegram_setting['channel'];
 		$chatid   = 46898544;
-		$text     = 'salam';
+
+		$photo    = \dash\data::productDataRow_thumb();
+		$text     = '<b>'. \dash\data::productDataRow_title(). '</b>';
+		$text     .= "\n";
+		$text     .= \dash\data::productDataRow_sharetext();
+		$text     .= "\n\n";
+		$text     .= $telegram_setting['share_text'];
+
+
 		$myData   = ['chat_id' => $chatid, 'text' => $text];
 		$myResult = \dash\social\telegram\tg::json_sendMessage($myData);
 
