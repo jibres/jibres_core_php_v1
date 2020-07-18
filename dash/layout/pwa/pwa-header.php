e1bc1694887842494ce6598f6578ce58b345530d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-18 20:17:32 +0430

    update pwa header

--- dash/layout/pwa/pwa-header.php
+++ dash/layout/pwa/pwa-header.php
@@ -1,12 +1,11 @@
-
-   <div class="back"><?php
+<?php
 if(\dash\data::back_link())
 {
-	echo "<a href='". \dash\data::back_link(). "'></a>";
+	echo "<a class='square back' href='". \dash\data::back_link(). "'></a>";
 }
-?></div>
-   <div class="title"><?php echo \dash\face::titlePWA(); ?></div>
-   <div class="action"><?php
+?>
+<div class="title"><?php echo \dash\face::titlePWA(); ?></div>
+<div class="action"><?php
 if(\dash\data::action_link() && \dash\data::action_text())
 {
 	if(\dash\data::action_icon())
@@ -23,3 +22,15 @@ elseif(\dash\face::btnSave())
 	echo "<button form='". \dash\face::btnSave(). "'>". T_("Save"). "</button>";
 }
 ?></div>
+<?php
+// add search btn
+if(\dash\data::search_link() or 1)
+{
+	echo "<a class='square search' href='". \dash\data::search_link(). "'></a>";
+}
+// add menu btn
+if(\dash\data::menu_link() or 1)
+{
+	echo "<div class='square menu'></div>";
+}
+?>
