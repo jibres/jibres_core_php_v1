dcee507d4df507cf603e4ca12b7e74e999a587ca
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-18 14:45:20 +0430

    some fix

--- content_business/home/layout/header/header_100.php
+++ content_business/home/layout/header/header_100.php
@@ -1,13 +1,13 @@
 <?php require_once('before_header.php'); ?>
 <div id='jHeader100' class="avand" data-circleEffect>
-  <div class="actionBar f align-center">
-    <div class="cauto s12 pRa10">
+  <div class="actionBar row align-center">
+    <div class="c-auto c-xs-0 pRa10">
       <a class="logo" href="<?php echo \dash\url::kingdom(); ?>"><?php
 $logo = \lib\website::logo();
 if($logo)
 {
   echo '<img src="'. $logo. '" alt="'. \dash\face::site(). '">';
-  echo '<h1 class="hide">'. \dash\face::site(). '</h1>';
+  echo '<h1>'. \dash\face::site(). '</h1>';
 }
 else
 {
@@ -16,13 +16,13 @@ else
 ?></a>
     </div>
     <div class="c s0"></div>
-    <div class="cauto pRa20">
+    <div class="c-auto pRa20">
       <a class="search" href="<?php echo \dash\url::kingdom(); ?>/search"></a>
     </div>
-    <div class="cauto pRa20">
-      <a class="cart" href="<?php echo \dash\url::kingdom(); ?>/cart" data-count="<?php echo \dash\fit::number(\lib\website::cart_count()); ?>"><?php echo T_("Cart"); ?></a>
+    <div class="c-auto pRa20">
+      <a class="cart" href="<?php echo \dash\url::kingdom(); ?>/cart" data-count="<?php echo \dash\fit::number(\lib\website::cart_count()); ?>"><span class="d-xs-none"><?php echo T_("Cart"); ?></span></a>
     </div>
-    <div class="cauto">
+    <div class="c-auto">
       <?php if(\dash\user::login()) {?>
         <a class="enter" href="<?php echo \dash\url::kingdom(); ?>/profile"><?php echo T_("Profile"); ?></a>
       <?php }else{ ?>
@@ -32,9 +32,9 @@ else
   </div>
 
   <?php if(\lib\website::have_header_menu()) {?>
-  <div class="menuBar f">
+  <div class="menuBar row">
     <div class="c"><?php \lib\website::menu('header_menu_1'); ?></div>
-    <div class="cauto os"><?php \lib\website::menu('header_menu_2', 'xs0'); ?></div>
+    <div class="c-auto os"><?php \lib\website::menu('header_menu_2', 'xs0'); ?></div>
   </div>
 <?php } //endif ?>
 
