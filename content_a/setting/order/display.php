b0fd180bb1f32b5341cdc499b8325529a986c263
Reza <rm.biqarar@gmail.com>
2020-07-19 17:43:49 +0430

    update translate

--- content_a/setting/order/display.php
+++ content_a/setting/order/display.php
@@ -4,4 +4,10 @@
       <li><a class="f" href="<?php echo \dash\url::that(); ?>/validity"><div class="key"><?php echo T_("Validity period of order"); ?></div><div class="go"></div></a></li>
     </ul>
   </nav>
+
+  <nav class="items">
+    <ul>
+      <li><a class="f" href="<?php echo \dash\url::here(); ?>/order"><div class="key"><?php echo T_("Orders List"); ?></div><div class="go"></div></a></li>
+    </ul>
+  </nav>
 </div>
