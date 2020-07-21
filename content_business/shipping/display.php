48f2266c452c810feef84cdd8ef09d68fd385f0c
Reza <rm.biqarar@gmail.com>
2020-07-21 20:10:02 +0430

    fix

--- content_business/shipping/display.php
+++ content_business/shipping/display.php
@@ -45,13 +45,17 @@
         <?php } //endif ?>
 
         <div class="box">
+          <input type="hidden" name="payway" value="online">
+          <?php if(false) {?>
             <?php if(\dash\data::paymentWay()) {?>
               <?php foreach (\dash\data::paymentWay() as $key => $value) {?>
+                <?php var_dump($value); ?>
                 <div class="radio3 mB10">
                   <input  id="payway<?php echo $key; ?>" type="radio" name="payway" value="<?php echo \dash\get::index($value, 'key'); ?>" <?php if($key === 'online') { echo 'checked';} ?>>
                   <label for="payway<?php echo $key; ?>"><?php echo \dash\get::index($value, 'title'); ?></label>
                 </div>
               <?php } //endfor ?>
+            <?php } // endif ?>
             <?php } // endif ?>
             <label for="desc"><?php echo T_("Order descripion"); ?></label>
             <textarea class="txt" name="desc" maxlength='300' rows="2" placeholder="<?php echo T_("If you have note about this order enter it here") ?>"></textarea>
