b80bf47fdeee9751d4f8f12e1d8700950b27a339
Reza <rm.biqarar@gmail.com>
2020-07-21 16:33:07 +0430

    Show address

--- content_business/orders/view/display.php
+++ content_business/orders/view/display.php
@@ -16,17 +16,36 @@
 
 					</div>
 
+          <div class="msg">
+
+          <?php $address = \dash\get::index(\dash\data::dataRow(), 'address'); ?>
+          <?php if(isset($address['country']) && $address['country']) {?><i class="flag <?php echo mb_strtolower($address['country']); ?>"></i><?php } //endif ?>
+          <span ><?php echo \dash\get::index($address, 'location_string'); ?></span>
+          <span><?php echo \dash\get::index($address, 'address'); ?></span>
+          <?php if(isset($address['postcode']) && $address['postcode']) {?>
+            <span title='<?php echo T_("Postal code"); ?>' class="compact"><?php echo \dash\fit::text($address['postcode']); ?><i class="sf-crosshairs mRL5"></i></span>
+          <?php }//endif ?>
+          <?php echo \dash\get::index($address, 'name'); ?></td>
+          <?php if(isset($address['phone']) && $address['phone']) {?>
+            <div title='<?php echo T_("Phone"); ?>'><i class="sf-phone"></i> <?php echo \dash\fit::text($address['phone']); ?></div>
+          <?php } //endif ?>
+          <?php if(isset($address['mobile']) && $address['mobile']) {?>
+           <div title='<?php echo T_("Mobile"); ?>' class="mT5"><i class="sf-mobile"></i> <?php echo \dash\fit::mobile($address['mobile']); ?></div>
+          <?php } //endif ?>
+          </div>
+
 				</div>
+
 					<?php if(!\dash\get::index(\dash\data::dataRow_order(), 'pay') && \dash\get::index(\dash\data::dataRow_order(), 'status') !== 'cancel') {?>
 						<footer class="txtRa">
 							<div class="btn warn" data-confirm data-data='{"set_status": "cancel"}'><?php echo T_("Cancel order") ?></div>
 						</footer>
 					<?php } //endif ?>
-			</div>
-			<div>
-				<?php \lib\website::product_list_raw(\dash\data::dataRow_products()); ?>
-			</div>
 
+
+
+			</div>
+				<?php \lib\website::product_list_raw(\dash\data::dataRow_products()); ?>
 
 		</div>
 	</div>
