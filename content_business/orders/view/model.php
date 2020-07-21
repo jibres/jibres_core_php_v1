0127aaa48d6632778cd80c6e3febf44b3bbcbf29
Reza <rm.biqarar@gmail.com>
2020-07-21 18:32:28 +0430

    fix add product to cart in factor detail

--- content_business/orders/view/model.php
+++ content_business/orders/view/model.php
@@ -6,6 +6,17 @@ class model
 {
 	public static function post()
 	{
+		if(\dash\request::post('cart') === 'buy')
+		{
+			$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
+
+		}
+
+		if(\dash\request::post('cart') === 'add')
+		{
+			$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
+		}
+
 		if(\dash\request::post('set_status') === 'cancel')
 		{
 			\lib\app\factor\edit::user_cancel(\dash\request::get('id'));
