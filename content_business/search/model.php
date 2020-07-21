2e5f3f0928ab43870d5f0805b1bda0becbf7ad8a
Reza <rm.biqarar@gmail.com>
2020-07-21 18:09:15 +0430

    Add search model

---
+++ content_business/search/model.php
@@ -0,0 +1,62 @@
+<?php
+namespace content_business\search;
+
+class model
+{
+	public static function post()
+	{
+		if(\dash\request::post('cart') === 'buy')
+		{
+			$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
+			if($result)
+			{
+				\dash\redirect::to(\dash\url::kingdom(). '/cart');
+			}
+		}
+
+		if(\dash\request::post('cart') === 'add')
+		{
+			$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
+			if($result)
+			{
+				\dash\redirect::to(\dash\url::pwd());
+			}
+		}
+
+		$product_id = \dash\request::post('product_id');
+
+		if(\dash\request::post('type') === 'minus_cart')
+		{
+			$result = \lib\app\cart\edit::update_cart($product_id, 1, null, 'minus_count');
+			if($result)
+			{
+				\dash\redirect::pwd();
+			}
+		}
+		elseif(\dash\request::post('type') === 'plus_cart')
+		{
+			$result = \lib\app\cart\edit::update_cart($product_id, 1, null, 'plus_count');
+			if($result)
+			{
+				\dash\redirect::pwd();
+			}
+		}
+		elseif(\dash\request::post('type') === 'update_cart')
+		{
+			$result = \lib\app\cart\edit::update_cart($product_id, \dash\request::post('count'), null, 'update_count');
+			if($result)
+			{
+				\dash\redirect::pwd();
+			}
+		}
+		elseif(\dash\request::post('type') === 'remove')
+		{
+			$result = \lib\app\cart\remove::from_cart($product_id);
+			if($result)
+			{
+				\dash\redirect::pwd();
+			}
+		}
+	}
+}
+?>
