22ca40fb796f1310aa25bcf6b04e0a4bc9523eb4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-22 19:55:07 +0430

    add productData

--- content_a/products/edit/display.php
+++ content_a/products/edit/display.php
@@ -5,7 +5,7 @@ $have_variant_child =\dash\data::productDataRow_variant_child();
 ?>
 
 
-<form class="jPage f" method="post" autocomplete="off" data-refresh data-autoScroll2="#productGallery">
+<form class="jPage f aProductData" method="post" autocomplete="off" data-refresh data-autoScroll2="#productGallery">
   <button class="hide"  name="submitall" type="submit" value="master"><?php echo T_("Save"); ?></button>
   <div class="c8 x9 s12 pRa10">
     <section class="jbox pad">
