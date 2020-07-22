3a83bf6a3d9a66492701aecea1465b438fbeaece
Reza <rm.biqarar@gmail.com>
2020-07-22 12:15:56 +0430

    fix name of copy

--- content_a/products/duplicate/display.php
+++ content_a/products/duplicate/display.php
@@ -6,7 +6,7 @@
           <h3><?php echo T_("Make a copy of this product"); ?></h3>
           <label for="title"><?php echo T_("New Title"); ?><span class="fc-red">*</span></label>
           <div class="input">
-           <input type="text" name="title" id="title" placeholder='<?php echo T_("Copy of"); ?> <?php echo \dash\data::productDataRow_title(); ?>' maxlength='200' autofocus>
+           <input type="text" name="title" id="title" placeholder='<?php echo T_("Copy of"); ?> <?php echo \dash\data::productDataRow_title(); ?>' value='<?php echo T_("Copy of"); ?> <?php echo \dash\data::productDataRow_title(); ?>' maxlength='200' autofocus>
           </div>
           <div class="txtRa">
             <button class="btn success"><?php echo T_("Copy"); ?></button>
