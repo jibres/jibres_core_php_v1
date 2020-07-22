9cdcbf1deb3f4f9b015b8d7cc24261fef2390e71
Reza <rm.biqarar@gmail.com>
2020-07-22 18:35:53 +0430

    test ajaxify

--- content_business/search/model.php
+++ content_business/search/model.php
@@ -6,11 +6,11 @@ class model
 	public static function post()
 	{
 
-		if(\dash\request::post('cart') === 'add')
-		{
-			$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
+		// if(\dash\request::post('cart') === 'add')
+		// {
+		// 	$result = \lib\app\cart\add::new_cart_website(\dash\request::post('product_id'), \dash\request::post('count'));
 
-		}
+		// }
 
 	}
 }
