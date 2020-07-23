ea5c64ea90f8f642abf767f59e0eb714c79e5763
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-23 16:06:24 +0430

    fix

--- content_a/products/edit/display.php
+++ content_a/products/edit/display.php
@@ -191,16 +191,8 @@ $have_variant_child =\dash\data::productDataRow_variant_child();
             <label for="itrackquantity"></label>
             <label for="itrackquantity"><?php echo T_("Track quantity"); ?><small></small></label>
           </div>
-          <p class="fs09 fc-mute mB0-f"><?php echo T_("Inventory tracking can help you avoid selling products that have run out of stock, or let you know when you need to order or make more of your product."); ?></p>
           <div data-response='itrackquantity' data-response-effect='slide' <?php if(\dash\data::productDataRow_trackquantity() || (\dash\url::child() === 'add' && \dash\data::productSettingSaved_defaulttracking())){}else{ echo 'data-response-hide';} ?>  >
-            <?php if(!$have_variant_child) {?>
-              <div class="c s12 pRa10">
-                <label for='stock'><?php echo T_("Stock"). ' '. \dash\fit::number(\dash\data::productDataRow_stock()) ; ?></label>
-                <div class="input">
-                  <input type="text" name="stock" id="stock" data-format='number' placeholder="<?php echo T_("If you want to change the stock enter current stock here") ?>" maxlength="7">
-                </div>
-              </div>
-            <?php } //endif ?>
+            <p class="fs09 fc-mute"><?php echo T_("Inventory tracking can help you avoid selling products that have run out of stock, or let you know when you need to order or make more of your product."); ?></p>
 
             <div class="switch1">
               <input type="checkbox" name="oversale" id="oversale"  <?php if(\dash\data::productDataRow_oversale()) {echo 'checked';}?> >
@@ -208,6 +200,16 @@ $have_variant_child =\dash\data::productDataRow_variant_child();
               <label for="oversale"><?php echo T_("Oversale"); ?></label>
             </div>
             <p class="fc-mute fs09"><?php echo T_("Allow to purchase when sold out of stock"); ?></p>
+            <?php if(!$have_variant_child) {?>
+              <div class="c s12 pRa10">
+                <label for='stock'><?php echo T_("Current Stock Count"); ?> <span class="txtB pRa10"><?php echo \dash\fit::number(\dash\data::productDataRow_stock()); ?></span>
+                </label>
+                <div class="input">
+                  <input type="text" name="stock" id="stock" data-format='number' placeholder="<?php echo T_('Current Stock Count'). ' '. \dash\fit::number(\dash\data::productDataRow_stock()); ?>" maxlength="7">
+                </div>
+                <p class="fc-mute fs09"><?php echo T_("If you want to change the stock enter current stock here") ?></p>
+              </div>
+            <?php } //endif ?>
             <div class="f">
               <div class="c s12 pRa10">
                 <label for='minstock'><?php echo T_("Min stock"); ?></label>
