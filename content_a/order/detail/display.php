149bc9d4600360493e1ec67ca2a9a4593cc2e2a3
Reza <rm.biqarar@gmail.com>
2020-07-24 18:31:13 +0430

    Add new design of manage order

--- content_a/order/detail/display.php
+++ content_a/order/detail/display.php
@@ -1,225 +1,181 @@
 <?php $orderDetail = \dash\data::orderDetail(); ?>
-
-<div class="avand">
-  <div class="box">
-    <header><h2><?php echo T_("Order detail") ?></h2></header>
-    <div class="body">
-
-      <div class="row">
-        <div class="c-md-6">
-
-          <?php if (\dash\get::index($orderDetail, 'factor', 'customer')) {?>
-
-            <div class="msg">
-              <?php echo \dash\get::index($orderDetail, 'factor', 'customer_displayname'); ?>
-              <span class="ltr compact"><?php echo \dash\fit::mobile(\dash\get::index($orderDetail, 'factor', 'customer_mobile')); ?></span>
-              <a class="link btn" href="<?php echo \dash\url::kingdom(). '/crm/member/glance?id='. $orderDetail['factor']['customer']; ?>"><?php echo T_("View profile") ?></a>
-            </div>
-
-          <?php }else{ ?>
-            <div class="msg"><?php echo T_("This order not set to any customer") ?></div>
-          <?php } // endif ?>
-        </div>
-        <div class="c-md-6">
-          <div class="btn mB10 info"><?php echo T_("Status"). ' '. \dash\get::index($orderDetail, 'factor', 'status'); ?></div>
-          <div class="btn mB10"><?php echo T_("Last modified"). ' '. \dash\fit::date_human(\dash\get::index($orderDetail, 'factor', 'datemodified')); ?></div>
-          <?php if (\dash\get::index($orderDetail, 'factor', 'pay')) {?>
-            <div class="btn mB10 success"><?php echo T_("Factor is payed") ?></div>
-          <?php }else{ ?>
-            <div class="btn mB10 warn"><?php echo T_("Factor is not payed") ?></div>
-          <?php } // endif ?>
-          <div data-confirm data-data='{"removeorder": "removeorder"}' class="btn danger"><?php echo T_("Remove order"); ?></div>
-        </div>
-      </div>
-
-
-      <div class="msg"><?php echo T_("Total"); ?><span class="txtB"><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'total')); ?></span></div>
-      <div class="msg"><?php echo T_("Discount"); ?><span class="txtB"><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'didscount')); ?></span></div>
-      <?php if(\dash\get::index($orderDetail, 'factor', 'desc')) {?><div class="msg"><?php echo T_("Description"); ?><span class="txtB"><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'desc')); ?></span></div><?php }// endif ?>
-    </div>
-
-  </div>
-
-  <?php $address = \dash\get::index(\dash\data::orderDetail(), 'address'); ?>
-  <div class="box">
-    <header><h2><?php echo T_("Order Address") ?></h2></header>
-    <div class="body">
-
-      <?php if(isset($address['company']) && $address['company']) {?>
-        <i class="fs16 mRa5 sf-building"></i>
-      <?php }else{ ?>
-        <i class="fs16 mRa5 sf-pin"></i>
-      <?php } // endif ?>
-      <?php if(isset($address['country']) && $address['country']) {?><i class="flag <?php echo mb_strtolower($address['country']); ?>"></i><?php } //endif ?>
-
-      <span ><?php echo \dash\get::index($address, 'location_string'); ?></span>
-
-      <span><?php echo \dash\get::index($address, 'address'); ?></span>
-
-      <?php if(isset($address['postcode']) && $address['postcode']) {?>
-
-        <span title='<?php echo T_("Postal code"); ?>' class="compact"><?php echo \dash\fit::text($address['postcode']); ?><i class="sf-crosshairs mRL5"></i></span>
-
-      <?php }//endif ?>
-      <?php echo \dash\get::index($address, 'name'); ?></td>
-
-
-      <?php if(isset($address['phone']) && $address['phone']) {?>
-
-        <div title='<?php echo T_("Phone"); ?>'><i class="sf-phone"></i> <?php echo \dash\fit::text($address['phone']); ?></div>
-      <?php } //endif ?>
-
-      <?php if(isset($address['mobile']) && $address['mobile']) {?>
-
-        <div title='<?php echo T_("Mobile"); ?>' class="mT5"><i class="sf-mobile"></i> <?php echo \dash\fit::mobile($address['mobile']); ?></div>
-      <?php } //endif ?>
-    </div>
-    <footer>
-      <div class="txtRa">
-        <a class="btn link" href="<?php echo \dash\url::this(). '/address?id='. \dash\request::get('id'); ?>"><?php echo T_("Edit address") ?></a>
-      </div>
-    </footer>
-
-
-  </div>
-
-
-
-  <div class="box">
-    <header><h2><?php echo T_("Order items") ?></h2></header>
-    <div class="body">
-
-      <table class="tbl1 v4">
-        <thead>
-          <tr>
-            <th class="collapsing">#</th>
-            <th><?php echo T_("Title"); ?></th>
-            <th><?php echo T_("Count"); ?></th>
-            <th><?php echo T_("Price"); ?></th>
-            <th><?php echo T_("Sum"); ?></th>
-          </tr>
-        </thead>
-        <tbody>
-
-          <?php foreach (\dash\get::index($orderDetail, 'factor_detail') as $key => $value) {?>
-            <tr>
-              <td class="collapsing"><?php echo \dash\fit::number($key + 1); ?></td>
-              <td><?php echo \dash\get::index($value, 'title'); ?></td>
-              <td><?php echo \dash\fit::number(\dash\get::index($value, 'count')); ?></td>
-              <td><?php echo \dash\fit::number(\dash\get::index($value, 'price')); ?></td>
-              <td><?php echo \dash\fit::number(\dash\get::index($value, 'sum')); ?></td>
-            </tr>
-          <?php } //endfor ?>
-        </tbody>
-      </table>
-
-    </div>
-    <?php if(false) {?>
-    <footer>
-      <div class="txtRa">
-        <a class="btn link" href="<?php echo \dash\url::this(). '/edit?id='. \dash\request::get('id'); ?>"><?php echo T_("Edit items") ?></a>
-      </div>
-    </footer>
-  <?php } //endif ?>
-
-  </div>
-
-  <form method="post" autocomplete="off">
+<div class="row">
+  <div class="c-xs-12 c-md-6">
     <div class="box">
-      <header><h2><?php echo T_("Order action") ?></h2></header>
       <div class="body">
-        <div>
-          <label for="orderaction"><?php echo T_("Add new action"); ?></label>
-          <select class="select22" name="orderaction">
-            <option value="comment"><?php echo T_("comment") ?></option>
-            <option value="pending_pay"><?php echo T_("Pending pay") ?></option>
-            <option value="pay_successfull"><?php echo T_("Pay successfull") ?></option>
-            <option value="pending_prepare"><?php echo T_("Pending prepare") ?></option>
-            <option value="pending_verify"><?php echo T_("Pending verify") ?></option>
-            <option value="pending_send"><?php echo T_("Pending send") ?></option>
-            <option value="sending"><?php echo T_("Sending") ?></option>
-            <option value="deliver"><?php echo T_("Deliver") ?></option>
-            <option value="pay_unverified"><?php echo T_("Pay unverified") ?></option>
-            <option value="reject"><?php echo T_("Reject") ?></option>
-            <option value="spam"><?php echo T_("Spam") ?></option>
-            <option value="expire"><?php echo T_("Expire") ?></option>
-            <?php if(false) {?>
-              <option value="go_to_bank"><?php echo T_("go_to_bank") ?></option>
-              <option value="pay_error"><?php echo T_("pay_error") ?></option>
-              <option value="pay_cancel"><?php echo T_("pay_cancel") ?></option>
-              <option value="pay_verified"><?php echo T_("pay_verified") ?></option>
-              <option value="cancel"><?php echo T_("cancel") ?></option>
-              <option value="order"><?php echo T_("order") ?></option>
-            <?php } //endif ?>
-
-          </select>
-        </div>
-
-        <div class="mB20">
-          <label for="desc"><?php echo T_("Description"); ?></label>
-          <textarea id="desc" name="desc" class="txt" rows="5"><?php echo \dash\data::dataRow_desc(); ?></textarea>
-        </div>
-
-        <label for="file1"><?php echo T_("Attachment"); ?> <small class="fc-mute"><?php echo T_("Maximum file size"). ' '. \dash\data::maxUploadSize(); ?></small></label>
-
-        <div class="box" data-uploader data-name='file'>
-          <input type="file"  id="file1">
-          <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
-        </div>
-
-      </div>
-      <footer>
-        <div class="txtRa">
-          <button class="btn success"><?php echo T_("Save") ?></button>
-        </div>
-      </footer>
-    </div>
-  </form>
-
-
-
-
-  <div class="box">
-    <header><h2><?php echo T_("Order Action list") ?></h2></header>
-    <div class="body">
-
-      <table class="tbl1 v4">
-        <thead>
-          <tr>
-            <th class="collapsing">#</th>
-            <th><?php echo T_("Action"); ?></th>
-            <th><?php echo T_("Date"); ?></th>
-            <th><?php echo T_("Description"); ?></th>
-            <th><?php echo T_("Attachment"); ?></th>
-            <th><?php echo T_("User"); ?></th>
-          </tr>
-        </thead>
-        <tbody>
-
-          <?php foreach (\dash\get::index($orderDetail, 'action') as $key => $value) {?>
+        <table class="tbl1 v4">
+          <tbody>
             <tr>
-              <td class="collapsing"><?php echo \dash\fit::number((count(\dash\get::index($orderDetail, 'action'))) - ($key)); ?></td>
-              <td><?php echo \dash\get::index($value, 'action'); ?></td>
-              <td><?php echo \dash\fit::date_time(\dash\get::index($value, 'datecreated')); ?></td>
-              <td><?php echo \dash\get::index($value, 'desc'); ?></td>
-              <td><?php if(\dash\get::index($value, 'file')) {?><img src="<?php echo \dash\get::index($value, 'file'); ?>" alt="<?php echo T_("Action image"). ' '. \dash\request::get('id'); ?>"><?php } //endif ?></td>
-
-              <td class="collapsing">
-                <a href="<?php echo \dash\url::that(). '?user='.\dash\get::index($value, 'user_id'); ?>" class="f align-center userPack">
-                  <div class="c pRa10">
-                    <div class="mobile" data-copy="<?php echo \dash\get::index($value, 'mobile'); ?>"><?php echo \dash\fit::mobile(\dash\get::index($value, 'mobile')); ?></div>
-                    <div class="name"><?php echo \dash\get::index($value, 'displayname'); ?></div>
-                  </div>
-                  <img class="cauto" src="<?php echo \dash\get::index($value, 'avatar'); ?>">
-                </a>
+              <td><?php echo T_("Customer") ?></td>
+              <td>
+                <?php if (\dash\get::index($orderDetail, 'factor', 'customer')) {?>
+                  <?php echo \dash\get::index($orderDetail, 'factor', 'customer_displayname'); ?>
+                  <span class="ltr compact"><?php echo \dash\fit::mobile(\dash\get::index($orderDetail, 'factor', 'customer_mobile')); ?></span>
+                  <a class="link btn" href="<?php echo \dash\url::kingdom(). '/crm/member/glance?id='. $orderDetail['factor']['customer']; ?>"><?php echo T_("View profile") ?></a>
+                <?php }else{ ?>
+                  <?php echo T_("The user is not logged in to save their details!") ?>
+                <?php } // endif ?>
               </td>
             </tr>
-          <?php } //endfor ?>
-        </tbody>
-      </table>
+            <tr>
+              <td><?php echo T_("Status") ?></td>
+              <td><?php echo \dash\get::index($orderDetail, 'factor', 'status'); ?></td>
+            </tr>
+            <tr>
+              <td><?php echo T_("Order date") ?></td>
+              <td><?php echo \dash\fit::date_time(\dash\get::index($orderDetail, 'factor', 'datecreated')); ?> <small class="fc-mute mLa20"><?php echo \dash\fit::date_human(\dash\get::index($orderDetail, 'factor', 'datecreated')) ?></small></td>
+            </tr>
 
+            <?php if(\dash\get::index($orderDetail, 'factor', 'discount')) {?>
+              <tr>
+                <td><?php echo T_("Discount") ?></td>
+                <td><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'discount')). ' '. \lib\store::currency(); ?></td>
+              </tr>
+            <?php } //endif ?>
+            <?php if(\dash\get::index($orderDetail, 'factor', 'shipping')) {?>
+              <tr>
+                <td><?php echo T_("Shipping") ?></td>
+                <td><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'shipping')). ' '. \lib\store::currency(); ?></td>
+              </tr>
+            <?php } //endif ?>
+
+            <tr>
+              <td><?php echo T_("Total") ?></td>
+              <td><?php echo \dash\fit::number(\dash\get::index($orderDetail, 'factor', 'total')). ' '. \lib\store::currency(); ?></td>
+            </tr>
+            <?php if(\dash\get::index($orderDetail, 'factor', 'desc')) {?>
+              <tr class="warning">
+                <td><?php echo T_("Description") ?></td>
+                <td><?php echo \dash\get::index($orderDetail, 'factor', 'desc'); ?></td>
+              </tr>
+            <?php } //endif ?>
+
+            <tr>
+              <td><?php echo T_("Address") ?></td>
+              <td>
+                <?php $address = \dash\get::index(\dash\data::orderDetail(), 'address'); ?>
+                <address>
+                  <div class="title"><?php echo \dash\get::index($address, 'name'); ?></div>
+                  <?php if(\dash\get::index($address, 'mobile')) {?>
+                    <div class="mobile"><?php echo T_("Mobile") ?> <b><?php echo \dash\fit::mobile(\dash\get::index($address, 'mobile')); ?></b></div>
+                  <?php } //endif ?>
+                  <div class="addr"><?php echo \dash\get::index($address, 'address'); ?></div>
+                  <?php if(\dash\get::index($address, 'postcode')) {?>
+                    <div class="postalcode"><?php echo T_("Postalcode") ?> <b><?php echo \dash\fit::text(\dash\get::index($address, 'postcode')); ?></b></div>
+                  <?php } //endif ?>
+                  <?php if(\dash\get::index($address, 'phone')) {?>
+                    <div class="phone"><?php echo T_("Phone") ?> <b><?php echo \dash\fit::text(\dash\get::index($address, 'phone')); ?></b></div>
+                  <?php } //endif ?>
+                </address>
+              </td>
+            </tr>
+            <tr class="<?php if (\dash\get::index($orderDetail, 'factor', 'pay')) { echo 'positive'; }else{ echo 'negative';}?>">
+              <td><?php echo T_("Pay status") ?></td>
+              <td>
+                <?php if (\dash\get::index($orderDetail, 'factor', 'pay')) {?>
+                  <i class="sf-check-circle fc-green mRa10 fs14"></i><?php echo T_("Factor is payed") ?>
+                <?php }else{ ?>
+                  <i class="sf-times-circle fc-red mRa10 fs14"></i><?php echo T_("Factor is not payed") ?>
+                  <p><?php echo T_("If you get the amount of this factor Set order as paid by click below link") ?></p>
+                  <div class="link btn" data-confirm data-data='{"paid": "paid"}' ><?php echo T_("Amount received") ?></div>
+                <?php } //endif ?>
+              </td>
+            </tr>
+            <tr>
+              <td><?php echo T_("Remove order") ?></td>
+              <td>
+                <p><?php echo T_("If you think this order is spam") ?></p>
+                <div data-confirm data-data='{"removeorder": "removeorder"}' class="linkDel"><?php echo T_("Remove now"); ?></div>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
     </div>
 
+    <h3><?php echo T_("Order products") ?></h3>
+    <nav class="items">
+      <ul>
+        <?php foreach (\dash\get::index($orderDetail, 'factor_detail') as $key => $value) {?>
+          <li>
+            <a class="f" href="<?php echo \dash\url::here(). '/products/edit?id='.\dash\get::index($value, 'product_id'); ?>">
+              <div class="key"><?php echo \dash\get::index($value, 'title');?></div>
+              <div class="value"><?php echo \dash\fit::number(\dash\get::index($value, 'count')). ' '. \dash\get::index($value, 'unit'); ?></div>
+            </a>
+          </li>
+        <?php } //endfor ?>
+      </ul>
+    </nav>
 
+  </div>
+  <div class="c-xs-12 c-md-6">
+    <div class="box">
+      <div class="body">
+        <table class="tbl1 v4">
+          <tbody>
+            <tr class="active">
+              <td><?php echo T_("Ready to send to customer?") ?></td>
+              <td>
+                <div data-confirm data-data='{"setasready": "setasready"}' class="link"><?php echo T_("Set as ready"); ?></div>
+              </td>
+            </tr>
+            <tr class="positive">
+              <td><?php echo T_("Set Deliver to customer") ?></td>
+              <td>
+                <div data-confirm data-data='{"setdeliver": "setdeliver"}' class="link"><?php echo T_("Set as deliver"); ?></div>
+              </td>
+            </tr>
+            <tr class="disable">
+              <td><?php echo T_("Cancel order") ?></td>
+              <td>
+                <div data-confirm data-data='{"setascancel": "setascancel"}' class="link"><?php echo T_("Cancel"); ?></div>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <form method="post" autocomplete="off">
+      <div class="box">
+        <header><h2><?php echo T_("Add comment to this order") ?></h2></header>
+        <div class="body padLess">
+          <input type="hidden" name="orderaction" value="comment">
+          <div class="mB20">
+            <textarea id="desc" name="desc" class="txt" rows="3" placeholder="<?php echo T_("Only admin can see the comments") ?>"><?php echo \dash\data::dataRow_desc(); ?></textarea>
+          </div>
+
+          <div class="showAttachment" data-kerkere-content='hide'>
+
+            <div class="box" data-uploader data-name='file'>
+              <input type="file"  id="file1">
+              <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
+            </div>
+          </div>
+        </div>
+        <footer class="f">
+          <div class="cauto"><i data-kerkere='.showAttachment' class="sf-attach fs14"></i></div>
+          <div class="c"></div>
+          <div class="cauto"><button class="btn success"><?php echo T_("Add comment") ?></button></div>
+
+        </footer>
+      </div>
+    </form>
+
+    <h3><?php echo T_("Order Action list") ?></h3>
+    <nav class="items">
+      <ul>
+        <?php foreach (\dash\get::index($orderDetail, 'action') as $key => $value) {?>
+          <li>
+            <a class="f">
+              <div class="key"><?php if(\dash\get::index($value, 'action') === 'comment'){ echo \dash\get::index($value, 'desc'); }else{ echo \dash\get::index($value, 'action');}?></div>
+              <div class="value"><?php echo \dash\fit::date_time(\dash\get::index($value, 'datecreated'));?></div>
+            </a>
+          </li>
+        <?php } //endfor ?>
+      </ul>
+    </nav>
   </div>
 </div>
