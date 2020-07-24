1c338ac8e8ef096ac0b09cb82052fbcc2ad2e453
Reza <rm.biqarar@gmail.com>
2020-07-24 14:00:34 +0430

    Remove useless code. Stay from tejarak!

--- content/home/controller.php
+++ content/home/controller.php
@@ -4,45 +4,6 @@ namespace content\home;
 class controller
 {
 
-	/**
-	 * the static page to not run any query
-	 * and brand black list
-	 * @var        array
-	 */
-	public static  $static_pages =
-	[
-		'benefits',
-		'pricing',
-		'jibres',
-		'help',
-		'admin',
-		'enter',
-		'about',
-		'social-responsibility',
-		'terms',
-		'privacy',
-		'changelog',
-		'logo',
-		'contact',
-		'api',
-		// brand black list
-		'branch',
-		'team',
-		'member',
-		'add',
-		'edit',
-		'delete',
-		'user',
-		'hours',
-		'report',
-		'last',
-		'daily',
-		'account',
-		'for',
-		'files',
-
-	];
-
 	// for routing check
 	public static function routing()
 	{
@@ -64,17 +25,6 @@ class controller
 			}
 		}
 
-		// if on homepage return false
-		$url = \dash\url::directory();
-		if(!$url)
-		{
-			return false;
-		}
-		// if the url in static page [and black list] return
-		if(in_array($url, self::$static_pages))
-		{
-			return;
-		}
 
 	}
 }
