1e94ce75434cf573e02145bc473e0b215ea7b99b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-25 21:04:08 +0430

    try to bugfix

--- content_a/products/edit/display.php
+++ content_a/products/edit/display.php
@@ -5,7 +5,8 @@ $have_variant_child =\dash\data::productDataRow_variant_child();
 ?>
 
 
-<form class="jPage row" id='aProductData' method="post" autocomplete="off" data-refresh data-autoScroll2="#productGallery">
+<form class="jPage" id='aProductData' method="post" autocomplete="off" data-refresh data-autoScroll2="#productGallery">
+ <div class="row">
   <button class="hide" name="submitall" type="submit" value="master"><?php echo T_("Save"); ?></button>
   <div class="c-xs-12 c-sm-12 c-md-8 c-xxl-9">
     <section class="box">
@@ -375,5 +376,6 @@ $have_variant_child =\dash\data::productDataRow_variant_child();
 
 <?php require_once (root. 'content_a/products/edit/display-sidebar-menu.php'); ?>
 
-</div>
+  </div>
+ </div>
 </form>
