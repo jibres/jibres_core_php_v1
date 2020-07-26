61a04a822a05dc166ae939827692f3e2a62662da
Reza <rm.biqarar@gmail.com>
2020-07-26 12:59:47 +0430

    fix domain

--- content_my/domain/home/display.php
+++ content_my/domain/home/display.php
@@ -73,7 +73,7 @@
      </div>
 
      <div class="c pRa10">
-      <a href="<?php echo \dash\url::kindgom() ?>/account/billing?from=domain" class="stat<?php if(\dash\get::index($myData, 'user_budget')>0) echo " green"; ?>">
+      <a href="<?php echo \dash\url::kingdom(); ?>/account/billing?from=domain" class="stat<?php if(\dash\get::index($myData, 'user_budget')>0) echo " green"; ?>">
        <h3><?php echo T_("Your Current Balance");?> <small>(<?php echo \lib\currency::unit(); ?>)</small></h3>
        <div class="val"><?php echo \dash\fit::number(\dash\get::index($myData, 'user_budget'));?></div>
       </a>
