f6db2f0378136298f09281486ac87493a068f72a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-26 18:45:54 +0430

    fix search placeholder

--- content_my/domain/buy/display-search.php
+++ content_my/domain/buy/display-search.php
@@ -7,7 +7,7 @@
         <p><?php echo T_("Every website start with a great domain name"); ?></p>
         <form method="get" action='<?php echo \dash\url::current() ?>' autocomplete='off'>
           <div class="input">
-            <input type="search" name="q" autocomplete="off" maxlength="65" value="<?php echo \dash\data::getDomain(); ?>" placeholder='<?php echo T_('Enter your idea for domain name') ?>' autofocus>
+            <input type="search" name="q" autocomplete="off" maxlength="65" value="<?php echo \dash\data::getDomain(); ?>" <?php if (!\dash\detect\device::detectPWA()) {echo "autofocus";} ?> >
             <button class="addon btn warn"><?php echo T_("Search"); ?></button>
           </div>
         </form>
