5ae850af0d890d59edf9351b2372730de367e2a0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-30 14:04:03 +0430

    try2

--- dash/engine/guard.php
+++ dash/engine/guard.php
@@ -101,7 +101,7 @@ class guard
 			}
 			else
 			{
-				$csp .= "frame-ancestors self ". self::csp_domain('*', 'jibres'). "; ";
+				$csp .= "frame-ancestors self ". self::csp_domain('*', 'jibres'). " ". \dash\url::site() "; ";
 				// $csp .= "frame-ancestors 'none'; ";
 			}
 		}
