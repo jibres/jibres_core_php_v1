74e75710f82b3d3f4a64fe7d6dddd79d05c367b1
Reza <rm.biqarar@gmail.com>
2020-07-31 19:37:39 +0430

    fix email

--- content_account/my/email/display.php
+++ content_account/my/email/display.php
@@ -18,28 +18,30 @@
 
   <?php if(\dash\data::dataTable() && is_array(\dash\data::dataTable())) {?>
     <?php foreach (\dash\data::dataTable() as $key => $value) {?>
-    <div class="box">
-      <div class="body">
-        <div class="txtL ltr txtB fs14"><?php echo \dash\get::index($value, 'email') ?> <?php if(\dash\get::index($value, 'verify')) {?><i class="sf-check fc-green"></i><?php }// endif ?></div>
-        <?php if(!\dash\get::index($value, 'verify')) {?>
+      <div class="box">
+        <div class="body">
+          <div class="txtL ltr txtB fs14"><?php echo \dash\get::index($value, 'email') ?> <?php if(\dash\get::index($value, 'verify')) {?><i class="sf-check fc-green"></i><?php }// endif ?></div>
+          <?php if(!\dash\get::index($value, 'verify')) {?>
 
-          <div class="btn link" data-confirm data-data='{"verify":"verify","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><?php echo T_("Send verify email") ?></div>
+            <div class="btn link" data-confirm data-data='{"verify":"verify","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><?php echo T_("Send verify email") ?></div>
 
-      <?php } //endif ?>
-        <?php if(!\dash\get::index($value, 'primary')) {?>
-          <div class="btn link" data-confirm data-data='{"primary":"primary","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><?php echo T_("Set as primary email") ?></div>
-      <?php }else{ ?>
-        <div class="badge success"><?php echo T_("Primary email") ?></div>
-      <?php } //endif ?>
-      </div>
-      <footer>
-        <div class="f">
-          <div class="cauto"><div data-confirm data-data='{"remove":"remove","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><i class="sf-trash fc-red fs14"></i></div></div>
-          <div class="c"></div>
-
+          <?php } //endif ?>
+          <?php if(\dash\get::index($value, 'verify')) {?>
+            <?php if(!\dash\get::index($value, 'primary') ) {?>
+              <div class="btn link" data-confirm data-data='{"primary":"primary","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><?php echo T_("Set as primary email") ?></div>
+            <?php }else{ ?>
+              <div class="badge success"><?php echo T_("Primary email") ?></div>
+            <?php } //endif ?>
+          <?php } //endif ?>
         </div>
-      </footer>
-    </div>
+        <footer>
+          <div class="f">
+            <div class="cauto"><div data-confirm data-data='{"remove":"remove","email":"<?php echo \dash\get::index($value, 'email') ?>" }'><i class="sf-trash fc-red fs14"></i></div></div>
+            <div class="c"></div>
+
+          </div>
+        </footer>
+      </div>
     <?php }//endfor ?>
   <?php } // endif ?>
 </div>
