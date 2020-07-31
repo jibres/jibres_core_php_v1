f9d191325cfc813a71b9f82d46f056061698c4a1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-31 20:04:40 +0430

    set mode on all pages

--- content_business/home/view.php
+++ content_business/home/view.php
@@ -6,21 +6,7 @@ class view
 {
 	public static function config()
 	{
-		// @todo reza use new class to only load and return store
-		\dash\layout\business::check_website();
 
-		switch (\dash\data::website_template())
-		{
-			case 'comingsoon':
-				break;
-
-			case 'visitcard':
-				\dash\face::css('business/visitcard-1/visitcard-1.css');
-				break;
-
-			default:
-				break;
-		}
 	}
 }
 ?>
