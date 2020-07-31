a12005ef711c905b609e285a03b690d1c017d65a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-07-31 18:35:24 +0430

    complete visit cart

--- content_business/home/layout/template/visitcard.php
+++ content_business/home/layout/template/visitcard.php
@@ -1,53 +1,32 @@
-  <div class="container">
-    <div class="box">
-      <div class="avatar">
-        <svg class="eyes" viewBox="472 333 249 30" version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink">
-          <defs></defs>
-          <path d="M472.247316,356.875752 C472.247316,356.875752 482.42051,362.696475 509.581058,362.696475 C536.741606,362.696475 553.07185,356.875752 553.07185,356.875752 C553.07185,356.875752 549.096455,349.99405 540.005959,345.061903 C532.007515,340.722261 515.480841,338.359617 502.769724,340.195049 C475.612002,344.116513 472.247316,356.875752 472.247316,356.875752 Z"
-           id="Path" stroke="none"></path>
-          <path d="M658.531652,350.802077 C658.531652,350.802077 658.383272,340.747587 676.992911,335.759203 C685.035875,333.603257 689.64968,332.158523 704.838832,334.221707 C720.027984,336.284891 720.767744,342.54934 720.767744,342.54934 C720.767744,342.54934 717.391753,360.239528 696.13389,360.239528 C688.756232,360.239528 681.970391,360.102811 676.254201,358.618384 C665.499856,355.825607 658.531652,350.802077 658.531652,350.802077 Z"
-           id="Path-2" stroke="none"></path>
-        </svg>
-        <img src="<?php echo \lib\store::logo() ?>" alt="<?php echo \lib\store::title(); ?>">
-      </div>
-      <div class="info">
-        <h1><?php echo \lib\store::title(); ?></h1>
-        <h2>
-          <a href="//archive.worldskills.org/2011london/info/teams/iran/index.html">WorldSkills Champion</a>, Software Developer,
-          <br class="br">
-          <a href="//cs.uni-paderborn.de/en/">Master’s Student in Computer Science</a>
-        </h2>
-        <div class="text">
-          <h1>
-            سامان سلطانی - Saman Soltani
-          </h1>
-          <h2>برنامه نویس و توسعه دهنده وب</h2> سایت شخصی سامان سلطانی، برنامه نویس و توسعه دهنده وب، پیاده سازی پروژهای نرم افزاری
-          برروی اینترنت با استفاده از فناوری های جدید PHP/MySQL JavaScript, jQuery, HTML5, CSS3, Web, Web design, develop, web
-          developer, Saman Soltani
-        </div>
-        <div class="social">
-          <a href="//linkedin.com/in/samansoltani">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin-button.svg" alt="linkedin-logo">
-          </a>
-          <a href="//github.com/saman">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/github.svg" alt="github-logo">
-          </a>
-          <a href="//facebook.com/samansoltani">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/facebook-logo-button.svg" alt="facebook-logo">
-          </a>
-          <a href="//instagram.com/samansoltani">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/instagram-logo.svg" alt="instagram-logo">
-          </a>
-          <a href="//twitter.com/saman_soltani">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/twitter.svg" alt="twitter-logo">
-          </a>
-          <a href="//t.me/samansoltani">
-            <img class="telegram" src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/telegram.svg" alt="telegram-logo">
-          </a>
-          <a href="/cdn-cgi/l/email-protection#80ede5c0f3e1ede1eef3efecf4e1eee9aee3efed">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/arroba.svg" alt="arroba-logo">
-          </a>
-        </div>
+  <div class="visitBox">
+    <img class="avatarImg" src="<?php echo \lib\store::logo() ?>" alt="<?php echo \lib\store::title(); ?>">
+    <div class="info">
+      <h1><?php echo \lib\store::title(); ?></h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</h2>
+
+      <div class="social">
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("linkedin"); ?>">
+        </a>
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/github.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("github"); ?>">
+        </a>
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/facebook.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("facebook"); ?>">
+        </a>
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/instagram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("instagram"); ?>">
+        </a>
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/twitter.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("twitter"); ?>">
+        </a>
+        <a href="">
+          <img class="telegram" src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/telegram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("telegram"); ?>">
+        </a>
+        <a href="">
+          <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/arroba.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("arroba"); ?>">
+        </a>
       </div>
     </div>
   </div>
