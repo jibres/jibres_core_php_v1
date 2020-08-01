eda0f931a1ecccd3caa31cfa55c2e8094ab6100a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-01 11:09:03 +0430

    add all social links into new page

--- content_business/home/layout/template/visitcard.php
+++ content_business/home/layout/template/visitcard.php
@@ -11,29 +11,29 @@
 <?php $social = \lib\store::social(); ?>
         <div class="social">
 <?php if(false) {?>
-          <a href="">
+          <a target="_blank" href="">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("linkedin"); ?>">
           </a>
-          <a href="">
+          <a target="_blank" href="">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/github.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("github"); ?>">
           </a>
-          <a href="">
+          <a target="_blank" href="">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/facebook.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("facebook"); ?>">
           </a>
-          <a href="">
+          <a target="_blank" href="">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/twitter.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("twitter"); ?>">
           </a>
-          <a href="">
+          <a target="_blank" href="">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/arroba.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("Email"); ?>">
           </a>
 <?php } //endif ?>
 <?php if(\dash\get::index($social, 'instagram')) {?>
-          <a href="<?php echo \dash\get::index($social, 'instagram', 'link'); ?>">
+          <a target="_blank" href="<?php echo \dash\get::index($social, 'instagram', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/instagram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("instagram"); ?>">
           </a>
 <?php } // ?>
 <?php if(\dash\get::index($social, 'telegram')) {?>
-          <a href="<?php echo \dash\get::index($social, 'telegram', 'link'); ?>">
+          <a target="_blank" href="<?php echo \dash\get::index($social, 'telegram', 'link'); ?>">
             <img class="telegram" src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/telegram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("telegram"); ?>">
           </a>
 <?php } // ?>
