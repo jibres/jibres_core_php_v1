aead478bde559d971e895f770fa5960de960e6fd
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-01 14:59:38 +0430

    fix

--- content_a/website/status/display.php
+++ content_a/website/status/display.php
@@ -5,17 +5,18 @@
           <header><h2><?php echo T_("Change website status"); ?></h2></header>
           <div class="body">
             <p><?php echo T_("Choose your website status"); ?></p>
-            <div class="radio3">
+
+            <div class="radio3 mB10">
               <input type="radio" name="status" value="publish" id="typepublish" <?php if(\dash\data::websiteStatus() === 'publish') {echo 'checked';} ?>>
               <label for="typepublish"><?php echo T_("Shoping website"); ?></label>
             </div>
 
-            <div class="radio3">
+            <div class="radio3 mB10">
               <input type="radio" name="status" value="visitcard" id="typevisitcard" <?php if(\dash\data::websiteStatus() === 'visitcard') {echo 'checked';} ?>>
               <label for="typevisitcard"><?php echo T_("Visit Card website"); ?></label>
             </div>
 
-            <div class="radio3">
+            <div class="radio3 mB10">
               <input type="radio" name="status" value="comingsoon" id="typecomingsoon" <?php if(\dash\data::websiteStatus() === 'comingsoon') {echo 'checked';} ?>>
               <label for="typecomingsoon"><?php echo T_("Coming Soon page"); ?></label>
             </div>
