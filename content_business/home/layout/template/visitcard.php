7a4a29a5700ba549359cd0b95d0346f37555a87e
Reza <rm.biqarar@gmail.com>
2020-08-02 11:27:11 +0430

    Fix visitcart

--- content_business/home/layout/template/visitcard.php
+++ content_business/home/layout/template/visitcard.php
@@ -15,37 +15,37 @@
 <?php } ?>
 <?php $social = \lib\store::social(); ?>
         <div class="social">
-<?php if(\dash\get::index($social, 'linkedin')) {?>
+<?php if(\dash\get::index($social, 'linkedin', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'linkedin', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("linkedin"); ?>">
           </a>
 <?php } //endif ?>
-<?php if(\dash\get::index($social, 'github')) {?>
+<?php if(\dash\get::index($social, 'github', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'github', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/github.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("github"); ?>">
           </a>
 <?php } //endif ?>
-<?php if(\dash\get::index($social, 'facebook')) {?>
+<?php if(\dash\get::index($social, 'facebook', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'facebook', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/facebook.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("facebook"); ?>">
           </a>
 <?php } //endif ?>
-<?php if(\dash\get::index($social, 'twitter')) {?>
+<?php if(\dash\get::index($social, 'twitter', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'twitter', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/twitter.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("twitter"); ?>">
           </a>
 <?php } //endif ?>
-<?php if(\dash\get::index($social, 'email')) {?>
+<?php if(\dash\get::index($social, 'email', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'email', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/arroba.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("Email"); ?>">
           </a>
 <?php } //endif ?>
-<?php if(\dash\get::index($social, 'instagram')) {?>
+<?php if(\dash\get::index($social, 'instagram', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'instagram', 'link'); ?>">
             <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/instagram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("instagram"); ?>">
           </a>
 <?php } // ?>
-<?php if(\dash\get::index($social, 'telegram')) {?>
+<?php if(\dash\get::index($social, 'telegram', 'user')) {?>
           <a target="_blank" href="<?php echo \dash\get::index($social, 'telegram', 'link'); ?>">
             <img class="telegram" src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/telegram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("telegram"); ?>">
           </a>
