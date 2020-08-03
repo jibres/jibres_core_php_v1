f59c65a074b04ed862a0fad86166103a3fd49ec1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-03 12:32:27 +0430

    fix design of uploader

--- content_a/setup/logo/display.php
+++ content_a/setup/logo/display.php
@@ -8,14 +8,19 @@
 
       <form method="post" enctype="multipart/form-data" autocomplete="off">
 
-        <img id="finalImage" src="<?php echo \dash\data::dataRow_logo(); ?>" class="mB20-f">
 
-        <div class="box" data-uploader data-name='logo' data-ratio="1" data-final='#finalImage'>
+        <div data-uploader data-name='logo' data-ratio="1" data-final='#finalImage'<?php if(\dash\data::dataRow_logo()) { echo " data-fill"; }?>>
           <input type="file" accept="image/jpeg, image/png" id="image1">
           <label for="image1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
+<?php if(\dash\data::dataRow_logo()) {?>
+          <label for="image1">
+            <img id="finalImage" class="mA0-f" src="<?php echo \dash\data::dataRow_logo(); ?>">
+          </label>
+          <span class="imageDel" data-confirm data-data='{"deletefile" : 1}'></span>
+<?php }?>
         </div>
 
-        <div class="f align-center mB10">
+        <div class="f align-center mB10 mT20">
           <div class="c fc-mute"><?php echo \dash\data::stepDesc(); ?></div>
           <div class="cauto os pRa10"><button class="btn outline secondary" name="skip" value="skip"><?php echo T_("Skip"); ?></button></div>
           <div class="cauto os"><button class="btn primary"><?php echo T_("Save"); ?></button></div>
