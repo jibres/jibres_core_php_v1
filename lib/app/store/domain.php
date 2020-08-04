7db408e8404f167dfd64628d4efac1434e886c52
Reza <rm.biqarar@gmail.com>
2020-08-04 13:31:10 +0430

    comment ssl request

--- lib/app/store/domain.php
+++ lib/app/store/domain.php
@@ -841,7 +841,7 @@ class domain
 							"ar_wildcard" => true,
 						];
 
-						$set_https = \lib\arvancloud\api::set_arvan_request($domain, $add_https_args);
+						// $set_https = \lib\arvancloud\api::set_arvan_request($domain, $add_https_args);
 
 						\lib\db\store_domain\update::record(['message' => 'request of https was sended', 'sslrequestdate' => date("Y-m-d H:i:s"), 'datemodified' => date("Y-m-d H:i:s")], $store_domain_id);
 					}
