f6960725de4bdbccb39b80057f92f4442ba5a651
Reza <rm.biqarar@gmail.com>
2020-08-05 17:33:49 +0430

    fix

--- content_love/store/domain/detail/display.php
+++ content_love/store/domain/detail/display.php
@@ -57,10 +57,10 @@
         <tr>
           <td><?php echo T_("Raw request") ?></td>
           <td>
-            <a class="btn link" href="<?php echo \dash\url::current(). '?rawrequest=https&domain='. \dash\get::index($value, 'domain') ?>">Get https</a>
-            <a class="btn link" href="<?php echo \dash\url::current(). '?rawrequest=get_domain&domain='. \dash\get::index($value, 'domain') ?>">Get get_domain</a>
-            <a class="btn link" href="<?php echo \dash\url::current(). '?rawrequest=get_ns_key&domain='. \dash\get::index($value, 'domain') ?>">Get get_ns_key</a>
-            <a class="btn link" href="<?php echo \dash\url::current(). '?rawrequest=get_dns_record&domain='. \dash\get::index($value, 'domain') ?>">Get get_dns_record</a>
+            <a class="btn link" href="<?php echo \dash\url::current(). '?id='. \dash\request::get('id').'&rawrequest=https&domain='. \dash\get::index($value, 'domain') ?>">Get https</a>
+            <a class="btn link" href="<?php echo \dash\url::current(). '?id='. \dash\request::get('id').'&rawrequest=get_domain&domain='. \dash\get::index($value, 'domain') ?>">Get get_domain</a>
+            <a class="btn link" href="<?php echo \dash\url::current(). '?id='. \dash\request::get('id').'&rawrequest=get_ns_key&domain='. \dash\get::index($value, 'domain') ?>">Get get_ns_key</a>
+            <a class="btn link" href="<?php echo \dash\url::current(). '?id='. \dash\request::get('id').'&rawrequest=get_dns_record&domain='. \dash\get::index($value, 'domain') ?>">Get get_dns_record</a>
           </td>
         </tr>
 
