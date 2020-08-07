96838c9e7052e18ad36df8003565f2e9d35c62be
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-07 18:47:34 +0430

    improve html

--- content_a/category/sort/display.php
+++ content_a/category/sort/display.php
@@ -11,12 +11,10 @@ if(!is_array($dataTable))
 <?php $loopTitle = \dash\get::index($value, 'title'); ?>
 
   <div class="roundedBox" data-handle>
-    <div class="overlay">
-      <figure>
-        <img src="<?php echo \dash\get::index($value, 'file'); ?>" alt="<?php echo $loopTitle; ?>">
-        <figcaption><h2><?php echo $loopTitle; ?></h2></figcaption>
-      </figure>
-    </div>
+    <figure class="overlay">
+      <img src="<?php echo \dash\get::index($value, 'file'); ?>" alt="<?php echo $loopTitle; ?>">
+      <figcaption><h2><?php echo $loopTitle; ?></h2></figcaption>
+    </figure>
   </div>
 <?php } //endfor ?>
 </div>
