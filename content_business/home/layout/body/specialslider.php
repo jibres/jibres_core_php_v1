c88122dddb40a836a7cc46284c1242d1fddcd5e0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-08 12:40:57 +0430

    fix special slider

--- content_business/home/layout/body/specialslider.php
+++ content_business/home/layout/body/specialslider.php
@@ -12,11 +12,11 @@ if(isset($line_detail['value']['specialslider']) && is_array($line_detail['value
 <?php if($specialslider && count($specialslider) >= 5) {?>
 <div class="avand">
 
-  <div class="row padLess roundedBox">
+  <div class="row padLess">
     <div class="c-xs-12 c-sm-12 c-lg-6">
     	<?php if(count($specialslider) === 5) {?>
-			<a class="overlay"<?php if(\dash\get::index($specialslider, 0, 'url')) { echo ' href="'.  \dash\get::index($specialslider, 0, 'url'). '"'; if(\dash\get::index($specialslider, 0, 'target')) { echo ' target="_blank"'; }} ?>>
-            <figure>
+			<a class="roundedBox"<?php if(\dash\get::index($specialslider, 0, 'url')) { echo ' href="'.  \dash\get::index($specialslider, 0, 'url'). '"'; if(\dash\get::index($specialslider, 0, 'target')) { echo ' target="_blank"'; }} ?>>
+            <figure class="overlay">
 			  			<img src="<?php echo \lib\filepath::fix(\dash\get::index($specialslider, 0, 'image')); ?>" alt="<?php echo \dash\get::index($specialslider, 0, 'alt'); ?>">
 	            <figcaption><h2><?php echo \dash\get::index($specialslider, 0, 'alt'); ?></h2></figcaption>
             </figure>
@@ -28,8 +28,8 @@ if(isset($line_detail['value']['specialslider']) && is_array($line_detail['value
 	    		$count = 0;
 	    		?>
 				<?php foreach ($specialslider as $key => $value) { $count++;?>
-					 <a class="overlay"<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
-					 	<figure>
+					 <a class="roundedBox"<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
+					 	<figure class="overlay">
 			    		<img src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'alt'); ?>">
 	             <figcaption><h2><?php echo \dash\get::index($value, 'alt'); ?></h2></figcaption>
 			    	</figure>
@@ -39,12 +39,12 @@ if(isset($line_detail['value']['specialslider']) && is_array($line_detail['value
     	<?php } //endif ?>
 
     </div>
-    <div class="c-xs-12 c-sm-12 c-lg-6">
+    <div class="c-xs-12 c-sm-12 c-lg-6 ">
       <div class="row padLess">
         <?php foreach ($specialslider as $key => $value) {?>
 	        <div class="c-6">
-	          <a class="overlay"<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
-	            <figure>
+	          <a class="roundedBox"<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
+	            <figure class="overlay">
 	              <img src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'alt'); ?>">
 	              <figcaption><h2><?php echo \dash\get::index($value, 'alt'); ?></h2></figcaption>
 	            </figure>
