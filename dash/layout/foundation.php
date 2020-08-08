ab103c14cf698ef98d9f196c0aae764ce58a3a55
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-08 15:31:13 +0430

    test pwa splash

--- dash/layout/foundation.php
+++ dash/layout/foundation.php
@@ -87,6 +87,15 @@ if(\dash\data::googleAnalytics())
  <link href="<?php echo \dash\url::cdn();?>/favicons/favicon.ico?v=1" rel="shortcut icon"/>
  <link href="<?php echo \dash\url::logo();?>" rel="apple-touch-startup-image"/>
  <link href="<?php echo \dash\url::kingdom();?>/manifest.webmanifest" rel="manifest"/>
+
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+  <link rel="apple-touch-startup-image" href="https://pwa-splash.now.sh/images/splash/launch-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
 <?php }?>
 <?php
 if (\dash\url::canonical())
