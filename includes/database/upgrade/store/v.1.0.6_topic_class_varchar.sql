6fcd2a3a11f5bc614a2d11fc3e6dfb664a92684b
Reza <rm.biqarar@gmail.com>
2020-08-13 13:30:37 +0430

    update coding topic and class to varchar

---
+++ includes/database/upgrade/store/v.1.0.6_topic_class_varchar.sql
@@ -0,0 +1,2 @@
+ALTER TABLE jibres_XXXXXXX.tax_coding CHANGE `topic` `topic` varchar(200) NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.tax_coding CHANGE `class` `class` varchar(200) NULL DEFAULT NULL;
