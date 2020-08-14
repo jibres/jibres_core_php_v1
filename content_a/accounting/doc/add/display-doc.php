eda27b9ae37ccfc0322392885293a43c18789ed6
Reza <rm.biqarar@gmail.com>
2020-08-14 16:38:57 +0430

    Some fix

---
+++ content_a/accounting/doc/add/display-doc.php
@@ -0,0 +1,69 @@
+
+<form method="post" autocomplete="off">
+  <div class="avand-lg">
+    <div class="box">
+      <header><h2><?php echo T_("Add new accounting doc") ?></h2></header>
+      <div class="body">
+
+        <?php $defaultYear = null; ?>
+
+        <?php if(\dash\data::accountingYear()) {?>
+          <label for="parent"><?php echo T_("Accounting year") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
+          <select class="select22" name="year_id" <?php foreach (\dash\data::accountingYear() as $key => $value) { if((!\dash\data::dataRow_id() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { $defaultYear = \dash\get::index($value, 'id'); echo 'disabled';} }?>>
+            <option value=""><?php echo T_("Please choose year") ?></option>
+            <?php foreach (\dash\data::accountingYear() as $key => $value) {?>
+              <option value="<?php echo \dash\get::index($value, 'id') ?>" <?php if((!\dash\data::dataRow_id() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { echo 'selected';} ?>><?php echo \dash\get::index($value, 'title'); ?></option>
+            <?php } // endfor ?>
+          </select>
+        <?php }else{ ?>
+          <div class="msg warn2"><a class="btn link" href="<?php echo \dash\url::this(). '/year/add' ?>"><?php echo T_("Add new accounting year") ?></a></div>
+        <?php } // endif ?>
+
+        <?php if($defaultYear) {?>
+          <input type="hidden" name="year_id" value="<?php echo $defaultYear; ?>">
+        <?php } //endif ?>
+
+        <div class="row">
+          <div class="c-sm-6">
+            <label for="number"><?php echo T_("Number") ?></label>
+            <div class="input">
+              <input type="number" min="1" max="999999999999999999" name="number" id="number" required  value="<?php echo \dash\data::dataRow_number() ?>">
+            </div>
+          </div>
+          <div class="c-sm-6">
+            <label for="date" ><?php echo T_("Date"); ?> <b><?php echo T_("yyyy/mm/dd"); ?></b></label>
+        		<div class="input">
+        		<input class="ltr" type="text" placeholder="yyyy/mm/dd" data-format="date" name="date" id="date" value="<?php echo \dash\utility\convert::to_en_number(\dash\fit::date(\dash\data::dataRow_date())); ?>" autocomplete='off' required>
+        		</div>
+          </div>
+        </div>
+
+
+
+        <label for="desc"><?php echo T_("Description") ?></label>
+        <div class="input">
+          <input type="text" name="desc" id="desc" value="<?php echo \dash\data::dataRow_desc() ?>">
+        </div>
+
+
+
+      </div>
+      <?php if(\dash\data::editMode()) {?>
+      <footer class="f">
+        <div class="cauto">
+          <div data-confirm data-data='{"remove": "remove"}' class="btn danger"><?php echo T_("Remove") ?></div>
+        </div>
+        <div class="c"></div>
+        <div class="cauto">
+          <button class="btn success"><?php echo T_("Edit") ?></button>
+        </div>
+
+      </footer>
+        <?php }else{ ?>
+      <footer class="txtRa">
+          <button class="btn master"><?php echo T_("Add") ?></button>
+      </footer>
+      <?php } //endif ?>
+    </div>
+  </div>
+</form>
