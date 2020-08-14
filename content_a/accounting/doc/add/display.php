4e396476281232370ee0d5fceaf9eaa1759aef87
Reza <rm.biqarar@gmail.com>
2020-08-14 16:09:42 +0430

    Bug fix choose accounting year

--- content_a/accounting/doc/add/display.php
+++ content_a/accounting/doc/add/display.php
@@ -13,10 +13,10 @@
 
         <?php if(\dash\data::accountingYear()) {?>
           <label for="parent"><?php echo T_("Accounting year") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
-          <select class="select22" name="year_id" <?php foreach (\dash\data::accountingYear() as $key => $value) { if((!\dash\data::dataRow() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { $defaultYear = \dash\get::index($value, 'id'); echo 'disabled';} }?>>
+          <select class="select22" name="year_id" <?php foreach (\dash\data::accountingYear() as $key => $value) { if((!\dash\data::dataRow_id() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { $defaultYear = \dash\get::index($value, 'id'); echo 'disabled';} }?>>
             <option value=""><?php echo T_("Please choose year") ?></option>
             <?php foreach (\dash\data::accountingYear() as $key => $value) {?>
-              <option value="<?php echo \dash\get::index($value, 'id') ?>" <?php if((!\dash\data::dataRow() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { echo 'selected';} ?>><?php echo \dash\get::index($value, 'title'); ?></option>
+              <option value="<?php echo \dash\get::index($value, 'id') ?>" <?php if((!\dash\data::dataRow_id() && \dash\get::index($value, 'isdefault')) || (\dash\get::index($value, 'id') === \dash\data::dataRow_year_id())) { echo 'selected';} ?>><?php echo \dash\get::index($value, 'title'); ?></option>
             <?php } // endfor ?>
           </select>
         <?php }else{ ?>
