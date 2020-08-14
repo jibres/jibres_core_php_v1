c9e358c6206183a7ab05315f13ba2ba7ac0400e1
Reza <rm.biqarar@gmail.com>
2020-08-14 15:23:00 +0430

    Fix notif

--- content_crm/member/avatar/model.php
+++ content_crm/member/avatar/model.php
@@ -32,6 +32,7 @@ class model
 			// we have an error in upload avatar
 			if($file_url === false)
 			{
+				\dash\notif::warn(T_("To change user avatar choose a new avatar"));
 				return false;
 			}
 
