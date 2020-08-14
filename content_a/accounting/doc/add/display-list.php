8fd3fe804d01fa9ca01a0e2c8c389348be16bd85
Reza <rm.biqarar@gmail.com>
2020-08-14 13:44:28 +0430

    update translate

--- content_a/accounting/doc/add/display-list.php
+++ content_a/accounting/doc/add/display-list.php
@@ -1,6 +1,6 @@
 <?php if(\dash\data::editMode() && \dash\data::docDetail()) {?>
   <?php if( \dash\data::summary_debtor() && \dash\data::summary_creditor() && floatval(\dash\data::summary_debtor()) === floatval(\dash\data::summary_creditor())) {?>
-    <div class="msg success txtB txtC fs14"><?php echo T_("Document balance!") ?></div>
+    <div class="msg success txtB txtC fs14"><?php echo T_("Document balance") ?></div>
   <?php }else{ ?>
     <div class="msg danger txtB txtC fs14"><?php echo T_("Accounting document is not balance!") ?></div>
   <?php }//endif ?>
