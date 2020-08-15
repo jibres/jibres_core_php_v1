0b6f1819d73a4bb01f20cf7b53d948052ccac38c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-15 18:36:09 +0430

    add su links inside dashboard

--- content_su/home/display.php
+++ content_su/home/display.php
@@ -60,5 +60,37 @@
 </div>
 
 
+<nav class="font-12">
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/backup"><?php echo T_("Backup"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/dbtables"><?php echo T_("Raw table"); ?></a>
+
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/time"><?php echo T_("Date and time"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/info"><?php echo T_("Server information"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/cronjob"><?php echo T_("Cronjob"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/ip"><?php echo T_("IP"); ?></a>
+
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/gitstatus"><?php echo T_("Git status"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/nano"><?php echo T_("Nano"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/update" data-shortkey="85+80" data-shortkey-timeout='500'><?php echo T_("Update"); ?><kbd class="floatLa mRa10 fs08">up</kbd></a>
+
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/log"><?php echo T_("Log"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/tempfile"><?php echo T_("Temp file"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/apilog"><?php echo T_("Api Log"); ?></a>
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/smsclient"><?php echo T_("Sms client"); ?></a>
+
+    <?php if(\dash\url::isLocal()) {?>
+
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/permission"><?php echo T_("Permission"); ?></a>
+
+    <?php } //endif ?>
+
+    <a class="btn outline mA5" href="<?php echo \dash\url::here(); ?>/tg" data-shortkey="84+71" data-shortkey-timeout='500'><?php echo T_("Telegram"); ?><kbd class="floatLa mRa10 fs08">tg</kbd></a>
+
+</nav>
+
+
+
+
+
 
 
