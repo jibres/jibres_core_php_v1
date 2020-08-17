83cda0ec597bcfa43009b9be535ae28e370639d1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-17 07:44:07 +0430

    fix report

--- content_a/accounting/report/detail/display.php
+++ content_a/accounting/report/detail/display.php
@@ -10,10 +10,10 @@
           <th><?php echo T_("Accounting total") ?></th>
           <th><?php echo T_("Accounting assistant") ?></th>
           <th><?php echo T_("Accounting details") ?></th>
-          <th><?php echo T_("Debtor") ?></th>
-          <th><?php echo T_("Creditor") ?></th>
-          <th><?php echo T_("Remain Debtor") ?></th>
-          <th><?php echo T_("Remain Creditor") ?></th>
+          <th class="txtRa"><?php echo T_("Debtor") ?></th>
+          <th class="txtRa"><?php echo T_("Creditor") ?></th>
+          <th class="txtRa"><?php echo T_("Remain Debtor") ?></th>
+          <th class="txtRa"><?php echo T_("Remain Creditor") ?></th>
         </tr>
       </thead>
       <tbody>
@@ -23,10 +23,10 @@
             <td><?php echo \dash\get::index($value, 'total_title') ?></td>
             <td><?php echo \dash\get::index($value, 'assistant_title') ?></td>
             <td><?php echo \dash\get::index($value, 'details_title') ?></td>
-            <td class="font-12 fc-red"><?php echo \dash\fit::number(\dash\get::index($value, 'debtor')) ?></td>
-            <td class="font-12 fc-green"><?php echo \dash\fit::number(\dash\get::index($value, 'creditor')) ?></td>
-            <td class="font-12 fc-red"><?php echo \dash\fit::number(\dash\get::index($value, 'remain_debtor')) ?></td>
-            <td class="font-12 fc-green"><?php echo \dash\fit::number(\dash\get::index($value, 'remain_creditor')) ?></td>
+            <td class="font-12 txtRa ltr fc-red"><?php echo \dash\fit::number(\dash\get::index($value, 'debtor')) ?></td>
+            <td class="font-12 txtRa ltr fc-green"><?php echo \dash\fit::number(\dash\get::index($value, 'creditor')) ?></td>
+            <td class="font-12 txtRa ltr fc-red"><?php echo \dash\fit::number(\dash\get::index($value, 'remain_debtor')) ?></td>
+            <td class="font-12 txtRa ltr fc-green"><?php echo \dash\fit::number(\dash\get::index($value, 'remain_creditor')) ?></td>
           </tr>
         <?php } //endif ?>
       </tbody>
