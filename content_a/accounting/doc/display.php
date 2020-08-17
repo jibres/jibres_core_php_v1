955ffcb0df576dfc91d612f8606db7f74034c15c
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-17 13:24:01 +0430

    fix doc list

--- content_a/accounting/doc/display.php
+++ content_a/accounting/doc/display.php
@@ -1,31 +1,30 @@
-<div class="avand">
 	<?php if(\dash\data::dataTable()) {?>
-		<table class="tbl1 v1 fs12">
+		<table class="tbl1 v6 font-12">
 			<thead>
 				<tr>
-					<th class="collapsing"><?php echo T_("Date") ?></th>
-					<th class="collapsing"><?php echo T_("Number") ?></th>
-
-					<th><?php echo T_("Description") ?></th>
+					<th><?php echo T_("Number") ?></th>
+					<th><?php echo T_("Date") ?></th>
+					<th><?php echo T_("Status") ?></th>
+					<th><?php echo T_("Item count") ?></th>
 					<th><?php echo T_("Total debtor") ?></th>
 					<th><?php echo T_("Total creditor") ?></th>
-					<th><?php echo T_("Item count") ?></th>
-					<th><?php echo T_("Status") ?></th>
-					<th class="collapsing"><?php echo T_("Edit") ?></th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach (\dash\data::dataTable() as $key => $value) {?>
-					<tr>
-						<td class="collapsing"><?php echo \dash\fit::date(\dash\get::index($value, 'date')) ?></td>
-						<td class="collapsing"><?php echo \dash\fit::number(\dash\get::index($value, 'number')) ?></td>
+					<tr class="font-12">
+						<td class="font-14">
+							<a class="btn link" href="<?php echo \dash\url::that(). '/edit?id='. \dash\get::index($value, 'id'); ?>">#<?php echo \dash\fit::number(\dash\get::index($value, 'number'), true, 'en'); ?></a>
+						</td>
+						<td class=""><?php echo \dash\fit::date(\dash\get::index($value, 'date')) ?></td>
+						<td class=""><?php echo T_(\dash\get::index($value, 'status')) ?></td>
+						<td class=""><?php echo \dash\fit::number(\dash\get::index($value, 'item_count')) ?></td>
 
-						<td><?php echo \dash\get::index($value, 'desc') ?></td>
-						<td class="collapsing"><span class="txtR txtB"><?php echo \dash\fit::number_decimal(\dash\get::index($value, 'sum_debtor'), 'en') ?></span></td>
-						<td class="collapsing"><span class="txtR txtB"><?php echo \dash\fit::number_decimal(\dash\get::index($value, 'sum_creditor'), 'en') ?></span></td>
-						<td class="collapsing"><?php echo \dash\fit::number(\dash\get::index($value, 'item_count')) ?></td>
-						<td class="collapsing"><?php echo T_(\dash\get::index($value, 'status')) ?></td>
-						<td class="collapsing"><a class="btn link" href="<?php echo \dash\url::that(). '/edit?id='. \dash\get::index($value, 'id'); ?>"><?php echo T_("Edit") ?></a></td>
+						<td class="font-14 fc-red"><span class="txtR txtB"><?php echo \dash\fit::number_decimal(\dash\get::index($value, 'sum_debtor'), 'en') ?></span></td>
+						<td class="font-14 fc-green"><span class="txtR txtB"><?php echo \dash\fit::number_decimal(\dash\get::index($value, 'sum_creditor'), 'en') ?></span></td>
+					</tr>
+					<tr>
+						<td class="pTB5-f" colspan="7"><?php echo \dash\get::index($value, 'desc') ?></td>
 					</tr>
 				<?php } //endif ?>
 			</tbody>
@@ -34,4 +33,3 @@
 	<?php }else{ ?>
 		<div class="msg fs14 success2"><?php echo T_("Hi!") ?> <a class="btn link" href="<?php echo \dash\url::that() ?>/add"><?php echo T_("Add new") ?></a></div>
 	<?php } //endif ?>
-</div>
