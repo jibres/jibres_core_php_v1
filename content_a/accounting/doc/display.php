bbff26961b2bdd6c828f6d2fcb95b5eda69180da
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-19 10:37:39 +0430

    fix

--- content_a/accounting/doc/display.php
+++ content_a/accounting/doc/display.php
@@ -35,7 +35,7 @@
 							<a class="link" href="<?php echo \dash\url::that(). '/edit?id='. \dash\get::index($value, 'id'); ?>">#<?php echo \dash\fit::number(\dash\get::index($value, 'number'), true, 'en'); ?></a>
 						</td>
 						<td class="txtB"><?php echo \dash\fit::date(\dash\get::index($value, 'date')) ?></td>
-						<td class=""><?php if(\dash\get::index($value, 'tstatus') === 'Permanent') { echo '<i class="compact sf-lock fc-green mRa10"></i>';} else { echo '<i class="compact sf-unlock fc-red mRa10"></i>';}  ?><?php echo T_(\dash\get::index($value, 'tstatus')) ?></td>
+						<td class=""><?php if(\dash\get::index($value, 'status') === 'lock') { echo '<i class="compact sf-lock fc-green mRa10"></i>';} else { echo '<i class="compact sf-unlock fc-red mRa10"></i>';}  ?><?php echo T_(\dash\get::index($value, 'tstatus')) ?></td>
 						<td class=""><?php echo \dash\fit::number(\dash\get::index($value, 'item_count')) ?></td>
 
 						<td class="font-14 fc-red"><span class="txtR txtB"><?php echo \dash\fit::number_decimal(\dash\get::index($value, 'sum_debtor'), 'en') ?></span></td>
