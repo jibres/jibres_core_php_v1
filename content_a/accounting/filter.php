5ff20ddb431d2552a261aabb260c5c4e67715cf5
Reza <rm.biqarar@gmail.com>
2020-08-20 13:18:42 +0430

    Reset number

--- content_a/accounting/filter.php
+++ content_a/accounting/filter.php
@@ -2,7 +2,7 @@
   <div class="box">
     <div class="pad">
       <?php if(\dash\url::child() === 'doc') {?>
-        <div class="row">
+        <div class="row padLess">
           <div class="c-xs-12 c-sm-6">
         <div class="input">
         <input type="search" name="q" placeholder='<?php echo T_("Search"); ?>' id="q" value="<?php echo \dash\request::get('q'); ?>" <?php \dash\layout\autofocus::html() ?> autocomplete='off'>
@@ -19,9 +19,9 @@
 
         </div>
       <?php } //endif ?>
-      <div class="row">
+      <div class="row padLess">
         <?php if(\dash\data::accountingYear()) {?>
-          <div class="c-xs-12 c-sm-4">
+          <div class="c-xs-12 c-sm-3">
             <label for="parent"><?php echo T_("Accounting year") ?></label>
             <select class="select22" name="year_id">
               <option value=""><?php echo T_("Please choose year") ?></option>
@@ -31,25 +31,32 @@
             </select>
           </div>
         <?php } // endif ?>
-        <div class="c-xs-12 c-sm-4">
+        <div class="c-xs-12 c-sm-3">
           <label for="startdate" ><?php echo T_("Start date"); ?></label>
           <div class="input mB0-f">
             <input class="ltr" type="text" placeholder="yyyy/mm/dd" data-format="date" name="startdate" id="startdate" value="<?php echo \dash\request::get('startdate'); ?>" autocomplete='off'>
           </div>
         </div>
-        <div class="c-xs-12 c-sm-4">
+        <div class="c-xs-12 c-sm-3">
           <label for="enddate" ><?php echo T_("End date"); ?></label>
           <div class="input mB0-f">
             <input class="ltr" type="text" placeholder="yyyy/mm/dd" data-format="date" name="enddate" id="enddate" value="<?php echo \dash\request::get('enddate'); ?>" autocomplete='off'>
           </div>
         </div>
+        <div class="c-xs-12 c-sm-3">
+          <div class="mT25 txtRa">
+            <?php if(\dash\request::get('year_id')) {?>
+              <div class="btn outline" data-title='<?php echo T_("Reset document number?") ?>' data-confirm data-data='{"resetnumber": "resetnumber", "year_id" : "<?php echo \dash\request::get('year_id'); ?>"}'><i class="sf-refresh"></i></div>
+            <?php } //endif ?>
+        <?php if(\dash\request::get()) {?>
+          <a href="<?php echo \dash\url::current() ?>" class="btn secondary outline"><?php echo T_("Clear filter") ?></a>
+        <?php } //endif ?>
+        <button class="btn master"><?php echo T_("Apply") ?></button>
+          </div>
+        </div>
+
       </div>
     </div>
-    <footer class="txtRa">
-      <?php if(\dash\request::get()) {?>
-        <a href="<?php echo \dash\url::current() ?>" class="btn secondary outline"><?php echo T_("Clear filter") ?></a>
-      <?php } //endif ?>
-      <button class="btn master"><?php echo T_("Apply") ?></button>
-    </footer>
+
   </div>
 </form>
