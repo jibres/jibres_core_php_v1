e91c29b14581a4ff43174d1ab8e06b1edb0d4045
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-21 16:28:11 +0430

    fix

--- content_a/chap/size-a4/a4-1.php
+++ content_a/chap/size-a4/a4-1.php
@@ -37,19 +37,19 @@ if(isset($factorDetail['factor_detail']) && is_array($factorDetail['factor_detai
 <?php require('oneSide-seller.php'); ?>
 <?php require('oneSide-buyer.php'); ?>
 
-    <table class="tbl1 v6 txtC">
+    <table class="tbl2 v1">
      <thead>
       <tr class="fs07">
        <th class="collapsing">#</th>
        <th><?php echo T_("Explanation"); ?></th>
-       <th class="collapsing"><?php echo T_("Qty"); ?></th>
+       <th class="collapsing txtR"><?php echo T_("Qty"); ?></th>
        <th class="collapsing"><?php echo T_("Unit"); ?></th>
-       <th><?php echo T_("Unit price"); ?></th>
-       <th><?php echo T_("Total price"); ?></th>
-       <th><?php echo T_("Total discount"); ?></th>
-       <th><?php echo T_("Total price after discount"); ?></th>
-       <th><?php echo T_("Total VAT"); ?></th>
-       <th><?php echo T_("Final Price"); ?></th>
+       <th class="txtR"><?php echo T_("Unit price"); ?></th>
+       <th class="txtR"><?php echo T_("Total price"); ?></th>
+       <th class="txtR"><?php echo T_("Total discount"); ?></th>
+       <th class="txtR"><?php echo T_("Total price after discount"); ?></th>
+       <th class="txtR"><?php echo T_("Total VAT"); ?></th>
+       <th class="txtR"><?php echo T_("Final Price"); ?></th>
       </tr>
      </thead>
      <tbody>
@@ -79,14 +79,14 @@ $tableTotal['FinalPrice'] += $FinalPrice;
       <tr>
        <td><?php echo \dash\fit::number($key + 1); ?></td>
        <td class="txtLa productTitle"><?php echo \dash\get::index($datarow, 'title');?></td>
-       <td><?php echo \dash\fit::text(\dash\get::index($datarow, 'count')); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price(\dash\get::index($datarow, 'count')); ?></td>
        <td><?php echo \dash\get::index($datarow, 'unit'); ?></td>
-       <td><?php echo \dash\fit::number(\dash\get::index($datarow, 'price')); ?></td>
-       <td><?php echo \dash\fit::number($totalPrice); ?></td>
-       <td><?php echo \dash\fit::number($totalDiscount); ?></td>
-       <td><?php echo \dash\fit::number($totalPriceAfterDiscount); ?></td>
-       <td><?php echo \dash\fit::number($totalVAT); ?></td>
-       <td><?php echo \dash\fit::number($FinalPrice); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price(\dash\get::index($datarow, 'price')); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($totalPrice); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($totalDiscount); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($totalPriceAfterDiscount); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($totalVAT); ?></td>
+       <td class="ltr txtR lastCol"><?php echo \dash\fit::price($FinalPrice); ?></td>
       </tr>
 <?php } //endfor ?>
      </tbody>
@@ -96,11 +96,11 @@ $tableTotal['FinalPrice'] += $FinalPrice;
        <td></td>
        <td></td>
        <td></td>
-       <td><?php echo $tableTotal['totalPrice']; ?></td>
-       <td><?php echo $tableTotal['totalDiscount']; ?></td>
-       <td><?php echo $tableTotal['totalPriceAfterDiscount']; ?></td>
-       <td><?php echo $tableTotal['totalVAT']; ?></td>
-       <td><?php echo $tableTotal['FinalPrice']; ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($tableTotal['totalPrice']); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($tableTotal['totalDiscount']); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($tableTotal['totalPriceAfterDiscount']); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($tableTotal['totalVAT']); ?></td>
+       <td class="ltr txtR"><?php echo \dash\fit::price($tableTotal['FinalPrice']); ?></td>
       </tr>
      </tfoot>
     </table>
