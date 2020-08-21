fb1160557844f9e7de0fd767ca3670c75c8c09b6
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-21 13:26:34 +0430

    complete one side

--- content_a/chap/size-a4/a4-1.php
+++ content_a/chap/size-a4/a4-1.php
@@ -35,18 +35,27 @@ if(isset($factorDetail['factor_detail']) && is_array($factorDetail['factor_detai
     </header>
 
     <div id="sellerDetails" class="oneSide">
-      <h2 class="txtC"><?php echo "Seller Details"; ?></h2>
       <div class="row">
-        <div class="c-6 title"><?php echo \dash\get::index($storeData,'title'); ?></div>
-      </div>
-
-      <div class="row">
-        <div class="c-6"><?php
+        <div class="c-1"><h2 class="txtC"><?php echo "Seller Details"; ?></h2></div>
+        <div class="c-11">
+          <div class="row padMore">
+            <div class="c-6 title"><?php echo \dash\get::index($storeData,'title'); ?></div>
+            <div class="c-3">
+              <span><?php echo T_("VAT Number"); ?></span>
+              <code>411492163378</code>
+            </div>
+            <div class="c-3">
+              <span><?php echo T_("Company ID Number"); ?></span>
+              <code>14005035554</code>
+            </div>
+          </div>
+          <div class="row padMore">
+            <div class="c-6"><?php
 $country = \dash\get::index($storeData,'country_detail', 'name');
 if($country)
 {
   echo '<span>';
-  echo \dash\get::index($storeData,'country_detail', 'name');
+  echo $country;
   echo '</span>';
 }
 
@@ -56,7 +65,7 @@ if($province)
   echo T_(', ');
   echo '<span>';
   echo T_("Province"). ' ';
-  echo \dash\get::index($storeData,'province_detail', 'name');
+  echo $province;
   echo '</span>';
 }
 
@@ -66,24 +75,63 @@ if($city)
   echo T_(', ');
   echo '<span>';
   echo T_("City"). ' ';
-  echo \dash\get::index($storeData,'city_detail', 'name');
+  echo $city;
   echo '</span>';
 }
 ?></div>
-      </div>
+            <div class="c-3">
+              <span><?php echo T_("Postal Code"); ?></span>
+              <code><?php echo \dash\get::index($storeData,'postcode'); ?></code>
+            </div>
+            <div class="c-3">
+              <span><?php echo T_("Company Registration Number"); ?></span>
+              <code>13552</code>
+            </div>
+          </div>
 
-      <div class="row">
-        <div class="c-6"><?php
+          <div class="row padMore">
+            <div class="c-6"><?php
 $address = \dash\get::index($storeData,'address');
 if($address)
 {
   echo '<span>'. T_('Address'). '</span>'. ' ';
   echo '<span>';
-  echo \dash\get::index($storeData,'address');
+  echo $address;
   echo '</span>';
 }
 ?></div>
+            <div class="c-3"><?php
+$phone = \dash\get::index($storeData,'phone');
+if($phone || 1)
+{
+  echo '<span>'. T_('Phone'). '</span>'. ' ';
+  echo '<code>';
+  // echo $phone;
+  echo '+98-25-36505281';
+  echo '</code>';
+}
+else if($fax || 1)
+{
+  echo '<span>'. T_('Fax'). '</span>'. ' ';
+  echo '<code>';
+  // echo $fax;
+  echo '+98-25-36505281';
+  echo '</code>';
+}
+?></div>
+            <div class="c-3"><?php
+{
+  echo '<span class="block ltr txtL pLR5">';
+  echo \lib\store::url();
+  echo '</span>';
+}
+?></div>
+          </div>
+
+        </div>
       </div>
+
+
 
 
 
@@ -91,7 +139,7 @@ if($address)
     </div>
 
 
-
+<!--  -->
 
   </div>
 
