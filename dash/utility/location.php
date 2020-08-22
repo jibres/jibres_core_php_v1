3a00312b7a3438daeacf43de70792600f004aac5
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-22 09:19:40 +0430

    add province

--- dash/utility/location.php
+++ dash/utility/location.php
@@ -4,7 +4,7 @@ namespace dash\utility;
 // create select for locations
 class location
 {
-	public static function countrySelector($_countryDefault = null, $_provinceDefault = null)
+	public static function countrySelector($_country = null, $_province = null)
 	{
 		$placeholder = T_("Choose your country");
 		echo '<div class="countrySelector">';
@@ -15,36 +15,89 @@ class location
 		echo '<select class="select22" name="country" id="country" data-model="country"';
 		echo ' data-next="#province"';
 		echo ' data-placeholder="'. $placeholder. '"';
-		if($_provinceDefault)
+		if($_province)
 		{
-			echo ' data-next-default="'. $_provinceDefault. '"';
+			echo ' data-next-default="'. $_province. '"';
 		}
 		echo '>';
 
 		{
-			$myCountryList = \dash\utility\location\countres::$data;
 			echo '<option value="">'. $placeholder. '</option>';
-			foreach ($myCountryList as $key => $value)
+			$myCountryList = \dash\utility\location\countres::$data;
+			if(is_array($myCountryList) && $myCountryList)
 			{
-				echo '<option value="';
-				echo $key;
-				echo '"';
-				if($_countryDefault == $key)
+				foreach ($myCountryList as $key => $value)
 				{
-					echo 'selected';
+					echo '<option value="';
+					echo $key;
+					echo '"';
+					if($_country == $key)
+					{
+						echo 'selected';
+					}
+					echo ">";
+					echo ucfirst($value["name"]);
+					if(\dash\language::current() != 'en')
+					{
+						echo ' - '. T_(ucfirst($value['name']));
+					}
+					echo '</option>';
 				}
-				echo ">";
-				echo ucfirst($value["name"]);
-				if(\dash\language::current() != 'en')
-				{
-					echo ' - '. T_(ucfirst($value['name']));
-				}
-				echo '</option>';
 			}
 		}
 		echo '</select>';
 
     echo '</div>';
 	}
+
+
+	public static function provinceSelector($_province = null, $_city = null)
+	{
+		$placeholder = T_("Choose your country");
+		echo '<div class="provinceSelector">';
+		echo '<label for="province">';
+		echo T_("Province");
+		echo '</label>';
+
+		echo '<select class="select22" name="province" id="province"';
+		echo ' data-next="#city"';
+		echo ' data-placeholder="'. $placeholder. '"';
+		if($_city)
+		{
+			echo ' data-next-default="'. $_city. '"';
+		}
+		echo '>';
+
+		{
+			$myProvinceList = \dash\utility\location\countres::$data;
+			echo '<option value="">'. $placeholder. '</option>';
+
+			if(is_array($myProvinceList) && $myProvinceList)
+			{
+				foreach ($myProvinceList as $key => $value)
+				{
+					echo '<option value="';
+					echo $key;
+					echo '"';
+					if($_province == $key)
+					{
+						echo 'selected';
+					}
+					echo ">";
+					echo ucfirst($value["name"]);
+					if(\dash\language::current() != 'en')
+					{
+						echo ' - '. T_(ucfirst($value['name']));
+					}
+					echo '</option>';
+				}
+			}
+		}
+		echo '</select>';
+
+    echo '</div>';
+	}
+
+
 }
 ?>
