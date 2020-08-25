147984cd2c6c79c8c40de7f3f7d2bb04e073530c
Reza <rm.biqarar@gmail.com>
2020-08-25 11:19:29 +0430

    add data-jform

--- content_business/f/home/display-form.php
+++ content_business/f/home/display-form.php
@@ -11,7 +11,7 @@ if(!is_array($formItems))
 	<div class="avand-md">
 		<div class="box">
 			<header class="c-xs-0"><h2><?php echo \dash\data::formDetail_title(); ?></h2></header>
-			<div class="body">
+			<div class="body" data-jform>
 				<?php if(\dash\data::formDetail_status() !== 'publish') {?>
 					<div class="msg warn txtC txtB"><?php echo T_("Your form is not publish. Only you can view this form.") ?> <a class="btn link" href="<?php echo \lib\store::admin_url(). '/a/form/edit?id='. \dash\data::formDetail_id() ?>"><?php echo T_("Edit form") ?></a></div>
 				<?php } //endif ?>
