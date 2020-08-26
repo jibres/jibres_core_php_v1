5d4100f0b2459bde89d4bbc673adbb3ad7486ac2
Reza <rm.biqarar@gmail.com>
2020-08-26 16:14:22 +0430

    fix fomr

--- content_business/home/layout/body/imageblock.php
+++ content_business/home/layout/body/imageblock.php
@@ -14,8 +14,9 @@ if(isset($line_detail['value']['imageblock']) && is_array($line_detail['value'][
 
 <div class="avand imgLine">
   <div class="row">
+  	<?php $count = count($imageblock); ?>
 	<?php foreach ($imageblock as $key => $value) {?>
-    	<div class="c">
+    	<div class="c <?php if(count($imageblock) === 2){ echo 'c-xs-12'; } ?>">
 			 <a<?php if(\dash\get::index($value, 'url')) { echo ' href="'.  \dash\get::index($value, 'url'). '"'; if(\dash\get::index($value, 'target')) { echo ' target="_blank"'; }} ?>>
 				<img src="<?php echo \lib\filepath::fix(\dash\get::index($value, 'image')); ?>" alt="<?php echo \dash\get::index($value, 'alt'); ?>">
 			</a>
