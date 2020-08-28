e7e50b69e0b357e0f1c0e082917bff7dcd1b07af
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-28 16:40:01 +0430

    fix back

--- content_enter/verify/view.php
+++ content_enter/verify/view.php
@@ -20,7 +20,7 @@ class view
 		self::verifyPageTitle();
 
 		// back
-		\dash\data::back_link(\dash\url::here(). '/pass');
+		\dash\data::back_link(\dash\url::here());
 		\dash\data::back_text(T_('Back'));
 		// action
 		// \dash\data::action_text(T_('Recovery'));
