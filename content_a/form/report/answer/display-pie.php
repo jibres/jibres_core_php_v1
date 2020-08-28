1100ea27479c1ff7c81eac0089e5b19e6c63bd54
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-28 17:45:28 +0430

    fix

--- content_a/form/report/answer/display-pie.php
+++ content_a/form/report/answer/display-pie.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="c-xs-12 c-6">
-    <div id="chartdivpie" class="box chart notActive x400" data-abc='a/form_chart notActives'></div>
+    <div id="chartdivpie" class="box chart notActive x400" data-abc='a/form_charts'></div>
 
   </div>
   <div class="c-xs-12 c-6">
