568d973315577cc1900b91f455f377b4713988b8
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-08-28 15:51:09 +0430

    fix ip

--- content/ip/home/view.php
+++ content/ip/home/view.php
@@ -9,39 +9,38 @@ class view
 		\dash\data::action_text(T_('Dashboard'));
 
 		$myIp = \dash\server::ip();
-
+		// fill ip from child
 		if(\dash\url::child())
 		{
 			$myIp = \dash\url::child();
-			if(\dash\validate::ip($myIp, false))
-			{
-				// get ip data
-				\dash\data::ip(\dash\utility\ipLocation::get($myIp));
-				\dash\face::title(T_("IP"). " ". $myIp);
-
-				\dash\data::ipDetail(\dash\utility\ip::fetch($myIp));
-
-				\dash\face::cover(\dash\url::cdn(). '/img/flags/png100px/'. \dash\data::ip_flag(). '.png');
-
-				$desc = T_(\dash\data::ip_country());
-				$desc .= ' / '.  T_("Province"). ' '. T_(ucwords(\dash\data::ip_state()));
-				$desc .= ' / '.  T_("City"). ' '. T_(ucwords(\dash\data::ip_city()));
-				$desc .= ' / '.  T_("ISP"). ' '. T_(ucwords(\dash\data::ip_isp()));
-				$desc .= ' / '.  T_(ucwords(\dash\data::ip_latitude()));
-				$desc .= '-'. T_(ucwords(\dash\data::ip_longitude()));
-
-				$desc .= ' ';
-				\dash\face::desc($desc);
-				// ip is okay
-			}
-			else
-			{
-				// invalid ip
-				\dash\header::status(400);
-			}
 		}
 
+		if(\dash\validate::ip($myIp, false))
+		{
+			// get ip data
+			\dash\data::ip(\dash\utility\ipLocation::get($myIp));
+			\dash\face::title(T_("IP"). " ". $myIp);
 
+			\dash\data::ipDetail(\dash\utility\ip::fetch($myIp));
+
+			\dash\face::cover(\dash\url::cdn(). '/img/flags/png100px/'. \dash\data::ip_flag(). '.png');
+
+			$desc = T_(\dash\data::ip_country());
+			$desc .= ' / '.  T_("Province"). ' '. T_(ucwords(\dash\data::ip_state()));
+			$desc .= ' / '.  T_("City"). ' '. T_(ucwords(\dash\data::ip_city()));
+			$desc .= ' / '.  T_("ISP"). ' '. T_(ucwords(\dash\data::ip_isp()));
+			$desc .= ' / '.  T_(ucwords(\dash\data::ip_latitude()));
+			$desc .= '-'. T_(ucwords(\dash\data::ip_longitude()));
+
+			$desc .= ' ';
+			\dash\face::desc($desc);
+			// ip is okay
+		}
+		else
+		{
+			// invalid ip
+			\dash\header::status(400);
+		}
 	}
 }
 ?>
