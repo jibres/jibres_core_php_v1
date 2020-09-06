fcc49c2a9fe3eaa5e6f033b8d54d319413f86dd4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-06 14:57:15 +0430

    save some text for homepage

---
+++ content/home/text.php
@@ -0,0 +1,480 @@
+Build an online business
+Try Shopify free for 14 days, no credit card required.
+
+Bring your business online
+Create an ecommerce website backed by powerful tools that help you find customers, drive sales, and manage your day-to-day.
+
+Take the best path forward
+
+Start an online business
+Create a business, whether you’ve got a fresh idea or are looking for a new way to make money.
+
+Move your business online
+Turn your retail store into an online store and keep serving customers without missing a beat.
+
+Switch to Shopify
+Bring your business to Shopify, no matter which ecommerce platform you’re currently using.
+
+Hire a Shopify expert
+Get setup with the help of a trusted freelancer or agency from the Shopify Experts Marketplace.
+
+
+One platform with all the ecommerce and point of sale features you need to start, run, and grow your business.
+
+Sell everywhere
+Use one platform to sell products to anyone, anywhere—in person with Point of Sale and online through your website, social media, and online marketplaces.
+
+Take the guesswork out of marketing with built-in tools that help you create, execute, and analyze digital marketing campaigns.
+
+
+Manage everything
+Gain the insights you need to grow—use a single dashboard to manage orders, shipping, and payments anywhere you go.
+
+
+Get the help you need, every step of the way
+Shopify support
+Contact support 24/7, whether you’re troubleshooting issues or looking for business advice.
+
+
+Start your business journey with Shopify
+Try Shopify for free, and explore all the tools and services you need to start, run, and grow your business.
+
+
+
+<!-- wix -->
+Create a Website You’re Proud Of
+Discover the platform that gives you the freedom to create, design, manage and develop your web presence exactly the way you want.
+
+
+
+The Freedom to Create the Websites You Want
+Design and build your own high-quality websites. Whether you’re promoting your business, showcasing your work, opening your store or starting a blog—you can do it all with the Wix website builder.
+
+
+
+Build Your Unique Online Presence
+
+Customize your site
+Pick a template and customize anything, or answer a few questions and get a free website designed just for you.
+
+Add advanced features
+Start your own blog, add an online store and accept bookings online. You can always add more features as you grow.
+
+Edit your mobile view
+Check out the mobile-optimized version of your site. Switch to the Mobile Editor to personalize it even more.
+
+Optimize for search engines
+Answer a few questions about your website, location and keywords and get a personalized SEO plan to get found online.
+
+
+You can make your own website exactly the way you want, whether it’s your first time creating a site or you’re a long time pro.
+
+Beautiful Blog
+Create a stunning blog, grow your community and share your ideas.
+
+Design your own professional logo and build your brand online.
+
+Mobile Optimized
+Look amazing on every screen with a mobile version of your site.
+
+Online Store
+Build your online business with a powerful ecommerce platform.
+
+The Best SEO
+Get found on search engines like Google with advanced SEO tools.
+
+Custom Domains
+Get a professional domain name that matches your business and brand.
+
+
+Manage and Grow Your Business Online
+The Wix website builder makes it possible for anyone to create a website and manage their business—all in one place. Effortlessly capture leads, build customer relationships and accept payments right from your website.
+
+
+Why the Wix Website Builder Is the Best Choice for You
+You can create a free and professional website all on your own. With Wix, you can start with a stunning template and customize it, or get a personalized website made just for you. When you choose Wix, you don’t just get a drag and drop website builder. You get the whole package. Free reliable web hosting, top security, the best SEO and a dedicated support team to help you along the way.
+
+Over 160 million people worldwide choose Wix to create a free website. And you can do it, too.
+
+
+How to Create a Free Website
+Follow these 6 simple steps to create a website today.
+
+Sign up for a free website builder. Choose what kind of website you want to create.
+
+Customize a template or get a website made for you. Choose your starting point.
+
+Drag and drop 100s of design features. Add text, galleries, videos, vector art and more.
+
+Get ready for business. Add an online store, booking system, members area and blog.
+
+Publish your website and go live. Start building your professional online presence.
+
+Drive traffic to your site. Use advanced SEO tools and integrated marketing solutions.
+
+
+Stunning Templates
+Start with free HTML website templates and bring your vision to life.
+
+
+
+
+
+<!-- https://woocommerce.com -->
+Build exactly the eCommerce website you want
+WooCommerce is a customizable, open-source eCommerce platform built on WordPress. Get started quickly and make your way.
+
+
+Everything You Need to Start Selling Online
+Add WooCommerce plugin to any WordPress site and set up a new store in minutes. Get secure payments, configurable shipping options, and more, out of the box–for free.
+
+
+Supported by real people
+Our team of Happiness Engineers works remotely from 58 countries providing customer support across multiple time zones.
+
+WooCommerce - the most customizable eCommerce platform for building your online business.
+
+
+<!-- https://www.shopware.com/ -->
+Start your ecommerce business now
+Shopware is the ideal solution for meeting the changing demands of ecommerce, giving you the freedom to quickly and easily leverage your growth potential and focus on the perfect customer experience.
+
+Unlimited flexibility thanks to the API first approach
+
+
+
+
+
+<!-- https://www.bigcommerce.com/ -->
+ECOMMERCE FOR A NEW ERA
+Build a business that’s ready for anything with the flexibility of Open SaaS.
+
+Ignite Growth, Not Complexity
+Meet the flexible, open SaaS platform leading a new era of ecommerce. Explore limitless possibilities to Build, Innovate and Grow.
+
+Build
+Start with the rock-solid foundation of a powerful ecommerce platform.
+
+Innovate
+Turn impossible commerce experiences into reality with the flexibility of open SaaS.
+
+Stay Agile
+Seize market opportunities and unleash new experiences at the speed of your business.
+
+Grow
+Scale and grow smarter without complexity holding you back.
+
+Convert More
+Surpass industry benchmarks by converting higher-value orders.
+
+Borderless Commerce
+Reach new audiences with omnichannel commerce anywhere in the world.
+
+Your Commerce, Without Limits
+Empower innovation for your brand—all at a lower total cost of ownership than other leading ecommerce platforms.
+
+API Driven
+
+Total Cost of Ownership
+Reduce replatforming time and minimize operating costs.
+
+Bigger than you need right now? Start with BigCommerce Essentials.     GET THE ESSENTIALS
+
+Built to Sell
+The easiest way to build, run, and grow a better online business is on BigCommerce.
+
+
+The simple way to tackle the complexity of selling online
+
+
+
+
+
+<!-- https://www.opencart.com/ -->
+The best FREE and open-source eCommerce platform
+Everything you need to create, scale and run your business
+
+Simple to start. Easy to run.
+Start your free OpenCart eCommerce store now!
+
+Powerful Store Management
+Benefit from our built-in SEO. Easy manage products, customers, orders, taxes rules, coupon codes and more.
+
+
+
+<!-- https://magento.com/ -->
+Create engaging shoppable experiences
+See how our next-generation technology, global partner ecosystem, and extensions marketplace can breathe life into your business.
+
+
+
+<!-- https://www.prestashop.com/en -->
+Create and develop your business with PrestaShop
+Launch your online store right now. There’s a solution to suit everyone’s needs.
+
+Discover PrestaShop
+PrestaShop is an efficient and innovative e-commerce solution with all the features you need to create an online store and grow your business.
+
+Customize your store
+Choose your colors, your logo and a design from among a selection of professional-looking themes.
+
+Drive your business
+Manage your products, orders, customer relationships, and track your statistics
+
+Attract visitors
+Share your content on the social networks and your blog and attract more visitors
+
+Sell globally
+Easily present your store in several different languages and currencies – your business has no borders!
+
+
+
+
+<!-- https://vtex.com/us-en/ -->
+Unleash your business with collaborative commerce
+VTEX is the first and only fully integrated commerce, marketplace and OMS solution that offers fastest time-to-revenue and no upgrades. Ever.
+
+The world's largest brands rely on VTEX to power digital commerce in 40+ countries
+
+Collaborative Commerce Platform - built for today’s and tomorrow’s challenges
+With VTEX, go beyond traditional ecommerce to collaborate with channel partners, suppliers, and even competitors. Expand assortment and grow revenue by delivering more value to customers.
+
+
+We focus on tech, so you can focus on revenue.
+
+Drive business growth through our platform's collaborative commerce engine.
+
+
+
+<!-- https://www.odoo.com/ -->
+Amazing employees deserve
+amazing software
+Odoo is a software suite to better manage and run your company.
+Millions of happy users work better with our integrated Apps.
+
+
+<!-- https://www.nopcommerce.com/en -->
+Small and medium business
+Quickly launch and scale your online store with rich built-in functionality: integration with payment and shipment services; warehouse managing; marketing and SEO tools; and mobile-friendly stores.
+
+
+Enterprise business
+Use multi-vendor and multi-store functionality (B2B and B2C). Get the maximum advantage of enterprise-grade performance. Easy integration and limitless customization opportunities.
+
+Global business
+A flexible system to fit your demands: GDPR; multi-currencies and multi-languages; regional taxes and laws support; management of multiple international stores; integration with local services and suppliers.
+
+Great Support
+Our team and our community is always here to help you
+
+
+<!-- https://www.lightspeedhq.com/ecommerce/ -->
+Sell through the unexpected with Lightspeed eCom
+Unlock a new revenue stream that never closes its doors. Manage inventory, shipping, marketing and reports from one platform in a booming online marketplace.
+
+Keep your stock moving.
+Keep costs low and profits high across channels with Lightspeed eCom's inventory management tools.
+
+Easily import stock from your physical store to your eCom shop in one click
+Create discount codes for dusty inventory to drive traffic to your online store
+Make quick and easy mass updates to your product data through spreadsheet imports and exports
+
+
+Turn browsers into buyers.
+Build an online store that shoppers want to buy from, over and over again. Choose from our selection of over fifty professional themes, specially designed for conversion.
+
+Showcase your products in HD and optimize web navigation
+Select a theme from a range of mobile-responsive and customizable templates
+Attract online customers with a sleek design for optimal user experience
+
+Tools that boost customer satisfaction.
+Online stores don't come with sales clerks. Provide your customers with a feature-packed website that automates the service they're used to in store.
+
+Offer a points-based loyalty program with Lightspeed Loyalty
+Streamline returns and exchanges by inviting customers to open a ticket through their profile
+Let shoppers add their favorite items to a wishlist for future purchase
+
+Smooth sailing when it comes to shipping.
+From dispatch to delivery, Lightspeed eCom's integrated shipping tools help you track orders every step of the way.
+
+View, edit and organize your shipments from your eCom back office
+Keep customers up-to-date on the status on their order with shipment tracking codes
+Save time with shipping integrations like EasyPost and ShipStation
+
+
+Market your business across channels.
+Lightspeed eCom helps you master online marketing in next to no time, and reach audiences across several online channels.
+
+Connect social media platforms like Facebook, Twitter and Instagram to your online shop
+Make use of built-in tools to boost SEO and drive traffic to your online store
+Stay connected to online customers through newsletters and notification emails
+
+
+Go from local to global.
+Don't be limited by time and space—sell to anyone, anywhere, anytime.
+
+Make your online store available in up to 14 languages
+Serve an international clientele by adding multiple currencies
+Offer variable or flat international shipping rates
+
+
+Grow your business with data.
+Make more informed business decisions with a handy overview of all your data points.
+
+Track your online performance with daily, weekly and monthly sales reports
+Never miss a shipment and quickly fix mistakes with order status reports
+Check if your online store needs optimization with detailed conversion reports
+
+
+
+<!-- https://www.flow.io/ -->
+A global e-commerce solution that simply works.
+Selling cross border shouldn’t be hard. There’s a better way.
+
+Let it Flow
+Flow is a comprehensive and flexible solution that simplifies the
+management of cross border e-commerce to power global growth.
+
+Grow business, generate revenue, and optimize profits anywhere in the world with no operational friction, risk, or surprises.
+
+The next-generation for simplifying global e-commerce.
+
+
+
+
+<!-- https://kajabi.com/ -->
+You’re serious about your online business. We are too.
+We’ve spent the last 10 years helping over 50,000 business owners just like yourself create and sustain a successful business online. So successful in fact, that our customers have made over 1.5 billion dollars on the platform to date!
+
+Start free trial
+
+But your business after Kajabi...
+...has everything you need to grow in one place. You can access your website, products, marketing, community and more from a single dashboard. And since everything is built together, it just works together, better. All that frustrated energy you’ve spent fixing tech problems? Now you can focus that same energy on growing your business.
+
+Integrated subscriptions, courses, and digital products
+In minutes you can take your ideas and turn them into a polished, high-value product. Customize pricing, delivery, and packaging. Then use our one-click templates (or build your own) and upload your content.
+
+
+Everything in one place
+On Kajabi, your website connects everything about your business in one place. Your content, your marketing, your products… it all works seamlessly together so you can focus on your business instead of tech.
+
+
+Get paid on your terms
+Simple, seamless integration with Stripe and PayPal gets you paid easier, faster. From recurring to one-time to trial payment plans to one-click upsells, Kajabi does it all.
+
+
+Smarter business moves with better data
+Make informed decisions with thoughtful, real-time insights into your business. View reports, user forecasting, customer progress tracking, and see how customers are engaging with your business. Then make your next move feeling confident in your data.
+
+Replaces...
+
+
+The fastest way to create a fully-integrated page
+Pick a theme. Customize with near limitless design flexibility. Then publish. Your page is now fully integrated with the rest of your business.
+
+Automations you’ll actually use (and enjoy)
+Let Kajabi Pipelines optimize, automate, and scale your business so you can focus on what fuels you instead. With custom automation triggers and full platform integration, you’ll be able to create personal experiences for your audience without extra effort for you and your team.
+
+Track your leads like a real business
+Keep track of the real humans your business serves. Tag people based on their behavior, make personal notes, and then use that in your marketing, automations and products.
+
+
+Your digital products and community, anywhere
+Give your customers full access to your content with the new Kajabi mobile app. Get ready for deeper engagement, more connections, and more learning as your audience truly connects with your courses, communities, and resources. Included with every Kajabi plan.
+
+
+Kajabi is everything you need,
+no plugins or integrations required
+(But if you just can’t let go of your favorite tools, we play nice with them, too. 😊)
+
+
+
+<!-- https://www.ecwid.com/ -->
+Start Selling
+Online for Free
+We’re putting free trials on trial. With Ecwid, you get free FOREVER. Set up your free account once, and keep it as long as you like. Seriously.
+
+Your free online store is just a few clicks away.
+Join hundreds of thousands of small businesses who trust Ecwid E-commerce to sell online.
+
+
+Sell anywhere
+Set up your Ecwid store once to easily sync and sell across a website, social media, marketplaces like Amazon, and live in-person. Get started with one, or try them all.
+
+
+Grow faster
+Want easy-to-use marketing tools like Mailchimp, plus Google and Facebook advertising to grow your business fast? You got it.
+
+
+Manage simply
+Control everything from a single dashboard with centralized inventory, order management, pricing, and more.
+
+Start Selling Online for Free
+
+No credit card required.
+
+
+
+<!-- https://www.haravan.com/ -->
+Basic for Growth
+With the best technology solutions for e-commerce, Omnichannel retail and Marketing, Haravan is the platform to help you grow your business.
+
+
+Comprehensive solution for all business needs.
+Accompanying the start-up business to the top brand, Haravan has all the technologies you need to build your online brand, succeed in retail and disruptive marketing.
+
+Outstanding Online
+with Professional Website
+More than a professional website building platform for e-commerce and businesses, the system also helps you operate your business and online marketing more effectively with hundreds of unique and unique features in Haravan.
+
+Convert "Chat" on
+Facebook & Messenger to order.
+Comprehensive solution for sales, marketing and order management on Facebook, helping customers easily order and increase orders from Livechat & Messenger Shopping.
+
+Preeminent solution for free
+You can start with Haravan free solutions and upgrade as you grow more efficient
+
+
+
+
+<!-- https://www.volusion.com/v1/ -->
+Ecommerce for everyone
+Everything you need to sell online. Try it free for 14 days. No credit card required.
+
+Create Something Stunning
+
+With responsive themes and a comprehensive site builder, you can create a unique store — without code — in minutes.
+
+Sell with Ease
+
+Powerful ecommerce features give you everything you need:
+
+Inventory management
+Payment collection
+Unlimited product options
+
+
+
+Market Your Business
+
+Reach more customers and grow your sales with built-in SEO management, newsletters and a CRM system.
+
+
+WE CAN HELP
+
+Success for You is
+Success for Us
+
+Enjoy 24/7 access to our Austin-based ecommerce experts, who are working together to make you a success.
+SUPPORT • WEB DESIGNERS • MARKETING SPECIALISTS
+
+
+
+<!-- https://www.bigcartel.com/ -->
+
+Easy online stores for artists & makers
+We make it simple to build a unique online store, sell your work, and run a creative business.
+
+Keep your money
+We don’t take a cut of your sales or charge a bunch of fees. Just pick one of our affordable monthly plans to fit your needs and budget.
+
