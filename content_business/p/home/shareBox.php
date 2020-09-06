a4aa0653efccc6aba70e5fcb4f5ff0561d31d82d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-06 23:04:18 +0430

    fix share link

--- content_business/p/home/shareBox.php
+++ content_business/p/home/shareBox.php
@@ -5,15 +5,15 @@
     	<?php echo \dash\face::site(); ?> <?php echo T_("facebook"); ?>
     </a>
 
-    <a target="_blank" title='<?php echo T_("twitter"); ?>' href="https://twitter.com/home?status=<?php echo \dash\url::pwd(); ?>" class="twitter">
+    <a target="_blank" title='<?php echo T_("twitter"); ?>' href="https://twitter.com/share?url=<?php echo \dash\url::pwd(); ?>&amp;text=<?php echo urlencode(\dash\face::desc()); ?>" class="twitter">
     	<?php echo \dash\face::site(). ' '. T_("twitter"); ?>
     </a>
 
-    <a target="_blank" title='<?php echo T_("linkedin"); ?>' href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo \dash\url::pwd(); ?>&title=<?php echo urlencode(\dash\face::title()); ?>&summary=<?php echo urlencode(\dash\face::desc()); ?>" class="linkedin">
+    <a target="_blank" title='<?php echo T_("linkedin"); ?>' href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo \dash\url::pwd(); ?>&amp;title=<?php echo urlencode(\dash\face::title()); ?>&amp;summary=<?php echo urlencode(\dash\face::desc()); ?>" class="linkedin">
     	<?php echo \dash\face::site(). ' '. T_("linkedin"); ?>
     </a>
 
-    <a target="_blank" title='<?php echo T_("telegram"); ?>' href="https://t.me/share/url?url=<?php echo \dash\url::pwd(); ?>&text=<?php echo urlencode(\dash\face::title()); ?>" class="telegram">
+    <a target="_blank" title='<?php echo T_("telegram"); ?>' href="https://t.me/share/url?url=<?php echo \dash\url::pwd(); ?>&amp;text=<?php echo urlencode(\dash\face::title()); ?>" class="telegram">
     	<?php echo \dash\face::site(). ' '. T_("telegram"); ?>
     </a>
 
