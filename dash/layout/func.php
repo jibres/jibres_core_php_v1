c066be2146c3347f75efb6f99133a75fb7ab280b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-12 12:04:49 +0430

    change to 202

--- dash/layout/func.php
+++ dash/layout/func.php
@@ -31,7 +31,7 @@ class func
 
 			if(\dash\request::ajax())
 			{
-				\dash\header::set(206, true);
+				\dash\header::set(202, true);
 				// read all notif
 				$notifs = \dash\notif::get();
 				if($notifs)
