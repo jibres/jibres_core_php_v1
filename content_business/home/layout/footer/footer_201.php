daa9a68f0c8ed941ab9e72aa96697afcff82c062
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-19 21:06:45 +0430

    add enamad and samandehi logo to footer

--- content_business/home/layout/footer/footer_201.php
+++ content_business/home/layout/footer/footer_201.php
@@ -2,13 +2,21 @@
 	<div class="avand-xl">
 
 		<div class="top">
-			<div class="row">
-				<div class="c-xs-12 c-sm-12 c-md-8">
+			<div class="row align-end">
+				<div class="c-xs-12 c-sm-12 c-md">
 					<h3><a href="<?php echo \dash\url::kingdom(); ?>" target="_blank"><?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?></a></h3>
 					<p><?php echo \dash\get::index($website, 'footer', 'maintext', 'text') ?></p>
 				</div>
-				<div class="c-xs-12 c-sm-12 c-md-auto os">
-					<a class="button" href="">Sign Up Free</a>
+				<div class="c-xs-12 c-sm-12 c-md-auto">
+					<div class="certifications txtRa">
+						<a class="cert enamad" href="">
+							<img src="<?php echo \dash\url::cdn(); ?>/img/business/cert/enamad1-org.png" alt="اینماد  <?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?>">
+						</a>
+						<div class="cert samandehi" id="samandehiCert" data-open="https://logo.samandehi.ir/Verify.aspx?id=162977&p=rfthgvkauiwkpfvljyoejyoe">
+							<img src="<?php echo \dash\url::cdn(); ?>/img/business/cert/samandehi-review.png" alt="مجوز ساماندهی  <?php echo \dash\get::index(\lib\store::detail(), 'store_data', 'title'); ?>">
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</div>
