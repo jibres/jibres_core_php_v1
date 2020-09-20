2a6137baed0d02a0257b45829c9512e59d10040d
Reza <rm.biqarar@gmail.com>
2020-09-20 19:48:56 +0430

    Fix

--- content_a/form/analytics/filter/display.php
+++ content_a/form/analytics/filter/display.php
@@ -18,7 +18,7 @@
             </tr>
           </thead>
           <tbody>
-            <?php $first = true; $last = null; foreach (\dash\data::whereList() as $key => $value) { $last = \dash\get::index($value, 'count_after'); ?>
+            <?php $i = 0; $first = true; $last = null; foreach (\dash\data::whereList() as $key => $value) { $i++; $last = \dash\get::index($value, 'count_after'); ?>
               <?php if($first) { $first = false;?>
                 <tr class="positive">
 
@@ -31,8 +31,8 @@
                 </tr>
               <?php } // endif ?>
               <tr>
-                <td class="collapsing font-12"><span class=""><?php echo \dash\fit::number(\dash\get::index($value, 'item_id')) ?>.</span></td>
-                <td class="collapsing font-12"><?php echo \dash\get::index($value, 'field_title') ?></td>
+                <td class="collapsing font-12"><?php echo \dash\fit::number($i) ?></td>
+                <td class="collapsing font-12"><span class="txtB"><?php echo \dash\fit::number(\dash\get::index($value, 'item_id')) ?>.</span> <?php echo \dash\get::index($value, 'field_title') ?></td>
                 <td class="collapsing"><?php echo \dash\get::index($value, 'condition_title') ?></td>
                 <td class="collapsing"><?php echo \dash\get::index($value, 'value') ?></td>
                 <td class="collapsing txtL ltr"><?php echo \dash\fit::price(\dash\get::index($value, 'inside')) ?></td>
