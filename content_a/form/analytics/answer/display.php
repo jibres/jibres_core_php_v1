a9d3f9bc47bee3cf0468f81fe89ca1fae702c631
Reza <rm.biqarar@gmail.com>
2020-09-24 15:38:21 +0330

    fix list

--- content_a/form/analytics/answer/display.php
+++ content_a/form/analytics/answer/display.php
@@ -3,15 +3,9 @@
     <table class="tbl1 v6 repeatHead">
       <thead>
         <tr>
-          <td colspan="2"><?php echo \dash\data::formDetail_title() ?></td>
-          <td><?php echo T_("Form ID") ?></td>
-          <td><code class="compact txtB"><?php echo \dash\request::get('id'); ?></code></td>
-        </tr>
-        <tr>
-          <td><?php echo T_("Filter ID") ?></td>
-          <td><code class="compact txtB"><?php echo \dash\request::get('fid'); ?></code></td>
-          <td><?php echo T_("Answer ID") ?></td>
-          <td><code class="compact txtB"><?php echo $key; ?></code></td>
+          <td colspan="2"><?php echo \dash\data::formDetail_title() ?> <span class="compact txtB"><?php echo \dash\fit::text(\dash\request::get('id')); ?></span></td>
+          <td><?php echo T_("Filter ID") ?> <span class="compact txtB"><?php echo \dash\fit::text(\dash\request::get('fid')); ?></span></td>
+          <td><?php echo T_("Answer ID") ?> <span class="compact txtB"><?php echo \dash\fit::text($key) ?></span></td>
 
         </tr>
       </thead>
@@ -20,8 +14,8 @@
         <?php $i=0; foreach ($value as $k => $v) { $extra = in_array($v['item_type'], ['descriptive_answer']); if(isset($items[$v['item_id']]['visible']) && $items[$v['item_id']]['visible']) {}else{continue;} $i++;  ?>
         <?php  if(($i % 2) || ($extra)) { echo '<tr>';} ?>
 
-        <th class="" <?php if($extra) { echo 'colspan="2"'; } ?>><?php echo \dash\get::index($v, 'item_title'); ?></th>
-        <td class="" <?php if($extra) { echo 'colspan="2"'; } ?>><?php echo \dash\get::index($v, 'answer'); ?><?php echo \dash\get::index($v, 'textarea'); ?></td>
+        <th <?php if($extra) { echo 'colspan=""'; }else{echo 'class="w25p"'; } ?>><?php echo \dash\get::index($v, 'item_title'); ?></th>
+        <td <?php if($extra) { echo 'colspan="3"'; }else{echo 'class="w25p"'; } ?>><?php echo \dash\get::index($v, 'answer'); ?><?php echo \dash\get::index($v, 'textarea'); ?></td>
         <?php  if(!($i % 2) || ($extra)) { echo '</tr>'; } ?>
       <?php } //endif ?>
     </tbody>
