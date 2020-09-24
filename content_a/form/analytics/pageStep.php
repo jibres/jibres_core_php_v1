be39cc019d0d65b56b0c74bd6fd98f0b3459f283
Reza <rm.biqarar@gmail.com>
2020-09-24 16:14:32 +0330

    Fix page step

--- content_a/form/analytics/pageStep.php
+++ content_a/form/analytics/pageStep.php
@@ -19,19 +19,19 @@
     </a>
   </div>
   <div class="c pRa10">
-    <a href="<?php echo \dash\url::that(). '/chart?'. \dash\request::fix_get(['field' => null]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart') { echo 'active'; } ?>">
+    <a href="<?php echo \dash\url::that(). '/chart?'. \dash\request::fix_get(['field' => null, 'inside' => 1]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart') { echo 'active'; } ?>">
       <h3><?php echo T_("Chart");?></h3>
       <div class="val"><i class="sf-chart"></i></div>
     </a>
   </div>
   <div class="c pRa10">
-    <a href="<?php echo \dash\url::that(). '/chart?'. \dash\request::fix_get(['field' => null]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart') { echo 'active'; } ?>">
+    <a href="<?php echo \dash\url::that(). '/chart?'. \dash\request::fix_get(['field' => null, 'inside' => null]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart') { echo 'active'; } ?>">
       <h3><?php echo T_("Chart inside");?></h3>
       <div class="val"><i class="sf-chart"></i></div>
     </a>
   </div>
   <div class="c pRa10">
-    <a href="<?php echo \dash\url::that(). '/chart2?'. \dash\request::fix_get(['field' => null]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart2') { echo 'active'; } ?>">
+    <a href="<?php echo \dash\url::that(). '/chart2?'. \dash\request::fix_get(['field' => null, 'inside' => null]); ?>" class="stat x70 <?php if(\dash\url::subchild() === 'chart2') { echo 'active'; } ?>">
       <h3><?php echo T_("Chart");?></h3>
       <div class="val"><i class="sf-chart"></i></div>
     </a>
