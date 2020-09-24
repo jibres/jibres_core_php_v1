cc99deff3406b3eb396ed57533f83b0fd3bf394a
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-24 14:21:13 +0330

    add height auto

--- content_a/accounting/doc/printall/display.php
+++ content_a/accounting/doc/printall/display.php
@@ -25,7 +25,7 @@ if(\dash\data::dataTableAll())
 			\dash\data::myTitle(\dash\data::myTitle(). ' | '. T_("Closing Document"));
 		}
 
-		echo "<div class='printArea pageBreak' data-size='A4'>";
+		echo "<div class='printArea pageBreak' data-size='A4' data-height='auto'>";
 			echo "<h1>". \dash\data::myTitle(). '</h1>';
 			echo '<div class="box ">';
 				echo '<div class="pad">';
