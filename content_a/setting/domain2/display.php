63655ea00d531b3d9e2d24b86a8b8d334d4e3230
Reza <rm.biqarar@gmail.com>
2020-09-24 17:22:37 +0330

    Fix link of domains

--- content_a/setting/domain2/display.php
+++ content_a/setting/domain2/display.php
@@ -12,7 +12,7 @@
     <ul>
       <?php foreach (\dash\data::dataTable() as $key => $value) {?>
       <li>
-          <a class="f item" target="_blank" href="<?php echo \dash\url::that(). '/manage?domain='. \dash\get::index($value, 'domain'); ?>">
+          <a class="f item" href="<?php echo \dash\url::that(). '/manage?domain='. \dash\get::index($value, 'domain'); ?>">
             <div class="key"><?php echo \dash\get::index($value, 'domain'); ?></div>
             <div class="value"><?php echo \dash\get::index($value, 'tstatus'); ?></div>
             <div class="go"></div>
