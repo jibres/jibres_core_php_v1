da6786beb362e2d71e544541a2eb246f3d30cabf
Reza <rm.biqarar@gmail.com>
2020-09-26 16:06:46 +0330

    Remove old domain module

--- content_a/setting/domain/add/display.php
+++ content_a/setting/domain/add/display.php
@@ -12,7 +12,6 @@
   <nav class="items">
     <ul>
       <li><a class="f" href="<?php echo \dash\url::that(); ?>/existdomain"><div class="key"><?php echo T_("Connect existing domain"); ?></div><div class="go"></div></a></li>
-      <li><a class="f" href="<?php echo \dash\url::that(); ?>/subdomain"><div class="key"><?php echo T_("Connect a subdomain"); ?></div><div class="go"></div></a></li>
     </ul>
   </nav>
 </div>
--- content_a/setting/domain/subdomain/display.php
+++
@@ -1,8 +0,0 @@
-
-<div class="avand-md">
-  <div class="box">
-  	<div class="body">
-  		<p><?php echo T_("To connect a subdomain to your business you must set a A record on you Domain") ?></p>
-  	</div>
-  </div>
-</div>
