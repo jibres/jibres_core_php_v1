30e3239dedfe5159d0bca6d2eb59bd7a9e29d542
Reza <rm.biqarar@gmail.com>
2020-09-27 15:12:11 +0330

    Show subdomain in domain list

--- content_a/setting/domain/display.php
+++ content_a/setting/domain/display.php
@@ -10,6 +10,13 @@
 <div class="avand-md">
   <nav class="items">
     <ul>
+      <li>
+          <a class="f item" href="<?php echo 'https://'. \lib\store::detail('subdomain').'.jibres.'. \dash\url::jibres_tld(); ?>" target='_blank'>
+            <div class="key"><?php echo \lib\store::detail('subdomain').'.jibres.'. \dash\url::jibres_tld(); ?></div>
+            <div class="value"><span class="badge success2"><?php echo T_("Connected"); ?></span></div>
+            <div class="go"></div>
+          </a>
+      </li>
       <?php foreach (\dash\data::dataTable() as $key => $value) {?>
       <li>
           <a class="f item" href="<?php echo \dash\url::that(). '/manage?domain='. \dash\get::index($value, 'domain'); ?>">
