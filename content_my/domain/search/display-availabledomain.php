c8a3768f4467e7329df96429677ffe0983999cde
Reza <rm.biqarar@gmail.com>
2020-09-27 15:57:13 +0330

    Fix domain list

--- content_my/domain/search/display-availabledomain.php
+++ content_my/domain/search/display-availabledomain.php
@@ -1,7 +1,24 @@
-<div class="f">
 
+<?php if (\dash\detect\device::detectPWA()) { ?>
+<nav class="items">
+  <ul>
+    <?php foreach (\dash\data::dataTable() as $key => $value) {?>
+     <li>
+       <a class="item f" href="<?php echo \dash\url::this(). '/buy/'. \dash\get::index($value, 'name'); ?>">
+        <div class="key"><code><?php echo \dash\get::index($value, 'name'); ?></code></div>
+        <div class="go"></div>
+      </a>
+     </li>
+    <?php } //endfor ?>
+  </ul>
+</nav>
+<?php } else { ?>
+
+
+
+<div class="f">
 <?php foreach (\dash\data::dataTable() as $key => $value) {?>
-    <div class="c2 pA5">
+    <div class="c2 m6 s12 pA5">
         <a class="stat x70 available" href="<?php echo \dash\url::this(). '/buy/'. \dash\get::index($value, 'name'); ?>">
             <h3><?php echo T_("Available") ?></h3>
             <div class="val ltr"><?php echo \dash\get::index($value, 'name'); ?></div>
@@ -9,4 +26,5 @@
     </div>
 <?php } //endfor ?>
 </div>
+<?php } //endif ?>
 <?php \dash\utility\pagination::html(); ?>
