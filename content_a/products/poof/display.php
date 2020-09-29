20c621955fc3780a03cc9d29ead0999ec2b6f85f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-09-29 18:47:29 +0330

    add poof

---
+++ content_a/products/poof/display.php
@@ -0,0 +1,5 @@
+<div class="avand">
+  <div class="row" id='poofImageFetch' data-title='<?php echo \dash\data::productDataRow_title(); ?>'>
+
+  </div>
+</div>
---
+++ content_a/products/poof/view.php
@@ -0,0 +1,37 @@
+<?php
+namespace content_a\products\poof;
+
+
+class view
+{
+	public static function config()
+	{
+		$id = \dash\request::get('id');
+
+		// set page title from product title
+		$title = \dash\data::productDataRow_title();
+		if(!isset($title))
+		{
+			$title = T_("Without name");
+		}
+
+		\dash\face::title(T_("Image Asistant"). ' | '. $title);
+
+
+		// back
+		\dash\data::back_text(T_('Back'));
+		\dash\data::back_link(\dash\url::this(). '/edit?id='. \dash\request::get('id'));
+
+
+
+		if(\dash\data::productDataRow_url())
+		{
+			\dash\face::btnView(\dash\data::productDataRow_url());
+		}
+
+		\dash\face::help(\dash\url::support().'/property');
+
+		\dash\data::global_scriptPage('a_product_poof.js');
+	}
+}
+?>
