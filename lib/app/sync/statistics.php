2de2347f2b4dfe737b20d45ca5804e0182a4610a
Reza <rm.biqarar@gmail.com>
2020-09-30 19:06:53 +0330

    Bug fix add store analytics

--- lib/app/sync/statistics.php
+++ lib/app/sync/statistics.php
@@ -25,14 +25,14 @@ class statistics
 	private static function calc()
 	{
 		$result                        = [];
-		$result['users']               = \dash\db\users::get_count();
-		$result['user_mobile']         = \dash\db\users::get_count(['mobile' => ['IS NOT', 'NULL']]);
-		$result['user_email']          = \dash\db\users::get_count(['email' => ['IS NOT', 'NULL']]);
-		$result['user_username']       = \dash\db\users::get_count(['username' => ['IS NOT', 'NULL']]);
-		$result['user_awaiting']       = \dash\db\users::get_count(['status' => 'awaiting']);
-		$result['user_removed']        = \dash\db\users::get_count(['status' => 'removed']);
-		$result['user_filter']         = \dash\db\users::get_count(['status' => 'filter']);
-		$result['user_unreachabl']     = \dash\db\users::get_count(['status' => 'unreachabl']);
+		$result['users']               = floatval(\dash\db\users::get_count());
+		$result['user_mobile']         = floatval(\dash\db\users::get_count(['mobile' => ['IS NOT', 'NULL']]));
+		$result['user_email']          = floatval(\dash\db\users::get_count(['email' => ['IS NOT', 'NULL']]));
+		$result['user_username']       = floatval(\dash\db\users::get_count(['username' => ['IS NOT', 'NULL']]));
+		$result['user_awaiting']       = floatval(\dash\db\users::get_count(['status' => 'awaiting']));
+		$result['user_removed']        = floatval(\dash\db\users::get_count(['status' => 'removed']));
+		$result['user_filter']         = floatval(\dash\db\users::get_count(['status' => 'filter']));
+		$result['user_unreachabl']     = floatval(\dash\db\users::get_count(['status' => 'unreachabl']));
 
 		$result['lastactivity']        = \lib\store::get_last_update();
 		$result['lastchangesetting']   = null;
@@ -43,48 +43,48 @@ class statistics
 		$result['dbtrafic']            = null;
 		$result['dbsize']              = null;
 
-		$result['customer']            = \dash\db\users::get_count(['status' => [' != ', "'removed'"]]);
-		$result['staff']               = \dash\db\users::get_count(['permission' => ['IS NOT', 'NULL']]);
+		$result['customer']            = floatval(\dash\db\users::get_count(['status' => [' != ', "'removed'"]]));
+		$result['staff']               = floatval(\dash\db\users::get_count(['permission' => ['IS NOT', 'NULL']]));
 
-		$result['sumplustransaction']  = \dash\db\transactions::sum_plus();
-		$result['summinustransaction'] = \dash\db\transactions::sum_minus();
+		$result['sumplustransaction']  = floatval(\dash\db\transactions::sum_plus());
+		$result['summinustransaction'] = floatval(\dash\db\transactions::sum_minus());
 
-		$result['product']             = \lib\db\products\get::count_all();
-		$result['factor']              = \lib\db\factors\get::count_all();
-		$result['factorbuy']           = \lib\db\factors\get::count_all_buy();
-		$result['factorsale']          = \lib\db\factors\get::count_all_sale();
-		$result['factordetail']        = \lib\db\factordetails\get::count_all();
-		$result['sumfactor']           = \lib\db\factors\get::sum_all();
-		$result['planhistory']         = null; // store.planhistory table not found
-		$result['cart']                = \lib\db\cart\get::count_all();
-		$result['sync']                = \lib\db\sync\get::count_all();
-		$result['apilog']              = \dash\db\apilog::get_count();
+		$result['product']             = floatval(\lib\db\products\get::count_all());
+		$result['factor']              = floatval(\lib\db\factors\get::count_all());
+		$result['factorbuy']           = floatval(\lib\db\factors\get::count_all_buy());
+		$result['factorsale']          = floatval(\lib\db\factors\get::count_all_sale());
+		$result['factordetail']        = floatval(\lib\db\factordetails\get::count_all());
+		$result['sumfactor']           = floatval(\lib\db\factors\get::sum_all());
+		$result['planhistory']         = null;
+		$result['cart']                = floatval(\lib\db\cart\get::count_all());
+		$result['sync']                = floatval(\lib\db\sync\get::count_all());
+		$result['apilog']              = floatval(\dash\db\apilog::get_count());
 
-		$result['log']                 = \dash\db\logs::get_count();
-		$result['agent']               = \dash\db\agents::get_count();
-		$result['session']             = \dash\db\sessions::get_count();
-		$result['ticket']              = \dash\db\tickets::get_count(['parent' => null]);
-		$result['ticket_message']      = \dash\db\tickets::get_count(['parent' => ['IS NOT', 'NULL']]);
-		$result['comment']             = \dash\db\comments::get_count();
-		$result['address']             = \dash\db\address::get_count();
-		$result['transaction']         = \dash\db\transactions::get_count();
+		$result['log']                 = floatval(\dash\db\logs::get_count());
+		$result['agent']               = floatval(\dash\db\agents::get_count());
+		$result['session']             = floatval(\dash\db\sessions::get_count());
+		$result['ticket']              = floatval(\dash\db\tickets::get_count(['parent' => null]));
+		$result['ticket_message']      = floatval(\dash\db\tickets::get_count(['parent' => ['IS NOT', 'NULL']]));
+		$result['comment']             = floatval(\dash\db\comments::get_count());
+		$result['address']             = floatval(\dash\db\address::get_count());
+		$result['transaction']         = floatval(\dash\db\transactions::get_count());
 
-		$result['news']                = \dash\db\posts::get_count(['type' => 'post']);
-		$result['page']                = \dash\db\posts::get_count(['type' => 'page']);
-		$result['help']                = \dash\db\posts::get_count(['type' => 'help']);
-		$result['attachment']          = \dash\db\posts::get_count(['type' => 'attachment']);
-		$result['post']                = \dash\db\posts::get_count(['type' => ['NOT IN ',"('post', 'page', 'help', 'attachment')"]]);
+		$result['news']                = floatval(\dash\db\posts::get_count(['type' => 'post']));
+		$result['page']                = floatval(\dash\db\posts::get_count(['type' => 'page']));
+		$result['help']                = floatval(\dash\db\posts::get_count(['type' => 'help']));
+		$result['attachment']          = floatval(\dash\db\posts::get_count(['type' => 'attachment']));
+		$result['post']                = floatval(\dash\db\posts::get_count(['type' => ['NOT IN ',"('post', 'page', 'help', 'attachment')"]]));
 
-		$result['term']                = \dash\db\terms::get_count();
-		$result['tag']                 = \dash\db\terms::get_count(['type' => 'tag']);
-		$result['cat']                 = \dash\db\terms::get_count(['type' => 'cat']);
-		$result['support_tag']         = \dash\db\terms::get_count(['type' => 'support_tag']);
-		$result['help_tag']            = \dash\db\terms::get_count(['type' => 'help_tag']);
+		$result['term']                = floatval(\dash\db\terms::get_count());
+		$result['tag']                 = floatval(\dash\db\terms::get_count(['type' => 'tag']));
+		$result['cat']                 = floatval(\dash\db\terms::get_count(['type' => 'cat']));
+		$result['support_tag']         = floatval(\dash\db\terms::get_count(['type' => 'support_tag']));
+		$result['help_tag']            = floatval(\dash\db\terms::get_count(['type' => 'help_tag']));
 
-		$result['termusages']          = \dash\db\termusages::get_count();
+		$result['termusages']          = floatval(\dash\db\termusages::get_count());
 
-		$result['user_chatid']         = \dash\db\user_telegram::get_count();
-		$result['user_android']        = \dash\db\user_android::get_count();
+		$result['user_chatid']         = floatval(\dash\db\user_telegram::get_count());
+		$result['user_android']        = floatval(\dash\db\user_android::get_count());
 
 		return $result;
 
