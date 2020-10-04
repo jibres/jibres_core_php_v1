d49080049f42ea02cc48ea8a4fd08ab5bdd105d0
Reza <rm.biqarar@gmail.com>
2020-10-04 20:01:49 +0330

    Add link of print A4 to factor

--- content_a/chap/display.php
+++ content_a/chap/display.php
@@ -1,3 +1,16 @@
+<div class="row p0">
+	<div class="c-xs-12 c-sm-3">
+		<nav class="items">
+		 <ul>
+		      <li><a class="f item" href="<?php echo \dash\url::this(). '/a4?id='. \dash\request::get('id'); ?>"><i class="sf-print"></i><div class="key"><?php echo T_("Print"). ' '. T_('A4');?></div><div class="go"></div></a></li>
+		      <li><a class="f item" href="<?php echo \dash\url::this(). '/receipt?id='. \dash\request::get('id'); ?>"><i class="sf-print"></i><div class="key"><?php echo T_("Print"). ' '. T_('Receipt');?></div><div class="go"></div></a></li>
+		 </ul>
+		</nav>
+	</div>
+	<div class="c">
+
+	</div>
+</div>
 
 <?php
 if(\dash\data::printFileUrl())
