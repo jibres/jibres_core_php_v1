7df8a9ebee68fbbd697da34c9d873c83778a16fa
Reza <rm.biqarar@gmail.com>
2020-10-12 14:59:12 +0330

    Load store icon in pay content

--- content_pay/view.php
+++ content_pay/view.php
@@ -8,6 +8,31 @@ class view
 		\dash\data::include_adminPanel(true);
 
 		// \dash\data::userToggleSidebar(false);
+
+		if(\dash\engine\store::inStore())
+		{
+			$store_title = \lib\store::detail('title');
+			if($store_title)
+			{
+				\dash\face::site($store_title);
+				\dash\face::title($store_title);
+			}
+
+			$store_desc = \lib\store::detail('desc');
+			if($store_desc)
+			{
+				\dash\face::desc($store_desc);
+				\dash\face::intro($store_desc);
+			}
+
+			$store_logo = \lib\store::logo();
+			if($store_logo)
+			{
+				\dash\face::cover($store_logo);
+				\dash\face::twitterCard('summary');
+				\dash\face::logo($store_logo);
+			}
+		}
 	}
 }
 ?>
