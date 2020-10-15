fd5c375da92b9a2f31e28ed13cc6833f0fd82c5a
Reza <rm.biqarar@gmail.com>
2020-10-15 17:51:32 +0330

    Bug fix sort video in gallery

--- content_a/products/gallerysort/display.php
+++ content_a/products/gallerysort/display.php
@@ -7,12 +7,19 @@
 ?>
 
 <div class="msg fs14"><?php echo T_("Select any of the image you want and move them to sort") ?></div>
-<form method="post" data-sortable  data-willy class="ltr">
+<form method="post" data-sortable data-willy class="ltr">
 <?php foreach ($gallery as $key => $value) {?>
-  <div class="roundedBox" data-handle>
+  <div class="roundedBox" data-handle <?php if(\dash\get::index($value, 'type') !== 'image') {echo 'data-gr="'. rand(1, 20). '"'; }  ?>>
     <figure class="overlay" >
     	<input type="hidden" name="sort[]" value="<?php echo \dash\get::index($value, 'id'); ?>">
-      <img src="<?php echo \dash\get::index($value, 'path'); ?>" alt="<?php echo T_("File :val", ['val' => \dash\fit::number($key)]); ?>" data-gr="3">
+      <?php if(\dash\get::index($value, 'type') === 'image') {?>
+        <img src="<?php echo \dash\get::index($value, 'path'); ?>" alt="<?php echo T_("File :val", ['val' => \dash\fit::number($key)]); ?>" data-gr="3">
+      <?php }else{ ?>
+        <div class="txtC mT10 txtB">
+          <span><?php echo \dash\get::index($value, 'type') ?></span>
+        </div>
+        <img src="<?php echo \dash\app::static_image_url(); ?>" alt="<?php echo T_("File :val", ['val' => \dash\fit::number($key)]); ?>" data-gr="2">
+      <?php } //endif ?>
     </figure>
   </div>
 <?php } //endfor ?>
