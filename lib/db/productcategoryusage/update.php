7b29f7494d55f9e5a63db9caff56a0083b744911
Reza <rm.biqarar@gmail.com>
2020-10-15 17:27:14 +0330

    Bug fix remove duplicate category

--- lib/db/productcategoryusage/update.php
+++ lib/db/productcategoryusage/update.php
@@ -4,12 +4,43 @@ namespace lib\db\productcategoryusage;
 
 class update
 {
-
-
-
 	public static function category_usage_cat_id($_old_id, $_new_id)
 	{
-		$query  = "UPDATE productcategoryusage SET productcategoryusage.productcategory_id = $_new_id WHERE productcategoryusage.productcategory_id = $_old_id ";
+		$query  =
+		"
+			DELETE FROM
+				productcategoryusage
+			WHERE
+				productcategoryusage.productcategory_id = $_old_id AND
+				productcategoryusage.product_id IN
+				(
+					SELECT x.product_id FROM
+					(
+						SELECT
+							productcategoryusage.product_id
+						FROM
+							productcategoryusage
+						WHERE
+							productcategoryusage.productcategory_id = $_old_id OR productcategoryusage.productcategory_id = $_new_id
+						GROUP BY productcategoryusage.product_id
+						HAVING COUNT(*) >= 2
+					)
+					AS `x`
+				)
+		";
+		$remove_duplicate = \dash\db::query($query);
+
+
+		$query  =
+		"
+			UPDATE
+				productcategoryusage
+			SET
+				productcategoryusage.productcategory_id = $_new_id
+			WHERE
+				productcategoryusage.productcategory_id = $_old_id
+
+		";
 		$result = \dash\db::query($query);
 		return $result;
 	}
--- lib/db/producttagusage/update.php
+++ lib/db/producttagusage/update.php
@@ -5,15 +5,46 @@ namespace lib\db\producttagusage;
 class update
 {
 
-
-
 	public static function tag_usage_tag_id($_old_id, $_new_id)
 	{
-		$query  = "UPDATE producttagusage SET producttagusage.producttag_id = $_new_id WHERE producttagusage.producttag_id = $_old_id ";
+		$query  =
+		"
+			DELETE FROM
+				producttagusage
+			WHERE
+				producttagusage.producttag_id = $_old_id AND
+				producttagusage.product_id IN
+				(
+					SELECT x.product_id FROM
+					(
+						SELECT
+							producttagusage.product_id
+						FROM
+							producttagusage
+						WHERE
+							producttagusage.producttag_id = $_old_id OR producttagusage.producttag_id = $_new_id
+						GROUP BY producttagusage.product_id
+						HAVING COUNT(*) >= 2
+					)
+					AS `x`
+				)
+		";
+		$remove_duplicate = \dash\db::query($query);
+
+
+		$query  =
+		"
+			UPDATE
+				producttagusage
+			SET
+				producttagusage.producttag_id = $_new_id
+			WHERE
+				producttagusage.producttag_id = $_old_id
+
+		";
 		$result = \dash\db::query($query);
 		return $result;
 	}
-
 
 }
 ?>
