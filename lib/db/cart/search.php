849c509a55a4e05b0eb085f1c6de7469a406a7e5
Reza <rm.biqarar@gmail.com>
2020-10-17 14:14:55 +0330

    Bug fix cart pagination

--- lib/db/cart/search.php
+++ lib/db/cart/search.php
@@ -5,64 +5,27 @@ namespace lib\db\cart;
 class search
 {
 
-	private static function ready_to_sql($_and, $_or, $_order_sort = null, $_meta = [])
-	{
-		$where = null;
-		$q     = [];
-
-		if($_and)
-		{
-			$_and = implode(' AND ', $_and);
-			$q[] = "$_and";
-
-		}
-
-		if($_or)
-		{
-			$_or = implode(' OR ', $_or);
-			$q[] = "($_or)";
-		}
-
-		if($q)
-		{
-			$where = 'WHERE '. implode(" AND ", $q);
-		}
-
-		$order = null;
-		if($_order_sort && is_string($_order_sort))
-		{
-			$order = $_order_sort;
-		}
-
-		$pagination = null;
-		if(array_key_exists('pagination', $_meta))
-		{
-			$pagination = $_meta['pagination'];
-		}
-
-		$limit = null;
-		if(array_key_exists('limit', $_meta))
-		{
-			$limit = $_meta['limit'];
-		}
-
-		return
-		[
-			'where'      => $where,
-			'order'      => $order,
-			'pagination' => $pagination,
-			'limit'      => $limit,
-		];
-	}
-
-
 
 	public static function list($_and = null, $_or = null, $_order_sort = null, $_meta = [])
 	{
 
-		$q = self::ready_to_sql($_and, $_or, $_order_sort, $_meta);
+		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
-		$pagination_query =	"SELECT COUNT(*) AS `count`	FROM cart $q[where] GROUP BY IFNULL(cart.user_id, cart.guestid)";
+		$pagination_query =
+		"
+			SELECT
+				COUNT(myCartTable.id) AS `count`
+			FROM
+			(
+				SELECT
+					IFNULL(cart.user_id, cart.guestid) AS `id`
+				FROM
+					cart
+					$q[where]
+				GROUP BY
+					IFNULL(cart.user_id, cart.guestid)
+			) AS `myCartTable`
+		";
 
 		$limit = null;
 		if($q['pagination'] !== false)
@@ -76,7 +39,8 @@ class search
 				COUNT(*) AS `item_count`,
 				SUM(cart.count) AS `product_count`,
 				MAX(cart.datecreated) AS `datecreated`,
-				MAX(cart.user_id) AS `user_id`
+				MAX(cart.user_id) AS `user_id`,
+				IFNULL(cart.user_id, cart.guestid) AS `identify`
 			FROM cart
 			$q[where]
 			GROUP by IFNULL(cart.user_id, cart.guestid)
@@ -96,7 +60,7 @@ class search
 	public static function detail($_and = null, $_or = null, $_order_sort = null, $_meta = [])
 	{
 
-		$q = self::ready_to_sql($_and, $_or, $_order_sort, $_meta);
+		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
 
 		$pagination_query =	"SELECT COUNT(*) AS `count`	FROM cart INNER JOIN users ON cart.user_id = users.id INNER JOIN products ON products.id = cart.product_id $q[where]";
