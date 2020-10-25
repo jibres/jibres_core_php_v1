5d94e419da1e26d4bdf830d04c67cd5e86f950f1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-10-25 16:34:52 +0330

    fix h problem

--- content/home/display.php
+++ content/home/display.php
@@ -99,15 +99,15 @@
     <div class="f txtC">
       <div class="c s12 pA10">
           <div class="fs50"><?php echo \dash\fit::stats(\dash\data::homepagenumber_product()) ?>+</div>
-          <h5><?php echo T_('Products'); ?></h5>
+          <h4><?php echo T_('Products'); ?></h4>
       </div>
       <div class="c s12 pA10">
           <div class="fs50"><?php echo \dash\fit::stats(\dash\data::homepagenumber_factor()) ?>+</div>
-          <h5><?php echo T_('Factor'); ?></h5>
+          <h4><?php echo T_('Factor'); ?></h4>
       </div>
       <div class="c s12 pA10">
           <div class="fs40"><?php echo \dash\fit::stats(\dash\data::homepagenumber_sum_factor()) ?>+</div>
-          <h5><?php echo T_('Sold on Jibres'); ?></h5>
+          <h4><?php echo T_('Sold on Jibres'); ?></h4>
       </div>
     </div>
   </div>
