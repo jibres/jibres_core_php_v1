117ee73a88bd2a436fe470fa040c46c45bfd7bb6
Reza <rm.biqarar@gmail.com>
2020-10-27 16:30:49 +0330

    Try to fix tg

--- content_hook/tg/controller.php
+++ content_hook/tg/controller.php
@@ -14,6 +14,8 @@ class controller
 
 		if(\dash\url::child() === $myhook)
 		{
+			\dash\temp::set('clesnse_not_end_with_error', true);
+
 			// fire telegram api
 			\dash\social\telegram\tg::fire();
 
