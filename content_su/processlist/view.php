7d31a09b53346935e99a728944dfa06b9648ca19
Reza <rm.biqarar@gmail.com>
2020-11-06 13:27:41 +0330

    Show process list

--- content_su/processlist/view.php
+++ content_su/processlist/view.php
@@ -9,7 +9,7 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
 
-		\dash\data::processlist(\dash\db::get('SHOW PROCESSLIST;'));
+		\dash\data::processlist(\dash\db::get('SHOW FULL PROCESSLIST;'));
 
 	}
 }
