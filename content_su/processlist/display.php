4248fc2f54ad4486e7b1409d4f40444b99995f58
Reza <rm.biqarar@gmail.com>
2020-11-19 09:30:26 +0330

    Fix show full processlist

--- content_su/processlist/display.php
+++ content_su/processlist/display.php
@@ -1,3 +1,33 @@
 <div class="cbox">
-<pre><?php print_r(\dash\data::processlist()); ?></pre>
+	<div class="tblBox">
+		<table class="tbl1 v3">
+			<thead>
+				<tr>
+					<th class="collapsing">Id</th>
+					<th class="collapsing">User</th>
+					<th class="collapsing">Host</th>
+					<th class="collapsing">db</th>
+					<th class="collapsing">Command</th>
+					<th class="collapsing">Time</th>
+					<th class="collapsing">State</th>
+					<th>Info</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach (\dash\data::processlist() as $key => $value) {?>
+					<tr>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'Id') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'User') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'Host') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'db') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'Command') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'Time') ?></td>
+						<td class="collapsing"><?php echo \dash\get::index($value, 'State') ?></td>
+						<td><?php echo \dash\get::index($value, 'Info') ?></td>
+					</tr>
+				<?php } //endfor ?>
+			</tbody>
+		</table>
+	</div>
+<pre><?php print_r(\dash\data::fullprocesslist()); ?></pre>
 </div>
--- content_su/processlist/view.php
+++ content_su/processlist/view.php
@@ -9,7 +9,8 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
 
-		\dash\data::processlist(\dash\db::get('SHOW FULL PROCESSLIST;'));
+		\dash\data::processlist(\dash\db::get('SHOW PROCESSLIST;'));
+		\dash\data::fullprocesslist(\dash\db::get('SHOW FULL PROCESSLIST;'));
 
 	}
 }
