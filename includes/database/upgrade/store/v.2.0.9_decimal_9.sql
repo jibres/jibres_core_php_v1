b41d58657a6d4d3fc7a506c88d66d8883cbbaa16
Reza <rm.biqarar@gmail.com>
2020-11-21 06:20:01 +0330

    @reza Need to upgrade later

--- includes/database/upgrade/store/v.2.0.9_decimal_9.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE jibres_XXXXXXX.tax_docdetail CHANGE `debtor` `debtor` DECIMAL(22, 4) NULL DEFAULT NULL;
-ALTER TABLE jibres_XXXXXXX.tax_docdetail CHANGE `creditor` `creditor` DECIMAL(22, 4) NULL DEFAULT NULL;
