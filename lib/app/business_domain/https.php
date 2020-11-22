02c0e023b85e75a085c8b82a340560a5baa6538f
Reza <rm.biqarar@gmail.com>
2020-11-22 11:36:03 +0330

    Add comment to check later

--- lib/app/business_domain/https.php
+++ lib/app/business_domain/https.php
@@ -77,6 +77,7 @@ class https
 			}
 			else
 			{
+				// @reza @todo @check too many request of this action on domain fmir.ir.
 				\lib\app\business_domain\action::new_action($_id, 'arvancloud_domain_dns_not_active', ['meta' => self::meta($get_domain)]);
 				\dash\notif::error(T_("Domain DNS is not active on CDN panel"));
 				return false;
