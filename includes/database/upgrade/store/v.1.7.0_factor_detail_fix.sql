97d3225c3b0c4ad39e6cd5585d2958c9d3c7798d
Reza Mohiti <rm.biqarar@gmail.com>
2020-11-22 04:18:15 +0330

    Remove useless upgrade file

--- includes/database/upgrade/store/v.1.7.0_factor_detail_fix.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE jibres_XXXXXXX.factordetails DROP CONSTRAINT `factordetails_factor_id`;
-ALTER TABLE jibres_XXXXXXX.factordetails DROP CONSTRAINT `factordetails_product_id`;
-
-ALTER TABLE jibres_XXXXXXX.factordetails DROP PRIMARY KEY ;
-ALTER TABLE jibres_XXXXXXX.factordetails ADD PRIMARY KEY (`id`) ;
-ALTER TABLE jibres_XXXXXXX.factordetails DROP INDEX `id` ;
-
-ALTER TABLE jibres_XXXXXXX.factordetails ADD CONSTRAINT `factordetails_factor_id` FOREIGN KEY (`factor_id`) REFERENCES `factors` (`id`) ON UPDATE CASCADE;
-ALTER TABLE jibres_XXXXXXX.factordetails ADD CONSTRAINT `factordetails_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE;
