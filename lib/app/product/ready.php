2d86982547c6ff6982fdde011b87bb5024500af8
Reza <rm.biqarar@gmail.com>
2020-11-25 19:06:35 +0330

    Add youtube video

--- lib/app/product/ready.php
+++ lib/app/product/ready.php
@@ -548,12 +548,22 @@ class ready
 
 		while(preg_match("/\[(video)\s+(from\=)([^\[\]\s]*)\s+(code\=)([^\[\]\s]*)\]/", $_data, $split))
 		{
-			$iframe = '<div class="shortcode" type="video" from="aparat">';
-			$iframe .= '<iframe src="https://www.aparat.com/video/video/embed/videohash/'. $split[5] .'/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>';
-			$iframe .= '</div>';
-			$_data = str_replace($split[0], $iframe, $_data);
-
+			if($split[3] === 'aparat')
+			{
+				$iframe = '<div class="shortcode" type="video" from="aparat">';
+				$iframe .= '<iframe src="https://www.aparat.com/video/video/embed/videohash/'. $split[5] .'/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>';
+				$iframe .= '</div>';
+				$_data = str_replace($split[0], $iframe, $_data);
+			}
+			elseif($split[3] === 'youtube')
+			{
+				$iframe = '<div class="shortcode" type="video" from="aparat">';
+				$iframe .= '<iframe src="https://www.youtube.com/embed/'. $split[5].'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+				$iframe .= '</div>';
+				$_data = str_replace($split[0], $iframe, $_data);
+			}
 		}
+
 
 		return $_data;
 	}
