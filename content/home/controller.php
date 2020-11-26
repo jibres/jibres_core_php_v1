0c6ecd245c3346777ff04c14f6bad76d91e70dec
Reza <rm.biqarar@gmail.com>
2020-11-26 16:05:07 +0330

    Redirect admin order page

--- content/home/controller.php
+++ content/home/controller.php
@@ -14,6 +14,15 @@ class controller
 				\lib\app\application\download::from_site();
 			}
 
+			$module = \dash\url::module();
+			if(!\dash\url::child() && $module && substr($module, 0, 1) === ':' && \dash\validate::id(substr($module, 1), false))
+			{
+				$factor_id = \dash\validate::id(substr($module, 1), false);
+				$new_url = \dash\url::kingdom(). '/a/order/detail?id='. $factor_id;
+				\dash\redirect::to($new_url);
+				return;
+			}
+
 			\dash\redirect::to(\dash\url::kingdom(). '/a');
 		}
 
