ef0dc9f250eaeb5a9736c14366fe5403d50bb3b5
Reza <rm.biqarar@gmail.com>
2020-11-27 16:51:59 +0330

    Fix create new business query

--- includes/database/customer/v.6.1.0_tax.sql
+++ includes/database/customer/v.6.1.0_tax.sql
@@ -30,6 +30,24 @@ KEY `tax_coding_search_index_class` (`class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
+CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.tax_year (
+`id` smallint UNSIGNED NOT NULL AUTO_INCREMENT,
+`title` varchar(300) NULL,
+`startdate` date NULL,
+`enddate` date NULL,
+`status` enum('lock','enable') DEFAULT NULL,
+`datecreated` timestamp NULL ,
+`datemodified` timestamp NULL ,
+`isdefault` bit(1) NULL DEFAULT NULL,
+`closing` text NULL DEFAULT NULL,
+`opening` text NULL DEFAULT NULL,
+PRIMARY KEY (`id`),
+KEY `tax_year_search_index_number` (`title`),
+KEY `tax_year_search_index_status` (`status`),
+KEY `tax_year_search_index_startdate` (`startdate`),
+KEY `tax_year_search_index_enddate` (`enddate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.tax_document (
 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -71,23 +89,4 @@ CONSTRAINT `tax_docdetail_assistant_id` FOREIGN KEY (`assistant_id`) REFERENCES 
 CONSTRAINT `tax_docdetail_year_id` FOREIGN KEY (`year_id`) REFERENCES `tax_year` (`id`) ON UPDATE CASCADE,
 KEY `tax_docdetail_search_index_debtor` (`debtor`),
 KEY `tax_docdetail_search_index_creditor` (`creditor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.tax_year (
-`id` smallint UNSIGNED NOT NULL AUTO_INCREMENT,
-`title` varchar(300) NULL,
-`startdate` date NULL,
-`enddate` date NULL,
-`status` enum('lock','enable') DEFAULT NULL,
-`datecreated` timestamp NULL ,
-`datemodified` timestamp NULL ,
-`isdefault` bit(1) NULL DEFAULT NULL,
-`closing` text NULL DEFAULT NULL,
-`opening` text NULL DEFAULT NULL,
-PRIMARY KEY (`id`),
-KEY `tax_year_search_index_number` (`title`),
-KEY `tax_year_search_index_status` (`status`),
-KEY `tax_year_search_index_startdate` (`startdate`),
-KEY `tax_year_search_index_enddate` (`enddate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- includes/database/customer/v.7.0.0_forms.sql
+++ includes/database/customer/v.7.0.0_forms.sql
@@ -60,6 +60,26 @@ CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_answer (
   CONSTRAINT `form_answer_form_id` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_choice (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `form_id` int(10) UNSIGNED NOT NULL,
+  `item_id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(200) CHARACTER SET utf8mb4,
+  `score` int(10) DEFAULT NULL,
+  `sort` int(10) DEFAULT NULL,
+  `desc` text CHARACTER SET utf8mb4,
+  `file` text CHARACTER SET utf8mb4,
+  `datecreated` timestamp NULL DEFAULT NULL,
+  `datemodified` timestamp NULL DEFAULT NULL,
+  `status` ENUM('enable', 'deleted') NULL DEFAULT 'enable',
+  PRIMARY KEY (`id`),
+  KEY `form_choice_index_sort` (`sort`),
+  KEY `form_choice_index_status` (`status`),
+  CONSTRAINT `form_choice_form_id` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `form_choice_item_id` FOREIGN KEY (`item_id`) REFERENCES `form_item` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_answerdetail (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `form_id` int(10) UNSIGNED NOT NULL,
@@ -82,26 +102,6 @@ CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_answerdetail (
   CONSTRAINT `form_answerdetail_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `form_answerdetail_choice_id` FOREIGN KEY (`choice_id`) REFERENCES `form_choice` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_choice (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `form_id` int(10) UNSIGNED NOT NULL,
-  `item_id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(200) CHARACTER SET utf8mb4,
-  `score` int(10) DEFAULT NULL,
-  `sort` int(10) DEFAULT NULL,
-  `desc` text CHARACTER SET utf8mb4,
-  `file` text CHARACTER SET utf8mb4,
-  `datecreated` timestamp NULL DEFAULT NULL,
-  `datemodified` timestamp NULL DEFAULT NULL,
-  `status` ENUM('enable', 'deleted') NULL DEFAULT 'enable',
-  PRIMARY KEY (`id`),
-  KEY `form_choice_index_sort` (`sort`),
-  KEY `form_choice_index_status` (`status`),
-  CONSTRAINT `form_choice_form_id` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `form_choice_item_id` FOREIGN KEY (`item_id`) REFERENCES `form_item` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 
