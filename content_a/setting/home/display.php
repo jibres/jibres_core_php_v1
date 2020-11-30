6711e6758fc98276a4987be23ebc3c5f2dcdf118
Reza <rm.biqarar@gmail.com>
2020-11-30 08:40:02 +0330

    Add crm, cms link to setting

--- content_a/setting/home/display.php
+++ content_a/setting/home/display.php
@@ -199,6 +199,33 @@
     </div>
 </div>
 
+
+<div class="box" data-settings>
+    <div class="body">
+        <div class="row">
+            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+                <a class="row" data-item href="<?php echo \dash\url::kingdom(); ?>/cms">
+                    <div class="c-auto"><i class="sf-file-text"></i></div>
+                    <div class="c">
+                        <h2><?php echo T_("CMS"); ?></h2>
+                        <p><?php echo T_("Manage your business news and contents"); ?></p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+                <a class="row" data-item href="<?php echo \dash\url::kingdom(); ?>/crm">
+                    <div class="c-auto"><i class="sf-atom"></i></div>
+                    <div class="c">
+                        <h2><?php echo T_("Customers - CRM"); ?></h2>
+                        <p><?php echo T_("Manage business customers"); ?></p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="box" data-settings>
     <div class="body">
         <div class="row">
