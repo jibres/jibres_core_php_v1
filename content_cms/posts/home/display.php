7d51996eed939f642387c9b0ac6c1ca11f7443db
Reza <rm.biqarar@gmail.com>
2020-11-30 12:35:22 +0330

    Fix cms

--- content_cms/posts/home/display.php
+++ content_cms/posts/home/display.php
@@ -49,15 +49,6 @@ else
 
 <?php function htmlTable() {?>
 
-<?php
-
-if(\dash\get::index(\dash\data::productSettingSaved(), 'default_pirce_list'))
-{
-  htmlTablePrice();
-  return;
-}
-?>
-
 <?php if (\dash\detect\device::detectPWA()) { ?>
 <nav class="items pwaMultiLine">
   <ul>
@@ -159,74 +150,13 @@ if(\dash\get::index(\dash\data::productSettingSaved(), 'default_pirce_list'))
 <?php } //endfunction ?>
 
 
-
-
-
-<?php function htmlTablePrice() {?>
-
-  <table class="tbl1 v1 cbox fs12">
-    <thead>
-      <tr class="fs08">
-        <th class="collapsing"></th>
-        <th class="collapsing"><?php echo T_("Title"); ?></th>
-      <th><?php echo T_("Buy price"); ?></th>
-        <th><?php echo T_("Price"); ?></th>
-        <th><?php echo T_("Discount"); ?></th>
-        <th><?php echo T_("Discount percent"); ?></th>
-        <th><?php echo T_("Final price"); ?></th>
-        <th><?php echo T_("Stock"); ?></th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <?php foreach (\dash\data::dataTable() as $key => $value) {?>
-
-
-      <tr>
-        <td class="collapsing"><img src="<?php echo \dash\get::index($value, 'thumb'); ?>" class="avatar fs14"></td>
-        <td class="collapsing">
-          <div><a href="<?php echo \dash\url::this(); ?>/edit?id=<?php echo \dash\get::index($value, 'id'); ?>"><i class="sf-edit mRa10"></i><?php echo \dash\get::index($value, 'title'); ?></a></div>
-          <?php if(isset($value['optionvalue1']) && $value['optionvalue1']) {?>
-            <div><?php echo \dash\get::index($value, 'optionname1'); ?> <b><?php echo \dash\get::index($value, 'optionvalue1'); ?></b></div>
-          <?php } //endif ?>
-
-          <?php if(isset($value['optionvalue2']) && $value['optionvalue2']) {?>
-            <div><?php echo \dash\get::index($value, 'optionname2'); ?> <b><?php echo \dash\get::index($value, 'optionvalue2'); ?></b></div>
-          <?php } //endif ?>
-
-          <?php if(isset($value['optionvalue3']) && $value['optionvalue3']) {?>
-            <div><?php echo \dash\get::index($value, 'optionname3'); ?> <b><?php echo \dash\get::index($value, 'optionvalue3'); ?></b></div>
-          <?php } //endif ?>
-        </td>
-
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'buyprice')); ?></td>
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'price')); ?></td>
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'discount')); ?></td>
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'discountpercent')); ?></td>
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'finalprice')); ?></td>
-        <td><?php echo \dash\fit::number(\dash\get::index($value, 'stock')); ?></td>
-
-      </tr>
-      <?php } //endfor ?>
-    </tbody>
-  </table>
-
-<?php \dash\utility\pagination::html(); ?>
-
-<?php } //endfunction ?>
-
-
-
-
-
-
 <?php function htmlFilter() {?>
 <p class="f fs14 msg warn2">
   <span class="c"><?php echo \dash\data::filterBox(); ?></span>
 
   <?php if(\dash\data::barcodeScaned()) {?>
 
-    <a class="c" href="<?php echo \dash\url::this(); ?>/add<?php echo \dash\data::barcodeScaned(); ?>" data-shortkey="118"><?php echo T_("Add new product"); ?> <kbd>f7</kbd></a>
+    <a class="c" href="<?php echo \dash\url::this(); ?>/add<?php echo \dash\data::barcodeScaned(); ?>" data-shortkey="118"><?php echo T_("Add new post"); ?> <kbd>f7</kbd></a>
 
   <?php } //endif ?>
 
@@ -242,12 +172,12 @@ if(\dash\get::index(\dash\data::productSettingSaved(), 'default_pirce_list'))
 
 <div class="fs14 msg info2 pTB20">
   <p><?php echo T_("Hi!"); ?></p>
-  <p><?php echo T_("First step to set up your online store is add products."); ?> <?php echo T_("After add products, you can sell them to your customers."); ?></p>
-  <p><a href="<?php echo \dash\url::that(); ?>/add"><?php echo T_("Try to start with add new product!"); ?></a></p>
+  <p><?php echo T_("Publish news and pages on your website."); ?>
+  <p><a href="<?php echo \dash\url::that(); ?>/add"><?php echo T_("Try to start with add new post!"); ?></a></p>
 
 </div>
 
-<img class="banner" src="<?php echo \dash\url::cdn(); ?>/img/product/camera1.png" align='<?php echo T_("add new product"); ?>'>
+<img class="banner" src="<?php echo \dash\url::cdn(); ?>/img/product/camera1.png" align='<?php echo T_("add new post"); ?>'>
 
 
 <?php } //endfunction ?>
