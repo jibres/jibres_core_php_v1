be1b7e536d84571b5972e6d53bc4707b17c8025f
Reza <rm.biqarar@gmail.com>
2020-11-30 11:21:35 +0330

    Try to redesign post search page

--- content_cms/posts/home/display.php
+++ content_cms/posts/home/display.php
@@ -1,10 +1,12 @@
 
 <?php
-pageSteps();
+
+require_once('filter.php');
+
 
 if(\dash\data::dataTable())
 {
-  if(\dash\data::dataFilter())
+  if(\dash\data::isFiltered())
   {
 
     htmlSearchBox();
@@ -20,11 +22,12 @@ if(\dash\data::dataTable())
 }
 else
 {
-  if(\dash\data::dataFilter())
+  if(\dash\data::isFiltered())
   {
 
     htmlSearchBox();
-    htmlFilterNoResult();
+    htmlFilter();
+
 
 
   }
@@ -42,93 +45,169 @@ else
 
 
 
-<?php function htmlSearchBox() {?>
-<div class="cbox fs12">
-  <form method="get" action='<?php echo \dash\url::this(); ?><?php echo \dash\data::moduleType() ?>' data-action>
-    <div class="input">
-      <?php if(\dash\data::listSpecial()) {?>
-        <?php if(\dash\request::get('special')) {?>
-          <?php $openKerkere = true; ?>
-        <?php } ?>
-        <label class="addon" data-kerkere='.showFilterSearch' data-kerkere-icon><?php echo T_("Advance search"); ?></label>
-      <?php } ?>
-      <input type="search" name="q" placeholder='<?php echo T_("Search"); ?>' id="q" value="<?php echo \dash\validate::search_string(); ?>" <?php \dash\layout\autofocus::html() ?> autocomplete='off'>
-
-      <?php if(\dash\request::get('type')) {?>
-      <input type="hidden" name="type" value="<?php echo \dash\request::get('type'); ?>">
-      <?php } ?>
-
-      <button class="addon btn "><?php echo T_("Search"); ?></button>
-    </div>
-      <?php if(\dash\data::listSpecial()) {?>
-
-        <div class="showFilterSearch" <?php if(isset($openKerkere) && $openKerkere) {}else{?> data-kerkere-content='hide' <?php } ?>>
-           <label for="special"><?php echo T_("Use Special mode"); ?></label>
-
-          <select name="special" class="select22">
-            <option value=""><i><?php echo T_("Please select one item"); ?></i></option>
-            <?php foreach (\dash\data::listSpecial() as $key => $value) {?>
-
-              <option value="<?php echo $key; ?>" <?php if(\dash\request::get('special') === $key)  { echo 'selected'; }?>> <?php echo $value; ?></option>
-            <?php } //endfor ?>
-
-          </select>
-            <a href="<?php echo \dash\url::this(); ?>" class="btn "><?php echo T_("Clear filter"); ?></a>
-            <button class="btn primary"><?php echo T_("Apply"); ?></button>
-
-        </div>
-      <?php } //endif ?>
-  </form>
-</div>
-<?php } //endfunction ?>
 
 
 <?php function htmlTable() {?>
 
 <?php
-$sortLink = \dash\data::sortLink();
+
+if(\dash\get::index(\dash\data::productSettingSaved(), 'default_pirce_list'))
+{
+  htmlTablePrice();
+  return;
+}
 ?>
 
-  <table class="tbl1 v1 fs11">
+<?php if (\dash\detect\device::detectPWA()) { ?>
+<nav class="items pwaMultiLine">
+  <ul>
+    <?php foreach (\dash\data::dataTable() as $key => $value) {?>
+     <li>
+      <a class="f" href="<?php echo \dash\url::that(); ?>/edit?id=<?php echo \dash\get::index($value, 'id'); ?>">
+        <img src="<?php echo \dash\get::index($value, 'thumb'); ?>" alt="<?php echo \dash\get::index($value, 'title'); ?>">
+        <div class="key">
+          <div class="line1"><?php echo \dash\get::index($value, 'title'); ?></div>
+          <div class="line2 f">
+          <?php if(isset($value['variants_detail']['stock'])) {?>
+            <div class="cauto stockCount"><b><?php echo \dash\fit::number($value['variants_detail']['stock']); ?></b> <?php echo T_("in stock"); ?></div>
+          <?php } //endif ?>
+
+          <?php if(isset($value['variants_detail']['count'])) {?>
+            <div class="c variantCount"><?php echo T_("For"); ?> <b><?php echo \dash\fit::number($value['variants_detail']['count']); ?></b> <?php echo T_("variants"); ?></div>
+          <?php } //endif ?>
+          <div class="cauto os"><?php echo \dash\get::index($value, 'variant_price'); ?></div>
+          </div>
+        </div>
+      </a>
+     </li>
+    <?php } //endfor ?>
+  </ul>
+</nav>
+<?php } else { ?>
+<nav class="items">
+  <ul>
+    <?php foreach (\dash\data::dataTable() as $key => $value) {?>
+     <li>
+      <a class="f" href="<?php echo \dash\url::that(); ?>/edit?id=<?php echo \dash\get::index($value, 'id'); ?>">
+        <img src="<?php echo \dash\get::index($value, 'thumb'); ?>" alt="<?php echo \dash\get::index($value, 'title'); ?>">
+        <div class="key"><?php echo \dash\get::index($value, 'title'); ?></div>
+
+            <?php if(isset($value['variants_detail']['stock'])) {?>
+              <div class="key"><b><?php echo \dash\fit::number($value['variants_detail']['stock']); ?></b> <?php echo T_("in stock"); ?></div>
+            <?php }elseif(\dash\get::index($value, 'stock')){ ?>
+              <div class="key"><b><?php echo \dash\fit::number($value['stock']); ?></b> <?php echo T_("in stock"); ?></div>
+            <?php } //endif ?>
+
+            <?php if(isset($value['variants_detail']['count'])) {?>
+              <div class="key cauto"><?php echo T_("For"); ?> <b><?php echo \dash\fit::number($value['variants_detail']['count']); ?></b> <?php echo T_("variants"); ?></div>
+            <?php } //endif ?>
+
+        <div class="value"><?php echo \dash\get::index($value, 'variant_price'); ?></div>
+        <div class="go"></div>
+      </a>
+     </li>
+    <?php } //endfor ?>
+  </ul>
+</nav>
+<?php } ?>
+
+
+<div class="tblBox hide">
+  <table class="tbl1 v1 fs12">
     <thead>
+      <tr class="fs08">
+        <th class="collapsing">&nbsp;</th>
+        <th><?php echo T_("Title"); ?></th>
+        <th><?php echo T_("Price"); ?></th>
+        <th><?php echo T_("Variants"); ?></th>
+      </tr>
+    </thead>
+
+
+    <tbody>
+      <?php foreach (\dash\data::dataTable() as $key => $value) {?>
+
       <tr>
-        <th data-sort="<?php echo \dash\get::index($value, 'title', 'order'); ?>"><a href='<?php echo \dash\get::index($value, 'title', 'link'); ?>'><?php echo T_("Title"); ?></a></th>
-        <th class="s0 max-w200" data-sort="<?php echo \dash\get::index($value, 'slug', 'order'); ?>"><a href='<?php echo \dash\get::index($value, 'slug', 'link'); ?>'><?php echo T_("Slug"); ?></a></th>
-        <?php if(!\dash\request::get('type') || \dash\request::get('type') == 'post') {?>
-          <th data-sort="<?php echo \dash\get::index($value, 'commentcount', 'order'); ?>"><a href='<?php echo \dash\get::index($value, 'commentcount', 'link'); ?>'><i class="sf-comments fs16"></i></a></th>
-        <?php } ?>
-        <th data-sort="<?php echo \dash\get::index($value, 'publishdate', 'order'); ?>"><a href='<?php echo \dash\get::index($value, 'publishdate', 'link'); ?>'><?php echo T_("Publish date"); ?></a></th>
-        <th data-sort="<?php echo \dash\get::index($value, 'status', 'order'); ?>"><a href='<?php echo \dash\get::index($value, 'status', 'link'); ?>'><?php echo T_("Status"); ?></a></th>
+        <td class="collapsing"><img src="<?php echo \dash\get::index($value, 'thumb'); ?>" class="avatar" alt="<?php echo \dash\get::index($value, 'title'); ?>"></td>
+        <td><a href="<?php echo \dash\url::that(); ?>/edit?id=<?php echo \dash\get::index($value, 'id'); ?>"><i class="sf-edit mRa10"></i><?php echo \dash\get::index($value, 'title'); ?></a></td>
+        <td class=""><?php echo \dash\fit::number(\dash\get::index($value, 'variant_price')); ?></td>
+
+        <td>
+          <?php if(isset($value['variants_detail']['stock'])) {?>
+
+            <span><b><?php echo \dash\fit::number($value['variants_detail']['stock']); ?></b> <?php echo T_("in stock"); ?></span>
+
+          <?php } //endif ?>
+
+          <?php if(isset($value['variants_detail']['count'])) {?>
+
+            <span> <?php echo T_("For"); ?> <b><?php echo \dash\fit::number($value['variants_detail']['count']); ?></b> <?php echo T_("variants"); ?></span>
+
+          <?php } //endif ?>
+
+        </td>
+      </tr>
+      <?php } //endfor ?>
+
+    </tbody>
+  </table>
+</div>
+
+<?php \dash\utility\pagination::html(); ?>
+
+
+<?php } //endfunction ?>
+
+
+
+
+
+<?php function htmlTablePrice() {?>
+
+  <table class="tbl1 v1 cbox fs12">
+    <thead>
+      <tr class="fs08">
+        <th class="collapsing"></th>
+        <th class="collapsing"><?php echo T_("Title"); ?></th>
+      <th><?php echo T_("Buy price"); ?></th>
+        <th><?php echo T_("Price"); ?></th>
+        <th><?php echo T_("Discount"); ?></th>
+        <th><?php echo T_("Discount percent"); ?></th>
+        <th><?php echo T_("Final price"); ?></th>
+        <th><?php echo T_("Stock"); ?></th>
       </tr>
     </thead>
 
     <tbody>
       <?php foreach (\dash\data::dataTable() as $key => $value) {?>
 
-      <tr class="">
-        <td>
-          <?php if(isset($value['parent_url']) && $value['parent_url'] && is_array($value['parent_url'])) {?>
-            <?php foreach ($value['parent_url'] as $pKey => $pValue) {?>
 
-              <a target="_blank" title='<?php echo T_("Parent"); ?> <?php echo \dash\fit::number($pKey + 1); ?>' href="<?php echo \dash\url::kingdom(); ?>/<?php echo $pValue; ?>"><i class="sf-angle-double-up"></i></a>
-            <?php }//endfor ?>
+      <tr>
+        <td class="collapsing"><img src="<?php echo \dash\get::index($value, 'thumb'); ?>" class="avatar fs14"></td>
+        <td class="collapsing">
+          <div><a href="<?php echo \dash\url::this(); ?>/edit?id=<?php echo \dash\get::index($value, 'id'); ?>"><i class="sf-edit mRa10"></i><?php echo \dash\get::index($value, 'title'); ?></a></div>
+          <?php if(isset($value['optionvalue1']) && $value['optionvalue1']) {?>
+            <div><?php echo \dash\get::index($value, 'optionname1'); ?> <b><?php echo \dash\get::index($value, 'optionvalue1'); ?></b></div>
           <?php } //endif ?>
-          <a href="<?php echo \dash\url::here(); ?>/posts/edit?id=<?php echo \dash\get::index($value, 'id'); ?>&type=<?php echo \dash\get::index($value, 'type'); ?>"><?php echo \dash\get::index($value, 'title'); ?></a>
+
+          <?php if(isset($value['optionvalue2']) && $value['optionvalue2']) {?>
+            <div><?php echo \dash\get::index($value, 'optionname2'); ?> <b><?php echo \dash\get::index($value, 'optionvalue2'); ?></b></div>
+          <?php } //endif ?>
+
+          <?php if(isset($value['optionvalue3']) && $value['optionvalue3']) {?>
+            <div><?php echo \dash\get::index($value, 'optionname3'); ?> <b><?php echo \dash\get::index($value, 'optionvalue3'); ?></b></div>
+          <?php } //endif ?>
         </td>
-        <td class="s0 m0 fs09 ltr txtL pRa10" title="<?php echo \dash\get::index($value, 'url'); ?>">
 
-          <a href="<?php echo \dash\get::index($value, 'link'); ?>" target="_blank"><span class="sf-share"></span> <?php echo \dash\get::index($value, 'slug'); ?></a></td>
-        <?php if(!\dash\request::get('type') || \dash\request::get('type') == 'post') {?>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'buyprice')); ?></td>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'price')); ?></td>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'discount')); ?></td>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'discountpercent')); ?></td>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'finalprice')); ?></td>
+        <td><?php echo \dash\fit::number(\dash\get::index($value, 'stock')); ?></td>
 
-
-
-        <td class="collapsing txtC"><?php if(isset($value['commentcount']) && $value['commentcount']) {?><a href="<?php echo \dash\url::here(); ?>/comments?post_id=<?php echo \dash\get::index($value, 'id'); ?>"><?php echo \dash\fit::number($value['commentcount']); ?></a><?php }else{ ?><span class="sf-mute">-</span><?php } ?></td>
-        <?php } ?>
-
-        <td class="collapsing" ><?php echo \dash\fit::date_human(\dash\get::index($value, 'publishdate')); ?></td>
-        <td class="collapsing"><a href="<?php echo \dash\url::here(); ?>/posts?status=<?php echo \dash\get::index($value, 'status'); ?>&type=<?php echo \dash\get::index($value, 'type'); ?>"><?php echo T_(\dash\get::index($value, 'status')); ?></a></td>
       </tr>
-      <?php }//endfor ?>
+      <?php } //endfor ?>
     </tbody>
   </table>
 
@@ -137,70 +216,39 @@ $sortLink = \dash\data::sortLink();
 <?php } //endfunction ?>
 
 
+
+
+
+
 <?php function htmlFilter() {?>
-<p class="f fs14 msg info2">
-  <span class="c"><?php echo \dash\data::dataFilter(); ?></span>
-  <a class="cauto" href="<?php echo \dash\url::here(); ?>/posts<?php echo \dash\data::moduleType() ?>"><?php echo T_("Clear filters"); ?></a>
-</p>
-<?php } //endfunction ?>
-
-
-<?php function htmlFilterNoResult() {?>
 <p class="f fs14 msg warn2">
-  <span class="c"><?php echo T_("Result not found!"); ?> <?php echo T_("Search with new keywords."); ?></span>
-  <a class="cauto" href="<?php echo \dash\url::here(); ?>/posts<?php echo \dash\data::moduleType() ?>"><?php echo T_("Clear filters"); ?></a>
+  <span class="c"><?php echo \dash\data::filterBox(); ?></span>
+
+  <?php if(\dash\data::barcodeScaned()) {?>
+
+    <a class="c" href="<?php echo \dash\url::this(); ?>/add<?php echo \dash\data::barcodeScaned(); ?>" data-shortkey="118"><?php echo T_("Add new product"); ?> <kbd>f7</kbd></a>
+
+  <?php } //endif ?>
+
+  <a class="cauto" href="<?php echo \dash\url::that(); ?>"><?php echo T_("Clear filters"); ?></a>
 </p>
+
 <?php } //endfunction ?>
+
+
 
 
 <?php function htmlStartAddNew() {?>
-<p class="fs14 msg success2 pTB20"><?php echo T_("Hi!"); ?> <a href="<?php echo \dash\url::here(); ?>/posts/add<?php echo \dash\data::moduleType() ?>"><?php echo T_("Try to start with add new record!"); ?></a></p>
+
+<div class="fs14 msg info2 pTB20">
+  <p><?php echo T_("Hi!"); ?></p>
+  <p><?php echo T_("First step to set up your online store is add products."); ?> <?php echo T_("After add products, you can sell them to your customers."); ?></p>
+  <p><a href="<?php echo \dash\url::that(); ?>/add"><?php echo T_("Try to start with add new product!"); ?></a></p>
+
+</div>
+
+<img class="banner" src="<?php echo \dash\url::cdn(); ?>/img/product/camera1.png" align='<?php echo T_("add new product"); ?>'>
+
+
 <?php } //endfunction ?>
 
-
-
-
-
-<?php function pageSteps() {?>
-  <div class="f">
-
-    <div class="c">
-    <a class="dcard <?php if(!\dash\request::get('status') ) { echo 'active';} ?>" href='<?php echo \dash\url::this(); ?><?php if(\dash\request::get('type')) { echo '?type='. \dash\request::get('type');} ?>' data-shortkey="49ctrlshift" title='<?php echo T_("All publish posts"); ?>'>
-     <div class="statistic">
-      <div class="value"><?php echo \dash\fit::number(\dash\data::postCounter_all()); ?></div>
-      <div class="label"><i class="sf-all"></i> <?php echo T_("All"); ?> </div>
-     </div>
-    </a>
-   </div>
-
-   <div class="c">
-    <a class="dcard <?php if(\dash\request::get('status') === 'publish') { echo 'active';} ?>" href='<?php echo \dash\url::this(); ?>?status=publish<?php if(\dash\request::get('type')) { echo '&type='. \dash\request::get('type');} ?>' data-shortkey="49ctrlshift" title='<?php echo T_("All publish posts"); ?>'>
-     <div class="statistic">
-      <div class="value"><?php echo \dash\fit::number(\dash\data::postCounter_publish()); ?></div>
-      <div class="label"><i class="sf-publish"></i> <?php echo T_("Published"); ?> </div>
-     </div>
-    </a>
-   </div>
-
-   <div class="c">
-    <a class="dcard <?php if(\dash\request::get('status') === 'draft') { echo 'active';} ?>" href='<?php echo \dash\url::this(); ?>?status=draft<?php if(\dash\request::get('type')) { echo '&type='. \dash\request::get('type');} ?>' data-shortkey="50ctrlshift" title='<?php echo T_("All draft posts"); ?>'>
-     <div class="statistic">
-      <div class="value"><?php echo \dash\fit::number(\dash\data::postCounter_draft()); ?></div>
-      <div class="label"><i class="sf-edit"></i> <?php echo T_("Draft"); ?></div>
-     </div>
-    </a>
-   </div>
-
-   <div class="c">
-    <a class="dcard <?php if(\dash\request::get('status') === 'deleted') { echo 'active';} ?>" href='<?php echo \dash\url::this(); ?>?status=deleted<?php if(\dash\request::get('type')) { echo '&type='. \dash\request::get('type');} ?>' data-shortkey="51ctrlshift" title='<?php echo T_("All trash posts"); ?>'>
-     <div class="statistic">
-      <div class="value"><?php echo \dash\fit::number(\dash\data::postCounter_deleted()); ?></div>
-      <div class="label"><i class="sf-trash"></i> <?php echo T_("Deleted"); ?></div>
-     </div>
-    </a>
-   </div>
-
-
-
-  </div>
-<?php } //endfunction ?>
