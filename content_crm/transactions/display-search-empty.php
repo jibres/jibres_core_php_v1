82f39997ea615421c7f3488145be8bd164f70f4f
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-01 15:29:59 +0330

    add empty search result design

---
+++ content_crm/transactions/display-search-empty.php
@@ -0,0 +1,6 @@
+<section class="emptyState search">
+  <img src="<?php echo \dash\url::cdn(); ?>/img/gif/jibres-not-found-1.gif" alt='<?php echo T_("No result found!") ?>'>
+  <h2><?php echo T_("No result found!") ?></h2>
+
+  <h3><?php echo T_("Please try searching with another term") ?></h3>
+</section>
