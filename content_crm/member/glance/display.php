96226b0dd98627f6f0589bac6dfec0ff71917f01
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-02 18:46:38 +0330

    fix user dash

--- content_crm/member/glance/display.php
+++ content_crm/member/glance/display.php
@@ -5,87 +5,121 @@ $dashboardDetail = \dash\data::dashboardDetail();
 
 ?>
 <div class="row">
-  <div class="c-xs-12 c-sm-12 c-md-4">
+  <div class="c-xs-12 c-sm-12 c-md-8">
 
-    <nav class="items long">
-      <ul>
-        <li>
-          <a class="item f">
-            <div class="key"><?php echo T_('Account Create Date');?></div>
-            <div class="value datetime"><?php echo \dash\fit::date_time(\dash\data::dataRowMember_datecreated());?></div>
-            <div class="go detail"></div>
-          </a>
-        </li>
-        <li>
-          <a class="item f">
-            <div class="key"><?php echo T_('Last Updated');?></div>
-            <div class="value datetime"><?php echo \dash\fit::date_time(\dash\data::dataRowMember_datecreated());?></div>
-            <div class="go detail"></div>
-          </a>
-        </li>
-        <li>
-          <a class="item f disabled" href="<?php echo \dash\url::that(). '/logins?id='. \dash\request::get('id');?>">
-            <div class="key"><?php echo T_('Last login at');?></div>
-            <div class="value"><?php echo \dash\fit::date_time(\dash\get::index($dashboardDetail, 'last_login'));?></div>
-            <div class="go detail"></div>
-          </a>
-        </li>
-         <li>
+    <section class="row">
+     <div class="c-4">
+      <a href="<?php echo \dash\url::here() ?>/business"  class="stat">
+       <h3><?php echo T_("Active Order");?></h3>
+       <div class="val"></div>
+      </a>
+     </div>
+     <div class="c-4">
+      <a href="<?php echo \dash\url::here() ?>/domain" class="stat">
+       <h3><?php echo T_("Active Ticket");?></h3>
+       <div class="val"></div>
+      </a>
+     </div>
+     <div class="c-4">
+      <a href="<?php echo \dash\url::kingdom() ?>/account/billing" class="stat <?php if(\dash\get::index($dashboardDetail, 'balance')>0) echo " green"; ?>">
+       <h3><?php echo T_("Account Balance");?></h3>
+       <div class="val"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'balance'));?></div>
+      </a>
+     </div>
+    </section>
+
+
+    <div class="row">
+      <div class="c-xs-12 c-sm-12 c-md-6">
+        <nav class="items long">
+          <ul>
+            <li>
+              <a class="item f">
+                <div class="key"><?php echo T_('Account Create Date');?></div>
+                <div class="value datetime"><?php echo \dash\fit::date_time(\dash\data::dataRowMember_datecreated());?></div>
+                <div class="go detail"></div>
+              </a>
+            </li>
+            <li>
+              <a class="item f">
+                <div class="key"><?php echo T_('Last Updated');?></div>
+                <div class="value datetime"><?php echo \dash\fit::date_time(\dash\data::dataRowMember_datecreated());?></div>
+                <div class="go detail"></div>
+              </a>
+            </li>
+            <li>
+              <a class="item f disabled" href="<?php echo \dash\url::that(). '/logins?id='. \dash\request::get('id');?>">
+                <div class="key"><?php echo T_('Last login at');?></div>
+                <div class="value"><?php echo \dash\fit::date_time(\dash\get::index($dashboardDetail, 'last_login'));?></div>
+                <div class="go detail"></div>
+              </a>
+            </li>
+             <li>
 <?php if(\dash\get::index($dashboardDetail, 'last_ip')) {?>
-          <a class="item f" target="_blank" href="<?php echo \dash\url::kingdom(). '/ip/'. \dash\get::index($dashboardDetail, 'last_ip');?>">
-            <div class="key"><?php echo T_('Last IP');?></div>
-            <code class="value"><?php echo \dash\get::index($dashboardDetail, 'last_ip');?></code>
-            <div class="go surveillance"></div>
-          </a>
+              <a class="item f" target="_blank" href="<?php echo \dash\url::kingdom(). '/ip/'. \dash\get::index($dashboardDetail, 'last_ip');?>">
+                <div class="key"><?php echo T_('Last IP');?></div>
+                <code class="value"><?php echo \dash\get::index($dashboardDetail, 'last_ip');?></code>
+                <div class="go external"></div>
+              </a>
 <?php } else {?>
-          <a class="item f">
-            <div class="key"><?php echo T_('Last IP');?></div>
-            <div class="value"><?php echo T_("Not logged in yet!");?></div>
-            <div class="go surveillance"></div>
-          </a>
+              <a class="item f">
+                <div class="key"><?php echo T_('Last IP');?></div>
+                <div class="value"><?php echo T_("Not logged in yet!");?></div>
+                <div class="go surveillance"></div>
+              </a>
 <?php }?>
-        </li>
-      </ul>
-    </nav>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="c-xs-12 c-sm-12 c-md-6">
+        <nav class="items long">
+          <ul>
+            <li>
+              <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
+                <div class="key"><?php echo T_('Last order');?></div>
+                <div class="value"><?php echo \dash\fit::date_time(\dash\get::index($dashboardDetail, 'last_order'));?></div>
+                <div class="go"></div>
+              </a>
+            </li>
+            <li>
+              <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
+                <div class="key"><?php echo T_('Total payed');?></div>
+                <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'total_paid'));?></div>
+                <div class="go"></div>
+              </a>
+            </li>
+            <li>
+              <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
+                <div class="key"><?php echo T_('Average order pay');?></div>
+                <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'average_order_pay'));?></div>
+                <div class="go"></div>
+              </a>
+            </li>
+             <li>
+              <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
+                <div class="key"><?php echo T_('Balance');?></div>
+                <div class="value"></div>
+                <div class="go"></div>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
 
 
-    <nav class="items long">
-      <ul>
-        <li>
-          <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
-            <div class="key"><?php echo T_('Last order');?></div>
-            <div class="value"><?php echo \dash\fit::date_time(\dash\get::index($dashboardDetail, 'last_order'));?></div>
-            <div class="go"></div>
-          </a>
-        </li>
-        <li>
-          <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
-            <div class="key"><?php echo T_('Total payed');?></div>
-            <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'total_paid'));?></div>
-            <div class="go"></div>
-          </a>
-        </li>
-        <li>
-          <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
-            <div class="key"><?php echo T_('Average order pay');?></div>
-            <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'average_order_pay'));?></div>
-            <div class="go"></div>
-          </a>
-        </li>
-         <li>
-          <a class="item f" href="<?php echo \dash\url::this(). '/detail'. $myID;?>">
-            <div class="key"><?php echo T_('Balance');?></div>
-            <div class="value"><?php echo \dash\fit::number(\dash\get::index($dashboardDetail, 'balance'));?></div>
-            <div class="go"></div>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div class="row">
+      <div class="c-xs-12 c-sm-12 c-md-6">
+        list1
+      </div>
+      <div class="c-xs-12 c-sm-12 c-md-6">
+        list2
+      </div>
+    </div>
 
   </div>
-  <div class="c-xs-12 c-sm-12 c-md-4">
 
-  </div>
   <div class="c-xs-12 c-sm-12 c-md-4">
     <nav class="items long">
       <ul>
