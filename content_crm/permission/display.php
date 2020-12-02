70339058435f2f3c2c8f640d52914c68fc7f30c0
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-02 16:11:53 +0330

    fix permission

--- content_crm/permission/display.php
+++ content_crm/permission/display.php
@@ -1,4 +1,3 @@
-<h3><?php echo T_("List of permissions") ?></h3>
 <nav class="items">
    <ul>
   <?php foreach (\dash\data::dataTable() as $key => $value) { ?>
@@ -6,8 +5,9 @@
         <a class="f" href="<?php echo \dash\url::this(). '/edit?id='. $value['id']; ?>">
           <div class="key"><?php echo \dash\get::index($value, 'key'); ?></div>
           <?php if(\dash\get::index($value, 'user_count')) {?>
-            <div class="value"><?php echo \dash\fit::number(\dash\get::index($value, 'user_count')). ' '. T_("Person") ?> </div>
+            <div class="key"><?php echo \dash\fit::number(\dash\get::index($value, 'user_count')). ' '. T_("Person") ?> </div>
           <?php } //endif ?>
+          <div class="value">5 permission</div>
           <div class="go"></div>
         </a>
       </li>
