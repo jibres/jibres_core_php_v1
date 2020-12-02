2b46fa9fb01a8d8f1fed246930fe96aa391c5d99
Reza <rm.biqarar@gmail.com>
2020-12-02 15:31:02 +0330

    Fix view

--- content_crm/transactions/view.php
+++ content_crm/transactions/view.php
@@ -21,59 +21,25 @@ class view
 		\dash\data::listEngine_filter(true);
 		\dash\data::listEngine_sort(true);
 
-
-		$search_string            = \dash\request::get('q');
-		if($search_string)
-		{
-			\dash\face::title(\dash\face::title() . ' | '. T_('Search for :search', ['search' => $search_string]));
-		}
-
 		$args =
 		[
-			'sort'  => \dash\request::get('sort'),
-			'order' => \dash\request::get('order'),
+			'order'  => \dash\request::get('order'),
+			'sort'   => \dash\request::get('sort'),
+			'status' => \dash\request::get('status'),
 		];
 
-		if(!$args['sort'])
+		$search_string   = \dash\validate::search(\dash\request::get('q'));
+		$transactionList = \dash\app\transaction\search::list($search_string, $args);
+
+		\dash\data::dataTable($transactionList);
+
+		$isFiltered = \dash\app\transaction\search::is_filtered();
+		\dash\data::isFiltered($isFiltered);
+
+		if($isFiltered)
 		{
-			$args['sort'] = 'transactions.id';
+			\dash\face::title(\dash\face::title() . '  '. T_('Filtered'));
 		}
-
-		if(!$args['order'])
-		{
-			$args['order'] = 'DESC';
-		}
-
-		if(\dash\request::get('status'))
-		{
-			$args['transactions.status'] = \dash\request::get('status');
-		}
-
-		if(\dash\request::get('condition'))
-		{
-			$args['condition'] = \dash\request::get('condition');
-		}
-
-		if(\dash\request::get('payment'))
-		{
-			$args['payment'] = \dash\request::get('payment');
-		}
-
-		if(\dash\request::get('type'))
-		{
-			$args['transactions.type'] = \dash\request::get('type');
-		}
-		$dataTable = \dash\app\transaction::list(\dash\request::get('q'), $args);
-
-		\dash\data::sortLink(\content_su\view::su_make_sortLink(\dash\app\transaction::$sort_field, \dash\url::this()));
-		\dash\data::dataTable($dataTable);
-
-		$check_empty_datatable = $args;
-		unset($check_empty_datatable['sort']);
-		unset($check_empty_datatable['order']);
-
-		// set dataFilter
-		\dash\data::dataFilter(\content_su\view::su_createFilterMsg($search_string, $check_empty_datatable));
 	}
 }
 ?>
