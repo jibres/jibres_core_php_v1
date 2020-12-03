00edbfe959e76f89a69e8d34a9c9333eefd7f5d5
Reza <rm.biqarar@gmail.com>
2020-12-03 16:38:13 +0330

    Remove content crm old

--- content_crmold/sms/home/display.php
+++
@@ -1,6 +0,0 @@
-
-<div class="dcard x3 txtC" >
- <div class="statistic"><div class="value"><i class="sf-card"></i></div></div>
-  <h2><?php echo T_("Your sms panel balance"); ?><br><b><?php echo \dash\fit::number(\dash\data::SMSbalance_remaincredit() / 10); ?></b> <?php echo \lib\currency::unit(); ?></h2>
-
-</div>
