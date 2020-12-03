6b2e38082c9ec43ea6a224c11c946b3b066f387c
Reza <rm.biqarar@gmail.com>
2020-12-03 16:39:48 +0330

    add sms view module

---
+++ content_crm/sms/home/controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace content_crm\staff;
+
+
+class controller
+{
+	public static function routing()
+	{
+		\dash\permission::access('crmSms');
+	}
+}
+?>
---
+++ content_crm/sms/view/view.php
@@ -0,0 +1,50 @@
+<?php
+namespace content_crm\staff;
+
+class view
+{
+
+	public static function config()
+	{
+		\dash\face::title(T_("Staff List"));
+
+
+		$args =
+		[
+			'order'     => \dash\request::get('order'),
+			'sort'      => \dash\request::get('sort'),
+			'status'    => \dash\request::get('status'),
+			'show_type' => 'staff',
+		];
+
+
+
+		$search_string   = \dash\validate::search(\dash\request::get('q'));
+		$userList = \dash\app\user\search::list($search_string, $args);
+
+		\dash\data::dataTable($userList);
+
+		$isFiltered = \dash\app\user\search::is_filtered();
+		\dash\data::isFiltered($isFiltered);
+
+		if($isFiltered)
+		{
+			\dash\face::title(\dash\face::title() . '  '. T_('Filtered'));
+		}
+
+		\dash\data::back_link(\dash\url::here());
+		\dash\data::back_text(T_('CRM'));
+
+		\dash\data::listEngine_start(true);
+		\dash\data::listEngine_search(\dash\url::that());
+		\dash\data::listEngine_filter(false);
+		\dash\data::listEngine_sort(false);
+
+
+
+	}
+
+
+
+}
+?>
