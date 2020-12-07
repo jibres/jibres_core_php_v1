1a17b2760b200583ec3e6efab345258b6a8d948d
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-07 17:39:30 +0330

    use large image

--- content_business/app/view.php
+++ content_business/app/view.php
@@ -14,7 +14,7 @@ class view
 		$myTitle .= ' | '. \lib\store::detail('title');
 
 		\dash\face::title($myTitle);
-		\dash\face::desc(T_("Download the app today! Shopping on the go is fast and easy with our free app."));
+		\dash\face::desc(T_("Download the app today! Shopping on the go is fast and easy with our free app. Enjoy more discounts on the our app and shop what's new & now"));
 
 		$appDetail = \lib\app\application\detail::get_dowload_page();
 		\dash\data::appDetail($appDetail);
@@ -24,6 +24,7 @@ class view
 		\dash\data::back_link(\dash\url::kingdom());
 
 		\dash\face::cover(\dash\url::cdn(). '/img/cover/Jibres-cover-app-1.jpg');
+		\dash\face::twitterCard('summary_large_image');
 	}
 }
 ?>
