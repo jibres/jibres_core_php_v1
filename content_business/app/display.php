1a91171aabab166c84c01fcf227507084940f417
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-07 13:17:20 +0330

    fix app page link

--- content_business/app/display.php
+++ content_business/app/display.php
@@ -8,19 +8,19 @@
           <p><?php echo \dash\data::appDetail_downloaddesc(); ?></p>
           <div class="dl">
             <?php if(\dash\data::appDetail_googleplay()) {?>
-              <a target="_blank" href="<?php echo \dash\data::appDetail_googleplay(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/app-dl-googleplay.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from Google Play'); ?>'></a>
+              <a target="_blank" href="<?php echo \dash\data::appDetail_googleplay(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/get/app-dl-googleplay.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from Google Play'); ?>'></a>
             <?php } //endif ?>
 
             <?php if(\dash\data::appDetail_cafebazar()) {?>
-            <a target="_blank" href="<?php echo \dash\data::appDetail_cafebazar(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/app-dl-cafebazar.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from CafeBazar'); ?>'></a>
+            <a target="_blank" href="<?php echo \dash\data::appDetail_cafebazar(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/get/app-dl-cafebazar.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from CafeBazar'); ?>'></a>
             <?php } //endif ?>
 
             <?php if(\dash\data::appDetail_myket()) {?>
-            <a target="_blank" href="<?php echo \dash\data::appDetail_myket(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/app-dl-direct.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from Myket'); ?>'></a>
+            <a target="_blank" href="<?php echo \dash\data::appDetail_myket(); ?>" rel="nofollow"><img src="<?php echo \dash\url::cdn(); ?>/img/app/get/app-dl-direct.png" alt='<?php echo \dash\data::appDetail_downloadtitle(). ' - '. T_('Download from Myket'); ?>'></a>
             <?php } //endif ?>
 
 
-			       <a data-direct href="<?php echo \dash\url::kingdom(). '/apk'; ?>"><img src="<?php echo \dash\url::cdn(); ?>/img/app/app-dl-direct.png" alt='<?php echo T_('Direct download'). ' - '. \dash\data::appDetail_downloadtitle(); ?>'></a>
+			       <a data-direct href="<?php echo \dash\url::kingdom(). '/apk'; ?>"><img src="<?php echo \dash\url::cdn(); ?>/img/app/get/app-dl-direct.png" alt='<?php echo T_('Direct download'). ' - '. \dash\data::appDetail_downloadtitle(); ?>'></a>
             </a>
           </div>
         </div>
