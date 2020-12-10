e9b101fa49e43f8211faa5729873be7687421634
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-10 17:44:59 +0330

    fix variant design

--- content_a/products/variants/display-add-variants.php
+++ content_a/products/variants/display-add-variants.php
@@ -5,18 +5,18 @@
 
         <p class="msg"><?php echo T_("You can differentiate your product in terms of three features"); ?></p>
 
-        <div class="f">
-          <div class="cauto mB10">
-            <label><?php echo T_("Property #1"); if(!\dash\get::index($variantsList, 'variants', 'option1', 'name')) {?> <small><?php echo T_("For example: Color") ?></small><?php } // endif ?></label>
+        <div class="row">
+          <div class="c-xs-12 c-sm-12 c-md-4">
+            <label><?php echo T_("Property #1"); ?></label>
             <div class="input">
-              <input type="text" name="optionname1" value="<?php echo \dash\get::index($variantsList, 'variants', 'option1', 'name'); ?>">
+              <input type="text" name="optionname1" value="<?php echo \dash\get::index($variantsList, 'variants', 'option1', 'name'); ?>" placeholder="<?php echo T_("Like Size") ?>">
             </div>
           </div>
 
-          <div class="c pLa5 mB10">
+          <div class="c-xs-12 c-sm-12 c-md-8">
+            <label><?php echo T_("Value"); ?></label>
             <div>
-              <label>&nbsp;</label>
-              <select name="optionvalue1[]" id="optionvalue1" class="select22" data-model="tag" multiple="multiple">
+              <select name="optionvalue1[]" id="optionvalue1" class="select22" data-model="tag" multiple="multiple" data-placeholder="<?php echo T_("Like Medium, Large, xLarge, xxLarge") ?>">
                 <?php if(isset($variantsList['variants']['option1']['value']) && is_array($variantsList['variants']['option1']['value'])) { foreach ($variantsList['variants']['option1']['value'] as $key => $value) {?>
                   <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
                 <?php } } //endfor //endif  ?>
@@ -26,18 +26,18 @@
 
         </div>
 
-        <div class="f">
-          <div class="cauto mB10">
-            <label><?php echo T_("Property #2"); if(!\dash\get::index($variantsList, 'variants', 'option2', 'name')) {?> <small><?php echo T_("For example: Size") ?></small><?php } // endif ?></label>
+        <div class="row">
+          <div class="c-xs-12 c-sm-12 c-md-4">
+            <label><?php echo T_("Property #2"); ?></label>
             <div class="input">
-              <input type="text" name="optionname2" value="<?php echo \dash\get::index($variantsList, 'variants', 'option2', 'name'); ?>">
+              <input type="text" name="optionname2" value="<?php echo \dash\get::index($variantsList, 'variants', 'option2', 'name'); ?>" placeholder="<?php echo T_("Like Color") ?>">
             </div>
           </div>
 
-          <div class="c pLa5 mB10">
+          <div class="c-xs-12 c-sm-12 c-md-8">
+            <label><?php echo T_("Value"); ?></label>
             <div>
-              <label>&nbsp;</label>
-              <select name="optionvalue2[]" id="optionvalue2" class="select22" data-model="tag" multiple="multiple">
+              <select name="optionvalue2[]" id="optionvalue2" class="select22" data-model="tag" multiple="multiple" data-placeholder="<?php echo T_("Like Red, Blue, Green") ?>">
                 <?php if(isset($variantsList['variants']['option2']['value']) && is_array($variantsList['variants']['option2']['value'])) { foreach ($variantsList['variants']['option2']['value'] as $key => $value) {?>
                   <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
                 <?php } } //endfor //endif  ?>
@@ -47,18 +47,18 @@
 
         </div>
 
-        <div class="f">
-          <div class="cauto mB10">
-            <label><?php echo T_("Property #3"); if(!\dash\get::index($variantsList, 'variants', 'option3', 'name')) {?> <small><?php echo T_("For example: Material") ?></small><?php } // endif ?></label>
+        <div class="row">
+          <div class="c-xs-12 c-sm-12 c-md-4">
+            <label><?php echo T_("Property #3"); ?></label>
             <div class="input">
-              <input type="text" name="optionname3" value="<?php echo \dash\get::index($variantsList, 'variants', 'option3', 'name'); ?>">
+              <input type="text" name="optionname3" value="<?php echo \dash\get::index($variantsList, 'variants', 'option3', 'name'); ?>" placeholder="<?php echo T_("Like Material") ?>">
             </div>
           </div>
 
-          <div class="c pLa5 mB10">
+          <div class="c-xs-12 c-sm-12 c-md-8">
+            <label><?php echo T_("Value"); ?></label>
             <div>
-            <label>&nbsp;</label>
-              <select name="optionvalue3[]" id="optionvalue3" class="select22" data-model="tag" multiple="multiple">
+              <select name="optionvalue3[]" id="optionvalue3" class="select22" data-model="tag" multiple="multiple" data-placeholder="<?php echo T_("Like Wood, Bamboo, xlarge") ?>">
                 <?php if(isset($variantsList['variants']['option3']['value']) && is_array($variantsList['variants']['option3']['value'])) { foreach ($variantsList['variants']['option3']['value'] as $key => $value) {?>
                   <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
                 <?php } } //endfor //endif  ?>
