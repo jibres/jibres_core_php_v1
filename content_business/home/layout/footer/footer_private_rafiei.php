d726a37bbd22228223336ebf15ca93b7192780a8
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-11 16:41:26 +0100

    fix

--- content_business/home/layout/footer/footer_private_rafiei.php
+++ content_business/home/layout/footer/footer_private_rafiei.php
@@ -1,4 +1,15 @@
-<div class="jFooter201" data-footer=201>
+<div class="jFooterRafiei">
+	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 100" xml:space="preserve">
+		<style type="text/css">
+			.st0{opacity:0.7;fill:#005E62;enable-background:new    ;}
+			.st1{fill:#005E62;}
+		</style>
+		<path class="st0" d="M2000,100V0c-312.4,67.2-649.1,98.6-1000,98.6S312.4,67.1,0,0v100H2000z"/>
+		<path class="st0" d="M2000,100V26.6c-312.4,46.6-649.1,72-1000,72S312.4,73.1,0,26.5V100H2000z"/>
+		<path class="st1" d="M2000,100V53.1c-312.4,29.4-649.1,45.5-1000,45.5S312.4,82.5,0,53v47H2000z"/>
+	</svg>
+
+
 	<div class="avand-xl">
 
 		<div class="top">
