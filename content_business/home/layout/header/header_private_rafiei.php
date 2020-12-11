54df9d7a8f8968504ccabd46b8020ab7c19236eb
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-11 13:40:50 +0330

    fix

--- content_business/home/layout/header/header_private_rafiei.php
+++ content_business/home/layout/header/header_private_rafiei.php
@@ -9,12 +9,12 @@
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 2000 100" style="enable-background:new 0 0 2000 100;" xml:space="preserve">
 <style type="text/css">
-	.header_curve1{opacity:0.7;fill:#053353;}
+	.header_curve1{opacity:0.7;fill:#053353;enable-background:new;}
 	.header_curve2{fill:#053353;}
 </style>
 <path class="header_curve1" d="M0,0v100C312.4,32.9,649.1,1.4,1000,1.4s687.6,31.4,1000,98.6V0H0z"/>
-<path class="header_curve1" d="M0,0v72.4C312.4,26.5,649.1,1.4,1000,1.4s687.6,25,1000,70.9V0H0z"/>
-<path class="header_curve2" d="M0,0v47.1C312.4,17.5,649.1,1.4,1000,1.4s687.6,16.1,1000,45.6V0H0z"/>
+<path class="header_curve1" d="M0,0v73.5C312.4,26.9,649.1,1.4,1000,1.4s687.6,25.4,1000,72V0H0z"/>
+<path class="header_curve2" d="M0,0v47C312.4,17.5,649.1,1.4,1000,1.4s687.6,16.1,1000,45.5V0H0z"/>
 </svg>
 
 
