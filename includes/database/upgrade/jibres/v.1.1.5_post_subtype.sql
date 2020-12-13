2674e3b3ab739f60ab917eacc6c75618a86c57d5
Reza <rm.biqarar@gmail.com>
2020-12-13 14:03:09 +0330

    update post subtype

--- includes/database/upgrade/jibres/v.1.1.5_post_subtype.sql
+++ includes/database/upgrade/jibres/v.1.1.5_post_subtype.sql
@@ -1,3 +1,6 @@
 ALTER TABLE jibres.posts DROP `subtype`;
 ALTER TABLE jibres.posts ADD `subtype` enum('standard', 'gallery','video','audio','help_center') NULL DEFAULT 'standard';
 ALTER TABLE jibres.posts ADD INDEX `search_index_subtype` (`subtype`);
+
+
+UPDATE jibres.posts SET posts.subtype = 'help_center', posts.type = 'post' WHERE posts.type = 'help';
