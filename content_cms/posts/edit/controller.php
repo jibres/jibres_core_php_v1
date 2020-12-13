f77fdec777a0c3ebb32f62f79a9ce42262d0e30d
Reza <rm.biqarar@gmail.com>
2020-12-13 17:39:54 +0330

    Fix route post and page

--- content_cms/posts/edit/controller.php
+++ content_cms/posts/edit/controller.php
@@ -14,6 +14,21 @@ class controller
 			\dash\header::status(403, T_("Invalid id"));
 		}
 
+		if(a($detail, 'type') === 'post' && \dash\url::module() !== 'posts')
+		{
+			\dash\header::status(404, T_("This item is not a post!"));
+		}
+
+		if(a($detail, 'type') === 'page' && \dash\url::module() !== 'pages')
+		{
+			\dash\header::status(404, T_("This item is not a page!"));
+		}
+
+		if(a($detail, 'type') === 'help' && \dash\url::module() !== 'helps')
+		{
+			\dash\header::status(404, T_("This item is not a help!"));
+		}
+
 		\dash\data::dataRow($detail);
 
 	}
