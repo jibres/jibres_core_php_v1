f420c7453ce49d8d600b28427a37b32b7408da8b
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-16 12:01:43 +0100

    fix header svg

--- content_business/home/layout/header/header_private_rafiei.php
+++ content_business/home/layout/header/header_private_rafiei.php
@@ -5,11 +5,5 @@
   </a>
   <?php \lib\website::menu('header_menu_1'); ?>
 
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 100" xml:space="preserve">
-	<path class="header_curve1" fill="rgba(5,51,83,0.7)" d="M0,0v100C312.4,32.9,649.1,1.4,1000,1.4s687.6,31.4,1000,98.6V0H0z"/>
-	<path class="header_curve1" fill="rgba(5,51,83,0.7)" d="M0,0v73.5C312.4,26.9,649.1,1.4,1000,1.4s687.6,25.4,1000,72V0H0z"/>
-	<path class="header_curve2" fill="rgba(5,51,83,1)" d="M0,0v47C312.4,17.5,649.1,1.4,1000,1.4s687.6,16.1,1000,45.5V0H0z"/>
-</svg>
-
-
+	<svg class='bottom' xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 100" xml:space="preserve"><path class="header_curve1" fill="rgba(5,51,83,0.7)" d="M0,0v100C312.4,32.9,649.1,1.4,1000,1.4s687.6,31.4,1000,98.6V0H0z"/><path class="header_curve1" fill="rgba(5,51,83,0.7)" d="M0,0v73.5C312.4,26.9,649.1,1.4,1000,1.4s687.6,25.4,1000,72V0H0z"/><path class="header_curve2" fill="rgba(5,51,83,1)" d="M0,0v47C312.4,17.5,649.1,1.4,1000,1.4s687.6,16.1,1000,45.5V0H0z"/></svg>
 </div>
