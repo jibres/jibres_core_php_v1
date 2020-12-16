567e84f33144c0b94134400e31c532136249dcfd
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-16 18:18:05 +0100

    add video line

--- content_business/home/layout/body/titleline.php
+++ content_business/home/layout/body/titleline.php
@@ -15,3 +15,36 @@ if($titleline)
 }
 
 ?>
+
+<?php if(\dash\url::isLocal()) { ?>
+<section class="avand-lg imgLine">
+		<video controls="controls" preload="metadata" poster="http://cloud.talambar.local/jb2mm/202012/1-78dbe6f473836e1717a971b2fed5f550.jpg">
+			 <source type="video/mp4" src="http://cloud.talambar.local/jb2mm/202012/9-eb5c1399a871211c7e7ed732d15e3a8b.mp4">
+		</video>
+
+		<div class="row padMore2">
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+			<div class="c-xs-12 c-sm-6 c-md-4">
+				<a href=""><img src="http://cloud.talambar.local/jb2mm/202012/5-df2917707a77bfb3b735c34687b49eda.jpg"></a>
+			</div>
+		</div>
+</section>
+
+
+
+<?php } ?>
+
