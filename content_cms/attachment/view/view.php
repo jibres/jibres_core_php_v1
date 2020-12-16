34ea747fea29e16f9eeb8472bde4d4d22e971f1a
Reza <rm.biqarar@gmail.com>
2020-12-16 15:32:21 +0330

    update transalte

--- content_cms/attachment/view/view.php
+++ content_cms/attachment/view/view.php
@@ -6,7 +6,7 @@ class view
 	public static function config()
 	{
 
-		\dash\face::title(T_("View Attachemnts"));
+		\dash\face::title(T_("View Attachemnt"));
 
 		\dash\data::back_text(T_("Attachemnts"));
 		\dash\data::back_link(\dash\url::this());
