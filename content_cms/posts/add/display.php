24f9681bc6c47031639cac58c7754099112e96ba
Reza <rm.biqarar@gmail.com>
2020-12-23 15:12:48 +0330

    fix position of subtype

--- content_cms/posts/add/display.php
+++ content_cms/posts/add/display.php
@@ -2,13 +2,8 @@
 	<form method="post" autocomplete="off">
 		<div class="box">
 			<div class="body">
-				<div class="input mB10">
-					<input type="text" name="title" id="title" placeholder='<?php echo T_("Enter title here"); ?> *'  <?php \dash\layout\autofocus::html() ?> required maxlength='200' minlength="1" pattern=".{1,200}">
-				</div>
-				<textarea class="txt mB10" data-editor id='descInput' name="content" placeholder='<?php echo T_("Write your post ..."); ?>' maxlength='100000' rows="15"></textarea>
-				<p class="fc-mute mB20"><?php echo T_("First type main text and save as draft, then complete and publish it."); ?></p>
 				<?php if(\dash\url::module() === 'posts') {?>
-					<div class="">
+					<div class="mB10">
 						<div class="txtB mB10"><?php echo T_("Choose post type") ?></div>
 						<div class="row">
 							<div class="c-xs-12 c-sm-6 c-md-3">
@@ -38,6 +33,11 @@
 						</div>
 					</div>
 				<?php } //endif ?>
+				<div class="input mB10">
+					<input type="text" name="title" id="title" placeholder='<?php echo T_("Enter title here"); ?> *'  <?php \dash\layout\autofocus::html() ?> required maxlength='200' minlength="1" pattern=".{1,200}">
+				</div>
+				<textarea class="txt mB10" data-editor id='descInput' name="content" placeholder='<?php echo T_("Write your post ..."); ?>' maxlength='100000' rows="15"></textarea>
+				<p class="fc-mute mB0-f"><?php echo T_("First type main text and save as draft, then complete and publish it."); ?></p>
 			</div>
 			<footer class="txtRa">
 				<button class="btn master"><?php echo T_("Save as draft"); ?></button>
