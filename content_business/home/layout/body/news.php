6206d26e4262550823d8bf2659a03fcbe8f8da57
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-23 16:57:04 +0100

    some html fix

--- content_business/home/layout/body/news.php
+++ content_business/home/layout/body/news.php
@@ -9,21 +9,23 @@ if($postList && is_array($postList)) {?>
    <div class="row padMore2">
 <?php foreach ($postList as $key => $value) {?>
 <?php $myPuzzle = \lib\app\website\puzzle::layout($key, $line_detail); ?>
-
     <div class="<?php echo a($myPuzzle, 'class'); ?>">
+<?php if( a($value, 'gallery_array', 0, 'path') && a($myPuzzle, 'playMode') === 'video') { ?>
+     <video controls preload='meta'<?php if(a($value, 'poster')) { echo " poster='". \lib\filepath::fix(a($value, 'poster')). "'";} ?>>
+      <source type="<?php echo a($value, 'gallery_array', 0, 'mime'); ?>" src="<?php echo \lib\filepath::fix(a($value, 'gallery_array', 0, 'path')); ?>">
+     </video>
+<?php } elseif( a($value, 'gallery_array', 0, 'path') && a($myPuzzle, 'playMode') === 'audio') { ?>
+     <audio controls preload='meta'>
+      <source type="<?php echo a($value, 'gallery_array', 0, 'mime'); ?>" src="<?php echo \lib\filepath::fix(a($value, 'gallery_array', 0, 'path')); ?>">
+     </audio>
+<?php } elseif (a($value, 'thumb')) { ?>
+     <a<?php if(a($value, 'link')) { echo ' href="'.  a($value, 'link'). '"'; if(a($value, 'target')) { echo ' target="_blank"'; }} ?>>
+      <img src="<?php echo \lib\filepath::fix(a($value, 'thumb')); ?>" alt="<?php echo a($value, 'title'); ?>">
+     </a>
+<?php } ?>
+
      <a<?php if(a($value, 'link')) { echo ' href="'.  a($value, 'link'). '"'; if(a($value, 'target')) { echo ' target="_blank"'; }} ?>>
       <figure>
-<?php if( a($value, 'gallery_array', 0, 'path') && a($myPuzzle, 'playMode') === 'video') { ?>
-      <video controls preload='meta'<?php if(a($value, 'poster')) { echo " poster='". \lib\filepath::fix(a($value, 'poster')). "'";} ?>>
-        <source type="<?php echo a($value, 'gallery_array', 0, 'mime'); ?>" src="<?php echo \lib\filepath::fix(a($value, 'gallery_array', 0, 'path')); ?>">
-      </video>
-<?php } elseif( a($value, 'gallery_array', 0, 'path') && a($myPuzzle, 'playMode') === 'audio') { ?>
-      <audio controls preload='meta'>
-        <source type="<?php echo a($value, 'gallery_array', 0, 'mime'); ?>" src="<?php echo \lib\filepath::fix(a($value, 'gallery_array', 0, 'path')); ?>">
-      </audio>
-<?php } elseif (a($value, 'thumb')) { ?>
-       <img src="<?php echo \lib\filepath::fix(a($value, 'thumb')); ?>" alt="<?php echo a($value, 'title'); ?>">
-<?php } ?>
        <figcaption><h2><?php echo a($value, 'title'); ?></h2></figcaption>
       </figure>
      </a>
