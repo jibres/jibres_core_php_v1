bc91273f889ed9238cddb23bedf4bb86c1ddaaa1
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-24 14:15:55 +0100

    create img, video, audio

--- lib/app/website/generator/datablock.php
+++ lib/app/website/generator/datablock.php
@@ -36,16 +36,101 @@ class datablock
 		return $html;
 	}
 
+
 	private static function everyItem($_list, $_blockSetting)
 	{
 		$html = '';
 		foreach ($_list as $key => $value)
 		{
 			$myPuzzle = \lib\app\website\puzzle::layout($key, $_blockSetting);
-			$html = '<div class="'. a($myPuzzle, 'class'). '">';
+			$html .= '<div class="'. a($myPuzzle, 'class'). '">';
+			{
+				$myGallery0 = a($value, 'gallery_array', 0, 'path');
+				$playMode   = a($myPuzzle, 'playMode');
+
+				if( $myGallery0 && $playMode === 'video')
+				{
+					$html .= self::createVideoEl($value);
+				}
+				elseif( $myGallery0 && $playMode === 'audio')
+				{
+					$html .= self::createAudioEl($value);
+				}
+				elseif (a($value, 'thumb'))
+				{
+					$html .= self::createImgEl($value);
+				}
+			}
+			$html .= '</div>';
 		}
 		return $html;
 	}
+
+
+	private static function createVideoEl($_prop)
+	{
+		if(!a($_prop, 'gallery_array', 0, 'path'))
+		{
+			return null;
+		}
+		$videoEl = '<video';
+		$videoEl .= ' controls';
+		$videoEl .= '  preload="meta"';
+		if(a($_prop, 'poster'))
+		{
+			$videoEl .= ' poster="'. \lib\filepath::fix(a($_prop, 'poster')). '"';
+		}
+		$videoEl .= '>';
+		{
+			$videoEl .= '<source';
+			$videoEl .= ' type="'. a($_prop, 'gallery_array', 0, 'mime'). '"';
+			$videoEl .= ' src="'. \lib\filepath::fix(a($_prop, 'gallery_array', 0, 'path')). '"';
+			$videoEl .= '>';
+		}
+
+		$videoEl .= '</video>';
+
+		return $videoEl;
+	}
+
+
+	private static function createAudioEl($_prop)
+	{
+		if(!a($_prop, 'gallery_array', 0, 'path'))
+		{
+			return null;
+		}
+		$audioEl = '<audio';
+		$audioEl .= ' controls';
+		$audioEl .= '  preload="meta"';
+		$audioEl .= '>';
+		{
+			$audioEl .= '<source';
+			$audioEl .= ' type="'. a($_prop, 'gallery_array', 0, 'mime'). '"';
+			$audioEl .= ' src="'. \lib\filepath::fix(a($_prop, 'gallery_array', 0, 'path')). '"';
+			$audioEl .= '>';
+		}
+
+		$audioEl .= '</audio>';
+
+		return $audioEl;
+	}
+
+
+	private static function createImgEl($_prop)
+	{
+		if(!a($_prop, 'thumb'))
+		{
+			return null;
+		}
+		$imgEl = '<img';
+		$imgEl .= ' src="'. \lib\filepath::fix(a($_prop, 'thumb')). '"';
+		$imgEl .= ' alt="'. a($_prop, 'title'). '"';
+		$imgEl .= '>';
+
+		return $imgEl;
+	}
+
 
 }
 ?>
