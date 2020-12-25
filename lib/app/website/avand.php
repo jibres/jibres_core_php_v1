86632b634f2e50a6dc09ed185ddd1ad1ab161e0d
Reza <rm.biqarar@gmail.com>
2020-12-25 09:13:24 +0330

    add class for avand

---
+++ lib/app/website/avand.php
@@ -0,0 +1,53 @@
+<?php
+namespace lib\app\website;
+
+class avand
+{
+	public static function list()
+	{
+
+		$list           = [];
+		$list["avand"]     = ['title' => T_("Container"), 'default' => true,];
+		$list["avand-sm"]  = ['title' => T_("Small"), 'default' => false,];
+		$list["avand-md"]  = ['title' => T_("Medium"), 'default' => false,];
+		$list["avand-lg"]  = ['title' => T_("Large"), 'default' => false,];
+		$list["avand-xl"]  = ['title' => T_("X Large"), 'default' => false,];
+		$list["avand-xxl"] = ['title' => T_("XX Large"), 'default' => false,];
+		$list["none"]      = ['title' => T_("Without container"), 'default' => false,];
+
+		return $list;
+
+	}
+
+
+	public static function input_check()
+	{
+		$list = self::list();
+		return ['enum' => array_keys($list)];
+	}
+
+
+	public static function select_html($_current_data = null)
+	{
+		$result = '';
+
+		$list = self::list();
+
+		foreach ($list as $key => $value)
+		{
+			$result.= '<option value="'. $key. '"';
+			if($key == $_current_data || (!$_current_data && $value['default']))
+			{
+				$result .= ' selected';
+			}
+			$result.= ' >';
+
+			$result .= $value['title'];
+
+			$result.= '</option>';
+		}
+
+		return $result;
+	}
+}
+?>
