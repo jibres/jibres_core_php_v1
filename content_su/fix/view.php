881c25f167e5381cf2467532764829a7beb74126
Reza <rm.biqarar@gmail.com>
2020-12-25 13:58:27 +0330

    Fix crop old file

--- content_su/fix/view.php
+++ content_su/fix/view.php
@@ -11,10 +11,131 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
 
+		self::file_croper();
 
 		// self::setting_news_fix();
 
 
+	}
+
+
+	private static function file_croper()
+	{
+		$store_folders = glob(YARD. 'talambar_cloud/*');
+		if(!$store_folders)
+		{
+			die('No folders');
+			return;
+		}
+
+		$result =
+		[
+			'count'      => 0,
+			'counImage'  => 0,
+			'CropedFile' => 0,
+			'isCroped'   => 0,
+			'NeedToCrop' => 0,
+			'CountForEeach' => 0,
+
+		];
+
+		foreach ($store_folders as $store_folder)
+		{
+			$result['CountForEeach']++;
+
+			$folders = glob($store_folder. '/*');
+
+			if(!$folders)
+			{
+				continue;
+			}
+
+			foreach ($folders as $date_folder)
+			{
+				$result['CountForEeach']++;
+
+				$date = glob($date_folder. '/*');
+
+				if(!$date)
+				{
+					continue;
+				}
+
+				foreach ($date as $file)
+				{
+					$result['CountForEeach']++;
+
+					$result['count']++;
+
+					$path_info = pathinfo($file);
+
+					if(isset($path_info['extension']) &&  in_array($path_info['extension'], ['jpg','jpeg','png','gif']))
+					{
+						$result['counImage']++;
+
+						if(self::the_croped_file($file))
+						{
+							$result['CropedFile']++;
+							continue;
+						}
+
+
+						if(self::is_croped($file, $path_info['extension']))
+						{
+							$result['isCroped']++;
+							continue;
+						}
+
+						\dash\upload\crop::pic($file, $path_info['extension']);
+
+						$result['NeedToCrop']++;
+
+
+						// var_dump($file);
+					}
+					// var_dump($path_info);
+					// var_dump($file);
+				}
+			}
+
+
+		}
+
+		var_dump($result);
+
+		exit();
+	}
+
+	private static function is_croped($_path, $_ext)
+	{
+		$a = ['-w120.', '-w220.', '-w300.', '-w460.', '-w780.', '-w1100.',];
+
+		$new_path = str_replace('.'. $_ext, '', $_path);
+
+		foreach ($a as $key => $value)
+		{
+			$new_file_check = $new_path. $value. $_ext;
+			if(is_file($new_file_check))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static function the_croped_file($_path)
+	{
+		$a = ['-w120.', '-w220.', '-w300.', '-w460.', '-w780.', '-w1100.',];
+		foreach ($a as $key => $value)
+		{
+			if(strpos($_path, $value) !== false)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 
