90311c71d501c2f28dffe3062427fc6b5a2669a4
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-12-26 12:07:31 +0100

    add expire header for manifest

--- dash/engine/pwa.php
+++ dash/engine/pwa.php
@@ -63,7 +63,8 @@ class pwa
 			$manifest['icons'] = self::logo_jibres();
 		}
 		$expire = (60 * 60 * 24);
-		@header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $expire)); // 24 hour
+		@header("Cache-Control: max-age=" + $expire);
+		@header("Expires: ".gmdate("D, d M Y H:i:s",time() + $expire)." GMT");
 
 		// show result of manifest
 		\dash\code::jsonBoom($manifest, false, 'manifest');
