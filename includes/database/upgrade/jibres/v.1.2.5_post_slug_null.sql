ae3932e7357f464589370ae0376e12c522bf1b21
Reza <rm.biqarar@gmail.com>
2020-12-27 10:30:47 +0330

    Slug of post can be null

---
+++ includes/database/upgrade/jibres/v.1.2.5_post_slug_null.sql
@@ -0,0 +1,1 @@
+ALTER TABLE jibres.posts CHANGE `slug` `slug` VARCHAR(100) CHARACTER SET utf8mb4 NULL;
---
+++ includes/database/upgrade/store/v.2.4.7_post_url_null.sql
@@ -0,0 +1,3 @@
+ALTER TABLE jibres_XXXXXXX.posts CHANGE `slug` `slug` VARCHAR(100) CHARACTER SET utf8mb4 NULL;
+
+
