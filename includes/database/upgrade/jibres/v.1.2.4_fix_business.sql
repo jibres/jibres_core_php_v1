37be5318a9ed8b9ac8b0d1c4ee1286656908ab13
Reza <rm.biqarar@gmail.com>
2020-12-27 10:30:35 +0330

    Remove old upgrade

--- includes/database/upgrade/jibres/v.1.2.4_fix_business.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO business_1000144.setting SET `cat` = 'store_setting', `key` = 'owner', `value` = 2166;
-INSERT INTO business_1000144.setting SET `cat` = 'store_setting', `key` = 'subdomain', `value` = 'jamhekalaa';
-INSERT INTO business_1000144.setting SET `cat` = 'store_setting', `key` = 'plan', `value` = 'free';
-INSERT INTO business_1000144.setting SET `cat` = 'store_setting', `key` = 'startplan', `value` = '2020-12-22 22:58:48';
-INSERT INTO business_1000144.setting SET `cat` = 'store_setting', `key` = 'expireplan', `value` = NULL;
-
