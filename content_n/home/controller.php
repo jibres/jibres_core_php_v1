e18053f58afb2d8a509697877947ff2b41de893d
Reza <rm.biqarar@gmail.com>
2020-12-27 11:52:34 +0330

    Try to route posts

--- content_n/home/controller.php
+++ content_n/home/controller.php
@@ -12,41 +12,42 @@ class controller
 			\dash\redirect::to(\dash\url::kingdom());
 		}
 
-		$module = \dash\coding::decode($module);
 
-		if(!$module)
-		{
-			\dash\redirect::to(\dash\url::kingdom());
-		}
 
 		$load_post = \dash\app\posts\get::get(\dash\url::module(), ['check_login' => false]);
 
+		// if(!isset($load_post['type']) || !isset($load_post['status']) || !isset($load_post['url']) || !isset($load_post['language']))
+		// {
+		// 	\dash\redirect::to(\dash\url::kingdom());
+		// }
 
-		if(!isset($load_post['type']) || !isset($load_post['status']) || !isset($load_post['url']) || !isset($load_post['language']))
-		{
-			\dash\redirect::to(\dash\url::kingdom());
-		}
+		// if(!in_array($load_post['type'], ['post', 'page', 'help']))
+		// {
+		// 	\dash\redirect::to(\dash\url::kingdom());
+		// }
 
-		if(!in_array($load_post['type'], ['post', 'page', 'help']))
-		{
-			\dash\redirect::to(\dash\url::kingdom());
-		}
+		// var_dump($load_post);exit();
 
-		if(!in_array($load_post['status'], ['publish']))
-		{
-			\dash\redirect::to(\dash\url::kingdom());
-		}
 
-		if(isset($load_post['link']))
-		{
-			\dash\log::set('newsCodeRedirect', ['code' => \dash\url::module(), 'link' => $load_post['link']]);
+		// if(!in_array($load_post['status'], ['publish']))
+		// {
+		// 	\dash\redirect::to(\dash\url::kingdom());
+		// }
 
-			\dash\redirect::to($load_post['link']);
+		// if(isset($load_post['link']))
+		// {
+		// 	\dash\log::set('newsCodeRedirect', ['code' => \dash\url::module(), 'link' => $load_post['link']]);
 
-			return;
-		}
+		// 	\dash\redirect::to($load_post['link']);
 
-		\dash\redirect::to(\dash\url::kingdom());
+		// 	return;
+		// }
+
+		// \dash\redirect::to(\dash\url::kingdom());
+
+		\dash\data::dataRow($load_post);
+
+		\dash\open::get();
 	}
 }
 ?>
