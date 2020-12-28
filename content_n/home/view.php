db173f967779d0f0dd2029356876452cc4e30cb3
Reza <rm.biqarar@gmail.com>
2020-12-28 13:18:29 +0330

    Try to  set url canonical

--- content_n/home/view.php
+++ content_n/home/view.php
@@ -5,7 +5,10 @@ class view
 {
 	public static function config()
 	{
-
+		if(\dash\data::dataRow_link() && \dash\data::dataRow_link() != \dash\url::this())
+		{
+			// set url canonical \dash\data::dataRow_link();
+		}
 	}
 }
 ?>
