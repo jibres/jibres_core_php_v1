98fecb2f6f33c879e427a6f85410ad68a67d6ccd
Reza <rm.biqarar@gmail.com>
2020-12-30 09:40:23 +0330

    Fix label

--- content_cms/files/add/display.php
+++ content_cms/files/add/display.php
@@ -4,8 +4,9 @@
 			<div data-uploader data-name='file' data-ratio=1  data-ratio-free data-final='#finalImage' data-file-max-size='<?php echo \dash\data::maxFileSize() ?>' data-autoSend>
 				<input type="file" accept="*/*" id="image1">
 				<label for="image1">
-					<small><?php echo T_("Maximum file size"); ?> <b><?php echo \dash\data::maxFileSizeTitle(); ?></b></small>
-					<?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
+					<?php echo T_('Drag &amp; Drop your files or Browse'); ?>
+					<small class="block"><?php echo T_("Maximum file size"); ?> <b><?php echo \dash\data::maxFileSizeTitle(); ?></b></small>
+				</label>
 				<label for="image1"><img id="finalImage"></label>
 			</div>
 		</section>
