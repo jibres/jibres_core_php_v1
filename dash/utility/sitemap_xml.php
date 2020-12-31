1355ebcd4932b37a9883e256369cdbac44effb61
Reza <rm.biqarar@gmail.com>
2020-12-31 16:26:26 +0330

    Add library to create sitemap xml

---
+++ dash/utility/sitemap_xml.php
@@ -0,0 +1,153 @@
+<?php
+namespace dash\utility;
+
+/** generating Sitemap files **/
+class sitemap_xml
+{
+	/**
+	 * Sitemap
+	 *
+	 * This class used for generating Google Sitemap files
+	 *
+	 * @package    Sitemap
+	 * @author     Reze Mohiti <rm.biqarar@gmail.com>
+	 * @copyright  2020
+	 * @license    http://opensource.org/licenses/MIT MIT License
+	 */
+
+
+	/**
+	 *
+	 * @var XMLWriter
+	 */
+	private $writer;
+
+	/**
+	 * @var sitemap addr
+	 */
+	private $addr;
+
+	const EXT                = '.xml';
+	const SCHEMA             = 'http://www.sitemaps.org/schemas/sitemap/0.9';
+
+
+	/**
+	 *
+	 * @param string $addr
+	 */
+	public function __construct($_addr)
+	{
+		$this->setAddr($_addr);
+		$this->startSitemap();
+	}
+
+
+	/**
+	 * Sets the address.
+	 *
+	 * @param      <type>  $addr   The address
+	 */
+	public function setAddr($_addr)
+	{
+		$this->addr = $_addr;
+		return $this;
+	}
+
+
+	/**
+	 * Gets the address.
+	 *
+	 * @return     <type>  The address.
+	 */
+	private function getAddr()
+	{
+		return $this->addr;
+	}
+
+
+	/**
+	 * Returns XMLWriter object instance
+	 *
+	 * @return XMLWriter
+	 */
+	private function getWriter()
+	{
+		return $this->writer;
+	}
+
+
+	/**
+	 * Assigns XMLWriter object instance
+	 *
+	 * @param XMLWriter $writer
+	 */
+	private function setWriter(\XMLWriter $_writer)
+	{
+		$this->writer = $_writer;
+	}
+
+
+	/**
+	 * Prepares sitemap XML document
+	 *
+	 */
+	private function startSitemap()
+	{
+		$this->setWriter(new \XMLWriter());
+		$this->getWriter()->openURI($this->getAddr() . self::EXT);
+		$this->getWriter()->startDocument('1.0', 'UTF-8');
+		$this->getWriter()->setIndent(true);
+		$this->getWriter()->startElement('urlset');
+		$this->getWriter()->writeAttribute('xmlns', self::SCHEMA);
+	}
+
+
+	/**
+	 * Adds an item.
+	 *
+	 * @param      <type>  $_loc           The location
+	 * @param      <type>  $_lastmodified  The lastmodified
+	 * @param      <type>  $_priority      The priority
+	 * @param      <type>  $_changefreq    The changefreq
+	 *
+	 * @return     self    ( description_of_the_return_value )
+	 */
+	public function addItem($_loc, $_lastmodified = NULL, $_priority = null, $_changefreq = NULL)
+	{
+		$this->getWriter()->startElement('url');
+		$this->getWriter()->writeElement('loc', $_loc);
+
+		if($_priority)
+		{
+			$this->getWriter()->writeElement('priority', $_priority);
+		}
+
+		if($_changefreq)
+		{
+			$this->getWriter()->writeElement('changefreq', $_changefreq);
+		}
+
+		if($_lastmodified)
+		{
+			$this->getWriter()->writeElement('lastmod', $_lastmodified);
+		}
+
+		$this->getWriter()->endElement();
+
+		return $this;
+	}
+
+
+	/**
+	 * Finalizes tags of sitemap XML document.
+	 *
+	 */
+	public function endSitemap()
+	{
+		$this->getWriter()->endElement();
+		$this->getWriter()->endDocument();
+		$this->getWriter()->flush();
+	}
+
+}
+?>
