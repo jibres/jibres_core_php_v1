61993c19241ecce6b52bed30835750993ba024cf
Reza <rm.biqarar@gmail.com>
2021-01-03 08:42:10 +0330

    Some fix on load template

--- dash/engine/template.php
+++ dash/engine/template.php
@@ -3,12 +3,10 @@ namespace dash\engine;
 
 class template
 {
-	public static $display_name    = null;
+
 	public static $display_addr    = null;
 	public static $finded_template = null;
 	public static $dataRow         = null;
-	public static $file_ext        = '.php';
-	public static $display_prefix  = true;
 
 
 	public static function find()
@@ -35,22 +33,18 @@ class template
 
 		$data  = null;
 		$type  = null;
-
-		// load simillary about or about-fa .html
-		if(self::fake_static_page())
-		{
-			self::$finded_template = true;
-			return true;
-		}
+		$display_addr = null;
 
 
 		if($data = self::find_tag(true))
 		{
-			$type = 'tag';
+			$type         = 'tag';
+			$display_addr = core . 'layout/tools/display-tag-view.php';
 		}
 		elseif($data = \dash\app\posts\find::post())
 		{
-			$type = 'posts';
+			$type         = 'posts';
+			$display_addr = core . 'layout/tools/display-post-view.php';
 		}
 		elseif ($data = self::find_tag())
 		{
@@ -61,63 +55,23 @@ class template
 				\dash\redirect::to($new_url);
 			}
 			return;
-
-		}
-		elseif(self::find_404())
-		{
-			// show customize 404 page
-			return true;
 		}
 
 
 		if($type)
 		{
-			self::set_display_name($data, $type);
+			self::$dataRow         = $data;
+			self::$finded_template = true;
+			self::$display_addr    = $display_addr;
 			return true;
 		}
 		else
 		{
 			return false;
 		}
-
 	}
 
 
-	public static function set_display_name($data, $type)
-	{
-		$finded_template = false;
-		$contentAddr = \dash\engine\content::get_addr();
-		$display_addr = null;
-
-		if( is_file($contentAddr. 'template/'.$type.self::$file_ext) )
-		{
-			$display_addr       = $contentAddr. 'template/'.$type.self::$file_ext;
-			self::$display_name = $type.self::$file_ext;
-		}
-		// elseif default template exist show it else use homepage!
-		elseif( is_file($contentAddr. 'template/default'. self::$file_ext) )
-		{
-			$display_addr       = $contentAddr. 'template/default'. self::$file_ext;
-			self::$display_name = 'default'. self::$file_ext;
-		}
-
-
-		// if find template for this url
-		// then if template for current lang is exist, set it
-		if(self::$display_name)
-		{
-			self::checkLangTemplate();
-			$finded_template = true;
-		}
-
-
-		if($finded_template)
-		{
-			self::$dataRow         = $data;
-			self::$finded_template = $finded_template;
-			self::$display_addr    = $display_addr;
-		}
-	}
 
 
 	public static function support_link()
@@ -186,100 +140,11 @@ class template
 	}
 
 
-	public static function fake_static_page()
-	{
-		$mymodule    = \dash\url::module();
-		$contentAddr = \dash\engine\content::get_addr();
-
-		// if user entered url contain one of our site language
-		$current_path = \dash\url::dir();
-		if(is_array($current_path))
-		{
-			$current_path = implode('/', $current_path);
-		}
-
-		// if custom template exist show this template
-		if( is_file($contentAddr. 'template/static_'. $current_path. self::$file_ext) )
-		{
-			self::$display_name = 'static_'. $current_path. self::$file_ext;
-
-		}
-		elseif( is_file($contentAddr. 'template/static/'. $current_path. self::$file_ext) )
-		{
-			self::$display_name = 'static\\'. $current_path. self::$file_ext;
-
-		}
-		else
-		{
-			// create special url for handle special type of syntax
-			// for example see below example
-			// ermile.com/legal			 	-> content/template/legal/home.html
-			// ermile.com/legal/privacy		-> content/template/legal/privacy.html
-			$my_special_url = substr($current_path, strlen($mymodule)+1);
-			if(!$my_special_url)
-			{
-				$my_special_url = 'home';
-			}
-			$my_special_url = $mymodule. '/'. $my_special_url;
-			if(is_file($contentAddr. 'template/static/'. $my_special_url. self::$file_ext))
-			{
-				self::$display_name = 'static\\'. $my_special_url. self::$file_ext;
-
-			}
-		}
-
-		if(self::$display_name)
-		{
-			self::checkLangTemplate();
-
-			return true;
-		}
-
-		return false;
-
-	}
-
-
-
-	private static function checkLangTemplate()
-	{
-		if(self::$display_prefix === true)
-		{
-			self::$display_name = \dash\engine\content::get(). '\template\\'. self::$display_name;
-		}
-		else if(self::$display_prefix === null)
-		{
-			self::$display_name = \dash\engine\content::get(). '\\'. self::$display_name;
-		}
-
-		$current_lang = \dash\language::current();
-
-		$current_lang_template = substr(self::$display_name, 0, -(strlen(self::$file_ext)));
-		$current_lang_template .= '-'.$current_lang . self::$file_ext;
-
-		$current_lang_template = str_replace("\\", DIRECTORY_SEPARATOR, $current_lang_template);
-		$current_lang_template = str_replace("/", DIRECTORY_SEPARATOR, $current_lang_template);
-
-		if(is_file(root.$current_lang_template))
-		{
-			self::$display_name	= $current_lang_template;
-		}
-	}
-
-
-	private static function get_my_url()
-	{
-		$myUrl = \dash\url::directory();
-		$myUrl = \dash\url::urlfilterer($myUrl);
-		return $myUrl;
-	}
-
-
-
 
 	public static function find_tag($_by_tag_url = false)
 	{
-		$myUrl = self::get_my_url();
+		$myUrl = \dash\url::directory();
+		$myUrl = \dash\url::urlfilterer($myUrl);
 
 		if(substr($myUrl, 0, 4) === 'tag/')
 		{
@@ -310,40 +175,6 @@ class template
 		if($cat_data)
 		{
 			return $cat_data;
-		}
-
-		return false;
-	}
-
-
-
-	public static function find_404()
-	{
-		if( is_file(root.'content/template/404.html') )
-		{
-			// header("HTTP/1.1 404 NOT FOUND");
-			self::$display_name	= 'content\\template\\404.html';
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	/**
-	* some url is ignored
-	*/
-	public static function ignore_url($_url)
-	{
-		if(substr($_url, 0, 7) === 'static/')
-		{
-			return true;
-		}
-
-		if(substr($_url, 0, 6) === 'files/')
-		{
-			return true;
 		}
 
 		return false;
