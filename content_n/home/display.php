0eaa0e8da71f3ee7619c909091ae40a910941a77
Reza <rm.biqarar@gmail.com>
2021-01-03 08:46:16 +0330

    Load display post view in content n

--- content_n/home/display.php
+++ content_n/home/display.php
@@ -1,14 +1,1 @@
-<div class="avand">
-  <div class="blogEx box">
-  <?php
-    if(\dash\data::dataRow_type() === 'post' || \dash\data::dataRow_type() === 'page')
-    {
-      require_once(core. '/layout/tools/display-post-view.php');
-    }
-    else
-    {
-      require_once(core. '/layout/tools/display-tag-view.php');
-    }
-  ?>
-  </div>
-</div>
+<?php require_once(core. '/layout/tools/display-post-view.php');  ?>
