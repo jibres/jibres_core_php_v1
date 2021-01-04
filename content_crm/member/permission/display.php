af0172bbcc7a522870cff1fdf5d7b9af7e363387
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-04 11:48:23 +0100

    fix on permission page

--- content_crm/member/permission/display.php
+++ content_crm/member/permission/display.php
@@ -34,9 +34,9 @@
         <?php if(\dash\data::UserApiKey()) {?>
           <div class="txtB mT10"><?php echo T_("API key") ?></div>
           <div class="msg ltr light">
-            <div title="<?php echo T_("Copy") ?>" class="link" data-copy='<?php echo \dash\data::UserApiKey_auth(); ?>'>
+            <div title="<?php echo T_("Copy") ?>" class="link" data-copy='<?php echo \dash\data::UserApiKey_auth(); ?>' data-copy-msg='<?php echo T_("Copied. Paste into your code!") ?>'>
               <span class="btn"><?php echo T_("Copy") ?></span>
-              <code><?php echo str_repeat('*', 16).  substr(\dash\data::UserApiKey_auth(), 16); ?></code>
+              <code><?php echo substr(\dash\data::UserApiKey_auth(), 0, 8). str_repeat('*', 16). substr(\dash\data::UserApiKey_auth(), 24); ?></code>
             </div>
           </div>
         <?php }// endif ?>
