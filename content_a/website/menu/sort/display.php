543cf7cc4f509708b9e40eb58dad6da1417e1d44
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-07 16:45:54 +0330

    add sortable2

--- content_a/website/menu/sort/display.php
+++ content_a/website/menu/sort/display.php
@@ -2,26 +2,23 @@
 
 <div class="avand-md">
   <div class="msg fs14"><?php echo T_("Select any of the menu items you want and move them to sort") ?></div>
-<form method="post">
+<form method="post" data-patch>
 
     <nav class="items">
-     <ul class="sortable" data-sortable>
+     <ol data-sortable2>
 
     <?php foreach (\dash\data::menuDetailList() as $key => $value) {?>
-       <li data-handle>
+       <li>
         <a class="f item">
-          <i class="sf-sort" >
+          <i class="sf-thumbnails" data-handle>
             <input type="hidden" name="sort[]" value="<?php echo $key; ?>">
           </i>
-          <div class="key"><?php echo a($value, 'title');?>
-            <?php if(a($value, 'target')) {?><i class="sf-external-link fc-mute"></i> <?php }// endif ?>
-          </div>
-
+          <div class="key"><?php echo a($value, 'title');?><?php if(a($value, 'target')) {?><i class="sf-external-link fc-mute"></i> <?php }// endif ?></div>
         </a>
        </li>
 
     <?php } //enfor ?>
-     </ul>
+     </ol>
    </nav>
 </form>
 </div>
