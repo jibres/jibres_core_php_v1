c27781a0d817ce2cc2b98006c8ee41f17ad9f0f1
Reza <rm.biqarar@gmail.com>
2021-01-09 12:04:45 +0330

    Got to admin from love/setting

--- content_love/store/setting/display.php
+++ content_love/store/setting/display.php
@@ -2,38 +2,48 @@
 <div class="avand-md">
   <nav class="items ltr">
     <ul>
-            <li>
-        			<a class="f item">
-        				<div class="key">ID</div>
-        				<div class="value txtB"><?php echo $dataRow['id'] ?></div>
-        			</a>
-            </li>
-            <li>
-   				   <a class="f item">
-        				<div class="key">Subdomain</div>
-        				<div class="value txtB"><?php echo $dataRow['subdomain'] ?></div>
-        			</a>
-            </li>
-            <li>
-        			<a class="f item">
-        				<div class="key">Fuel</div>
-        				<div class="value txtB"><?php echo $dataRow['fuel'] ?></div>
-        			</a>
-            </li>
-            <li>
+      <li>
+        <a class="f item">
+          <div class="key">ID</div>
+          <div class="value txtB"><?php echo $dataRow['id'] ?></div>
+        </a>
+      </li>
+      <li>
+        <a class="f item">
+          <div class="key">Subdomain</div>
+          <div class="value txtB"><?php echo $dataRow['subdomain'] ?></div>
+        </a>
+      </li>
+      <li>
+        <a class="f item">
+          <div class="key">Fuel</div>
+          <div class="value txtB"><?php echo $dataRow['fuel'] ?></div>
+        </a>
+      </li>
+      <li>
+        <a class="f item">
+          <div class="key">IP</div>
+          <div class="value txtB"><?php echo long2ip($dataRow['ip']) ?></div>
+        </a>
+      </li>
+      <li>
+        <a class="f item">
+          <div class="key">Date created</div>
+          <div class="value txtB"><?php echo \dash\fit::date_time($dataRow['datecreated']) ?></div>
+        </a>
+      </li>
+    </ul>
+  </nav>
 
-      			<a class="f item">
-      				<div class="key">IP</div>
-      				<div class="value txtB"><?php echo long2ip($dataRow['ip']) ?></div>
-      			</a>
-            </li>
-            <li>
 
-      			<a class="f item">
-      				<div class="key">Date created</div>
-      				<div class="value txtB"><?php echo \dash\fit::date_time($dataRow['datecreated']) ?></div>
-      			</a>
-            </li>
+   <nav class="items">
+    <ul>
+      <li>
+        <a class="f item" href="<?php echo \dash\url::kingdom(). '/'.\dash\store_coding::encode(a($dataRow, 'id'));?>">
+          <div class="key"><?php echo T_("Go to admin") ?></div>
+          <div class="value ltr"><?php echo \dash\store_coding::encode(a($dataRow, 'id'));  ?></div>
+        </a>
+      </li>
     </ul>
   </nav>
 
@@ -58,16 +68,16 @@
 
 
   <form method="post" autocomplete="off">
-	  <div class="box">
-	  	<div class="body">
-	  		<p><?php echo T_("You can change business subdomain") ?></p>
-	  		<div class="input ltr">
-	  			<input type="text" name="subdomain" value="<?php echo $dataRow['subdomain'] ?>" required>
-	  		</div>
-	  	</div>
-	  	<footer class="txtRa">
-	  		<button class="btn danger"><?php echo T_("Change subdomian") ?></button>
-	  	</footer>
-	  </div>
+    <div class="box">
+      <div class="body">
+        <p><?php echo T_("You can change business subdomain") ?></p>
+        <div class="input ltr">
+          <input type="text" name="subdomain" value="<?php echo $dataRow['subdomain'] ?>" required>
+        </div>
+      </div>
+      <footer class="txtRa">
+        <button class="btn danger"><?php echo T_("Change subdomian") ?></button>
+      </footer>
+    </div>
   </form>
 </div>
