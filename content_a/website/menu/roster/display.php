d5179fd6aa56799a3fc50c8a076378fa0dea0fab
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-10 17:34:59 +0330

    fix on pwa

--- content_a/website/menu/roster/display.php
+++ content_a/website/menu/roster/display.php
@@ -12,8 +12,8 @@ if(\dash\data::menuDetail_list() && is_array(\dash\data::menuDetail_list()))
   <div class="box">
     <div class="body">
       <div class="row">
-        <div class="c-xs-12 c-sm txtB"><?php echo \dash\data::menuDetail_title() ?></div>
-        <div class="c-xs-12 c-sm-auto fc-mute font-12"><?php echo \dash\fit::number(count($list)). ' '. T_("Link"); ?></div>
+        <div class="c txtB"><?php echo \dash\data::menuDetail_title() ?></div>
+        <div class="c-auto os fc-mute font-12"><?php echo \dash\fit::number(count($list)). ' '. T_("Link"); ?></div>
       </div>
     </div>
   </div>
@@ -24,7 +24,7 @@ if(\dash\data::menuDetail_list() && is_array(\dash\data::menuDetail_list()))
           <div class="f item">
             <i class="sf-thumbnails" data-handle><input type="hidden" name="sort[]" value="<?php echo $key; ?>"></i>
             <div class="key"><?php echo a($value, 'title');?><?php if(a($value, 'target')) {?><i class="sf-external-link fc-mute"></i> <?php }// endif ?></div>
-            <div class="value addChild pRa20-f"><a href="<?php echo \dash\url::that(). '/item'. \dash\request::full_get(['child' => $key]) ?>"><?php echo T_("Add Subitem"); ?></a></div>
+            <div class="value addChild pRa20-f s0"><a href="<?php echo \dash\url::that(). '/item'. \dash\request::full_get(['child' => $key]) ?>"><?php echo T_("Add Subitem"); ?></a></div>
             <div class="value"><a href="<?php echo \dash\url::that(). '/item'. \dash\request::full_get(['key' => $key]) ?>"><?php echo T_("Edit"); ?></a></div>
           </div>
           <ol data-sortable></ol>
