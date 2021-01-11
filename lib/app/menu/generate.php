903d5769ca73c2e3ad8141e7e8d130a659fbeb85
Reza <rm.biqarar@gmail.com>
2021-01-11 17:27:59 +0330

    Fix menu

--- lib/app/menu/generate.php
+++ lib/app/menu/generate.php
@@ -28,12 +28,15 @@ class generate
 	{
 		foreach ($_menu as $key => $value)
 		{
-			self::$result .= '<li>';
 
 			if(isset($value['list']) && is_array($value['list']))
 			{
-				foreach ($value['list'] as $one_item)
+				$endkey = end($value['list']);
+				$endkey = array_search($endkey, $value['list']);
+
+				foreach ($value['list'] as $check_end_key => $one_item)
 				{
+					self::$result .= '<li>';
 					self::$result .='<div class="f item">';
 					self::$result .='<i class="sf-thumbnails" data-handle><input type="hidden" name="sort[]" data-id="'. a($one_item, 'id'). '"></i>';
 					self::$result .='<div class="key">'. a($one_item, 'title');
@@ -47,17 +50,18 @@ class generate
 					self::$result .= '<div class="value addChild pRa20-f s0"><a href="'. \dash\url::that(). '/item'. \dash\request::full_get(['id' => a($one_item, 'id')]). '">'. T_("Add Subitem"). '</a></div>';
 					self::$result .= '<div class="value"><a href="'. \dash\url::that(). '/item'. \dash\request::full_get(['id' => a($one_item, 'id')]). '">'. T_("Edit"). '</a></div>';
 					self::$result .= '</div>';
+
+					if(isset($value['child']) && is_array($value['child']) && $check_end_key === $endkey)
+					{
+						self::$result .= '<ol data-sortable>';
+						self::create_admin($value['child']);
+						self::$result .='</ol>';
+					}
+
+					self::$result .= '</li>';
 				}
 			}
 
-			if(isset($value['child']) && is_array($value['child']))
-			{
-				self::$result .= '<ol data-sortable>';
-				self::create_admin($value['child']);
-				self::$result .='</ol>';
-			}
-
-			self::$result .= '</li>';
 		}
 	}
 
