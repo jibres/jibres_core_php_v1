ea179634967c3f1984099b46dc5abe7c3f018b84
Reza <rm.biqarar@gmail.com>
2021-01-12 18:08:06 +0330

    Add files count to cms dashboard

--- dash/app/posts/dashboard.php
+++ dash/app/posts/dashboard.php
@@ -9,22 +9,24 @@ class dashboard
 
 		$total_post = floatval(\dash\db\posts::get_active_count());
 
-		$dashboard_detail                           = [];
+		$dashboard_detail                      = [];
 
-		$dashboard_detail['post']                   = $total_post;
+		$dashboard_detail['post']              = $total_post;
 
-		$dashboard_detail['standard']               = floatval(\dash\db\posts::get_active_count_subtype('standard'));
-		$dashboard_detail['gallery']                = floatval(\dash\db\posts::get_active_count_subtype('gallery'));
-		$dashboard_detail['video']                  = floatval(\dash\db\posts::get_active_count_subtype('video'));
-		$dashboard_detail['audio']                  = floatval(\dash\db\posts::get_active_count_subtype('audio'));
+		$dashboard_detail['standard']          = floatval(\dash\db\posts::get_active_count_subtype('standard'));
+		$dashboard_detail['gallery']           = floatval(\dash\db\posts::get_active_count_subtype('gallery'));
+		$dashboard_detail['video']             = floatval(\dash\db\posts::get_active_count_subtype('video'));
+		$dashboard_detail['audio']             = floatval(\dash\db\posts::get_active_count_subtype('audio'));
 
-		$dashboard_detail['comments']               = floatval(\dash\db\comments::get_count());
-		$dashboard_detail['comments_awaiting']      = floatval(\dash\db\comments::get_count(['status' => 'awaiting']));
-		$dashboard_detail['comments_approved']      = floatval(\dash\db\comments::get_count(['status' => 'approved']));
+		$dashboard_detail['comments']          = floatval(\dash\db\comments::get_count());
+		$dashboard_detail['comments_awaiting'] = floatval(\dash\db\comments::get_count(['status' => 'awaiting']));
+		$dashboard_detail['comments_approved'] = floatval(\dash\db\comments::get_count(['status' => 'approved']));
+
+		$dashboard_detail['files']             = floatval(\dash\db\files::count_all());
 		//
-		$dashboard_detail['tags']                   = floatval(\dash\db\terms\get::get_count(['type' => 'tag']));
-		$dashboard_detail['latesPost']              = \dash\app\posts\search::lates_post();
-		$dashboard_detail['latesComment']           = \dash\app\comment\search::lates_comment();
+		$dashboard_detail['tags']              = floatval(\dash\db\terms\get::get_count(['type' => 'tag']));
+		$dashboard_detail['latesPost']         = \dash\app\posts\search::lates_post();
+		$dashboard_detail['latesComment']      = \dash\app\comment\search::lates_comment();
 
 		if(!$total_post)
 		{
