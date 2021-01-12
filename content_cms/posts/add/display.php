22872585c19cce1e19e35d334093077c7a42104d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-12 18:44:18 +0330

    fix add post

--- content_cms/posts/add/display.php
+++ content_cms/posts/add/display.php
@@ -3,29 +3,28 @@
 		<div class="box">
 			<div class="body">
 				<?php if(\dash\url::module() === 'posts') {?>
-					<div class="mB20">
-						<div class="txtB mB10"><?php echo T_("Choose post type") ?></div>
+					<div class="">
 						<div class="row">
-							<div class="c-xs-12 c-sm-6 c-md-3">
-								<div class="radio3">
+							<div class="c-xs-6 c-sm-6 c-md-3">
+								<div class="radio3 mB10">
 									<input type="radio" name="subtype" value="standard" id="standard" <?php if(\dash\request::get('subtype') === 'standard' || !\dash\request::get('subtype') ) {echo 'checked';} ?>>
 									<label for="standard"><?php echo T_("Standard") ?></label>
 								</div>
 							</div>
-							<div class="c-xs-12 c-sm-6 c-md-3">
-								<div class="radio3">
+							<div class="c-xs-6 c-sm-6 c-md-3">
+								<div class="radio3 mB10">
 									<input type="radio" name="subtype" value="gallery" id="gallery" <?php if(\dash\request::get('subtype') === 'gallery') {echo 'checked';} ?>>
 									<label for="gallery"><?php echo T_("Gallery") ?></label>
 								</div>
 							</div>
-							<div class="c-xs-12 c-sm-6 c-md-3">
-								<div class="radio3">
+							<div class="c-xs-6 c-sm-6 c-md-3">
+								<div class="radio3 mB10">
 									<input type="radio" name="subtype" value="video" id="video" <?php if(\dash\request::get('subtype') === 'video') {echo 'checked';} ?>>
 									<label for="video"><?php echo T_("Video") ?></label>
 								</div>
 							</div>
-							<div class="c-xs-12 c-sm-6 c-md-3">
-								<div class="radio3">
+							<div class="c-xs-6 c-sm-6 c-md-3">
+								<div class="radio3 mB10">
 									<input type="radio" name="subtype" value="audio" id="audio" <?php if(\dash\request::get('subtype') === 'audio') {echo 'checked';} ?>>
 									<label for="audio"><?php echo T_("Podcast") ?></label>
 								</div>
@@ -38,8 +37,8 @@
 						<input type="text" name="title" id="title" placeholder='<?php echo T_("Enter title here"); ?> *'  <?php \dash\layout\autofocus::html() ?> required maxlength='200' minlength="1" pattern=".{1,200}">
 					</div>
 				</div>
-				<textarea class="txt mB10" data-editor id='descInput' name="content" placeholder='<?php echo T_("Write your post ..."); ?>' maxlength='100000' rows="15"></textarea>
-				<p class="fc-mute mB0-f"><?php echo T_("First type main text and save as draft, then complete and publish it."); ?></p>
+				<textarea class="txt mB10" data-editor id='descInput' name="content" placeholder='<?php echo T_("Write your post ..."); ?>' maxlength='100000' rows="10"></textarea>
+				<p class="fc-mute mB0-f s0"><?php echo T_("First type main text and save as draft, then complete and publish it."); ?></p>
 			</div>
 			<footer class="txtRa">
 				<button class="btn master"><?php echo T_("Save as draft"); ?></button>
