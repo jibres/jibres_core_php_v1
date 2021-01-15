835128a1888c5b0d1a4318084bd67856b1ef9792
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-15 17:36:04 +0330

    some fix

--- content_a/setting/thirdparty/googleanalytics/display.php
+++ content_a/setting/thirdparty/googleanalytics/display.php
@@ -1,10 +1,10 @@
 <?php $storeData = \dash\data::store_store_data(); ?>
 
-<div class="avand-sm zero">
+<div class="avand-sm impact zero">
   <form method="post" autocomplete="off" id='aThirdParty'>
-    <div class="box impact">
+    <div class="box">
       <div class="body">
-        <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/google-analytics-banner.png" alt='Google Analytics'>
+        <img class="block mB10" src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/google-analytics-banner.png" alt='Google Analytics'>
         <p class="msg"><?php echo T_("Google Analytics is a web analytics service offered by Google that tracks and reports website traffic, currently as a platform inside the Google Marketing Platform brand. As of 2019, Google Analytics is the most widely used web analytics service on the web. Google Analytics provides an SDK that allows gathering usage data from iOS and Android app, known as Google Analytics for Mobile Apps."); ?></p>
 
           <label for="igoogleanalytics"><?php echo T_("Your Google Analytics Tracking ID"); ?> <span class="fc-red">*</span></label>
--- content_a/setting/thirdparty/tawk/display.php
+++ content_a/setting/thirdparty/tawk/display.php
@@ -1,11 +1,12 @@
 <?php $storeData = \dash\data::store_store_data(); ?>
 
-
-<div class="avand-sm zero">
+<div class="avand-sm impact zero">
   <form method="post" autocomplete="off" id='aThirdParty'>
-    <div  class="box impact mB25-f">
-      <header><h2><?php echo T_("Set tawk live chat setting");?></h2></header>
+    <div class="box">
+          <img class="block mB10" src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/tawk-banner.jpg" alt='tawk.to'>
         <div class="body">
+          <p class="msg"><?php echo T_("Google Analytics is a web analytics service offered by Google that tracks and reports website traffic, currently as a platform inside the Google Marketing Platform brand. As of 2019, Google Analytics is the most widely used web analytics service on the web. Google Analytics provides an SDK that allows gathering usage data from iOS and Android app, known as Google Analytics for Mobile Apps."); ?></p>
+
           <p class="mB0-f"><?php echo T_("If you want to have live chat in your website, enter your tawk token code here. \nTo do this, you need to register on tawk.to and get the code from there"); ?></p>
             <label for="itawk"><?php echo T_("Tawk code"); ?> <span class="fc-red">*</span></label>
             <div class="input ltr">
