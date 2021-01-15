70bd27780b360cdf5e3b96c224e25a7511ebef9a
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-15 17:45:49 +0330

    add tawk detail

--- content_a/setting/thirdparty/tawk/display.php
+++ content_a/setting/thirdparty/tawk/display.php
@@ -3,11 +3,13 @@
 <div class="avand-sm impact zero">
   <form method="post" autocomplete="off" id='aThirdParty'>
     <div class="box">
-          <img class="block mB10" src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/tawk-banner.jpg" alt='tawk.to'>
+          <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/tawk-banner.jpg" alt='tawk.to'>
         <div class="body">
-          <p class="msg"><?php echo T_("Google Analytics is a web analytics service offered by Google that tracks and reports website traffic, currently as a platform inside the Google Marketing Platform brand. As of 2019, Google Analytics is the most widely used web analytics service on the web. Google Analytics provides an SDK that allows gathering usage data from iOS and Android app, known as Google Analytics for Mobile Apps."); ?></p>
+          <div class="msg">
+            <p><?php echo T_("tawk.to is the world's #1 most widely used live chat software. More than 35% of all websites that use live chat. tawk.to is a 100% free live chat application designed to increase the effectiveness in managing the online customer engagement experience, enabling multiple websites and agents in a single dashboard interface to chat with the visitors on your website."); ?></p>
+            <p><?php echo T_("tawk.to offers iOS, Android, Windows and Mac OSX apps to stay connected, or you can log in via any modern browser."); ?></p>
+          </div>
 
-          <p class="mB0-f"><?php echo T_("If you want to have live chat in your website, enter your tawk token code here. \nTo do this, you need to register on tawk.to and get the code from there"); ?></p>
             <label for="itawk"><?php echo T_("Tawk code"); ?> <span class="fc-red">*</span></label>
             <div class="input ltr">
             <input type="text" name="addon_tawk" id="itawk" value="<?php echo a($storeData, 'addon_tawk'); ?>" <?php \dash\layout\autofocus::html() ?> maxlength='50' minlength="1"  required>
