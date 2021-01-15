3fc3ba45bfa1d458193200897b24fa30d0b1eb87
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-15 16:04:42 +0330

    change location of certificate link

--- content_a/setting/thirdparty/display.php
+++ content_a/setting/thirdparty/display.php
@@ -1,19 +1,69 @@
 
 <div class="f justify-center">
   <div class="c8 s12 m10 x6 pA20">
-    <h1><?php echo T_("Analytics"); ?></h1>
-    <nav class="items">
+    <h2><?php echo T_("Analytics"); ?></h2>
+    <nav class="items mB25-f">
       <ul>
-        <li><a class="f" href="<?php echo \dash\url::this(); ?>/general/title"><div class="key"><?php echo T_("Store title"); ?></div><div class="go"></div></a></li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/addon/googleanalytics">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/google_analytics.svg" alt='Google Analytics'>
+            <div class="key"><?php echo T_("Google Analytics"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
       </ul>
     </nav>
 
-    <h1><?php echo T_("Live Chat"); ?></h1>
-    <nav class="items">
+
+    <h2><?php echo T_("Live Chat"); ?></h2>
+    <nav class="items mB25-f">
       <ul>
-        <li><a class="f" href="<?php echo \dash\url::this(); ?>/general/lang"><div class="key"><?php echo T_("Business language"); ?></div><div class="go"></div></a></li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/addon/tawk">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/tawk.png" alt='tawk.to'>
+            <div class="key"><?php echo T_("tawk.to - 100% FREE live chat software for your website!"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/addon/imber">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/imber.png" alt='Imber'>
+            <div class="key"><?php echo T_("Imber - All in One Marketing Automation Platform"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/addon/raychat">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/thirdparty/raychat.jpg" alt='RayChat'>
+            <div class="key"><?php echo T_("RayChat - Online Chat Platform"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
       </ul>
     </nav>
+
+
+    <h2><?php echo T_("Certificates"); ?></h2>
+    <nav class="items mB25-f">
+      <ul>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/enamad">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/business/cert/enamad.jpg" alt='Enamad'>
+            <div class="key"><?php echo T_("Enamad"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+        <li>
+          <a class="f" href="<?php echo \dash\url::here(); ?>/setting/samandehi">
+            <img src="<?php echo \dash\url::cdn(); ?>/img/business/cert/samandehi.jpg" alt='Samandehi'>
+            <div class="key"><?php echo T_("Samandehi"); ?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+
 
   </div>
 </div>
