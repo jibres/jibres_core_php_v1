cc89200dbbd7832edc3b8fa8d62bef351cbaf5a7
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-16 04:02:34 +0330

    answer 2 question

--- content/investment/display.php
+++ content/investment/display.php
@@ -116,122 +116,180 @@
 
  <article>
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Our company and the problem we're solving"); ?></h3>
-   <p></p>
-   <p class="mB0-f">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Our company and the problem we're solving"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
+   <p>قبل از شروع برای اینکه با ما بیشتر آشنا بشید پیشنهاد میکنم داستان جیبرس رو بخونید. ما یک شرکت دانش‌بنیان متفاوت نسبت به ۹۹.۹ درصد استارتاپ‌های دیگه هستیم.</p>
+   <p>اما مشکل کجاست و ما چه می‌کنیم؟ چهارچوب صحبت ما درباره پول هست. فروش. در عصر ارتباطات ما هنوز مشکل در ارتباط داریم. یه فروشنده رو درنظر بگیرید. میخواد محصول خودش رو بفروشه. شاید یه کسب و کار محلی یا حتی خانگی. فروش حضوری داره. میخواد یه سایت داشته باشه. میخواد تو این دوره زمونه که همه گوشی دارن، یه اپلیکیشن خوب داشته باشه و توی شبکه‌های اجتماعی که خودش به چندین و چند مدل متفاوت تقسیم میشه هم فعال باشه. ما این‌هارو کانال فروش می‌نامیم.</p>
+   <p>معمولا بیزینس‌هایی که میبینیم توی یکی از این کانال‌های فروش مشغول هستند و از بین هزاران هزار کسب و کار، تعداد کمی از اون‌ها توی دو یا چند کانال فروش واقعا فعال هستند. مشکل دقیقا همین‌جاست. مگه از پول بدشون میاد؟ مسلما نه! ولی چون وقت و انرژی و هزینه بسیار زیادی لازمه تا یه کسب و کار بتونه توی همه این کانال‌ها فعال باشه، در عمل نمیتونن.</p>
+   <p>ده‌ها نمونه سراغ داریم که تلاش کردند یک کانال فروش جدید راه بندازن و حسابی هم زحمت کشیدند ولی بعد از مدتی اون کانال مثلا وب‌سایت غیرفعال شده. البته که تورم فعلی و افزایش قیمت‌های شدید هم تاثیر مضاعفی در این شرایط داره. چون یه کارمند باید استخدام بشه تا هر روز قیمت محصولات رو بالا و بالاتر ببره! همین میشه که توی سایت‌های بزرگ هم میزنن برای دریافت آخرین قیمت، تماس بگیرید.</p>
+   <p class="msg minimal info2 txtB txtC font-18a">جزیره‌های متروک؛ آدم‌های خسته؛ پول‌های منتظر</p>
+   <p class="mB0-f">این تازه اول داستانه. از یک کاسب که میشناسید بپرسید ماه گذشته چقدر سود کردی؟ نسبت به ماه قبل چطور بود؟ نسبت به سال پیش چی؟ میانگین چند درصد سود داری؟ بهترین مشتری‌هات کیا هستن؟ پر سودترین کالاهات کدومه؟ پرفروش‌ترین محصولاتت چی؟ و ده‌ها سوال دیگه که دونستن جواب اون‌ها میتونه تصمیم‌گیری آینده یک کسب و کار رو متحول کنه. جواب این سوالات از پیش معلوم هستند. هرچقدر هم که یه کسب و کار با برنامه و دقیق باشه بازم پاسخ به این سوالات سخت هست و احتمالا نمیتونن بهتون جواب درستی بدن.</p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Is our solution uniquely addressing this problem?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Is our solution uniquely addressing this problem?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
+   <p>جیبرس ساخته شد تا با یکپارچه‌سازی سیستم فروش و حل چالش‌های الکترونیکی کسب و کارها بهشون قدرتی باورنکردنی بده تا بتونن پرواز کنن. ما برای تمام مشکلاتی که گفته شد و حتی گفته نشده راه‌حل داریم؛)</p>
+   <p>برای هرکدوم از این کانال‌های فروش ما راه‌حل ارائه میدیم که همه به هم متصل و پکپارچه هستن تا فقط یکجا قیمت وارد بشه. یکجا فروش ثبت بشه. یکجا گزارش گرفته بشه و کسب و کار بجای فکر کردن به زیرساخت به فکر توسعه و رشد خودش باشه.</p>
+   <p>ما برای فروش حضوری در محل کسب و کار، امکانات کامل نرم‌افزاهای سنتی رو ارائه میدیم. یک سیستم فروشگاهی کامل که بدون محدودیت به پرینتر وصل میشه و میتونه سریع‌تر از هر سیستمی که تاحالا دیدید فاکتور صادر کنه. فیش‌پرینت چاپ کنه. به بارکدخوان وصل بشه. بتونه وزن رو از ترازو دریافت کنه و حتی به پی‌سی‌پوز هم وصل بشه تا بدون نیاز به ورود مبلغ، مشتری فروشگاه فقط کارتش رو بکشه. تازه به هرچند تا سیستم هم بخواین وصل میشه و لازم نیست کار خاصی کنید یا پول خاصی به‌ازای هر سیستم پرداخت بشه!</p>
+   <p>کانال بعدی در فروش، فروشگاه‌ساز و سایت‌ساز جیبرس هست. یک بیزینس میتونه خیلی خیلی سریع یه دامنه برای فروشگاهش بگیره چون ما خودمون نماینده ثبت بیش از ۴۰۰ تا دامنه داخلی و خارجی هستیم و این دامنه‌هارو در سریع‌ترین زمان ممکن و با کمترین کارمزد ممکن ثبت می‌کنیم. روی دامنه گواهی اس‌اس‌ال رایگان ارائه میدیم تا همه ارتباط امن داشته باشیم و برای گرفتن مجوز اینماد و ساماندهی هم کمک‌های لازم رو انجام میدیم. به‌سادگی میشه سرویس‌های ثانویه مثل آمارگیر گوگل و سرویس‌های چت زنده رو به سایت وصل کرد. سبد خرید و فرایند ثبت سفارش هم خودکار پیاده‌سازی شده و اتصال به درگاه اینترنتی هم به‌راحتی انجام میشه. فقط کافیه یکبار سایت مطابق سلیقه و نیاز چیده بشه و تمام. تازه علاوه بر فروشگاه‌ساز میشه باهاش سایت شخصی و سازمانی و هر مدلی که فکر کنید هم ساخت. سایت‌هایی که از با سئوی هوشمند می‌تونند به سرعت در موتورهای جستجو لیست بشن تا مشتریان واقعی زودتر نمایان بشن.</p>
+   <p>و اما بخش محبوب در فروش این روزها اپلیکیشن هست. ما یک کارخونه ساخت اپ راه انداختیم تا بتونیم برای هر کسب و کار در چند دقیقه اپ اختصاصی خودش رو آماده کنیم. تا اونجا که میشد سفارشی‌سازی در اپ اعمال میشه و تمام قسمت‌ها قابلیت تنظیم رو داراست تا مطابق با سلیقه و نیاز صاحب کسب و کار اپ ساخته بشه. این اپ نیتیو اندروید بوده و با سرعتی بالا تمام عملکردهای فروشگاه رو ارائه میده تا مشتریان بتونن راحت به محصولات دسترسی داشته باشن و سفارش خودشون رو ثبت کنند. اپ آی‌اواس به دلیل تحریم‌ها و محدودیت‌های اون به‌صورت PWA ارائه میشه و نسخه موبایل وب‌سایت هم روی گوشی‌ها کاملا قابلیت سفارشی‌سازی رو داره. حقیقتا تجربه خرید از نسخه موبایل یک بیزینس که با جیبرس پیاده‌شده کم‌نظیر است.</p>
+   <p>آخرین پازل در کانال‌های فروش، شبکه‌های اجتماعی هستند. جیبرس به تلگرام وصل میشه تا بتونه سریع اطلاعات کسب و کار مثل محصولات و خبرهاش رو به اشتراک بگذاره. ربات اختصاصی هر کسب و کار میتونه تمام فرآیندهای فروش رو ساپورت کنه و حتی فراتر از اون برای ارتباط با مشتری هم در دسترس قرار بگیره. با اتصال به اینستاگرام، با فشردن یک دکمه محصول توی اینستاگرام شیر میشه و قیمت و موجودی و توضیحات اون به‌محض تغییر، در پست مربوطه آپدیت میشن. دستیار هوشمند فروش میتونه به دایرکت‌های اینستاگرام پاسخ بده و کمک کنه تا مشتری سفارش خودش رو ثبت کنه و این به معنی فروش ۲۴ ساعته از طریق اینستاگرام است.</p>
+   <p>در کنار این ما فرم‌ساز پیشرفته‌ای داریم تا هر مدل فرم و داده‌ای که لازمه از مخاطب گرفته بشه رو بتونیم بگیرم. از یه فرم تماس ساده تا فرم رضایت مشتری و رفتارسنجی با گزارشات آماری و نموداری جامع. سیستم مدیریت ارتباط با مشتری یا CRM‌ به‌صورت کامل در جیبرس پیاده شده تا بی‌نیاز به سیستم دیگه باشید. سامانه تیکتینگ حرفه‌ای هم در اختیار کسب و کارها قرار میگیره و میتونن در کنار فروشگاه بلاگ هم داشته باشند.</p>
+   <p>ما یک نرم‌افزار حسابداری کامل مطابق با استاندارد بین‌المللی و برای مخاطب ایرانی با کدینگ جدید جامعه حسابرسان ایران هم ارائه میدیم که کاملا رایگان در اختیار بیزینس‌هاست تا کمکشون کنیم دقیق کار کنن و گرفتار مالیات ناحق و جریمه‌هاش نشن.</p>
+   <p class="mB0-f">و اما همه این عظمت که ماه‌ها از ما زمان گرفته تا پیاده‌سازی بشه و قطعا میتونه روز به روز بهتر و بهتر باشه رو بزارید زمین لطفا. ما عمیقا به مشتری مداری اعتقاد داریم. فراتر از هر نمونه‌ای که تاکنون دیده‌اید. شاید تمام این معماری و تکنولوژی‌هایی که اجرا کردیم به‌سختی کپی شوند ولی مطمئن باشید که طرز فکر ما غیرقابل کپی‌برداری است.</p>
+<?php } else { echo "Wait..."; }?>
+  </section>
+
+  <section class="avand-lg impact font-16">
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Who are the Jibres target market and customers?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Who are the Jibres target market and customers?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How have our customers solve their problems so far?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How have our customers solve their problems so far?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("What is our estimate of the size of the Jibers business market?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("What is our estimate of the size of the Jibers business market?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Jibres business model"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Jibres business model"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How will our company change the world?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How will our company change the world?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("What is the big picture and vision?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("What is the big picture and vision?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Why our team is uniquely qualified to solve this problem?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Why our team is uniquely qualified to solve this problem?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How long have we been working together? How did we meet?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How long have we been working together? How did we meet?"); ?></h3>
-   <p class="mB0-f"><?php echo T_("."); ?></p>
-  </section>
-
-  <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("What are our KPIs or metrics for growth?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("What are our KPIs or metrics for growth?"); ?></h3>
    <p class="mB0-f">revenue/customer/user</p>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("What are our totals and month over month growth?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("What are our totals and month over month growth?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How do we plan to acquire customers/users?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How do we plan to acquire customers/users?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Explain any capital raised to date and terms"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Explain any capital raised to date and terms"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("What is our monthly burn? How much runway do we have?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("What is our monthly burn? How much runway do we have?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Jibres competitors"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Jibres competitors"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Jibres competitive advantage over competitors"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Jibres competitive advantage over competitors"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Can our all founders work full-time?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Can our all founders work full-time?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("Our shareholders of the company"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("Our shareholders of the company"); ?></h3>
    <p class="mB0-f">title and percentage</p>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How much capital do we need to grow at this stage?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How much capital do we need to grow at this stage?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("At this stage, what percentage of the shares will we transfer to the investor?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("At this stage, what percentage of the shares will we transfer to the investor?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How much has been invested in Jibres so far?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How much has been invested in Jibres so far?"); ?></h3>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
   <section class="avand-lg impact font-16">
-   <h3 class="txtB font-22"><?php echo T_("How long have we taken the time to develop our idea?"); ?></h3>
+   <h3 class="txtB font-22 fc-pink"><?php echo T_("How long have we taken the time to develop our idea?"); ?></h3>
    <p>spend time in future</p>
+<?php if(\dash\language::current() === 'fa') {?>
    <p class="mB0-f"><?php echo T_("."); ?></p>
+<?php } else { echo "Wait..."; }?>
   </section>
 
  </article>
