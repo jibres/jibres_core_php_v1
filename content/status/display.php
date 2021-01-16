ea547fd5459e4d58a4dd18b57262c9ad16ca69fd
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-01-16 10:38:52 +0330

    fix tld of status

--- content/status/display.php
+++ content/status/display.php
@@ -1,7 +1,7 @@
 <div class="jibresBanner">
 
  <div class="avand impact zero">
-  <iframe class="block" src="https://status.jibres.<?php if(\dash\url::tld() === 'fa') {echo 'ir';} else {echo 'com';} ?>" height="1800"></iframe>
+  <iframe class="block" src="https://status.jibres.<?php if(\dash\url::tld() === 'ir') {echo 'ir';} else {echo 'com';} ?>" height="1800"></iframe>
  </div>
 
 </div>
