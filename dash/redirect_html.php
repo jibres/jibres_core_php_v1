f0f5eae20fd5c6434cff3a6172d0fe65a069105a
Reza <rm.biqarar@gmail.com>
2021-01-16 11:53:55 +0330

    Need redirect to full url not strtok ?. For XSS must be check another place

--- dash/redirect_html.php
+++ dash/redirect_html.php
@@ -8,7 +8,7 @@
  <meta name ="viewport" content="width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0 maximum-scale=1.5, minimal-ui"/>
  <link href="<?php echo(\dash\url::cdn()); ?>/images/favicon-error.png" rel="shortcut icon"/>
  <link href="<?php echo \dash\layout\func::staticmtime('css/jibres.min.css');?>" rel="stylesheet"/>
- <meta http-equiv="refresh" content="2; URL=<?php echo(strtok($_loc, '?'));?>">
+ <meta http-equiv="refresh" content="2; URL=<?php echo($_loc);?>">
 </head>
 <body class='redirecting' data-model='<?php echo $_model; ?>'>
 
@@ -19,7 +19,7 @@
   </div>
   <div class="detail">
     <h1<?php if($_txt) {echo ' title="'. $_txt. '"';} ?>><?php echo T_('REDIRECTING ...') ?></h1>
-    <?php echo '  <h2><a href='. strtok($_loc, '?'). '>'. strtok($_loc, '?') .'</a></h2>';
+    <?php echo '  <h2><a href='. $_loc. '>'. strtok($_loc, '?') .'</a></h2>';
     if($_txt)
     {
       echo '  <br><p>'. $_txt .'</p>';
@@ -27,7 +27,6 @@
     ?>
 
   </div>
-
  <script src="<?php echo \dash\layout\func::staticmtime('js/jibres.min.js');?>"></script>
 </body>
 </html>
