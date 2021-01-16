d405ebd459b1b7653676bd0033c58536e26c6aea
Reza <rm.biqarar@gmail.com>
2021-01-16 11:58:28 +0330

    Fix back link of site map

--- content_cms/sitemap/view.php
+++ content_cms/sitemap/view.php
@@ -7,8 +7,8 @@ class view
 	{
 		\dash\face::title(T_('Sitemap'));
 
-		\dash\data::back_text(T_('Setting'));
-		\dash\data::back_link(\dash\url::here(). '/setting');
+		\dash\data::back_text(T_('CMS'));
+		\dash\data::back_link(\dash\url::here());
 
 	}
 }
