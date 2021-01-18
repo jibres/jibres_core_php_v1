b77707105c07fa30d68ad0bef992a32d4eb9bf19
Reza <rm.biqarar@gmail.com>
2021-01-18 15:49:11 +0330

    Show some detail

--- content_crm/telegram/view/display.php
+++ content_crm/telegram/view/display.php
@@ -1,4 +1,4 @@
-<?php $data = \dash\data::dataRow(); ?>
+<?php $data = \dash\data::dataRow();?>
 <nav class="items">
   <ul>
       <li>
@@ -9,10 +9,27 @@
       </a>
      </li>
 
-    <li>
+       <li>
+      <a class="f item">
+        <div class="key"><?php echo T_("Send Method") ?></div>
+        <div class="value txtB"><?php echo \dash\data::dataRow_sendmethod(); ?></div>
+        <div class="go detail"></div>
+      </a>
+     </li>
+
+
+       <li>
       <a class="f item">
         <div class="key"><?php echo T_("Date send") ?></div>
         <div class="value txtB"><?php echo \dash\fit::date_time(\dash\data::dataRow_senddate()); ?></div>
+        <div class="go detail"></div>
+      </a>
+     </li>
+
+    <li>
+      <a class="f item">
+        <div class="key"><?php echo T_("Date response") ?></div>
+        <div class="value txtB"><?php echo \dash\fit::date_time(\dash\data::dataRow_responsedate()); ?></div>
         <div class="go detail"></div>
       </a>
      </li>
