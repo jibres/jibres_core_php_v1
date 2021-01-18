2d25f477be7eb50e67ed35f85836f39df0dfb5ff
Reza <rm.biqarar@gmail.com>
2021-01-18 17:30:31 +0330

    Add logo

--- content_n/view.php
+++ content_n/view.php
@@ -6,7 +6,16 @@ class view
 {
 	public static function config()
 	{
-
+		if(\dash\engine\store::inStore())
+		{
+			$store_logo = \lib\store::logo();
+			if($store_logo && !\dash\face::cover())
+			{
+				\dash\face::cover($store_logo);
+				\dash\face::twitterCard('summary');
+				\dash\face::logo($store_logo);
+			}
+		}
 	}
 }
 ?>
