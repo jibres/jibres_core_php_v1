a7ed283fcf0984f442ee61fd613eb5e8ac4cb842
Reza <rm.biqarar@gmail.com>
2021-01-21 16:37:19 +0330

    Fix show telegram notif list

--- content_crm/telegram/datalist/display.php
+++ content_crm/telegram/datalist/display.php
@@ -4,7 +4,7 @@
      <li>
       <a class="f align-center" href="<?php echo \dash\url::this(). '/view?id='. $value['id'] ?>">
 
-        <div class="key"><?php echo substr(a($value, 'sendtext'), 0, 70); ?></div>
+        <div class="key"><?php echo substr(strip_tags(a($value, 'sendtext')), 0, 100); ?></div>
 
         <div class="value txtB s0"><?php echo \dash\fit::mobile(a($value, 'mobile')); ?></div>
         <div class="value datetime s0"><?php echo \dash\fit::date_time($value['senddate']) ?></div>
