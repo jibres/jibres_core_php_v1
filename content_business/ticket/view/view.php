06c24dd70e0c71aa64ab5955b0b19f93ba38ff0d
Reza <rm.biqarar@gmail.com>
2021-01-25 19:51:52 +0330

    Fix link

--- content_business/ticket/view/view.php
+++ content_business/ticket/view/view.php
@@ -12,7 +12,7 @@ class view
 
 		// btn
 		\dash\data::back_text(T_('Tickets'));
-		\dash\data::back_link(\dash\url::support(). '/ticket');
+		\dash\data::back_link(\dash\url::this());
 
 
 		$ticket_id = \dash\data::dataRow_id();
