9244f4ad44bdde66687d8a4c3ecd330622eac40b
Reza <rm.biqarar@gmail.com>
2021-01-28 11:05:24 +0330

    fix save sms

--- content_crm/sms/send/view.php
+++ content_crm/sms/send/view.php
@@ -6,7 +6,7 @@ class view
 {
 	public static function config()
 	{
-		\dash\face::title(T_("Conversation"));
+		\dash\face::title(T_("Send sms"));
 
 		\dash\data::back_link(\dash\url::this());
 		\dash\data::back_text(T_('SMS'));
