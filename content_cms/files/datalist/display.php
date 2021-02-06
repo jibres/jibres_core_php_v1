03671d549a189cc798bcc3a64f91d4ce580ad965
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-06 17:50:54 +0330

    fix file list

--- content_cms/files/datalist/display.php
+++ content_cms/files/datalist/display.php
@@ -2,13 +2,43 @@
   <ul>
     <?php foreach (\dash\data::dataTable() as $key => $value) {?>
      <li>
-      <a class="f align-center" href="<?php echo \dash\url::this(). '/view?id='.  a($value, 'id') ?>">
+      <a class="item f align-center" href="<?php echo \dash\url::this(). '/view?id='.  a($value, 'id') ?>">
         <img src="<?php echo a($value, 'thumb'); ?>" alt="<?php echo T_("Post image") ?>">
         <div class="key"><?php echo a($value, 'filename'). '.'. a($value, 'ext'); ?></div>
+
         <div class="value s0"><?php echo \dash\fit::file_size(a($value, 'size')); ?></div>
-        <div class="value"><span class="badge light"><?php echo a($value, 't_type'); ?></span></div>
-        <div class="value ltr s0"><?php echo \dash\fit::date_time(a($value, 'datecreated')); ?></div>
-        <div class="go <?php echo $value['icon_list'] ?>"></div>
+
+<?php
+$fType = 'file';
+switch (a($value, 'type'))
+{
+  case 'image':
+    $fType = 'file-image-o';
+    break;
+
+  case 'audio':
+    $fType = 'music';
+    break;
+
+  case 'video':
+    $fType = 'file-video-o';
+    break;
+
+  case 'pdf':
+    $fType = 'file-pdf-o';
+    break;
+
+  case 'zip':
+    $fType = 'file-zip-o';
+    break;
+
+  default:
+    $fType = 'file-o';
+    break;
+}
+echo "<i class='sf-". $fType. "' title='". a($value, 't_type'). "'>". "</i>";
+?>
+        <time class="value s0" datetime="<?php echo a($value, 'datecreated'); ?>"><?php echo \dash\fit::date_time(a($value, 'datecreated')); ?></time>
       </a>
      </li>
     <?php } //endfor ?>
