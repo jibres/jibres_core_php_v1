e7c0b02e4a10297e2e40f6cb1d1e7a98ef64051d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-06 19:08:52 +0330

    show preview of audio and video

--- content_cms/files/view/display.php
+++ content_cms/files/view/display.php
@@ -1,8 +1,20 @@
 <?php $dataRow = \dash\data::dataRow(); ?>
 <div class="avand-md">
-	<a target="_blank" href="<?php echo a($dataRow, 'path') ?>">
-		<img src="<?php echo a($dataRow, 'thumb_raw'); ?>" alt="<?php echo a($dataRow, 'title') ?>">
-	</a>
+	<div class="mB10">
+
+<?php if(a($dataRow, 'type') === 'video') { ?>
+	  <video class="block" controls>
+	    <source src="<?php echo a($dataRow, 'path'); ?>" type="<?php echo a($dataRow, 'mime'); ?>">
+	  </video>
+<?php } else if(a($dataRow, 'type') === 'audio') { ?>
+  <audio class="block" controls>
+    <source src="<?php echo a($dataRow, 'path'); ?>" type="<?php echo a($dataRow, 'mime'); ?>">
+  </audio>
+<?php } else if(a($dataRow, 'type') === 'image') { ?>
+			<img class="block" src="<?php echo a($dataRow, 'thumb_raw'); ?>" alt="<?php echo a($dataRow, 'title') ?>">
+<?php } else { ?>
+<?php } ?>
+	</div>
 	<nav class="items long">
 		<ul>
 			<li>
@@ -15,25 +27,57 @@
 				<a class="f item">
 					<div class="key"><?php echo T_("Type") ?></div>
 					<div class="value"><?php echo a($dataRow, 't_type') ?></div>
+<?php
+$fType = 'file';
+switch (a($dataRow, 'type'))
+{
+  case 'image':
+    $fType = 'file-image-o';
+    break;
+
+  case 'audio':
+    $fType = 'music';
+    break;
+
+  case 'video':
+    $fType = 'file-video-o';
+    break;
+
+  case 'pdf':
+    $fType = 'file-pdf-o';
+    break;
+
+  case 'zip':
+    $fType = 'file-zip-o';
+    break;
+
+  default:
+    $fType = 'file-o';
+    break;
+}
+echo "<i class='sf-". $fType. "' title='". a($dataRow, 't_type'). "'>". "</i>";
+?>
 				</a>
 			</li>
 			<li>
-				<a class="f item" data-copy="<?php echo a($dataRow, 'path') ?>">
+				<div class="f item" data-copy="<?php echo a($dataRow, 'path') ?>">
 					<div class="key"><?php echo T_("Path") ?></div>
-					<div class="value"><?php echo a($dataRow, 'path') ?></div>
-				</a>
+					<div class="value ltr font-11"><?php echo a($dataRow, 'path') ?> <a class="font-18 mL10" target="_blank" href="<?php echo a($dataRow, 'path') ?>"><i class="sf-link-external"></i></a></div>
+					<i class="sf-link"></i>
+				</div>
 			</li>
 			<li>
 				<a class="f item">
 					<div class="key"><?php echo T_("size") ?></div>
-					<div class="value"><?php echo \dash\fit::file_size(a($dataRow, 'size')) ?></div>
+					<div class="value ltr"><?php echo \dash\fit::file_size(a($dataRow, 'size'), true); ?></div>
+					<div class="go detail"></div>
 				</a>
 			</li>
 			<?php if(a($dataRow, 'totalsize') && a($dataRow, 'size') !== a($dataRow, 'totalsize')) {?>
 			<li>
 				<a class="f item" href="<?php echo \dash\url::this(). '/images'. \dash\request::full_get() ?>">
 					<div class="key"><?php echo T_("Total size") ?></div>
-					<div class="value"><?php echo \dash\fit::file_size(a($dataRow, 'totalsize')) ?></div>
+					<div class="value ltr"><?php echo \dash\fit::file_size(a($dataRow, 'totalsize'), true); ?></div>
 					<div class="go"></div>
 				</a>
 			</li>
@@ -51,7 +95,8 @@
 			<li>
 				<a class="f item">
 					<div class="key"><?php echo T_("Date created") ?></div>
-					<div class="value"><?php echo \dash\fit::date_time(a($dataRow, 'datecreated')) ?></div>
+					<div class="value ltr"><?php echo \dash\fit::date_time(a($dataRow, 'datecreated')) ?></div>
+					<div class="go detail"></div>
 				</a>
 			</li>
 			<?php if(\dash\data::usageCount()) {?>
