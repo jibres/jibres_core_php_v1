47332596b697fbcb3eacce2aa165dc0eaa5a6397
Reza <rm.biqarar@gmail.com>
2021-02-09 10:48:18 +0330

    Fix

--- content_my/domain/search/display.php
+++ content_my/domain/search/display.php
@@ -9,28 +9,28 @@ foreach (\dash\data::dataTable() as $key => $value)
     $result .= '<a class="f item" href="'. \dash\url::this(). '/setting?domain='. a($value, 'name'). '">';
     $result .= '<div class="key">'. a($value, 'name'). '</div>';
 
-    $result .= '<div class="value">'. a($value, 'status_text'). '</div>';
+    $result .= '<div class="value s0">'. a($value, 'status_text'). '</div>';
 
     if(isset($value['autorenew']) && $value['autorenew'])
     {
-      $result .= '<div title="'. T_("Autorenew is active").'" class="value"><i class="sf-refresh fc-blue"></i></div>';
+      $result .= '<div title="'. T_("Autorenew is active").'" class="value s0"><i class="sf-refresh fc-blue"></i></div>';
     }
     else
     {
-      $result .= '<div title="'. T_("Autorenew is deactive").'" class="value"><i class="sf-refresh fc-mute"></i></div>';
+      $result .= '<div title="'. T_("Autorenew is deactive").'" class="value s0"><i class="sf-refresh fc-mute"></i></div>';
     }
 
     if(isset($value['lock']) && $value['lock'] == 1 )
     {
-      $result .= '<div title="'. T_("Domain is Lock").'" class="value"><i class="sf-lock fc-green"></i></div>';
+      $result .= '<div title="'. T_("Domain is Lock").'" class="value s0"><i class="sf-lock fc-green"></i></div>';
     }
     elseif(isset($value['lock']) && $value['lock'] == 0)
     {
-      $result .= '<div title="'. T_("Domain is Unlock").'" class="value"><i class="sf-unlock fc-red"></i></div>';
+      $result .= '<div title="'. T_("Domain is Unlock").'" class="value s0"><i class="sf-unlock fc-red"></i></div>';
     }
     else
     {
-      $result .= '<div title="'. T_("Unknown lock status").'" class="value"><i class="sf-lock"></i></div>';
+      $result .= '<div title="'. T_("Unknown lock status").'" class="value s0"><i class="sf-lock"></i></div>';
     }
 
     $result .= '<time class="value datetime">'. \dash\fit::date(a($value, 'dateexpire')). '</time>';
