f1fe9de6aad547d716c655b3e72de484f5ee3df2
Reza <rm.biqarar@gmail.com>
2021-02-11 17:00:12 +0330

    Fix show auto renew list in admin

--- content_love/domain/autorenew/display.php
+++ content_love/domain/autorenew/display.php
@@ -1,53 +1,26 @@
-  <?php foreach (\dash\data::autorenewList() as $time => $detail) { ?>
-        <div class="msg xl"><?php echo T_("At time:"). ' '. \dash\fit::number($time); ?></div>
-        <?php if(!$detail) {
-            echo T_("No domain renew in this time");
-         continue;} ?>
-<?php $sortLink = \dash\data::sortLink(); ?>
+<table class="tbl1 v1 ltr font-18">
+    <thead>
+        <tr>
+            <th class="ltr txtL">#</th>
+            <th class="ltr txtL">Date expire</th>
+            <th class="ltr txtL">Domain life time</th>
+            <th class="ltr txtL">Auto renew period IR</th>
+            <th class="txtC">Auto renew period COM</th>
+        </tr>
+    </thead>
+    <tbody class="ltr txtL">
 
-<div class="fs12">
-    <table class="tbl1 v1 responsive">
-        <thead>
-            <tr class="fs09">
+        <?php foreach (\dash\data::myList() as $key => $value) {?>
+        <tr class="ltr txtL">
+            <td class="ltr txtL">
+                <a href="<?php echo \dash\url::this(); ?>/setting?id=<?php echo \dash\coding::encode(a($value, 'myid')); ?>" class="link"><code><?php echo a($value, 'name'); ?></code></a>
+            </td>
+            <td class="ltr txtL"><?php echo \dash\fit::date_time(a($value, 'dateexpire')); ?></td>
+            <td class="ltr txtL"><?php echo a($value, 'domainlifetime'); ?></td>
+            <td class="ltr txtL"><?php echo a($value, 'autorenewperiod'); ?></td>
+            <td class="txtC"><?php echo a($value, 'autorenewperiodcom'); ?></td>
 
-                <th data-sort="<?php echo a($sortLink, 'name', 'order'); ?>" ><a href="<?php echo a($sortLink, 'name', 'link'); ?>"><?php echo T_("Domain"); ?></a></th>
-
-                <th class="txtL" data-sort="<?php echo a($sortLink, 'status', 'order'); ?>"><a href="<?php echo a($sortLink, 'status', 'link'); ?>"><?php echo T_("Status"); ?></a></th>
-                <th class="txtL" data-sort="<?php echo a($sortLink, 'dateexpire', 'order'); ?>"><a href="<?php echo a($sortLink, 'dateexpire', 'link'); ?>"><?php echo T_("Expire date"); ?></a></th>
-                <th class="txtL" data-sort="<?php echo a($sortLink, 'dateregister', 'order'); ?>"><a href="<?php echo a($sortLink, 'dateregister', 'link'); ?>"><?php echo T_("Create date"); ?></a></th>
-                <th class="txtL" data-sort="<?php echo a($sortLink, 'dateupdate', 'order'); ?>"><a href="<?php echo a($sortLink, 'dateupdate', 'link'); ?>"><?php echo T_("Date modified"); ?></a></th>
-
-                <th class="txtL"><?php echo T_("User"); ?></th>
-            </tr>
-        </thead>
-        <tbody class="fs12">
-
-            <?php foreach ($detail as $key => $value) {?>
-
-            <tr>
-                <td>
-                    <a href="<?php echo \dash\url::this(); ?>/setting?id=<?php echo a($value, 'id'); ?>" class="link"><code><?php echo a($value, 'name'); ?></code></a>
-                </td>
-
-                <td class="collapsing txtL"><?php echo a($value, 'tstatus'); ?></td>
-                <td class="collapsing txtL"><?php echo \dash\fit::date_time(a($value, 'dateexpire')); ?></td>
-                <td class="collapsing txtL"><?php echo \dash\fit::date_time(a($value, 'dateregister')); ?></td>
-                <td class="collapsing txtL"><?php echo \dash\fit::date_time(a($value, 'dateupdate')); ?></td>
-
-
-                <td class="collapsing">
-                  <a href="<?php echo \dash\url::that(). '?user='. a($value, 'user_id'); ?>" class="f align-center userPack">
-                    <div class="c pRa10">
-                      <div class="mobile"><?php echo \dash\fit::mobile(a($value, 'user_detail', 'mobile')); ?></div>
-                      <div class="name"><?php echo a($value, 'user_detail', 'displayname'); ?></div>
-                    </div>
-                    <img class="cauto" src="<?php echo a($value, 'user_detail', 'avatar'); ?>">
-                  </a>
-                </td>
-
-            </tr>
-            <?php } //endfor ?>
-        </tbody>
-    </table>
-</div>
-<?php } //endfor ?>
+        </tr>
+        <?php } //endfor ?>
+    </tbody>
+</table>
--- content_love/domain/autorenew/view.php
+++ content_love/domain/autorenew/view.php
@@ -13,6 +13,26 @@ class view
 		\dash\data::back_link(\dash\url::this());
 
 
+		$my_list = [];
+		foreach (['1week', '1month', '6month'] as $date)
+		{
+			$args =
+			[
+				'predict'        => true,
+				'autorenew_mode' => $date,
+				'autorenew_notif' => 'yes',
+			];
+
+			$list = \lib\app\nic_domain\search::get_list(null, $args);
+
+			if($list && is_array($list))
+			{
+				$my_list = array_merge($my_list, $list);
+			}
+		}
+
+		\dash\data::myList($my_list);
+
 
 
 	}
