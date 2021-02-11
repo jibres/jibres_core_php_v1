06456c3c0834ca8e666880702e3bffdada5b26c3
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-11 19:54:08 +0330

    seperate gallery

--- dash/layout/post/post-layout.php
+++ dash/layout/post/post-layout.php
@@ -26,49 +26,11 @@ echo '<div class="postContent">';
 echo \dash\data::dataRow_content();
 echo '</div>';
 
+require_once('gallery-box.php');
 
-if(isset($dataRow['gallery_array']) && is_array($dataRow['gallery_array']))
-{
-  echo '<div class="gallery" id="lightgallery">';
-  foreach ($dataRow['gallery_array'] as $key => $myUrl)
-  {
-    if(isset($myUrl['path']))
-    {
-      $endUrl = substr($myUrl['path'], -4);
-      if(in_array($endUrl, ['.jpg', '.png', '.gif']))
-      {
-        echo '<a data-action href="'. $myUrl['path'].'"><img src="'. $myUrl['path']. '" alt="'. \dash\data::dataRow_title(). '"></a>';
-      }
-    }
-  }
-  echo '</div>';
-
-  echo '<div class="gallery2">';
-  foreach ($dataRow['gallery_array'] as $key => $myUrl)
-  {
-    if(isset($myUrl['path']))
-    {
-      $endUrl = substr($myUrl['path'], -4);
-      if(in_array($endUrl, ['.mp4']))
-      {
-        echo '<video controls><source src="'. $myUrl['path']. '" type="video/mp4"></video>';
-      }
-      elseif(in_array($endUrl, ['.mp3']))
-      {
-        echo '<audio controls><source src="'. $myUrl['path'] .'" type="audio/mpeg"></audio>';
-      }
-      elseif(in_array($endUrl, ['.pdf']))
-      {
-        echo '<a href="'. $myUrl['path'] .'" class="btn lg mT25 primary">'. T_("Download"). ' '. T_("PDF"). '</a>';
-      }
-    }
-  }
-  echo '</div>';
-}
 
 if(\dash\data::dataRow_datemodified())
 {
-
 echo '<div class="msg simple f mT20">';
 echo '<div class="c">';
 if(\dash\data::dataRow_showdate() === 'visible' || a($cmsSetting, 'defaultshowdate') === 'visible')
