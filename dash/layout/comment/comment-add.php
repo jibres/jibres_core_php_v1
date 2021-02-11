6507950125edc1fdab6c95cf8409cf071a3f6221
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-11 18:59:12 +0330

    compact design for star on add new comment

--- dash/layout/comment/comment-add.php
+++ dash/layout/comment/comment-add.php
@@ -6,22 +6,6 @@
     <form method="post" data-refresh autocomplete="off" action="<?php echo \dash\url::here(); ?>/comment">
       <input type="hidden" name="post_id" class="hide" value="<?php echo \dash\data::dataRow_id(); ?>">
 
-      <div class="starRating">
-        <fieldset>
-          <input type="radio" name="rating" id="star5" value="5"/>
-          <label for="star5" title="<?php echo T_("Outstanding");?>">5 stars</label>
-          <input type="radio" name="rating" id="star4" value="4"/>
-          <label for="star4" title="<?php echo T_("Very Good");?>">4 stars</label>
-          <input type="radio" name="rating" id="star3" value="3"/>
-          <label for="star3" title="<?php echo T_("Good");?>">3 stars</label>
-          <input type="radio" name="rating" id="star2" value="2"/>
-          <label for="star2" title="<?php echo T_("Poor");?>">2 stars</label>
-          <input type="radio" name="rating" id="star1" value="1"/>
-          <label for="star1" title="<?php echo T_("Very Poor");?>">1 star</label>
-        </fieldset>
-      </div>
-
-
     <?php if(!\dash\user::id()) { ?>
           <div class="row">
             <div class="c c-xs-12 mB10">
@@ -49,8 +33,24 @@
         <textarea name="content" class="txt" rows="4" placeholder='<?php echo T_("Write your review about this post..."); ?>'></textarea>
       </div>
 
-      <div class="row ">
+      <div class="row align-center">
         <div class="c">
+          <div class="starRating">
+            <fieldset>
+              <input type="radio" name="rating" id="star5" value="5"/>
+              <label for="star5" title="<?php echo T_("Outstanding");?>">5 stars</label>
+              <input type="radio" name="rating" id="star4" value="4"/>
+              <label for="star4" title="<?php echo T_("Very Good");?>">4 stars</label>
+              <input type="radio" name="rating" id="star3" value="3"/>
+              <label for="star3" title="<?php echo T_("Good");?>">3 stars</label>
+              <input type="radio" name="rating" id="star2" value="2"/>
+              <label for="star2" title="<?php echo T_("Poor");?>">2 stars</label>
+              <input type="radio" name="rating" id="star1" value="1"/>
+              <label for="star1" title="<?php echo T_("Very Poor");?>">1 star</label>
+            </fieldset>
+          </div>
+        </div>
+        <div class="c-auto os">
           <button class="btn primary"><?php echo T_("Send"); ?></button>
         </div>
 
