41f8377c359a9d279d4c70f9c9bcda5dba5d6d9f
Reza <rm.biqarar@gmail.com>
2021-02-11 15:58:01 +0330

    Enable autorenew sms

--- lib/app/log/caller/domain/domain_AutoRenewAlert.php
+++ lib/app/log/caller/domain/domain_AutoRenewAlert.php
@@ -96,7 +96,27 @@ class domain_AutoRenewAlert
 
 	public static function sms()
 	{
-		return false;
+		return true;
+	}
+
+
+
+	public static function sms_text($_args, $_mobile)
+	{
+		$title = self::get_msg($_args);
+
+		$sms =
+		[
+			'mobile' => $_mobile,
+			'text'   => $title,
+			'meta'   =>
+			[
+				'header' => false,
+				'footer' => false
+			]
+		];
+
+		return json_encode($sms, JSON_UNESCAPED_UNICODE);
 	}
 
 
