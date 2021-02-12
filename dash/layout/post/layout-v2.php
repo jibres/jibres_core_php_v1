c14f1ee2b18cc689a73d6c93f4b51aa8336d53ef
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-12 16:35:51 +0330

    some fix on layout v2

--- dash/layout/post/layout-v2.php
+++ dash/layout/post/layout-v2.php
@@ -1,91 +1,93 @@
 <?php
 $cmsSetting = \lib\app\setting\get::cms_setting();
-
-
-$dataRow = \dash\data::dataRow();
-echo '<div class="postBlock">';
-echo '<div class="avand-md zero">';
-echo '<div class="box">';
-echo '<div class="body">';
-
-echo \dash\layout\post\part::title();
-
-
-if(\dash\data::dataRow_thumb())
+$subType = \dash\data::dataRow_subtype();
+echo '<section class="postBlock" data-type="'. $subType. '">';
 {
-  echo '<a href="'.  \dash\data::dataRow_link(). '" class="thumb">';
-  echo '<img src="'. \dash\fit::img(\dash\data::dataRow_thumb(), 1100). '" alt="'. \dash\data::dataRow_title(). '">';
-  echo '</a>';
-}
 
-echo '<div class="postContent">';
-echo \dash\data::dataRow_content();
-echo '</div>';
-
-require_once('gallery-box.php');
-
-
-if(\dash\data::dataRow_datemodified())
-{
-  echo '<div class="msg simple f mT20">';
-  echo '<div class="c">';
-  if(\dash\data::dataRow_showdate() === 'visible' || a($cmsSetting, 'defaultshowdate') === 'visible')
+  echo '<div class="avand-md zero">';
   {
-    echo '<time class="ltr compact" datetime="'. \dash\data::dataRow_datemodified(). '">'. \dash\fit::date_time(\dash\data::dataRow_publishdate()). '</time>';
+    echo \dash\layout\post\part::header($subType);
+    echo \dash\layout\post\part::article();
+
   }
+
+  echo '<div class="box">';
+
+  if(\dash\data::dataRow_thumb())
+  {
+    echo '<a href="'.  \dash\data::dataRow_link(). '" class="thumb">';
+    echo '<img src="'. \dash\fit::img(\dash\data::dataRow_thumb(), 1100). '" alt="'. \dash\data::dataRow_title(). '">';
+    echo '</a>';
+  }
+
+  require_once('gallery-box.php');
+
+
+  if(\dash\data::dataRow_datemodified())
+  {
+    echo '<div class="msg simple f mT20">';
+    echo '<div class="c">';
+    if(\dash\data::dataRow_showdate() === 'visible' || a($cmsSetting, 'defaultshowdate') === 'visible')
+    {
+      echo '<time class="ltr compact" datetime="'. \dash\data::dataRow_datemodified(). '">'. \dash\fit::date_time(\dash\data::dataRow_publishdate()). '</time>';
+    }
+
+    echo '</div>';
+    echo '<div class="cauto os">';
+    echo '<a data-copy="'.  \dash\url::kingdom(). '/n/'. \dash\data::dataRow_id(). '" href="'. \dash\url::kingdom(). '/n/'. \dash\data::dataRow_id(). '" title="'.  T_("For share via social networks"). '">'. T_("Short Code") .' <span class="txtB">' . \dash\data::dataRow_id(). '</span></a>';
+    echo '</div>';
+    echo '</div>';
+  }
+
+
+  $tags = \dash\data::dataRow_tags();
+  if($tags && is_array($tags))
+  {
+    echo '<div class="tagBox msg simple">';
+    foreach ($tags as $key => $value)
+    {
+      echo "<span class='btn'>";
+      echo a($value, 'title');
+      echo "</span> ";
+    }
 
   echo '</div>';
-  echo '<div class="cauto os">';
-  echo '<a data-copy="'.  \dash\url::kingdom(). '/n/'. \dash\data::dataRow_id(). '" href="'. \dash\url::kingdom(). '/n/'. \dash\data::dataRow_id(). '" title="'.  T_("For share via social networks"). '">'. T_("Short Code") .' <span class="txtB">' . \dash\data::dataRow_id(). '</span></a>';
+  }
+  echo '<div class="msg">';
+  require_once('share-box.php');
+  echo '</div>';
+
+  $myPostSimilar = \dash\app\posts\search::similar_post(\dash\data::dataRow_id());
+  if($myPostSimilar)
+  {
+    echo '<nav class="msg">';
+    echo '<h4 class="mB20-f">'. T_("Recommended for you"). '</h4>';
+    foreach ($myPostSimilar as $key => $value)
+    {
+      echo '<a class="block" href="'. a($value, 'link') .'">'. $value['title']. '</a>';
+    }
+    echo '</nav>';
+  }
   echo '</div>';
   echo '</div>';
-}
 
 
-if(a($dataRow, 'tags') && is_array(a($dataRow, 'tags')))
-{
-  echo '<div class="tagBox msg simple">';
-  foreach ($dataRow['tags'] as $key => $value)
+  // comment box
   {
-    echo "<span class='btn'>";
-    echo a($value, 'title');
-    echo "</span> ";
+    // add new comment
+    if(\dash\data::dataRow_comment() === 'open' || ( \dash\data::dataRow_comment() === 'default' && a($cmsSetting, 'defaultcomment') === 'open' ))
+    {
+      require_once(core. 'layout/comment/comment-add.php');
+    }
+    // show list of comments
+    require_once(core. 'layout/comment/comment-list.php');
   }
 
-echo '</div>';
-}
-echo '<div class="msg">';
-require_once('share-box.php');
-echo '</div>';
+  // close avand
+  echo '</div>';
 
-$myPostSimilar = \dash\app\posts\search::similar_post(\dash\data::dataRow_id());
-if($myPostSimilar)
-{
-  echo '<nav class="msg">';
-  echo '<h4 class="mB20-f">'. T_("Recommended for you"). '</h4>';
-  foreach ($myPostSimilar as $key => $value)
-  {
-    echo '<a class="block" href="'. a($value, 'link') .'">'. $value['title']. '</a>';
-  }
-  echo '</nav>';
 }
-echo '</div>';
-echo '</div>';
-
-// comment box
-{
-  // add new comment
-  if(\dash\data::dataRow_comment() === 'open' || ( \dash\data::dataRow_comment() === 'default' && a($cmsSetting, 'defaultcomment') === 'open' ))
-  {
-    require_once(core. 'layout/comment/comment-add.php');
-  }
-  // show list of comments
-  require_once(core. 'layout/comment/comment-list.php');
-}
-
-// close avand
-echo '</div>';
 
 // close post Block
-echo '</div>';
+echo '</section>';
 ?>
--- dash/layout/post/part.php
+++ dash/layout/post/part.php
@@ -4,9 +4,122 @@ namespace dash\layout\post;
 
 class part
 {
+	public static function header($_subType)
+	{
+		$html = '';
+
+		// check gallery items
+		$myGallery = \dash\data::dataRow_gallery_array();
+		$myGalleryTopMedia = null;
+		if(count($myGallery) === 1)
+		{
+			$myGalleryTopMedia = $myGallery[0];
+		}
+
+    $html .= '<header>';
+    // show
+    switch ($_subType)
+    {
+      case 'video':
+        $html .= self::topVideo($myGalleryTopMedia);
+        $html .= self::title();
+        break;
+
+      case 'standard':
+      case 'gallery':
+      default:
+        $html .= self::title();
+        break;
+    }
+
+    $html .= '</header>';
+
+    return $html;
+	}
+
+
 	public static function title($_heading = 2)
 	{
 		return '<h'. $_heading. '>'. \dash\data::dataRow_title(). '</h'. $_heading. '>';
+	}
+
+
+	public static function article()
+	{
+		$html = '';
+    $html .= '<article>';
+    $html .= \dash\data::dataRow_content();
+    $html .= '</article>';
+
+    return $html;
+	}
+
+
+	public static function topVideo($_data)
+	{
+		$htmlVideo = '';
+
+		switch (a($_data, 'ext'))
+		{
+			case 'mp4':
+			case 'ogg':
+			case 'webm':
+				// do nothing
+				break;
+
+			default:
+				return null;
+
+				break;
+		}
+
+		$path   = a($_data, 'path');
+		$endUrl = substr($path, -4);
+    if(in_array($endUrl, ['.mp4', '.ogg', 'webm']))
+    {
+      $htmlVideo .= '<video controls>';
+      $htmlVideo .= '<source src="'. $path. '" type="'. a($_data, 'mime'). '">';
+      $htmlVideo .= '</video>';
+    }
+
+    return $htmlVideo;
+	}
+
+
+	public static function topAudio($_data)
+	{
+		$htmlAudio = '';
+
+		switch (a($_data, 'ext'))
+		{
+			case 'mp3':
+			case 'ogg':
+			case 'wav':
+				// do nothing
+				break;
+
+			default:
+				return null;
+
+				break;
+		}
+
+		$path   = a($_data, 'path');
+		$endUrl = substr($path, -4);
+    if(in_array($endUrl, ['.mp3', '.ogg', '.wav']))
+    {
+      $htmlAudio .= '<audio controls>';
+      $htmlAudio .= '<source src="'. $path. '" type="'. a($_data, 'mime'). '">';
+      $htmlAudio .= '</audio>';
+    }
+
+    return $htmlAudio;
+	}
+
+
+	public static function thumb()
+	{
+
 	}
 
 }
