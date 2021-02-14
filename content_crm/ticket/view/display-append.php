b834d11125219068557e4e9508c5d29a6638afed
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-14 19:18:59 +0330

    use simple editor

--- content_crm/ticket/view/display-append.php
+++ content_crm/ticket/view/display-append.php
@@ -2,7 +2,7 @@
   <?php if($customer_mode){ \dash\csrf::html(false); } ?>
   <div class="box">
     <div class="pad">
-      <textarea class="txt" <?php if(!$customer_mode){echo 'name="answer" data-editor';}else{echo 'name="content"';} ?> rows="4" <?php \dash\layout\autofocus::html() ?> placeholder='<?php if(!$customer_mode) { echo T_("Answer to ticket"); }else{ echo T_("Write your message");} ?>'></textarea>
+      <textarea class="txt" <?php if(!$customer_mode){echo 'name="answer" data-editor="simple"';}else{echo 'name="content"';} ?> rows="4" <?php \dash\layout\autofocus::html() ?> placeholder='<?php if(!$customer_mode) { echo T_("Answer to ticket"); }else{ echo T_("Write your message");} ?>'></textarea>
       <div class="mT10" data-uploader data-name='file' data-file-max-size='<?php echo \dash\data::maxFileSize() ?>'>
         <input type="file"  id="file1">
         <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
