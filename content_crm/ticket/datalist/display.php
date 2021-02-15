3b23e765505ee974ca145e75777a5e7f381d7404
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-15 12:02:25 +0330

    improve ticket list

--- content_crm/ticket/datalist/display.php
+++ content_crm/ticket/datalist/display.php
@@ -18,22 +18,15 @@ echo '<nav class="items long">';
 
         echo a($value, 'statuclass');
 
-        echo '<div class="value">'. T_("Ticket"). ' #'. $value['id']. '</div>';
+        echo '<div class="value">'. T_("Ticket"). ' '. $value['id']. '</div>';
+
+        echo "<i class='sf-retweet' data-count='". \dash\fit::number(a($value, 'plus')). "'></i>";
 
         echo '<div class="key">'. a($value, 'title'). '</div>';
 
-        if(a($value, 'plus'))
-        {
-
-          // echo "<i class='sf-retweet' data-count='". \dash\fit::number(a($value, 'plus')). "'></i>";
-          echo '<div class="value">';
-          echo ' <span class="badge rounded light s0"> <i class="sf-refresh"></i> '. \dash\fit::number(a($value, 'plus')). '</span>';
-          echo '</div>';
-        }
-
-
         echo '<div class="value s0">'. a($value, 'displayname'). '</div>';
-        echo '<div class="value"><img class="avatar" alt="'. a($value, 'displayname'). '" src="'. a($value, 'avatar'). '"></div>';
+        echo '<div class="value"><img class="avatar" alt="'. a($value, 'displayname'). '" src="'. \dash\fit::img(a($value, 'avatar')). '"></div>';
+
         echo '<time class="value" datetime="'. a($value, 'datecreated'). '">'. \dash\fit::date_time(a($value, 'datecreated')). '</time>';
       }
       echo '</a>';
