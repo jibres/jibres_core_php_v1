a561a40de349d995fbd1982158ba4e6ced619d04
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-17 10:49:07 +0330

    fix status height

--- content/status/display.php
+++ content/status/display.php
@@ -1,7 +1,7 @@
 <div class="jibresBanner">
 
  <div class="avand impact zero">
-  <iframe class="block" src="https://status.jibres.com" height="1800"></iframe>
+  <iframe class="block" src="https://status.jibres.com" height="3500"></iframe>
  </div>
 
 </div>
