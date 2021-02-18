838c8c7fa52336d2c9b5a23213f606088561c659
Reza <rm.biqarar@gmail.com>
2021-02-18 08:46:40 +0330

    Check crm and cms controller

--- content_cms/controller.php
+++ content_cms/controller.php
@@ -6,6 +6,16 @@ class controller
 
 	public static function routing()
 	{
+		if(!\dash\url::store())
+		{
+			\dash\redirect::to(\dash\url::kingdom());
+		}
+
+		if(!\lib\store::id())
+		{
+			\dash\header::status(404, T_("Store not found"));
+		}
+
 		\dash\redirect::to_login();
 
 		\dash\permission::access('_group_cms');
--- content_crm/controller.php
+++ content_crm/controller.php
@@ -7,6 +7,15 @@ class controller
 	public static function routing()
 	{
 
+		if(\dash\engine\store::inStore())
+		{
+			if(!\dash\url::store())
+			{
+				\dash\redirect::to(\dash\url::kingdom());
+			}
+		}
+
+
 		\dash\redirect::to_login();
 
 		if(\dash\url::module() === 'api' && \dash\permission::has_permission())
