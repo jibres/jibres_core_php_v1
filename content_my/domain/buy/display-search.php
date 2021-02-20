b0f6a37189389c5aacd44c753fa61c80e277d5f9
Reza <rm.biqarar@gmail.com>
2021-02-20 10:17:48 +0330

    Fix show error

--- content_my/domain/buy/display-search.php
+++ content_my/domain/buy/display-search.php
@@ -39,7 +39,7 @@ if($result)
   }
 
 }
-else
+elseif(!\dash\data::InvalidDomain() && \dash\request::get('q'))
 {
   echo '<div class="msg warn txtC mB0">'. T_("Please enter valid domain name!"). "</div>";
 }
