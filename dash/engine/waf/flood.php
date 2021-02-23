ab25e350cf05a11b14e43a9ccb80a7ecdd7ac20d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-23 12:20:40 +0330

    seperate flood detection

---
+++ dash/engine/waf/flood.php
@@ -0,0 +1,40 @@
+<?php
+namespace dash\engine\waf;
+
+class flood
+{
+	public static function stop()
+	{
+		// visitor request 10 times under e.g. 2 seconds will be stopped!
+		$flood_interval = 1;
+
+		$counter   = \dash\session::get('counter', 'waf');
+		$last_post = \dash\session::get('last', 'waf');
+		$ip        = \dash\session::get('ip', 'waf');
+
+		if(!$counter)
+		{
+			$counter = 1;
+		}
+
+		// do nothing if ip is changed for this session
+		if($ip && $ip === \dash\server::ip())
+		{
+			if($counter > 15)
+			{
+				if(($last_post + $flood_interval) > time())
+				{
+					// Use this if you want to reset counter
+					\dash\session::set(0, 'counter', 'waf');
+					\dash\header::status(416, 'Please be patient');
+				}
+			}
+		}
+
+		// save session
+		\dash\session::set('ip', \dash\server::ip(), 'waf');
+		\dash\session::set('counter', $counter + 1, 'waf');
+		\dash\session::set('last', time(), 'waf');
+	}
+}
+?>
--- dash/engine/waf/protection.php
+++ dash/engine/waf/protection.php
@@ -3,8 +3,6 @@ namespace dash\engine\waf;
 
 class protection
 {
-	private static $ipSecAddr = YARD.'jibres_ipsec/';
-
 	public static function start()
 	{
 		// block baby to not allow to harm yourself :/
@@ -14,42 +12,7 @@ class protection
 		\dash\engine\waf\ip::checkLimit();
 
 		// disallow flood
-		// self::floodProtection();
-	}
-
-
-	public static function floodProtection()
-	{
-		// visitor request 10 times under e.g. 2 seconds will be stopped!
-		$flood_interval = 1;
-
-		$counter   = \dash\session::get('counter', 'waf');
-		$last_post = \dash\session::get('last', 'waf');
-		$ip        = \dash\session::get('ip', 'waf');
-
-		if(!$counter)
-		{
-			$counter = 1;
-		}
-
-		// do nothing if ip is changed for this session
-		if($ip && $ip === \dash\server::ip())
-		{
-			if($counter > 15)
-			{
-				if(($last_post + $flood_interval) > time())
-				{
-					// Use this if you want to reset counter
-					\dash\session::set(0, 'counter', 'waf');
-					\dash\header::status(416, 'Please be patient');
-				}
-			}
-		}
-
-		// save session
-		\dash\session::set('ip', \dash\server::ip(), 'waf');
-		\dash\session::set('counter', $counter + 1, 'waf');
-		\dash\session::set('last', time(), 'waf');
+		\dash\engine\waf\flood::stop();
 	}
 }
 ?>
