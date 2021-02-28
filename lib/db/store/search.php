5ee2e9eb7b8d8a06d308d3fcd5386ac6e0e1a847
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-02-28 13:13:48 +0330

    fix

--- lib/db/store/search.php
+++ lib/db/store/search.php
@@ -3,10 +3,8 @@ namespace lib\db\store;
 
 class search
 {
-
 	public static function list($_and, $_or, $_order_sort = null, $_meta = [])
 	{
-
 		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
 		$pagination_query = "SELECT COUNT(*) AS `count` FROM store $q[join] $q[where]";
@@ -17,9 +15,7 @@ class search
 			$limit = \dash\db\mysql\tools\pagination::pagination_query($pagination_query, $q['limit']);
 		}
 
-		$query =
-		"
-			SELECT
+		$query = "SELECT
 				store.*,
 				store.status AS `store_status`,
 				store_data.*
@@ -27,8 +23,7 @@ class search
 				store
 				$q[join]
 
-			$q[where] $q[order] $limit
-		";
+			$q[where] $q[order] $limit";
 
 		$result = \dash\db::get($query, null, false);
 
@@ -36,11 +31,8 @@ class search
 	}
 
 
-
 	public static function list_analytics($_and, $_or, $_order_sort = null, $_meta = [], $_f = null)
 	{
-
-
 		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
 		$pagination_query = "SELECT COUNT(*) AS `count` FROM store INNER JOIN store_data ON store_data.id = store.id
@@ -52,9 +44,7 @@ class search
 			$limit = \dash\db\mysql\tools\pagination::pagination_query($pagination_query, $q['limit']);
 		}
 
-		$query =
-		"
-			SELECT
+		$query = "SELECT
 				store.*,
 				store_data.*,
 				store_analytics.*
@@ -63,8 +53,7 @@ class search
 			INNER JOIN store_data ON store_data.id = store.id
 			LEFT JOIN store_analytics ON store_analytics.id = store.id
 
-			$q[where] $q[order] $limit
-		";
+			$q[where] $q[order] $limit";
 
 		$result = \dash\db::get($query, null, false);
 
@@ -74,7 +63,6 @@ class search
 
 	public static function list_domain($_and, $_or, $_order_sort = null, $_meta = [])
 	{
-
 		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
 		$pagination_query = "SELECT COUNT(*) AS `count` FROM store_domain INNER JOIN store_data ON store_data.id = store_domain.store_id $q[where]";
@@ -85,25 +73,18 @@ class search
 			$limit = \dash\db\mysql\tools\pagination::pagination_query($pagination_query, $q['limit']);
 		}
 
-		$query =
-		"
-			SELECT
+		$query ="SELECT
 				store_domain.*,
 				store_data.title
 			FROM
 				store_domain
 			INNER JOIN store_data ON store_data.id = store_domain.store_id
 
-			$q[where] $q[order] $limit
-		";
+			$q[where] $q[order] $limit";
 
 		$result = \dash\db::get($query, null, false);
 
 		return $result;
 	}
-
-
-
-
 }
 ?>
