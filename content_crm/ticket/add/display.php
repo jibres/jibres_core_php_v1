421e4092c0630c85fb8233da5ca279ef56b778e6
Reza <rm.biqarar@gmail.com>
2021-03-03 07:01:24 +0330

    Disable ticket editor

--- content_crm/ticket/add/display.php
+++ content_crm/ticket/add/display.php
@@ -1,25 +1,27 @@
-<form method="post" autocomplete="off" >
-  <div class="box">
-    <div class="pad">
-      <label><?php echo T_("Ticket owner") ?></label>
-      <div>
-       <select name="user_id" class="select22"  data-model='html'  data-ajax--url='<?php echo \dash\url::kingdom(); ?>/crm/api?type=sale&json=true&list=customer' data-shortkey-search data-placeholder='<?php echo T_("Choose ticket owner"); ?>'>
-        </select>
-      </div>
-      <label for="title"><?php echo T_("Subject") ?></label>
-      <div class="input">
-        <input  type="text" name="title" placeholder="<?php echo T_("Subject") ?>" maxlength="100">
-      </div>
-      <textarea class="txt" name="content" data-editor rows="3" <?php \dash\layout\autofocus::html() ?> placeholder='<?php echo T_("Type here ...") ?>'></textarea>
+<div class="avand-md">
+  <form method="post" autocomplete="off" >
+    <div class="box">
+      <div class="pad">
+        <label><?php echo T_("Ticket owner") ?></label>
+        <div>
+         <select name="user_id" class="select22"  data-model='html'  data-ajax--url='<?php echo \dash\url::kingdom(); ?>/crm/api?type=sale&json=true&list=customer' data-shortkey-search data-placeholder='<?php echo T_("Choose ticket owner"); ?>'>
+          </select>
+        </div>
+        <label for="title"><?php echo T_("Subject") ?></label>
+        <div class="input">
+          <input  type="text" name="title" placeholder="<?php echo T_("Subject") ?>" maxlength="100">
+        </div>
+        <textarea class="txt" name="content" rows="3" <?php \dash\layout\autofocus::html() ?> placeholder='<?php echo T_("Type here ...") ?>'></textarea>
 
-      <div class="mT10" data-uploader data-name='file' data-file-max-size='<?php echo \dash\data::maxFileSize() ?>'>
-        <input type="file"  id="file1">
-        <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
-      </div>
+        <div class="mT10" data-uploader data-name='file' data-file-max-size='<?php echo \dash\data::maxFileSize() ?>'>
+          <input type="file"  id="file1">
+          <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
+        </div>
 
+      </div>
+      <footer class="txtRa">
+        <button class="btn master"><?php echo T_("Add Ticket") ?></button>
+      </footer>
     </div>
-    <footer class="txtRa">
-      <button class="btn master"><?php echo T_("Add Ticket") ?></button>
-    </footer>
-  </div>
-</form>
+  </form>
+</div>
