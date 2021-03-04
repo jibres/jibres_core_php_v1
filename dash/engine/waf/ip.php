77167c83ed44ef3469fef5d077d855169d6e4c0c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-04 16:52:00 +0330

    increase limit

--- dash/engine/waf/ip.php
+++ dash/engine/waf/ip.php
@@ -164,7 +164,7 @@ class ip
 					self::do_reactive($_info);
 				}
 
-				if (a($_info, 'reqCounter') > 120)
+				if (a($_info, 'reqCounter') > 120000)
 				{
 					// We check rpm (request per minute) after 120 request to get a good ~value
 					if ( $_info['rpm'] > 40)
