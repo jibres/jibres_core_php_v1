87578e46cc3b98b9d005b545ec966e4058066c2b
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-05 20:02:25 +0330

    save tag

--- dash/waf/gate/toys/block.php
+++ dash/waf/gate/toys/block.php
@@ -44,7 +44,7 @@ class block
 		$strippedText = strip_tags($_text);
 		if($_text !== $strippedText)
 		{
-			$msg = 'ooh Tag!';
+			$msg = 'ooh Tag! '. $_text;
 			if($_from)
 			{
 				$msg = 'ooh Tag inside '. $_from;
