4f4397a34e4be44ed5e31348036458e400e3fd86
Reza <rm.biqarar@gmail.com>
2021-03-06 14:37:53 +0330

    Show ip detail

--- content_love/ip/display.php
+++ content_love/ip/display.php
@@ -1,16 +1,37 @@
-<form method="post" autocomplete="off">
+
+<?php if(\dash\data::countFile()) {?>
+<div class="avand-md ltr">
+  <?php foreach (\dash\data::countFile() as $key => $value) {?>
+    <div class="msg row">
+      <div class="c"><?php echo $key ?></div>
+      <div class="c"><?php echo $value ?></div>
+    </div>
+  <?php } //endif ?>
+</div>
+<?php } //endif ?>
+
+<form method="get" autocomplete="off" action="<?php echo \dash\url::this(); ?>">
   <div class="avand-md">
     <div class="box ltr txtL">
       <div class="pad">
-        <p>Whitelist an IP</p>
         <label>IP</label>
         <div class="input ltr">
-          <input type="text" name="ip">
+          <input type="text" name="ip" value="<?php echo \dash\data::myIP() ?>">
         </div>
       </div>
       <footer class="txtLa">
-        <button class="btn success">Set</button>
+        <button class="btn master">Check</button>
       </footer>
     </div>
   </div>
 </form>
+
+<?php if(\dash\data::ipNotFound()) {?>
+<div class="avand-md">
+<div class="msg warn2"><?php echo T_("IP detail not found") ?></div>
+</div>
+<?php }elseif(\dash\data::ipDetail()) {?>
+<pre>
+  <?php print_r(\dash\data::ipDetail()) ?>
+</pre>
+<?php } //endif ?>
--- content_love/ip/view.php
+++ content_love/ip/view.php
@@ -10,6 +10,30 @@ class view
 		// btn
 		\dash\data::back_text(T_('Dashboard'));
 		\dash\data::back_link(\dash\url::here());
+
+
+		$count_file = \dash\waf\ip::countFileInFolder();
+		\dash\data::countFile($count_file);
+
+
+		$ip = \dash\request::get('ip');
+		if($ip)
+		{
+			$ip = \dash\validate::ip($ip);
+
+			if($ip)
+			{
+				\dash\data::myIP($ip);
+
+				$ipDetail = \dash\waf\ip::open_ip_file($ip);
+				\dash\data::ipDetail($ipDetail);
+
+				if($ip && !$ipDetail)
+				{
+					\dash\data::ipNotFound(true);
+				}
+			}
+		}
 	}
 }
 ?>
