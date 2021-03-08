1f914915ef56a3bf06e3126e5201c9c35f4845a7
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-08 20:13:06 +0330

    save

--- content_su/passwd/display.php
+++ content_su/passwd/display.php
@@ -2,7 +2,7 @@
   <div class="avand-sm">
     <div class="box">
       <div class="pad">
-        <label for="a"><?php echo T_("Enter Password") ?></label>
+        <label for="a"><?php echo T_("Hello Dear") ?></label>
         <div class="input ltr">
           <input type="password" name="a" id="a" autocomplete="new-password" <?php \dash\layout\autofocus::html(); ?>>
         </div>
