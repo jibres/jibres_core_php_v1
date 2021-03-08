8e957975d3fe6c02dcc39f4616908bcbcabadfe1
Reza <rm.biqarar@gmail.com>
2021-03-08 12:20:01 +0330

    Enable check url

--- dash/waf/dog.php
+++ dash/waf/dog.php
@@ -27,8 +27,8 @@ class dog
 		\dash\waf\gate\headers::inspection();
 
 		// // ip
-		// self::grade('url', false, 3);
-		// \dash\waf\gate\url::inspection();
+		self::grade('url', false, 3);
+		\dash\waf\gate\url::inspection();
 
 		// get
 		self::grade('get', false, 3);
