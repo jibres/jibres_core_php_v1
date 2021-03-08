1ce65624f64df5fb9b30dc09cc0295858d939d4f
Reza <rm.biqarar@gmail.com>
2021-03-08 16:32:53 +0330

    save store ip inside database

--- dash/db/ip.php
+++ dash/db/ip.php
@@ -4,11 +4,29 @@ namespace dash\db;
 
 class ip
 {
+	/**
+	 * If jibres the fuel is visitor
+	 * if in stroe current databse is fuel
+	 *
+	 * @return     string  ( description_of_the_return_value )
+	 */
+	private static function ip_fuel()
+	{
+		if(\dash\engine\store::inStore())
+		{
+			return null;
+		}
+		else
+		{
+			return 'visitor';
+		}
+	}
+
 
 	public static function get_ipv4($_ip)
 	{
 		$query  = "SELECT * FROM ip WHERE ip.ipv4 = '$_ip' LIMIT 1";
-		$result = \dash\db::get($query, null, true, 'visitor');
+		$result = \dash\db::get($query, null, true, self::ip_fuel());
 		return $result;
 	}
 
@@ -16,7 +34,7 @@ class ip
 	public static function get_ipv6($_ip)
 	{
 		$query  = "SELECT * FROM ip WHERE ip.ipv6 = '$_ip' LIMIT 1";
-		$result = \dash\db::get($query, null, true, 'visitor');
+		$result = \dash\db::get($query, null, true, self::ip_fuel());
 		return $result;
 	}
 
@@ -24,7 +42,7 @@ class ip
 	public static function count_modified_date($_date)
 	{
 		$query = "SELECT COUNT(*) AS `count` FROM ip WHERE DATE(ip.datemodified) = DATE('$_date') ";
-		$result = \dash\db::get($query, 'count', true, 'visitor');
+		$result = \dash\db::get($query, 'count', true, self::ip_fuel());
 		return $result;
 	}
 
@@ -32,7 +50,7 @@ class ip
 	public static function new_list()
 	{
 		$query  = "SELECT * FROM ip WHERE ip.block = 'new' LIMIT 1000";
-		$result = \dash\db::get($query, null, false, 'visitor');
+		$result = \dash\db::get($query, null, false, self::ip_fuel());
 		return $result;
 	}
 
@@ -40,7 +58,7 @@ class ip
 	public static function set_block($_id, $_count_block = null)
 	{
 		$query = "UPDATE ip SET ip.block = 'block', ip.countblock = $_count_block WHERE ip.id = $_id LIMIT 1";
-		$result = \dash\db::query($query, 'visitor');
+		$result = \dash\db::query($query, self::ip_fuel());
 		return $result;
 	}
 
@@ -48,7 +66,7 @@ class ip
 	public static function set_unblock($_id, $_count_block = null)
 	{
 		$query = "UPDATE ip SET ip.block = 'unblock', ip.countblock = $_count_block WHERE ip.id = $_id LIMIT 1";
-		$result = \dash\db::query($query, 'visitor');
+		$result = \dash\db::query($query, self::ip_fuel());
 		return $result;
 	}
 
@@ -56,7 +74,7 @@ class ip
 	public static function set_unknown($_id, $_count_block = null)
 	{
 		$query = "UPDATE ip SET ip.block = 'unknown', ip.countblock = $_count_block WHERE ip.id = $_id LIMIT 1";
-		$result = \dash\db::query($query, 'visitor');
+		$result = \dash\db::query($query, self::ip_fuel());
 		return $result;
 	}
 
@@ -68,7 +86,7 @@ class ip
 		if($set)
 		{
 			$query = " INSERT INTO `ip` SET $set ";
-			if(\dash\db::query($query, 'visitor'))
+			if(\dash\db::query($query, self::ip_fuel()))
 			{
 				return \dash\db::insert_id();
 			}
@@ -83,76 +101,25 @@ class ip
 		}
 	}
 
-	private static function ready_to_sql($_and, $_or, $_order_sort = null, $_meta = [])
-	{
-		$where = null;
-		$q     = [];
-
-		if($_and)
-		{
-			$_and = implode(' AND ', $_and);
-			$q[] = "$_and";
-
-		}
-
-		if($_or)
-		{
-			$_or = implode(' OR ', $_or);
-			$q[] = "($_or)";
-		}
-
-		if($q)
-		{
-			$where = 'WHERE '. implode(" AND ", $q);
-		}
-
-		$order = null;
-		if($_order_sort && is_string($_order_sort))
-		{
-			$order = $_order_sort;
-		}
-
-		$pagination = null;
-		if(array_key_exists('pagination', $_meta))
-		{
-			$pagination = $_meta['pagination'];
-		}
-
-		$limit = null;
-		if(array_key_exists('limit', $_meta))
-		{
-			$limit = $_meta['limit'];
-		}
-
-		return
-		[
-			'where'      => $where,
-			'order'      => $order,
-			'pagination' => $pagination,
-			'limit'      => $limit,
-		];
-	}
-
-
 
 
 
 	public static function list($_and, $_or, $_order_sort = null, $_meta = [])
 	{
 
-		$q = self::ready_to_sql($_and, $_or, $_order_sort, $_meta);
+		$q = \dash\db\config::ready_to_sql($_and, $_or, $_order_sort, $_meta);
 
 		$pagination_query = "SELECT COUNT(*) AS `count` FROM ip $q[where] ";
 
 		$limit = null;
 		if($q['pagination'] !== false)
 		{
-			$limit = \dash\db\mysql\tools\pagination::pagination_query($pagination_query, $q['limit'], 'visitor');
+			$limit = \dash\db\mysql\tools\pagination::pagination_query($pagination_query, $q['limit'], self::ip_fuel());
 		}
 
 		$query = "SELECT ip.* FROM ip $q[where] $q[order] $limit ";
 
-		$result = \dash\db::get($query, null, false, 'visitor');
+		$result = \dash\db::get($query, null, false, self::ip_fuel());
 
 		return $result;
 	}
