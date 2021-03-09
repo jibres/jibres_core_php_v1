73f363fd95be60305e41259b3fa229136b64fad9
Reza <rm.biqarar@gmail.com>
2021-03-09 13:59:25 +0330

    Check domain count

--- content/domains/search/view.php
+++ content/domains/search/view.php
@@ -17,9 +17,47 @@ class view
 		if($q)
 		{
 			\dash\data::myDomain($q);
+
+			self::check_limit();
+
 			$info = \lib\app\domains\check::multi_check($q);
 			\dash\data::infoResult($info);
 		}
+	}
+
+
+	private static function check_limit()
+	{
+
+		$check_log           = [];
+		$check_log['caller'] = 'check_domain_request';
+
+		$this_hour           = date("Y-m-d H:i:s", (time() - (60*60)));
+
+		if(\dash\user::id())
+		{
+			$check_log['from']   = \dash\user::id();
+
+			$get_count_log = \dash\db\logs::count_where_date($check_log, $this_hour);
+
+			if($get_count_log > 50)
+			{
+				\dash\waf\ip::isolateIP(1, 'check_domain_request per user');
+			}
+		}
+		else
+		{
+			$check_log['ip_id']   = \dash\utility\ip::id();
+
+			$get_count_log = \dash\db\logs::count_where_date($check_log, $this_hour);
+
+			if($get_count_log > 50)
+			{
+				\dash\waf\ip::isolateIP(2, 'check_domain_request per ip');
+			}
+		}
+
+		\dash\log::set('check_domain_request');
 	}
 }
 ?>
