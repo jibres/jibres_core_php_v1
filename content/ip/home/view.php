7a7bcb9a86425ee1bd85e651fcb3ea97ec6a3a42
Reza <rm.biqarar@gmail.com>
2021-03-09 13:51:51 +0330

    Check ip detail limit

--- content/ip/home/view.php
+++ content/ip/home/view.php
@@ -13,6 +13,9 @@ class view
 		if(\dash\url::child())
 		{
 			$myIp = \dash\url::child();
+
+			// only check limit if try to find other ip
+			self::check_limit();
 		}
 
 		if(\dash\validate::ip($myIp, false))
@@ -48,6 +51,41 @@ class view
 			// invalid ip
 			\dash\header::status(400);
 		}
+	}
+
+
+	private static function check_limit()
+	{
+
+		$check_log           = [];
+		$check_log['caller'] = 'ip_fetch_request';
+
+		$this_hour           = date("Y-m-d H:i:s", (time() - (60*60)));
+
+		if(\dash\user::id())
+		{
+			$check_log['from']   = \dash\user::id();
+
+			$get_count_log = \dash\db\logs::count_where_date($check_log, $this_hour);
+
+			if($get_count_log > 50)
+			{
+				\dash\waf\ip::isolateIP(2, 'ip_fetch_request per user');
+			}
+		}
+		else
+		{
+			$check_log['ip_id']   = \dash\utility\ip::id();
+
+			$get_count_log = \dash\db\logs::count_where_date($check_log, $this_hour);
+
+			if($get_count_log > 50)
+			{
+				\dash\waf\ip::isolateIP(2, 'ip_fetch_request per ip');
+			}
+		}
+
+		\dash\log::set('ip_fetch_request');
 	}
 }
 ?>
