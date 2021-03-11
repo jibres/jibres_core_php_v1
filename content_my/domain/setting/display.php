c253502585c992795dcf6aeb5fa9af3b83aee460
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-12 03:13:35 +0330

    save

--- content_my/domain/setting/display.php
+++ content_my/domain/setting/display.php
@@ -1,37 +1,9 @@
-<section class="f" data-option='cms-thumb-ratio'>
-  <div class="c8 s12">
-    <div class="data">
-      <h3><?php echo T_("Nameservers");?></h3>
-      <div class="body">
-        <p><?php echo T_("Nameservers define your domain's current DNS provider. With Jibres, you can either use our Free DNS or DNS provided with domain registration and hosting services.");?></p>
-      </div>
-    </div>
-  </div>
-  <div class="c4 s12">
-    <div class="action">
-      <a class="btn primary" href="">Update Name Serves</a>
-    </div>
-  </div>
-  <table class="tbl1 minimal">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Value</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Nameserver 1</td>
-        <td>anna.ns.cloudflare.com</td>
-      </tr>
-      <tr>
-        <td>Nameserver 2</td>
-        <td>damon.ns.cloudflare.com</td>
-      </tr>
-    </tbody>
-  </table>
-</section>
 <?php
+
+require_once "elements/nameservers.php";
+
+
+
 
 
 
