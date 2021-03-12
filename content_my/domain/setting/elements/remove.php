c3686af7246a9829f531bfc27e8785e039fae9e7
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-12 19:13:14 +0330

    save

--- content_my/domain/setting/elements/remove.php
+++ content_my/domain/setting/elements/remove.php
@@ -2,9 +2,9 @@
 <section class="f" data-option='domain-remove'>
   <div class="c8 s12">
     <div class="data">
-      <h3><?php echo T_("Remove this domain from your account");?></h3>
+      <h3 class="mB0-f"><?php echo T_("Remove domain");?></h3>
       <div class="body">
-
+        <p><?php echo T_("Do you want to remove this domain from your account?");?></p>
       </div>
     </div>
   </div>
