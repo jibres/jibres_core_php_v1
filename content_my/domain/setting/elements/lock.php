e8aa8d2dbf16deeae88f2891a4d8026343360161
Reza <rm.biqarar@gmail.com>
2021-03-12 12:43:32 +0330

    Fix domain transfer lock page

--- content_my/domain/setting/elements/lock.php
+++ content_my/domain/setting/elements/lock.php
@@ -1,5 +1,3 @@
-
-
 <section class="f" data-option='domain-lock'>
   <div class="c8 s12">
     <div class="data">
@@ -9,13 +7,9 @@
       </div>
     </div>
   </div>
-  <form class="c4 s12" method="post" data-patch>
-    <input type="hidden" name="set_defaultcomment" value="1">
+  <div class="c4 s12">
       <div class="action">
-        <div class="switch1">
-          <input id="idefaultcomment" type="checkbox" name="defaultcomment" <?php if(\dash\data::cmsSettingSaved_defaultcomment() == 'open') { echo 'checked'; } ?>>
-          <label for="idefaultcomment"></label>
-        </div>
+        <a class="btn secondary" href="<?php echo \dash\url::that(). '/transfer?domain='. \dash\request::get('domain') ?>"><?php echo T_("Manage Lock domain") ?></a>
       </div>
-  </form>
+  </div>
 </section>
--- content_my/domain/setting/transfer/display.php
+++ content_my/domain/setting/transfer/display.php
@@ -1,32 +1,32 @@
 <?php require_once (root. 'content_my/domain/setting/pageStep.php'); ?>
-<div class="f justify-center">
- <div class="c9 m12 s12">
-  <div class="cbox">
+<div class="avand-md">
+  <div class="box">
+    <div class="pad">
 
-   <p class="msg danger2"><?php echo T_("From here, you can transfer your domain name to another person. To transfer out, you will need to make sure that Domain Lock is turned OFF and get an Auth Code. After you place the request here, we'll send your Auth Code to the registrant email address specified for this domain. It may take up to 5 days for the transfer to be completed."); ?></p>
+      <p class="msg danger2"><?php echo T_("From here, you can transfer your domain name to another person. To transfer out, you will need to make sure that Domain Lock is turned OFF and get an Auth Code. After you place the request here, we'll send your Auth Code to the registrant email address specified for this domain. It may take up to 5 days for the transfer to be completed."); ?></p>
+      <?php if(\dash\data::domainDetail_alertholderaccessdeny()) {?>
+        <div class="fc-mute mB10"><?php echo T_("We do not find our contact ID in your domain details. If you want to update your domain transafe lock in jibres, you must set our contact ID (ji128-irnic) as the admin owner of your domain, or allow to all agents to access your domain. To do this, go to nic.ir and set this setting, otherwise we can not update your domain transafe lock") ?></div>
+      <?php } //endif ?>
+      <div class="f fs06">
+        <div class="c6 s12">
+          <div class="dcard x1 <?php if(\dash\data::domainDetail_lock()) { echo ' active';} ?>" data-confirm data-data='{"myaction" : "lock"}'>
+            <div class="statistic green">
+              <div class="value"><i class="sf-lock"></i></div>
+              <div class="label"><?php echo T_("Lock domain"); ?></div>
+            </div>
+          </div>
+        </div>
+        <div class="c6 s12">
+          <div class="dcard x1 <?php  if( (string) \dash\data::domainDetail_lock() === '0') { echo ' active';} ?>" data-confirm data-data='{"myaction" : "unlock"}'>
+            <div class="statistic red">
+              <div class="value"><i class="sf-unlock"></i></div>
+              <div class="label"><?php echo T_("Unlock domain"); ?></div>
+            </div>
+          </div>
+        </div>
 
-   <div class="f fs06">
-    <div class="c6 s12">
-     <div class="dcard x1 <?php if(\dash\data::domainDetail_lock()) { echo ' active';} ?>" data-confirm data-data='{"myaction" : "lock"}'>
-      <div class="statistic green">
-       <div class="value"><i class="sf-lock"></i></div>
-       <div class="label"><?php echo T_("Lock domain"); ?></div>
       </div>
-     </div>
-    </div>
-    <div class="c6 s12">
-     <div class="dcard x1 <?php  if( (string) \dash\data::domainDetail_lock() === '0') { echo ' active';} ?>" data-confirm data-data='{"myaction" : "unlock"}'>
-      <div class="statistic red">
-       <div class="value"><i class="sf-unlock"></i></div>
-       <div class="label"><?php echo T_("Unlock domain"); ?></div>
-      </div>
-     </div>
-    </div>
 
-   </div>
-
+    </div>
   </div>
- </div>
 </div>
-
-
