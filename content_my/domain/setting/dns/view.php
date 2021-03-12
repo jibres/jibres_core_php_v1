7f80f56ff76dca28d33e04bc434fce689149a403
Reza <rm.biqarar@gmail.com>
2021-03-12 11:24:21 +0330

    Advance dns setting

--- content_my/domain/setting/dns/view.php
+++ content_my/domain/setting/dns/view.php
@@ -8,9 +8,11 @@ class view
 	{
 		\dash\face::title(T_("Domain DNS"));
 
-				// btn
+		// btn
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::that(). '?domain='. \dash\request::get('domain'));
+
+		\dash\face::btnSetting(\dash\url::that(). '/dnsadvance?domain='. \dash\request::get('domain'));
 	}
 }
 ?>
---
+++ content_my/domain/setting/dnsadvance/view.php
@@ -0,0 +1,16 @@
+<?php
+namespace content_my\domain\setting\dnsadvance;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\face::title(T_("Domain Advance DNS"));
+
+				// btn
+		\dash\data::back_text(T_('Back'));
+		\dash\data::back_link(\dash\url::that(). '/dns?domain='. \dash\request::get('domain'));
+	}
+}
+?>
