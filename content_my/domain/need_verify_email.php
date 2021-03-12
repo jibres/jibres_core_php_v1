cab649bab6afa7186b186b74c53886afde7c4ae7
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-12 17:58:12 +0330

    save

--- content_my/domain/need_verify_email.php
+++ content_my/domain/need_verify_email.php
@@ -4,17 +4,19 @@ $result = '';
 
 if(\dash\data::needVerifyEmail())
 {
-    $result .= '<div class="msg info2 font-12">';
-    $result .= '<p>';
+    $result .= '<div class="msg info2 font-16">';
+    $result .= '<p class="mB5-f">';
     $result .= T_("If you want to manage this domain you must add this emails to your account and verify it");
     $result .= '</p>';
 
+    $result .= '<div class="ltr txtRa">';
     foreach (\dash\data::needVerifyEmail() as $key => $value)
     {
-    	$result .= '<a class="badge light font-12 mLa5" href="'. \dash\url::kingdom(). '/account/my/email?v=1&email='. $value. '">';
+    	$result .= '<a class="btn primary font-12 mRa5" href="'. \dash\url::kingdom(). '/account/my/email?v=1&email='. $value. '">';
     	$result .= $value;
     	$result .= '</a>';
     }
+    $result .= '</div>';
 
     $result .= '</div>';
 }
