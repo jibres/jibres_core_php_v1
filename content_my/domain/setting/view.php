1692297f41215575d13b169dac60dbaf46e0763e
Reza <rm.biqarar@gmail.com>
2021-03-12 06:00:41 +0330

    Fix title

--- content_my/domain/setting/view.php
+++ content_my/domain/setting/view.php
@@ -6,7 +6,7 @@ class view
 {
 	public static function config()
 	{
-		\dash\face::title(\dash\data::domainDetail_name());
+		\dash\face::title(T_("Domain"));
 
 		// btn
 		\dash\data::back_text(T_('Domains'));
