5b57ecaabb690d7cb026047a485e7adc51740979
Reza <rm.biqarar@gmail.com>
2021-03-13 17:06:26 +0330

    Fix nameserver

--- content_my/domain/setting/dns/display.php
+++ content_my/domain/setting/dns/display.php
@@ -21,34 +21,21 @@ if(\dash\data::domainDetail_jibres_dns())
               <div data-confirm data-data='{"jibresdns":"jibresdns"}' data-title='<?php echo T_("Are you sure to set your domain Nameserver record on Jibres Nameserver?") ?>' class="btn link"><?php echo T_("Click here") ?></div>
             </div>
           </div>
+
         <?php } //endif ?>
+
     <div class="box">
       <div class="pad">
-        <?php if(!\dash\data::domainDetail_jibres_dns()) {?>
-          <div class="msg info2 f">
-            <div class="cauto"><img class="avatar mRa10" src="<?php echo \dash\url::icon() ?>" alt="<?php echo T_("Jibres") ?>"></div>
-            <div class="c">
-              <?php echo T_("If you want to set your domain Nameserver as our jibres dns record") ?>
-              <div data-confirm data-data='{"jibresdns":"jibresdns"}' data-title='<?php echo T_("Are you sure to set your domain Nameserver record on Jibres Nameserver?") ?>' class="btn link"><?php echo T_("Click here") ?></div>
-            </div>
-          </div>
-        <?php }else{ ?>
-          <div class="msg success2">
-              <?php echo T_("Powerd by Jibres") ?>
-          </div>
-        <?php } //endif ?>
-
-
         <label for="ns1"><?php echo T_("Nameserver #1"); ?></label>
         <div class="input ltr">
           <input type="text" name="ns1" id="ns1" maxlength="50" value="<?php echo \dash\data::domainDetail_ns1(); ?>" >
-          <label class="addon"><img class="avatar" src="<?php echo \dash\url::icon() ?>"></label>
+          <?php if(\dash\data::domainDetail_jibres_dns()) {?><label class="addon"><img class="avatar" src="<?php echo \dash\url::icon() ?>"></label><?php }//endif ?>
         </div>
 
         <label for="ns2"><?php echo T_("Nameserver #2"); ?></label>
         <div class="input ltr">
           <input type="text" name="ns2" id="ns2" maxlength="50" value="<?php echo \dash\data::domainDetail_ns2(); ?>" >
-          <label class="addon"><img class="avatar" src="<?php echo \dash\url::icon() ?>"></label>
+          <?php if(\dash\data::domainDetail_jibres_dns()) {?><label class="addon"><img class="avatar" src="<?php echo \dash\url::icon() ?>"></label><?php }//endif ?>
         </div>
       </div>
       <footer class="txtRa">
