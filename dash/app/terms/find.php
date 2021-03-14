2456e4b26287ba8c211de1a9f03c09582498db8b
Reza <rm.biqarar@gmail.com>
2021-03-14 19:36:49 +0330

    Fix find tag and link tags

--- dash/app/terms/find.php
+++ dash/app/terms/find.php
@@ -6,11 +6,11 @@ class find
 {
 	public static function tag(string $_string, array $_current_tag = [])
 	{
-		$check = preg_match_all("/#(\w+)/u", $_string, $split);
+		$check = preg_match_all("/(\s|\>)#(\w+)/u", $_string, $split);
 
-		if(isset($split[1]) && is_array($split[1]))
+		if(isset($split[2]) && is_array($split[2]))
 		{
-			foreach ($split[1] as $key => $value)
+			foreach ($split[2] as $key => $value)
 			{
 				if(mb_strlen($value) < 50)
 				{
@@ -24,6 +24,30 @@ class find
 
 		return $_current_tag;
 
+	}
+
+
+
+	public static function tags_link(string $_string, array $_tags)
+	{
+		$new_string = null;
+
+		foreach ($_tags as $key => $value)
+		{
+			if(isset($value['title']) && isset($value['link']))
+			{
+				$link = '$1<a href="'. $value['link']. '"> #$2</a> ';
+
+				$new_string = preg_replace("/(\s|\>)#(\w+)/u", $link, $_string);
+			}
+		}
+
+		if($new_string === null)
+		{
+			$new_string = $_string;
+		}
+
+		return $new_string;
 	}
 }
 ?>
