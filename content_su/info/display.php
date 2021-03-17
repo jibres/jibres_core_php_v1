efd0cf720c268cb8faaf8733195e709f2f409a80
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-18 01:06:49 +0330

    save

--- content_su/info/display.php
+++ content_su/info/display.php
@@ -5,9 +5,10 @@
    	<a class="btn" target='_blank' href="<?php echo \dash\url::here(); ?>/info/php"><?php echo T_("Show PHP info"); ?></a>
  </div>
 
-<pre>php8.0-fpm php8.0-cli php8.0-common</pre>
-<pre>php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext php-curl</pre>
-<pre>libwebp-dev php-yaml php-ssh2</pre>
+<pre>apt install php8.0-fpm php8.0-cli php8.0-common</pre>
+<pre>apt install php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext</pre>
+<pre>apt install php-curl libwebp-dev php-yaml php-ssh2</pre>
+<pre>apt install libwebp-dev</pre>
 
 <div class="tblBox fs14">
 	<table class="tbl1 v3">
