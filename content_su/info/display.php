7d6e3a1b3f7f1c8d851685f80c5939a05f11d05a
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-18 01:09:49 +0330

    save

--- content_su/info/display.php
+++ content_su/info/display.php
@@ -7,7 +7,7 @@
 
 <pre>apt install php8.0-fpm php8.0-cli php8.0-common</pre>
 <pre>apt install php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext</pre>
-<pre>apt install php-curl libwebp-dev php-yaml php-ssh2</pre>
+<pre>apt install php-curl libwebp-dev php-yaml php-ssh2 php-imagick</pre>
 <pre>apt install libwebp-dev</pre>
 
 <div class="tblBox fs14">
