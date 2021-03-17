b759db47d9b03f1e29da7e349b69c12825a2b0d2
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-18 00:45:50 +0330

    remove test dump

--- dash/engine/fuel.php
+++ dash/engine/fuel.php
@@ -107,11 +107,6 @@ class fuel
 		$target = $target. '_from_'. $gethostname;
 		$result = \dash\setting\fuel::server($target);
 
-		if(\dash\url::tld() === 'xyz')
-		{
-			var_dump($target);
-			var_dump($result);
-		}
 		if(isset($result[$_requested_fuel]))
 		{
 			// return detail for master, nic, nic_log
