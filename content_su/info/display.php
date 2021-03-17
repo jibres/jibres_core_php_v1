8cd1fa8a66b1021f2b67ea032780dce0337c6c32
Reza <rm.biqarar@gmail.com>
2021-03-17 14:04:53 +0330

    Add yaml

--- content_su/info/display.php
+++ content_su/info/display.php
@@ -6,7 +6,7 @@
  </div>
 
 <pre>
-php8.0-fpm php8.0-cli php8.0-common php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext php-curl php8.0-xml libwebp-dev
+php8.0-fpm php8.0-cli php8.0-common php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext php-curl php8.0-xml libwebp-dev php-yaml
 </pre>
 <div class="tblBox fs14">
 	<table class="tbl1 v3">
