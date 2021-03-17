1f353b4ffa590594b06e06af78ebd02a5cae13ef
Reza <rm.biqarar@gmail.com>
2021-03-17 14:03:06 +0330

    Add command of php module

--- content_su/info/display.php
+++ content_su/info/display.php
@@ -3,9 +3,11 @@
   <h2><?php echo T_("Info"); ?></h2>
    	<a class="btn" target='_blank' href="<?php echo \dash\url::here(); ?>/info/server"><?php echo T_("Show server info"); ?></a>
    	<a class="btn" target='_blank' href="<?php echo \dash\url::here(); ?>/info/php"><?php echo T_("Show PHP info"); ?></a>
-
  </div>
 
+<pre>
+php8.0-fpm php8.0-cli php8.0-common php8.0-mbstring php8.0-gd php8.0-intl php8.0-xml php8.0-mysql php8.0-zip php8.0-curl php8.0-bz2 php8.0-soap php8.0-xmlrpc php8.0-gettext php-curl php8.0-xml libwebp-dev
+</pre>
 <div class="tblBox fs14">
 	<table class="tbl1 v3">
 		<?php foreach (\dash\data::phpIniInfo() as $key => $value) {?>
@@ -18,4 +20,3 @@
 	 	<?php } ?>
 	</table>
 </div>
-
