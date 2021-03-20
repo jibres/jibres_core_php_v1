0cc886b03edfa803f12c0e63829ace12cec1dbdb
Reza <rm.biqarar@gmail.com>
2021-03-20 10:43:33 +0330

    Update store detail

--- lib/db/store/update.php
+++ lib/db/store/update.php
@@ -6,7 +6,7 @@ class update
 {
 	public static function store_data($_field, $_value, $_store_id)
 	{
-		$query = "UPDATE store_data SET store_data.$_field = '$_value' WHERE store_data.id = $_store_id LIMIT 1";
+		$query  = "UPDATE store_data SET store_data.$_field = '$_value' WHERE store_data.id = $_store_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 
@@ -14,9 +14,8 @@ class update
 
 	public static function subdomain($_subdomain, $_id)
 	{
-		$date = date("Y-m-d H:i:s");
-
-		$query = "UPDATE store SET store.subdomain = '$_subdomain', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store SET store.subdomain = '$_subdomain', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
@@ -24,8 +23,17 @@ class update
 
 	public static function set_deleted($_id)
 	{
-		$date = date("Y-m-d H:i:s");
-		$query = "UPDATE store SET store.status = 'deleted', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store SET store.status = 'deleted', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
+		$result = \dash\db::query($query, 'master');
+		return $result;
+	}
+
+
+	public static function set_transfer($_id)
+	{
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store SET store.status = 'transfer', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
@@ -34,12 +42,20 @@ class update
 
 	public static function set_enable($_id)
 	{
-		$date = date("Y-m-d H:i:s");
-		$query = "UPDATE store SET store.status = 'enable', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store SET store.status = 'enable', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
 
+
+	public static function new_fuel($_id, $_new_fuel)
+	{
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store SET store.fuel = '$_new_fuel', store.datemodified = '$date' WHERE store.id = $_id LIMIT 1";
+		$result = \dash\db::query($query, 'master');
+		return $result;
+	}
 
 	public static function enterprise($_enterprise, $_id)
 	{
@@ -52,7 +68,7 @@ class update
 		{
 			$enterprise = " store_data.enterprise = NULL ";
 		}
-		$query = "UPDATE store_data SET $enterprise, store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
+		$query  = "UPDATE store_data SET $enterprise, store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 
@@ -61,8 +77,8 @@ class update
 
 	public static function storage($_storage, $_id)
 	{
-		$date = date("Y-m-d H:i:s");
-		$query = "UPDATE store_data SET store_data.storage = $_storage , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store_data SET store_data.storage = $_storage , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
@@ -70,8 +86,8 @@ class update
 
 	public static function uploadsize($_uploadsize, $_id)
 	{
-		$date = date("Y-m-d H:i:s");
-		$query = "UPDATE store_data SET store_data.uploadsize = $_uploadsize , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store_data SET store_data.uploadsize = $_uploadsize , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
@@ -80,8 +96,8 @@ class update
 
 	public static function branding($_expire_branding, $_id)
 	{
-		$date = date("Y-m-d H:i:s");
-		$query = "UPDATE store_data SET store_data.branding = '$_expire_branding' , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
+		$date   = date("Y-m-d H:i:s");
+		$query  = "UPDATE store_data SET store_data.branding = '$_expire_branding' , store_data.datemodified = '$date' WHERE store_data.id = $_id LIMIT 1";
 		$result = \dash\db::query($query, 'master');
 		return $result;
 	}
