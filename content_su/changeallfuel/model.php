b02ef509cb24ce26bd64e4063769054fb9a1aa95
Reza <rm.biqarar@gmail.com>
2021-03-21 21:49:38 +0330

    Try to fix

--- content_su/changeallfuel/model.php
+++ content_su/changeallfuel/model.php
@@ -7,21 +7,18 @@ class model
 	{
 		set_time_limit(0);
 
-		if(\dash\request::post('newfuel'))
+		$old_fuel = 'jibres101';
+		$all_store = \lib\db\store\get::all_store_fuel_detail();
+
+		foreach ($all_store as $key => $value)
 		{
-			$old_fuel = 'jibres101';
-			$all_store = \lib\db\store\get::all_store_fuel_detail();
-
-			foreach ($all_store as $key => $value)
+			if(isset($value['fuel']))
 			{
-				if(isset($value['fuel']))
+				if($value['fuel'] === $old_fuel)
 				{
-					if($value['fuel'] === $old_fuel)
-					{
-						\lib\app\store\changefuel::request($value['id'], '501');
+					\lib\app\store\changefuel::request($value['id'], '501');
 
-						\lib\app\store\changefuel::run();
-					}
+					\lib\app\store\changefuel::run();
 				}
 			}
 		}
