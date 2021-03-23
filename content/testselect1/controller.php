dd326f1cfeac685229e0224b777e727dbb081224
Reza <rm.biqarar@gmail.com>
2021-03-23 16:29:41 +0430

    Show reuslt

--- content/testselect1/controller.php
+++ content/testselect1/controller.php
@@ -9,15 +9,15 @@ class controller
 		$time = [];
 		$time['start'] = microtime(true);
 		$time['start_master_fuel'] = microtime(true);
-		\dash\db::get('SELECT 1', null, false, 'master');
+		$time['resutl_master'] = \dash\db::get('SELECT 1', null, false, 'master');
 		$time['end_master_fuel'] = round(microtime(true) - $time['start_master_fuel'], 2). ' ms';
 
 		$time['start_501_fuel'] = microtime(true);
-		\dash\db::get('SELECT 1', null, false, '501');
+		$time['resutl_501'] = \dash\db::get('SELECT 1', null, false, '501');
 		$time['end_501_fuel'] = round(microtime(true) - $time['start_501_fuel'], 2). ' ms';
 
 		$time['start_400_fuel'] = microtime(true);
-		\dash\db::get('SELECT 1', null, false, '400');
+		$time['resutl_400'] = \dash\db::get('SELECT 1', null, false, '400');
 		$time['end_400_fuel'] = round(microtime(true) - $time['start_400_fuel'], 2). ' ms';
 
 		$time['end'] = microtime(true);
