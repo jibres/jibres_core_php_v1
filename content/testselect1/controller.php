d09f0f22288d108fed11f125effbca1cfe103014
Reza <rm.biqarar@gmail.com>
2021-03-23 16:16:22 +0430

    fix test query

--- content/testselect1/controller.php
+++ content/testselect1/controller.php
@@ -9,15 +9,15 @@ class controller
 		$time = [];
 		$time['start'] = microtime(true);
 		$time['start_master_fuel'] = microtime(true);
-		\dash\db::get('SELECT NULL', null, false, 'master');
+		\dash\db::get('SELECT * FROM users ORDER BY users.id DESC Limit 1', null, false, 'master');
 		$time['end_master_fuel'] = round(microtime(true) - $time['start_master_fuel'], 2). ' ms';
 
 		$time['start_501_fuel'] = microtime(true);
-		\dash\db::get('SELECT NULL', null, false, '501');
+		\dash\db::get('SELECT * FROM users ORDER BY users.id DESC Limit 1', null, false, '501');
 		$time['end_501_fuel'] = round(microtime(true) - $time['start_501_fuel'], 2). ' ms';
 
 		$time['start_400_fuel'] = microtime(true);
-		\dash\db::get('SELECT NULL', null, false, '400');
+		\dash\db::get('SELECT * FROM users ORDER BY users.id DESC Limit 1', null, false, '400');
 		$time['end_400_fuel'] = round(microtime(true) - $time['start_400_fuel'], 2). ' ms';
 
 		$time['end'] = microtime(true);
