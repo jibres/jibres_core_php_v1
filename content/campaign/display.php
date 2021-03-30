46150918cf4377bf6865337fe764ce86d15068e5
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-31 04:03:37 +0430

    add campaign page

---
+++ content/campaign/display.php
@@ -0,0 +1,7 @@
+<div class="jibresBanner">
+ <div class="avand-lg impact zero">
+ 	<img class="block" src="<?php echo \dash\face::cover(); ?>" alt='<?php echo \dash\face::title();?>'>
+ </div>
+
+ </div>
+</div>
---
+++ content/campaign/view.php
@@ -0,0 +1,19 @@
+<?php
+namespace content\campaign;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\face::title(T_('Jibres Campaigns'));
+		\dash\face::desc(T_("We do a lot of marketing campaigns for you."));
+		// btn
+		\dash\data::back_text(T_('Home'));
+		\dash\data::back_link(\dash\url::kingdom());
+
+		// \dash\face::cover(\dash\url::cdn(). '/img/cover/Jibres-cover-campaign-1.jpg');
+
+	}
+}
+?>
