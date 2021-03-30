3fd1e76091661729aec7048eb0f837ed0d4f5f6c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-31 04:29:11 +0430

    show img

--- content/campaign/nowruz1400/view.php
+++ content/campaign/nowruz1400/view.php
@@ -6,8 +6,8 @@ class view
 {
 	public static function config()
 	{
-		\dash\face::title(T_('Jibres Campaigns'));
-		\dash\face::desc(T_("We do a lot of marketing campaigns for you."));
+		\dash\face::title(T_('Nowruz 1400 | Jibres Campaign'));
+		\dash\face::desc(T_("We have a special gift for your. 1000 .ir domain for 1000 person."));
 		// btn
 		\dash\data::back_text(T_('Home'));
 		\dash\data::back_link(\dash\url::kingdom());
