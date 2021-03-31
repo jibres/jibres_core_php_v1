7ff235fbfcb62e0a47a3113c845dd07910262bd4
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-03-31 04:52:27 +0430

    add video

--- content/campaign/nowruz1400/display.php
+++ content/campaign/nowruz1400/display.php
@@ -3,5 +3,12 @@
  	<img class="block" src="<?php echo \dash\url::cdn(); ?>/img/campaign/nowruz1400/nowruz1400-gift.jpg" alt='<?php echo \dash\face::title();?>'>
  </div>
 
+	<?php include root. "content/domains/home/domain-topBox.php"; ?>
+
+ <div class="avand-lg impact zero">
+ 	<video controls class="block">
+ 		<source src="https://cloud.talambar.ir/jb278/202103/37-3c01332203cd78404db6223b93379f62.mp4" type="video/mp4">
+ 	</video>
  </div>
+
 </div>
