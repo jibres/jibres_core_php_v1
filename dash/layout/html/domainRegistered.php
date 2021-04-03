6c2a13235f940f39e588e152e92587a73b4081ac
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-03 16:53:46 +0430

    fix msg of domains on jibres

--- dash/layout/html/domainRegistered.php
+++ dash/layout/html/domainRegistered.php
@@ -2,12 +2,10 @@
 <html lang="en" dir="ltr">
  <head>
   <meta charset="UTF-8" />
-  <title>Jibres Domain</title>
+  <title><?php echo \dash\url::domain(); ?></title>
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><![endif]-->
   <meta content="width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0, maximum-scale=1.1, user-scalable=0" name="viewport"/>
-  <meta content="Jibres is fantastic! new service is comming soon" name="description">
-  <meta content="Javad Adib | Jibres" name="author">
-  <meta content="summary_large_image" name="twitter:card"/>
+  <meta content="summary" name="twitter:card"/>
   <meta content="https://cdn.jibres.com/img/cover/Jibres-cover-bot-1.jpg?v=2" name="twitter:image"/>
   <link href="https://cdn.jibres.com/css/jibres.min.css?1585664188" rel="stylesheet"/>
   <link href="https://cdn.jibres.com/css/jibres-domain-pin.css" rel="stylesheet" type="text/css">
@@ -16,9 +14,12 @@
  </head>
  <body class="domainPin">
  	<main>
- 		<h1>Domain Name</h1>
     <h1><?php echo \dash\url::domain(); ?></h1>
+<?php if (\dash\url::tld() === 'ir') {?>
+    <h2>This domain is registered by <a target="_blank" href="https://jibres.ir">Jibres</a></h2>
+<?php } else {?>
     <h2>This domain is registered by <a target="_blank" href="https://jibres.com">Jibres</a></h2>
+<?php }?>
  	</main>
  </body>
 </html>
