0615cdd542e723e62462b0296c6a2b25e9da89c3
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-07 14:24:07 +0430

    fix website builder page

--- content_a/website/home/display.php
+++ content_a/website/home/display.php
@@ -1,44 +1,47 @@
-<nav class="items">
-  <ul>
-    <?php if(\dash\data::issetHeader()) {?>
-      <li>
-        <a class="f" href="<?php echo \dash\url::this();?>/header">
-          <div class="key"><?php echo T_('Customize header');?></div>
-          <div class="go"></div>
-        </a>
-      </li>
-    <?php }else{ ?>
-      <li>
-        <a class="f" href="<?php echo \dash\url::this();?>/header">
-          <div class="key"><?php echo T_('Choose header');?></div>
-          <div class="go"></div>
-        </a>
-      </li>
-    <?php } ?>
-     </ul>
-</nav>
+<div class="avand-md">
+  <nav class="items">
+    <ul>
+      <?php if(\dash\data::issetHeader()) {?>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/header">
+            <div class="key"><?php echo T_('Customize header');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      <?php }else{ ?>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/header">
+            <div class="key"><?php echo T_('Choose header');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      <?php } ?>
+       </ul>
+  </nav>
 
 
-<?php require_once(root. 'content_a/website/body/display.php'); ?>
+  <?php require_once(root. 'content_a/website/body/display.php'); ?>
 
 
-<nav class="items">
-  <ul>
+  <nav class="items">
+    <ul>
 
-    <?php if(\dash\data::issetFooter()) {?>
-      <li>
-        <a class="f" href="<?php echo \dash\url::this();?>/footer">
-          <div class="key"><?php echo T_('Customize footer');?></div>
-          <div class="go"></div>
-        </a>
-      </li>
-    <?php }else{ ?>
-      <li>
-        <a class="f" href="<?php echo \dash\url::this();?>/footer">
-          <div class="key"><?php echo T_('Choose footer');?></div>
-          <div class="go"></div>
-        </a>
-      </li>
-    <?php } ?>
-      </ul>
-</nav>
+      <?php if(\dash\data::issetFooter()) {?>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/footer">
+            <div class="key"><?php echo T_('Customize footer');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      <?php }else{ ?>
+        <li>
+          <a class="f" href="<?php echo \dash\url::this();?>/footer">
+            <div class="key"><?php echo T_('Choose footer');?></div>
+            <div class="go"></div>
+          </a>
+        </li>
+      <?php } ?>
+        </ul>
+  </nav>
+
+</div>
