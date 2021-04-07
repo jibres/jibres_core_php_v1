9559e97dbcca5fda08e41a5b0a1b19cb24fe7cf0
Reza <rm.biqarar@gmail.com>
2021-04-07 16:31:01 +0430

    Add design subscribe

--- content_business/home/layout/body/subscribe.php
+++ content_business/home/layout/body/subscribe.php
@@ -1,8 +1,19 @@
 <?php
 if(isset($line_detail['value']['publish']) && $line_detail['value']['publish'])
 {
-	echo '<div class="avand">';
-	echo 'Hi this is a subscribe page';
-	echo '</div>';
+	$result = '';
+	$result .= '<div class="avand-sm mTB50-f">';
+	$result .= '<form method="post" autocomplete="off" action="'. \dash\url::herer(). '/subscribe">';
+	$result .= '<label for="subscribe">'. T_("Subscribe").'</label>';
+	$result .= '<div class="input">';
+	$result .= '<input type="tel" name="mobile" placeholder="09123456789">';
+	$result .= '<button class="btn addon master">'. T_("Send") . '</button>';
+	$result .= '</div>';
+	$result .= '</form>';
+	$result .= '</div>';
+
+	echo $result;
+
+	unset($result); // to use in other moduel!
 }
 ?>
