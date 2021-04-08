fdd230a116b7c922b75c3e8c4342a9cb0ffcf79d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-08 05:55:12 +0430

    show audio before images

--- dash/layout/post/gallery-box.php
+++ dash/layout/post/gallery-box.php
@@ -2,32 +2,6 @@
 $dataRow = \dash\data::dataRow();
 if(isset($dataRow['gallery_array']) && is_array($dataRow['gallery_array']))
 {
-  echo '<div class="gallery" id="lightgallery">';
-  {
-    echo '<div class="row">';
-
-    foreach ($dataRow['gallery_array'] as $key => $myUrl)
-    {
-      if(isset($myUrl['path']))
-      {
-        $endUrl = substr($myUrl['path'], -4);
-        if(in_array($endUrl, ['.jpg', '.png', '.gif', '.webp']))
-        {
-          echo '<div class="c-xs-6 c-sm-6 c-md-4 c-lg-3 c-xxl-2">';
-          {
-            echo '<a data-action href="'. $myUrl['path'].'" data-fancybox="productGallery">';
-            echo '<img src="'. \dash\fit::img($myUrl['path'], 460). '" alt="'. \dash\data::dataRow_title(). '">';
-            echo '</a>';
-          }
-          echo '</div>';
-        }
-      }
-    }
-    echo '</div>';
-  }
-  echo '</div>';
-
-
 
   echo '<div class="gallery2">';
   foreach ($dataRow['gallery_array'] as $key => $myUrl)
@@ -48,6 +22,32 @@ if(isset($dataRow['gallery_array']) && is_array($dataRow['gallery_array']))
         echo '<a href="'. $myUrl['path'] .'" class="btn lg mT25 primary">'. T_("Download"). ' '. T_("PDF"). '</a>';
       }
     }
+  }
+  echo '</div>';
+
+
+  echo '<div class="gallery" id="lightgallery">';
+  {
+    echo '<div class="row">';
+
+    foreach ($dataRow['gallery_array'] as $key => $myUrl)
+    {
+      if(isset($myUrl['path']))
+      {
+        $endUrl = substr($myUrl['path'], -4);
+        if(in_array($endUrl, ['.jpg', '.png', '.gif', '.webp']))
+        {
+          echo '<div class="c-xs-6 c-sm-6 c-md-4 c-lg-3">';
+          {
+            echo '<a data-action href="'. $myUrl['path'].'" data-fancybox="productGallery">';
+            echo '<img src="'. \dash\fit::img($myUrl['path'], 460). '" alt="'. \dash\data::dataRow_title(). '">';
+            echo '</a>';
+          }
+          echo '</div>';
+        }
+      }
+    }
+    echo '</div>';
   }
   echo '</div>';
 }
