a28b844ff0c6ca6a767dc33cfba7565f26120fec
Reza <rm.biqarar@gmail.com>
2021-04-09 15:52:44 +0430

    Fix all ratio image

--- content_su/fix/view.php
+++ content_su/fix/view.php
@@ -11,7 +11,7 @@ class view
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
 
-
+		self::image_ratio();
 
 	}
 
@@ -19,6 +19,9 @@ class view
 
 	private static function image_ratio()
 	{
+		set_time_limit(0);
+
+
 		$list = \lib\db\store\get::all_store_fuel_detail();
 
 		$setting = [];
@@ -42,50 +45,36 @@ class view
 				foreach ($resutl as $one_file)
 				{
 					$dir = 'talambar_cloud/'.$one_file['path'];
-					$scp_path = \dash\scp::get_streams($dir);
-					var_dump($dir);
-					var_dump(\dash\scp::check_dir('/'.$dir));
-					var_dump(getimagesize($scp_path));
 
-					var_dump($scp_path);exit();
-					var_dump($one_file);exit();
+					$tmp_dir = '/tmp/myTempImageToGetDetail.reza.image';
 
-					$query = "	SELECT *  FROM productcompany where productcompany.id  = '$one_product[company_id]' LIMIT 1 ";
-					$company_detail = \dash\db::get($query, null, true, $value['fuel'], ['database' => $dbname]);
-
-					if(isset($company_detail['title']))
+					$resv = \dash\scp::recv($dir, $tmp_dir);
+					if($resv)
 					{
-						$query = "	SELECT *  FROM producttag where producttag.title  = '$company_detail[title]' LIMIT 1 ";
-						$tag_detail = \dash\db::get($query, null, true, $value['fuel'], ['database' => $dbname]);
+						$ratio_detail = \dash\utility\image::get_ratio($tmp_dir, true);
 
-						if(isset($tag_detail['id']))
+						$update_file = [];
+
+						if(isset($ratio_detail['height']))
 						{
-							$tag_id = $tag_detail['id'];
+							$update_file['height'] = $ratio_detail['height'];
 						}
-						else
+
+						if(isset($ratio_detail['width']))
 						{
-							$date = date("Y-m-d H:i:s");
-							$slug = \dash\validate::slug($company_detail['title']);
-							$query = " INSERT INTO producttag SET producttag.title = '$company_detail[title]', producttag.slug = '$slug', producttag.status = 'enable', producttag.datecreated = '$date' ";
+							$update_file['width'] = $ratio_detail['width'];
+						}
+
+						if(isset($ratio_detail['ratio']))
+						{
+							$update_file['ratio'] = $ratio_detail['ratio'];
+						}
+
+						if(!empty($update_file))
+						{
+							$set = \dash\db\config::make_set($update_file);
+							$query = " UPDATE  files SET $set WHERE files.id = $one_file[id] LIMIT 1 ";
 							\dash\db::query($query, $value['fuel'], ['database' => $dbname]);
-							$tag_id = \dash\db::insert_id();
-						}
-
-
-						if($tag_id)
-						{
-							$query = "	SELECT *  FROM producttagusage where producttagusage.producttag_id  = '$tag_id' AND producttagusage.product_id = '$one_product[id]' LIMIT 1 ";
-							$tag_usage_detail = \dash\db::get($query, null, true, $value['fuel'], ['database' => $dbname]);
-
-							if(isset($tag_usage_detail['product_id']))
-							{
-								// this product have this tag
-							}
-							else
-							{
-								$query = " INSERT INTO producttagusage SET producttagusage.producttag_id = '$tag_id', producttagusage.product_id = '$one_product[id]' ";
-								\dash\db::query($query, $value['fuel'], ['database' => $dbname]);
-							}
 						}
 					}
 				}
