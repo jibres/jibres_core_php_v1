9c74699fd6ed18e19e5fb89e51e1318c07c0bc4e
Reza <rm.biqarar@gmail.com>
2021-04-12 15:26:09 +0430

    Show subdomain

--- content_a/setting/domain/display.php
+++ content_a/setting/domain/display.php
@@ -1,16 +1,23 @@
-<?php
-
-$domain = (\dash\url::tld() === 'com') ? '.myjibres.com': '.jibres.store';
-
-?>
+<?php $domain = (\dash\url::tld() === 'com') ? '.myjibres.com': '.jibres.store'; ?>
 <div class="avand-md">
-  <?php if(\dash\data::dataTable()){ ?>
   <nav class="items">
     <ul>
-      <?php foreach (\dash\data::dataTable() as $key => $value) {?>
       <li>
-          <a class="f item" href="<?php echo \dash\url::that(). '/manage?domain='. a($value, 'domain'); ?>">
-            <div class="key"><?php echo a($value, 'domain'); ?>
+        <a class="f item" href="<?php  echo 'https://'. \lib\store::detail('subdomain'). $domain; ?>" target='_blank'>
+          <div class="key"><?php echo \lib\store::detail('subdomain'). $domain; ?></div>
+          <div class="value"><?php echo T_("Connected"); ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <?php if(\dash\data::dataTable()){ ?>
+    <nav class="items">
+      <ul>
+        <?php foreach (\dash\data::dataTable() as $key => $value) {?>
+          <li>
+            <a class="f item" href="<?php echo \dash\url::that(). '/manage?domain='. a($value, 'domain'); ?>">
+              <div class="key"><?php echo a($value, 'domain'); ?>
               <?php if(a($value, 'master')) {?>
                 <span class="badge rounded success2"><?php echo T_("Master domain") ?></span>
               <?php } //endif ?>
@@ -18,14 +25,14 @@ $domain = (\dash\url::tld() === 'com') ? '.myjibres.com': '.jibres.store';
             <div class="value"><?php echo a($value, 'tstatus'); ?></div>
             <div class="go"></div>
           </a>
-      </li>
+        </li>
       <?php } //endfor ?>
     </ul>
   </nav>
-      <?php } // endif ?>
+<?php } // endif ?>
 </div>
 <?php
 \dash\utility\pagination::html();
 echo '<br>';
-  require_once('add/display.php');
+require_once('add/display.php');
 ?>
