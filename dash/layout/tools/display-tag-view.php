0cdf9ec1f61ba7b3c635f3e0997ec990eaf98544
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-13 17:42:03 +0430

    fix show tumb

--- dash/layout/tools/display-tag-view.php
+++ dash/layout/tools/display-tag-view.php
@@ -1,4 +1,4 @@
-<div class="avand">
+<div class="avand-md">
 <?php
 
   $myPostByThisCat = \dash\app\posts\search::by_tag_id(\dash\data::dataRow_id());
@@ -10,9 +10,9 @@
     foreach ($myPostByThisCat as $key => $value)
     {
       echo "<section class='f'>";
-      if(isset($value['meta']['thumb']))
+      if(isset($value['thumb']))
       {
-        echo "<div class='cauto s12 pRa10 txtC'><a href='$value[link]'><img src='". $value['meta']['thumb']. "' alt='$value[title]' width='100px'></a></div>";
+        echo "<div class='cauto s12 pRa10 txtC'><a href='$value[link]'><img src='". $value['thumb']. "' alt='$value[title]' width='100px'></a></div>";
       }
       echo "<div class='c s12'><h3><a href='$value[link]'>$value[title]</a></h3><p>$value[excerpt]</p></div>";
 
