2969803658bd3d08402ff27d4eb6086829c5f84e
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-04-13 17:56:22 +0430

    style post list

--- dash/layout/tools/display-tag-view.php
+++ dash/layout/tools/display-tag-view.php
@@ -1,26 +1,31 @@
-<div class="avand-md">
+<div class="postBlock">
+
+ <div class="avand-md">
 <?php
 
   $myPostByThisCat = \dash\app\posts\search::by_tag_id(\dash\data::dataRow_id());
 
   if($myPostByThisCat)
   {
-    echo "<article class='postListPreview'>";
+    echo "<article class='postList'>";
 
     foreach ($myPostByThisCat as $key => $value)
     {
+      echo "<div class='text'>";
       echo "<section class='f'>";
       if(isset($value['thumb']))
       {
-        echo "<div class='cauto s12 pRa10 txtC'><a href='$value[link]'><img src='". $value['thumb']. "' alt='$value[title]' width='100px'></a></div>";
+        echo "<div class='cauto s12 pRa10 txtC'><a href='$value[link]'><img class='box100' src='". $value['thumb']. "' alt='$value[title]'></a></div>";
       }
       echo "<div class='c s12'><h3><a href='$value[link]'>$value[title]</a></h3><p>$value[excerpt]</p></div>";
 
       echo "</section>";
+      echo "</div>";
     }
     echo "</article>";
 
     \dash\utility\pagination::html();
   }
 ?>
+ </div>
 </div>
