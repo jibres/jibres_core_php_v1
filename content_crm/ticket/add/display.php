eaae398e2663599f317537278ce3cb3eb1073800
Reza <rm.biqarar@gmail.com>
2021-05-09 12:12:26 +0430

    Fix upload ticket

--- content_crm/ticket/add/display.php
+++ content_crm/ticket/add/display.php
@@ -13,7 +13,7 @@
         </div>
         <textarea class="txt" name="content" rows="3" <?php \dash\layout\autofocus::html() ?> placeholder='<?php echo T_("Type here ...") ?>'></textarea>
 
-        <div class="mT10" data-uploader data-name='file' data-file-max-size='<?php echo \dash\data::maxFileSize() ?>'>
+        <div class="mT10" data-uploader data-name='file' data-ratio-free data-file-max-size='<?php echo \dash\data::maxFileSize() ?>'>
           <input type="file"  id="file1">
           <label for="file1"><?php echo T_('Drag &amp; Drop your files or Browse'); ?></label>
         </div>
