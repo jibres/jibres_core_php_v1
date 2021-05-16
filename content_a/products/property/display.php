82ea3b596b31c93c1c4c8170f64ad05df97979b6
Reza <rm.biqarar@gmail.com>
2021-05-16 17:25:20 +0430

    Update translate

--- content_a/products/property/display.php
+++ content_a/products/property/display.php
@@ -34,7 +34,7 @@ require_once(root. 'content_a/products/productName.php');
                     <div class="item f">
                       <div class="key">
                         <div class="row">
-                          <?php if(a($value, 'value') || a($value, 'value') == '0') {?>
+                          <?php if((a($value, 'value') || a($value, 'value') == '0') && !a($value, 'lock')) {?>
                             <div class="cauto handle" data-handle><i class="sf-sort"></i></div>
                           <?php }else{ ?>
                             <i class="sf-sort disabled"></i>
