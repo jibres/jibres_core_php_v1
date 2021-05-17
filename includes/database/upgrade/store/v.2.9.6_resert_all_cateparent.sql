d6dbc7c5b37f0aa537abcba4282c63081761249a
Reza <rm.biqarar@gmail.com>
2021-05-17 10:17:39 +0430

    Update old tag

--- includes/database/upgrade/store/v.2.9.6_resert_all_cateparent.sql
+++ includes/database/upgrade/store/v.2.9.6_resert_all_cateparent.sql
@@ -1,1 +1,2 @@
 UPDATE jibres_XXXXXXX.productcategory SET productcategory.parent1 = NULL, productcategory.parent2 = NULL, productcategory.parent3 = NULL, productcategory.parent4 = NULL WHERE 1;
+UPDATE jibres_XXXXXXX.productcategory SET productcategory.firstlevel = 1 WHERE productcategory.showonwebsite = 1;
