1588c1019d660ddfc03801cf48d107fe0e33c9de
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-23 14:52:53 +0430

    add design to businessplan page

--- content/business_plan/display.php
+++ content/business_plan/display.php
@@ -1,40 +1,98 @@
 <div class="jibresBanner">
- <div class="avand-md impact zero">
-  <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/bg/jibres-investment-1.jpg" alt='<?php echo T_("Jibres Investment Opportunity");?>'>
- </div>
 
   <section class="avand-md impact font-16a">
    <p><?php echo T_("We have designed this page to answer your questions that you may be interested in investing in Jibres. These are just some of the questions that investors want to know about Jibres. Of course, you may have more and specific questions, so do not hesitate to contact us. We mentioned the version and the date of editing. So reading this page once will probably not be enough.") ?></p>
 
    <div class="row msg info2 mT25 mB0-f font-14">
      <div class="c-auto"><?php echo T_("Version"). ' '. \dash\fit::number(1, false); ?></div>
-     <div class="c txtRa"><?php echo T_("Last Update"). ' '. \dash\fit::date("2021-05-05"); ?></div>
+     <div class="c txtRa"><?php echo T_("Last Update"). ' '. \dash\fit::date("2021-05-23"); ?></div>
    </div>
 
-   <div class="mT25 txtC">
-    <a class="btn master" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1.pdf"><?php echo T_("Download Jibres Business Plan") ?></a>
-   </div>
  </section>
 
+ <div class="avand txtC mB50">
+  <div class="row">
+
+    <div class="c-xs-12 c-sm-6 c-md-4">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/pitch-deck/v1/Jibres-PitchDeck-v1.pdf">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/pitchdeck.svg" alt="Jibres PitchDeck"></div>
+          <h2 class="title"><?php echo T_("Pitch Deck"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Brief but informative overview of Jibres") ?></div>
+        </div>
+      </a>
+    </div>
+    <div class="c-xs-12 c-sm-6 c-md-4">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/financial/Jibres-Financial-Projection-v1.xlsx">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/financial-growth.svg" alt="Jibres "></div>
+          <h2 class="title"><?php echo T_("Financial Projection"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Forecast of future revenues and expenses") ?></div>
+        </div>
+      </a>
+    </div>
+    <div class="c-xs-12 c-sm-6 c-md-4">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/pitch-deck/v1/Jibres-PitchDeck-v1.pptx">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/pitchdeck-presentation.svg" alt="Jibres PitchDeck Presentation"></div>
+          <h2 class="title"><?php echo T_("Pitch Deck Slides"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Presentation that helps potential investors") ?></div>
+        </div>
+      </a>
+    </div>
+
+    <div class="c-xs-12 c-sm-6 c-md-4 mB-10">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-OnePage.jpg">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/onepage-bp.svg" alt="Jibres OnePage Business Plan"></div>
+          <h2 class="title"><?php echo T_("One Page Business Plan"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Essential information about Jibres strategy") ?></div>
+        </div>
+      </a>
+    </div>
+
+    <div class="c-xs-12 c-sm-6 c-md-4 mB-10">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-SWOT.jpg">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/swot.svg" alt="Jibres SWOT"></div>
+          <h2 class="title"><?php echo T_("SWOT Analysis"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Strengths, Weaknesses, Opportunities, and Threats") ?></div>
+        </div>
+      </a>
+    </div>
+
+    <div class="c-xs-12 c-sm-6 c-md-4">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1.pdf">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/bp.svg" alt="Jibres Business Plan"></div>
+          <h2 class="title"><?php echo T_("Business Plan"); ?></h2>
+          <div class="fc-mute"><?php echo T_("A business plan is more than a document") ?></div>
+        </div>
+      </a>
+    </div>
+
+    <div class="c-xs-12 c-sm-6 c-md-4">
+      <a class="box card1" target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-model-canvas/jibres-business-model-canvas-v1.jpg">
+        <div class="body maxPad">
+          <div class="imgBox"><img class="img50p" src="<?php echo \dash\url::cdn(); ?>/img/page/bp/business-model-canvas.svg" alt="Jibres Business Plan"></div>
+          <h2 class="title"><?php echo T_("Business Model Canvas"); ?></h2>
+          <div class="fc-mute"><?php echo T_("Strategic management tool to define concept") ?></div>
+        </div>
+      </a>
+    </div>
+
+  </div>
+ </div>
+
+
+
+ <div class="avand-md impact zero">
+  <img class="block" src="<?php echo \dash\url::cdn(); ?>/img/bg/jibres-investment-1.jpg" alt='<?php echo T_("Jibres Investment Opportunity");?>'>
+ </div>
 <?php require_once "display-company-info.php"; ?>
 
- <div class="avand-lg impact zero">
-  <a target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-model-canvas/jibres-business-model-canvas-v1.jpg">
-   <img class="block" src="<?php echo \dash\url::cdn(); ?>/business/business-model-canvas/jibres-business-model-canvas-v1.jpg" alt='<?php echo T_("Jibres Business Model Canvas"); ?>'>
-  </a>
- </div>
 
- <div class="avand-lg impact zero">
-  <a target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-OnePage.jpg">
-   <img class="block" src="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-OnePage.jpg" alt='<?php echo T_("Jibres One Page Business Plan"); ?>'>
-  </a>
- </div>
 
- <div class="avand-lg impact zero">
-  <a target="_blank" href="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-SWOT.jpg">
-   <img class="block" src="<?php echo \dash\url::cdn(); ?>/business/business-plan/Jibres-BusinessPlan-v1-SWOT.jpg" alt='<?php echo T_("Jibres SWOT"); ?>'>
-  </a>
- </div>
 
  <div class="avand-md impact zero">
   <img class="block" src="<?php echo \dash\face::cover(); ?>" alt='<?php echo T_("Invest in Jibres"); ?>'>
