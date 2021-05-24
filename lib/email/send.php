b4ef5f1af94a6adb1451d163743b422ce9707121
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-24 18:00:06 +0430

    add test function

--- lib/email/send.php
+++ lib/email/send.php
@@ -9,5 +9,28 @@ class send
 	{
 		return \lib\email\sendgrid::send($_args);
 	}
+
+
+	public static function test($_to)
+	{
+		$url = \dash\url::kingdom(). '/enter/verifyemail/'. 123;
+		$body = '';
+		$body .= '<p>';
+		$body .= T_("To confirm your email in Jibres"). ' ';
+		$body .= '<a href="'.$url.'" target="_blank" clicktracking=off >'. T_("Click here"). '</a>';
+		$body .= '</p>';
+
+		$email =
+		[
+			'to'       => $_to,
+			'body'     => $body,
+			'template' => 'html',
+			'subject'  => T_("Verify your mail"),
+		];
+
+		$send = \lib\email\send::send($email);
+
+		return $send;
+	}
 }
 ?>
