7215278e97a9505a4d2767932a5646509d6de0d2
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-24 18:51:34 +0430

    add link to love

--- content_love/home/display.php
+++ content_love/home/display.php
@@ -128,5 +128,14 @@
 	</div>
 
 
+	<div class="c4 s12">
+		<a class="dcard x1" href='<?php echo \dash\url::this(); ?>/email'>
+			<div class="statistic purple">
+				<div class="value"><i class="sf-envelope"></i></div>
+				<div class="label"><?php echo T_("Email"); ?></div>
+			</div>
+		</a>
+	</div>
+
 </div>
 
