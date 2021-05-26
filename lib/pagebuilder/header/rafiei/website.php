3706abeaa8bd9c8fd07b081c8b369f4457fddbbe
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-26 17:45:37 +0430

    transfer to tailwind

--- lib/pagebuilder/header/rafiei/website.php
+++ lib/pagebuilder/header/rafiei/website.php
@@ -1,7 +1,7 @@
- <div id='jHeaderRafiei'>
-  <a class="logo" href="<?php echo \dash\url::kingdom(); ?>">
-    <h1><?php \dash\face::site() ?></h1>
-   <img src="<?php echo \dash\url::cdn(); ?>/enterprise/rafiei/header/rafiei-header-v1.png" alt="<?php echo \dash\face::site(); ?>">
+ <div id="jHeaderRafiei" class="relative pt-6">
+  <a class="logo block mx-auto mb-3 sm:w-8/12 lg:w-5/12 max-w-lg" href="<?php echo \dash\url::kingdom(); ?>">
+    <h1 class="hidden"><?php \dash\face::site() ?></h1>
+   <img class="block" src="<?php echo \dash\url::cdn(); ?>/enterprise/rafiei/header/rafiei-header-v1.png" alt="<?php echo \dash\face::site(); ?>">
   </a>
   <?php echo \lib\pagebuilder::menu('header_menu_1'); ?>
 
