f36a89e357a6a86ddb5519ed01b98a991d62227f
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-29 09:58:57 +0430

    add some detail to preview

--- content_a/site/builder/display.php
+++ content_a/site/builder/display.php
@@ -5,11 +5,33 @@ if(\dash\data::lineSetting())
 }
 else
 {
-  echo '<div class="browserFrame">';
-  echo '<div class="toolbar">';
+  echo '<div class="browserFrame w-full h-full shadow-lg overflow-hidden rounded-t-2xl rounded-b-md flex flex-col bg-white">';
+  echo '<div class="toolbar flex-grow-0 flex-none flex content-center">';
+  {
+    // dots
+    echo '<div class="relative flex items-center space-x-3 px-5">';
+    {
+      echo '<div class="w-3 h-3 bg-gray-300 hover:bg-gray-500 transition rounded-full"></div>';
+      echo '<div class="w-3 h-3 bg-gray-300 hover:bg-gray-500 transition rounded-full"></div>';
+      echo '<div class="w-3 h-3 bg-gray-300 hover:bg-gray-500 transition rounded-full"></div>';
+    }
+    echo '</div>';
+    // address line
+    echo '<div class="relative flex flex-grow items-center px-10 bg-gray-100 hover:bg-gray-200 rounded-full my-3 text-xl text-gray-700 transition">';
+    {
+      echo \dash\url::pwd();
+    }
+    echo '</div>';
+    // zoom icon
+    echo '<div class="relative flex items-center space-x-3 px-5">';
+    {
+      echo '<div class="sf-search-minus text-gray-300 hover:text-gray-500 text-3xl transition cursor-pointer"></div>';
+    }
+    echo '</div>';
+  }
   echo '</div>';
   // echo '<iframe src="http://rafiei.local/"></iframe>';
-  echo '<iframe src="http://jibres.local/billboard"></iframe>';
+  echo '<iframe class="flex-grow w-full h-full" src="http://jibres.local/billboard"></iframe>';
   echo '</div>';
 
   // echo '<div class="avand-md">';
