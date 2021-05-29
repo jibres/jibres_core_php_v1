58e8c6caa2a70c04da6f7805d6c2e90f71da13e5
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-29 07:53:11 +0430

    fix header

--- content_a/site/layout/header.php
+++ content_a/site/layout/header.php
@@ -1,18 +1,17 @@
-<div class="h-full flex flex-wrap content-center text-lg">
+<div class="h-full flex flex-wrap content-center">
 
   <a class="backBtn px-5 w-50 text-2xl text-gray-600 hover:text-gray-400 transition" href="">
     <i class="inline-block transition-none sf-chevron-left"></i>
     <span class="inline-block transition-none"><?php echo T_("Back"); ?></span>
   </a>
+  <div class="mx-5"><?php echo T_("Homepage"); ?></div>
 
-  <div class="relative flex items-center space-x-1 px-3 my-2 bg-green-200 text-gray-900 rounded-full">
-    <div class="w-3 h-3 mx-1 bg-green-700 rounded-full animate-ping2 opacity-50"></div>
+  <div class="mx-5 relative flex items-center space-x-1 px-3 my-4 bg-green-200 text-gray-900 rounded-full leading-3 text-base">
+    <div class="w-3 h-3 mx-1 bg-green-800 rounded-full animate-ping2 opacity-50"></div>
     <div><?php echo T_("Live"); ?></div>
   </div>
 
   <div class="flex-grow"></div>
-
-
 
 
   <button class="inline-block mx-5 px-6 py-5 text-center text-white transition bg-blue-400 rounded-lg shadow hover:shadow-lg hover:bg-blue-500 focus:outline-none cursor-pointer "><?php echo T_("Publish"); ?></button>
