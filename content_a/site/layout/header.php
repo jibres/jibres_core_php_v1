b30bf4dbba90f15b60c0cbbd71860a0455c3109c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-05-29 07:56:29 +0430

    add device btn

--- content_a/site/layout/header.php
+++ content_a/site/layout/header.php
@@ -13,6 +13,11 @@
 
   <div class="flex-grow"></div>
 
+  <div class="switchDevice">
+    <i class="sf-mobile text-2xl"></i>
+    <i class="sf-monitor text-2xl"></i>
+  </div>
+
 
   <button class="inline-block mx-5 px-6 py-5 text-center text-white transition bg-blue-400 rounded-lg shadow hover:shadow-lg hover:bg-blue-500 focus:outline-none cursor-pointer "><?php echo T_("Publish"); ?></button>
 
