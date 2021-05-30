1ee8d98f6dfd4c0ae8deace45cf7176e3a960079
Reza <rm.biqarar@gmail.com>
2021-05-30 14:37:48 +0430

    Show menu in mobile

--- content_business/ticket/home/ticketList.php
+++ content_business/ticket/home/ticketList.php
@@ -1,1 +1,10 @@
-<?php      require_once(root. 'content_crm/ticket/datalist/display.php'); ?>
+<?php
+if(!\dash\data::dataTable())
+{
+	echo '<div class="msg txtC txtB">'. T_("No ticket found"). '</div>';
+}
+else
+{
+	require_once(root. 'content_crm/ticket/datalist/display.php');
+}
+?>
