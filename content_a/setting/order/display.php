52b5e55aee742c98f8e898e315b1925f22deff61
Reza <rm.biqarar@gmail.com>
2021-06-01 16:59:40 +0430

    Fix

--- content_a/setting/order/display.php
+++ content_a/setting/order/display.php
@@ -23,9 +23,12 @@
       <h3><?php echo T_("Online payment"); ?></h3>
       <div class="body">
         <p><?php echo T_("Such as Credit card, PayPal and Stripe."); ?></p>
-        <?php foreach (\dash\utility\pay\get::active_payment() as $key => $value) {?>
-          <span class=""><img class="avatar" src="<?php echo a($value, 'icon') ?>" alt="<?php echo a($value, 'title') ?>"></span>
-        <?php } //endif ?>
+        <div class="flex space-s-3 items-center">
+          <?php foreach (\dash\utility\pay\get::active_payment() as $key => $value) {?>
+           <div class="w-16"><img class="" src="<?php echo a($value, 'icon') ?>" alt="<?php echo a($value, 'title') ?>"></div>
+          <?php } //endif ?>
+        </div>
+
       </div>
     </div>
   </div>
@@ -51,9 +54,12 @@
       <h3><?php echo T_("Default payment"); ?></h3>
       <div class="body">
         <p><?php echo T_("Default payment"); ?></p>
-        <?php foreach (\dash\utility\pay\get::active_payment() as $key => $value) {  if(\lib\store::detail('default_payment') !== a($value, 'key')) { continue;} ?>
-          <span class=""><img class="avatar" src="<?php echo a($value, 'icon') ?>" alt="<?php echo a($value, 'title') ?>"></span>
-        <?php } //endif ?>
+        <div class="flex space-s-3 items-center">
+          <?php foreach (\dash\utility\pay\get::active_payment() as $key => $value) { if(\lib\store::detail('default_payment') !== a($value, 'key')) { continue;}?>
+           <div class="w-16"><img class="" src="<?php echo a($value, 'icon') ?>" alt="<?php echo a($value, 'title') ?>"></div>
+          <?php } //endif ?>
+        </div>
+
       </div>
     </div>
   </div>
