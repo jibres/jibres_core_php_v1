c88ff3788814620829c524b5480cab0658ef6ce0
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-03 23:04:53 +0430

    use background image for email bg

--- dash/email/design/design1.php
+++ dash/email/design/design1.php
@@ -30,7 +30,7 @@ table{border-collapse:collapse;}
 .mobileOnly{ display:none!important;}
 /*Hide Full-Width Image on Desktop*/
 a, a:hover{color:#a80a5a!important;text-decoration:none!important;}
-p{margin-top: 0;margin-bottom: 15px;}
+p{margin-top:0;margin-bottom:15px;}
 
 @media only screen and (max-width:650px){
  img{width:auto!important;max-width:100%!important;height:auto!important;}
@@ -46,9 +46,9 @@ p{margin-top: 0;margin-bottom: 15px;}
 }
 </style>
 </head>
-<body style="background:#f5f5ff!important;margin:0;padding:0;font-family: IRANYekan, IRANSansX, IRANSans,tahoma!important">
- <div class="content" style="width:650px;max-width:100%;margin-left:auto;margin-right:auto;margin-bottom:20px;border-top:10px solid #a80a5a;white-space:normal;padding-top:20px;background-color:#ffffff!important;border-radius: 0 0 10px 10px;overflow:hidden;">
-  <img src="<?php if(\dash\language::current() === 'fa') { echo 'https://cdn.jibres.com/logo/fa/png/Jibres-Logo-fa-5000.png'; } else { echo 'https://cdn.jibres.com/logo/en/png/Jibres-Logo-en-5000.png';}?>" alt="Jibres logo" height="60" style="display:block;margin-right:auto;margin-left:auto;margin-bottom: 20px; max-width: 200px!important;max-height: 60px!important;">
+<body style="background:#f5f5ff!important;margin:0;padding:0;font-family:IRANYekan, IRANSansX, IRANSans,tahoma!important">
+ <div class="content" style="width:650px;max-width:100%;margin-left:auto;margin-right:auto;margin-bottom:20px;border-top:10px solid #a80a5a;white-space:normal;padding-top:20px;background-color:#ffffff!important;border-radius:0 0 10px 10px;overflow:hidden;">
+  <img src="<?php if(\dash\language::current() === 'fa') { echo 'https://cdn.jibres.com/logo/fa/png/Jibres-Logo-fa-5000.png'; } else { echo 'https://cdn.jibres.com/logo/en/png/Jibres-Logo-en-5000.png';}?>" alt="Jibres logo" height="60" style="display:block;margin-right:auto;margin-left:auto;margin-bottom:20px; max-width:200px!important;max-height:60px!important;">
 
   <div style="direction:<?php echo \dash\language::dir();?>;padding-right:40px;padding-left:40px;padding-top:80px;padding-bottom:20px;overflow:hidden;background-color:#e8edfa;">
 <?php
@@ -61,7 +61,7 @@ if(isset($dataLines) && is_array($dataLines))
    switch ($line['element'])
    {
     case 'btn.green':
-     echo '<div style="text-align:center;padding-top: 40px;padding-bottom: 20px;">';
+     echo '<div style="text-align:center;padding-top:40px;padding-bottom:20px;">';
      echo '<a target="_blank"';
      echo ' href="'. $line['link']. '"';
      echo ' style="display:inline-block;border-radius:5px;color:#ffffff!important;font-size:18px;font-weight:bold;background-color:#80a555!important;padding:10px 20px;"';
@@ -90,10 +90,26 @@ elseif(isset($dataLines))
 ?>
   </div>
 
-  <img src="https://cdn.jibres.com/email/wave1.png" alt="Jibres Email Wave1" style="display:block;width:100%; margin-right:auto;margin-left:auto; max-width: 100%!important;max-height: 366px!important;">
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+  <tr>
+    <td background="https://cdn.jibres.com/email/wave1.png" bgcolor="#e8edfa" valign="top" style="max-height:210px!important;min-height:150px!important;height:210px;background-position:center;background-size:cover;background-image:url('https://cdn.jibres.com/email/wave1.png');">
+      <!--[if gte mso 9]>
+      <v:rect xmlns_v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
+        <v:fill type="tile" src="https://cdn.jibres.com/email/wave1.png" color="#e8edfa" />
+        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
+      <![endif]-->
+      <div>
+      </div>
+      <!--[if gte mso 9]>
+        </v:textbox>
+      </v:rect>
+      <![endif]-->
+    </td>
+  </tr>
+</table>
 
 <?php if(isset($dataLinesFooter)) {?>
-  <div style="direction:<?php echo \dash\language::dir();?>;padding-right:40px;padding-left:40px;padding-top:20px;padding-bottom:20px;font-size: 14px;color:#777;">
+  <div style="direction:<?php echo \dash\language::dir();?>;padding-right:40px;padding-left:40px;padding-top:20px;padding-bottom:20px;font-size:14px;color:#777;">
    <p><?php echo $dataLinesFooter; ?></p>
   </div>
 <?php }?>
