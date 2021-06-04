7cfdba46a36c63f6722e68df60cb0ae857f938b6
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-04 20:34:49 +0430

    save

--- content_love/email/model.php
+++ content_love/email/model.php
@@ -9,9 +9,8 @@ class model
 		// get to
 		$to = \dash\request::post('to');
 
-		$sendLog = \dash\email\template::verify(true, $to, 'Javad Adib', 'https://jibres.ir/about');
 		// try to send
-		// $sendLog = \dash\email\mail::sampleEmail($to);
+		$sendLog = \dash\email\template::verify(true, $to, 'Javad Adib', 'https://jibres.ir/about');
 
 		// show result
 		\dash\notif::debug($sendLog);
