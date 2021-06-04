163daa6e04e90ef2228f91c835e1dbb16185b657
Reza <rm.biqarar@gmail.com>
2021-06-04 18:37:28 +0430

    Load engine list in pagebuilder

--- dash/layout/panelBuilder/panelBuilder-main.php
+++ dash/layout/panelBuilder/panelBuilder-main.php
@@ -6,7 +6,35 @@
 	}
 	else
 	{
-		require_once \dash\layout\func::display();
+		if(\dash\data::listEngine())
+		{
+			if(\dash\data::listEngine_before() && is_string(\dash\data::listEngine_before()) && is_file(\dash\data::listEngine_before()))
+			{
+				require_once \dash\data::listEngine_before();
+			}
+
+			if(\dash\data::dataTable())
+			{
+			    require_once(core. 'layout/search/search-bar.php');
+			    require_once \dash\layout\func::display();
+			}
+			else
+			{
+			  if(\dash\data::isFiltered() || \dash\validate::search_string())
+			  {
+			    require_once(core. 'layout/search/search-bar.php');
+			    require_once(core. 'layout/search/search-empty.php');
+			  }
+			  else
+			  {
+			    require_once(core. 'layout/tools/display-start.php');
+			  }
+			}
+		}
+		else
+		{
+			require_once \dash\layout\func::display();
+		}
 	}
 ?>
 </div>
