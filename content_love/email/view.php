dce2afb10751ddfa3a5a2f9cafc219e0cc3cb4ee
Reza <rm.biqarar@gmail.com>
2021-06-04 17:41:49 +0430

    Show history link

--- content_love/email/view.php
+++ content_love/email/view.php
@@ -10,6 +10,9 @@ class view
 		// btn
 		\dash\data::back_text(T_('Back'));
 		\dash\data::back_link(\dash\url::here());
+
+		\dash\data::action_text(T_('History'));
+		\dash\data::action_link(\dash\url::this(). '/history');
 	}
 }
 ?>
