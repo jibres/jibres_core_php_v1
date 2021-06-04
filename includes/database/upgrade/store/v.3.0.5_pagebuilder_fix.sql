5a7dd9a21e7d6bea415ff31220a76fac9e42e265
Reza <rm.biqarar@gmail.com>
2021-06-04 18:37:44 +0430

    Add body and preview field to pagebuilder

---
+++ includes/database/upgrade/store/v.3.0.5_pagebuilder_fix.sql
@@ -0,0 +1,2 @@
+ALTER TABLE jibres_XXXXXXX.pagebuilder ADD `body` mediumtext NULL DEFAULT NULL AFTER `title`;
+ALTER TABLE jibres_XXXXXXX.pagebuilder ADD `preview` mediumtext NULL DEFAULT NULL AFTER `title`;
