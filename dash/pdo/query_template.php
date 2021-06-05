61a4b4245ddf8b38209d0d51e99f344b7653fb27
Reza <rm.biqarar@gmail.com>
2021-06-05 08:42:58 +0430

    Add template pdo query

--- dash/pdo/query_template.php
+++ dash/pdo/query_template.php
@@ -22,20 +22,59 @@ class query_template
 			$param[$new_key] = $value;
 		}
 
-		$query = "INSERT INTO `crm_email` SET ";
+		$query = "INSERT INTO `$_table` SET ";
 
 		$query_set = [];
 
 		foreach ($set as $key => $value)
 		{
-			$query_set[] = " crm_email.$key = $value ";
+			$query_set[] = " $_table.$key = $value ";
 		}
 
 		$query .= implode(',', $query_set);
 
 		$result = \dash\pdo::query($query, $param);
 
-		return $insert;
+		return \dash\pdo::insert_id();
+	}
+
+
+	public static function update($_table, $_args, $_id)
+	{
+		if(empty($_args))
+		{
+			return false;
+		}
+
+		$set   = [];
+		$param = [];
+
+		foreach ($_args as $key => $value)
+		{
+			$fields[]        = $key;
+			$new_key         = ':'. $key;
+			$set[$key]       = $new_key;
+			$param[$new_key] = $value;
+		}
+
+		$query = "UPDATE `$_table` SET ";
+
+		$query_set = [];
+
+		foreach ($set as $key => $value)
+		{
+			$query_set[] = " $_table.$key = $value ";
+		}
+
+		$query .= implode(',', $query_set);
+
+		$query .= " WHERE $_table.id = :_id LIMIT 1 ";
+
+		$param[':_id'] = $_id;
+
+		$result = \dash\pdo::query($query, $param);
+
+		return $result;
 	}
 }
 ?>
