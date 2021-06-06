de8512ec57fe4a64b556786b3e5498670889fb38
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-07 03:23:10 +0430

    save

--- dash/layout/panelBuilder/panelBuilder-sidebar-options.php
+++ dash/layout/panelBuilder/panelBuilder-sidebar-options.php
@@ -1,4 +1,3 @@
-<div id="siteBuilderSidebar" class="h-full flex flex-col">
 <?php
 $options_list   = \content_site\section\blog\controller::options();
 $section_detail = \dash\data::currentSectionDetail();
@@ -8,4 +7,3 @@ echo \lib\sitebuilder\options::admin_html($options_list, $section_detail);
 
  <div class="gap flex-grow"></div>
  <?php echo \lib\sitebuilder\section_tools::remove_hide_html($section_detail); ?>
-</div>
