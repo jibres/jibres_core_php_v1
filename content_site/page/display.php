0f49794caff8ab765b5ab8739d9dd1bb019f24f1
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-06 21:23:43 +0430

    save

--- content_site/page/display.php
+++ content_site/page/display.php
@@ -3,7 +3,7 @@
     <ul>
       <li>
         <a class="item f" href="<?php echo \dash\url::here() ?>/header">
-          <img src="<?php echo \dash\utility\icon::url('Header'); ?>">
+          <img class="icon" src="<?php echo \dash\utility\icon::url('Header'); ?>">
           <div class="key"><?php echo T_("Header") ?></div>
         </a>
       </li>
@@ -11,22 +11,33 @@
   </nav>
 
   <nav class="sections items">
-    <ul>
+
+    <ul data-sortable>
       <?php foreach (\dash\data::currentSectionList() as $key => $value) {?>
         <li>
           <a class="item f" href="<?php echo \dash\url::here(). '/section/'. a($value, 'preview', 'key'). \dash\request::full_get(['sid' => a($value, 'id')]); ?>">
-            <img src="<?php echo \dash\utility\icon::url('tick', 'minor'); ?>">
+            <img class="icon" src="<?php echo \dash\utility\icon::url('tick', 'minor'); ?>">
             <div class="key"><?php echo a($value, 'preview', 'key') ?></div>
 
-            <?php if(a($value, 'status') === 'draft'){ echo \dash\utility\icon::svg('hide', 'minor'); }else{ echo \dash\utility\icon::svg('view', 'minor');}  ?>
-            <?php echo \dash\utility\icon::svg('DragHandle', 'minor'); ?>
+            <?php if(a($value, 'status') === 'draft'){ ?>
+              <img class="switch" src="<?php echo \dash\utility\icon::url('hide', 'minor'); ?>">
+            <?php } else { ?>
+              <img class="switch" src="<?php echo \dash\utility\icon::url('view', 'minor'); ?>">
+            <?php } //endif ?>
+<?php if (count(\dash\data::currentSectionList()) > 1) { ?>
+            <img data-handle src="<?php echo \dash\utility\icon::url('DragHandle', 'minor'); ?>">
+<?php } ?>
           </a>
         </li>
       <?php } //endfor ?>
+    </ul>
+  </nav>
 
+  <nav class="sections items">
+    <ul>
       <li>
         <a class="item f" href="<?php echo \dash\url::here(). '/section'. \dash\request::full_get(); ?>">
-          <?php echo \dash\utility\icon::svg('add'); ?>
+          <img class="icon" src="<?php echo \dash\utility\icon::url('add'); ?>">
           <div class="key text-blue-500"><?php echo T_("Add Section") ?></div>
         </a>
       </li>
@@ -37,7 +48,7 @@
     <ul>
       <li>
         <a class="item f" href="<?php echo \dash\url::here() ?>/footer">
-          <img src="<?php echo \dash\utility\icon::url('Footer'); ?>">
+          <img class="icon" src="<?php echo \dash\utility\icon::url('Footer'); ?>">
           <div class="key"><?php echo T_("Footer") ?></div>
         </a>
       </li>
@@ -50,7 +61,7 @@
     <ul>
       <li>
         <a class="item f" href="<?php echo \dash\url::here() ?>">
-          <img src="<?php echo \dash\utility\icon::url('Settings', 'minor'); ?>">
+          <img class="icon" src="<?php echo \dash\utility\icon::url('Settings', 'minor'); ?>">
           <div class="key"><?php echo T_("Page Settings") ?></div>
           <div class="go"></div>
         </a>
