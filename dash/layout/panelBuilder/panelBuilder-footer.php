d33edb4f9a0e3cc37a08c09c5f2eb28b4118a2ff
Reza <rm.biqarar@gmail.com>
2021-06-07 19:39:07 +0430

    Fix

--- dash/layout/panelBuilder/panelBuilder-footer.php
+++ dash/layout/panelBuilder/panelBuilder-footer.php
@@ -4,12 +4,4 @@ if(is_file($myFooterFile))
 {
   require_once $myFooterFile;
 }
-else if(\dash\data::include_adminPanelBuilder() === 'siteLiveOptions')
-{
-  require_once(core. 'layout/panelBuilder/panelBuilder-footer-options.php');
-}
-else
-{
-  // we don't have footer
-}
 ?>
--- dash/layout/panelBuilder/panelBuilder-sidebar.php
+++ dash/layout/panelBuilder/panelBuilder-sidebar.php
@@ -4,12 +4,4 @@ if(is_file($sidebarFile))
 {
   require_once $sidebarFile;
 }
-else if(\dash\data::include_adminPanelBuilder() === 'siteLiveOptions')
-{
-  require_once(core. 'layout/panelBuilder/panelBuilder-sidebar-options.php');
-}
-else
-{
-  /* we don't have footer*/
-}
 ?>
