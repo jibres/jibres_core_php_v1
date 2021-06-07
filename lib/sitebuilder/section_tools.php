afe1179b3644cfc35c5cede9a9840af858baaf8d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-07 04:34:17 +0430

    fix footer of blog post

--- lib/sitebuilder/section_tools.php
+++ lib/sitebuilder/section_tools.php
@@ -14,27 +14,27 @@ class section_tools
 
 
 		$html = '';
-		$html .= '<div class="box">';
-		$html .= '<div class="pad">';
-		$html .= '<div class="row">';
-		$html .= "<div class='cauto' data-confirm data-title='$remove_title' data-data='$delete_json'>";
-		$html .= '<img class="avatar" src="'. \dash\utility\icon::url('Delete', 'minor'). '">';
+		$html .= '<div class="row w-full">';
+		$html .= '<div class="cauto">';
+		$html .= "<a tabindex=0 class='inline-block bg-gray-50 hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 hover:text-red-500 focus:text-red-600 active:text-red-700 transition p-3 rounded-lg' data-ajaxify data-confirm data-title='$remove_title' data-data='$delete_json'>";
+		$html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('Delete', 'minor'). '">';
+		$html .= '<span class="inline-block align-middle ps-2">'. T_("Remove section").'<span>';
+		$html .= '</a>';
 		$html .= '</div>';
 		$html .= '<div class="c"></div>';
-		$html .= "<div class='cauto' data-ajaxify data-data='$hide_view_json'>";
+		$html .= "<div class='cauto os' >";
+		$html .= "<a tabindex=0 class='inline-block bg-gray-50 hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 transition p-3 rounded-lg' data-ajaxify data-data='$hide_view_json'>";
 
 		if(a($_section_detail, 'status') === 'draft')
 		{
-			$html .= '<img class="avatar" src="'. \dash\utility\icon::url('hide', 'minor'). '">';
-
+			$html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('hide', 'minor'). '">';
 		}
 		else
 		{
-			$html .= '<img class="avatar" src="'. \dash\utility\icon::url('view', 'minor'). '">';
+			$html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('view', 'minor'). '">';
 		}
 
-		$html .= '</div>';
-		$html .= '</div>';
+		$html .= '</a>';
 		$html .= '</div>';
 		$html .= '</div>';
 
