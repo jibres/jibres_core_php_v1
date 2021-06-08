59bd7935d20f0078fb9763010c69bcd9c24fd615
Reza <rm.biqarar@gmail.com>
2021-06-08 13:19:34 +0430

    Update call function

--- content_site/call_function.php
+++ content_site/call_function.php
@@ -4,7 +4,15 @@ namespace content_site;
 
 class call_function
 {
-	private static function _namespace($_child = null)
+
+	/**
+	 * Get ganje namespace
+	 *
+	 * @param      string  $_child  The child
+	 *
+	 * @return     string  ( description_of_the_return_value )
+	 */
+	private static function ganje_namespace($_child = null)
 	{
 		if(!$_child)
 		{
@@ -15,15 +23,105 @@ class call_function
 	}
 
 
+	/**
+	 * Get option namespace
+	 *
+	 * @param      string  $_child  The child
+	 *
+	 * @return     string  ( description_of_the_return_value )
+	 */
+	private static function option_namespace($_option = null)
+	{
+		return '\\content_site\\options\\'.$_option;
+	}
 
 
+
+	/**
+	 * Call function
+	 *
+	 * @param      <type>  $_fn    The function
+	 * @param      array   $_args  The arguments
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	private static function _call($_fn, $_args = [])
+	{
+		if(is_callable($_fn))
+		{
+			return call_user_func($_fn, $_args);
+		}
+		else
+		{
+			if(\dash\url::isLocal())
+			{
+				var_dump(func_get_args());exit;
+			}
+			return null;
+		}
+
+	}
+
+
+	/**
+	 * Call every function you need
+	 *
+	 * @param      <type>  $_fn    The function
+	 * @param      <type>  $_args  The arguments
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function __callStatic($_fn, $_args)
+	{
+		// call option_admin_html
+		// call option_validator
+		if(substr($_fn, 0, 7) === 'option_')
+		{
+			$function = substr($_fn, 7);
+
+			$option = null;
+
+			if(isset($_args[0]))
+			{
+				$option = $_args[0];
+			}
+
+			unset($_args[0]);
+
+  			return self::_call([self::option_namespace($option), $function], ...$_args);
+		}
+		else
+		{
+			// call detail($_section)
+			if(isset($_args[0]) && is_string($_args[0]))
+			{
+				$namespace = self::ganje_namespace($_args[0]);
+			}
+			else
+			{
+				$namespace = self::ganje_namespace();
+			}
+
+			$namespace = sprintf($namespace, 'option');
+
+			return self::_call([$namespace, $_fn], ...$_args);
+		}
+
+	}
+
+
+
+	/**
+	 * Get default of section detail
+	 * by load option and get default detail
+	 */
 	public static function default($_child = null)
 	{
-		$namespace = self::_namespace($_child);
+		$namespace = self::ganje_namespace($_child);
 
 		$namespace = sprintf($namespace, 'option');
 
-		$full_option_list = call_user_func([$namespace, 'option'], 'full');
+		$full_option_list = self::_call([$namespace, 'option'], 'full');
 
 		if(isset($full_option_list['default']) && is_array($full_option_list['default']))
 		{
@@ -35,82 +133,22 @@ class call_function
 
 
 
-	public static function __callStatic($_fn, $_args)
-	{
-		if(isset($_args[0]) && is_string($_args[0]))
-		{
-			$namespace = self::_namespace($_args[0]);
-		}
-		else
-		{
-			$namespace = self::_namespace();
-		}
 
-		$namespace = sprintf($namespace, 'option');
-
-		if(is_callable([$namespace, $_fn]))
-		{
-			return call_user_func([$namespace, $_fn], ...$_args);
-		}
-		else
-		{
-			var_dump(func_get_args(), $namespace);exit;
-		}
-	}
-
-
+	/**
+	 * Call layout of every section
+	 *
+	 * @param      <type>  $_child  The child
+	 * @param      array   $_args   The arguments
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
 	public static function layout($_child = null, $_args = [])
 	{
-		$namespace = self::_namespace($_child);
+		$namespace = self::ganje_namespace($_child);
 
 		$namespace = sprintf($namespace, 'layout');
 
-		if(is_callable([$namespace, 'layout']))
-		{
-			return call_user_func([$namespace, 'layout'], $_args);
-		}
-
-		return null;
+		return self::_call([$namespace, 'layout'], $_args);
 	}
-
-
-
-
-
-
-	public static function option_admin_html($_option, $_args)
-	{
-		$fn = ['\\content_site\\options\\'. $_option, 'admin_html'];
-
-  		return call_user_func($fn, $_args);
-	}
-
-
-	public static function option_specialsave($_option, $_args)
-	{
-		$fn = ['\\content_site\\options\\'. $_option, 'specialsave'];
-
-		if(is_callable($fn))
-		{
-			return call_user_func($fn);
-		}
-		else
-		{
-			\dash\notif::error(T_("Can not save this special option"));
-			return false;
-		}
-
-	}
-
-	public static function option_validator($_option, $_args)
-	{
-		$fn = ['\\content_site\\options\\'. $_option, 'validator'];
-
-  		return call_user_func($fn, $_args);
-	}
-
-
-
-
 }
 ?>
