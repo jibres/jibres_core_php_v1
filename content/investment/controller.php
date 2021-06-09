555312b1fae54f7bf20c742c8ed30b8e2b41d75f
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-09 06:41:55 +0430

    add short link of some other investment source

--- content/investment/controller.php
+++ content/investment/controller.php
@@ -22,8 +22,21 @@ class controller
 				break;
 
 			case 'businessplan':
-				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v1.1.pdf?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.pdf?v=1');
 				break;
+
+			case 'businessplan-onepage':
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1-OnePage.jpg?v=1');
+				break;
+
+			case 'swot':
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1-SWOT.jpg?v=1');
+				break;
+
+			case 'canvas':
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-model-canvas/jibres-business-model-canvas-v1.jpg?v=1');
+				break;
+
 		}
 	}
 }
