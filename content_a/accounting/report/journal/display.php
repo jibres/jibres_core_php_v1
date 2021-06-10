941da3624e7c502c1ead11bdac7993e1fa8559ab
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-06-10 20:05:23 +0430

    some change

--- content_a/accounting/report/journal/display.php
+++ content_a/accounting/report/journal/display.php
@@ -2,16 +2,31 @@
 <?php if(!\dash\data::reportDetail()) {?>
   <div class="msg"><?php echo T_("No detail was founded") ?></div>
 <?php }else{ ?>
-    <table class="tbl1 v4 fs14">
-    <thead>
-      <tr class="">
-        <th class="collapsing">#</th>
-        <th class="collapsing"><?php echo T_("Number") ?></th>
-        <th class="collapsing"><?php echo T_("Date") ?></th>
-
-        <th><?php echo T_("Description") ?></th>
-        <th class="txtR"><?php echo T_("Debtor") ?></th>
-        <th class="txtR"><?php echo T_("Creditor") ?></th>
+<?php $pageNumber = 1; ?>
+  <div class="printArea" data-size='A4.landscape'>
+    <div class="msg f align-center txtC font-16">
+      <div class="c3"></div>
+      <div class="c6"><h2 class="txtB"><?php echo T_("General Journal"); ?></h2></div>
+      <div class="c3"><span class="inline-block w-16 h-16 rounded-full bg-gray-200 font-22"><?php echo \dash\fit::number($pageNumber); ?></span></div>
+    </div>
+    <table class="table-fixed w-full border border-indigo-600">
+    <thead class="text-center font-20">
+      <tr>
+        <th class="w-10 h-32 font-10" rowspan="2"><span class="transform rotate-90 inline-block whitespace-nowrap translate-x-5" style="--tw-rotate: 270deg;"><?php echo T_("General Journal Number") ?></span></th>
+        <th class="w-20" colspan="2"><?php echo T_("Date") ?></th>
+        <th class="w-10 font-10" rowspan="2"><span class="transform rotate-90 block whitespace-nowrap -translate-y-6" style="--tw-rotate: 270deg;"><?php echo T_("Ledger Number") ?></span></th>
+        <th class=""><?php echo T_("Description") ?></th>
+        <th class="w-10"></th>
+        <th class="w-1/6"><?php echo T_("Debtor") ?></th>
+        <th class="w-1/6"><?php echo T_("Creditor") ?></th>
+      </tr>
+      <tr>
+        <th class=""><?php echo T_("Day"); ?></th>
+        <th class=""><?php echo T_("Month"); ?></th>
+        <th class=""></th>
+        <th class=""></th>
+        <th class=""><?php echo T_("Rial"); ?></th>
+        <th class=""><?php echo T_("Rial"); ?></th>
       </tr>
     </thead>
     <tbody>
@@ -54,5 +69,6 @@
       <?php } //endif ?>
     </tbody>
   </table>
+ </div>
 
 <?php } //endif ?>
