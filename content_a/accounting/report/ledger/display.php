e6af254cf342ea4ed041edaecd102a21598982f2
Reza <rm.biqarar@gmail.com>
2021-06-11 14:22:50 +0430

    Some fix

--- content_a/accounting/report/ledger/display.php
+++ content_a/accounting/report/ledger/display.php
@@ -19,15 +19,16 @@
         <th class="w-10 h-32 font-10 border-solid border-b-2 border-e border-blue-900" rowspan="2"><span class="transform rotate-90 inline-block whitespace-nowrap translate-x-5" style="--tw-rotate: 270deg;"><?php echo T_("General Journal Number") ?></span></th>
         <th class="w-20 font-16 border-solid border-b border-e border-blue-900" colspan="2"><?php echo T_("Date") ?></th>
         <th class="border-solid border-b-2 border-e border-blue-900" rowspan="2"><?php echo T_("Explanation") ?></th>
-        <th class="w-10 border-solid border-e border-blue-900"></th>
+
         <th class="w-1/6 border-solid border-b border-e-2 border-blue-900"><?php echo T_("Debtor") ?></th>
         <th class="w-1/6 border-solid border-b border-e-2 border-blue-900"><?php echo T_("Creditor") ?></th>
+        <th class="w-20 h-32 font-10 border-solid border-b-2 border-e-2 border-blue-900" rowspan="2"><span class="transform rotate-90 inline-block whitespace-nowrap translate-x-5" style="--tw-rotate: 270deg;"><?php echo T_("Detect") ?></span></th>
         <th class="w-1/6 border-solid border-b border-blue-900"><?php echo T_("Remain") ?></th>
       </tr>
       <tr>
         <th class="border-solid border-b-2 border-blue-900 border-e font-12"><?php echo T_("Day"); ?></th>
         <th class="border-solid border-b-2 border-blue-900 border-e font-12"><?php echo T_("Month"); ?></th>
-        <th class="border-solid border-b-2 border-blue-900 border-e"></th>
+
         <th class="border-solid border-b-2 border-blue-900 font-12 border-e-2"><?php echo T_("Rial"); ?></th>
         <th class="border-solid border-b-2 border-blue-900 font-12 border-e-2"><?php echo T_("Rial"); ?></th>
         <th class="border-solid border-b-2 border-blue-900 font-12 "><?php echo T_("Rial"); ?></th>
@@ -40,19 +41,16 @@
           <td class="border-solid border-e border-blue-900 text-center"><?php echo \dash\fit::number(a($value, 'myNumber')); ?></td>
           <td class="border-solid border-e border-blue-900 text-center"><?php if(isset($value['show_date'])) { echo \dash\utility\jdate::date("j", strtotime($value['show_date'])); } ?></td>
           <td class="border-solid border-e border-blue-900 text-center"><?php if(isset($value['show_date'])) { echo \dash\utility\jdate::date("n", strtotime($value['show_date'])); } ?></td>
-
+          <td class="border-solid border-e border-blue-900 pLa5"><?php echo a($value, 'total_title') ?></td>
           <?php if(a($value, 'mode') === 'debtor') {?>
-            <td class="border-solid border-e border-blue-900 pLa5"><?php echo a($value, 'total_title') ?></td>
-            <td class="border-solid border-e border-blue-900"></td>
-            <td class="border-solid border-e-2 border-purple-700" data-copy='<?php echo a($value, 'show_value'); ?>' class="ltr txtR fc-green"><code><?php echo \dash\fit::number(a($value, 'show_value'), true, 'en') ?></code></td>
+            <td class="border-solid border-e-2 border-purple-700" data-copy='<?php echo a($value, 'show_value'); ?>' class="ltr fc-green"><code><?php echo \dash\fit::number(a($value, 'show_value'), true, 'en') ?></code></td>
             <td class="border-solid border-e-2 border-purple-700"></td>
           <?php }elseif(a($value, 'mode') === 'creditor') {?>
-            <td class="border-solid border-e border-blue-900  pRa5"><?php echo a($value, 'total_title') ?></td>
-            <td class="border-solid border-e border-blue-900"></td>
             <td class="border-solid border-e-2 border-purple-700"></td>
-            <td data-copy='<?php echo a($value, 'show_value'); ?>' class="border-solid border-e-2 border-purple-700" ltr txtR fc-red"><code><?php echo \dash\fit::number(a($value, 'show_value'), true, 'en') ?></code></td>
+            <td class="border-solid border-e-2 border-purple-700 ltr fc-red" data-copy='<?php echo a($value, 'show_value'); ?>'><code><?php echo \dash\fit::number(a($value, 'show_value'), true, 'en') ?></code></td>
           <?php } //endif ?>
-            <td class="border-solid border-e-2 border-purple-700" data-copy='<?php echo a($value, 'remain_value'); ?>' class="ltr txtR fc-green"><code><?php echo \dash\fit::number(a($value, 'remain_value'), true, 'en') ?></code></td>
+          <th class="border-solid border-e-2 border-purple-700"><?php echo a($value, 'detect_title') ?></th>
+          <td class="border-solid border-e-2 border-purple-700" data-copy='<?php echo a($value, 'remain_value'); ?>'><code><?php echo \dash\fit::number(a($value, 'remain_value'), true, 'en') ?></code></td>
 
         </tr>
       <?php } //endif ?>
