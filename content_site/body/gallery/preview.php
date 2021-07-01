fb91ee4ad1042498e88021d966743fabf746a05f
Reza <rm.biqarar@gmail.com>
2021-07-01 06:50:20 +0430

    Try to add preview list

---
+++ content_site/body/gallery/preview.php
@@ -0,0 +1,20 @@
+<?php
+namespace content_site\body\gallery;
+
+
+class preview
+{
+
+	public static function preview_1()
+	{
+		return
+		[
+			'style'     => 'style_1',
+			'container' => 'xl',
+			'height'    => 'xs',
+		];
+	}
+
+
+}
+?>
