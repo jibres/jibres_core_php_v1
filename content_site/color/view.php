cb51f6e5d112ed1b9b006e926c2b7c1c532de9d4
Reza <rm.biqarar@gmail.com>
2021-07-04 14:33:52 +0430

    Add color page

---
+++ content_site/color/view.php
@@ -0,0 +1,18 @@
+<?php
+namespace content_site\color;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\face::title(T_('Color'));
+
+		\dash\data::back_text(T_('Back'));
+		\dash\data::back_link(\dash\url::here());
+
+		\dash\data::include_adminPanelBuilder(true);
+
+	}
+}
+?>
