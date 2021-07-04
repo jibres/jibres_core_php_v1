0400aea3a32604a1c6fc18ef6d71a5dbbd67cb77
Reza <rm.biqarar@gmail.com>
2021-07-04 15:57:19 +0430

    Show color in select

--- content_site/color/controller.php
+++ content_site/color/controller.php
@@ -9,7 +9,20 @@ class controller
 		if(\dash\request::get('json'))
 		{
 			$list = color::list();
-			\dash\code::jsonBoom($list);
+
+			$new_list = [];
+
+			foreach ($list as $key => $value)
+			{
+				$new_list[] =
+				[
+					'html' => "<div class='h-10 w-full rounded ring-1 bg-$value[color]'>$value[color]</div>",
+					'id'   => $value['color'],
+				];
+			}
+
+			$result = ['results' => $new_list];
+			\dash\code::jsonBoom($result);
 		}
 	}
 }
--- content_site/options/background/background_color.php
+++ content_site/options/background/background_color.php
@@ -7,100 +7,14 @@ class background_color
 
 	private static function enum()
 	{
-		$enum   = [];
-
-		$enum[] = ['key' => 'bg-black', 'default' => true ];
-		$enum[] = ['key' => 'bg-white', ];
-		$enum[] = ['key' => 'bg-gray-50',];
-		$enum[] = ['key' => 'bg-gray-100',];
-		$enum[] = ['key' => 'bg-gray-200',];
-		$enum[] = ['key' => 'bg-gray-300',];
-		$enum[] = ['key' => 'bg-gray-400',];
-		$enum[] = ['key' => 'bg-gray-500',];
-		$enum[] = ['key' => 'bg-gray-600',];
-		$enum[] = ['key' => 'bg-gray-700',];
-		$enum[] = ['key' => 'bg-gray-800',];
-		$enum[] = ['key' => 'bg-gray-900',];
-		$enum[] = ['key' => 'bg-red-50',];
-		$enum[] = ['key' => 'bg-red-100',];
-		$enum[] = ['key' => 'bg-red-200',];
-		$enum[] = ['key' => 'bg-red-300',];
-		$enum[] = ['key' => 'bg-red-400',];
-		$enum[] = ['key' => 'bg-red-500',];
-		$enum[] = ['key' => 'bg-red-600',];
-		$enum[] = ['key' => 'bg-red-700',];
-		$enum[] = ['key' => 'bg-red-800',];
-		$enum[] = ['key' => 'bg-red-900',];
-		$enum[] = ['key' => 'bg-yellow-50',];
-		$enum[] = ['key' => 'bg-yellow-100',];
-		$enum[] = ['key' => 'bg-yellow-200',];
-		$enum[] = ['key' => 'bg-yellow-300',];
-		$enum[] = ['key' => 'bg-yellow-400',];
-		$enum[] = ['key' => 'bg-yellow-500',];
-		$enum[] = ['key' => 'bg-yellow-600',];
-		$enum[] = ['key' => 'bg-yellow-700',];
-		$enum[] = ['key' => 'bg-yellow-800',];
-		$enum[] = ['key' => 'bg-yellow-900',];
-		$enum[] = ['key' => 'bg-green-50',];
-		$enum[] = ['key' => 'bg-green-100',];
-		$enum[] = ['key' => 'bg-green-200',];
-		$enum[] = ['key' => 'bg-green-300',];
-		$enum[] = ['key' => 'bg-green-400',];
-		$enum[] = ['key' => 'bg-green-500',];
-		$enum[] = ['key' => 'bg-green-600',];
-		$enum[] = ['key' => 'bg-green-700',];
-		$enum[] = ['key' => 'bg-green-800',];
-		$enum[] = ['key' => 'bg-green-900',];
-		$enum[] = ['key' => 'bg-blue-50',];
-		$enum[] = ['key' => 'bg-blue-100',];
-		$enum[] = ['key' => 'bg-blue-200',];
-		$enum[] = ['key' => 'bg-blue-300',];
-		$enum[] = ['key' => 'bg-blue-400',];
-		$enum[] = ['key' => 'bg-blue-500',];
-		$enum[] = ['key' => 'bg-blue-600',];
-		$enum[] = ['key' => 'bg-blue-700',];
-		$enum[] = ['key' => 'bg-blue-800',];
-		$enum[] = ['key' => 'bg-blue-900',];
-		$enum[] = ['key' => 'bg-indigo-50',];
-		$enum[] = ['key' => 'bg-indigo-100',];
-		$enum[] = ['key' => 'bg-indigo-200',];
-		$enum[] = ['key' => 'bg-indigo-300',];
-		$enum[] = ['key' => 'bg-indigo-400',];
-		$enum[] = ['key' => 'bg-indigo-500',];
-		$enum[] = ['key' => 'bg-indigo-600',];
-		$enum[] = ['key' => 'bg-indigo-700',];
-		$enum[] = ['key' => 'bg-indigo-800',];
-		$enum[] = ['key' => 'bg-indigo-900',];
-		$enum[] = ['key' => 'bg-purple-50',];
-		$enum[] = ['key' => 'bg-purple-100',];
-		$enum[] = ['key' => 'bg-purple-200',];
-		$enum[] = ['key' => 'bg-purple-300',];
-		$enum[] = ['key' => 'bg-purple-400',];
-		$enum[] = ['key' => 'bg-purple-500',];
-		$enum[] = ['key' => 'bg-purple-600',];
-		$enum[] = ['key' => 'bg-purple-700',];
-		$enum[] = ['key' => 'bg-purple-800',];
-		$enum[] = ['key' => 'bg-purple-900',];
-		$enum[] = ['key' => 'bg-pink-50',];
-		$enum[] = ['key' => 'bg-pink-100',];
-		$enum[] = ['key' => 'bg-pink-200',];
-		$enum[] = ['key' => 'bg-pink-300',];
-		$enum[] = ['key' => 'bg-pink-400',];
-		$enum[] = ['key' => 'bg-pink-500',];
-		$enum[] = ['key' => 'bg-pink-600',];
-		$enum[] = ['key' => 'bg-pink-700',];
-		$enum[] = ['key' => 'bg-pink-800',];
-		$enum[] = ['key' => 'bg-pink-900',];
-
-
-
+		$enum   = \content_site\color\color::list();
 		return $enum;
 	}
 
 
 	public static function validator($_data)
 	{
-		$data = \dash\validate::enum($_data, true, ['enum' => array_column(self::enum(), 'key'), 'field_title' => T_('Color')]);
+		$data = \dash\validate::enum($_data, true, ['enum' => array_column(self::enum(), 'color'), 'field_title' => T_('Color')]);
 		return $data;
 	}
 
@@ -121,14 +35,14 @@ class background_color
 			{
 				if(isset($value['default']) && $value['default'])
 				{
-					return $value['key'];
+					return $value['color'];
 				}
 			}
 			else
 			{
-				if($value['key'] === $_key)
+				if($value['color'] === $_key)
 				{
-					return $value['key'];
+					return $value['color'];
 				}
 			}
 		}
@@ -145,6 +59,7 @@ class background_color
 			$default = self::default();
 		}
 
+		$color_ajax = \dash\url::here(). '/color?json=1';
 
 		$title = T_("Set item background_color");
 
@@ -153,20 +68,20 @@ class background_color
 		{
 	    	$html .= '<input type="hidden" name="option" value="background_color">';
 			$html .= "<label for='background_color'>$title</label>";
-	        $html .= '<select name="background_color" class="select22"  id="background_color">';
+	        $html .= '<select name="background_color" class="select22" data-model="html" data-ajax--url="'.$color_ajax.'"  id="background_color">';
 
 	        foreach (self::enum() as $key => $value)
 	        {
 	        	$selected = null;
 
-	        	if($value['key'] === $default)
+	        	if($value['color'] === $default)
 	        	{
 	        		$selected = ' selected';
 	        	}
 
-	        	$html .= "<option value='$value[key]'$selected>";
-	        	$html .= $value['key'];
-	        	// $html .= "<div class='$value[key]'>salam</div>";
+	        	$html .= "<option value='$value[color]'$selected>";
+	        	$html .= $value['color'];
+	        	// $html .= "<div class='$value[color]'>salam</div>";
 	        	$html .= "</option>";
 	        }
 
