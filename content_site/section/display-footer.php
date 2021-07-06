0bf3054ad0064dfb44342efd321e6a2b7ffb80d4
Reza <rm.biqarar@gmail.com>
2021-07-06 16:16:53 +0430

    Fix remove block btn

--- content_site/section/display-footer.php
+++ content_site/section/display-footer.php
@@ -4,27 +4,30 @@ if(\dash\url::child())
 
   if(\dash\url::subchild())
   {
-   /**
-     * btn remove and hide
-     */
-    $delete_json    = json_encode(['delete' => 'block']);
+    if(\dash\request::get('index'))
+    {
+     /**
+       * btn remove and hide
+       */
+      $delete_json    = json_encode(['delete' => 'block']);
 
-    $remove_title = T_("Are you sure to remove this block?");
+      $remove_title = T_("Are you sure to remove this block?");
 
-    $html = '';
-    $html .= '<div class="row w-full">';
-    $html .= '<div class="cauto">';
-    $html .= "<div tabindex=0 class='inline-block bg-gray-50 hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 hover:text-red-500 focus:text-red-600 active:text-red-700 transition p-3 rounded-lg' data-confirm data-title='$remove_title' data-data='$delete_json'>";
-    $html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('Delete', 'minor'). '">';
-    $html .= '<span class="inline-block align-middle ps-2">'. T_("Remove block").'<span>';
-    $html .= '</div>';
-    $html .= '</div>';
-    $html .= '<div class="c"></div>';
-    $html .= "<div class='cauto os' >";
-    $html .= '</div>';
-    $html .= '</div>';
+      $html = '';
+      $html .= '<div class="row w-full">';
+      $html .= '<div class="cauto">';
+      $html .= "<div tabindex=0 class='inline-block bg-gray-50 hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 hover:text-red-500 focus:text-red-600 active:text-red-700 transition p-3 rounded-lg' data-confirm data-title='$remove_title' data-data='$delete_json'>";
+      $html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('Delete', 'minor'). '">';
+      $html .= '<span class="inline-block align-middle ps-2">'. T_("Remove block").'<span>';
+      $html .= '</div>';
+      $html .= '</div>';
+      $html .= '<div class="c"></div>';
+      $html .= "<div class='cauto os' >";
+      $html .= '</div>';
+      $html .= '</div>';
 
-    echo $html;
+      echo $html;
+    }
   }
   else
   {
