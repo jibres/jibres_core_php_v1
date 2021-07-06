8405e8c5c0085683fef62c9067a3e84057019d95
Reza <rm.biqarar@gmail.com>
2021-07-06 09:54:01 +0430

    check preview

--- content_site/body/blog/preview.php
+++ content_site/body/blog/preview.php
@@ -14,5 +14,27 @@ class preview
 	}
 
 
+	public static function preview_gradient_1()
+	{
+		return
+		[
+			"type"          => "type_1",
+			"heading"       => "daadPost blog",
+			"post_template" => "standard",
+			"background"    =>
+		    [
+				"background_pack"          => "gradient",
+				"background_gradient_from" => "green-500",
+				"background_gradient_via"  => "white",
+				"background_gradient_to"   => "red-500",
+				"background_gradient_type" => "gradient-to-b",
+				"background_opacity"       => "100"
+		    ],
+		    "height" => "sm"
+		];
+	}
+
+
+
 }
 ?>
