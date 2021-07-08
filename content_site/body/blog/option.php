c273b33ee859a052dd0bde7af6a46ab80a4322ab
Reza <rm.biqarar@gmail.com>
2021-07-08 15:17:45 +0430

    Add option group

--- content_site/body/blog/option.php
+++ content_site/body/blog/option.php
@@ -128,6 +128,7 @@ class option
 	{
 		$option =
 		[
+			'group_setting',
 			// text
 			'heading',
 
---
+++ content_site/options/group/group_setting.php
@@ -0,0 +1,16 @@
+<?php
+namespace content_site\options\group;
+
+
+class group_setting
+{
+
+	public static function admin_html()
+	{
+		$html = '<hr class="mTB20">';
+		$html .= 'group settting';
+		return $html;
+	}
+
+}
+?>
