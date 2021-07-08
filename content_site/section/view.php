6246e885cf4580f4db6ff516b36988af581d66c1
Reza <rm.biqarar@gmail.com>
2021-07-08 13:13:07 +0430

    Fix generate hidden section

--- content_site/section/view.php
+++ content_site/section/view.php
@@ -245,43 +245,42 @@ class view
 			}
 		}
 
-		if(!a($result, 'preview') && a($result, 'body'))
-		{
-			$result['preview'] = $result['body'];
-		}
-
-		if(!a($result, 'preview_layout') && a($result, 'body_layout'))
-		{
-			$result['preview_layout'] = $result['body_layout'];
-		}
-
 		$default = [];
 
 		$detail  = [];
 
-		$detail = \content_site\call_function::detail($section_key);
-
-		if(!is_array($detail))
+		if(a($result, 'status_preview') === 'hidden')
 		{
-			$detail = [];
+			// needless to generate layout
+		}
+		else
+		{
+
+			$detail = \content_site\call_function::detail($section_key);
+
+			if(!is_array($detail))
+			{
+				$detail = [];
+			}
+
+			$default = \content_site\call_function::default($section_key);
+
+			if(!is_array($default))
+			{
+				$default = [];
+			}
+
+			$result['preview']                 = array_merge($detail, $default, $result['preview']);
+			$result['preview']['id']           = a($result, 'id');
+			$result['preview']['preview_mode'] = true;
+
+			$result['preview_layout'] = \content_site\call_function::layout($section_key, $result['preview']);
+
 		}
 
-		$default = \content_site\call_function::default($section_key);
+		// $result['body']           = array_merge($detail, $default, $result['body']);
 
-		if(!is_array($default))
-		{
-			$default = [];
-		}
-
-		$result['preview']                 = array_merge($detail, $default, $result['preview']);
-		$result['preview']['id']           = a($result, 'id');
-		$result['preview']['preview_mode'] = true;
-
-		$result['preview_layout'] = \content_site\call_function::layout($section_key, $result['preview']);
-
-		$result['body']           = array_merge($detail, $default, $result['body']);
-
-		$result['body_layout']    = \content_site\call_function::layout($section_key, $result['body']);
+		// $result['body_layout']    = \content_site\call_function::layout($section_key, $result['body']);
 
 		return $result;
 	}
