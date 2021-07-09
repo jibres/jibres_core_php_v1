a256dac2fdb7de5295035350fd0cfc9c3df30476
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-07-09 13:41:32 +0430

    some change on datetime

--- dash/datetime.php
+++ dash/datetime.php
@@ -78,13 +78,7 @@ class datetime
 	}
 
 
-	public static function get(
-		$_datetime = null,
-		$_format = null,
-		$_type = 'datetime',
-		$_lang = null,
-		$_convertNumber = null
-	)
+	public static function get($_datetime = null, $_format = null, $_type = 'datetime', $_lang = null, $_convertNumber = null)
 	{
 		// step1 - check datetime
 		if($_datetime === null)
@@ -156,11 +150,6 @@ class datetime
 			return null;
 		}
 
-		if($_format === 'humanTime')
-		{
-			return \dash\utility\human::time($_datetime, $_type);
-		}
-
 		// check if strtotime is valid, on linux say invalid
 		// on windows because of shamsi problem on strtotime check length
         $isValidDate = strtotime($_datetime);
@@ -187,10 +176,19 @@ class datetime
         }
 
 
-		if($_format === 'human')
-		{
-			return \dash\utility\human::timing($_datetime, $_type);
-		}
+        switch ($_format)
+        {
+        	case 'humanTime':
+				return \dash\utility\human::time($_datetime, $_type);
+        		break;
+
+        	case 'human':
+				return \dash\utility\human::timing($_datetime, $_type);
+        		break;
+
+        	default:
+        		break;
+        }
 
 		return self::get($_datetime, $_format, $_type, $_calendar);
 	}
