c836a56abc94a6c83ac4f48c275fad744279be9b
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-07-09 12:56:33 +0430

    fix iframe font size

--- dash/layout/panelBuilder/panelBuilder-main-iframe.php
+++ dash/layout/panelBuilder/panelBuilder-main-iframe.php
@@ -31,18 +31,18 @@ $html .= '<div class="toolbar flex-grow-0 flex-none flex content-center mx-2">';
   }
   $html .= '</div>';
   // address line
-  $html .= '<div class="ltr relative flex flex-grow items-center px-7 bg-gray-100 hover:bg-gray-200 rounded-full my-3 text-xl text-gray-700 transition">';
+  $html .= '<div class="ltr relative flex flex-grow items-center px-7 bg-gray-100 hover:bg-gray-200 rounded-full my-3 text-gray-700 transition">';
   {
-    $html .= '<a class="address flex-grow" href='. $page_url. '>';
+    $html .= '<a class="address flex-grow text-xs" href='. $page_url. '>';
     $html .= $page_url_view;
     $html .= '</a>';
 
     if(!\lib\store::is_connected_to_domain())
     {
-      $html .= '<a class="connectDomain" href="'.\lib\store::admin_url().'/a/setting/domain">'. T_("Connect to your Domain"). '</a>';
+      $html .= '<a class="connectDomain text-xs" href="'.\lib\store::admin_url().'/a/setting/domain">'. T_("Connect to your Domain"). '</a>';
     }
 
-    $html .= '<div class="mx-2 relative flex items-center space-x-1 px-3 bg-green-200 text-gray-900 rounded-full text-base">';
+    $html .= '<div class="mx-2 relative flex items-center space-x-1 px-3 bg-green-200 text-gray-900 rounded-full text-sm">';
     $html .= '<div class="w-3 h-3 mx-1 bg-green-800 rounded-full animate-ping2 opacity-50"></div>';
     $html .= '<div>'. T_("Live"). '</div>';
     $html .= '</div>';
@@ -51,8 +51,8 @@ $html .= '<div class="toolbar flex-grow-0 flex-none flex content-center mx-2">';
   // zoom icon
   $html .= '<div class="resizePreview relative flex flex-none items-center space-x-3 px-5">';
   {
-    $html .= '<div data-mode="mobile" class="sf-mobile rounded-full hover:bg-gray-300 text-gray-700 hover:text-gray-900 text-3xl transition cursor-pointer"></div>';
-    $html .= '<div data-mode="desktop" class="sf-monitor rounded-full hover:bg-gray-300 text-gray-700 hover:text-gray-900 text-3xl transition cursor-pointer"></div>';
+    $html .= '<div data-mode="mobile" class="sf-mobile rounded-full hover:bg-gray-300 text-gray-700 hover:text-gray-900 transition cursor-pointer"></div>';
+    $html .= '<div data-mode="desktop" class="sf-monitor rounded-full hover:bg-gray-300 text-gray-700 hover:text-gray-900 transition cursor-pointer"></div>';
   }
   $html .= '</div>';
 }
