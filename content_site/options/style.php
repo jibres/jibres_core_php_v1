bd16b8a07017d0ab80c9ad8f70919b61a124a611
Reza <rm.biqarar@gmail.com>
2021-07-11 18:58:39 +0430

    fix

--- content_site/options/style.php
+++ content_site/options/style.php
@@ -9,6 +9,17 @@ class style
 
 	public static function admin_html($_section_detail)
 	{
+
+
+		$current_style = a($_section_detail, 'preview' , 'style');
+
+		if(!is_array($current_style))
+		{
+			$current_style = [];
+		}
+
+		$selected = false;
+
 		$html = '';
 
 		$url = \dash\url::that(). '/style'. \dash\request::full_get();
@@ -16,8 +27,9 @@ class style
 		$background       = \content_site\options\background\background_pack::get_full_backgroun_class($current_style);
 		$background_class = a($background, 'class');
 		$background_attr  = a($background, 'attr');
+		$text_color = \content_site\options\background\background_color::color_text_class_name(a($_section_detail, 'preview'));
 
-		$html .= "<a href='$url' class='btn mt-10 block $background_class' $background_attr>". T_("Personalization Desgin"). '</a>';
+		$html .= "<a href='$url' class='btn mt-10 block $background_class $text_color' $background_attr>". T_("Personalization Desgin"). '</a>';
 
 
 		return $html;
