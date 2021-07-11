05d61c566847df7a9405000568c731a99d08626f
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-07-11 11:31:31 +0430

    some change on design

--- content_site/options/generate_radio_line.php
+++ content_site/options/generate_radio_line.php
@@ -11,11 +11,12 @@ class generate_radio_line
 
 
 		$html = '';
-		$html .= '<ul id="'. $_uniqueName. '" class="filter-switch flex items-center relative p-1.5 space-x-4 bg-gray-200 rounded-md font-bold text-blue-600 mb-3">';
+		$html .= '<ul id="'. $_uniqueName. '" class="filter-switch flex fix items-center relative p-2 space-x-2 bg-gray-100 text-center rounded-lg font-bold text-blue-600 mb-3">';
 		{
 			$html .= self::itemText($_uniqueName, $_uniqueName.'-s', 'S');
-			$html .= self::itemText($_uniqueName, $_uniqueName.'-m', 'M');
+			$html .= self::itemText($_uniqueName, $_uniqueName.'-m', 'M', true);
 			$html .= self::itemText($_uniqueName, $_uniqueName.'-l', 'L');
+			$html .= self::itemText($_uniqueName, $_uniqueName.'-more', '...');
 		}
 		$html .= '</ul>';
 
@@ -24,13 +25,21 @@ class generate_radio_line
 	}
 
 
-	private static function itemText($_name, $_id, $_text)
+	private static function itemText($_name, $_id, $_text, $_checked = null)
 	{
 		$html = '';
-		$html .= '<li class="filter-switch-item flex relative h-10 bg-gray-300x">';
+		$html .= '<li class="filter-switch-item flex-1 relative">';
 		{
-			$html .= '<input type="radio" name="'. $_name. '" id="'. $_id. '-0" class="sr-only" checked>';
-			$html .= '<label for="'. $_id. '-0" class="h-10 py-1 px-3 text-sm leading-6 text-gray-600 hover:text-gray-800 bg-white rounded shadow">';
+			$checkedBg = null;
+			$html .= '<input type="radio" name="'. $_name. '" id="'. $_id. '-0" class="sr-only"';
+			if($_checked)
+			{
+				$checkedBg = ' bg-white';
+				$html .= ' checked';
+			}
+			$html .= '>';
+
+			$html .= '<label for="'. $_id. '-0" class="block h-10 py-1 px-3 text-sm leading-6 text-gray-600 hover:text-gray-800 rounded-full shadow'. $checkedBg. '">';
 			$html .= $_text;
 			$html .= '</label>';
 			// $html .= '<div aria-hidden="true" class="filter-active"></div>';
