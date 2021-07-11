03294f89ef6e1ee5a4327674adecf3d7651eebe3
Reza <rm.biqarar@gmail.com>
2021-07-11 15:21:34 +0430

    Save heading option

--- content_site/options/heading.php
+++ content_site/options/heading.php
@@ -7,20 +7,28 @@ class heading
 
 	public static function validator($_data)
 	{
-		$data = \dash\validate::string_100($_data);
-		return $data;
+		$new_data            = [];
+		$heading             = a($_data, 'heading');
+		$new_data['heading'] = \dash\validate::string_100($heading);
+
+		$heading_position             = a($_data, 'heading_position');
+		$new_data['heading_position'] = \dash\validate::enum($heading_position, true, ['enum' => ['left', 'center', 'right'], 'field_title' => T_('Heading Position')]);
+
+		return $new_data;
 	}
 
 
 	public static function admin_html($_section_detail)
 	{
-		$default = \content_site\section\view::get_current_index_detail('heading');
+		$default          = \content_site\section\view::get_current_index_detail('heading');
+		$default_position = \content_site\section\view::get_current_index_detail('heading_position');
 
 		$html = '';
 		$html .= '<form method="post" data-patch autocomplete="off">';
 		{
+			$html .= '<input type="hidden" name="multioption" value="multi">';
 			$html .= '<input type="hidden" name="not_redirect" value="1">';
-	    $html .= '<label for="heading">'. T_("Heading"). '</label>';
+		    $html .= '<label for="heading">'. T_("Heading"). '</label>';
 
 			$html .= '<div class="input">';
 			{
@@ -29,7 +37,20 @@ class heading
 				{
 					$realtime = 'data-sync="heading-'. a($_section_detail, 'id'). '"';
 				}
-	    	$html .= "<input type='text' name='opt_heading' value='$default' placeholder='' $realtime>";
+	    		$html .= "<input type='text' name='opt_heading' value='$default' placeholder='' $realtime>";
+	    		$html .= '<label class="addon btn light" data-kerkere=".showHeadingOption">...</label>';
+			}
+			$html .= "</div>";
+
+			$html .= '<div class="showHeadingOption" data-kerkere-content="hide">';
+			{
+
+				$radio_html = '';
+				$radio_html .= \content_site\options\generate_radio_line::itemText('heading_position', 'left', T_("Left"), (($default_position === 'left')? true : false));
+				$radio_html .= \content_site\options\generate_radio_line::itemText('heading_position', 'center', T_("Center"), (($default_position === 'center')? true : false));
+				$radio_html .= \content_site\options\generate_radio_line::itemText('heading_position', 'right', T_("Right"), (($default_position === 'right')? true : false));
+
+				$html .= \content_site\options\generate_radio_line::add_ul('heading_position', $radio_html);
 			}
 			$html .= "</div>";
 		}
