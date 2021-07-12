3b4a828e474f865dd0184260008a2ea3166c552f
Reza <rm.biqarar@gmail.com>
2021-07-12 16:11:25 +0430

    Fix

--- content_site/options/style.php
+++ content_site/options/style.php
@@ -24,12 +24,13 @@ class style
 
 		$url = \dash\url::that(). '/style'. \dash\request::full_get();
 
-		$background       = \content_site\options\background\background_pack::get_full_backgroun_class($current_style);
-		$background_class = a($background, 'class');
-		$background_attr  = a($background, 'attr');
-		$text_color = \content_site\options\background\background_color::color_text_class_name(a($_section_detail, 'preview'));
+		// $background       = \content_site\options\background\background_pack::get_full_backgroun_class($current_style);
+		// $background_class = a($background, 'class');
+		// $background_attr  = a($background, 'attr');
+		// $text_color = \content_site\options\background\background_color::color_text_class_name(a($_section_detail, 'preview'));
+		// $html .= "<a href='$url' class='btn mt-10 block $background_class $text_color' $background_attr>". T_("Personalization Desgin"). '</a>';
+		$html .= "<a href='$url' class='btn mt-10 block'>". T_("Personalization Desgin"). '</a>';
 
-		$html .= "<a href='$url' class='btn mt-10 block $background_class $text_color' $background_attr>". T_("Personalization Desgin"). '</a>';
 
 
 		return $html;
