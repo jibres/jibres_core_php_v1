7ebc58419aff0d98505ac90232afe3fc6b9e9802
Reza <rm.biqarar@gmail.com>
2021-07-18 16:15:54 +0430

    Fix title

--- content_a/accounting/irvat/add/display.php
+++ content_a/accounting/irvat/add/display.php
@@ -61,21 +61,21 @@ switch(\dash\data::myType())
           </div>
         </div>
         <?php if(\dash\data::detailsList()) {?>
-          <label for="put_on"><?php echo T_("Put ON") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
+          <label for="put_on"><?php echo T_("Cost type") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
           <select class="select22" name="put_on" <?php echo $disableInput; ?>>
             <option value=""><?php echo T_("Please choose put_on") ?></option>
             <?php foreach (\dash\data::detailsList() as $key => $value) {?>
               <option value="<?php echo a($value, 'id') ?>" <?php if(a($dataRow, 'fill_value', 'put_on', 'details_id') === a($value, 'id')) { echo 'selected'; } ?>><?php echo a($value, 'full_title'); ?></option>
             <?php } // endfor ?>
           </select>
-          <label for="thirdparty"><?php echo T_("Thirdparty") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
+          <label for="thirdparty"><?php echo T_("Thirdparty") ?></label>
           <select class="select22" name="thirdparty" <?php echo $disableInput; ?>>
             <option value=""><?php echo T_("Please choose thirdparty") ?></option>
             <?php foreach (\dash\data::detailsList() as $key => $value) {?>
               <option value="<?php echo a($value, 'id') ?>" <?php if(a($dataRow, 'fill_value', 'thirdparty', 'details_id') === a($value, 'id')) { echo 'selected'; } ?>><?php echo a($value, 'full_title'); ?></option>
             <?php } // endfor ?>
           </select>
-          <label for="pay_from"><?php echo T_("Pay from") ?> <small class="fc-red"><?php echo T_("Required") ?></small></label>
+          <label for="pay_from"><?php echo T_("Payer") ?></label>
           <select class="select22" name="pay_from" <?php echo $disableInput; ?>>
             <option value=""><?php echo T_("Please choose pay_from") ?></option>
             <?php foreach (\dash\data::detailsList() as $key => $value) {?>
