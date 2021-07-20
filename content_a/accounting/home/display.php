2a537487447a97cad9e7640c914650b9df918151
Reza <rm.biqarar@gmail.com>
2021-07-20 09:56:19 +0430

    Remove old coding list

--- content_a/accounting/home/display.php
+++ content_a/accounting/home/display.php
@@ -49,16 +49,6 @@
     </div>
 
 
-    <div class="c4 s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(). '/coding/all'; ?>'>
-        <div class="statistic blue">
-          <div class="value"><i class="sf-list-ul"></i></div>
-          <div class="label"><?php echo T_("Accounting Coding list"); ?></div>
-        </div>
-      </a>
-    </div>
-
-
      <div class="c4 s12">
       <a class="dcard x1" href='<?php echo \dash\url::this(). '/year'; ?>'>
         <div class="statistic red">
