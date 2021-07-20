1e95b102bf07a8b2ed043f35c436e8fc5e29dd22
Reza <rm.biqarar@gmail.com>
2021-07-20 10:34:07 +0430

    Add manualy

--- content_a/accounting/welcome/display.php
+++ content_a/accounting/welcome/display.php
@@ -3,7 +3,8 @@
     <p><?php echo T_("We make all coding list for you"); ?></p>
     <h2><?php echo T_("Easily Import accounting coding"); ?></h2>
     <div class="buildBtn">
-      <a class="btn xl master" data-data='{"first": "init"}' data-confirm ><?php echo T_("Import it now"); ?></a>
+      <a class="btn xl master" data-data='{"first": "init"}' data-ajaxify ><?php echo T_("Import it now"); ?></a>
     </div>
+    <a href="<?php echo \dash\url::this(). '/coding/add' ?>"><?php echo T_("I want to add manually") ?></a>
   </div>
   <?php } //endif ?>
