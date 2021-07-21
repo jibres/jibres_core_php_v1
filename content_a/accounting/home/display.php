4c6d5de6930eba303453a2c34ef1a7c0fd96016a
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-07-21 17:23:18 +0430

    new dashboard for accounting

--- content_a/accounting/home/display.php
+++ content_a/accounting/home/display.php
@@ -1,6 +1,63 @@
 <div class="row">
   <div class="c-xs-12 c-sm-12 c-md-8">
 
+    <div class="row">
+      <div class="c-12">
+      </div>
+
+      <div class="c-4">
+       <p class="mB5-f font-14"><?php echo T_("Legal offices Monthly") ?></p>
+       <nav class="items long">
+         <ul>
+          <li>
+            <a class="item f" href="<?php echo \dash\url::this(). '/report/journal'; ?>">
+              <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('column with text'); ?>">
+              <div class="key"><?php echo T_("General Journal") ?></div>
+              <div class="value"><?php echo T_("Monthly"); ?></div>
+              <div class="go"></div>
+            </a>
+          </li>
+          <li>
+            <a class="item f" href="<?php echo \dash\url::this(). '/report/ledger'; ?>">
+              <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('column with text'); ?>">
+              <div class="key"><?php echo T_("Ledger") ?></div>
+              <div class="value"><?php echo T_("Monthly"); ?></div>
+              <div class="go"></div>
+            </a>
+          </li>
+         </ul>
+       </nav>
+
+       <p class="mB5-f font-14"><?php echo T_("Legal offices Daily") ?></p>
+       <nav class="items long">
+         <ul>
+          <li>
+            <a class="item f" href="<?php echo \dash\url::this(). '/report/journal?daily=1'; ?>">
+              <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('edit'); ?>">
+              <div class="key"><?php echo T_("General Journal") ?></div>
+              <div class="value"><?php echo T_("Daily"); ?></div>
+              <div class="go"></div>
+            </a>
+          </li>
+          <li>
+            <a class="item f" href="<?php echo \dash\url::this(). '/report/Ledger?daily=1'; ?>">
+              <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('edit'); ?>">
+              <div class="key"><?php echo T_("Ledger") ?></div>
+              <div class="value"><?php echo T_("Daily"); ?></div>
+              <div class="go"></div>
+            </a>
+          </li>
+         </ul>
+       </nav>
+
+      </div>
+      <div class="c-8">
+      </div>
+
+    </div>
+
+
+
 
 
   </div>
@@ -34,7 +91,7 @@
         </a>
       </li>
       <li>
-        <a class="item f" href="<?php echo \dash\url::this(). '//doc/add'; ?>">
+        <a class="item f" href="<?php echo \dash\url::this(). '/doc/add'; ?>">
           <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Add Note'); ?>">
           <div class="key"><?php echo T_('Add Accounting Document');?></div>
           <div class="go plus"></div>
@@ -52,47 +109,70 @@
           <div class="go"></div>
         </a>
       </li>
-     </ul>
-   </nav>
-
-   <p class="mB5-f font-14"><?php echo T_("Legal offices Monthly") ?></p>
-   <nav class="items long">
-     <ul>
       <li>
-        <a class="item f" href="<?php echo \dash\url::this(). '/report/journal'; ?>">
-          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('column with text'); ?>">
-          <div class="key"><?php echo T_("General Journal") ?></div>
-          <div class="value"><?php echo T_("Monthly"); ?></div>
-          <div class="go"></div>
-        </a>
-      </li>
-      <li>
-        <a class="item f" href="<?php echo \dash\url::this(). '/report/ledger'; ?>">
-          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('column with text'); ?>">
-          <div class="key"><?php echo T_("Ledger") ?></div>
-          <div class="value"><?php echo T_("Monthly"); ?></div>
+        <a class="item f" href="<?php echo \dash\url::this(). '/turnover?123'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Exchange'); ?>">
+          <div class="key"><?php echo T_('Petty Cash Turnover');?></div>
           <div class="go"></div>
         </a>
       </li>
      </ul>
    </nav>
 
-   <p class="mB5-f font-14"><?php echo T_("Legal offices Daily") ?></p>
+
+
+
    <nav class="items long">
      <ul>
       <li>
-        <a class="item f" href="<?php echo \dash\url::this(). '/report/journal?daily=1'; ?>">
-          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('edit'); ?>">
-          <div class="key"><?php echo T_("General Journal") ?></div>
-          <div class="value"><?php echo T_("Daily"); ?></div>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=cost'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Point Of Sale'); ?>">
+          <div class="key"><?php echo T_("Cost purchases") ?></div>
           <div class="go"></div>
         </a>
       </li>
       <li>
-        <a class="item f" href="<?php echo \dash\url::this(). '/report/Ledger?daily=1'; ?>">
-          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('edit'); ?>">
-          <div class="key"><?php echo T_("Ledger") ?></div>
-          <div class="value"><?php echo T_("Daily"); ?></div>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=asset'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('capital'); ?>">
+          <div class="key"><?php echo T_("Buy assets") ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
+      <li>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=bill'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Payments'); ?>">
+          <div class="key"><?php echo T_("Payment bills") ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
+     </ul>
+   </nav>
+
+   <nav class="items long">
+     <ul>
+      <li>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=income'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Managed Store'); ?>">
+          <div class="key"><?php echo T_("Revenues and sales invoices") ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
+     </ul>
+   </nav>
+
+   <nav class="items long">
+     <ul>
+      <li>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=petty_cash'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Bank'); ?>">
+          <div class="key"><?php echo T_("Charge Petty Cash from bank") ?></div>
+          <div class="go"></div>
+        </a>
+      </li>
+      <li>
+        <a class="item f" href="<?php echo \dash\url::this(). '/factor?template=partner'; ?>">
+          <img class="bg-gray-100 hover:bg-gray-200 p-2" src="<?php echo \dash\utility\icon::url('Incoming'); ?>">
+          <div class="key"><?php echo T_("Charge Petty Cash from partners") ?></div>
           <div class="go"></div>
         </a>
       </li>
@@ -101,71 +181,6 @@
 
   </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-  <h2 class="mTB20"><?php echo T_("Income-cost management") ?></h2>
-  <div class="f">
-    <div class="c s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/factor/add?type=cost'>
-        <div class="statistic red">
-          <div class="value"><i class="sf-plus"></i></div>
-          <div class="label"><?php echo T_("Add cost"); ?></div>
-        </div>
-      </a>
-    </div>
-
-    <div class="c s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/factor/add?type=income'>
-        <div class="statistic red">
-          <div class="value"><i class="sf-plus"></i></div>
-          <div class="label"><?php echo T_("Add income"); ?></div>
-        </div>
-      </a>
-    </div>
-
-    <div class="c s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/factor/add?type=petty_cash'>
-        <div class="statistic red">
-          <div class="value"><i class="sf-plus"></i></div>
-          <div class="label"><?php echo T_("Add petty cash"); ?></div>
-        </div>
-      </a>
-    </div>
-
-     <div class="c s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/factor/add?type=partner'>
-        <div class="statistic red">
-          <div class="value"><i class="sf-plus"></i></div>
-          <div class="label"><?php echo T_("Add petty cash from Accounting Partner"); ?></div>
-        </div>
-      </a>
-    </div>
-
-
-
-
-
-    <div class="c s12">
-      <a class="dcard x1" href='<?php echo \dash\url::this(); ?>/factor'>
-        <div class="statistic red">
-          <div class="value"><i class="sf-list-ul"></i></div>
-          <div class="label"><?php echo T_("List"); ?></div>
-        </div>
-      </a>
-    </div>
-
-  </div>
-
-
 
 
 
