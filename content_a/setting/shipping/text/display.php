d625e3f7765b712c1a1b935d7ceadd94c53a6618
Reza <rm.biqarar@gmail.com>
2021-07-21 12:26:58 +0430

    Fix shipping text

--- content_a/setting/shipping/text/display.php
+++ content_a/setting/shipping/text/display.php
@@ -1,48 +1,24 @@
-<form method="post" autocomplete="off">
-
 <div class="avand-md">
-
-  <div  class="box impact mB25-f">
-    <header><h2><?php echo T_("Set Shipping Text");?></h2></header>
-      <div class="body">
-        <p><?php echo T_("If you want to show a message to all customers on the shopping shipping page, write it here. You can also adjust the color of this text. <br>
-After setting this text, you will see a sample of it at the bottom of this page");?></p>
-        <div class="c4 s12" method="post" autocomplete="off">
-          <div class="action f">
-
-            <div class="c12 mB5">
-              <label for="color"><?php echo T_("Message Type"); ?></label>
-
-              <div>
-                <select class="select22" name="color">
-                  <option value="0"><?php echo T_("Default"); ?></option>
-                  <option value="red" <?php if(\dash\data::shippingSettingSaved_color() === 'red') {echo 'selected';} ?>><?php echo T_("Red (For important warning message)") ?></option>
-                  <option value="green" <?php if(\dash\data::shippingSettingSaved_color() === 'green') {echo 'selected';} ?>><?php echo T_("Green (For thank you message)") ?></option>
-                  <option value="blue" <?php if(\dash\data::shippingSettingSaved_color() === 'blue') {echo 'selected';} ?>><?php echo T_("Blue (For information message)") ?></option>
-                  <option value="yellow" <?php if(\dash\data::shippingSettingSaved_color() === 'yellow') {echo 'selected';} ?>><?php echo T_("Yellow (For warning message)") ?></option>
-                </select>
-              </div>
-
-            </div>
-
-            <div class="c12 mB5">
-              <label for="page_text"><?php echo T_("Text"); ?></label>
-              <textarea name="page_text" id="page_text" class="txt" rows="5"><?php echo \dash\data::shippingSettingSaved_page_text(); ?></textarea>
-            </div>
-
-
-          </div>
+  <form method="post" autocomplete="off">
+    <div class="box">
+      <div class="pad">
+        <p><?php echo T_("If you want to show a message to all customers on the shopping shipping page, write it here. You can also adjust the color of this text. <br> After setting this text, you will see a sample of it at the bottom of this page");?></p>
+        <label for="color"><?php echo T_("Message Type"); ?></label>
+        <div>
+          <select class="select22" name="color">
+            <option value="0"><?php echo T_("Default"); ?></option>
+            <option value="red" <?php if(\dash\data::shippingSettingSaved_color() === 'red') {echo 'selected';} ?>><?php echo T_("Red (For important warning message)") ?></option>
+            <option value="green" <?php if(\dash\data::shippingSettingSaved_color() === 'green') {echo 'selected';} ?>><?php echo T_("Green (For thank you message)") ?></option>
+            <option value="blue" <?php if(\dash\data::shippingSettingSaved_color() === 'blue') {echo 'selected';} ?>><?php echo T_("Blue (For information message)") ?></option>
+            <option value="yellow" <?php if(\dash\data::shippingSettingSaved_color() === 'yellow') {echo 'selected';} ?>><?php echo T_("Yellow (For warning message)") ?></option>
+          </select>
         </div>
+        <label for="page_text"><?php echo T_("Text"); ?></label>
+        <textarea name="page_text" id="page_text" class="txt" rows="5"><?php echo \dash\data::shippingSettingSaved_page_text(); ?></textarea>
       </div>
       <footer class="txtRa">
         <button  class="btn success" ><?php echo T_("Save"); ?></button>
       </footer>
-  </div>
-    <?php if(\dash\data::shippingSettingSaved_page_text()) {?>
-      <div class="fs12">
-        <div class="msg <?php echo \dash\data::shippingSettingSaved_color_class() ?>"><?php echo nl2br(\dash\data::shippingSettingSaved_page_text()) ?></div>
-      </div>
-    <?php } //endif ?>
+    </div>
+  </form>
 </div>
-
-</form>
