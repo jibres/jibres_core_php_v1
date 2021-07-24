92a9b558c9011440356dc8f7926ac4f6fdb9a8a3
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-07-24 16:27:03 +0430

    update js link

--- content_a/accounting/factor/add/view.php
+++ content_a/accounting/factor/add/view.php
@@ -22,7 +22,7 @@ class view
 	public static function static_var()
 	{
 
-		\dash\data::global_scriptPage('a_accounting_factor.js?v=2');
+		\dash\data::global_scriptPage('a_accounting_factor.js?v=3');
 
 		$year = \lib\app\tax\year\get::list();
 		\dash\data::accountingYear($year);
