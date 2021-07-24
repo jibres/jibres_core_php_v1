e3c7cfa6a026d48f39ecba8f9b9a33c59e8113fb
Reza <rm.biqarar@gmail.com>
2021-07-24 16:08:45 +0430

    Add total include vat

--- includes/database/upgrade/store/v.3.1.0_tax_new.sql
+++ includes/database/upgrade/store/v.3.1.0_tax_new.sql
@@ -1,10 +1,6 @@
 ALTER TABLE jibres_XXXXXXX.tax_document ADD `producttitle` text NULL DEFAULT NULL;
 ALTER TABLE jibres_XXXXXXX.tax_document ADD `totalnotincludevat` DECIMAL(20, 4) NULL DEFAULT NULL;
-ALTER TABLE jibres_XXXXXXX.tax_document ADD `totalnotincludevat` DECIMAL(20, 4) NULL DEFAULT NULL;
-
-ALTER TABLE jibres_XXXXXXX.tax_document ADD INDEX `tax_document_index_totalincludevat` (`totalincludevat`);
 ALTER TABLE jibres_XXXXXXX.tax_document ADD INDEX `tax_document_index_totalnotincludevat` (`totalnotincludevat`);
-
 ALTER TABLE jibres_XXXXXXX.tax_year ADD `vatsetting` text NULL DEFAULT NULL;
 ALTER TABLE jibres_XXXXXXX.tax_year ADD `remainvatlastyear` DECIMAL(20, 4) NULL DEFAULT NULL;
 ALTER TABLE jibres_XXXXXXX.tax_year ADD `quorumprice` DECIMAL(20, 4) NULL DEFAULT NULL;
---
+++ includes/database/upgrade/store/v.3.1.5_tax_new.sql
@@ -0,0 +1,2 @@
+ALTER TABLE jibres_XXXXXXX.tax_document ADD `totalincludevat` DECIMAL(20, 4) NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.tax_document ADD INDEX `tax_document_index_totalincludevat` (`totalincludevat`);
