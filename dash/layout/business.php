b292930cbbaf12f5cfe8c88c432c460d2a17791a
Reza <rm.biqarar@gmail.com>
2021-07-31 15:34:00 +0430

    Enable old page builder from local

--- dash/layout/business.php
+++ dash/layout/business.php
@@ -97,7 +97,7 @@ class business
 		}
 
 		// check route sitebuilder
-		if(\dash\url::isLocal() /*&& \dash\request::get('preview')*/)
+		if(\dash\url::isLocal() && \dash\request::get('preview'))
 		{
 			// load page builder by detect current page
 			$pagebuilder = \content_site\load\load::current_page();
