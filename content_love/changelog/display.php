77e008edd01fa70117e6a820bbb771028d085b52
Reza <rm.biqarar@gmail.com>
2021-08-04 13:39:24 +0430

    Fix show changelog

--- content_love/changelog/display.php
+++ content_love/changelog/display.php
@@ -1,6 +1,20 @@
 <nav class="items long">
   <ul>
-    <?php foreach (\dash\data::dataTable() as $key => $value) { ?>
+    <?php foreach (\dash\data::dataTable() as $key => $value) {  if(strtotime(a($value, 'date')) < time()){continue;}?>
+     <li>
+      <a class="item f align-center" href="<?php echo \dash\url::that(). '/edit?id='. a($value, 'id'); ?>">
+        <div class="key"><?php echo strip_tags(a($value, 'title')); ?></div>
+        <div class="value"><?php echo htmlGenerateTag($value) ?></div>
+        <time class="value"><?php if(a($value, 'date')) { echo \dash\fit::date(a($value, 'date'));}else{echo T_("Soon");} ?></time>
+        <div class="go"></div>
+      </a>
+     </li>
+    <?php } //endfor ?>
+  </ul>
+</nav>
+<nav class="items long">
+  <ul>
+    <?php foreach (\dash\data::dataTable() as $key => $value) {  if(strtotime(a($value, 'date')) > time()){continue;}?>
      <li>
       <a class="item f align-center" href="<?php echo \dash\url::that(). '/edit?id='. a($value, 'id'); ?>">
         <div class="key"><?php echo strip_tags(a($value, 'title')); ?></div>
