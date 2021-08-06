b05857f3d802201ad4abea30e46fffa71d88fbac
Reza <rm.biqarar@gmail.com>
2021-08-06 11:28:41 +0430

    Add function to load default data

--- content_site/utility.php
+++ content_site/utility.php
@@ -4,6 +4,22 @@ namespace content_site;
 
 class utility
 {
+	private static $fill_by_default_data = false;
+
+
+	public static function fill_by_default_data($_set = null)
+	{
+		if($_set === false || $_set === true)
+		{
+			self::$fill_by_default_data = $_set;
+		}
+		else
+		{
+			return self::$fill_by_default_data;
+		}
+	}
+
+
 	public static function unset_option(&$_option_list, $_need_unset)
 	{
 		if(($myKey = array_search($_need_unset, $_option_list)) !== false)
