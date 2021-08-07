56fa8b0dccded097729fc6d0eb68c20e4e7d2376
Reza <rm.biqarar@gmail.com>
2021-08-07 15:33:23 +0430

    Fix

--- content_site/options/background/background_color_random.php
+++ content_site/options/background/background_color_random.php
@@ -73,6 +73,7 @@ class background_color_random
 		$json =
 		[
 			'opt_background_color_random' => 'random_bg_color',
+			// 'notredirect' => 1,
 		];
 
 		$json = json_encode($json);
@@ -85,6 +86,7 @@ class background_color_random
 		$json =
 		[
 			'opt_background_color_random' => 'random_bg_gradient',
+			// 'notredirect' => 1,
 		];
 
 		$json = json_encode($json);
