df9363ac6d0a29810e239090b3553ceb4c18dec2
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-07 17:37:57 +0430

    fix time ltr

--- content_site/body/blog/b1.php
+++ content_site/body/blog/b1.php
@@ -147,7 +147,7 @@ class b1
 									{
 										if($myDate)
 										{
-											$card .= "<time class='text-gray-600 text-2xs' datetime='$myDate' title='". T_("Published"). " $myDate'>";
+											$card .= "<time class='text-gray-600 text-2xs ltr' datetime='$myDate' title='". T_("Published"). " $myDate'>";
 
 											$card .= \content_site\assemble\tools::date($myDate, a($_args, 'post_show_date'));
 
