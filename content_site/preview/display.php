4f79a17925168b4f8a2157dc4adcecce03d68c41
Reza <rm.biqarar@gmail.com>
2021-08-08 11:56:15 +0430

    Add pointer event none on preview

--- content_site/preview/display.php
+++ content_site/preview/display.php
@@ -1,5 +1,11 @@
 <?php
 $preview  = \dash\data::myPreviewDetail();
+$html = '';
+$html .= '<div class="pointer-events-none">';
+{
+	$html .= a($preview, 'preview_html');
+}
+$html .= '</div>';
 
-echo a($preview, 'preview_html');
+echo $html;
 ?>
