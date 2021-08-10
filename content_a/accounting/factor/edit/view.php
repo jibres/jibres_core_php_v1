377b79bf13720da57586a8c013c534a62c7ff80c
Reza <rm.biqarar@gmail.com>
2021-08-10 18:37:34 +0430

    Fix duplicate doc

--- content_a/accounting/factor/edit/view.php
+++ content_a/accounting/factor/edit/view.php
@@ -39,11 +39,13 @@ class view
 		// duplicate btn
 		$duplicateLinkArgs =
 		[
-			'type'       => \dash\data::myType(),
-			'put_on'     => a($dataRow, 'fill_value', 'put_on', 'details_id'),
-			'thirdparty' => a($dataRow, 'fill_value', 'thirdparty', 'details_id'),
-			'pay_from'   => a($dataRow, 'fill_value', 'pay_from', 'details_id'),
-			'date'       => \dash\fit::date_en(a($dataRow, 'tax_document', 'date')),
+			'type'        => \dash\data::myType(),
+			'put_on'      => a($dataRow, 'fill_value', 'put_on', 'details_id'),
+			'thirdparty'  => a($dataRow, 'fill_value', 'thirdparty', 'details_id'),
+			'pay_from'    => a($dataRow, 'fill_value', 'pay_from', 'details_id'),
+			'bank'        => a($dataRow, 'fill_value', 'bank', 'details_id'),
+			'bank_profit' => a($dataRow, 'fill_value', 'bank_profit', 'details_id'),
+			'date'        => \dash\fit::date_en(a($dataRow, 'tax_document', 'date')),
 		];
 		$duplicateLink = \dash\url::that(). '/add?';
 		$duplicateLink .= \dash\request::build_query($duplicateLinkArgs);
