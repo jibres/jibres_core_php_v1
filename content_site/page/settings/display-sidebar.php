f75cda6efbf5eb0a021ba86ca193d4a4e97a8547
Reza <rm.biqarar@gmail.com>
2021-08-10 15:33:00 +0430

    Try to get page title

---
+++ content_site/page/settings/display-sidebar.php
@@ -0,0 +1,12 @@
+<?php
+$currentPageDetail = \dash\data::currentPageDetail();
+
+
+
+?>
+<form method="post" autocomplete="off" data-patch>
+	<label for="pagetitle"><?php echo T_("Page title") ?></label>
+	<div class="input">
+		<input type="text" name="title" value="<?php echo a($currentPageDetail, 'title'); ?>" id="<?php echo T_("Page title") ?>">
+	</div>
+</form>
