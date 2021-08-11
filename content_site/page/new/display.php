c465e387b7dafe6736989d17566258418db564b6
Reza <rm.biqarar@gmail.com>
2021-08-11 08:53:22 +0430

    Fix show template

--- content_site/page/new/display.php
+++ content_site/page/new/display.php
@@ -13,28 +13,41 @@
 </form>
 <?php
 $html = '';
-$json = json_encode(['key' => 'blank']);
-$html .= "<div class='w-3/12' data-ajaxify data-data='$json'>";
+$html .= '<div class="row">';
 {
-  $html .= T_("Blank page");
-  $html .= '<img src="'. \dash\sample\img::background(). '" alt="'. T_("Blank page"). '">';
+  $html .= '<div class="c-xs-12 c-sm-4">';
+  {
+    $json = json_encode(['key' => 'blank']);
+    $html .= "<div class='' data-ajaxify data-data='$json'>";
+    {
+      $html .= T_("Blank page");
+      $html .= '<img src="'. \dash\sample\img::background(). '" alt="'. T_("Blank page"). '">';
+    }
+    $html .= '</div>';
+  }
+  $html .= '</div>';
+
+    if(\dash\data::templateList_preview())
+    {
+     foreach (\dash\data::templateList_preview() as $key => $value)
+     {
+      $html .= '<div class="c-xs-12 c-sm-4">';
+      {
+        $json = json_encode(['key' => a($value, 'key')]);
+
+        $html .= "<div class='' data-ajaxify data-data='$json'>";
+        {
+          $html .= a($value, 'title');
+          $html .= '<img src="'. a($value, 'image'). '" alt="'. a($value, 'title'). '">';
+        }
+        $html .= '</div>';
+      }
+      $html .= '</div>';
+     }
+  }
 }
 $html .= '</div>';
 
-if(\dash\data::templateList_preview())
-{
- foreach (\dash\data::templateList_preview() as $key => $value)
- {
-  $json = json_encode(['key' => a($value, 'key')]);
-
-  $html .= "<div class='w-3/12' data-ajaxify data-data='$json'>";
-  {
-    $html .= a($value, 'title');
-    $html .= '<img src="'. a($value, 'image'). '" alt="'. a($value, 'title'). '">';
-  }
-  $html .= '</div>';
- }
-}
 
 echo $html;
 ?>
