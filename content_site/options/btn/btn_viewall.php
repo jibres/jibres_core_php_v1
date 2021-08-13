6295cfa630bf0d1e1328c0d5bcad8d68c366886c
Reza <rm.biqarar@gmail.com>
2021-08-13 16:54:27 +0430

    Add btn view all mode

--- content_site/options/btn/btn_viewall.php
+++ content_site/options/btn/btn_viewall.php
@@ -5,12 +5,31 @@ namespace content_site\options\btn;
 class btn_viewall
 {
 
+	public static function btn_mode()
+	{
+		$list = [];
+		$list[] = ['key' => 'red', 'class' => 'fc-red', 'style' => 'background:red;'];
+		$list[] = ['key' => 'green', 'class' => 'fc-green', 'style' => 'background:green;'];
+		$list[] = ['key' => 'blue', 'class' => 'fc-blue', 'style' => 'background:blue;'];
+		$list[] = ['key' => 'black', 'class' => 'fc-black', 'style' => 'background:black;'];
+		return $list;
+	}
+
 	public static function validator($_data)
 	{
 		$new_data                       = [];
 
 		$new_data['btn_viewall_check'] = \dash\validate::bit(a($_data, 'btn_viewall_check'));
 		$new_data['btn_viewall']       = \dash\validate::string_100(a($_data, 'btn_viewall'));
+		$new_data['btn_viewall_mode']  = \dash\validate::enum(a($_data, 'btn_viewall_mode'), false, ['enum' => array_column(self::btn_mode(), 'key')]);
+
+		if($new_data['btn_viewall_mode'])
+		{
+			\content_site\utility::need_redirect(true);
+
+			unset($new_data['btn_viewall_check']);
+			unset($new_data['btn_viewall']);
+		}
 
 		return $new_data;
 	}
@@ -42,6 +61,11 @@ class btn_viewall
 			$default = self::default();
 		}
 
+		$btn_mode = null;
+		if(isset($_section_detail['preview']['btn_viewall_mode']) && $_section_detail['preview']['btn_viewall_mode'])
+		{
+			$btn_mode = $_section_detail['preview']['btn_viewall_mode'];
+		}
 
 		$html = '';
 		$html .= '<form method="post" data-patch autocomplete="off">';
@@ -63,6 +87,27 @@ class btn_viewall
 					$html .= '<input type="text" name="opt_btn_viewall" placeholder="'.self::default().'" value="'.$default.'">';
 				}
 				$html .= '</div>';
+
+				$html .= '<div class="relative flex flex-none items-center px-3 mt-5">';
+				{
+					$list = self::btn_mode();
+
+					foreach ($list as $key => $value)
+					{
+						$selected = null;
+						if($btn_mode == $value['key'])
+						{
+							$selected = 'border-4';
+						}
+
+						$json = json_encode(['opt_btn_viewall' => 1, 'multioption' => 'multi', 'btn_viewall_mode' => $value['key']]);
+
+						$html .= "<div data-ajaxify data-data='$json' class='w-10 h-10 transition rounded-full mRa5 $selected' style='$value[style]'></div>";
+
+					}
+				}
+				$html .= '</div>';
+
 			}
 			$html .= '</div>';
 		}
