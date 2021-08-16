2e62e91d2210faf8871176765a91f78ff9f6db97
Reza <rm.biqarar@gmail.com>
2021-08-16 17:11:36 +0430

    Some fix on b5

--- content_site/body/blog/html/b5.php
+++ content_site/body/blog/html/b5.php
@@ -108,21 +108,26 @@ class b5
 
 										$html .= '<a href="'.$myLink.'" class="text-indigo-500 inline-flex items-center sm:mb-2 lg:mb-0">'.T_("Read more").'</a>';
 
-										$html .= '<span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto sm:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">';
+										if(a($_args, 'post_show_comment_count'))
 										{
 
-											$html .= '<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>';
-											$html .= '1.2K';
-										}
-										$html .= '</span>';
+											$html .= '<span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto sm:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-gray-200">';
+											{
 
-										$html .= '<span class="text-gray-400 inline-flex items-center leading-none text-sm">';
-										{
+												// $html .= '<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>';
+												// $html .= '1.2K';
+											}
+											$html .= '</span>';
 
-											$html .= '<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path></svg>';
-											$html .= 6;
+											$html .= '<span class="text-gray-400 inline-flex items-center leading-none text-sm">';
+											{
+
+												$html .= '<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path></svg>';
+												$html .= \dash\fit::number(floatval(a($value, 'comment_count')));
+											}
+											$html .= '</span>';
+
 										}
-										$html .= '</span>';
 									}
 									$html .= '</div>';
 								}
