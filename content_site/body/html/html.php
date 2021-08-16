c3008a5e0998a1cdeb8aad80fa6655a7fbd8969d
Reza <rm.biqarar@gmail.com>
2021-08-16 21:08:10 +0430

    Add html

---
+++ content_site/body/html/html.php
@@ -0,0 +1,52 @@
+<?php
+namespace content_site\body\html;
+
+
+class html
+{
+
+	/**
+	 * Style 1
+	 *
+	 * @return     array  ( description_of_the_return_value )
+	 */
+	public static function option()
+	{
+		return
+		[
+			'title'        => T_("html 1"),
+			'default'      => ['type' => 'html'],
+			'options'      =>
+			[
+				'heading_raw',
+				'html',
+			],
+
+			'preview_list' =>
+			[
+				'p1',
+			],
+		];
+	}
+
+
+	/**
+	 * Preview 1
+	 */
+	public static function p1()
+	{
+		return
+		[
+			'preview_title'  => T_("Preview :val", ['val' => \dash\fit::number(1)]),
+			'version'        => 1,
+			'options' =>
+			[
+
+			],
+		];
+	}
+
+
+
+}
+?>
