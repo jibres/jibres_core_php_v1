dace52ea2741a3c08b7ebbe87a1480eae52e23bc
Reza <rm.biqarar@gmail.com>
2021-08-17 19:20:38 +0430

    Move download php to utility

--- content_site/utility.php
+++ content_site/utility.php
@@ -45,6 +45,95 @@ class utility
 	}
 
 
+	public static function downloadjson()
+	{
+		$section_detail = \dash\data::currentSectionDetail();
+
+		$load = \lib\db\pagebuilder\get::by_id(a($section_detail, 'id'));
+
+		$need_unset =
+		[
+			'btn_viewall_mode',
+			'post_order',
+			'post_template',
+		];
+
+		$preview = a($load, 'preview');
+		$preview = json_decode($preview, true);
+		if(!is_array($preview))
+		{
+			$preview = [];
+		}
+
+		krsort($preview);
+
+		$max_len = 0;
+		foreach ($preview as $key => $value)
+		{
+			if(mb_strlen($key) > $max_len)
+			{
+				$max_len = mb_strlen($key);
+			}
+		}
+
+		$folder      = a($section_detail, 'mode');
+		$section_key = a($preview, 'key');
+		$type        = a($preview, 'type');
+
+		$code = '';
+		$code .= '<?php ';
+		$code .= "\n";
+		$code .= "/** \n";
+		$code .= " * This is options of one preview function \n";
+		$code .= " * Put this code on content_site/$folder/$section_key/$type.php \n";
+		$code .= " */ ";
+		$code .= "\n\n\n";
+		$code .= '[';
+		$code .= "\n";
+
+		foreach ($preview as $key => $value)
+		{
+			if(in_array($key, $need_unset))
+			{
+				continue;
+			}
+
+			if(!is_array($value))
+			{
+				if(is_numeric($value) || $value === '1')
+				{
+					$myValue = "$value";
+				}
+				elseif(is_null($value))
+				{
+					$myValue = "null";
+				}
+				elseif($value === 'View all')
+				{
+					$myValue = 'T_("View all")';
+				}
+				elseif($key === 'heading')
+				{
+					$myValue = 'T_("'.$value.'")';
+				}
+				else
+				{
+					$myValue = "'$value'";
+				}
+
+				$space = str_repeat(' ', $max_len - mb_strlen($key));
+
+				$code .= "\t'$key'$space => $myValue, \n";
+			}
+		}
+		$code .= "],";
+		$code .= "\n\n\n";
+		$code .= '?>';
+
+		\dash\code::jsonBoom($code);
+	}
+
+
 
 }
 ?>
