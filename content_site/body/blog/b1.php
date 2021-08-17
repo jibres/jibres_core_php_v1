d1b4e8f2fab60d3e9d1335d91858e602c28fa6ed
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-17 14:56:18 +0430

    save b1

--- content_site/body/blog/b1.php
+++ content_site/body/blog/b1.php
@@ -28,15 +28,34 @@ class b1
 	/**
 	 * Preview 1
 	 */
-	public static function p1()
+	public static function p1($_title = null)
 	{
+		$_title = T_("Latest Posts");
 		return
 		[
 			'preview_title'  => T_("Preview :val", ['val' => \dash\fit::number(1)]),
 			'version'        => 1,
 			'options' =>
 			[
-
+				'type'                  => 'b1',
+				'post_template'         => 'any',
+				'post_tag'              => 'R',
+				'post_show_readingtime' => '1',
+				'post_show_image'       => '1',
+				'post_show_excerpt'     => '1',
+				'post_show_date'        => 'relative',
+				'post_show_author'      => '1',
+				'post_order'            => 'latest',
+				'key'                   => 'blog',
+				'height'                => 'm',
+				'heading_position'      => 'center',
+				'heading'               => $_title,
+				'coverratio'            => '16:9',
+				'count'                 => 3,
+				'color_text'            => '#333333',
+				'btn_viewall_check'     => '1',
+				'btn_viewall'           => T_('View all'),
+				'background_pack'       => 'none',
 			],
 		];
 	}
