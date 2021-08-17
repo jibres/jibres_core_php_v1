8a3b6fd337bda3f1656ac02969247248cfbdacdf
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-17 18:54:04 +0430

    save preview

--- content_site/section/display.php
+++ content_site/section/display.php
@@ -10,7 +10,18 @@ $html = '';
 $html .= '<div class="grid grid-cols-1 sm:grid-cols-2 gap-5 md:gap-6">';
 foreach ($previewSectionList as $value)
 {
-	$html .= '<figure class="group relative bg-white rounded-lg shadow-sm overflow-hidden ring-1 ring-black ring-opacity-5">';
+	// variables
+	$select_preview =
+	[
+		'section'     => 'preview',
+		'key'         => a($value, 'key'),
+		'opt_type'    => a($value, 'opt_type'),
+		'type'        => a($value, 'opt_type'),
+		'preview_key' => a($value, 'preview_key'),
+	];
+	$ajaxify = "data-ajaxify data-data='".json_encode($select_preview)."'";
+
+	$html .= '<button class="group relative bg-white rounded-lg shadow-sm overflow-hidden transition hover:shadow-md focus:shadow-lg ring-1 ring-black ring-opacity-5"'. $ajaxify. '>';
 	{
 		$html .= '<div class="relative bg-gray-100 border-b overflow-hidden">';
 		{
@@ -18,32 +29,25 @@ foreach ($previewSectionList as $value)
 		}
 		$html .= '</div>';
 
-		// variables
-		$select_preview =
-		[
-			'section'     => 'preview',
-			'key'         => a($value, 'key'),
-			'opt_type'    => a($value, 'opt_type'),
-			'type'        => a($value, 'opt_type'),
-			'preview_key' => a($value, 'preview_key'),
-		];
-		$html .= '<figcaption class="p-5">';
+		$html .= '<div class="p-5">';
 		{
-			$html .= '<a class="" href="'. a($value, 'demo_url'). '" target="_blank">'. T_("Live Preview").'</a>';
+			$html .= '<p class="text-sm font-medium text-gray-900 text-justify">';
+			{
+				// $html .= T_('Sample'). ' '. implode(' ', [a($value, 'key'), a($value, 'opt_type'), a($value, 'preview_key')]);
+				$html .= T_('Sample'). ' '. a($value, 'preview_title');
+
+			}
+			$html .= '</p>';
+			// $html .= '<p class="text-xs font-medium text-gray-500">';
+			// {
+			// 	$html .= a($value, 'desc');
+			// 	$html .= 'lorem desc';
+			// }
+			// $html .= '</p>';
 		}
-		$html .= '</figcaption>';
-
-
-		$html .= "<div data-ajaxify data-data='".json_encode($select_preview)."'>";
-		{
-
-			$html .= implode(' ', [a($value, 'key'), a($value, 'opt_type'), a($value, 'preview_key')]);
-
-			// $html .= '<img src="'. a($value, 'preview_image'). '" alt="'.a($value, 'key').'">';
-		}
-		$html .= "</div>";
+		$html .= '</div>';
 	}
-	$html .= '</figure>';
+	$html .= '</button>';
 }
 $html .= '</div>';
 
