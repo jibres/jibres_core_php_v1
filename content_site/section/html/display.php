afb36704fb8bdaea1e41e870edb0172b7aa203eb
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-18 14:47:55 +0430

    fix html

--- content_site/section/html/display.php
+++ content_site/section/html/display.php
@@ -1,5 +1,5 @@
 <form method="post" autocomplete="off"  id="savehtmlform" class="ltr h-full w-full">
 	<input type="hidden" name="savehtml" value="html">
-	<pre id="codeEditorLive" data-code-editor='html' data-code-editor-sync='[name="html"]' class="h-full"><?php echo htmlentities(\dash\data::myHtmlText()); ?></pre>
-	<textarea name="html" class="hide ltr w-full h-full p-5 resize-none mt-5" placeholder="Write yout HTML here ..."></textarea>
+	<pre id="codeEditorLive" data-code-editor="html" data-code-editor-sync="[name='html']" class="h-full"><?php echo htmlentities(\dash\data::myHtmlText()); ?></pre>
+	<textarea name="html" class="hide ltr w-full h-full p-5 resize-none mt-5" placeholder="Write yout HTML here ..."><?php echo htmlentities(\dash\data::myHtmlText()); ?></textarea>
 </form>
