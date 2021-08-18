a38f6f6424737d02af63854b1b79089014bb15ce
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-18 16:43:20 +0430

    add some info to sidebar of html

--- content_site/section/html/display-sidebar.php
+++ content_site/section/html/display-sidebar.php
@@ -6,8 +6,9 @@
     </div>
 </form>
 
-<hr>
-<h4>Hi Developer</h4>
-<div class="jalert">
-    You can add some html here
+<div>
+  <h3 class="text-blue-800 text-lg mt-10">TailwindCSS</h3>
+  <p class="text-gray-500 text-md leading-6"><?php echo T_("TailwindCSS is an absolutely amazing front end CSS library that allows you to create stunning CSS layouts. Using TailwindCSS in your Jibres Theme will help add beauty and simplicity to your design. It will also make your theme scalable and easy to manage. Working with TailwindCSS means using a set of utility classes that lets you work with exactly what you need.") ?></p>
+  <a target="_blank" rel="nofollow noopener" href="https://tailwindcss.com/" class="btn mt-5"><?php echo T_("Read more"); ?></a>
 </div>
+
