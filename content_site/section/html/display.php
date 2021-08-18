2160ce67bba03ff91c6fc91e09e74df6eec13a94
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-18 16:20:29 +0430

    show code preview

--- content_site/section/html/display.php
+++ content_site/section/html/display.php
@@ -1,5 +1,11 @@
-<form method="post" autocomplete="off"  id="savehtmlform" class="ltr h-full w-full">
+<form method="post" autocomplete="off"  id="savehtmlform" class="ltr h-1/2 w-full rounded-lg overflow-hidden">
 	<input type="hidden" name="savehtml" value="html">
 	<pre id="codeEditorLive" data-code-editor="html" data-code-editor-sync="[name='html']" class="h-full"><?php echo htmlentities(\dash\data::myHtmlText()); ?></pre>
 	<textarea name="html" class="hide ltr w-full h-full p-5 resize-none mt-5" placeholder="Write yout HTML here ..."><?php echo htmlentities(\dash\data::myHtmlText()); ?></textarea>
 </form>
+
+<div class="preview h-1/2 pt-10 rounded-lg">
+ <div class="h-full rounded-lg overflow-hidden ring ring-gray-500 shadow-md hover:shadow-lg transition">
+<?php echo \dash\data::myHtmlText(); ?>
+ </div>
+</div>
