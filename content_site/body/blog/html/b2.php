b74a861927c2a64c3f0c5ce24eef983ccc50f486
Reza <rm.biqarar@gmail.com>
2021-08-20 15:34:26 +0430

    FIx

--- content_site/body/blog/html/b2.php
+++ content_site/body/blog/html/b2.php
@@ -13,7 +13,7 @@ class b2
 		$id             = a($_args, 'id');
 		$type           = a($_args, 'type');
 		$title_position = a($_args, 'post_title_position');
-		$link_color     = a($_args, 'link_mode');
+		$link_color     = a($_args, 'link_color');
 		$maskImg        = 'mask mask-heart';
 		$borderRadius   = \content_site\options\radius::class_name(a($_args, 'radius'));
 		$coverRatio     = \content_site\options\coverratio::get_class(a($_args, 'coverratio'));
