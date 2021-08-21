fc95f604012ef16258defe79f1237def005d3e67
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-21 12:14:59 +0430

    add p10

--- content_site/body/blog/b2.php
+++ content_site/body/blog/b2.php
@@ -446,7 +446,34 @@ class b2
 			'version'        => 1,
 			'options' =>
 			[
-
+				'type'                     => 'b2',
+				'radius_normal'            => 'none',
+				'radius'                   => 'normal',
+				'post_title_position'      => 'outside',
+				'post_show_readingtime'    => '1',
+				'post_show_image'          => '1',
+				'post_show_excerpt'        => '1',
+				'post_show_date'           => 'relative',
+				'post_show_author'         => '1',
+				'link_color_post_title'    => 'light',
+				'link_color'               => 'light',
+				'key'                      => 'blog',
+				'image_mask'               => 'parallelogram-4',
+				'height'                   => 'fullscreen',
+				'heading_position'         => 'center',
+				'heading'                  => $_title,
+				'effect'                   => 'dark',
+				'coverratio'               => '1:1',
+				'count'                    => 4,
+				'color_text'               => '#333333',
+				'color_heading'            => '#ffffff',
+				'btn_viewall_check'        => 1,
+				'btn_viewall'              => T_("View all"),
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#674c4b',
+				'background_gradient_from' => '#1f1004',
+				'background_color'         => '#8fa2a6',
 			]
 		];
 	}
