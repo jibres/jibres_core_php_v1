86bcf00cb890fc6dd6f898e4b433c063208ccb4d
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-24 20:12:42 +0430

    save

--- content_site/body/gallery/g1.php
+++ content_site/body/gallery/g1.php
@@ -69,6 +69,10 @@ class g1
 				'p6',
 				'p7',
 				'p8',
+				'p9',
+				'p10',
+				'p11',
+				'p12',
 			],
 		];
 	}
@@ -121,7 +125,7 @@ class g1
 				'image_list' =>
 				[
 					[
-						// 'file'  => \dash\url::cdn().'/img/sitebuilder/gallery/g1/bg-3.jpg',
+						'file'  => \dash\url::cdn().'/img/sitebuilder/gallery/g1/bg-3.jpg',
 						'title' => 'Image 1',
 					],
 				],
@@ -229,18 +233,109 @@ class g1
 			'version'        => 1,
 			'options' =>
 			[
-				'image_count'             => 3,
+				'image_count'              => 3,
+				'type'                     => 'g1',
+				'radius'                   => '3xl',
+				'magicbox_title_position'  => 'hide',
+				'magicbox_gap'             => 'md',
+				'link_color'               => 'light',
+				'key'                      => 'gallery',
+				'image_random'             => 1,
+				'image_mask'               => 'hexagon',
+				'height'                   => 's',
+				'effect'                   => 'dark',
+				'coverratio'               => '1:1',
+				'container'                => '2xl',
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#fda085',
+				'background_gradient_from' => '#f6d365',
+				'background_color'         => '#a9c8c0',
+			],
+		];
+	}
+
+
+	public static function p7()
+	{
+		return
+		[
+			'preview_title'  => T_("Gallery Magic Box"). ' - '. T_("Sample :val", ['val' => \dash\fit::number(7)]),
+			'version'        => 1,
+			'options' =>
+			[
+				'image_count'              => 4,
+				'type'                     => 'g1',
+				'radius'                   => 'lg',
+				'magicbox_title_position'  => 'hide',
+				'magicbox_gap'             => 'md',
+				'link_color'               => 'light',
+				'key'                      => 'gallery',
+				'image_random'             => 1,
+				'image_mask'               => 'none',
+				'height'                   => 's',
+				'effect'                   => 'zoom',
+				'coverratio'               => 'free',
+				'container'                => '2xl',
+				'background_pack'          => 'none',
+			],
+		];
+	}
+
+
+	public static function p8()
+	{
+		return
+		[
+			'preview_title'  => T_("Gallery Magic Box"). ' - '. T_("Sample :val", ['val' => \dash\fit::number(8)]),
+			'version'        => 1,
+			'options' =>
+			[
+				'image_count'             => 4,
 				'type'                    => 'g1',
-				'radius'                  => '3xl',
+				'radius'                  => 'none',
 				'magicbox_title_position' => 'hide',
-				'magicbox_gap'            => 'md',
+				'magicbox_gap'            => 'none',
 				'link_color'              => 'light',
 				'key'                     => 'gallery',
 				'image_random'            => 1,
-				'height'                  => 's',
+				'image_mask'              => 'none',
+				'height'                  => 'auto',
 				'effect'                  => 'zoom',
 				'coverratio'              => 'free',
-				'container'               => '2xl',
+				'container'               => 'fluid',
+				'background_pack'         => 'none',
+			],
+		];
+	}
+
+
+	public static function p9()
+	{
+		return
+		[
+			'preview_title'  => T_("Gallery Magic Box"). ' - '. T_("Sample :val", ['val' => \dash\fit::number(9)]),
+			'version'        => 1,
+			'options' =>
+			[
+				'image_count'              => 5,
+				'type'                     => 'g1',
+				'radius'                   => 'lg',
+				'magicbox_title_position'  => 'hide',
+				'magicbox_gap'             => 'md',
+				'link_color'               => 'light',
+				'key'                      => 'gallery',
+				'image_random'             => 1,
+				'image_mask'               => 'none',
+				'height'                   => 's',
+				'effect'                   => 'zoom',
+				'coverratio'               => 'free',
+				'container'                => '2xl',
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#e2ebf0',
+				'background_gradient_from' => '#cfd9df',
+				'background_color'         => '#c6ac85',
 			],
 		];
 	}
