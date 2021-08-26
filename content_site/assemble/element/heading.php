6857a2e07388d1f1841cc5a8185c39992145ec55
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-26 13:37:35 +0430

    add heading class

---
+++ content_site/assemble/element/heading.php
@@ -0,0 +1,31 @@
+<?php
+namespace content_site\assemble\element;
+
+
+class heading
+{
+	public static function simple1($_args)
+	{
+		$color_heading    = a($_args, 'color_heading:full_style');
+		$heading_class    = a($_args, 'heading:class');
+
+		$heading = '';
+		if(a($_args, 'heading') !== null)
+		{
+			$heading .= '<header>';
+			{
+				$heading .= "<h2 class='text-3xl font-black leading-6 mb-5 $heading_class' $color_heading>";
+				{
+					$heading .= a($_args, 'heading');
+				}
+				$heading .= '</h2>';
+			}
+			$heading .= '</header>';
+		}
+
+		return $heading;
+	}
+
+
+}
+?>
--- content_site/body/gallery/html/g1.php
+++ content_site/body/gallery/html/g1.php
@@ -6,30 +6,12 @@ class g1
 {
 	public static function html($_args, $_image_list)
 	{
-		$html             = '';
-
-		// define variables
-		$color_heading    = a($_args, 'color_heading:full_style');
-		$heading_class    = a($_args, 'heading:class');
-
-		$html .= \content_site\assemble\element\section::element_start($_args);
+		$html = \content_site\assemble\element\section::element_start($_args);
 		{
 			$container = a($_args, 'container:class');
 			$html .= "<div class='$container m-auto relative'>";
 			{
-				// if(a($_args, 'heading') !== null)
-				// {
-				// 	$html .= '<header>';
-				// 	{
-				// 		$html .= "<h2 class='text-3xl font-black leading-6 mb-5 $heading_class' $color_heading>";
-				// 		{
-				// 			$html .= a($_args, 'heading');
-				// 		}
-				// 		$html .= '</h2>';
-				// 	}
-				// 	$html .= '</header>';
-				// }
-
+				// $html .= \content_site\assemble\element\heading::simple1($_args);
 
 				$grid_cols = 'grid grid-cols-'. count($_image_list);
 				if(count($_image_list) > 12)
@@ -46,12 +28,10 @@ class g1
 					$html .= \content_site\assemble\element\magicbox::html($_args, $_image_list);
 				}
 				$html .= '</div>';
-
 			}
 			$html .= "</div>";
 		}
 		$html .= \content_site\assemble\element\section::element_end($_args);
-
 
 		return $html;
 	}
