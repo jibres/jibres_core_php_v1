2df26068409b54e7a2f8905185659f6ba3091e13
Reza <rm.biqarar@gmail.com>
2021-08-26 18:14:07 +0430

    Fix show all post list

--- content_business/n/display.php
+++ content_business/n/display.php
@@ -1,8 +1,52 @@
 <?php
 if(\dash\data::displayShowPostList())
 {
-	require_once(root. '/content_cms/posts/home/display.php');
-	// show post list
+	$html = '';
+	$html .= '<nav class="items">';
+	{
+
+	  $html .= '<ul>';
+	  {
+
+	    foreach (\dash\data::dataTable() as $key => $value)
+	    {
+	      $date_title = '';
+	      if(a($value, 'datemodified'))
+	      {
+	        $date_title .= T_("Date modified"). ': '. \dash\fit::date_time(a($value, 'datemodified')). "\n";
+	      }
+	      if(a($value, 'publishdate'))
+	      {
+	        $date_title .= T_("Publish date"). ': '. \dash\fit::date_time(a($value, 'publishdate'));
+	      }
+
+	     $html .= '<li>';
+	     {
+
+		     $html .= '<a class="item f align-center" href="'.  a($value, 'link') . '">';
+		     {
+
+				if(a($value, 'thumb'))
+				{
+			        $html .= '<img src="'. \dash\fit::img(a($value, 'thumb')). '" alt="'. T_("Post image"). '">';
+				}
+
+		        $html .= '<div class="key">'.  a($value, 'title'). '</div>';
+		        $html .= '<time class="value" datatime="'. $date_title. '">'. \dash\fit::date_time(a($value, 'datecreated')). '</time>';
+		        $html .= '<div class="go '. $value['icon_list']. '"></div>';
+		     }
+		     $html .= '</a>';
+	     }
+	     $html .= '</li>';
+	    } // endfi
+	  }
+	  $html .= '</ul>';
+	}
+	$html .= '</nav>';
+
+	echo $html;
+
+	\dash\utility\pagination::html();
 }
 else
 {
