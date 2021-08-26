ab9f2b7fc783b60c1dc697ff45df74795eb186a6
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-26 17:24:52 +0430

    create each item of magicbox in another function

--- content_site/assemble/element/magicbox.php
+++ content_site/assemble/element/magicbox.php
@@ -8,112 +8,119 @@ class magicbox
 	{
 		$html = '';
 
-		foreach ($_datalist as $key => $value)
+		foreach ($_datalist as $key => $item)
 		{
-			$myTitle      = a($value, 'title');
-			$borderRadius = a($_args, 'radius:class');
-			$effect       = a($_args, 'effect');
-			$maskImg      = a($_args, 'image_mask:class');
-
-			if(!a($value, 'file'))
-			{
-				$value['file'] = \dash\sample\img::image();
-			}
-
-			$myThumb      = \lib\filepath::fix(\dash\fit::img(a($value, 'file'), 'raw'));
-
-			$myMagicBoxEl = 'div';
-			$myLinkHref   = '';
-			if(a($value, 'link'))
-			{
-				$myLinkHref   = "href='". a($value, 'link'). "'";
-				$myMagicBoxEl = 'a';
-			}
-
-			$card = '';
-			$magicBoxClass = '';
-			if($_magicModel === 'blog')
-			{
-				// get grid class name by analyse
-				$gridCol = \content_site\assemble\grid::className(a($_args, 'count'), count($_datalist), $key);
-				$magicBoxClass = "class='$gridCol flex flex-col max-w-md'";
-			}
-
-			$card .= "<$myMagicBoxEl data-magicbox='$effect' $myLinkHref $magicBoxClass>";
-			{
-				// thumb
-				// if($myThumb && a($_args, 'post_show_image'))
-				{
-					$pictureClass = 'transition shadow-sm hover:shadow-md';
-					if(a($_args, 'coverratio:class'))
-					{
-						$pictureClass .= ' '. a($_args, 'coverratio:class');
-					}
-					if($maskImg)
-					{
-						$pictureClass .= ' '. $maskImg;
-					}
-					if($borderRadius)
-					{
-						$pictureClass .= ' '. $borderRadius;
-					}
-					if($effect !== 'zoom')
-					{
-						$pictureClass .= ' overflow-hidden';
-					}
-
-					$card .= "<picture class='$pictureClass'>";
-					{
-						$imgClass = 'object-cover';
-						if(a($_args, 'coverratio') === 'free')
-						{
-							$imgClass = 'h-auto';
-						}
-						if($borderRadius)
-						{
-							$imgClass .= ' '. $borderRadius;
-						}
-						$card .= "<img loading='lazy' class='$imgClass' src='#' data-src='$myThumb' alt='$myTitle'>";
-					}
-					$card .= "</picture>";
-				}
-				$linkColorClass = 'link-'. a($_args, 'link_color');
-				$linkAlign = '';
-				if($borderRadius === 'rounded-full')
-				{
-					$linkAlign = 'text-center';
-				}
-				elseif($maskImg)
-				{
-					$linkAlign = 'text-center';
-				}
-
-				if(a($_args, 'magicbox_title_position') === 'inside')
-				{
-					// title
-					$card .= "<h3 class='absolute inset-x-0 bottom-0 block $linkColorClass leading-7 transition text-white px-4 py-2 line-clamp-3 z-10 $linkAlign'>";
-					{
-						$card .= $myTitle;
-					}
-					$card .= '</h3>';
-				}
-				elseif(a($_args, 'magicbox_title_position') === 'outside')
-				{
-					// title
-					$card .= "<h3 class='block $linkColorClass leading-7 transition text-white px-4 py-2 line-clamp-3 z-10 $linkAlign'>";
-					{
-						$card .= $myTitle;
-					}
-					$card .= '</h3>';
-				}
-
-			}
-			$card .= "</$myMagicBoxEl>";
-
-			$html .= $card;
+			$html .= self::eachItem($_args, $_datalist, $_magicModel, $key, $item);
 		}
 
 		return $html;
 	}
+
+
+	public static function eachItem($_args, $_datalist, $_magicModel, $_key, $_item)
+	{
+		$myTitle      = a($_item, 'title');
+		$borderRadius = a($_args, 'radius:class');
+		$effect       = a($_args, 'effect');
+		$maskImg      = a($_args, 'image_mask:class');
+
+		if(!a($_item, 'file'))
+		{
+			$_item['file'] = \dash\sample\img::image();
+		}
+
+		$myThumb      = \lib\filepath::fix(\dash\fit::img(a($_item, 'file'), 'raw'));
+
+		$myMagicBoxEl = 'div';
+		$myLinkHref   = '';
+		if(a($_item, 'link'))
+		{
+			$myLinkHref   = "href='". a($_item, 'link'). "'";
+			$myMagicBoxEl = 'a';
+		}
+
+		$card = '';
+		$magicBoxClass = '';
+		if($_magicModel === 'blog')
+		{
+			// get grid class name by analyse
+			$gridCol = \content_site\assemble\grid::className(a($_args, 'count'), count($_datalist), $_key);
+			$magicBoxClass = "class='$gridCol flex flex-col max-w-md'";
+		}
+
+		$card .= "<$myMagicBoxEl data-magicbox='$effect' $myLinkHref $magicBoxClass>";
+		{
+			// thumb
+			// if($myThumb && a($_args, 'post_show_image'))
+			{
+				$pictureClass = 'transition shadow-sm hover:shadow-md';
+				if(a($_args, 'coverratio:class'))
+				{
+					$pictureClass .= ' '. a($_args, 'coverratio:class');
+				}
+				if($maskImg)
+				{
+					$pictureClass .= ' '. $maskImg;
+				}
+				if($borderRadius)
+				{
+					$pictureClass .= ' '. $borderRadius;
+				}
+				if($effect !== 'zoom')
+				{
+					$pictureClass .= ' overflow-hidden';
+				}
+
+				$card .= "<picture class='$pictureClass'>";
+				{
+					$imgClass = 'object-cover';
+					if(a($_args, 'coverratio') === 'free')
+					{
+						$imgClass = 'h-auto';
+					}
+					if($borderRadius)
+					{
+						$imgClass .= ' '. $borderRadius;
+					}
+					$card .= "<img loading='lazy' class='$imgClass' src='#' data-src='$myThumb' alt='$myTitle'>";
+				}
+				$card .= "</picture>";
+			}
+			$linkColorClass = 'link-'. a($_args, 'link_color');
+			$linkAlign = '';
+			if($borderRadius === 'rounded-full')
+			{
+				$linkAlign = 'text-center';
+			}
+			elseif($maskImg)
+			{
+				$linkAlign = 'text-center';
+			}
+
+			if(a($_args, 'magicbox_title_position') === 'inside')
+			{
+				// title
+				$card .= "<h3 class='absolute inset-x-0 bottom-0 block $linkColorClass leading-7 transition text-white px-4 py-2 line-clamp-3 z-10 $linkAlign'>";
+				{
+					$card .= $myTitle;
+				}
+				$card .= '</h3>';
+			}
+			elseif(a($_args, 'magicbox_title_position') === 'outside')
+			{
+				// title
+				$card .= "<h3 class='block $linkColorClass leading-7 transition text-white px-4 py-2 line-clamp-3 z-10 $linkAlign'>";
+				{
+					$card .= $myTitle;
+				}
+				$card .= '</h3>';
+			}
+
+		}
+		$card .= "</$myMagicBoxEl>";
+
+		return $card;
+	}
+
 }
 ?>
