55adfcb6472e9c0b737a66e075cbae3f3313cb79
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-27 18:55:11 +0430

    fix

--- content_site/options/style/style.php
+++ content_site/options/style/style.php
@@ -21,7 +21,7 @@ class style
       		$html .= "<a class='item f' href='$url'>";
       		{
         		$html .= '<img alt="Style" class="bg-gray-100 hover:bg-gray-200 p-2" src="'. \dash\utility\icon::url('Colors'). '">';
-        		$html .= '<div class="key">'. T_("Personalization Desgin"). '</div>';
+        		$html .= '<div class="key">'. T_("Personalize"). '</div>';
         		$html .= '<div class="go"></div>';
       		}
       		$html .= '</a>';
