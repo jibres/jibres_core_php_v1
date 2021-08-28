98fa149dc9e1c01709c37b32060dc96b7359ffdc
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-28 18:56:29 +0430

    add p1

--- content_site/body/product/html/p1.php
+++ content_site/body/product/html/p1.php
@@ -1,12 +1,31 @@
 <?php
-namespace content_site\body\product\html;
+namespace content_site\body\blog\html;
 
 
-class p1
+class b1
 {
-	public static function html($_args, $_productList)
+	public static function html($_args, $_blogList)
 	{
-		return \content_site\body\blog\html\b1::html($_args, $_productList);
+		$html = \content_site\assemble\wrench\section::element_start($_args);
+		{
+			$html .= \content_site\assemble\wrench\section::container_auto($_args, a($_args, 'count'));
+			{
+				$html .= \content_site\assemble\wrench\heading::simple1($_args);
+
+				$html .= \content_site\assemble\wrench\section::grid_12($_args);
+				{
+					$html .= \content_site\assemble\element\card::html($_args, $_blogList);
+				}
+				$html .= '</div>';
+
+				$html .= \content_site\body\blog\share::btn_viewall($_args);
+
+			}
+			$html .= "</div>";
+		}
+		$html .= \content_site\assemble\wrench\section::element_end($_args);
+
+		return $html;
 	}
 }
 ?>
--- content_site/body/product/p1.php
+++ content_site/body/product/p1.php
@@ -1,8 +1,8 @@
 <?php
-namespace content_site\body\product;
+namespace content_site\body\blog;
 
 
-class p1
+class b1
 {
 
 	/**
@@ -19,12 +19,19 @@ class p1
 			[
 				'heading_full',
 
-				'product_tag',
-				'product_order',
-				'count_product',
-				'product_show_image',
-				'product_show_excerpt',
+				'post_tag',
+				'post_template',
+				'post_order',
+				'count_post',
+
+				'post_show_image',
+				'post_show_readingtime',
+				'post_show_excerpt',
+				'post_show_author',
+
 				'btn_viewall',
+				'post_show_date',
+
 				// sub page
 				'style' => \content_site\utility::set_style_option(
 				[
@@ -40,11 +47,15 @@ class p1
 			],
 			'default'      =>
 			[
-				'heading'               => T_("Products"),
-				'product_order'            => 'latest',
+				'heading'               => T_("Latest Posts"),
+				'post_template'         => 'any',
+				'post_order'            => 'latest',
 				'count'                 => 3,
-				'product_show_excerpt'     => true,
-				'product_show_image'       => true,
+				'post_show_excerpt'     => true,
+				'post_show_image'       => true,
+				'post_show_date'        => 'relative',
+				'post_show_author'      => true,
+				'post_show_readingtime' => true,
 				'btn_viewall_check'     => true,
 				'btn_viewall'           => T_("View all"),
 				'background_pack'       => 'none',
@@ -58,6 +69,14 @@ class p1
 			'preview_list' =>
 			[
 				'p1',
+				'p2',
+				'p3',
+				'p4',
+				'p5',
+				'p6',
+				'p7',
+				'p8',
+				'p9',
 			],
 		];
 	}
@@ -68,14 +87,17 @@ class p1
 	 */
 	public static function p1($_title = null)
 	{
-		$_title = T_("Products");
+		$_title = T_("Latest Posts");
 		return
 		[
 			'version'        => 1,
 			'options' =>
 			[
-				'product_show_image'       => 1,
-				'product_show_excerpt'     => 1,
+				'post_show_readingtime' => 1,
+				'post_show_image'       => 1,
+				'post_show_excerpt'     => 1,
+				'post_show_date'        => 'relative',
+				'post_show_author'      => 1,
 				'height'                => 'md',
 				'heading_position'      => 'center',
 				'heading'               => $_title,
@@ -85,7 +107,254 @@ class p1
 				'radius'                => 'lg',
 				'btn_viewall_check'     => 1,
 				'btn_viewall'           => T_('View all'),
+				'btn_viewall_mode'      => 'dark',
 				'background_pack'       => 'none',
+			],
+		];
+	}
+
+
+	public static function p2($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime' => null,
+				'post_show_image'       => 1,
+				'post_show_excerpt'     => 1,
+				'post_show_date'        => 'no',
+				'post_show_author'      => null,
+				'height'                => 'md',
+				'heading_position'      => 'center',
+				'heading'               => $_title,
+				'coverratio'            => '1:1',
+				'count'                 => 4,
+				'color_text'            => '#333333',
+				'radius'                => 'lg',
+				'btn_viewall_check'     => 1,
+				'btn_viewall'           => T_('View all'),
+				'btn_viewall_mode'      => 'dark',
+				'background_pack'       => 'solid',
+				'background_color'      => '#dec4d6',
+			],
+		];
+	}
+
+
+	public static function p3($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime'    => null,
+				'post_show_image'          => 1,
+				'post_show_excerpt'        => 1,
+				'post_show_date'           => 'date',
+				'post_show_author'         => null,
+				'height'                   => 'lg',
+				'heading_position'         => 'center',
+				'heading'                  => null,
+				'coverratio'               => '16:9',
+				'count'                    => 2,
+				'color_text'               => '#333333',
+				'radius'                   => 'lg',
+				'btn_viewall_check'        => null,
+				'btn_viewall'              => T_('View all'),
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#1e19c5',
+				'background_gradient_from' => '#f81b73',
+			],
+		];
+	}
+
+
+	public static function p4($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime' => null,
+				'post_show_image'       => null,
+				'post_show_excerpt'     => 1,
+				'post_show_date'        => 'full',
+				'post_show_author'      => 1,
+				'height'                => 'lg',
+				'heading_position'      => 'center',
+				'heading'               => null,
+				'coverratio'            => '16:9',
+				'count'                 => 6,
+				'color_text'            => '#333333',
+				'radius'                => 'lg',
+				'btn_viewall_check'     => null,
+				'background_pack'       => 'solid',
+				'background_color'      => '#90cdc3',
+			],
+		];
+	}
+
+
+	public static function p5($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime'    => null,
+				'post_show_image'          => null,
+				'post_show_excerpt'        => 1,
+				'post_show_date'           => 'no',
+				'post_show_author'         => null,
+				'height'                   => 'lg',
+				'heading_position'         => 'center',
+				'heading'                  => null,
+				'coverratio'               => '16:9',
+				'count'                    => 1,
+				'color_text'               => '#333333',
+				'radius'                   => 'lg',
+				'btn_viewall_check'        => null,
+				'btn_viewall'              => T_('View all'),
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#e378d1',
+				'background_gradient_from' => '#9ef5e6',
+			],
+		];
+	}
+
+
+	public static function p6($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime'    => null,
+				'post_show_image'          => 1,
+				'post_show_excerpt'        => 1,
+				'post_show_date'           => 'relative',
+				'post_show_author'         => 1,
+				'height'                   => 'md',
+				'heading_position'         => 'center',
+				'heading'                  => null,
+				'coverratio'               => '3:4',
+				'count'                    => 4,
+				'color_text'               => '#333333',
+				'radius'                   => 'lg',
+				'btn_viewall_check'        => null,
+				'btn_viewall'              => T_("View all"),
+				'background_pack'          => 'solid',
+				'background_gradient_type' => 'to bottom right',
+				'background_gradient_to'   => '#e378d1',
+				'background_gradient_from' => '#9ef5e6',
+				'background_color'         => '#e7cba9',
+			],
+		];
+	}
+
+
+	public static function p7($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime'    => null,
+				'post_show_image'          => 1,
+				'post_show_excerpt'        => 1,
+				'post_show_date'           => 'no',
+				'post_show_author'         => null,
+				'height'                   => 'sm',
+				'heading_position'         => 'center',
+				'heading'                  => null,
+				'coverratio'               => '3:1',
+				'count'                    => 9,
+				'color_text'               => '#022b79',
+				'radius'                   => 'lg',
+				'btn_viewall_check'        => null,
+				'btn_viewall'              => T_('View all'),
+				'background_size'          => 'auto',
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom',
+				'background_gradient_to'   => '#ffffff',
+				'background_gradient_from' => '#c1e3ec',
+			],
+		];
+	}
+
+
+	public static function p8($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime' => null,
+				'post_show_image'       => 1,
+				'post_show_excerpt'     => null,
+				'post_show_date'        => 'no',
+				'post_show_author'      => null,
+				'height'                => 'md',
+				'heading_position'      => 'left',
+				'heading'               => $_title,
+				'coverratio'            => '16:9',
+				'count'                 => 7,
+				'color_text'            => '#333333',
+				'radius'                => 'lg',
+				'btn_viewall_check'     => null,
+				'btn_viewall'           => T_("View all"),
+				'background_pack'       => 'solid',
+				'background_color'      => '#eeeeee',
+			],
+		];
+	}
+
+
+	public static function p9($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_readingtime'    => null,
+				'post_show_image'          => null,
+				'post_show_excerpt'        => 1,
+				'post_show_date'           => 'no',
+				'post_show_author'         => null,
+				'height'                   => 'md',
+				'heading_position'         => 'left',
+				'heading'                  => $_title,
+				'coverratio'               => '16:9',
+				'count'                    => 9,
+				'color_text'               => '#333333',
+				'radius'                   => 'lg',
+				'btn_viewall_check'        => 1,
+				'btn_viewall'              => T_("View all"),
+				'btn_viewall_mode'         => 'dark',
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom left',
+				'background_gradient_to'   => '#b3c6db',
+				'background_gradient_from' => '#e4f2f7',
+				'background_color'         => '#eeeeee',
 			],
 		];
 	}
