28b9ab9a7845c5c17e8c64ae668a703585d406ff
Reza <rm.biqarar@gmail.com>
2021-08-29 10:01:35 +0430

    Add sample quote

---
+++ content_site/body/quote/option.php
@@ -0,0 +1,65 @@
+<?php
+namespace content_site\body\quote;
+
+
+class option
+{
+
+	/**
+	 * Call when publish the page
+	 *
+	 * @return     bool  ( description_of_the_return_value )
+	 */
+	public static function premium()
+	{
+		return false;
+	}
+
+
+	/**
+	 * Get detail
+	 *
+	 * @return     array  ( description_of_the_return_value )
+	 */
+	public static function detail()
+	{
+		return
+		[
+			'group'   => T_("Quote"),
+			'section' => 'quote',
+			'title'   => T_("Quote"),
+			'icon'    => \dash\utility\icon::url('ChecklistAlternate'),
+		];
+	}
+
+
+
+	/**
+	 * Get model list
+	 *
+	 * @return     array  ( description_of_the_return_value )
+	 */
+	public static function model_list()
+	{
+		return
+		[
+			'quote1',
+
+		];
+
+	}
+
+
+	public static function popular()
+	{
+		return
+		[
+			'quote1:p1',
+		];
+	}
+
+
+
+
+}
+?>
---
+++ content_site/body/quote/quote1.php
@@ -0,0 +1,66 @@
+<?php
+namespace content_site\body\quote;
+
+
+class quote1
+{
+
+	/**
+	 * Style 1
+	 *
+	 * @return     array  ( description_of_the_return_value )
+	 */
+	public static function option()
+	{
+		return
+		[
+			'title'        => T_("Quote 1"),
+			'options'      =>
+			[
+				'heading_raw',
+				'description',
+
+				'style' => \content_site\utility::set_style_option(
+				[
+					'font',
+
+					'height',
+
+					'background_pack',
+
+				]
+				),
+			],
+			'default'      =>
+			[
+				'heading'          => T_("quote"),
+				'height'           => 'fullscreen',
+			],
+			'preview_list' =>
+			[
+				'p1',
+				'p2'
+			],
+		];
+	}
+
+
+	/**
+	 * Preview 1
+	 */
+	public static function p1()
+	{
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+
+			],
+		];
+	}
+
+
+
+}
+?>
