bf827dc0f5c7451da2064fb72600eca316620f9a
Reza <rm.biqarar@gmail.com>
2021-08-29 11:43:35 +0430

    Add static file url

---
+++ content_site/home/display-sidebar.php
@@ -0,0 +1,30 @@
+<?php
+$html = '';
+
+$html .= '<nav class="sections items">';
+{
+  $html .= '<ul>';
+  {
+    $staticfile = \lib\store::admin_url(). '/a/setting/staticfile';
+
+    $html .= '<li>';
+    {
+      $html .= "<a class='item f' href='". $staticfile. "'>";
+      {
+        $html .= '<div class="key">'. T_("Static file"). '</div>';
+        {
+          $html .= '<img class="p-4" src="'. \dash\utility\icon::url('Tools'). '">';
+        }
+        $html .= '</a>';
+      }
+    }
+    $html .= '</li>';
+  }
+  $html .= '</ul>';
+}
+$html .= '</nav>';
+
+
+
+echo $html;
+?>
