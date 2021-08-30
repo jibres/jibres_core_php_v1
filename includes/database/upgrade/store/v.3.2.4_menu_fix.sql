a7f5d69f04e07d6a9ec064ccf7c2a9a289c2df0f
Reza <rm.biqarar@gmail.com>
2021-08-30 09:50:45 +0430

    Add pointer self file

---
+++ includes/database/upgrade/store/v.3.2.4_menu_fix.sql
@@ -0,0 +1,1 @@
+ALTER TABLE jibres_XXXXXXX.menu CHANGE `pointer` `pointer` ENUM('homepage','products','posts','forms','tags','hashtag','socialnetwork','other','title','separator','selffile') CHARACTER SET utf8mb4  NULL DEFAULT NULL;
