2324c3b55ad642d3709826525d2bb0e73dcbf0b2
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-08-30 19:20:49 +0430

    fix card caption padding size

--- content_site/assemble/element/card.php
+++ content_site/assemble/element/card.php
@@ -136,7 +136,14 @@ class card
 			if($showCaptionBox)
 			{
 				$captionClass = 'flex-grow';
-				$captionClass .= ' px-6 py-4';
+				if($insideSlider)
+				{
+					$captionClass .= ' px-2 py-1 md:px-3 md:py-2 lg:px-4 lg:py-2';
+				}
+				else
+				{
+					$captionClass .= ' px-4 py-2 md:px-5 md:py-3 lg:px-6 lg:py-4';
+				}
 
 				$card .= "<div class='$captionClass '>";
 				{
