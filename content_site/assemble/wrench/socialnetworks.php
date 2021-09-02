d047fb6994850051cce8b1f91796c2e075d45851
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-02 14:42:56 +0430

    fix social links

--- content_site/assemble/wrench/socialnetworks.php
+++ content_site/assemble/wrench/socialnetworks.php
@@ -8,55 +8,73 @@ class socialnetworks
   {
     $html = '<nav class="social">';
     {
-      if(a($social, 'linkedin', 'link'))
+      if(a($_social, 'linkedin', 'link'))
       {
-        $html .= '<a target="_blank" href="'. a($social, 'linkedin', 'link') .'">';
-        $html .= '<img src="'. \dash\url::cdn()'/business/visitcard-1/img/linkedin.svg" alt=""';
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("linkedin"); ?>">
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/linkedin.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'linkedin', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("linkedin"). '"';
         $html .= '</a>';
       }
+
+      if(a($_social, 'github', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/github.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'github', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("github"). '"';
+        $html .= '</a>';
+      }
+
+      if(a($_social, 'facebook', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/facebook.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'facebook', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("facebook"). '"';
+        $html .= '</a>';
+      }
+
+      if(a($_social, 'twitter', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/twitter.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'twitter', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("twitter"). '"';
+        $html .= '</a>';
+      }
+
+      if(a($_social, 'email', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/email.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'email', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("email"). '"';
+        $html .= '</a>';
+      }
+
+      if(a($_social, 'instagram', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/instagram.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'instagram', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("instagram"). '"';
+        $html .= '</a>';
+      }
+
+
+      if(a($_social, 'telegram', 'link'))
+      {
+        $imgSrc = \dash\url::cdn(). '/img/social/type1/telegram.svg';
+
+        $html .= '<a target="_blank" href="'. a($_social, 'telegram', 'link') .'">';
+        $html .= '<img class="overflow-hidden rounded-full" src="'. $imgSrc. '" alt="'. T_("telegram"). '"';
+        $html .= '</a>';
+      }
+
     }
     $html .= '</nav>';
 
+    return $html;
   }
 }
-
-<?php $social = \lib\store::social(); ?>
-        <div class="social">
-<?php if(a($social, 'linkedin', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'linkedin', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/linkedin.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("linkedin"); ?>">
-          </a>
-<?php } //endif ?>
-<?php if(a($social, 'github', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'github', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/github.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("github"); ?>">
-          </a>
-<?php } //endif ?>
-<?php if(a($social, 'facebook', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'facebook', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/facebook.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("facebook"); ?>">
-          </a>
-<?php } //endif ?>
-<?php if(a($social, 'twitter', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'twitter', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/twitter.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("twitter"); ?>">
-          </a>
-<?php } //endif ?>
-<?php if(a($social, 'email', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'email', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/arroba.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("Email"); ?>">
-          </a>
-<?php } //endif ?>
-<?php if(a($social, 'instagram', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'instagram', 'link'); ?>">
-            <img src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/instagram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("instagram"); ?>">
-          </a>
-<?php } // ?>
-<?php if(a($social, 'telegram', 'user')) {?>
-          <a target="_blank" href="<?php echo a($social, 'telegram', 'link'); ?>">
-            <img class="telegram" src="<?php echo \dash\url::cdn() ?>/business/visitcard-1/img/telegram.svg" alt=" <?php echo \lib\store::title(). ' - '. T_("telegram"); ?>">
-          </a>
-<?php } // ?>
-
-        </div>
