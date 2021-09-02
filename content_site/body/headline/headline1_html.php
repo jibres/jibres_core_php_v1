5e8fc90bf8f9376c5178e6508a3e51b321438872
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-02 18:10:41 +0430

    apply heading color

--- content_site/body/headline/headline1_html.php
+++ content_site/body/headline/headline1_html.php
@@ -7,25 +7,17 @@ class headline1_html
 
 	public static function html($_args)
 	{
-		$color_text       = a($_args, 'color_text:full_style');
+		$color_text    = a($_args, 'color_text:full_style');
+		$color_heading = a($_args, 'color_heading:full_style');
 
 		$html = \content_site\assemble\wrench\section::element_start($_args);
 		{
-			// $style = 'style="background-image:url('. \dash\url::cdn(). '/img/sitebuilder/headline/headline1/mesh.png);background-repeat:repeat;background-attachment:fixed;"';
-
-			// $style = 'style="background-image:linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))"';
-
-			// a($_args, 'background:full_style')
-
-			// $html .= '<div class="w-full h-full absolute" '. $style. '></div>';
-
-
 			$html .= \content_site\options\background\background_effect::html(a($_args, 'background_effect'));
 
 			$html .= '<div class="sm:max-w-xl p-5 md:p-10 lg:p-14 z-10">';
 			{
 
-				$html .='<h2 class="text-2xl sm:text-3xl md:text-4xl leading-normal sm:md:leading-normal md:leading-normal mb-2 sm:mb-4 md:mb-6" '. $color_text.'>';
+				$html .='<h2 class="text-2xl sm:text-3xl md:text-4xl leading-normal sm:md:leading-normal md:leading-normal mb-2 sm:mb-4 md:mb-6" '. $color_heading.'>';
 				{
 					$html .= a($_args, 'heading');
 				}
