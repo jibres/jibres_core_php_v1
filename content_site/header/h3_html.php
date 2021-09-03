25531284242dce5d98f0ae21c5be2776dff2b02a
Reza <rm.biqarar@gmail.com>
2021-09-03 11:55:41 +0430

    Fix check footer

--- content_site/header/h3_html.php
+++ content_site/header/h3_html.php
@@ -21,70 +21,74 @@ class h3_html
 		{
 			$html .= '<div class="max-w-screen-lg w-full px-2 sm:px-4 lg:px-5 m-auto">';
 			{
-				// action bar
-				$html .= '<div class="actionBar flex items-center p-1 sm:p-2 md:p-3 bg-gray-50 rounded shadow-inner">';
+				if(a($_args, 'logo') || a($_args, 'heading') || a($_args, 'link_search') || a($_args, 'link_cart') || a($_args, 'link_enter'))
 				{
-					$html .= '<a href="" class="flex-1">';
+					// action bar
+					$html .= '<div class="actionBar flex items-center p-1 sm:p-2 md:p-3 bg-gray-50 rounded shadow-inner">';
 					{
-						$logo = a($_args, 'logo');
-
-						if($logo)
+						$html .= '<a href="" class="flex-1">';
 						{
-							$html .= '<img class="inline-block w-16 h-16 rounded" src="'. $logo. '" alt="'. a($_args, 'heading'). '">';
-						}
+							$logo = a($_args, 'logo');
 
-						if(a($_args, 'heading'))
-						{
-							// add title
-							$html .= '<h1 class="inline-block px-2 text-2xl font-bold">';
+							if($logo)
 							{
-								$html .= $_args['heading'];
+								$html .= '<img class="inline-block w-16 h-16 rounded" src="'. $logo. '" alt="'. a($_args, 'heading'). '">';
 							}
-							$html .= '</h1>';
-						}
-					}
-					$html .= '</a>';
 
-
-					if(a($_args, 'link_search'))
-					{
-						$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/search">';
-						{
-							$html .= \dash\utility\icon::svg('search');
-						}
-						$html .= '</a>';
-					}
-
-					if(a($_args, 'link_cart'))
-					{
-						$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/cart">';
-						{
-							$html .= \dash\utility\icon::svg('cart');
-							$html .= '<span class="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 text-center leading-5 p-0.5 text-sm">';
-							$html .= '4';
-							$html .= '</span>';
+							if(a($_args, 'heading'))
+							{
+								// add title
+								$html .= '<h1 class="inline-block px-2 text-2xl font-bold">';
+								{
+									$html .= $_args['heading'];
+								}
+								$html .= '</h1>';
+							}
 						}
 						$html .= '</a>';
-					}
 
-					if(a($_args, 'link_enter'))
-					{
-						$enterTxt  = T_("Enter to account");
-						$enterLink = \dash\url::kingdom(). '/enter';
-						if(\dash\user::login())
-						{
-							$enterTxt  = T_("Profile");
-							$enterLink = \dash\url::kingdom(). '/profile';
-						}
-						$html .= '<a class="p-3 mx-1 rounded link-secondary" href="'. $enterLink. '">';
-						{
-							$html .= $enterTxt;
-						}
-						$html .= '</a>';
-					}
 
-				}
-				$html .= '</div>';
+						if(a($_args, 'link_search'))
+						{
+							$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/search">';
+							{
+								$html .= \dash\utility\icon::svg('search');
+							}
+							$html .= '</a>';
+						}
+
+						if(a($_args, 'link_cart'))
+						{
+							$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/cart">';
+							{
+								$html .= \dash\utility\icon::svg('cart');
+								$html .= '<span class="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 text-center leading-5 p-0.5 text-sm">';
+								$html .= '4';
+								$html .= '</span>';
+							}
+							$html .= '</a>';
+						}
+
+						if(a($_args, 'link_enter'))
+						{
+							$enterTxt  = T_("Enter to account");
+							$enterLink = \dash\url::kingdom(). '/enter';
+							if(\dash\user::login())
+							{
+								$enterTxt  = T_("Profile");
+								$enterLink = \dash\url::kingdom(). '/profile';
+							}
+							$html .= '<a class="p-3 mx-1 rounded link-secondary" href="'. $enterLink. '">';
+							{
+								$html .= $enterTxt;
+							}
+							$html .= '</a>';
+						}
+
+					}
+					$html .= '</div>';
+
+				} // empty header
 
 				if(a($_args, 'menu_1') || a($_args, 'menu_2'))
 				{
