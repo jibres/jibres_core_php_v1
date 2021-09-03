3b09bd7f1761819df1d62c60f6c43c9baaf96001
Reza <rm.biqarar@gmail.com>
2021-09-03 13:22:36 +0430

    Route hashtag by id

--- content_business/hashtag/controller.php
+++ content_business/hashtag/controller.php
@@ -8,8 +8,22 @@ class controller
 		$child = \dash\url::child();
 		if($child)
 		{
+
 			// default route
 			return false;
+		}
+
+		if(\dash\request::get('id'))
+		{
+			$id = \dash\validate::code(\dash\request::get('id'));
+			if($id)
+			{
+				$load = \dash\app\terms\get::get($id, true);
+				if(a($load, 'link'))
+				{
+					\dash\redirect::to($load['link']);
+				}
+			}
 		}
 
 		$load_hot_tag = \dash\app\terms\search::hot_tag();
