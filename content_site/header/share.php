11972afc2cfcaf63730d617576e9487220ca247a
Reza <rm.biqarar@gmail.com>
2021-09-03 14:30:49 +0430

    Add animation

--- content_site/header/share.php
+++ content_site/header/share.php
@@ -17,7 +17,7 @@ class share
 			$link = a($link_detail, 'link');
 			$target = a($link_detail, 'target_blank_html');
 
-			$html .= "<a href='$link' $target class='block text-center text-base'  style='height:50px;line-height:50px;background-color:#e73c7e;background:linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);background-size: 400% 400%;animation: topLineGradient 15s ease infinite;color: #fff;'>";
+			$html .= "<a href='$link' $target class='block text-center text-base'  style='@keyframes topLineGradient{0%{background-position: 0% 50%;}50%{background-position:100% 50%;}100%{background-position:% 50%;}}; height:50px;line-height:50px;background-color:#e73c7e;background:linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);background-size: 400% 400%;animation: topLineGradient 15s ease infinite;color: #fff; '>";
 			{
 				$html .= a($_args, 'announcement_description');
 			}
