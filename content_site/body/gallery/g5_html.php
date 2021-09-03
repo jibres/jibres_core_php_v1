86a5b23db3c99f32fc0efe7c89741bf8ae3cc4ef
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-03 20:09:25 +0430

    add g5

---
+++ content_site/body/gallery/g5_html.php
@@ -0,0 +1,33 @@
+<?php
+namespace content_site\body\gallery;
+
+
+class g5_html
+{
+	public static function html($_args, $_blogList)
+	{
+		$html = \content_site\assemble\wrench\section::element_start($_args);
+		{
+			$html .= \content_site\assemble\wrench\section::container_auto($_args, a($_args, 'count'));
+			{
+				$html .= \content_site\assemble\wrench\heading::simple1($_args);
+
+				$html .= \content_site\assemble\wrench\section::grid_12($_args);
+				{
+					$optMagicBox = [
+						'grid' => true,
+					];
+					$html .= \content_site\assemble\element\magicbox::html($_args, $_blogList, $optMagicBox);
+				}
+				$html .= '</div>';
+
+				$html .= \content_site\body\blog\share::btn_viewall($_args);
+			}
+			$html .= "</div>";
+		}
+		$html .= \content_site\assemble\wrench\section::element_end($_args);
+
+		return $html;
+	}
+}
+?>
