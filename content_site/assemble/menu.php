c9a59d1301cf2b39499c815290a820afa4ea5341
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-03 15:54:18 +0430

    fix menu

--- content_site/assemble/menu.php
+++ content_site/assemble/menu.php
@@ -5,24 +5,89 @@ namespace content_site\assemble;
 class menu
 {
 
-	public static function generate($_menu_id, $_class = null)
+	/**
+	 * based on wp menu creator
+	 * https://developer.wordpress.org/reference/functions/wp_nav_menu/
+	 * @param  [type] $_menu_id [description]
+	 * @param  [type] $_arg     [description]
+	 * @return [type]           [description]
+	 */
+	public static function generate($_menu_id, $_arg = null)
 	{
-		$html = '';
-
 		$load_menu = \lib\app\menu\get::load_menu($_menu_id);
 
-		if(is_array(a($load_menu, 'list')))
+		$html = '<nav';
+		if(a($_arg, 'nav_class'))
 		{
-			foreach ($load_menu['list'] as $key => $value)
-			{
-				$target = a($value, 'target') ? 'target="_blank"' : null;
-
-				$html .= "<a href='$value[url]' $target class='$_class'>$value[title]</a>";
-			}
+			$html .= ' class="'. a($_arg, 'nav_class'). '"';
 		}
+		$html .= '>';
+		// loop to create list item
+		{
+			$html .= self::menuLi($load_menu['list'], 1, $_arg);
+		}
+		$html .= '</nav>';
 
-		return $html;
+		return;
 	}
 
+
+
+	private static function menuLi($_list, $_layer, $_arg)
+	{
+		if($_layer > 5)
+		{
+			return null;
+		}
+
+		$menuLi = '<ul';
+		if(a($_arg, 'ul_class'))
+		{
+			$html .= ' class="'. a($_arg, 'ul_class'). '"';
+		}
+		$menuLi .= '>';
+
+		foreach ($_list as $key => $myLiData)
+		{
+			$menuLi .= '<li';
+			$menuLi .= ' data-layer="'. $_layer. '"';
+			$menuLi .= ' data-key="'. $key. '"';
+
+			if(a($_arg, 'li_class'))
+			{
+				$menuLi .= ' class="'. a($_arg, 'li_class'). '"';
+			}
+			$menuLi .= '>';
+			{
+				$menuLi .= self::menuLink(a($myLiData, 'title'), a($myLiData, 'url'), a($myLiData, 'target'));
+				if(isset($myLiData['child']) && count($myLiData['child']))
+				{
+					$menuLi .= self::menuLi($myLiData['child'], $_layer + 1, $_arg);
+				}
+			}
+			$menuLi .= '</li>';
+		}
+		$menuLi .= '</ul>';
+		return $menuLi;
+	}
+
+
+	private static function menuLink($_text, $_link = null, $_target = null)
+	{
+		$menuLinkEl = '<a';
+		if($_target)
+		{
+			$menuLinkEl .= ' target="_blank"';
+		}
+		if($_link)
+		{
+			$menuLinkEl .= ' href="'. $_link. '"';
+		}
+		$menuLinkEl .= '>';
+		$menuLinkEl .= $_text;
+		$menuLinkEl .= '</a>';
+
+		return $menuLinkEl;
+	}
 }
 ?>
