5595f848c037f56c81b292766446be09a56399d8
Reza <rm.biqarar@gmail.com>
2021-09-06 17:56:30 +0430

    Fix

--- content_business/tag/display-all-tag-list.php
+++ content_business/tag/display-all-tag-list.php
@@ -3,7 +3,7 @@
 <div class="avand category">
     <div class="row">
       <?php foreach ($categoryDataTable as $key => $value) {?>
-        <div class="c-xs-6 c-sm-6 c-md-4 c-lg-4 c-xl-3 c-xxl-2">
+        <div class="c-xs-6 c-sm-6 c-md-4 c-lg-4 c-xl-4 c-xxl-4">
           <div class="roundedBox"<?php if(a($value, 'file_default') === true) { echo ' data-gr="'.rand(1, 20).'"';} ?>>
             <a class="overlay"<?php if(a($value, 'url')) { echo ' href="'.  a($value, 'url'). '"'; if(a($value, 'target')) { echo ' target="_blank"'; }} ?>>
               <figure>
