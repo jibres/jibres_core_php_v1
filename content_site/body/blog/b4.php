b62023df59408730ac7e2414e7e81660a97e70c0
Reza <rm.biqarar@gmail.com>
2021-09-06 15:05:26 +0430

    Add sample premium blog

---
+++ content_site/body/blog/b4.php
@@ -0,0 +1,204 @@
+<?php
+namespace content_site\body\blog;
+
+
+class b4
+{
+
+	public static function is_private()
+	{
+		return !\dash\url::isLocal();
+	}
+
+
+	public static function premium()
+	{
+		return true;
+	}
+
+	/**
+	 * Style 1
+	 *
+	 * @return     array  ( description_of_the_return_value )
+	 */
+	public static function option()
+	{
+		return
+		[
+			'title'        => T_("Premium"),
+			'options'      =>
+			[
+				'heading_full',
+				'description',
+				'post_tag',
+				'post_template',
+				'post_order',
+				'count_post_50',
+				'post_show_date',
+				'post_show_excerpt',
+				'post_show_read_more',
+				'btn_viewall',
+				'style' => \content_site\utility::set_style_option(
+				[
+					'font',
+					'height',
+					'background_pack',
+					'color_heading',
+					'color_text',
+					'link_color_post_read_more',
+					'btn_viewall_mode',
+				]),
+				'responsive' => \content_site\utility::set_responsive_option(),
+
+			],
+			'default'      =>
+			[
+				'heading'           => T_("Latest Posts"),
+				'post_template'     => 'any',
+				'post_order'        => 'latest',
+				'count'             => 3,
+				'post_show_date'    => 'relative',
+				'post_show_excerpt' => true,
+				'btn_viewall_check' => true,
+				'btn_viewall'       => T_("View all"),
+				'background_pack'   => 'none',
+				'height'            => 'md',
+				'color_text'        => '#333333',
+				'heading_position'  => 'center',
+				'btn_viewall_mode'  => 'dark',
+			],
+
+			'preview_list' =>
+			[
+				'p1',
+				'p2',
+				'p3',
+				'p4',
+			],
+		];
+	}
+
+
+	/**
+	 * Preview 1
+	 */
+	public static function p1($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_read_more' => true,
+				'post_show_excerpt'   => true,
+				'post_show_date'      => 'relative',
+				'link_color'          => 'primary',
+				'height'              => 'fullscreen',
+				'heading_position'    => 'left',
+				'heading'             => $_title,
+				'description'         => 'All the latest news, straight from the team.',
+				'count'               => 5,
+				'color_text'          => '#444444',
+				'color_heading'       => '#000000',
+				'btn_viewall_mode'    => 'dark',
+				'btn_viewall_check'   => '1',
+				'btn_viewall'         => T_("View all"),
+				'background_pack'     => 'none',
+			],
+		];
+	}
+
+
+	public static function p2($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_read_more' => null,
+				'post_show_excerpt'   => true,
+				'post_show_date'      => 'no',
+				'link_color'          => 'primary',
+				'height'              => 'fullscreen',
+				'heading_position'    => 'left',
+				'heading'             => $_title,
+				'description'         => 'All the latest news, straight from the team.',
+				'count'               => 2,
+				'color_text'          => '#444444',
+				'color_heading'       => '#000000',
+				'btn_viewall_mode'    => 'secondary',
+				'btn_viewall_check'   => '1',
+				'btn_viewall'         => T_("View all"),
+				'background_pack'     => 'solid',
+				'background_color'    => '#eeeeee',
+			],
+		];
+	}
+
+
+	public static function p3($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_read_more' => null,
+				'post_show_excerpt'   => true,
+				'post_show_date'      => 'date',
+				'link_color'          => 'primary',
+				'height'              => 'fullscreen',
+				'heading_position'    => 'left',
+				'heading'             => $_title,
+				'description'         => 'All the latest news, straight from the team.',
+				'count'               => 2,
+				'color_text'          => '#ffffff',
+				'color_heading'       => '#c2b500',
+				'btn_viewall_mode'    => 'light',
+				'btn_viewall_check'   => '1',
+				'btn_viewall'         => T_("View all"),
+				'background_pack'     => 'solid',
+				'background_color'    => '#000000',
+			],
+		];
+	}
+
+
+	public static function p4($_title = null)
+	{
+		$_title = T_("Latest Posts");
+		return
+		[
+			'version'        => 1,
+			'options' =>
+			[
+				'post_show_read_more'      => null,
+				'post_show_excerpt'        => null,
+				'post_show_date'           => 'full',
+				'link_color'               => 'primary',
+				'key'                      => 'blog',
+				'height'                   => 'fullscreen',
+				'heading_position'         => 'left',
+				'heading'                  => $_title,
+				'description'              => 'All the latest news, straight from the team.',
+				'count'                    => 2,
+				'color_text'               => '#ffffff',
+				'color_heading'            => '#ffffff',
+				'btn_viewall_mode'         => 'light',
+				'btn_viewall_check'        => '1',
+				'btn_viewall'              => T_("View all"),
+				'background_pack'          => 'gradient',
+				'background_gradient_type' => 'to bottom left',
+				'background_gradient_to'   => '#bac8e0',
+				'background_gradient_from' => '#6a85b6',
+				'background_color'         => '#000000',
+			],
+		];
+	}
+
+}
+?>
---
+++ content_site/body/blog/b4_html.php
@@ -0,0 +1,119 @@
+<?php
+namespace content_site\body\blog;
+
+
+class b4_html
+{
+	public static function html($_args, $_blogList)
+	{
+		$color_heading    = a($_args, 'color_heading:full_style');
+		$color_text       = a($_args, 'color_text:full_style');
+
+		$html = \content_site\assemble\wrench\section::element_start($_args);
+		{
+			$containerClass = 'max-w-3xl w-full m-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 divide-y divide-gray-200';
+			$html .= '<div class="'. $containerClass. '">';
+			{
+				if(a($_args, 'heading') !== null)
+				{
+					$html .= '<header class="pt-6 pb-8 space-y-2">';
+					{
+						$heading_class    = a($_args, 'heading:class');
+						$html .= "<h2 class='font-bold text-4xl md:text-5xl lg:text-6xl $heading_class' $color_heading>";
+						{
+							$html .= a($_args, 'heading');
+						}
+						$html .= '</h2>';
+
+						$html .= '<p class="text-xl opacity-60"'.$color_text.'>';
+						{
+							$html .= a($_args, 'description');
+						}
+						$html .= '</p>';
+					}
+					$html .= '</header>';
+				}
+
+				$html .= '<div class="divide-y divide-gray-200">';
+				{
+
+					foreach ($_blogList as $key => $value)
+					{
+						$myLink       = a($value, 'link');
+						$myTitle      = a($value, 'title');
+						$myThumb      = \dash\fit::img(a($value, 'thumb'), 460);
+						$myExcerpt    = a($value, 'excerpt');
+						$myDate       = a($value, 'publishdate');
+						$myAuthorPage = a($value, 'authorpage');
+						$writerName   = a($value, 'user_detail', 'displayname');
+
+						$eachItemClass = 'py-5 lg:py-12 space-y-2';
+						$postTextClass = 'space-y-2';
+
+						if(a($_args, 'post_show_date') !== 'no')
+						{
+							$eachItemClass = 'py-5 lg:py-12 space-y-2 lg:grid lg:grid-cols-4 lg:space-y-0 lg:items-baseline';
+							$postTextClass = 'space-y-2 lg:col-span-3';
+						}
+
+						$html .= '<article class="'. $eachItemClass. '">';
+						{
+								if(a($_args, 'post_show_date') !== 'no')
+								{
+									$html .= "<dl>";
+									{
+										$html .= '<dt class="sr-only">'. T_('Published on'). '</dt>';
+										$html .= '<dd class="text-base font-medium">';
+										{
+												$ltrDate = 'ltr';
+												if(a($_args, 'post_show_date') === 'relative')
+												{
+													$ltrDate = '';
+												}
+												$html .= "<time class='$ltrDate opacity-80' datetime='$myDate' title='". T_("Published"). " $myDate' $color_text>";
+												$html .= \content_site\body\blog\share::date($myDate, a($_args, 'post_show_date'));
+												$html .= "</time>";
+										}
+										$html .= '</dd>';
+									}
+									$html .= '</dl>';
+								}
+
+							$html .= "<div class='$postTextClass'>";
+							{
+								$html .= '<h3 class="text-xl font-bold">';
+								$html .= '<a href="'.$myLink.'" class="inline-flex items-center"'. $color_heading. '>'. $myTitle. '</a>';
+								$html .= '</h3>';
+
+								if($myExcerpt && a($_args, 'post_show_excerpt'))
+								{
+									$html .= "<p class='leading-7' $color_text>";
+									$html .= $myExcerpt;
+									$html .= "</p>";
+								}
+
+								if(a($_args, 'post_show_read_more'))
+								{
+									$html .= "<div class='text-base font-medium'>";
+									$html .= '<a href="'.$myLink.'" class="inline-flex items-center opacity-70 link-'. a($_args, 'link_color').'">'.T_("Read more").'</a>';
+									$html .= "</div>";
+								}
+
+							}
+							$html .= '</div>';
+						}
+						$html .= '</article>';
+					} // endfor
+				}
+				$html .= '</div>';
+
+				$html .= \content_site\body\blog\share::btn_viewall($_args);
+			}
+			$html .= '</div>';
+		}
+		$html .= \content_site\assemble\wrench\section::element_end($_args);
+
+		return $html;
+	}
+}
+?>
