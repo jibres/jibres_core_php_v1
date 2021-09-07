685bf81ad4d87eee7546d67e334c7c46f67592fc
Reza <rm.biqarar@gmail.com>
2021-09-07 06:44:46 +0430

    get features detail

--- lib/features/get.php
+++ lib/features/get.php
@@ -15,5 +15,29 @@ class get
 
 		return $price;
 	}
+
+
+	public static function detail($_feature)
+	{
+		$price = \lib\features\call_function::price($_feature);
+
+		if(!$price)
+		{
+			$price = 0;
+		}
+
+		$title = \lib\features\call_function::title($_feature);
+		$description = \lib\features\call_function::description($_feature);
+
+
+		return
+		[
+			'feature_key' => $_feature,
+			'title'       => $title,
+			'price'       => $price,
+			'description' => $description,
+		];
+
+	}
 }
 ?>
