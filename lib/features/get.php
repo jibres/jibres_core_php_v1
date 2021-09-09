b6be7aad053ce796da3680578b1ebdc0afab4406
Reza <rm.biqarar@gmail.com>
2021-09-09 10:17:55 +0430

    Get all features list for admin

--- lib/features/get.php
+++ lib/features/get.php
@@ -4,6 +4,24 @@ namespace lib\features;
 
 class get
 {
+	public static function all_list()
+	{
+		$list = glob(__DIR__. '/items/*');
+
+		$features = [];
+
+		foreach ($list as $key => $value)
+		{
+			$feature_key = str_replace('.php', '', basename($value));
+
+			$features[] = self::detail($feature_key);
+		}
+
+		return $features;
+
+	}
+
+
 	public static function price($_feature)
 	{
 		$price = \lib\features\call_function::price($_feature);
