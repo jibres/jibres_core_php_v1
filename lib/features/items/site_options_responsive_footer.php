8cfdf9bf0ab7b2b0920c492aa6f7617af429d8c6
Reza <rm.biqarar@gmail.com>
2021-09-09 13:23:38 +0430

    Add option responsive footer as premium if used

---
+++ lib/features/items/site_options_responsive_footer.php
@@ -0,0 +1,24 @@
+<?php
+namespace lib\features\items;
+
+
+class site_options_responsive_footer
+{
+	public static function price()
+	{
+		return 200000;
+	}
+
+	public static function description()
+	{
+		return T_("Customize footer in for application (Responsive) description ...");
+	}
+
+
+	public static function title()
+	{
+		return T_("Customize footer in for application (Responsive)");
+	}
+
+}
+?>
