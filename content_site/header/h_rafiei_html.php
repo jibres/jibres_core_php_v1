f219a4aa6b769427d6207c5e8cf158b8e86ed864
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-11 06:58:32 +0430

    fix refiei header

--- content_site/header/h_rafiei_html.php
+++ content_site/header/h_rafiei_html.php
@@ -4,8 +4,6 @@ namespace content_site\header;
 
 class h_rafiei_html
 {
-
-
 	/**
 	 * Layout blog html
 	 *
@@ -25,100 +23,24 @@ class h_rafiei_html
 			$html .= \content_site\assemble\wrench\section::container($_args, 'relative');
 			{
 				$color = '#eda336';
-				$topCircleStyle = 'top:-40px;border-radius:50%;background-color:'. $color.';';
-				$html .= '<div id="topLine" class="absolute w-full h-8 mx-auto absolute right-0 left-0 z-100 bg-red-100" style="'. $topCircleStyle. '"></div>';
+				$topCircleStyle = 'height:40px;top:-30px;border-radius:50%;background-color:'. $color.';';
+				$html .= '<div id="topLine" class="fixed w-full mx-auto right-0 left-0 z-100" style="'. $topCircleStyle. '"></div>';
 
-				if(a($_args, 'logo') || a($_args, 'heading') || a($_args, 'link_search') || a($_args, 'link_cart') || a($_args, 'link_enter'))
+				$html .= '<a href="'. \dash\url::kingdom() .'" class="block max-w-md mx-auto my-2">';
+				if(a($_args, 'logo'))
 				{
-					// action bar
-					$html .= '<div class="actionBar flex items-center p-1 sm:p-2 md:p-3 bg-gray-50 rounded shadow-inner">';
-					{
-						$html .= '<a href="'. \dash\url::kingdom() .'" class="flex-1 flex items-center">';
-						{
-							$logo = a($_args, 'logo');
-
-							if($logo)
-							{
-								$html .= '<img class="inline-block w-16 h-16 rounded" src="'. $logo. '" alt="'. a($_args, 'heading'). '">';
-							}
-
-							if(a($_args, 'heading'))
-							{
-								// add title
-								$html .= '<h1 class="inline-block px-2 text-2xl font-bold">';
-								{
-									$html .= $_args['heading'];
-								}
-								$html .= '</h1>';
-							}
-						}
-						$html .= '</a>';
-
-
-						if(a($_args, 'link_search'))
-						{
-							$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/search">';
-							{
-								$html .= \dash\utility\icon::svg('search');
-							}
-							$html .= '</a>';
-						}
-
-						if(a($_args, 'link_cart'))
-						{
-							$html .= '<a class="h-12 w-12 p-3 mx-1 bg-gray-50 rounded-full relative transition hover:shadow-sm" href="'. \dash\url::kingdom(). '/cart">';
-							{
-								$html .= \dash\utility\icon::svg('cart');
-								$html .= '<span class="absolute top-0 right-0 bg-red-500 text-white rounded-full w-5 h-5 text-center leading-5 p-0.5 text-sm">';
-								$html .= \dash\fit::number(a($_args, 'cart_count'));
-								$html .= '</span>';
-							}
-							$html .= '</a>';
-						}
-
-						if(a($_args, 'link_enter'))
-						{
-							$enterTxt  = T_("Enter to account");
-							$enterLink = \dash\url::kingdom(). '/enter';
-							if(\dash\user::login())
-							{
-								$enterTxt  = T_("Profile");
-								$enterLink = \dash\url::kingdom(). '/profile';
-							}
-							$html .= '<a class="p-3 mx-1 rounded link-secondary" href="'. $enterLink. '">';
-							{
-								$html .= $enterTxt;
-							}
-							$html .= '</a>';
-						}
-
-					}
-					$html .= '</div>';
-
-				} // empty header
-
-				if(a($_args, 'menu_1') || a($_args, 'menu_2'))
-				{
-					// menu bar
-					$html .= '<div class="menuBar flex items-center bg-gray-50 rounded shadow-inner mt-2">';
-					{
-						$menuOpt =
-						[
-							'nav_class' => '',
-							'ul_class'  => 'flex',
-							'li_class'  => '',
-							'a_class'   => 'block p-1 sm:p-2 md:p-3 bg-gray-200 bg-opacity-0 hover:bg-opacity-70 transition',
-						];
-
-						$menuOptPrimary                   = $menuOpt;
-						$menuOptPrimary['nav_class']      = 'flex-1';
-						$menuOptPrimary['force_show_box'] = true;
-						$html .= \content_site\assemble\menu::generate(a($_args, 'menu_1'), $menuOptPrimary);
-
-						$html .= \content_site\assemble\menu::generate(a($_args, 'menu_2'), $menuOpt);
-					}
-					$html .= '</div>';
+					$html .= '<img class="block" src="'. a($_args, 'logo'). '" alt="'. a($_args, 'heading'). '">';
 				}
+				$html .= '</a>';
+
+				$menuOpt =
+				[
+					'nav_class' => '',
+					'ul_class'  => 'flex justify-center',
+					'li_class'  => '',
+					'a_class'   => 'block p-1 sm:p-2 md:p-3 bg-gray-200 bg-opacity-0 hover:bg-opacity-70 transition',
+				];
+				$html .= \content_site\assemble\menu::generate(a($_args, 'menu_1'), $menuOpt);
 
 			}
 			$html .= '</div>';
