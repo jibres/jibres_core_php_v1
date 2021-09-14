96924f36fc7049a1727d8e06aba36d4d74e2362c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-14 22:16:46 +0430

    update trans

--- content_site/page/display-sidebar.php
+++ content_site/page/display-sidebar.php
@@ -38,7 +38,7 @@ foreach ($list as $key => $value)
   <ul>
     <li>
       <a class="item f" href="<?php echo $header_link ?>">
-        <img class="bg-gray-100 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('Header'); ?>">
+        <img class="bg-blue-50 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('Header'); ?>">
         <div class="key"><?php echo T_("Header") ?></div>
       </a>
     </li>
@@ -61,10 +61,10 @@ foreach ($list as $key => $value)
         <li>
           <a title="<?php echo $myTitle ?>" class="item f <?php if(a($value, 'status_preview') === 'hidden'){ echo 'opacity-30';} ?>" href="<?php echo \dash\url::here(). '/section/'. a($value, 'section'). \dash\request::full_get(['sid' => a($value, 'id')]); ?>">
             <input type="hidden" name="sort_section[]" value="<?php echo a($value, 'id') ?>">
-            <img class="bg-gray-100 hover:bg-gray-200 p-2.5" src="<?php echo a($value, 'preview', 'icon') ?>">
+            <img class="bg-blue-50 hover:bg-gray-200 p-2.5" src="<?php echo a($value, 'preview', 'icon') ?>">
             <div class="key"><?php if(a($value, 'preview', 'heading') !== null) { echo a($value, 'preview', 'heading'); }else{ echo '<i class="fc-mute">'. T_("Without title"). '</i>';} ?></div>
             <?php if (count($body) > 1) { ?>
-              <img class="p-5 opacity-70 hover:bg-gray-200" data-handle src="<?php echo \dash\utility\icon::url('DragHandle', 'minor'); ?>">
+              <img class="p-3 opacity-70 hover:bg-gray-300" data-handle src="<?php echo \dash\utility\icon::url('DragHandle', 'minor'); ?>">
             <?php } ?>
           </a>
         </li>
@@ -77,7 +77,7 @@ foreach ($list as $key => $value)
   <ul>
     <li>
       <a class="item f" href="<?php echo \dash\url::here(). '/section'. \dash\request::full_get(['folder' => 'body']); ?>">
-        <img class="bg-gray-100 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('add'); ?>">
+        <img class="bg-blue-50 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('add'); ?>">
         <div class="key text-blue-500"><?php echo T_("Add Section") ?></div>
       </a>
     </li>
@@ -88,7 +88,7 @@ foreach ($list as $key => $value)
   <ul>
     <li>
       <a class="item f" href="<?php echo $footer_link ?>">
-        <img class="bg-gray-100 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('Footer'); ?>">
+        <img class="bg-blue-50 hover:bg-gray-200 p-2.5" src="<?php echo \dash\utility\icon::url('Footer'); ?>">
         <div class="key"><?php echo T_("Footer") ?></div>
       </a>
     </li>
@@ -104,9 +104,9 @@ foreach ($list as $key => $value)
       <?php foreach ($deleted_section as $key => $value) {?>
         <li>
           <a class="item f opacity-40" href="<?php echo \dash\url::here(). '/section/'. a($value, 'section'). \dash\request::full_get(['sid' => a($value, 'id')]); ?>">
-            <img class="bg-gray-100 hover:bg-gray-200 p-2.5" src="<?php echo a($value, 'preview', 'icon') ?>">
+            <img class="bg-blue-50 hover:bg-gray-200 p-2.5" src="<?php echo a($value, 'preview', 'icon') ?>">
             <div class="key"><?php if(a($value, 'preview', 'heading') !== null) { echo a($value, 'preview', 'heading'); }else{ echo '<i class="fc-mute">'. T_("Without title"). '</i>';} ?></div>
-            <img class="p-5 opacity-70 hover:bg-gray-200" src="<?php echo \dash\utility\icon::url('Delete', 'minor'); ?>">
+            <img class="p-3 opacity-70 hover:bg-gray-300" src="<?php echo \dash\utility\icon::url('Delete', 'minor'); ?>">
           </a>
         </li>
       <?php } //endfor ?>
@@ -119,35 +119,36 @@ foreach ($list as $key => $value)
 <?php
   $html = '';
 
-  $html .= '<div class="row w-full mt-12">';
+  $html .= '<div class="flex flex-wrap mt-10">';
   {
-    $html .= '<div class="cauto">';
+    // $html .= '<div class="cauto">';
     {
-      $html .= '<a tabindex=0 class="btn " href="'.\dash\url::this(). '/settings'. \dash\request::full_get().'">';
+      $html .= '<a class="btn-light btn-sm" href="'.\dash\url::this(). '/settings'. \dash\request::full_get().'">';
       {
-        $html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('Settings', 'minor'). '" alt="Setting">';
-        $html .= '<span class="inline-block align-middle ps-2">'. T_("Page setting").'</span>';
+        $html .= '<img class="w-6 inline-block" src="'. \dash\utility\icon::url('Settings', 'minor'). '" alt="Setting">';
+        $html .= '<span class="inline-block align-middle px-2">'. T_("Page setting").'</span>';
       }
       $html .= '</a>';
     }
-    $html .= '</div>';
+    // $html .= '</div>';
 
     $html .= '<div class="c"></div>';
 
 
 
 
-    $html .= "<div class='cauto os pLa5'>";
+    // $html .= "<div class='cauto os pLa5'>";
     {
       $model_url = \dash\url::that(). '/design'. \dash\request::full_get();
-      $html .= "<a href='$model_url' class='inline-block bg-gray-50 hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 transition p-3 rounded-lg'  title='". T_("Change Background") ."'>";
-      $html .= '<img class="w-8 inline-block" src="'. \dash\utility\icon::url('Colors'). '" alt="page background">';
+      $html .= "<a href='$model_url' class='btn-light btn-sm'  title='". T_("Change Background") ."'>";
+      $html .= '<img class="w-6 inline-block" src="'. \dash\utility\icon::url('Colors'). '" alt="page background">';
+        $html .= '<span class="inline-block align-middle px-2">'. T_("Personalize").'</span>';
       $html .= '</a>';
     }
     $html .= '</div>';
 
   }
-  $html .= '</div>';
+  // $html .= '</div>';
 
 
   echo $html;
