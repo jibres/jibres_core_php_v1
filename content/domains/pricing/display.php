ccbb70ca578009085bcfa24f0d4c83c8ca5c7a95
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-16 03:59:08 +0430

    fix pricing table

--- content/domains/pricing/display.php
+++ content/domains/pricing/display.php
@@ -1,14 +1,12 @@
 <div class="jibresBanner">
  <div class="avand impact">
 
-  <h2 class="txtC"><?php echo T_('Search for your dream domain'); ?></h2>
-
   <div class="tblBox">
-    <table class="tbl1 v4">
+    <table class="tbl1 v4" data-datatable="pricing">
       <thead>
         <tr>
         <th class="collapsing"></th>
-        <th><?php echo T_("Domain TLD") ?></th>
+        <th><?php echo T_("TLD") ?></th>
         <th><?php echo T_("Price") ?> <small><?php echo T_("Toman"); ?></small></th>
         <?php if(\dash\permission::supervisor()) {?>
           <th><?php echo T_("Price") ?> <small><?php echo T_("Dollar"); ?></small></th>
