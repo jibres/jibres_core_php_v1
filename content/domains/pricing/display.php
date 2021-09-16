c49a2c1c7fa481f55eb302dfd09a2875e3e0577c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-16 18:27:48 +0430

    some fix on price

--- content/domains/pricing/display.php
+++ content/domains/pricing/display.php
@@ -1,25 +1,25 @@
 <div class="jibresBanner">
  <div class="avand impact">
 
-  <div class="tblBox">
+  <div class="tblBox1 ltr">
     <table class="tbl1 v4" data-datatable="pricing">
       <thead>
         <tr>
-          <th class="collapsing"></th>
-          <th><?php echo T_("TLD") ?></th>
-          <th><?php echo T_("Register") ?></th>
-          <th><?php echo T_("Renew") ?></th>
-          <th><?php echo T_("Transfer") ?></th>
+          <th class="collapsing txtL"></th>
+          <th class="txtL"><?php echo T_("TLD") ?></th>
+          <th class="txtL"><?php echo T_("Register") ?></th>
+          <th class="txtL"><?php echo T_("Renew") ?></th>
+          <th class="txtL"><?php echo T_("Transfer") ?></th>
         </tr>
       </thead>
       <tbody>
         <?php $count = 0; foreach (\dash\data::dataTable() as $key => $value) { $count++;?>
           <tr>
-            <td class="collapsing"><?php echo \dash\fit::number($count); ?></td>
-            <td class="ltr"><?php echo a($value, 'TLD') ?></td>
-            <td class="txtB" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'register')); ?></td>
-            <td class="txtB" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'renew')) ?></td>
-            <td class="txtB" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'transfer')) ?></td>
+            <td class="collapsing" data-order="<?php echo $count; ?>"><?php echo \dash\fit::number($count); ?></td>
+            <td class="ltr txtL"><?php echo a($value, 'TLD') ?></td>
+            <td class="txtB txtL" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'register')); ?></td>
+            <td class="txtB txtL" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'renew')) ?></td>
+            <td class="txtB txtL" data-order="<?php echo a($value, 'register'); ?>"><?php echo \content\domains\pricing\controller::priceEl(a($value, 'transfer')) ?></td>
           </tr>
         <?php } // endfor ?>
       </tbody>
