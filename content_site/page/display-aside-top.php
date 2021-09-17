00c63a1fdda7ec3df893ec99ded31d5109cbe628
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-09-17 18:35:27 +0430

    bugfix sidebar

--- content_site/page/display-aside-top.php
+++ content_site/page/display-aside-top.php
@@ -34,9 +34,6 @@ foreach ($list as $key => $value)
 }
 
 ?>
-<div class="h-full flex flex-col">
- <div class="flex-1">
-
   <nav class="header items">
     <ul>
       <li>
@@ -117,29 +114,6 @@ foreach ($list as $key => $value)
     </nav>
 
     <?php } //endif ?>
-  </div>
 
 
-  <?php
-    $html = '';
-    $html .= '<div">';
-    {
-      $html .= '<a class="btn-outline-dark btn-sm flex align-center" href="'.\dash\url::this(). '/settings'. \dash\request::full_get().'">';
-      {
-        $html .= '<img class="w-6" src="'. \dash\utility\icon::url('Settings', 'minor'). '" alt="Setting">';
-        $html .= '<span class="px-2">'. T_("Page setting").'</span>';
-      }
-      $html .= '</a>';
 
-      $model_url = \dash\url::that(). '/design'. \dash\request::full_get();
-      $html .= "<a href='$model_url' class='btn-outline-dark btn-sm flex align-center mt-2'  title='". T_("Change Background") ."'>";
-      $html .= '<img class="w-6" src="'. \dash\utility\icon::url('Colors'). '" alt="page background">';
-        $html .= '<span class=" px-2">'. T_("Personalize").'</span>';
-      $html .= '</a>';
-    }
-    $html .= '</div>';
-
-
-    echo $html;
-  ?>
-</div>
