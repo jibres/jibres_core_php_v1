af1975f75df2663b1aaa31bb3dc7c5e4852eb5a3
Reza <rm.biqarar@gmail.com>
2021-09-18 21:41:05 +0430

    Save

--- content_a/discount/add/display.php
+++ content_a/discount/add/display.php
@@ -65,15 +65,15 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="type" value="fixedamount" id="type-fixedamount">';
-          $html .= '<label for="type-fixedamount">'. T_("Fixed amount"). '</label>';
+          $html .= '<input type="radio" name="type" value="fixed_amount" id="type-fixed_amount">';
+          $html .= '<label for="type-fixed_amount">'. T_("Fixed amount"). '</label>';
         }
         $html .= '</div>';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="type" value="freeshipping" id="type-freeshipping">';
-          $html .= '<label for="type-freeshipping">'. T_("Free shipping"). '</label>';
+          $html .= '<input type="radio" name="type" value="free_shipping" id="type-free_shipping">';
+          $html .= '<label for="type-free_shipping">'. T_("Free shipping"). '</label>';
         }
         $html .= '</div>';
       }
@@ -86,7 +86,7 @@ $html .= '<div class="max-w-xl m-auto">';
     /*=============================
     =            value            =
     =============================*/
-    $html .= '<div data-response="type" data-response-where="percentage|fixedamount">';
+    $html .= '<div data-response="type" data-response-where="percentage|fixed_amount">';
     {
       $html .= '<div class="box">';
       {
@@ -97,29 +97,29 @@ $html .= '<div class="max-w-xl m-auto">';
 
           $html .= '<div data-response="type" data-response-where="percentage">';
           {
-            $html .= '<label for="giftpercent">'. T_("Discount value"). '</label>';
+            $html .= '<label for="percentage">'. T_("Discount value"). '</label>';
             $html .= '<div class="input">';
             {
-              $html .= '<input type="tel" name="giftpercent" id="giftpercent" value="'.a($dataRow.'giftpercent').'" placeholder="'.T_("%").'">';
+              $html .= '<input type="tel" name="percentage" id="percentage" value="'.a($dataRow.'percentage').'" placeholder="'.T_("%").'">';
             }
             $html .= '</div>';
 
-            $html .= '<label for="giftmax">'. T_("Maximum amount"). '</label>';
+            $html .= '<label for="maxamount">'. T_("Maximum amount"). '</label>';
             $html .= '<div class="input">';
             {
-              $html .= '<input type="tel" name="giftmax" id="giftmax" value="'.a($dataRow, 'giftmax').'" placeholder="'. $currency.'">';
+              $html .= '<input type="tel" name="maxamount" id="maxamount" value="'.a($dataRow, 'maxamount').'" placeholder="'. $currency.'">';
             }
             $html .= '</div>';
 
           }
           $html .= '</div>';
 
-          $html .= '<div data-response="type" data-response-where="fixedamount" data-response-hide>';
+          $html .= '<div data-response="type" data-response-where="fixed_amount" data-response-hide>';
           {
-            $html .= '<label for="giftamount">'. T_("Discount value"). '</label>';
+            $html .= '<label for="fixedamount">'. T_("Discount value"). '</label>';
             $html .= '<div class="input">';
             {
-              $html .= '<input type="tel" name="giftamount" id="giftamount" value="'.a($dataRow, 'giftamount').'" placeholder="'. $currency.'">';
+              $html .= '<input type="tel" name="fixedamount" id="fixedamount" value="'.a($dataRow, 'fixedamount').'" placeholder="'. $currency.'">';
             }
             $html .= '</div>';
           }
@@ -145,19 +145,19 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="applyto" value="allproduct" id="applyto-allproduct" checked>';
-          $html .= '<label for="applyto-allproduct">'. T_("All product"). '</label>';
+          $html .= '<input type="radio" name="applyto" value="all_products" id="applyto-all_products" checked>';
+          $html .= '<label for="applyto-all_products">'. T_("All product"). '</label>';
         }
         $html .= '</div>';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="applyto" value="specialcategory" id="applyto-specialcategory">';
-          $html .= '<label for="applyto-specialcategory">'. T_("Special category"). '</label>';
+          $html .= '<input type="radio" name="applyto" value="special_category" id="applyto-special_category">';
+          $html .= '<label for="applyto-special_category">'. T_("Special category"). '</label>';
         }
         $html .= '</div>';
 
-        $html .= '<div data-response="applyto" data-response-where="specialcategory" data-response-hide>';
+        $html .= '<div data-response="applyto" data-response-where="special_category" data-response-hide>';
         {
           $html .= '<select name="product_category[]" id="product_category" class="select22" data-model="tag" multiple="multiple">';
           {
@@ -179,6 +179,24 @@ $html .= '<div class="max-w-xl m-auto">';
         $html .= '</div>';
 
 
+        $html .= '<div class="radio1">';
+        {
+          $html .= '<input type="radio" name="applyto" value="special_products" id="applyto-special_products">';
+          $html .= '<label for="applyto-special_products">'. T_("Special category"). '</label>';
+        }
+        $html .= '</div>';
+
+        $html .= '<div data-response="applyto" data-response-where="special_products" data-response-hide>';
+        {
+           $html .= '<select name="special_products[]" id="special_products" class="select22" data-model="tag" multiple="multiple" data-ajax--delay="100" data-ajax--url="'. \dash\url::kingdom(). '/crm/member/api?json=true'.'" data-placeholder="'. T_('Search in customers'). '">';
+            foreach ([] as $key => $value)
+            {
+              $html .= '<option value="<?php echo $value["title"]; ?>" selected><?php echo $value["title"]; ?></option>';
+            }
+            $html .= '</select>';
+        }
+        $html .= '</div>';
+
       }
       $html .= '</div>';
     }
@@ -198,23 +216,23 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="minimumrequirements" value="none" id="minimum-none" checked>';
+          $html .= '<input type="radio" name="minrequirements" value="none" id="minimum-none" checked>';
           $html .= '<label for="minimum-none">'. T_("None"). '</label>';
         }
         $html .= '</div>';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="minimumrequirements" value="amount" id="minimum-amount">';
+          $html .= '<input type="radio" name="minrequirements" value="amount" id="minimum-amount">';
           $html .= '<label for="minimum-amount">'. T_("Minimum purchase amount :currency", ['currency' => '('.$currency.')']). '</label>';
         }
         $html .= '</div>';
 
-        $html .= '<div data-response="minimumrequirements" data-response-where="amount" data-response-hide>';
+        $html .= '<div data-response="minrequirements" data-response-where="amount" data-response-hide>';
         {
             $html .= '<div class="input">';
             {
-              $html .= '<input type="tel" name="pricefloor" value="" placeholder="'. $currency.'">';
+              $html .= '<input type="tel" name="minpurchase" value="'. a($dataRow, 'minpurchase'). '" placeholder="'. $currency.'">';
             }
             $html .= '</div>';
         }
@@ -222,16 +240,16 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="minimumrequirements" value="quantity" id="minimum-quantity">';
+          $html .= '<input type="radio" name="minrequirements" value="quantity" id="minimum-quantity">';
           $html .= '<label for="minimum-quantity">'. T_("Minimum quantity of items"). '</label>';
         }
         $html .= '</div>';
 
-        $html .= '<div data-response="minimumrequirements" data-response-where="quantity" data-response-hide>';
+        $html .= '<div data-response="minrequirements" data-response-where="quantity" data-response-hide>';
         {
             $html .= '<div class="input">';
             {
-              $html .= '<input type="tel" name="quantityfloor" value="">';
+              $html .= '<input type="tel" name="minquantity" value="'. a($dataRow, 'minquantity'). '">';
             }
             $html .= '</div>';
         }
@@ -264,12 +282,12 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="customer" value="specialgroup" id="customer-specialgroup">';
-          $html .= '<label for="customer-specialgroup">'. T_("Special group of customers"). '</label>';
+          $html .= '<input type="radio" name="customer" value="special_customer_group" id="customer-special_customer_group">';
+          $html .= '<label for="customer-special_customer_group">'. T_("Special group of customers"). '</label>';
         }
         $html .= '</div>';
 
-        $html .= '<div data-response="customer" data-response-where="specialgroup" data-response-hide>';
+        $html .= '<div data-response="customer" data-response-where="special_customer_group" data-response-hide>';
         {
           $html .= '<select name="customer_group" id="customer_group" class="select22">';
           {
@@ -299,12 +317,12 @@ $html .= '<div class="max-w-xl m-auto">';
 
         $html .= '<div class="radio1">';
         {
-          $html .= '<input type="radio" name="customer" value="specialcustomers" id="customer-specialcustomers">';
-          $html .= '<label for="customer-specialcustomers">'. T_("Special customers"). '</label>';
+          $html .= '<input type="radio" name="customer" value="special_customer" id="customer-special_customer">';
+          $html .= '<label for="customer-special_customer">'. T_("Special customers"). '</label>';
         }
         $html .= '</div>';
 
-        $html .= '<div data-response="customer" data-response-where="specialcustomers" data-response-hide>';
+        $html .= '<div data-response="customer" data-response-where="special_customer" data-response-hide>';
         {
           $html .= '<select name="special_customer[]" id="special_customer" class="select22" data-model="tag" multiple="multiple" data-ajax--delay="100" data-ajax--url="'. \dash\url::kingdom(). '/crm/member/api?json=true'.'" data-placeholder="'. T_('Search in customers'). '">';
             foreach ([] as $key => $value)
@@ -369,7 +387,6 @@ $html .= '<div class="max-w-xl m-auto">';
     ===================================*/
     $html .= '<div class="box">';
     {
-
       $html .= '<div class="body">';
       {
         $html .= '<h2>'. T_("Active date"). '</h2>';
@@ -381,7 +398,7 @@ $html .= '<div class="max-w-xl m-auto">';
             $html .= '<label for="startdate">'. T_("Start date"). '</label>';
             $html .= '<div class="input ltr">';
             {
-              $html .= '<input type="text" name="code" value="" id="startdate" placeholder="'.\dash\fit::date_en(date("Y-m-d")).'" data-format="date">';
+              $html .= '<input type="text" name="startdate" value="'. a($dataRow, 'startdate'). '" id="startdate" placeholder="'.\dash\fit::date_en(date("Y-m-d")).'" data-format="date">';
             }
             $html .= '</div>';
           }
@@ -393,7 +410,7 @@ $html .= '<div class="max-w-xl m-auto">';
             $html .= '<label for="starttime">'. T_("Time"). '</label>';
             $html .= '<div class="input ltr">';
             {
-              $html .= '<input type="text" name="code" value="" id="starttime" placeholder="'.date("H:i").'" data-format="time">';
+              $html .= '<input type="text" name="starttime" value="'. a($dataRow, 'starttime'). '" id="starttime" placeholder="'.date("H:i").'" data-format="time">';
             }
             $html .= '</div>';
           }
@@ -419,7 +436,7 @@ $html .= '<div class="max-w-xl m-auto">';
               $html .= '<label for="enddate">'. T_("End date"). '</label>';
               $html .= '<div class="input ltr">';
               {
-                $html .= '<input type="text" name="code" value="" id="enddate" placeholder="'.\dash\fit::date_en(date("Y-m-d")).'" data-format="date">';
+                $html .= '<input type="text" name="enddate" value="'. a($dataRow, 'enddate'). '" id="enddate" placeholder="'.\dash\fit::date_en(date("Y-m-d")).'" data-format="date">';
               }
               $html .= '</div>';
             }
@@ -431,7 +448,7 @@ $html .= '<div class="max-w-xl m-auto">';
               $html .= '<label for="endtime">'. T_("Time"). '</label>';
               $html .= '<div class="input ltr">';
               {
-                $html .= '<input type="text" name="code" value="" id="endtime" placeholder="'.date("H:i").'" data-format="time">';
+                $html .= '<input type="text" name="endtime" value="'. a($dataRow, 'endtime'). '" id="endtime" placeholder="'.date("H:i").'" data-format="time">';
               }
               $html .= '</div>';
             }
