b39d553a1f5069605f16cd4455f457e18548286b
Reza <rm.biqarar@gmail.com>
2021-09-18 09:32:07 +0430

    Convert page builder header to php mode

--- dash/layout/panelBuilder/panelBuilder-header.php
+++ dash/layout/panelBuilder/panelBuilder-header.php
@@ -1,39 +1,67 @@
-<div class="h-full flex flex-wrap content-center px-3">
-
- <div class="backBtn">
-  <a class="btn-light btn-sm" href="<?php echo \dash\data::back_link(); ?>" <?php if(\dash\data::back_direct()) { echo 'data-direct';} ?>>
 <?php
-if(\dash\language::dir() === 'rtl')
+$html = '';
+
+$html .= '<div class="h-full flex flex-wrap content-center px-3">';
 {
- echo \dash\utility\icon::svg('ChevronRight', 'minor');
+  $html .= '<div class="backBtn">';
+  {
+
+    $html .= '<a class="btn-light btn-sm" href="'. \dash\data::back_link(). '" ';
+    if(\dash\data::back_direct())
+    {
+      $html .= 'data-direct';
+    }
+    $html .= '>';
+    {
+
+      if(\dash\language::dir() === 'rtl')
+      {
+       $html .= \dash\utility\icon::svg('ChevronRight', 'minor');
+      }
+      else
+      {
+       $html .= \dash\utility\icon::svg('ChevronLeft', 'minor');
+      }
+
+      $html .= '<span class="px-1">'. \dash\data::back_text(). '</span>';
+    }
+    $html .= '</a>';
+  }
+  $html .= '</div>';
+
+  $html .= '<div class="flex-grow px-5 font-bold">'. \dash\face::title(). '</div>';
+
+  $html .= '<div class="actionBtn">';
+  {
+
+    if(\dash\data::action_link() && \dash\data::action_text())
+    {
+      $html .= '<a href="'. \dash\data::action_link(). '" class="btn-secondary">'. \dash\data::action_text(). '</a>';
+    }
+
+    if(\dash\data::btnSaveSiteBuilder())
+    {
+      $html .= '<form method="post" autocomplete="off" action="'. \dash\url::here(). '/page?id='. \dash\request::get('id'). '">';
+      {
+        $html .= '<input type="hidden" name="savepage" value="savepage">';
+        $html .= '<button class="btn-primary">'. T_("Save & Publish"). '</button>';
+      }
+      $html .= '</form>';
+    }
+    if(\dash\data::btnSaveSiteBuilderHtml())
+    {
+      $html .= '<button form="savehtmlform" class="btn-secondary">'. T_("Save HTML"). '</button>';
+    }
+    if(\dash\data::btnSaveSiteBuilderEditor())
+    {
+      $html .= '<button form="sectioneditorhtml" class="btn-secondary">'. T_("Save Text"). '</button>';
+    }
+  }
+  $html .= '</div>';
+
 }
-else
-{
- echo \dash\utility\icon::svg('ChevronLeft', 'minor');
-}
+$html .= '</div>';
+
+
+echo $html;
 ?>
-    <span class="px-1"><?php echo \dash\data::back_text(); ?></span>
-  </a>
- </div>
-  <div class="flex-grow px-5 font-bold"><?php echo \dash\face::title(); ?></div>
-
- <div class="actionBtn">
-<?php if(\dash\data::action_link() && \dash\data::action_text()) {?>
-  <a href="<?php echo \dash\data::action_link(); ?>" class="btn-secondary"><?php echo \dash\data::action_text(); ?></a>
-<?php }?>
-<?php if(\dash\data::btnSaveSiteBuilder()) { ?>
-<form method="post" autocomplete="off" action="<?php echo \dash\url::here(). '/page?id='. \dash\request::get('id'); ?>">
-  <input type="hidden" name="savepage" value="savepage">
-  <button class="btn-primary"><?php echo T_("Save & Publish"); ?></button>
-</form>
-<?php } // endif ?>
-<?php if(\dash\data::btnSaveSiteBuilderHtml()) { ?>
-  <button form="savehtmlform" class="btn-secondary"><?php echo T_("Save HTML"); ?></button>
-<?php }//endif ?>
-<?php if(\dash\data::btnSaveSiteBuilderEditor()) { ?>
-  <button form="sectioneditorhtml" class="btn-secondary"><?php echo T_("Save Text"); ?></button>
-<?php }//endif ?>
- </div>
-
-
-</div>
