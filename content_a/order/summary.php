da416f752a409b6d707e291b65234c08552ca55f
Reza <rm.biqarar@gmail.com>
2021-09-25 11:31:14 +0330

    Show discount link

--- content_a/order/summary.php
+++ content_a/order/summary.php
@@ -5,14 +5,14 @@
               <?php if(a($orderDetail, 'factor', 'subprice')) {?>
               <tr class="">
                 <td class="collapsing fc-mute"><?php echo T_("Subprice") ?></td>
-                <td class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subprice')). ' '. \lib\store::currency(); ?></td>
+                <td colspan="3" class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subprice')). ' '. \lib\store::currency(); ?></td>
               </tr>
             <?php } //endif ?>
 
               <?php if(a($orderDetail, 'factor', 'subdiscount')) {?>
               <tr class="">
                 <td class="collapsing fc-mute"><?php echo T_("Subdiscount") ?></td>
-                <td class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subdiscount')). ' '. \lib\store::currency(); ?></td>
+                <td colspan="3" class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subdiscount')). ' '. \lib\store::currency(); ?></td>
               </tr>
             <?php } //endif ?>
 
@@ -20,38 +20,40 @@
             <?php if(a($orderDetail, 'factor', 'subvat')) {?>
               <tr class="">
                 <td class="collapsing fc-mute"><?php echo T_("Vat") ?></td>
-                <td class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subvat')). ' '. \lib\store::currency(); ?></td>
+                <td colspan="3" class=""><?php echo \dash\fit::number(a($orderDetail, 'factor', 'subvat')). ' '. \lib\store::currency(); ?></td>
               </tr>
             <?php } //endif ?>
 
               <?php if(a($orderDetail, 'factor', 'discount')) {?>
                 <tr>
                   <td class="collapsing fc-mute"><?php echo T_("Discount") ?></td>
-                  <td><?php echo \dash\fit::number(a($orderDetail, 'factor', 'discount')). ' '. \lib\store::currency(); ?></td>
+                  <td colspan="3"><?php echo \dash\fit::number(a($orderDetail, 'factor', 'discount')). ' '. \lib\store::currency(); ?></td>
                 </tr>
               <?php } //endif ?>
 
 
               <?php if(a($orderDetail, 'factor', 'discount2')) {?>
-                <tr>
-                  <td class="collapsing fc-mute"><?php echo T_("Discount code") ?> <small><code><?php echo a($orderDetail, 'discount_code', 'code') ?></code></small></td>
+                <tr class="active">
+                  <td class="collapsing fc-mute"><?php echo T_("Discount code") ?></td>
                   <td><?php echo \dash\fit::number(a($orderDetail, 'factor', 'discount2')). ' '. \lib\store::currency(); ?></td>
+                  <td><code><?php echo a($orderDetail, 'discount_code', 'code') ?></code></td>
+                  <td><a class="link sm" href="<?php echo \dash\url::here(). '/discount/edit?id='. a($orderDetail, 'factor', 'discount_id') ?>" data-direct><?php echo T_("Show Discount") ?></a></td>
                 </tr>
               <?php } //endif ?>
 
                 <tr>
                   <td class="collapsing fc-mute"><?php echo T_("Shipping") ?></td>
               		<?php if(a($orderDetail, 'factor', 'shipping')) {?>
-                  		<td><?php echo \dash\fit::number(a($orderDetail, 'factor', 'shipping')). ' '. \lib\store::currency(); ?></td>
+                  		<td colspan="3"><?php echo \dash\fit::number(a($orderDetail, 'factor', 'shipping')). ' '. \lib\store::currency(); ?></td>
 	              	<?php }else{ ?>
-	              		<td><span class="fc-green"><?php echo T_("Free") ?></span></td>
+	              		<td colspan="3"><span class="fc-green"><?php echo T_("Free") ?></span></td>
 	              	<?php } //endif ?>
                 </tr>
 
 
               <tr class="active">
                 <td class="collapsing fc-mute"><?php echo T_("Total") ?></td>
-                <td class="txtB font-16"><?php echo \dash\fit::number(a($orderDetail, 'factor', 'total')). ' '. \lib\store::currency(); ?></td>
+                <td colspan="3" class="txtB font-16"><?php echo \dash\fit::number(a($orderDetail, 'factor', 'total')). ' '. \lib\store::currency(); ?></td>
               </tr>
 
 		</tbody>
