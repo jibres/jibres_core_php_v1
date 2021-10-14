c3728160add44e070abe3c3cc68e474ef7fff3be
Reza <rm.biqarar@gmail.com>
2021-10-14 09:58:40 +0330

    Remove menu link from setting

--- content_a/setting/home/display.php
+++ content_a/setting/home/display.php
@@ -101,15 +101,6 @@
                     </div>
                 </a>
             </div>
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
-                <a class="row" data-item href="<?php echo \dash\url::here(); ?>/setting/menu">
-                    <div class="c-auto"><i class="sf-bars"></i></div>
-                    <div class="c">
-                        <h2><?php echo T_("Menu"); ?></h2>
-                        <p><?php echo T_("Manage menu"); ?></p>
-                    </div>
-                </a>
-            </div>
         </div>
     </div>
 </div>
