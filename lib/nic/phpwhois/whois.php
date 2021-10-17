7716f98eaf65ccc5c3aa4b14539d60984532e417
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-10-17 16:19:16 +0330

    save

--- lib/nic/phpwhois/whois.php
+++ lib/nic/phpwhois/whois.php
@@ -84,7 +84,7 @@ class whois
                 {
 
                     // Getting whois information
-                    $fp = @fsockopen($whois_server, 43, $errno, $errstr, 60);
+                    $fp = @fsockopen($whois_server, 43, $errno, $errstr, 50);
                     if (!$fp)
                     {
                         return T_("Connection error!") . ' '. $errno. ' - '. $errstr;
