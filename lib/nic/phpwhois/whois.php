84677042a7a9401fce4b78bd236e7039cf904291
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-10-17 15:43:28 +0330

    increase timeout

--- lib/nic/phpwhois/whois.php
+++ lib/nic/phpwhois/whois.php
@@ -84,7 +84,7 @@ class whois
                 {
 
                     // Getting whois information
-                    $fp = @fsockopen($whois_server, 43, $errno, $errstr, 10);
+                    $fp = @fsockopen($whois_server, 43, $errno, $errstr, 20);
                     if (!$fp)
                     {
                         return T_("Connection error!") . ' '. $errno. ' - '. $errstr;
