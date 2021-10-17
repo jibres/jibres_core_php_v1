675694a90c774b4ab359d0789e88827d0ae5eb2b
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-10-17 18:22:09 +0330

    fix video aspect ratio and size

--- lib/shortcode.php
+++ lib/shortcode.php
@@ -206,14 +206,14 @@ class shortcode
 		{
 			if($split[3] === 'aparat')
 			{
-				$iframe = '<div class="shortcode" type="video" from="aparat">';
+				$iframe = '<div class="shortcode aspect-w-16 aspect-h-9" type="video" from="aparat">';
 				$iframe .= '<iframe src="https://www.aparat.com/video/video/embed/videohash/'. $split[5] .'/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>';
 				$iframe .= '</div>';
 				$_data = str_replace($split[0], $iframe, $_data);
 			}
 			elseif($split[3] === 'youtube')
 			{
-				$iframe = '<div class="shortcode" type="video" from="youtube">';
+				$iframe = '<div class="shortcode aspect-w-16 aspect-h-9" type="video" from="youtube">';
 				$iframe .= '<iframe src="https://www.youtube.com/embed/'. $split[5].'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 				$iframe .= '</div>';
 				$_data = str_replace($split[0], $iframe, $_data);
