ba35c01943d19ffe73fbf14b8ce6462254729b3e
Reza <rm.biqarar@gmail.com>
2021-10-18 13:24:16 +0330

    Save

--- content_a/setting/shipping/package/display.php
+++ content_a/setting/shipping/package/display.php
@@ -29,21 +29,21 @@ if(\lib\store::detail('mass_unit'))
           <div class="c-xs c-sm">
             <label for="length"><?php echo T_("Length"); ?></label>
             <div class="input">
-              <input type="number" step="0.1" max="99999" name="length" id="length" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_length() ?>">
+              <input type="number" step="0.01" max="99999" name="length" id="length" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_length() ?>">
 
             </div>
           </div>
           <div class="c-xs c-sm">
             <label for="width"><?php echo T_("Width"); ?></label>
             <div class="input">
-              <input type="number" step="0.1" max="99999" name="width" id="width" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_width() ?>">
+              <input type="number" step="0.01" max="99999" name="width" id="width" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_width() ?>">
 
             </div>
           </div>
           <div class="c-xs c-sm">
             <label for="height"><?php echo T_("Height"); ?></label>
             <div class="input">
-              <input type="number" step="0.1" max="99999" name="height" id="height" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_height() ?>">
+              <input type="number" step="0.01" max="99999" name="height" id="height" placeholder="<?php echo \lib\store::detail('length_unit') ?>" value="<?php echo \dash\data::dataRow_height() ?>">
             </div>
           </div>
           <div class="c-xs-2 c-sm-2">
@@ -55,7 +55,7 @@ if(\lib\store::detail('mass_unit'))
         </div>
         <label for="weight"><?php echo T_("Weight"); ?> <small><?php echo T_("When empty") ?></small></label>
         <div class="input">
-          <input type="number" step="0.1" max="99999" name="weight" id="weight" value="<?php echo \dash\data::dataRow_weight() ?>">
+          <input type="number" step="0.01" max="99999" name="weight" id="weight" value="<?php echo \dash\data::dataRow_weight() ?>">
           <label for="weight" class="addon"><?php echo $mass_unit ?></label>
         </div>
 
@@ -75,19 +75,21 @@ if(\lib\store::detail('mass_unit'))
     <?php foreach (\dash\data::packageList() as $key => $value){ ?>
       <div class="box">
         <div class="pad">
-          <div class="txtB fs14"><?php echo a($value, 'title') ?></div>
+          <div class="txtB mB10 fs14"><?php echo a($value, 'title') ?></div>
           <?php if(a($value, 'length')) {?><div><small><?php echo T_("Length"); ?></small> <?php echo \dash\fit::text(a($value, 'length')) ?> <small><?php echo $lenght_unit ?></small></div><?php } //endif ?>
           <?php if(a($value, 'width')) {?><div><small><?php echo T_("Width"); ?></small> <?php echo \dash\fit::text(a($value, 'width')) ?> <small><?php echo $lenght_unit ?></small></div><?php } //endif ?>
           <?php if(a($value, 'height')) {?><div><small><?php echo T_("Height"); ?></small> <?php echo \dash\fit::text(a($value, 'height')) ?> <small><?php echo $lenght_unit ?></small></div><?php } //endif ?>
           <?php if(a($value, 'weight')) {?><div><small><?php echo T_("Weight"); ?></small> <?php echo \dash\fit::text(a($value, 'weight')) ?> <small><?php echo $mass_unit ?></small></div><?php } //endif ?>
         </div>
-        <footer class="row">
-          <div class="c-auto"><div class="btn danger outline" data-confirm data-data='{"remove": "remove", "id" : "<?php echo a($value, 'id') ?>"}'><?php echo T_("Remove") ?></div></div>
-          <div class="c"></div>
-          <div class="c-auto">
-            <?php if(\dash\data::editMode() && \dash\request::get('id') == a($value, 'id')) { /*Nothing*/ }else{ ?>
-              <a href="<?php echo \dash\url::current(). '?id='. a($value, 'id') ?>" class="btn primary outline"><?php echo T_("Edit") ?></a>
-            <?php } //endif ?>
+        <footer class="">
+          <div class="row">
+            <div class="c-auto"><div class="linkDel btn" data-confirm data-data='{"remove": "remove", "id" : "<?php echo a($value, 'id') ?>"}'><?php echo T_("Remove") ?></div></div>
+            <div class="c"></div>
+            <div class="c-auto">
+              <?php if(\dash\data::editMode() && \dash\request::get('id') == a($value, 'id')) { /*Nothing*/ }else{ ?>
+                <a href="<?php echo \dash\url::current(). '?id='. a($value, 'id') ?>" class="btn primary outline"><?php echo T_("Edit") ?></a>
+              <?php } //endif ?>
+            </div>
           </div>
 
         </footer>
