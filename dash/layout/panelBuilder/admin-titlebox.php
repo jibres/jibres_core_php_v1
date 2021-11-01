d57789290ee715e9095d345e5117f1f9234c5b7e
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-01 13:40:38 +0330

    add new pageNav

---
+++ dash/layout/panelBuilder/admin-titlebox.php
@@ -0,0 +1,163 @@
+
+<?php
+if(\dash\face::boxTitle() !== false)
+{
+?>
+  <div class="titleBox">
+   <div class="row align-center">
+<?php if(\dash\data::back_text() && \dash\data::back_link()) { ?>
+    <div class="c-auto">
+     <a class="btn master back" <?php if(\dash\data::back_direct()) { echo 'data-direct';} ?> href="<?php echo \dash\data::back_link(); ?>"><i class="pRa5 sf-chevron-<?php if(\dash\language::dir() === 'rtl') { echo 'right'; } else { echo 'left'; } ?>"></i><span class="s0"><?php echo \dash\data::back_text(); ?></span></a>
+    </div>
+    <?php } // ?>
+    <div class="c pageTitle">
+     <h2><?php echo \dash\face::title(); ?></h2>
+    </div>
+
+
+    <nav class="c-auto actions">
+<?php if(\dash\face::btnImport()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnImport(); ?>"><i class="pRa5 compact sf-in"></i><span><?php echo T_("Import"); ?></span></a>
+<?php } // endif ?>
+<?php if(\dash\face::btnExport()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnExport(); ?>"><i class="pRa5 compact sf-out"></i><span><?php echo T_("Export"); ?></span></a>
+<?php } // endif ?>
+<?php if(\dash\face::btnNew()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnNew(); ?>"><i class="pRa5 compact sf-plus-circle"></i><span><?php echo T_("New"); ?></span></a>
+<?php } // endif ?>
+
+
+<?php if(\dash\face::btnDuplicate()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnDuplicate(); ?>"><i class="pRa5 compact sf-files-o"></i><span><?php echo T_("Duplicate"); ?></span></a>
+<?php } // endif ?>
+<?php if(\dash\face::btnSetting()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnSetting(); ?>"><i class="pRa5 compact sf-tools"></i><span><?php echo T_("Advance"); ?></span></a>
+<?php } // endif ?>
+
+<?php if(\dash\face::btnPreview()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnPreview(); ?>" target="_blank" data-fancybox='btnPreview' data-type="iframe" data-preload="false"><i class="pRa5 compact sf-binoculars"></i><span><?php echo T_("Preview"); ?></span></a>
+<?php } // endif ?>
+<?php if(\dash\face::btnView()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::btnView(); ?>" target="_blank" data-fancybox='btnView' data-type="iframe" data-preload="false"><i class="pRa5 compact sf-eye"></i><span><?php echo T_("View"); ?></span></a>
+<?php } // endif ?>
+<?php if(\dash\face::help()) { ?>
+     <a class="btn light" href="<?php echo \dash\face::help(); ?>" target="_blank"><i class="pRa5 compact sf-question-circle"></i><span><?php echo T_("Help"); ?></span></a>
+<?php } // endif ?>
+    </nav>
+
+
+<?php if(\dash\face::btnPrev() || \dash\face::btnNext()) { ?>
+    <nav class="c-auto os nav">
+     <a class="btn light <?php if(\dash\face::btnPrev() === 'disabled') { echo 'disabled'; } ?>" <?php if(\dash\face::btnPrev() !== 'disabled') { echo 'href="'. \dash\face::btnPrev().'"'; } ?> title='<?php echo T_("Previous item"); ?>'><i class="sf-chevron-<?php if(\dash\language::dir() === 'rtl') { echo 'right'; } else { echo 'left'; } ?>"></i></a>
+     <a class="btn light <?php if(\dash\face::btnNext() === 'disabled') { echo 'disabled'; } ?>" <?php if(\dash\face::btnNext() !== 'disabled') { echo 'href="'. \dash\face::btnNext().'"'; } ?>  title='<?php echo T_("Next item"); ?>'><i class="sf-chevron-<?php if(\dash\language::dir() === 'rtl') { echo 'left'; } else { echo 'right'; } ?>"></i></a>
+    </nav>
+<?php } // endif ?>
+
+
+<?php if(\dash\face::btnPrint()) { ?>
+     <a class="btn master" data-exec='print'><i class="pRa5 compact sf-print"></i><span><?php echo T_("Print"); ?></span></a>
+<?php } // endif ?>
+
+
+
+
+
+
+
+
+
+
+
+<?php if(\dash\data::action_text() && \dash\data::action_link()) { ?>
+    <nav class="c-auto os">
+     <a class="btn master" href="<?php echo \dash\data::action_link(); ?>" data-shortkey="120"><span><?php echo \dash\data::action_text(); ?></span> <kbd>F9</kbd></a>
+    </nav>
+<?php } // endif ?>
+<?php if(\dash\face::btnSave()) { ?>
+    <nav class="c-auto os btnSave"><?php
+  echo '<button class="btn master save" form="';
+  echo \dash\face::btnSave();
+  echo '"';
+  echo " name='submitall'";
+  if(\dash\face::btnSaveValue())
+  {
+    echo " value='". \dash\face::btnSaveValue(). "'";
+  }
+  echo '>';
+  if(\dash\face::btnSaveText())
+  {
+    echo \dash\face::btnSaveText();
+  }
+  else
+  {
+    echo T_("Save");
+  }
+  echo "</button>";
+ ?></nav>
+<?php } // endif ?>
+<?php if(\dash\face::btnInsert()) { ?>
+    <nav class="c-auto os btnInsert"><?php
+  echo '<button class="btn master insert" form="';
+  echo \dash\face::btnInsert();
+  echo '"';
+  echo " name='submitall'";
+  if(\dash\face::btnInsertValue())
+  {
+    echo " value='". \dash\face::btnInsertValue(). "'";
+  }
+  echo '>';
+  if(\dash\face::btnInsertText())
+  {
+    echo \dash\face::btnInsertText();
+  }
+  else
+  {
+    echo T_("Add");
+  }
+  echo "</button>";
+ ?></nav>
+<?php } // endif ?>
+   </div>
+
+
+
+<?php if(\dash\face::breadcrumb() && false)  {?>
+   <nav class="breadcrumb">
+    <?php foreach (\dash\face::breadcrumb() as $key => $value)
+    {
+      echo '<a';
+      if(isset($value['link']) && $value['link'])
+      {
+        echo ' href="'. $value['link']. '"';
+      }
+
+      if(isset($value['title']) && $value['title'])
+      {
+        echo ' title="'. $value['title']. '"';
+      }
+
+
+      if(isset($value['attr']) && $value['attr'])
+      {
+        echo $value['attr'];
+      }
+
+      echo '>';
+
+      if(isset($value['icon']) && $value['icon'])
+      {
+        echo '<span class="sf-'. $value['icon'].' mRa5"></span>';
+      }
+
+      if(isset($value['text']))
+      {
+        echo $value['text'];
+      }
+
+      echo '</a>';
+    }
+    ?>
+   </nav>
+<?php } // endif ?>
+  </div>
+<?php } // endif ?>
---
+++ dash/layout/panelBuilder/pageNav.php
@@ -0,0 +1,158 @@
+<?php
+namespace dash\layout\panelBuilder;
+
+
+
+class pageNav
+{
+  public static function html()
+  {
+    if(\dash\face::boxTitle() === false)
+    {
+      return null;
+    }
+
+    $html = '<div class="titleBox-v2">';
+    {
+      $html .= '<div class="flex flex-wrap content-center align-center">';
+      {
+        if(\dash\data::back_text() && \dash\data::back_link())
+        {
+          $html .= self::backBtn();
+        }
+        $html .= '<div class="flex-grow px-5 font-bold">'. \dash\face::title(). '</div>';
+        // actionGroup
+        $html .= '<nav class="actions flex-none">';
+        {
+          $html .= self::btn(\dash\face::btnImport(), T_("Import"), ['icon' => 'import', 'iconGroup' => 'minor']);
+          $html .= self::btn(\dash\face::btnExport(), T_("Export"), ['icon' => 'export', 'iconGroup' => 'minor']);
+          $html .= self::btn(\dash\face::btnNew(), T_("New"), ['icon' => 'add']);
+          $html .= self::btn(\dash\face::btnDuplicate(), T_("Duplicate"), ['icon' => 'duplicate', 'iconGroup' => 'minor']);
+          $html .= self::btn(\dash\face::btnSetting(), T_("Advance"),  ['icon' => 'tools']);
+          $html .= self::btn(\dash\face::btnPreview(), T_("Preview"),  ['icon' => 'view', 'iconGroup' => 'minor', 'special' => 'preview']);
+          $html .= self::btn(\dash\face::btnPrint(), T_("Print"),  ['icon' => 'print', 'iconGroup' => null, 'special' => 'print']);
+          $html .= self::btn(\dash\face::btnView(), T_("View"),  ['icon' => 'view', 'iconGroup' => null, 'special' => 'view']);
+          $html .= self::btn(\dash\face::help(), T_("Help"),  ['icon' => 'Question Mark', 'iconGroup' => 'minor', 'special' => 'help']);
+        }
+        $html .= '</nav>';
+        if(\dash\face::btnPrev() || \dash\face::btnNext() or 1)
+        {
+          $html .= self::nextPrevBtn();
+        }
+      }
+      $html .= '</div>';
+    }
+    $html .= '</div>';
+
+    return $html;
+  }
+
+
+  private static function backBtn()
+  {
+    $html = '';
+    $html .= '<div class="flex-none backBtn">';
+    {
+      $html .= '<a class="btn-light btn-icon btn-sm" href="'. \dash\data::back_link(). '" ';
+      if(\dash\data::back_direct())
+      {
+        $html .= 'data-direct';
+      }
+      $html .= '>';
+      {
+        if(\dash\language::dir() === 'rtl')
+        {
+         $html .= \dash\utility\icon::svg('ChevronRight', 'minor');
+        }
+        else
+        {
+         $html .= \dash\utility\icon::svg('ChevronLeft', 'minor');
+        }
+        $html .= '<span>'. \dash\data::back_text(). '</span>';
+      }
+      $html .= '</a>';
+    }
+    $html .= '</div>';
+    return $html;
+  }
+
+
+  private static function btn($_link, $_text = null, $_arg = null)
+  {
+    $html = '';
+    if($_link or 1)
+    {
+      $html .= '<a';
+      {
+        $html .= ' class="btn-light btn-icon btn-sm mx-0.5 my-0.5';
+        if($_link === 'disabled')
+        {
+          $html .= ' disabled';
+        }
+        $html .= '"';
+        if($_link)
+        {
+          $html .= ' href="'. $_link. '"';
+        }
+
+        if(a($_arg, 'desc'))
+        {
+          $html .= ' title="'. a($_arg, 'desc'). '"';
+        }
+
+        switch (a($_arg, 'special'))
+        {
+          case 'preview':
+            $html .= ' target="_blank" data-type="iframe" data-preload="false" data-fancybox="btnPreview"';
+            break;
+
+          case 'view':
+            $html .= ' target="_blank" data-type="iframe" data-preload="false" data-fancybox="btnView"';
+            break;
+
+          case 'help':
+            $html .= ' target="_blank"';
+            break;
+
+          case 'print':
+            $html .= ' data-exec="print"';
+            break;
+
+          default:
+            break;
+        }
+        $html .= '>';
+        $html .= \dash\utility\icon::svg(a($_arg, 'icon'), a($_arg, 'iconGroup'));
+        if($_text)
+        {
+          $html .= '<span>'. $_text. '</span>';
+        }
+      }
+      $html .= '</a>';
+    }
+    return $html;
+  }
+
+
+
+  private static function nextPrevBtn()
+  {
+    $nextIcon = 'Arrow Right';
+    $prevIcon = 'Arrow Left';
+    if(\dash\language::dir() === 'rtl')
+    {
+      $nextIcon = 'Arrow Left';
+      $prevIcon = 'Arrow Right';
+    }
+
+    $html = '';
+    $html .= '<nav>';
+    {
+      $html .= self::btn(\dash\face::btnPrev(), 'P', ['icon' => $prevIcon, 'iconGroup' => 'minor', 'desc' => T_("Previous item")]);
+      $html .= self::btn(\dash\face::btnNext(), 'N', ['icon' => $nextIcon, 'iconGroup' => 'minor', 'desc' => T_("Next item")]);
+    }
+    $html .= '</nav>';
+    return $html;
+  }
+
+}
