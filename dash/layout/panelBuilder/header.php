d845d73dfaf8eaacec928aaa8f7ae27fc8f02ffa
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-03 20:14:31 +0330

    fix header

--- dash/layout/panelBuilder/header.php
+++ dash/layout/panelBuilder/header.php
@@ -15,9 +15,45 @@ class header
       $targetLink = ' target="_blank"';
     }
 
-    $html .= '<div class="h-full flex flex-wrap content-center px-3">';
+    $html .= '<div class="h-full flex flex-wrap content-center align-center px-3">';
+    {
+      // logo
+      $html .= '<a class="logo" href="'. $masterUrl. '"';
+      if($targetLink)
+      {
+        $html .= $targetLink;
+      }
+      $html .= '>';
+      $html .= '<img class="h-12 w-12 rounded-lg" src="'. \dash\face::logo(). '" alt="'. \dash\face::site(). '">';
+      $html .= '</a>';
 
-    $html .= '123';
+      // title
+      $html .= '<h1 class="mx-2 flex-grow">';
+      $html .= '<a class="cauto logo" href="'. $masterUrl. '"';
+      if($targetLink)
+      {
+        $html .= $targetLink;
+      }
+      $html .= '>';
+      $html .= \dash\face::site();
+      $html .= '</a>';
+      $html .= '</h1>';
+
+
+      $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light rounded-lg" href="'. \dash\url::support(). '" target="_blank" title="'. T_("Help Center"). '">';
+      $html .= \dash\utility\icon::svg('Question Mark Inverse');
+      $html .= '</a>';
+
+      $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light rounded-lg" href="'. \dash\url::sitelang(). '/account/notification" title="'. \dash\user::detail('displayname'). '">';
+      $html .= \dash\utility\icon::svg('Notification');
+      $html .= '</a>';
+
+      $html .= '<a class="h-12 w-12 p-1 mx-1 btn-light rounded-lg" href="'. \dash\url::kingdom(). '/account">';
+      // $html .= '<img class="" href="'. \dash\fit::img(\dash\user::detail('avatar')). '" title="'. T_("Avatar of you"). '">';
+      $html .= '<img class="" href="'. \dash\face::logo(). '" title="'. T_("Avatar of you"). '">';
+      $html .= '</a>';
+
+    }
     $html .= '</div>';
 
 
