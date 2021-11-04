c65d1dd72739bc3c5ec68ac93472389f8ee55334
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-04 15:32:08 +0330

    fix header

--- dash/layout/panelBuilder/header.php
+++ dash/layout/panelBuilder/header.php
@@ -40,18 +40,48 @@ class header
       $html .= '</h1>';
 
 
-      $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light rounded-lg" href="'. \dash\url::support(). '" target="_blank" title="'. T_("Help Center"). '">';
-      $html .= \dash\utility\icon::svg('Question Mark Inverse');
-      $html .= '</a>';
+      if(\dash\url::support())
+      {
+        $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light transition rounded-lg" href="'. \dash\url::support(). '" target="_blank" title="'. T_("Help Center"). '">';
+        $html .= \dash\utility\icon::svg('Question Mark Inverse');
+        $html .= '</a>';
+      }
 
-      $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light rounded-lg" href="'. \dash\url::sitelang(). '/account/notification" title="'. \dash\user::detail('displayname'). '">';
-      $html .= \dash\utility\icon::svg('Notification');
-      $html .= '</a>';
+      if(\dash\user::id())
+      {
+        $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light transition rounded-lg" href="'. \dash\url::kingdom(). '/a/order/unprocessed" title="'. T_("Unprocessed Orders"). '">';
+        $html .= \dash\utility\icon::svg('First Order');
+        $html .= '</a>';
 
-      $html .= '<a class="h-12 w-12 p-1 mx-1 btn-light rounded-lg" href="'. \dash\url::kingdom(). '/account">';
-      // $html .= '<img class="" href="'. \dash\fit::img(\dash\user::detail('avatar')). '" title="'. T_("Avatar of you"). '">';
-      $html .= '<img class="" href="'. \dash\face::logo(). '" title="'. T_("Avatar of you"). '">';
-      $html .= '</a>';
+        // notification
+        $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light transition rounded-lg" href="'. \dash\url::sitelang(). '/account/notification" title="'. T_("Notifications"). '">';
+        $html .= \dash\utility\icon::svg('Notification');
+        $html .= '</a>';
+
+        // avatar
+        $html .= '<a class="h-12 w-12 p-1 mx-1 btn-light transition rounded-lg" href="'. \dash\url::kingdom(). '/account" title="'. \dash\user::detail('displayname'). '">';
+        if(\dash\user::detail('avatar'))
+        {
+          $html .= '<img class="rounded-full" src="'. \dash\fit::img(\dash\user::detail('avatar')). '" alt="'. \dash\user::detail('displayname'). '">';
+        }
+        else
+        {
+          $html .= '<img class="rounded-full" src="'. \dash\url::cdn(). '/img/avatar/guest.png" alt="'. T_('Default Avatar'). '">';
+        }
+        $html .= '</a>';
+
+
+      }
+      else
+      {
+        // enter to jibres
+        $html .= '<a class="h-12 w-12 p-3 mx-1 btn-light transition rounded-lg" href="'. \dash\url::kingdom(). '/enter?referer='. urlencode(\dash\url::location()). '" title="'. T_("Enter to have better experience"). '">';
+        $html .= \dash\utility\icon::svg('Circle Alert', null, '#c80a5a');
+        $html .= '</a>';
+      }
+
+
+
 
     }
     $html .= '</div>';
