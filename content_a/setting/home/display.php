e82b0472b31556ac4a018ea48aff47f2c87ac1e7
Reza <rm.biqarar@gmail.com>
2021-11-05 15:28:54 +0330

    Save

--- content_a/setting/home/display.php
+++ content_a/setting/home/display.php
@@ -1,85 +1,78 @@
-<div class="avand-sm zero">
-    <div class="box">
-      <div class='font-16'>
-        <select class="select22" data-model='html' data-ajax--url="<?php echo \dash\url::this() ?>/search" data-shortkey-search data-placeholder="<?php echo T_("Find a setting") ?>"></select>
-      </div>
-    </div>
-</div>
 
 <div class="box" data-settings>
     <div class="body">
         <div class="row">
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/general">
-                    <div class="c-auto"><i class="sf-cog"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Settings') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("General"); ?></h2>
-                        <p><?php echo T_("View and update your business details"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("View and update your business details"); ?></p>
                     </div>
                 </a>
             </div>
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/product">
-                    <div class="c-auto"><i class="sf-package"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Products') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Products"); ?></h2>
-                        <p><?php echo T_("Manage how your business products organized"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage how your business products organized"); ?></p>
                     </div>
                 </a>
             </div>
 
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/factor">
-                    <div class="c-auto"><i class="sf-receipt-shopping-streamline"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Orders') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Factors"); ?></h2>
-                        <p><?php echo T_("Manage how your business factors setting"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage how your business factors setting"); ?></p>
                     </div>
                 </a>
             </div>
 
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/shipping">
-                    <div class="c-auto"><i class="sf-plane-airport"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Shipment') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Shipping"); ?></h2>
-                        <p><?php echo T_("Manage how you ship orders to customers"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage how you ship orders to customers"); ?></p>
                     </div>
                 </a>
             </div>
 
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/order">
-                    <div class="c-auto"><i class="sf-receipt-shopping-streamline"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Orders') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Orders"); ?></h2>
-                        <p><?php echo T_("Order setting"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Order setting"); ?></p>
                     </div>
                 </a>
             </div>
 
 
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/thirdparty">
-                    <div class="c-auto"><i class="sf-lamp"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Hint') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Third Party Services"); ?></h2>
-                        <p><?php echo T_("Connect to other services like google analytics, live chat"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Connect to other services like google analytics, live chat"); ?></p>
                     </div>
                 </a>
             </div>
 
             <?php if(\dash\url::isLocal()) {?>
-             <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+             <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::here(); ?>/plugin">
-                    <div class="c-auto"><i class="sf-plug fc-green"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('ManagedStore') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Plugin"); ?></h2>
-                        <p><?php echo T_("Plugin and failures"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Plugin and failures"); ?></p>
                     </div>
                 </a>
             </div>
@@ -92,12 +85,12 @@
 <div class="box" data-settings>
     <div class="body">
         <div class="row">
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::here(); ?>/accounting">
-                    <div class="c-auto"><i class="sf-book"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Legal') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Accounting"); ?></h2>
-                        <p><?php echo T_("Manage your business accounting"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage your business accounting"); ?></p>
                     </div>
                 </a>
             </div>
@@ -111,43 +104,43 @@
     <div class="body">
         <div class="row">
         <?php if(\dash\permission::check('_group_setting')) {?>
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-direct data-item href="<?php echo \dash\url::kingdom(). '/site'; ?>">
-                    <div class="c-auto"><i class="sf-monitor"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Store') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Website Builder"); ?></h2>
-                        <p><?php echo T_("Manage your business website settings"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage your business website settings"); ?></p>
                     </div>
                 </a>
             </div>
         <?php } // endif ?>
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::here(); ?>/android">
-                    <div class="c-auto"><i class="sf-mobile"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('Mobile') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Mobile Online Store"); ?></h2>
-                        <p><?php echo T_("Manage your business app settings"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage your business app settings"); ?></p>
                     </div>
                 </a>
             </div>
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/pos">
-                    <div class="c-auto"><i class="sf-cash-register"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('PointOfSale') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Point of Sale Software"); ?></h2>
-                        <p><?php echo T_("Manage your business pos configuration"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage your business pos configuration"); ?></p>
                     </div>
                 </a>
             </div>
 
-            <div class="c-xs-12 c-md-6 c-lg-4 c-xxl-3">
+            <div class="mt-5 c-xs-12 c-md-6 c-lg-4 c-xxl-3">
                 <a class="row" data-item href="<?php echo \dash\url::this(); ?>/social">
-                    <div class="c-auto"><i class="sf-internet"></i></div>
+                    <div class="c-auto"><div class=" w-20 p-5 pt-1"><?php echo \dash\utility\icon::svg('SocialAd') ?></div></div>
                     <div class="c">
                         <h2><?php echo T_("Social Marketing"); ?></h2>
-                        <p><?php echo T_("Manage your business social marketing"); ?></p>
+                        <p class="text-gray-500"><?php echo T_("Manage your business social marketing"); ?></p>
                     </div>
                 </a>
             </div>
