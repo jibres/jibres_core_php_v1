77271ae47d9be87d0422ff69523ffdf4a81daac2
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-13 20:34:16 +0330

    fix msg

--- content_a/chap/size-a4/a4-1.php
+++ content_a/chap/size-a4/a4-1.php
@@ -41,7 +41,7 @@ if(isset($factorDetail['factor_detail']) && is_array($factorDetail['factor_detai
 <?php require('oneSide-seller.php'); ?>
 <?php require('oneSide-buyer.php'); ?>
 
-    <table class="w-full table-auto border border-gray-400 rounded">
+    <table class="w-full table-auto border border-gray-400 rounded mb-1">
      <thead>
       <tr class="bg-blue-100 text-xs leading-10 text-right border-b-2 border-blue-500">
        <th></th>
@@ -116,7 +116,7 @@ $tableTotal['FinalPrice'] += $FinalPrice;
 <div class="f">
   <div class="c">
     <?php if(a($factorDetail, 'factor', 'desc')) {?>
-      <p class="msg font-14"><?php echo nl2br(a($factorDetail, 'factor', 'desc')) ?></p>
+      <p class="text-sm bg-gray-50 p-2 mb-1"><?php echo nl2br(a($factorDetail, 'factor', 'desc')) ?></p>
     <?php } //endif ?>
   </div>
   <?php if(floatval(a($factorDetail, 'factor', 'total')) !== floatval($tableTotal['FinalPrice'])) {?>
