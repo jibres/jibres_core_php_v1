ab2d733fe6662c8e0bc098101d63180401e87a08
Reza <rm.biqarar@gmail.com>
2021-11-14 16:47:26 +0330

    Add clickable option

---
+++ content_site/options/video/video_clickable.php
@@ -0,0 +1,21 @@
+<?php
+namespace content_site\options\video;
+
+
+trait video_clickable
+{
+	use video_option;
+
+	public static function name()
+	{
+		return 'video_clickable';
+	}
+
+	public static function title()
+	{
+		return T_("Clickable");
+	}
+
+
+}
+?>
---
+++ content_site/options/video/video_clickable_gallery.php
@@ -0,0 +1,17 @@
+<?php
+namespace content_site\options\video;
+
+
+class video_clickable_gallery
+{
+	use video_clickable;
+
+
+	public static function gallery_mode()
+	{
+		return true;
+	}
+
+
+}
+?>
