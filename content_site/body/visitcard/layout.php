a6d3776a532b1fc4ba31b4d8c04a1fae7bf50c4b
Reza <rm.biqarar@gmail.com>
2021-11-18 12:49:01 +0330

    Fix ready visitcard detail in assemble fire

--- content_site/body/visitcard/layout.php
+++ content_site/body/visitcard/layout.php
@@ -15,49 +15,8 @@ class layout
 	 */
 	public static function layout($_args)
 	{
-
-		$social = \lib\store::all_social_list();
-
-		foreach ($social as $key => $value)
-		{
-			if(a($_args, 'use_as_socialnetwork') === 'business_socialnetwork')
-			{
-				$my_social = \lib\store::social($key);
-				if($my_social)
-				{
-					$_args[$key] = a($my_social, 'user');
-				}
-				else
-				{
-					$_args[$key] = null;
-				}
-			}
-
-			if(a($_args, $key))
-			{
-				$_args[$key] = array_merge($value, ['user' => $_args[$key], 'link' => a($value, 'link'). $_args[$key]]);
-			}
-		}
-
-		if(a($_args, 'use_as_logo')   === 'business_logo')
-		{
-			$_args['logo'] = \lib\store::logo();
-		}
-
-		if(a($_args, 'logo'))
-		{
-			$_args['logo'] = \lib\filepath::fix($_args['logo']);
-		}
-
-		if(a($_args, 'use_as_heading')       === 'business_heading')
-		{
-			$_args['heading'] = \lib\store::title();
-		}
-
-		if(a($_args, 'use_as_description')   === 'business_description')
-		{
-			$_args['description'] = \lib\store::desc();
-		}
+		$_args = \content_site\assemble\fire::store_detail($_args);
+		$_args = \content_site\assemble\fire::store_social($_args);
 
 		return \content_site\call_function::final_html(__NAMESPACE__, a($_args, 'model'), $_args);
 	}
