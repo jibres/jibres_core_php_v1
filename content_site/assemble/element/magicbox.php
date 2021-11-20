5cf3fdf2b4802935bd6cb20e828caa98372b8fe4
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-20 17:38:15 +0330

    save

--- content_site/assemble/element/magicbox.php
+++ content_site/assemble/element/magicbox.php
@@ -265,7 +265,7 @@ class magicbox
 					}
 					$card .= "<div data-magic-caption class='absolute flex align-center inset-x-0 block px-4 py-2 z-10 transition $cardClass'>";
 					{
-						$card .= self::htmlCaptionBox($_item, $showTitle, $showPrice);
+						$card .= self::htmlCaptionBox($_item, $_args, $showTitle, $showPrice);
 					}
 					$card .= '</div>';
 				}
@@ -273,7 +273,7 @@ class magicbox
 				{
 					$card .= "<div class='block transition text-white px-4 py-2 z-10 $cardClass'>";
 					{
-						$card .= self::htmlCaptionBox($_item, $showTitle, $showPrice);
+						$card .= self::htmlCaptionBox($_item, $_args, $showTitle, $showPrice);
 					}
 					$card .= '</div>';
 				}
@@ -286,7 +286,7 @@ class magicbox
 	}
 
 
-	private static function htmlCaptionBox($_itemData, $_showTitle = null, $_showPrice = null)
+	private static function htmlCaptionBox($_itemData, $_args, $_showTitle = null, $_showPrice = null)
 	{
 		$myTitle   = a($_itemData, 'title');
 		$myDesc    = a($_itemData, 'desc');
@@ -336,13 +336,21 @@ class magicbox
 		if($myBtnText)
 		{
 			$btnClass = 'btn-primary btn-sm';
-			// if(a($_args, 'link_color'))
-			// {
-			// 	$btnClass .= ' link-'. a($_args, 'link_color');
-			// }
+			if(a($_args, 'btn_color'))
+			{
+				$btnClass = 'btn-'. a($_args, 'btn_color'). ' btn-sm';
+			}
 			$html .= "<div class='". $btnClass. "'>";
 			{
 				$html .= $myBtnText;
+				// if(\dash\language::dir() === 'rtl')
+				// {
+				// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="px-1" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg>';
+				// }
+				// else
+				// {
+				// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>';
+				// }
 			}
 			$html .= '</div>';
 		}
