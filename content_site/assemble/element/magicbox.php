9e791e9ae382bea53ac0aa0228638f45d402e3df
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-22 16:23:56 +0330

    if 1 magicbox on row use btn on seperate line

--- content_site/assemble/element/magicbox.php
+++ content_site/assemble/element/magicbox.php
@@ -277,7 +277,7 @@ class magicbox
 					{
 						$cardClass .= ' bottom-0';
 					}
-					$card .= "<div data-magic-caption class='absolute flex align-center inset-x-0 block px-2 lg:px-3 py-2 lg:py-3 z-10 transition $cardClass'>";
+					$card .= "<div data-magic-caption class='absolute flex flex-wrap align-center inset-x-0 block px-2 lg:px-3 py-2 lg:py-3 z-10 transition $cardClass'>";
 					{
 						$card .= self::htmlCaptionBox($_item, $_args, $showTitle, $showPrice);
 					}
@@ -349,27 +349,38 @@ class magicbox
 		// show btn
 		if($myBtnText)
 		{
+			$btnLineClass = 'flex-none';
 			$btnClass = 'btn-primary';
-			if(a($_args, 'btn_color'))
+
+			if(a($_args, 'image_count') === 1)
 			{
-				$btnClass = 'btn-'. a($_args, 'btn_color');
+				$btnLineClass .= ' w-full mt-2';
+				$btnClass .= ' px-4';
 			}
-			$btnClass .= ' btn-sm';
-			if(a($_args, 'radius:class'))
+			$html .= "<div class='". $btnLineClass. "'>";
 			{
-				$btnClass .= ' '. a($_args, 'radius:class');
-			}
-			$html .= "<div class='". $btnClass. "'>";
-			{
-				$html .= $myBtnText;
-				// if(\dash\language::dir() === 'rtl')
-				// {
-				// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="px-1" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg>';
-				// }
-				// else
-				// {
-				// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>';
-				// }
+				if(a($_args, 'btn_color'))
+				{
+					$btnClass = 'btn-'. a($_args, 'btn_color');
+				}
+				$btnClass .= ' btn-sm';
+				if(a($_args, 'radius:class'))
+				{
+					$btnClass .= ' '. a($_args, 'radius:class');
+				}
+				$html .= "<div class='". $btnClass. "'>";
+				{
+					$html .= $myBtnText;
+					// if(\dash\language::dir() === 'rtl')
+					// {
+					// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="px-1" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/></svg>';
+					// }
+					// else
+					// {
+					// 	$html .= '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>';
+					// }
+				}
+				$html .= '</div>';
 			}
 			$html .= '</div>';
 		}
