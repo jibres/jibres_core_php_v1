081243e5efbd8d117c68fb28c0ce4bb94653cd74
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-24 15:03:46 +0330

    bugfix 2 item in row

--- content_site/assemble/grid.php
+++ content_site/assemble/grid.php
@@ -33,7 +33,7 @@ class grid
 				{
 					$colStart   = 'col-start-1 md:col-start-3';
 				}
-				$colSpan = 'col-span-12 sm:col-span-6 md:col-span-4';
+				$colSpan = 'col-span-12 sm:col-span-6';
 				break;
 
 			case '3':
