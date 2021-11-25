cce1a48c28609fb789aba4b18fb85f8abc405a06
Reza <rm.biqarar@gmail.com>
2021-11-25 16:47:59 +0330

    Fix padding

--- content_site/options/padding/padding.php
+++ content_site/options/padding/padding.php
@@ -6,27 +6,25 @@ trait padding
 {
 	public static function enum()
 	{
-	$enum   = [];
+		$enum   = [];
+		$enum[] = ['key' => '0', 'class' => 'p-px'];
 		$enum[] = ['key' => '1', 'class' => 'py-1 md:py-1.5 lg:py-2', ];
 		$enum[] = ['key' => '2', 'class' => 'py-2 md:py-3 lg:py-4', ];
 		$enum[] = ['key' => '3', 'class' => 'py-3 md:py-4 lg:py-6', ];
-		$enum[] = ['key' => 'manual','title' => '...',   	   'class' => '', 	'icon' => \dash\utility\icon::svg('three-dots', 'bootstrap'),];
-
-		$enum[] = ['key' => '0', 'hide' => true, 'is_range' => true, 'class' => 'p-px'];
-		$enum[] = ['key' => '4', 'hide' => true, 'is_range' => true, 'class' => 'py-4 md:py-6 lg:py-8'];
-		$enum[] = ['key' => '6', 'hide' => true, 'is_range' => true, 'class' => 'py-6 md:py-8 lg:py-12'];
-		$enum[] = ['key' => '8', 'hide' => true, 'is_range' => true, 'class' => 'py-8 md:py-10 lg:py-16'];
-		// $enum[] = ['key' => '10', 'hide' => true, 'is_range' => true, 'class' => 'py-10 md:py-6 lg:py-20'];
-		$enum[] = ['key' => '12', 'hide' => true, 'is_range' => true, 'class' => 'py-12 md:py-6 lg:py-24'];
-		// $enum[] = ['key' => '14', 'hide' => true, 'is_range' => true, 'class' => 'py-14 md:py-6 lg:py-28'];
-		$enum[] = ['key' => '16', 'hide' => true, 'is_range' => true, 'class' => 'py-16 md:py-6 lg:py-32'];
-		// $enum[] = ['key' => '20', 'hide' => true, 'is_range' => true, 'class' => 'py-20 md:py-6 lg:py-40'];
-		$enum[] = ['key' => '24', 'hide' => true, 'is_range' => true, 'class' => 'py-24 md:py-6 lg:py-48'];
-		// $enum[] = ['key' => '28', 'hide' => true, 'is_range' => true, 'class' => 'py-28 md:py-6 lg:py-56'];
-		$enum[] = ['key' => '32', 'hide' => true, 'is_range' => true, 'class' => 'py-32 md:py-6 lg:py-64'];
-		// $enum[] = ['key' => '36', 'hide' => true, 'is_range' => true, 'class' => 'py-36 md:py-6 lg:py-72'];
-		// $enum[] = ['key' => '40', 'hide' => true, 'is_range' => true, 'class' => 'py-40 md:py-6 lg:py-80'];
-		$enum[] = ['key' => '48', 'hide' => true, 'is_range' => true, 'class' => 'py-48 md:py-6 lg:py-96'];
+		$enum[] = ['key' => '4', 'class' => 'py-4 md:py-6 lg:py-8'];
+		$enum[] = ['key' => '6', 'class' => 'py-6 md:py-8 lg:py-12'];
+		$enum[] = ['key' => '8', 'class' => 'py-8 md:py-10 lg:py-16'];
+		// $enum[] = ['key' => '10', 'class' => 'py-10 md:py-6 lg:py-20'];
+		$enum[] = ['key' => '12', 'class' => 'py-12 md:py-6 lg:py-24'];
+		// $enum[] = ['key' => '14', 'class' => 'py-14 md:py-6 lg:py-28'];
+		$enum[] = ['key' => '16', 'class' => 'py-16 md:py-6 lg:py-32'];
+		// $enum[] = ['key' => '20', 'class' => 'py-20 md:py-6 lg:py-40'];
+		$enum[] = ['key' => '24', 'class' => 'py-24 md:py-6 lg:py-48'];
+		// $enum[] = ['key' => '28', 'class' => 'py-28 md:py-6 lg:py-56'];
+		$enum[] = ['key' => '32', 'class' => 'py-32 md:py-6 lg:py-64'];
+		// $enum[] = ['key' => '36', 'class' => 'py-36 md:py-6 lg:py-72'];
+		// $enum[] = ['key' => '40', 'class' => 'py-40 md:py-6 lg:py-80'];
+		$enum[] = ['key' => '48', 'class' => 'py-48 md:py-6 lg:py-96'];
 
 		return $enum;
 	}
@@ -35,17 +33,7 @@ trait padding
 
 	public static function this_range()
 	{
-		$range = [];
-		foreach (self::enum() as $key => $value)
-		{
-			if(a($value, 'is_range'))
-			{
-				$range[] = $value['key'];
-			}
-		}
-
-
-		return $range;
+		return array_column(self::enum(), 'key');
 	}
 
 
@@ -114,79 +102,11 @@ trait padding
 		$title = self::title();
 
 		$html = '';
-		$html .= '<div class="accordion" id="'.self::name().'Option">';
+		$html .= \content_site\options\generate::form();
 		{
-			$html .= \content_site\options\generate::form();
-			{
-
-					$html .= "<label>$title</label>";
-
-					$name       = 'opt_'. self::name();
-
-					$radio_html = '';
-
-					foreach (self::enum() as $key => $value)
-					{
-						if(isset($value['hide']) && $value['hide'])
-						{
-							continue;
-						}
-
-						$selected = false;
-
-						if($default === $value['key'])
-						{
-							$selected = true;
-						}
-
-						$my_name = a($value, 'title');
-						if(a($value, 'icon'))
-						{
-							$my_name = $value['icon'];
-						}
-
-						$option = [];
-
-						if($value['key'] === 'manual')
-						{
-							$option =
-							[
-								'input'        => false,
-								'class'        => 'accordion-collapse ',
-								'attr'         => ' data-bs-toggle="collapse" data-bs-target="#optionShowMore'.self::name().'" aria-expanded="true" aria-controls="optionShowMore'.self::name().'"',
-							];
-						}
-
-						$radio_html .= \content_site\options\generate::radio_line_itemText($name, $value['key'], $my_name, $selected, null, $option);
-					}
-
-					$html .= \content_site\options\generate::radio_line_add_ul($name, $radio_html);
-
-
-
-			}
-			$html .= \content_site\options\generate::_form();
-
-			$html .= \content_site\options\generate::form();
-			{
-
-					$visible_class = 'show';
-
-					if($default === 'manual' || $default === 'auto' || $default === 'fullscreen')
-					{
-						$visible_class = null;
-					}
-
-					$html .= "<div  id='optionShowMore".self::name()."' class='collapse $visible_class' aria-labelledby='optionMore' data-bs-parent='#".self::name()."Option'>";
-					{
-						$html .= \content_site\options\generate::rangeslider($name, self::this_range(), $default, null);
-					}
-					$html .= '</div>';
-			}
-			$html .= \content_site\options\generate::_form();
+			$html .= \content_site\options\generate::rangeslider('opt_'. self::name(), self::this_range(), $default, self::title());
 		}
-		$html .= '</div>';
-
+		$html .= \content_site\options\generate::_form();
 
 		return $html;
 
