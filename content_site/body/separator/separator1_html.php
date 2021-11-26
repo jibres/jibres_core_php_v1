2be5db941a031d04004531d6eb981eb7d72b526c
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-26 14:54:29 +0330

    add text to seperator 1 & 2

--- content_site/body/separator/separator1_html.php
+++ content_site/body/separator/separator1_html.php
@@ -8,18 +8,43 @@ class separator1_html
 	{
 		$html = \content_site\assemble\wrench\section::element_start($_args);
 		{
-			$html .= \content_site\assemble\wrench\section::container($_args);
+			$html .= \content_site\assemble\wrench\section::container($_args, ['class' => 'text-center']);
 			{
-				$width = '5px';
-				$style = 'double';
-				$color = '#999';
+				$height = '10px';
+				$style = 'solid';
+				$color = '#358ab1';
+				$text = '§';
 
-				$hrStyle = 'border:none;width:100%;';
-				$hrStyle .= 'border-bottom:'. $width. ' '. $style. ' '. $color. ';';
+				$hrStyle = 'border:none;width:100%;display:block;margin:10px 0;';
+				$hrStyle .= 'border-bottom:'. $height. ' '. $style. ' '. $color. ';';
 
 				$html .= '<hr';
 				$html .= ' style="'. $hrStyle. '"';
 				$html .= '>';
+
+				if($text)
+				{
+					$topOffset = str_replace('px', '', $height);
+					$topOffset = round(intval($topOffset) / 2) - 1;
+					$topOffset = $topOffset + 20;
+
+					$textStyle = 'position:relative;display:inline-block;line-height:20px;height:20px;max-width:200px;padding:0 10px;';
+					$textStyle .= 'top:-'. $topOffset.'px;';
+					$textStyle .= 'color:'. $color. ';';
+					$textStyle .= 'font-size:28px;';
+					if(a($_args, 'background:style'))
+					{
+						$textStyle .= a($_args, 'background:style');
+					}
+
+					$html .= '<span';
+					$html .= ' style="'. $textStyle. '"';
+					$html .= '>';
+					{
+						$html .= $text;
+					}
+					$html .= '</span>';
+				}
 			}
 			$html .= "</div>";
 		}
--- content_site/body/separator/separator2_html.php
+++ content_site/body/separator/separator2_html.php
@@ -8,20 +8,43 @@ class separator2_html
 	{
 		$html = \content_site\assemble\wrench\section::element_start($_args);
 		{
-			$html .= \content_site\assemble\wrench\section::container($_args);
+			$html .= \content_site\assemble\wrench\section::container($_args, ['class' => 'text-center']);
 			{
 				$height = '10px';
-
 				$color = '#004bb0';
+				$text = '§';
 
 				$hrStyle = 'border:none;width:100%;display:block;margin:10px 0;';
 				$hrStyle .= 'height:'. $height. ';';
 				$hrStyle .= 'background-image:linear-gradient(to right,rgba(0,0,0,0),'. $color. ',rgba(0,0,0,0));';
 
-
 				$html .= '<hr';
 				$html .= ' style="'. $hrStyle. '"';
 				$html .= '>';
+
+				if($text)
+				{
+					$topOffset = str_replace('px', '', $height);
+					$topOffset = round(intval($topOffset) / 2) - 1;
+					$topOffset = $topOffset + 20;
+
+					$textStyle = 'position:relative;display:inline-block;line-height:20px;height:20px;max-width:200px;padding:0 10px;';
+					$textStyle .= 'top:-'. $topOffset.'px;';
+					$textStyle .= 'color:'. $color. ';';
+					$textStyle .= 'font-size:28px;';
+					if(a($_args, 'background:style'))
+					{
+						$textStyle .= a($_args, 'background:style');
+					}
+
+					$html .= '<span';
+					$html .= ' style="'. $textStyle. '"';
+					$html .= '>';
+					{
+						$html .= $text;
+					}
+					$html .= '</span>';
+				}
 			}
 			$html .= "</div>";
 		}
