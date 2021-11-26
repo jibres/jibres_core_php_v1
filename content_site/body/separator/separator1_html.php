7781243db7d5a910fb3dfc16d620c7f97e5c7f60
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-26 14:05:05 +0330

    add style for hr

--- content_site/body/separator/separator1_html.php
+++ content_site/body/separator/separator1_html.php
@@ -8,15 +8,24 @@ class separator1_html
 	{
 		$html = \content_site\assemble\wrench\section::element_start($_args);
 		{
-			$hrStyle = 'border:none'
-			$html .= '<hr';
-			$html .= ' style="'. $hrStyle. '"';
-			$html .= '>';
+			$html .= \content_site\assemble\wrench\section::container($_args);
+			{
+				$width = '5px';
+				$style = 'dashed';
+				$color = '#999';
+
+				$hrStyle = 'border:none;width:100%;';
+				$hrStyle .= 'border-bottom:'. $width. ' '. $style. ' '. $color. ';';
+
+				$html .= '<hr';
+				$html .= ' style="'. $hrStyle. '"';
+				$html .= '>';
+			}
+			$html .= "</div>";
 		}
 		$html .= \content_site\assemble\wrench\section::element_end($_args);
 
 		return $html;
 	}
-
 }
 ?>
