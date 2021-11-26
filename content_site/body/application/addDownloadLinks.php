9720ca64fa9d860f1e14a5d59f906ad2f5fcff28
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-26 18:51:30 +0330

    save

--- content_site/body/application/addDownloadLinks.php
+++ content_site/body/application/addDownloadLinks.php
@@ -16,69 +16,59 @@ class addDownloadLinks
     {
       // dl link of all type
       // google play
-      if($link_googleplay)
-      {
-        $html .= '<a target="_blank" rel="noopener" class="" href="'. $link_googleplay. '">';
-        $img_googleplay = \dash\url::cdn(). '/img/app/get/googleplay';
-        if(\dash\language::current() === 'fa')
-        {
-          $img_googleplay .= '-fa';
-        }
-        $img_googleplay .= '.png';
-
-        $html .= '<img src="'. $img_googleplay. '" alt="'. T_('Download from Google Play'). '" class="">';
-        $html .= '</a>';
-      }
-
-      // direct download
-      if($link_directdl or 1)
-      {
-        $html .= '<a target="_blank" rel="noopener" class="" href="'. $link_directdl. '">';
-        $img_directdl = \dash\url::cdn(). '/img/app/get/downloadapk';
-        if(\dash\language::current() === 'fa')
-        {
-          $img_directdl .= '-fa';
-        }
-        $img_directdl .= '.png';
-
-        $html .= '<img src="'. $img_directdl. '" alt="'. T_('Direct download'). '" class="">';
-        $html .= '</a>';
-      }
-
-      // mayket
-      if($link_myket or 1)
-      {
-        $html .= '<a target="_blank" rel="noopener" class="" href="'. $link_myket. '">';
-        $img_myket = \dash\url::cdn(). '/img/app/get/myket';
-        if(\dash\language::current() === 'fa')
-        {
-          $img_myket .= '-fa';
-        }
-        $img_myket .= '.png';
-
-        $html .= '<img src="'. $img_myket. '" alt="'. T_('Download from Myket'). '" class="">';
-        $html .= '</a>';
-      }
-
-      // cafebazaar
-      if($link_cafebazar or 1)
-      {
-        $html .= '<a target="_blank" rel="noopener" class="" href="'. $link_cafebazar. '">';
-        $img_cafebazar = \dash\url::cdn(). '/img/app/get/cafebazar';
-        if(\dash\language::current() === 'fa')
-        {
-          $img_cafebazar .= '-fa';
-        }
-        $img_cafebazar .= '.png';
-
-        $html .= '<img src="'. $img_cafebazar. '" alt="'. T_('Download from CafeBazar'). '" class="">';
-        $html .= '</a>';
-      }
+      $html .= self::createDlLink('googleplay', a($_args, 'link_googleplay'));
+      $html .= self::createDlLink('downloadapk', a($_args, 'android_apk_link'));
+      $html .= self::createDlLink('myket', a($_args, 'link_myket'));
+      $html .= self::createDlLink('cafebazar', a($_args, 'link_cafebazar'));
     }
 
 		return $html;
 	}
 
 
+  private static function createDlLink($_type, $_link)
+  {
+    if(!$_link)
+    {
+      return null;
+    }
+
+    $myEl   = '<a target="_blank" rel="noopener" class="" href="'. $_link. '">';
+    $imgSrc = \dash\url::cdn(). '/img/app/get/'. $_type;
+    if(\dash\language::current() === 'fa')
+    {
+      $imgSrc .= '-fa';
+    }
+    $imgSrc .= '.png';
+
+    // set img alt
+    $imgAlt = T_("Download App");
+    switch ($_type)
+    {
+      case 'googleplay':
+        $imgAlt = T_('Download from Google Play');
+        break;
+
+      case 'myket':
+        $imgAlt = T_('Download from Myket');
+        break;
+
+      case 'cafebazar':
+        $imgAlt = T_('Download from CafeBazar');
+        break;
+
+      case 'downloadapk':
+        $imgAlt = T_('Direct download');
+        break;
+
+      default:
+        break;
+    }
+
+    $myEl .= '<img src="'. $imgSrc. '" alt="'. $imgAlt. '" class="">';
+    $myEl .= '</a>';
+
+    return $myEl;
+  }
 }
 ?>
