c2e0f7c35521ac9c8768b91b8d47dd4cccdbac8e
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-27 10:57:29 +0330

    try to style twitter

--- content_site/body/twitter/twitter1_html.php
+++ content_site/body/twitter/twitter1_html.php
@@ -6,6 +6,15 @@ class twitter1_html
 {
 	public static function html($_args, $_blogList)
 	{
+    // declare vaeiables
+    $borderRadius   = a($_args, 'radius:class');
+    $darkMode       = true;
+    $theme          = 1;
+    // get theme colors
+    $sectionBgStyle = self::themeColor($theme, 'section');
+    $cardBgStyle    = self::themeColor($theme, 'card');
+
+
 
     $html = \content_site\assemble\wrench\section::element_start($_args);
     {
@@ -13,17 +22,24 @@ class twitter1_html
       {
         $html .= \content_site\assemble\wrench\heading::simple1($_args);
 
-        $html .= \content_site\assemble\wrench\section::grid_12($_args);
+        $boxClass     = 'max-w-xl mx-auto w-full transition relative z-0 overflow-hidden bg-red-200 py-4 h-40 '. $borderRadius;
+
+
+        $html .= '<div class="'. $boxClass. '">';
         {
-          $optCard = [
-            'grid' => true,
-          ];
-          $html .= \content_site\assemble\element\card::html($_args, $_blogList, $optCard);
+          // background
+          $cardOverlayStyle = 'z-index:-1;background:linear-gradient(-50deg,rgba(255,255,255,.5),rgba(255,255,255,.95) 80%);';
+          if($darkMode)
+          {
+            $cardOverlayStyle = 'z-index:-1;background:linear-gradient(-50deg,rgba(0,0,0,.94),rgba(0,0,0,.58) 100%);';
+          }
+          $cardBgClass = 'absolute inset-0 z-0 '. $borderRadius;
+          $html .= '<div style="'. $cardOverlayStyle. '" class="'. $cardBgClass. '"></div>';
+
+          $html .= "hi";
+
         }
-        $html .= '</div>';
-
-        $html .= \content_site\body\blog\share::btn_viewall($_args);
-
+        $html .= "</div>";
       }
       $html .= "</div>";
     }
@@ -31,7 +47,38 @@ class twitter1_html
 
 		return $html;
 	}
-
-
 }
+
+
+private static function themeColor($_theme, $request = null)
+{
+  $bg
+  [
+    'section' => '',
+    'card'    => '',
+  ];
+
+  switch ($_theme)
+  {
+    case 1:
+      $bg['section'] = '';
+      $bg['card']    = '';
+      break;
+
+    default:
+      break;
+  }
+
+
+  if($request === 'section')
+  {
+    return $bg['section'];
+  }
+  if($request === 'card')
+  {
+    return $bg['card'];
+  }
+  return $bg;
+}
+
 ?>
