d57a408022195415a6594348ee6ecdd4b018f76f
Reza <rm.biqarar@gmail.com>
2021-11-29 15:19:57 +0330

    Set coverratio

--- content_site/options/coverratio/coverratio.php
+++ content_site/options/coverratio/coverratio.php
@@ -9,17 +9,26 @@ trait coverratio
 	{
 		$enum   = [];
 
-		$enum[] = ['key' => '3:1', 'title' => '3:1', 'class' => 'aspect-w-3 aspect-h-1'];
-		$enum[] = ['key' => '16:9', 'title' => '16:9', 'class' => 'aspect-w-16 aspect-h-9'];
-		$enum[] = ['key' => '1:1', 'title' => '1:1', 'class' => 'aspect-w-1 aspect-h-1'];
+
+		$enum[] = ['key' => '2.5:1', 	'title' => '2.5:1', 	'class' => ''];
+		$enum[] = ['key' => '4:3', 		'title' => '4:3', 		'class' => ''];
+		$enum[] = ['key' => '5:3', 		'title' => '5:3', 		'class' => ''];
+		$enum[] = ['key' => '16:10', 	'title' => '16:10', 	'class' => ''];
+		$enum[] = ['key' => '19:10', 	'title' => '19:10', 	'class' => ''];
+		$enum[] = ['key' => '32:9', 	'title' => '32:9', 		'class' => ''];
+		$enum[] = ['key' => '64:27', 	'title' => '64:27', 	'class' => ''];
+
+		$enum[] = ['key' => '3:1', 		'title' => '3:1', 		'class' => 'aspect-w-3 aspect-h-1'];
+		$enum[] = ['key' => '16:9', 	'title' => '16:9', 		'class' => 'aspect-w-16 aspect-h-9'];
+		$enum[] = ['key' => '1:1', 		'title' => '1:1', 		'class' => 'aspect-w-1 aspect-h-1'];
 
 		// vertical
-		$enum[] = ['key' => '3:4', 'title' => '3:4', 'class' => 'aspect-w-3 aspect-h-4'];
+		$enum[] = ['key' => '3:4', 		'title' => '3:4', 		'class' => 'aspect-w-3 aspect-h-4'];
 
 		if(self::have_free_ratio())
 		{
 			// free
-			$enum[] = ['key' => 'free',  'title' => 'Free' ,'class' => ''];
+			$enum[] = ['key' => 'free',  'title' => 'Free' ,		'class' => ''];
 		}
 
 		return $enum;
@@ -78,6 +87,7 @@ trait coverratio
 	public static function admin_html()
 	{
 
+		$html = '';
 		$default = \content_site\section\view::get_current_index_detail(self::db_key());
 
 		if(!$default)
@@ -85,38 +95,18 @@ trait coverratio
 			$default = self::default();
 		}
 
-
 		$title = T_("Featured image ratio");
 
-		$this_range = array_column(self::enum(), 'key');
+		$name       = 'opt_'. \content_site\utility::className(__CLASS__);
 
-
-
-		$html = '';
 		$html .= \content_site\options\generate::form();
 		{
-			$html .= "<label>$title</label>";
-
-			$name       = 'opt_'. \content_site\utility::className(__CLASS__);
-
-			$radio_html = '';
-
-			foreach (self::enum() as $key => $value)
-			{
-				$selected = false;
-
-				if($default === $value['key'])
-				{
-					$selected = true;
-				}
-
-				$radio_html .= \content_site\options\generate::radio_line_itemText($name, $value['key'], $value['title'], $selected);
-			}
-			$html .= \content_site\options\generate::radio_line_add_ul($name, $radio_html);
+			$html .= \content_site\options\generate::select(__CLASS__, self::enum(), $default, $title);
 		}
 		$html .= \content_site\options\generate::_form();
 
 		return $html;
+
 	}
 
 
