c63850b345c8cf094e59df67566680fc677cfd4f
Javad Adib <Mr.Javad.Adib@gmail.com>
2021-11-29 15:36:30 +0330

    set class for cover ratio

--- content_site/options/coverratio/coverratio.php
+++ content_site/options/coverratio/coverratio.php
@@ -9,27 +9,33 @@ trait coverratio
 	{
 		$enum   = [];
 
-
-		$enum[] = ['key' => '2.5:1', 	'title' => '2.5:1', 	'class' => ''];
-		$enum[] = ['key' => '4:3', 		'title' => '4:3', 		'class' => ''];
-		$enum[] = ['key' => '5:3', 		'title' => '5:3', 		'class' => ''];
-		$enum[] = ['key' => '16:10', 	'title' => '16:10', 	'class' => ''];
-		$enum[] = ['key' => '19:10', 	'title' => '19:10', 	'class' => ''];
-		$enum[] = ['key' => '32:9', 	'title' => '32:9', 		'class' => ''];
-		$enum[] = ['key' => '64:27', 	'title' => '64:27', 	'class' => ''];
-
-		$enum[] = ['key' => '3:1', 		'title' => '3:1', 		'class' => 'aspect-w-3 aspect-h-1'];
-		$enum[] = ['key' => '16:9', 	'title' => '16:9', 		'class' => 'aspect-w-16 aspect-h-9'];
-		$enum[] = ['key' => '1:1', 		'title' => '1:1', 		'class' => 'aspect-w-1 aspect-h-1'];
-
-		// vertical
-		$enum[] = ['key' => '3:4', 		'title' => '3:4', 		'class' => 'aspect-w-3 aspect-h-4'];
-
 		if(self::have_free_ratio())
 		{
 			// free
 			$enum[] = ['key' => 'free',  'title' => 'Free' ,		'class' => ''];
 		}
+
+		$enum[] = ['key' => '1:1', 		'title' => '1:1', 		'class' => 'aspect-w-1 aspect-h-1'];
+		$enum[] = ['key' => '4:3', 		'title' => '4:3', 		'class' => 'aspect-w-4 aspect-h-3'];
+		$enum[] = ['key' => '16:9', 	'title' => '16:9', 		'class' => 'aspect-w-16 aspect-h-9'];
+		$enum[] = ['key' => '32:9', 	'title' => '32:9', 		'class' => 'aspect-w-32 aspect-h-9'];
+		$enum[] = ['key' => '64:9', 	'title' => '64:9', 		'class' => 'aspect-w-64 aspect-h-9'];
+
+		// less common
+		$enum[] = ['key' => '3:1', 		'title' => '3:1', 		'class' => 'aspect-w-3 aspect-h-1'];
+		$enum[] = ['key' => '4:1', 		'title' => '4:1', 		'class' => 'aspect-w-4 aspect-h-1'];
+		$enum[] = ['key' => '5:3', 		'title' => '5:3', 		'class' => 'aspect-w-5 aspect-h-3'];
+		$enum[] = ['key' => '16:10', 	'title' => '16:10', 	'class' => 'aspect-w-16 aspect-h-10'];
+		$enum[] = ['key' => '19:10', 	'title' => '19:10', 	'class' => 'aspect-w-19 aspect-h-10'];
+		$enum[] = ['key' => '64:27', 	'title' => '64:27', 	'class' => 'aspect-w-64 aspect-h-27'];
+
+
+		// vertical
+		$enum[] = ['key' => '9:16', 	'title' => '9:16',	  'class' => 'aspect-w-9 aspect-h-16'];
+		$enum[] = ['key' => '2:3', 		'title' => '2:3', 		'class' => 'aspect-w-2 aspect-h-3'];
+		$enum[] = ['key' => '3:4', 		'title' => '3:4', 		'class' => 'aspect-w-3 aspect-h-4'];
+		$enum[] = ['key' => '4:5', 		'title' => '4:5', 		'class' => 'aspect-w-4 aspect-h-5'];
+
 
 		return $enum;
 	}
