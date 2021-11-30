be1e9e654dccbc2b040a7bf6fc214d280e989901
Reza <rm.biqarar@gmail.com>
2021-11-30 11:09:32 +0330

    Add subtype

---
+++ includes/database/upgrade/store/v.3.5.1_post_subtype.sql
@@ -0,0 +1,1 @@
+ALTER TABLE jibres_XXXXXXX.posts CHANGE `subtype` `subtype` ENUM('standard','gallery','video','audio','helpcenter', 'link', 'quote', 'status') CHARACTER SET utf8mb4  NULL DEFAULT 'standard';
