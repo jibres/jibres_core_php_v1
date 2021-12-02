77b5d6c109d86716d2f4e51991d9b6293548fd02
Reza <rm.biqarar@gmail.com>
2021-12-02 16:07:36 +0330

    Add color and preview  field to file

---
+++ includes/database/upgrade/jibres/v.1.9.0_files_color.sql
@@ -0,0 +1,2 @@
+ALTER TABLE jibres.files ADD `color` varchar(50) CHARACTER SET utf8mb4  NULL DEFAULT NULL AFTER `ratio`;
+ALTER TABLE jibres.files ADD `preview` text CHARACTER SET utf8mb4  NULL DEFAULT NULL;
---
+++ includes/database/upgrade/store/v.3.5.3_files_color.sql
@@ -0,0 +1,2 @@
+ALTER TABLE jibres_XXXXXXX.files ADD `color` varchar(50) CHARACTER SET utf8mb4  NULL DEFAULT NULL AFTER `ratio`;
+ALTER TABLE jibres_XXXXXXX.files ADD `preview` text CHARACTER SET utf8mb4  NULL DEFAULT NULL;
