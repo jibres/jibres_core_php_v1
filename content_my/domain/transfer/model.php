7e2cb22e1f6af4b47b19fd39fbc8df2cf920fd12
Reza <rm.biqarar@gmail.com>
2021-12-03 11:37:59 +0330

    Bug fix transfer domain

--- content_my/domain/transfer/model.php
+++ content_my/domain/transfer/model.php
@@ -18,23 +18,26 @@ class model
 
 			// .com request
 			'fullname'     => \dash\request::post('fullname'),
-			'org'          => \dash\request::post('org'),
-			'nationalcode' => \dash\request::post('nationalcode'),
-			'country'      => \dash\request::post('country'),
-			'province'     => \dash\request::post('province'),
-			'city'         => \dash\request::post('city'),
-			'address'      => \dash\request::post('address'),
-			'postcode'     => \dash\request::post('postcode'),
-
-			'phonecc'      => \dash\request::post('phonecc'),
-			'phone'        => \dash\request::post('phone'),
-			'faxcc'        => \dash\request::post('faxcc'),
-			'fax'          => \dash\request::post('fax'),
-
 			'email'        => \dash\request::post('email'),
+
+			// 'org'          => \dash\request::post('org'),
+			// 'nationalcode' => \dash\request::post('nationalcode'),
+			// 'country'      => \dash\request::post('country'),
+			// 'province'     => \dash\request::post('province'),
+			// 'city'         => \dash\request::post('city'),
+			// 'address'      => \dash\request::post('address'),
+			// 'postcode'     => \dash\request::post('postcode'),
+
+			// 'phonecc'      => \dash\request::post('phonecc'),
+			// 'phone'        => \dash\request::post('phone'),
+			// 'faxcc'        => \dash\request::post('faxcc'),
+			// 'fax'          => \dash\request::post('fax'),
+
 			// 'whoistype'    => \dash\request::post('whoistype'),
 
 		];
+
+		$post = array_merge(\content_my\domain\whoisdetail\model::whoisdetail_person(), $post);
 
 		$result = \lib\app\domains\transfer::transfer($post);
 
