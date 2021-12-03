36baf203bbbfa6948110a0261b643c09d327094f
Reza <rm.biqarar@gmail.com>
2021-12-03 12:14:59 +0330

    Bug fix design sort footer

--- content_a/form/sorting/display.php
+++ content_a/form/sorting/display.php
@@ -14,13 +14,13 @@ if(!is_array($dataTable))
 }
 ?>
 
-<div class="msg minimal info2 font-bold font-14"><?php echo T_("Drag the item and move it where you want to change the question sort. Items is sorted from left to right") ?></div>
+<div class="alert2-info font-bold font-14"><?php echo T_("Drag the item and move it where you want to change the question sort. Items is sorted from left to right") ?></div>
 
 <form method="post" data-sortable data-willy class="ltr row">
 <?php foreach ($dataTable as $key => $value) {?>
  <div class="c-xs-6 c-sm-6 c-md-4 c-xl-3 c-xxl-2">
  	<input type="hidden" name="sort[]" value="<?php echo a($value, 'id'); ?>">
-  <div class="sortItem sortHandle" data-handle><?php echo \dash\fit::number($key+1). '. '. a($value, 'title'); ?></div>
+  <div class="m-1 relative bg-gray-200 border-gray-900 h-28 rounded-lg text-sm leading-8	p-1 overflow-ellipsis overflow-hidden	cursor-move	transition sortHandle" data-handle><?php echo \dash\fit::number($key+1). '. '. a($value, 'title'); ?></div>
  </div>
 <?php } //endfor ?>
 </form>
