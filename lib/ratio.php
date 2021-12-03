37b08c10446b14fd1e467fdfeb5d61fe8e9ff958
Reza <rm.biqarar@gmail.com>
2021-12-03 12:01:44 +0330

    Update to php8.1

--- lib/ratio.php
+++ lib/ratio.php
@@ -97,7 +97,7 @@ class ratio
 			$ratio = $_data;
 		}
 
-		if(strpos($ratio, ':') === false)
+		if(is_null($ratio) || strpos($ratio, ':') === false)
 		{
 			$ratio = self::default_ratio('ratio', $_type);
 		}
