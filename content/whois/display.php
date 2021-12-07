f439eb832a53ec323532f87873be2892f229f821
Reza <rm.biqarar@gmail.com>
2021-12-07 12:46:45 +0330

    Html bug fix

--- content/whois/display.php
+++ content/whois/display.php
@@ -1,19 +1,21 @@
 <div class="jibresBanner">
- <div class="avand impact">
-<div class="f justify-center">
-	<div class="c6 s12">
-		<form method="get" autocomplete="off" action="<?php echo \dash\url::this(); ?>"  >
-			<div class="input ltr">
-				<input type="text" name="domain" placeholder='<?php echo T_("Domain"); ?>' value="<?php echo \dash\data::myDomain(); ?>">
-				<button class="btn addon success"><?php echo T_("Check domain"); ?></button>
+	<div class="avand impact">
+		<div class="f justify-center">
+			<div class="c6 s12">
+				<form method="get" autocomplete="off" action="<?php echo \dash\url::this(); ?>"  >
+					<div class="input ltr">
+						<input type="text" name="domain" placeholder='<?php echo T_("Domain"); ?>' value="<?php echo \dash\data::myDomain(); ?>">
+						<button class="btn addon success"><?php echo T_("Check domain"); ?></button>
+					</div>
+				</form>
+				<?php
+				if(\dash\data::domainError())
+				{
+					echo '<div class="alert-danger mT20">'. \dash\data::domainError().'</div>';
+				}
+				?>
 			</div>
-		</form>
-		<?php
-		if(\dash\data::domainError())
-		{
-			echo '<div class="alert-danger mT20">'. \dash\data::domainError().'</div>';
-		}
-		?>
+		</div>
+	<?php require_once(root. 'content/whois/whoisDisplay.php'); ?>
 	</div>
 </div>
-<?php require_once(root. 'content/whois/whoisDisplay.php'); ?>
