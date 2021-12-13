c853ebc0e6519f254c4dccd6306c17cc1ee064f2
Reza <rm.biqarar@gmail.com>
2021-12-13 11:10:01 +0330

    Add ready pdo query args

--- dash/pdo/query_template.php
+++ dash/pdo/query_template.php
@@ -113,5 +113,86 @@ class query_template
 
 		return $result;
 	}
+
+
+
+	public static function ready_pdo_query_args($_and, $_or, $_order_sort = null, $_meta = [])
+	{
+		$where = null;
+		$q     = [];
+
+
+		if($_and)
+		{
+			$_and = implode(' AND ', $_and);
+			$q[] = "$_and";
+
+		}
+
+		if($_or)
+		{
+			$_or = implode(' OR ', $_or);
+			$q[] = "($_or)";
+		}
+
+		if($q)
+		{
+			$where = 'WHERE '. implode(" AND ", $q);
+		}
+
+		$order = null;
+		if($_order_sort && is_string($_order_sort))
+		{
+			$order = $_order_sort;
+		}
+
+		$pagination = null;
+		if(array_key_exists('pagination', $_meta))
+		{
+			$pagination = $_meta['pagination'];
+		}
+
+		$limit = null;
+		if(array_key_exists('limit', $_meta))
+		{
+			$limit = $_meta['limit'];
+		}
+
+		$start_limit = null;
+		if(array_key_exists('start_limit', $_meta))
+		{
+			$start_limit = $_meta['start_limit'];
+		}
+
+		if(isset($_meta['join']) && is_array($_meta['join']) && $_meta['join'])
+		{
+			$join = implode(' ', $_meta['join']);
+		}
+		else
+		{
+			$join = null;
+		}
+
+		$fields = '*';
+		if(isset($_meta['fields']))
+		{
+			$fields = $_meta['fields'];
+		}
+
+		$result =
+		[
+			'where'       => $where,
+			'order'       => $order,
+			'pagination'  => $pagination,
+			'limit'       => $limit,
+			'start_limit' => $start_limit,
+			'join'        => $join,
+			'fields'      => $fields,
+			'limit_string' => "LIMIT $limit",
+		];
+
+		return $result;
+	}
+
 }
 ?>
