3c0e4bd550642ae6251c4880d413bd35e9e2a165
Reza <rm.biqarar@gmail.com>
2021-12-16 09:55:47 +0330

    Add lib vat

---
+++ lib/vat.php
@@ -0,0 +1,23 @@
+<?php
+namespace lib;
+
+class vat
+{
+
+	/**
+	 * Get vat percent
+	 *
+	 * @return     int   ( description_of_the_return_value )
+	 */
+	public static function percent()
+	{
+		/**
+			TODO:
+			- need check Country and setting to return this number
+		 */
+
+		return 9;
+	}
+
+}
+?>
