57b0fab88c58358619836a2cdbe5359b96c9a0a7
Reza <rm.biqarar@gmail.com>
2021-12-18 18:18:32 +0330

    Add template binded query mysql

---
+++ dash/db/mysql/prepare_query.php
@@ -0,0 +1,158 @@
+<?php
+namespace dash\db\mysql;
+
+
+class prepare_query
+{
+
+	public static function ready_for_multi_insert($_args, $_raw = false)
+	{
+		if(!is_array($_args))
+		{
+			return false;
+		}
+
+		// marge all input array to creat list of field to be insert
+		$fields = [];
+		foreach ($_args as $key => $value)
+		{
+			$fields = array_merge($fields, $value);
+		}
+
+		$fields = array_keys($fields);
+
+		// creat multi insert query : INSERT INTO TABLE (FIELDS) VLUES (values), (values), ...
+		$values         = [];
+		$param          = [];
+		$value_position = [];
+
+		foreach ($_args	 as $key => $value)
+		{
+			$temp = [];
+
+			foreach ($fields as $field_name)
+			{
+				$myKey = ':k_'. $key. '_';
+				$myKey .= $field_name;
+				$temp[] = $myKey;
+
+
+				$values[$myKey] = null;
+				if(array_key_exists($field_name, $value))
+				{
+					$values[$myKey] = $value[$field_name];
+				}
+
+				$param[$myKey] = $values[$myKey];
+
+			}
+
+			$value_position[] = '('. implode(',', $temp). ')';
+
+		}
+
+		$fields = '`'.  implode("`,`", $fields). '`';
+
+		$values = implode(",", $value_position);
+
+		$temp_query = "($fields) VALUES $values ";
+
+		return
+		[
+			'fields' => $fields,
+			'param'  => $param,
+			'query'  => $temp_query,
+		];
+	}
+
+
+	/**
+	 * Ready args for search
+	 *
+	 * @param      <type>  $_and         And
+	 * @param      <type>  $_or          { parameter_description }
+	 * @param      <type>  $_order_sort  The order sort
+	 * @param      array   $_meta        The meta
+	 *
+	 * @return     array   ( description_of_the_return_value )
+	 */
+	public static function ready_pdo_query_args($_and, $_or, $_order_sort = null, $_meta = [])
+	{
+		$where = null;
+		$q     = [];
+
+
+		if($_and)
+		{
+			$_and = implode(' AND ', $_and);
+			$q[] = "$_and";
+
+		}
+
+		if($_or)
+		{
+			$_or = implode(' OR ', $_or);
+			$q[] = "($_or)";
+		}
+
+		if($q)
+		{
+			$where = 'WHERE '. implode(" AND ", $q);
+		}
+
+		$order = null;
+		if($_order_sort && is_string($_order_sort))
+		{
+			$order = $_order_sort;
+		}
+
+		$pagination = null;
+		if(array_key_exists('pagination', $_meta))
+		{
+			$pagination = $_meta['pagination'];
+		}
+
+		$limit = null;
+		if(array_key_exists('limit', $_meta))
+		{
+			$limit = $_meta['limit'];
+		}
+
+		$start_limit = null;
+		if(array_key_exists('start_limit', $_meta))
+		{
+			$start_limit = $_meta['start_limit'];
+		}
+
+		if(isset($_meta['join']) && is_array($_meta['join']) && $_meta['join'])
+		{
+			$join = implode(' ', $_meta['join']);
+		}
+		else
+		{
+			$join = null;
+		}
+
+		$fields = '*';
+		if(isset($_meta['fields']))
+		{
+			$fields = $_meta['fields'];
+		}
+
+		$result =
+		[
+			'where'       => $where,
+			'order'       => $order,
+			'pagination'  => $pagination,
+			'limit'       => $limit,
+			'start_limit' => $start_limit,
+			'join'        => $join,
+			'fields'      => $fields,
+			'limit_string' => "LIMIT $limit",
+		];
+
+		return $result;
+	}
+
+}
+?>
