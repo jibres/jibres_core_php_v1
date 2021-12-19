e6f05b2529377eca02867cc333020bab984f6a91
Reza <rm.biqarar@gmail.com>
2021-12-19 10:59:24 +0330

    Get customer debt until order

--- content_a/order/detail/view.php
+++ content_a/order/detail/view.php
@@ -8,6 +8,13 @@ class view
 	{
 		\content_a\order\view::master_order_view();
 
+		$orderDetail = \dash\data::orderDetail();
+		if(($customer_id = a($orderDetail, 'factor', 'customer')) && \dash\url::isLocal())
+		{
+			$customer_debt = \lib\app\order\get::customer_debt($orderDetail['factor']['id'], $customer_id);
+			\dash\data::customerDebt($customer_debt);
+        }
+
 	}
 }
 ?>
