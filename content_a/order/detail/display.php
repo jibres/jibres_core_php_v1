aba6ae31031a9bfa40195ce7acb061c23ff69d08
Reza <rm.biqarar@gmail.com>
2021-12-19 11:00:04 +0330

    Show customer debt in order detail

--- content_a/order/detail/display.php
+++ content_a/order/detail/display.php
@@ -1,5 +1,4 @@
 <?php $orderDetail = \dash\data::orderDetail(); ?>
-
 <div class="row">
   <div class="c-xs-12 c-sm-12 c-md-4">
     <?php require_once(root. '/content_a/order/links.php'); ?>
@@ -13,24 +12,24 @@
           <table class="tbl1 v4 responsive">
             <tbody>
               <?php if (a($orderDetail, 'factor', 'customer')) {?>
-              <tr>
-                <td class="collapsing fc-mute"><?php echo T_("Customer") ?></td>
-                <td>
+                <tr>
+                  <td class="collapsing fc-mute"><?php echo T_("Customer") ?></td>
+                  <td>
                     <?php echo a($orderDetail, 'factor', 'customer_detail', 'displayname'); ?>
                     <span class="ltr compact"><?php echo \dash\fit::mobile(a($orderDetail, 'factor', 'customer_detail', 'mobile')); ?></span>
-                </td>
-              </tr>
+                  </td>
+                </tr>
               <?php } // endif ?>
               <tr>
                 <td class="collapsing fc-mute"><?php echo T_("Order Type") ?></td>
                 <td><?php echo a($orderDetail, 'factor', 't_type'); ?></td>
               </tr>
               <?php if(a($orderDetail, 'factor', 'status') !== 'draft') {?>
-              <tr>
-                <td class="collapsing fc-mute"><?php echo T_("Status") ?></td>
-                <td><?php echo a($orderDetail, 'factor', 't_status'); ?></td>
-              </tr>
-            <?php } //endif ?>
+                <tr>
+                  <td class="collapsing fc-mute"><?php echo T_("Status") ?></td>
+                  <td><?php echo a($orderDetail, 'factor', 't_status'); ?></td>
+                </tr>
+              <?php } //endif ?>
               <tr>
                 <td class="collapsing fc-mute"><?php echo T_("Order date") ?></td>
                 <td>
@@ -43,46 +42,46 @@
 
               <?php $address = a(\dash\data::orderDetail(), 'address'); ?>
               <?php if($address) {?>
-              <tr>
-                <td class="collapsing fc-mute"><?php echo T_("Address") ?></td>
-                <td>
+                <tr>
+                  <td class="collapsing fc-mute"><?php echo T_("Address") ?></td>
+                  <td>
 
-                  <address>
-                     <?php if(a($address, 'name')) {?>
-                      <div class=""><?php echo \dash\fit::text(a($address, 'name')); ?></div>
-                    <?php } //endif ?>
+                    <address>
+                      <?php if(a($address, 'name')) {?>
+                        <div class=""><?php echo \dash\fit::text(a($address, 'name')); ?></div>
+                      <?php } //endif ?>
 
-                    <?php if(a($address, 'location_string')) {?>
-                      <div class=""><b><?php echo \dash\fit::text(a($address, 'location_string')); ?></b></div>
-                    <?php } //endif ?>
+                      <?php if(a($address, 'location_string')) {?>
+                        <div class=""><b><?php echo \dash\fit::text(a($address, 'location_string')); ?></b></div>
+                      <?php } //endif ?>
 
-                    <div class="addr"><?php echo a($address, 'address'); ?></div>
-                    <?php if(a($address, 'mobile')) {?>
-                      <div class="mobile"><?php echo T_("Mobile") ?> <b class="ltr compact"><?php echo \dash\fit::mobile(a($address, 'mobile')); ?></b></div>
-                    <?php } //endif ?>
-                    <?php if(a($address, 'phone')) {?>
-                      <div class="phone"><?php echo T_("Phone") ?> <b class="ltr compact"><?php echo \dash\fit::text(a($address, 'phone')); ?></b></div>
-                    <?php } //endif ?>
-                    <?php if(a($address, 'postcode')) {?>
-                      <div class="postalcode"><?php echo T_("Postalcode") ?> <b class="ltr compact"><?php echo \dash\fit::text(a($address, 'postcode')); ?></b></div>
-                    <?php } //endif ?>
-                  </address>
-                </td>
-              </tr>
-            <?php } //endif ?>
-            <?php if(a($orderDetail, 'factor', 'desc')) {?>
-              <tr class="warning">
-                <td class="collapsing"><?php echo T_("Description") ?></td>
-                <td><?php echo a($orderDetail, 'factor', 'desc'); ?></td>
-              </tr>
-            <?php } //endif ?>
+                      <div class="addr"><?php echo a($address, 'address'); ?></div>
+                      <?php if(a($address, 'mobile')) {?>
+                        <div class="mobile"><?php echo T_("Mobile") ?> <b class="ltr compact"><?php echo \dash\fit::mobile(a($address, 'mobile')); ?></b></div>
+                      <?php } //endif ?>
+                      <?php if(a($address, 'phone')) {?>
+                        <div class="phone"><?php echo T_("Phone") ?> <b class="ltr compact"><?php echo \dash\fit::text(a($address, 'phone')); ?></b></div>
+                      <?php } //endif ?>
+                      <?php if(a($address, 'postcode')) {?>
+                        <div class="postalcode"><?php echo T_("Postalcode") ?> <b class="ltr compact"><?php echo \dash\fit::text(a($address, 'postcode')); ?></b></div>
+                      <?php } //endif ?>
+                    </address>
+                  </td>
+                </tr>
+              <?php } //endif ?>
+              <?php if(a($orderDetail, 'factor', 'desc')) {?>
+                <tr class="warning">
+                  <td class="collapsing"><?php echo T_("Description") ?></td>
+                  <td><?php echo a($orderDetail, 'factor', 'desc'); ?></td>
+                </tr>
+              <?php } //endif ?>
 
-            <?php if(a($orderDetail, 'factor', 'paystatus')) {?>
-              <tr>
-                <td class="collapsing"><?php echo T_("Payment status") ?></td>
-                <td><?php echo a($orderDetail, 'factor', 't_paystatus'); ?></td>
-              </tr>
-            <?php } //endif ?>
+              <?php if(a($orderDetail, 'factor', 'paystatus')) {?>
+                <tr>
+                  <td class="collapsing"><?php echo T_("Payment status") ?></td>
+                  <td><?php echo a($orderDetail, 'factor', 't_paystatus'); ?></td>
+                </tr>
+              <?php } //endif ?>
 
             </tbody>
           </table>
@@ -90,6 +89,49 @@
         <?php require_once(root. '/content_a/order/summary.php'); ?>
       </div>
     </div>
+        <?php if($customerDebt = \dash\data::customerDebt()) {?>
+        <div class="box">
+          <div class="pad">
+            <div class="tblBox">
+              <table class="tbl1 v4 responsive">
+                <tbody>
+
+                    <tr>
+                      <td class="collapsing fc-mute"><?php echo T_("Customer") ?></td>
+                      <td>
+                        <?php echo a($orderDetail, 'factor', 'customer_detail', 'displayname'); ?>
+                        <span class="ltr compact"><?php echo \dash\fit::mobile(a($orderDetail, 'factor', 'customer_detail', 'mobile')); ?></span>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="collapsing fc-mute"><?php echo T_("Debt unti this order") ?></td>
+                      <td>
+                        <?php echo \dash\fit::number(a($customerDebt, 'debt_until_order')); ?>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="collapsing fc-mute"><?php echo T_("Debt whit this order") ?></td>
+                      <td>
+                        <?php echo \dash\fit::number(a($customerDebt, 'debt_with_order')); ?>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="collapsing fc-mute"><?php echo T_("Current debt") ?></td>
+                      <td>
+                        <?php echo \dash\fit::number(a($customerDebt, 'current_debt')); ?>
+                      </td>
+                    </tr>
+
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      <?php } //endif ?>
 
     <?php require_once(root. 'content_a/order/productList.php') ?>
 
