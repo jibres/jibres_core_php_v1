4e0534cebeb0cbeed8471e5217434b390a3b9d89
Reza <rm.biqarar@gmail.com>
2021-12-20 12:51:54 +0330

    Save bulk

--- content_a/products/bulk/model.php
+++ content_a/products/bulk/model.php
@@ -6,7 +6,28 @@ class model
 {
 	public static function post()
 	{
+		$id   = \dash\request::post('id');
 
+		$post = [];
+
+		foreach (\dash\request::post() as $key => $value)
+		{
+			if(substr($key, 0, 1) === '_')
+			{
+				$post[substr($key, 1)] = $value;
+			}
+		}
+
+		if(!empty($post) && \dash\validate::id($id))
+		{
+			\lib\app\product\edit::edit($post, $id);
+
+			if(\dash\engine\process::status())
+			{
+				\dash\notif::clean();
+				\dash\notif::complete();
+			}
+		}
 
 	}
 }
