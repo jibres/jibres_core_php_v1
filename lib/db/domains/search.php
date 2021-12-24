637b8b7b4647a0ed43ee1e0322473b9df6137d32
Reza <rm.biqarar@gmail.com>
2021-12-24 15:10:18 +0330

    Remove ready to sql function

--- lib/db/domains/search.php
+++ lib/db/domains/search.php
@@ -5,55 +5,6 @@ namespace lib\db\domains;
 class search
 {
 
-	private static function ready_to_sql($_and, $_or, $_order_sort = null, $_meta = [])
-	{
-		$where = null;
-		$q     = [];
-
-		if($_and)
-		{
-			$_and = implode(' AND ', $_and);
-			$q[] = "$_and";
-
-		}
-
-		if($_or)
-		{
-			$_or = implode(' OR ', $_or);
-			$q[] = "($_or)";
-		}
-
-		if($q)
-		{
-			$where = 'WHERE '. implode(" AND ", $q);
-		}
-
-		$order = null;
-		if($_order_sort && is_string($_order_sort))
-		{
-			$order = $_order_sort;
-		}
-
-		$pagination = null;
-		if(array_key_exists('pagination', $_meta))
-		{
-			$pagination = $_meta['pagination'];
-		}
-
-		$limit = null;
-		if(array_key_exists('limit', $_meta))
-		{
-			$limit = $_meta['limit'];
-		}
-
-		return
-		[
-			'where'      => $where,
-			'order'      => $order,
-			'pagination' => $pagination,
-			'limit'      => $limit,
-		];
-	}
 
 	public static function list($_and, $_or, $_order_sort = null, $_meta = [])
 	{
