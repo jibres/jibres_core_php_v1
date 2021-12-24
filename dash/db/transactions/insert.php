2549a88b8253d8ce6546e340b63256a075eda46e
Reza <rm.biqarar@gmail.com>
2021-12-24 10:29:16 +0330

    Bug fix transaction

--- dash/db/transactions/insert.php
+++ dash/db/transactions/insert.php
@@ -7,6 +7,11 @@ class insert
 
 	public static function new_record($_args)
 	{
+		if(a($_args, 'status') === null)
+		{
+			$_args['status'] = 'enable';
+		}
+
 		return \dash\pdo\query_template::insert('transactions', $_args);
 	}
 }
