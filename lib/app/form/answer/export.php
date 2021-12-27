e44af34b58b8115126b1c6161f5807ec86026b5d
Reza <rm.biqarar@gmail.com>
2021-12-27 14:35:42 +0330

    Need to fix later

--- lib/app/form/answer/export.php
+++ lib/app/form/answer/export.php
@@ -312,10 +312,109 @@ class export
 		$sql = [];
 		foreach ($new_result as $key => $value)
 		{
-			$sql[] = "INSERT IGNORE INTO `$_table_name` SET ". \dash\db\config::make_set($value, ['type' => 'insert']);
+			$sql[] = "INSERT IGNORE INTO `$_table_name` SET ". self::make_set($value, ['type' => 'insert']);
 		}
 
 		return $sql;
 	}
+
+
+	/**
+	 * Makes a set.
+	 *
+	 * @param      <type>  $_args  The arguments
+	 */
+	public static function make_set($_args, $_options = [])
+	{
+		$default_options =
+		[
+			'type' => 'update',
+		];
+
+		if(!is_array($_options))
+		{
+			$_options = [];
+		}
+
+		$_options = array_merge($default_options, $_options);
+
+		$set = [];
+		foreach ($_args as $key => $value)
+		{
+			if(!is_string($key))
+			{
+				continue;
+			}
+
+			if($value === null)
+			{
+				if($_options['type'] === 'insert')
+				{
+					// continue;
+				}
+				else
+				{
+					$set[] = " `$key` = NULL ";
+				}
+			}
+			elseif(is_string($value) && (!isset($value) || $value == '' ))
+			{
+				if($_options['type'] === 'insert')
+				{
+					// continue;
+				}
+				else
+				{
+					$set[] = " `$key` = NULL ";
+				}
+			}
+			elseif(is_string($value))
+			{
+				$set[] = " `$key` = '$value' ";
+			}
+			elseif(is_numeric($value))
+			{
+				$set[] = " `$key` = $value ";
+			}
+			elseif(is_bool($value))
+			{
+				if($value)
+				{
+					$set[] = " `$key` = 1 ";
+				}
+				else
+				{
+					$set[] = " `$key` = NULL ";
+				}
+			}
+			else
+			{
+				$set[] = " `$key` = '$value' ";
+			}
+		}
+
+		if(!empty($set))
+		{
+			if($_options['type'] === 'update')
+			{
+				$set = implode(',', $set);
+			}
+			elseif($_options['type'] === 'insert')
+			{
+				$set = implode(',', $set);
+			}
+			else
+			{
+				$set = false;
+			}
+		}
+		else
+		{
+			$set = false;
+		}
+		return $set;
+	}
+
+
 }
 ?>
