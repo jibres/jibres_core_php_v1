f97d5288d7c2bd9479d8343bccbc13434b3e03d7
Reza <rm.biqarar@gmail.com>
2021-12-27 10:25:55 +0330

    Remove old db function

--- dash/db/config.php
+++ dash/db/config.php
@@ -244,7 +244,6 @@ class config
 		{
 			$default_options =
 			[
-				'master_join'       => null,
 				'order'             => null,
 				'table_name'        => null,
 				'db_name'           => true,
@@ -277,7 +276,7 @@ class config
 			$where = \dash\db\config::make_where($_where, $_options);
 			if($where)
 			{
-				$query = "SELECT * FROM $_table $_options[master_join] WHERE $where $_options[order] $limit";
+				$query = "SELECT * FROM $_table  WHERE $where $_options[order] $limit";
 				$result = \dash\pdo::get($query, [], null, $only_one_value, $_options['db_name']);
 				return $result;
 			}
