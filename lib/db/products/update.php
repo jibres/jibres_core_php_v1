74c9d621e05fdda8ff1e1d5df7ef7ac880b89fd6
Reza <rm.biqarar@gmail.com>
2022-01-01 14:09:10 +0330

    Convert db product update to binded query

--- lib/db/products/update.php
+++ lib/db/products/update.php
@@ -1,7 +1,13 @@
 <?php
 namespace lib\db\products;
 
-
+/**
+ * This class describes an update.
+ *
+ * @author Reza
+ * All function of this class converted to PDO binded query
+ * @date 2022-01-01 13:49:09
+ */
 class update
 {
 
@@ -43,8 +49,15 @@ class update
 	// UPDATE VARIANTS FIELD
 	public static function variants($_variants, $_id)
 	{
-		$query  = "UPDATE products SET products.variants = '$_variants' WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.variants = :variants WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':variants' => $_variants,
+			':id'       => $_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -67,8 +80,14 @@ class update
 
 	public static function variant_child($_id)
 	{
-		$query  = "UPDATE products SET products.variant_child = 1 WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.variant_child = 1 WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id'       => $_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -78,14 +97,23 @@ class update
 		"
 			UPDATE products
 			SET products.status = 'deleted'
-			WHERE products.parent = $_parent AND
+			WHERE products.parent = :parent AND
 			(
-				(products.optionname1 = '$_optionname' AND products.optionvalue1 = '$_optionvalue') OR
-				(products.optionname2 = '$_optionname' AND products.optionvalue2 = '$_optionvalue') OR
-				(products.optionname3 = '$_optionname' AND products.optionvalue3 = '$_optionvalue')
+				(products.optionname1 = :optionname AND products.optionvalue1 = :optionvalue) OR
+				(products.optionname2 = :optionname AND products.optionvalue2 = :optionvalue) OR
+				(products.optionname3 = :optionname AND products.optionvalue3 = :optionvalue)
 			)
 		";
-		$result = \dash\pdo::query($query, []);
+
+
+		$param =
+		[
+			':parent'      => $_parent,
+			':optionname'  => $_optionname,
+			':optionvalue' => $_optionvalue,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -93,11 +121,20 @@ class update
 	{
 		$query  =
 		"
-			UPDATE products SET products.optionname1 = NULL, products.optionvalue1 = NULL WHERE products.parent = $_parent  AND products.optionname1 = '$_optionname' AND products.optionvalue1 = '$_optionvalue' ;
-			UPDATE products SET products.optionname2 = NULL, products.optionvalue2 = NULL WHERE products.parent = $_parent  AND products.optionname2 = '$_optionname' AND products.optionvalue2 = '$_optionvalue' ;
-			UPDATE products SET products.optionname3 = NULL, products.optionvalue3 = NULL WHERE products.parent = $_parent  AND products.optionname3 = '$_optionname' AND products.optionvalue3 = '$_optionvalue' ;
+			UPDATE products SET products.optionname1 = NULL, products.optionvalue1 = NULL WHERE products.parent = :parent  AND products.optionname1 = :optionname AND products.optionvalue1 = :optionvalue ;
+			UPDATE products SET products.optionname2 = NULL, products.optionvalue2 = NULL WHERE products.parent = :parent  AND products.optionname2 = :optionname AND products.optionvalue2 = :optionvalue ;
+			UPDATE products SET products.optionname3 = NULL, products.optionvalue3 = NULL WHERE products.parent = :parent  AND products.optionname3 = :optionname AND products.optionvalue3 = :optionvalue ;
 		";
-		$result = \dash\pdo::query($query, [], null, ['multi_query' => true]);
+
+		$param =
+		[
+			':parent'      => $_parent,
+			':optionname'  => $_optionname,
+			':optionvalue' => $_optionvalue,
+		];
+
+		$result = \dash\pdo::query($query, $param, null, ['multi_query' => true]);
+
 		return $result;
 	}
 
@@ -108,7 +145,7 @@ class update
 			UPDATE products
 			SET products.status = 'deleted'
 			WHERE
-				products.parent = $_parent AND
+				products.parent = :parent AND
 				products.optionname1 IS NULL AND
 				products.optionname2 IS NULL AND
 				products.optionname3 IS NULL AND
@@ -117,21 +154,33 @@ class update
 				products.optionvalue2 IS NULL AND
 				products.optionvalue3 IS NULL
 		";
-		$result = \dash\pdo::query($query, []);
+
+		$param =
+		[
+			':parent'      => $_parent,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 
 	public static function variant_child_calc($_id)
 	{
-		$query  = "SELECT products.id AS `id` FROM products WHERE products.status != 'deleted' AND products.parent = $_id LIMIT 1";
-		$have_child = \dash\pdo::get($query, [], 'id', true);
+		$query  = "SELECT products.id AS `id` FROM products WHERE products.status != 'deleted' AND products.parent = :id LIMIT 1";
+
+		$param =
+		[
+			':id'      => $_id,
+		];
+
+		$have_child = \dash\pdo::get($query, $param, 'id', true);
 
 		$have_child = $have_child ? 1 : null;
 		if(!$have_child)
 		{
-			$query  = "UPDATE products SET products.variant_child = NULL WHERE products.id = $_id LIMIT 1";
-			$result = \dash\pdo::query($query, []);
+			$query  = "UPDATE products SET products.variant_child = NULL WHERE products.id = :id LIMIT 1";
+			$result = \dash\pdo::query($query, $param);
 			return $result;
 		}
 	}
@@ -139,32 +188,61 @@ class update
 
 	public static function update_all_unit($_new_unit_id, $_old_unit_id)
 	{
-		$query  = "UPDATE products SET products.unit_id = $_new_unit_id WHERE products.unit_id = $_old_unit_id";
-		$result = \dash\pdo::query($query, []);
+
+		$query  = "UPDATE products SET products.unit_id = :new_unit_id WHERE products.unit_id = :old_unit_id";
+
+		$param =
+		[
+			':new_unit_id'      => $_new_unit_id,
+			':old_unit_id'      => $_old_unit_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
+
 		return $result;
 	}
 
 
 	public static function clean_all_unit($_old_unit_id)
 	{
-		$query  = "UPDATE products SET products.unit_id = NULL WHERE products.unit_id = $_old_unit_id";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.unit_id = NULL WHERE products.unit_id = :old_unit_id";
+
+		$param =
+		[
+			':old_unit_id'      => $_old_unit_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 
 	public static function update_all_company($_new_company_id, $_old_company_id)
 	{
-		$query  = "UPDATE products SET products.company_id = $_new_company_id WHERE products.company_id = $_old_company_id";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.company_id = :new_company_id WHERE products.company_id = :old_company_id";
+
+		$param =
+		[
+			':new_company_id'      => $_new_company_id,
+			':old_company_id'      => $_old_company_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 
 	public static function clean_all_company($_old_company_id)
 	{
-		$query  = "UPDATE products SET products.company_id = NULL WHERE products.company_id = $_old_company_id";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.company_id = NULL WHERE products.company_id = :old_company_id";
+
+		$param =
+		[
+			':old_company_id'      => $_old_company_id,
+		];
+
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -181,31 +259,67 @@ class update
 
 	public static function thumb($_thumb, $_id)
 	{
+		$param =
+		[
+			':id'      => $_id,
+		];
+
 		if($_thumb)
 		{
-			$query  = "UPDATE products SET products.thumb = '$_thumb' WHERE products.id = $_id LIMIT 1";
+			$query  = "UPDATE products SET products.thumb = :thumb WHERE products.id = :id LIMIT 1";
+			$param[':thumb'] = $_thumb;
 		}
 		else
-
 		{
-			$query  = "UPDATE products SET products.thumb = NULL WHERE products.id = $_id LIMIT 1";
+			$query  = "UPDATE products SET products.thumb = NULL WHERE products.id = :id LIMIT 1";
 		}
-		$result = \dash\pdo::query($query, []);
+
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 
 	public static function gallery($_gallery, $_id)
 	{
-		$query  = "UPDATE products SET products.gallery = '$_gallery' WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.gallery = :gallery WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id'      => $_id,
+			':gallery' => $_gallery,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 	public static function gallery_set_null($_id)
 	{
-		$query  = "UPDATE products SET products.gallery = NULL WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.gallery = NULL WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id'      => $_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
+		return $result;
+	}
+
+
+
+	public static function status_by_parent($_status, $_parent)
+	{
+		$query  = "UPDATE products SET products.status = :status WHERE products.parent = :parent ";
+
+		$param =
+		[
+			':parent' => $_parent,
+			':status' => $_status,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -213,8 +327,15 @@ class update
 
 	public static function status($_status, $_id)
 	{
-		$query  = "UPDATE products SET products.status = '$_status' WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.status = :status WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id' => $_id,
+			':status' => $_status,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
@@ -242,15 +363,28 @@ class update
 
 	public static function variants_update($_variants, $_id)
 	{
-		$query  = "UPDATE products SET products.variants = '$_variants' WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.variants = :variants WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id'       => $_id,
+			':variants' => $_variants,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
 	public static function variants_clean_product($_id)
 	{
-		$query  = "UPDATE products SET products.variants = NULL WHERE products.id = $_id LIMIT 1";
-		$result = \dash\pdo::query($query, []);
+		$query  = "UPDATE products SET products.variants = NULL WHERE products.id = :id LIMIT 1";
+
+		$param =
+		[
+			':id'       => $_id,
+		];
+
+		$result = \dash\pdo::query($query, $param);
 		return $result;
 	}
 
