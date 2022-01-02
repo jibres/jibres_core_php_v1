16fa93796b168bb79c70d33785179f3854968c36
Reza <rm.biqarar@gmail.com>
2022-01-02 12:22:41 +0330

    Bug fix

--- dash/db/pagination.php
+++ dash/db/pagination.php
@@ -13,7 +13,7 @@ namespace dash\db;
 class pagination
 {
 
-	public static function pagination_query($_query, $_param, $_length = 10, $_fuel = null)
+	public static function pagination_query($_query, $_param = [], $_length = 10, $_fuel = null)
 	{
 		$total_rows = \dash\pdo::get($_query, $_param, 'count', true, $_fuel);
 		$total_rows = floatval($total_rows);
