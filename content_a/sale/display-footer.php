150732540f60837fbf0ac2fb71641940e67545b9
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-01-03 20:09:46 +0330

    add product from footer

--- content_a/sale/display-footer.php
+++ content_a/sale/display-footer.php
@@ -48,90 +48,51 @@ if(\dash\url::isLocal())
         $html .= ' data-kerkere-content="hide"';
       }
       $html .= '>';
-      $html .= '<div class="flex">';
-      if(isset($category['products']) && is_array($category['products']))
       {
-        // $html .= $key;
-        // we have some products
-        $myProducts = $category['products'];
-        foreach ($myProducts as $key2 => $product)
+        $html .= '<div class="flex">';
+        if(isset($category['products']) && is_array($category['products']))
         {
-          // loop to draw one product item
-          $img = a($product, 'thumb');
-          $title = a($product, 'title');
-          $price = a($product, 'finalprice');
-          $currency = a($product, 'currency');
-          if($img)
+          // $html .= $key;
+          // we have some products
+          $myProducts = $category['products'];
+          foreach ($myProducts as $key2 => $product)
           {
-            $html .= '<div class="h-32 w-32 rounded relative overflow-hidden p-1">';
+            // loop to draw one product item
+            $id = a($product, 'id');
+            $img = a($product, 'thumb');
+            $title = a($product, 'title');
+            $price = a($product, 'finalprice');
+            $currency = a($product, 'currency');
+            if($img)
             {
-              $html .= '<img class="block rounded" src="'. $img. '" alt="'. $title.'">';
-              $html .= '<div class="absolute inset-x-0 bottom-0 overflow-hidden p-2">';
-              if($title)
+              $html .= '<div class="h-32 w-32 rounded relative overflow-hidden p-1"';
+              $html .= ' data-quick-addProduct="'. $id .'"';
+              $html .= '>';
               {
-                $html .= '<h4 class="line-clamp-2 text-gray-700">'. $title. '</h4>';
-              }
-              if($price)
-              {
-                $html .= '<div class="line-clamp-1 text-gray-900">';
-                $html .= "<span class='font-bold text-lg'>". \dash\fit::price($price). "</span>";
-                $html .= ' ';
-                $html .= $currency;
-                $html .= '</div>';
+                $html .= '<img class="block rounded" src="'. $img. '" alt="'. $title.'">';
+                $html .= '<div class="absolute inset-x-0 bottom-0 overflow-hidden p-2">';
+                if($title)
+                {
+                  $html .= '<h4 class="line-clamp-2 text-gray-700">'. $title. '</h4>';
+                }
+                if($price)
+                {
+                  $html .= '<div class="line-clamp-1 text-gray-900">';
+                  $html .= "<span class='font-bold text-lg'>". \dash\fit::price($price). "</span>";
+                  $html .= ' ';
+                  $html .= $currency;
+                  $html .= '</div>';
 
+                }
+                $html .= '</div>';
               }
               $html .= '</div>';
             }
-            $html .= '</div>';
           }
         }
+        $html .= '</div>';
       }
-      $html .= '</div>';
 
-
-      // var_dump($category['products']);
-
-      // {
-      //   $optMagicBox =
-      //   [
-      //       'grid' => true,
-      //   ];
-
-      //   $args =
-      //   [
-      //       'magicbox_title_position' =>  'inside',
-      //       'coverratio'              =>  '1:1',
-      //       'effect'                  =>  'zoom',
-      //       'image_mask'              =>  'none',
-      //       'height'                  =>  'sm',
-      //       'padding_top'             =>  '2',
-      //       'padding_bottom'          =>  '2',
-      //       'container'               =>  '2xl',
-      //       'magicbox_gap'            =>  'md',
-      //       'coverratio:class'        =>  'aspect-w-1 aspect-h-1',
-      //       'radius:class'            =>  'rounded-lg',
-      //       'padding:class'           =>  'py-2 md:py-3 lg:py-4',
-      //       'padding_top:class'       =>  'pt-2 md:pt-3 lg:pt-4 ',
-      //       'height:style'            =>  'min-height: 25vh;',
-      //       'container:class'         =>  'max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5',
-      //       'magicbox_gap:class'      =>  'gap-1 sm:gap-2 md:gap-4 lg:gap-6',
-      //   ];
-      //   $html .= '<div data-type="g1" class="bg-white flex overflow-hidden relative py-2 md:py-3 lg:py-4">';
-      //   {
-      //     $html .= '<div class="m-auto max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5">';
-      //     {
-      //       $html .= '<div class="grid grid-cols-10 gap-1 sm:gap-2 md:gap-4 lg:gap-6">';
-      //       {
-      //         $html .= \content_site\assemble\element\magicbox::html($args, $category['products'], $optMagicBox);
-      //       }
-      //       $html .= '</div>';
-      //     }
-      //     $html .= '</div>';
-
-      //   }
-      //   $html .= '</div>';
-
-      // }
       $html .= '</div>';
     }
     $html .= '</div>';
