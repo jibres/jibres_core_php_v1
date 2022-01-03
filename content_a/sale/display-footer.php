18094cb14891cd5e8dd72aac2a5eb46b0dfaa559
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-01-03 19:57:20 +0330

    fix footer

--- content_a/sale/display-footer.php
+++ content_a/sale/display-footer.php
@@ -34,6 +34,7 @@ if(\dash\url::isLocal())
 
 
     $oneActive = null;
+    $html .= '<div class="h-32 m-1">';
     foreach ($saleQuickAccess as $key => $category)
     {
       $html .= '<div class="showQuickAccess-'. a($category, 'id'). '"';
@@ -47,50 +48,74 @@ if(\dash\url::isLocal())
         $html .= ' data-kerkere-content="hide"';
       }
       $html .= '>';
-
+      $html .= '<div class="flex">';
+      if(isset($category['products']) && is_array($category['products']))
       {
-        $optMagicBox =
-        [
-            'grid' => true,
-        ];
-
-        $args =
-        [
-            'magicbox_title_position' =>  'inside',
-            'coverratio'              =>  '1:1',
-            'effect'                  =>  'zoom',
-            'image_mask'              =>  'none',
-            'height'                  =>  'sm',
-            'padding_top'             =>  '2',
-            'padding_bottom'          =>  '2',
-            'container'               =>  '2xl',
-            'magicbox_gap'            =>  'md',
-            'coverratio:class'        =>  'aspect-w-1 aspect-h-1',
-            'radius:class'            =>  'rounded-lg',
-            'padding:class'           =>  'py-2 md:py-3 lg:py-4',
-            'padding_top:class'       =>  'pt-2 md:pt-3 lg:pt-4 ',
-            'height:style'            =>  'min-height: 25vh;',
-            'container:class'         =>  'max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5',
-            'magicbox_gap:class'      =>  'gap-1 sm:gap-2 md:gap-4 lg:gap-6',
-        ];
-        $html .= '<div data-type="g1" class="bg-white flex overflow-hidden relative py-2 md:py-3 lg:py-4">';
+        // $html .= $key;
+        // we have some products
+        $myProducts = $category['products'];
+        foreach ($myProducts as $key2 => $product)
         {
-          $html .= '<div class="m-auto max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5">';
+          // loop to draw one product item
+          $img = a($product, 'thumb');
+          $title = a($product, 'title');
+          if($img)
           {
-            $html .= '<div class="grid grid-cols-10 gap-1 sm:gap-2 md:gap-4 lg:gap-6">';
-            {
-              $html .= \content_site\assemble\element\magicbox::html($args, $category['products'], $optMagicBox);
-            }
+            $html .= '<div class="h-32 w-32 rounded relative overflow-hidden p-1">';
+            $html .= '<img class="block rounded" src="'. $img. '" alt="'. $title.'">';
             $html .= '</div>';
           }
-          $html .= '</div>';
-
         }
-        $html .= '</div>';
-
       }
       $html .= '</div>';
+
+
+      // var_dump($category['products']);
+
+      // {
+      //   $optMagicBox =
+      //   [
+      //       'grid' => true,
+      //   ];
+
+      //   $args =
+      //   [
+      //       'magicbox_title_position' =>  'inside',
+      //       'coverratio'              =>  '1:1',
+      //       'effect'                  =>  'zoom',
+      //       'image_mask'              =>  'none',
+      //       'height'                  =>  'sm',
+      //       'padding_top'             =>  '2',
+      //       'padding_bottom'          =>  '2',
+      //       'container'               =>  '2xl',
+      //       'magicbox_gap'            =>  'md',
+      //       'coverratio:class'        =>  'aspect-w-1 aspect-h-1',
+      //       'radius:class'            =>  'rounded-lg',
+      //       'padding:class'           =>  'py-2 md:py-3 lg:py-4',
+      //       'padding_top:class'       =>  'pt-2 md:pt-3 lg:pt-4 ',
+      //       'height:style'            =>  'min-height: 25vh;',
+      //       'container:class'         =>  'max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5',
+      //       'magicbox_gap:class'      =>  'gap-1 sm:gap-2 md:gap-4 lg:gap-6',
+      //   ];
+      //   $html .= '<div data-type="g1" class="bg-white flex overflow-hidden relative py-2 md:py-3 lg:py-4">';
+      //   {
+      //     $html .= '<div class="m-auto max-w-screen-2xl w-full px-2 sm:px-4 lg:px-5">';
+      //     {
+      //       $html .= '<div class="grid grid-cols-10 gap-1 sm:gap-2 md:gap-4 lg:gap-6">';
+      //       {
+      //         $html .= \content_site\assemble\element\magicbox::html($args, $category['products'], $optMagicBox);
+      //       }
+      //       $html .= '</div>';
+      //     }
+      //     $html .= '</div>';
+
+      //   }
+      //   $html .= '</div>';
+
+      // }
+      $html .= '</div>';
     }
+    $html .= '</div>';
     // close footer box
     $html .= '</nav>';
   }
