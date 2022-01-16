9216c7f65c0552e9d0a71e70e380a68e39406f53
Reza <rm.biqarar@gmail.com>
2022-01-16 10:45:38 +0330

    Some fix on plugin detail

--- content_a/plugin/home/display.php
+++ content_a/plugin/home/display.php
@@ -19,30 +19,30 @@ if(!is_array($pluginKeywords))
 $html = '';
 
 
-foreach ($pluginKeywords as $key => $value)
-{
-	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). \dash\request::full_get(['category' => $value]). '">#'. $value. '</a>';
-}
-if(\dash\request::get())
-{
-	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). '">'. T_("Clear filter"). '</a>';
-}
-	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). \dash\request::full_get(['activated' => 1]). '">'. T_("Activated"). '</a>';
+// foreach ($pluginKeywords as $key => $value)
+// {
+// 	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). \dash\request::full_get(['category' => $value]). '">#'. $value. '</a>';
+// }
+// if(\dash\request::get())
+// {
+// 	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). '">'. T_("Clear filter"). '</a>';
+// }
+// 	$html.= '<a class="btn-outline-primary mr-5" href="'. \dash\url::that(). \dash\request::full_get(['activated' => 1]). '">'. T_("Activated"). '</a>';
 
 
 /*========================================
 =            search in plugin            =
 ========================================*/
-$html .= '<form method="get" class="mt-5 mb-5" autocomplete="off" action="'.\dash\url::that().'">';
-{
-	$html .= '<div class="input">';
-	{
-		$html .= '<input type="text" name="q" value="'. \dash\request::get('q'). '">';
-		$html .= '<button class="addon btn"><img class="w-3" src="'. \dash\utility\icon::url('Search'). '"></button>';
-	}
-	$html .= '</div>';
-}
-$html .= '</form>';
+// $html .= '<form method="get" class="mt-5 mb-5" autocomplete="off" action="'.\dash\url::that().'">';
+// {
+// 	$html .= '<div class="input">';
+// 	{
+// 		$html .= '<input type="text" name="q" value="'. \dash\request::get('q'). '">';
+// 		$html .= '<button class="addon btn"><img class="w-3" src="'. \dash\utility\icon::url('Search'). '"></button>';
+// 	}
+// 	$html .= '</div>';
+// }
+// $html .= '</form>';
 
 
 
@@ -56,7 +56,7 @@ $html .= '<section class="text-gray-600 body-font">';
 	{
 		foreach ($pluginList as $key => $value)
 		{
-			$html .= '<div class="c-xs-12 c-sm-12 c-md-6">';
+			$html .= '<div class="c-xs-12 c-sm-12 c-md-12 c-lg-6">';
 			{
 
 				$is_activated = \lib\app\plugin\business::is_activated(a($value, 'plugin'));
@@ -67,13 +67,27 @@ $html .= '<section class="text-gray-600 body-font">';
 					$html .= '<div class="p-4 flex">';
 					{
 
-						$html .= '<div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">';
+						$html .= '<div class="flex flex-col">';
 						{
-							$icon = a($value, 'icon');
-
-							if($icon && is_array($icon))
+							$html .= '<div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0 p-1">';
 							{
-								$html .= \dash\utility\icon::svg(...$icon);
+								$icon = a($value, 'icon');
+
+								if($icon && is_array($icon))
+								{
+									$html .= \dash\utility\icon::svg(...$icon);
+								}
+
+							}
+							$html .= '</div>';
+
+							if($is_activated)
+							{
+								$html .= '<div class="w-8 h-8 inline-flex items-center justify-center rounded-full mb-4 flex-shrink-0 p-1" title="'.T_("Activated").'">';
+								{
+									$html .= \dash\utility\icon::svg('check-circle-fill', 'bootstrap', 'green');
+								}
+								$html .= '</div>';
 							}
 						}
 						$html .= '</div>';
@@ -84,17 +98,33 @@ $html .= '<section class="text-gray-600 body-font">';
 							$html .= '<h2 class="text-gray-900 text-lg title-font font-medium mb-2">';
 							{
 								$html .= a($value, 'title');
-								$html .= a($value, 'type');
-								if(a($value, 'price') === 0)
-								{
-									$html .= '<div class="text-green-500">'.  T_("Free"). '</div>';
-								}
-								else
-								{
-									$html .= '<div class="">'.  \dash\fit::number(a($value, 'price')). '</div>';
-								}
 							}
 							$html .= '</h2>';
+
+							// if(a($value, 'price') === 0)
+							// {
+							// 	$html .= '<div class="text-green-500">'.  T_("Free"). '</div>';
+							// }
+							// else
+							// {
+							// 	$html .= '<div class="">';
+							// 	{
+							// 		$html .= '<span class="">';
+							// 		{
+							// 			$html .= T_("Price");
+							// 		}
+							// 		$html .= '</span> ';
+
+							// 		$html .= '<span class="">';
+							// 		{
+							// 			$html .= \dash\fit::number(a($value, 'price'));
+							// 		}
+							// 		$html .= '</span>';
+
+
+							// 	}
+							// 	$html .= '</div>';
+							// }
 
 							$html .= '<p class="leading-relaxed text-base">';
 							{
@@ -104,7 +134,15 @@ $html .= '<section class="text-gray-600 body-font">';
 
 							$html .= '<a class="mt-3 text-indigo-500 inline-flex items-center" href="'. \dash\url::that(). '/view/'.  a($value, 'plugin'). '">';
 							{
-								$html .= T_("Read more");
+								if($is_activated)
+								{
+									$html .= T_("View detail");
+
+								}
+								else
+								{
+									$html .= T_("Add plugin");
+								}
 							}
 							$html .= '</a>';
 						}
