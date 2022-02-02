a33e860c6fd5b17137b03aa735353f659452b629
Reza <rm.biqarar@gmail.com>
2022-02-02 17:29:55 +0330

    Clean

--- content_a/view.php
+++ content_a/view.php
@@ -10,7 +10,6 @@ class view
 		\dash\face::intro(T_("Jibres is not just an online accounting software;"). ' '.  T_("We try to create the best financial platform that has everything you need to sale and manage your financial life."));
 		\dash\face::slogan(T_("Integrated Sales and Online Accounting"));
 
-
 		// enable title box
 		if(\dash\url::module() == 'setup')
 		{
@@ -50,26 +49,6 @@ class view
 			default:
 				break;
 		}
-
-		// $cache_key = 'staff_list_'.\dash\url::subdomain();
-		// $cache = \dash\session::get($cache_key, 'jibres_store');
-		// if(!$cache)
-		// {
-		// 	$cache = \lib\app\thirdparty::list(null, ['staff' => 1]);
-		// 	$new = [];
-		// 	foreach ($cache as $key => $value)
-		// 	{
-		// 		$new[] =
-		// 		[
-		// 			'id'        => isset($value['id']) ? $value['id'] : null,
-		// 			'firstname' => isset($value['firstname']) ? $value['firstname'] : null,
-		// 			'lastname'  => isset($value['lastname']) ? $value['lastname'] : null,
-		// 			'mobile'    => isset($value['mobile']) ? $value['mobile'] : null,
-		// 		];
-		// 	}
-		// 	\dash\session::set($cache_key, $new, 'jibres_store', (60*10));
-		// }
-		// \dash\data::staffList($cache);
 
 	}
 }
