da1b20c196ce0938bfc28992336e0769bfc44fad
Reza <rm.biqarar@gmail.com>
2022-02-05 14:22:28 +0330

    Add api sms

---
+++ content_r10/jibres/sms/controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace content_r10\jibres\sms;
+
+
+class controller
+{
+	public static function routing()
+	{
+
+	}
+}
+?>
---
+++ content_r10/jibres/sms/model.php
@@ -0,0 +1,35 @@
+<?php
+namespace content_r10\jibres\sms;
+
+
+class model
+{
+	public static function post()
+	{
+		$post = \dash\request::input_body();
+
+		$jibres_sms =
+		[
+			'local_id'    => a($post, 'local_id'),
+			'store_id'    => \content_r10\tools::get_current_business_id(),
+			'mobile'      => a($post, 'mobile'),
+			'message'     => a($post, 'message'),
+			'sender'      => a($post, 'sender'),
+			'len'         => a($post, 'len'),
+			'smscount'    => a($post, 'smscount'),
+			'status'      => a($post, 'status'),
+			'type'        => a($post, 'type'),
+			'mode'        => a($post, 'mode'),
+			'datecreated' => date("Y-m-d H:i:s"),
+		];
+
+		// save db
+		$jibres_sms_id = \lib\db\store_sms\insert::new_record($jibres_sms);
+
+		$result = ['jibres_id' => $jibres_sms_id];
+
+		\content_r10\tools::say($result);
+
+	}
+}
+?>
