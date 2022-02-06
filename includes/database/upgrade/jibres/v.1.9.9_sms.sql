24bf32c6cdd130a6919e5826e96c8baa5df4f595
Reza <rm.biqarar@gmail.com>
2022-02-06 14:42:13 +0330

    Save

--- includes/database/upgrade/jibres/v.1.9.9_sms.sql
+++ includes/database/upgrade/jibres/v.1.9.9_sms.sql
@@ -61,6 +61,7 @@ INDEX `sms_type` (`type`)
 ALTER TABLE jibres.sms_log DROP `mobiles`;
 ALTER TABLE jibres.sms_log CHANGE `status` `status` enum('register', 'pending','sending','expired', 'moneylow', 'unknown', 'send', 'sended','delivered','queue','failed','undelivered','cancel','block','other') NULL DEFAULT NULL;
 ALTER TABLE jibres.sms_log ADD `template` varchar(100) NULL DEFAULT NULL;
+ALTER TABLE jibres.sms_log ADD `jibres_sms_id` bigint UNSIGNED NULL DEFAULT NULL;
 
 
 
--- includes/database/upgrade/store/v.3.5.6_fix_sms_log_package_id.sql
+++ includes/database/upgrade/store/v.3.5.6_fix_sms_log_package_id.sql
@@ -1,3 +1,4 @@
 ALTER TABLE jibres_XXXXXXX.sms_log DROP `mobiles`;
 ALTER TABLE jibres_XXXXXXX.sms_log CHANGE `status` `status` enum('register', 'pending','sending','expired', 'moneylow', 'unknown', 'send', 'sended','delivered','queue','failed','undelivered','cancel','block','other') NULL DEFAULT NULL;
 ALTER TABLE jibres_XXXXXXX.sms_log ADD `template` varchar(100) NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.sms_log ADD `jibres_sms_id` bigint UNSIGNED NULL DEFAULT NULL;
