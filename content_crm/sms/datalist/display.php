fd4e1817ab84fc615215db71406635ef6ba2338e
Reza <rm.biqarar@gmail.com>
2022-02-23 17:31:27 +0330

    bug fix show message

--- content_crm/sms/datalist/display.php
+++ content_crm/sms/datalist/display.php
@@ -4,7 +4,7 @@
      <li>
       <a class="f align-center" href="<?php echo \dash\url::this(). '/view?id='. $value['id'] ?>">
 
-        <div class="key"><?php echo T_($value['message']); ?></div>
+        <div class="key"><?php echo \dash\str::substr_space($value['message'], 200); ?></div>
 
         <div class="value font-bold s0"><?php echo \dash\fit::mobile(a($value, 'mobile')); ?></div>
         <div class="value datetime s0"><?php echo \dash\fit::date_time($value['datecreated']) ?></div>
