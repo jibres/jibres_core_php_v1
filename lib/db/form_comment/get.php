5159adc53c797e4a2d03d6c073d8b7793a0577df
Reza <rm.biqarar@gmail.com>
2022-02-28 18:02:43 +0330

    Save

--- lib/db/form_comment/get.php
+++ lib/db/form_comment/get.php
@@ -6,7 +6,19 @@ class get
 {
 	public static function get_by_answer_id($_answer_id)
 	{
-		$query = "SELECT * FROM form_comment WHERE form_comment.answer_id = $_answer_id  ORDER BY form_comment.id DESC";
+		$query =
+		"
+			SELECT
+				form_comment.*,
+				users.mobile,
+				users.displayname
+			FROM
+				form_comment
+			LEFT JOIN users ON users.id = form_comment.user_id
+			WHERE
+				form_comment.answer_id = $_answer_id
+			ORDER BY form_comment.id DESC
+		";
 		$result = \dash\pdo::get($query);
 		return $result;
 	}
