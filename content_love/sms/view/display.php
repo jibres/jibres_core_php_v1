75a9959f35154d8deacd4e694c84666087c8b5cf
Reza <rm.biqarar@gmail.com>
2022-03-05 12:50:39 +0330

    Show json result

--- content_love/sms/view/display.php
+++ content_love/sms/view/display.php
@@ -1,15 +1,1 @@
-<?php $data = \dash\data::dataRow(); ?>
-<div class="box">
-  <div class="body">
-    <div class="tblBox">
-      <table class="tbl1 v4">
-        <tbody>
-          <?php foreach ($data as $key => $value) {?>
-            <tr><td><?php echo $key ?></td><td><?php echo $value; ?></td></tr>
-          <?php } //endif ?>
-
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
+<?php require_once(root. 'content_love/telegram/view/display.php') ?>
--- content_love/telegram/view/display.php
+++ content_love/telegram/view/display.php
@@ -4,9 +4,33 @@
     <div class="tblBox">
       <table class="tbl1 v4">
         <tbody>
-          <?php foreach ($data as $key => $value) {?>
+          <?php foreach ($data as $key => $value)
+          {
+            echo '<tr>';
+              echo '<td>';
+              echo $key;
+              echo '</td>';
+              echo '<td>';
+            if(substr(strval($value), 0, 1) === '[' || substr(strval($value), 0, 1) === '{')
+            {
+              $value = json_decode($value, true);
+              echo '<pre>';
+              echo json_encode($value, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+              echo '</pre>';
+
+            }
+            else
+            {
+              echo $value;
+            }
+
+              echo '</td>';
+            echo '</tr>';
+          }
+          ?>
+
             <tr><td><?php echo $key ?></td><td><?php echo $value; ?></td></tr>
-          <?php } //endif ?>
+
 
         </tbody>
       </table>
