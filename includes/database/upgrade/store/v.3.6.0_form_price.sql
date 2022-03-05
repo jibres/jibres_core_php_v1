0e64b4c6af46bfab1ea8c538b62b7d80a1e9c961
Reza <rm.biqarar@gmail.com>
2022-03-05 19:22:29 +0330

    Add form price

---
+++ includes/database/upgrade/store/v.3.6.0_form_price.sql
@@ -0,0 +1,4 @@
+ALTER TABLE jibres_XXXXXXX.form_choice ADD `price` decimal(31, 4) NULL DEFAULT NULL AFTER `score`;
+ALTER TABLE jibres_XXXXXXX.form_answer ADD `transaction_id` bigint NULL DEFAULT NULL AFTER `factor_id`;
+
+
