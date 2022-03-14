41720f12e2b431500ca5a09336987cc7e82efd46
Reza <rm.biqarar@gmail.com>
2022-03-14 10:25:30 +0330

    Remove useless upgrade

--- includes/database/upgrade/jibres/v.2.1.2_fix_domani_dateexpire.sql
+++ includes/database/upgrade/jibres/v.2.1.2_fix_domani_dateexpire.sql
@@ -1,8 +1,1 @@
 ALTER TABLE jibres_nic.domain CHANGE `dateexpire` `dateexpire` date NULL DEFAULT NULL;
-
-
-
-UPDATE jibres_nic.domain SET  domain.status = 'enable' ,
-domain.available = 0 , domain.dateexpire = '2027-03-22' ,
- domain.datemodified = '2022-03-08 14:19:28'
- WHERE domain.id = 430 LIMIT 1;
