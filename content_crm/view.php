65b60ac4a4c2cc81adacc35792ef48e15fd433ce
Reza <rm.biqarar@gmail.com>
2022-03-28 17:21:06 +0430

    Fix crm

--- content_crm/view.php
+++ content_crm/view.php
@@ -8,8 +8,16 @@ class view
 
 		\dash\upload\size::set_default_file_size('crm');
 
+		if(!\dash\engine\store::inStore())
+		{
+			\dash\data::include_m2('dark');
 
-		\dash\data::include_m2(true);
+		}
+		else
+		{
+			\dash\data::include_m2(true);
+		}
+
 		\dash\data::include_m2_search(\dash\url::kingdom(). '/a/setting/search/full');
 		\dash\data::include_m2_searchPlaceHolder(T_('Search'));
 	}
