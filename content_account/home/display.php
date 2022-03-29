530af82d091feac597780157163b5274556cdd7f
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-03-29 17:49:07 +0430

    add logout

--- content_account/home/display.php
+++ content_account/home/display.php
@@ -1,27 +1,28 @@
 
-<section class="text-center mb-4 select-none">
+<section class="text-center my-2 select-none">
   <a href="<?php echo \dash\url::here(); ?>/my/avatar" class="inline-block mb-2">
     <?php if(\dash\user::detail('avatar'))  {?>
-      <img class="w-36 rounded-full" src="<?php echo \dash\user::detail('avatar'); ?>" alt='<?php echo T_("Avatar of you"); ?> <?php echo \dash\user::detail('displayname'); ?>'>
+      <img class="w-36 h-36 rounded-full" src="<?php echo \dash\user::detail('avatar'); ?>" alt='<?php echo T_("Avatar of you"); ?> <?php echo \dash\user::detail('displayname'); ?>'>
     <?php }elseif(\dash\user::id()) {?>
-      <img class="w-36 rounded-full" src="<?php echo \dash\url::siftal(); ?>/images/default/avatar.png" alt='<?php echo T_("Default Avatar"); ?>'>
+      <img class="w-36 h-36 rounded-full" src="<?php echo \dash\url::siftal(); ?>/images/default/avatar.png" alt='<?php echo T_("Default Avatar"); ?>'>
     <?php } // endif ?>
   </a>
   <h2 class="leading-loose text-lg"><?php echo T_("Welcome"); ?><?php echo T_(","); ?> <span class="font-bold"><?php echo \dash\user::detail('fullname'); ?></span></h2>
-  <p class="leading-loose text-sm text-zinc-600"><?php echo T_("Manage your info, privacy, and security to make us work better for you"); ?></p>
+  <p class="leading-loose text-xs text-zinc-600"><?php echo T_("Manage your info, privacy, and security to make us work better for you"); ?></p>
 </section>
 
 <?php if(!\dash\user::detail('verifymobile'))  {?>
   <a href="<?php echo \dash\url::kingdom(). '/enter/verify'; ?>" target="_blank" class="alert-warning text-center font-bold block mb-2"><?php echo T_("Your account is not verify! Please verify your mobile number."); ?></a>
 <?php }//endif ?>
 
-<div class="flex flex-row flex-wrap text-sm select-none">
-  <div class="basis-full lg:basis-1/2 p-2">
 
+<div class="flex flex-row flex-wrap text-sm select-none">
+
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="flex items-center p-4">
+      <div class="flex items-center p-2 lg:p-4">
         <div class="grow">
-          <h3 class="text-xl font-bold leading-loose"><?php echo T_("Personal info"); ?></h3>
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Personal info"); ?></h3>
           <p class="text-zinc-500 leading-5"><?php echo T_("Basic info, like your name and photo, that you use on our services"); ?></p>
         </div>
         <div class="flex-none">
@@ -29,17 +30,17 @@
         </div>
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::here(); ?>/my"><?php echo T_("Manage profile"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::here(); ?>/my"><?php echo T_("Manage profile"); ?></a>
       </footer>
     </section>
   </div>
 
 
-  <div class="basis-full lg:basis-1/2 p-2">
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="flex items-center p-4">
+      <div class="flex items-center p-2 lg:p-4">
         <div class="grow">
-          <h3 class="text-xl font-bold leading-loose"><?php echo T_("Security"); ?></h3>
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Security"); ?></h3>
           <p class="text-zinc-500 leading-5"><?php echo T_("Settings and recommendations to help you keep your account secure"); ?></p>
         </div>
         <div class="flex-none">
@@ -47,18 +48,16 @@
         </div>
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::here(); ?>/security"><?php echo T_("Keep your account protected"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::here(); ?>/security"><?php echo T_("Keep your account protected"); ?></a>
       </footer>
     </section>
-
   </div>
 
-  <div class="basis-full lg:basis-1/2 p-2">
-
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="flex items-center p-4">
+      <div class="flex items-center p-2 lg:p-4">
         <div class="grow">
-          <h3 class="text-xl font-bold leading-loose"><?php echo T_("Billing"); ?></h3>
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Billing"); ?></h3>
           <p class="text-zinc-500 leading-5"><?php echo T_("Check your account balance, charge your account, and bill your invoices!"); ?></p>
         </div>
         <div class="flex-none">
@@ -66,18 +65,16 @@
         </div>
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::here(); ?>/billing"><?php echo T_("Check"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::here(); ?>/billing"><?php echo T_("Check"); ?></a>
       </footer>
     </section>
-
   </div>
 
-  <div class="basis-full lg:basis-1/2 p-2">
-
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="flex items-center p-4">
+      <div class="flex items-center p-2 lg:p-4">
         <div class="grow">
-          <h3 class="text-xl font-bold leading-loose"><?php echo T_("Notifications"); ?></h3>
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Notifications"); ?></h3>
           <p class="text-zinc-500 leading-5"><?php echo T_("Check your last messages."); ?> <?php echo T_("Maybe some messages need your action!"); ?></p>
         </div>
         <div class="flex-none">
@@ -85,18 +82,17 @@
         </div>
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::here(); ?>/notification"><?php echo T_("Read your messages"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::here(); ?>/notification"><?php echo T_("Read your messages"); ?></a>
       </footer>
     </section>
   </div>
 
 
-  <div class="basis-full lg:basis-1/2 p-2">
-
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="flex items-center p-4">
+      <div class="flex items-center p-2 lg:p-4">
         <div class="grow">
-          <h3 class="text-xl font-bold leading-loose"><?php echo T_("Support"); ?></h3>
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Support"); ?></h3>
           <p class="text-zinc-500 leading-5"><?php echo T_("Get expert answers and advice on our service or contact our legendary support team"); ?></p>
         </div>
         <div class="flex-none">
@@ -104,30 +100,29 @@
         </div>
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::kingdom(). '/my/ticket'; ?>"><?php echo T_("Tickets"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::kingdom(). '/my/ticket'; ?>"><?php echo T_("Tickets"); ?></a>
       </footer>
     </section>
-
   </div>
 
-  <div class="basis-full lg:basis-1/2 p-2">
+
+
+  <div class="basis-full lg:basis-1/2 p-1 lg:p-2">
     <section class="bg-white rounded-lg border">
-      <div class="body pad f">
-        <div class="c">
-
-          <?php echo T_("Logout from your account!"); ?>
+      <div class="flex items-center p-2 lg:p-4">
+        <div class="grow">
+          <h3 class="text-lg font-bold leading-loose"><?php echo T_("Bye!"); ?></h3>
+          <p class="text-zinc-500 leading-5"><?php echo T_("If you finish, you can log out so that no one else can access your account without permission."); ?></p>
         </div>
-        <div class="cauto os">
-          <i class="sf-log-out fs14"></i>
+        <div class="flex-none">
+          <img class="w-16 h-16" src="<?php echo \dash\utility\icon::url('door-closed', 'bootstrap'); ?>" alt='<?php echo T_("logout"); ?>'>
         </div>
-
       </div>
       <footer class="border-t">
-        <a class="px-4 py-2 block btn-outline-primary transition" href="<?php echo \dash\url::sitelang(); ?>/logout"><?php echo T_("Logout"); ?></a>
+        <a class="px-4 py-2 block btn-outline-primary transition rounded-b-lg" href="<?php echo \dash\url::kingdom(). '/logout'; ?>"><?php echo T_("Logout"); ?></a>
       </footer>
     </section>
   </div>
-
 
 </div>
 
