03b56d754075a2852d10080bc7d2d13251a7eecb
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-04-07 10:32:25 +0430

    bugfix developer page

--- content_developers/home/display.php
+++ content_developers/home/display.php
@@ -1,36 +1,36 @@
 
 	<div class="avand">
-		<div class="f text-center pT50 pB100">
+		<div class="f text-center py-8">
 
 			<div class="c4 s12 p-2">
 				<div class="box hasCover">
-					<img class="cover" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-guides-1.jpg" alt='<?php echo T_("Jibres Guides"); ?>'>
+					<img class="cover mx-auto" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-guides-1.jpg" alt='<?php echo T_("Jibres Guides"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Guides"); ?></h2>
-						<p class="mB50"><?php echo T_("Learn the ins and outs of our<br>API and get started."); ?></p>
-				  <a class="btn2 block blue" href="<?php echo \dash\url::kingdom() ?>/guides"><?php echo T_("View Guides"); ?></a>
+						<p class="mb-6"><?php echo T_("Learn the ins and outs of our<br>API and get started."); ?></p>
+				  <a class="btn-primary" href="<?php echo \dash\url::kingdom() ?>/guides"><?php echo T_("View Guides"); ?></a>
 					</div>
 				</div>
 			</div>
 
 			<div class="c4 s12 p-2">
 				<div class="box hasCover">
-					<img class="cover" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-libraries-1.jpg" alt='<?php echo T_("Jibres Libraries"); ?>'>
+					<img class="cover mx-auto" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-libraries-1.jpg" alt='<?php echo T_("Jibres Libraries"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Libraries"); ?></h2>
-						<p class="mB50"><?php echo T_("Find our API translated to<br>your preferred language."); ?></p>
-				  <a class="btn2 block green" href="<?php echo \dash\url::kingdom() ?>/libraries"><?php echo T_("View Libraries"); ?></a>
+						<p class="mb-6"><?php echo T_("Find our API translated to<br>your preferred language."); ?></p>
+				  <a class="btn-primary" href="<?php echo \dash\url::kingdom() ?>/libraries"><?php echo T_("View Libraries"); ?></a>
 					</div>
 				</div>
 			</div>
 
 			<div class="c4 s12 p-2">
 				<div class="box hasCover">
-					<img class="cover" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-docs-1.jpg" alt='<?php echo T_("Jibres Docs"); ?>'>
+					<img class="cover mx-auto" src="<?php echo \dash\url::cdn();?>/img/developers/jibres-docs-1.jpg" alt='<?php echo T_("Jibres Docs"); ?>'>
 					<div class="body">
 						<h2><?php echo T_("Documentation"); ?></h2>
-						<p class="mB50"><?php echo T_("Get everything you need to<br>build on top of our API."); ?></p>
-				  <a class="btn2 block red" href="<?php echo \dash\url::kingdom() ?>/docs"><?php echo T_("View Docs"); ?></a>
+						<p class="mb-6"><?php echo T_("Get everything you need to<br>build on top of our API."); ?></p>
+				  <a class="btn-primary" href="<?php echo \dash\url::kingdom() ?>/docs"><?php echo T_("View Docs"); ?></a>
 					</div>
 				</div>
 			</div>
