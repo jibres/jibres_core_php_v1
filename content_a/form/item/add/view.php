2690f1bab847d942de5c90a371536078d6a6308c
Reza <rm.biqarar@gmail.com>
2022-04-16 18:15:56 +0430

    Fix back link

--- content_a/form/item/add/view.php
+++ content_a/form/item/add/view.php
@@ -10,7 +10,7 @@ class view
 
 		// back
 		\dash\data::back_text(T_('Back'));
-		\dash\data::back_link(\dash\url::this());
+		\dash\data::back_link(\dash\url::this(). '/edit?id='. \dash\request::get('id'));
 
 		$form_id = \dash\request::get('id');
 
