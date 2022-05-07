4e8d982a97160fb3444c3160da1c128f80806d68
Reza <rm.biqarar@gmail.com>
2022-05-07 12:06:45 +0430

    Save

--- content_a/accounting/filter.php
+++ content_a/accounting/filter.php
@@ -114,7 +114,7 @@
           </div>
         <?php } //endif ?>
         <div class="c-xs-12 c-sm-auto mt-6">
-          <div class="mt-6 txtRa">
+          <div class="txtRa">
             <?php if(\dash\url::subchild() != 'balancesheet') {?>
               <?php if(\dash\request::get('year_id') || \lib\app\tax\year\get::default_year('id')) {?>
                 <div class="btn-secondary " data-title='<?php echo T_("Reset document number?") ?>' data-confirm data-data='{"resetnumber": "resetnumber", "year_id" : "<?php echo \dash\request::get('year_id'); ?>"}'><i class="sf-refresh"></i></div>
