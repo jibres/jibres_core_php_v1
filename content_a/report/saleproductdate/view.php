767dbf1c77db2abc7f20231d459ba84fc532701c
Reza <rm.biqarar@gmail.com>
2022-05-12 12:35:12 +0430

    Add sale product per date module

---
+++ content_a/report/saleproductdate/view.php
@@ -0,0 +1,16 @@
+<?php
+namespace content_a\report\saleproductdate;
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('_group_setting');
+
+		\dash\face::title(T_('Sale Product per date'));
+
+		\dash\data::back_text(T_('Back'));
+		\dash\data::back_link(\dash\url::this());
+	}
+}
+?>
