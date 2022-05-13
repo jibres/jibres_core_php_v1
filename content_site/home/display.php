7bfd2b8e71e95441b6c3dade9b052de204d2a46b
Reza <rm.biqarar@gmail.com>
2022-05-13 14:17:06 +0430

    Add link of sutebuilder dashboard

--- content_site/home/display.php
+++ content_site/home/display.php
@@ -1,3 +1,47 @@
 <?php
+$html = '';
 
+
+
+echo $html;
 ?>
+<section class="row">
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::this(). '/pages' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Pages") ?></div>
+			</div>
+		</a>
+	</div>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::kingdom(). '/a/setting/menu' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Menu") ?></div>
+			</div>
+		</a>
+	</div>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::kingdom(). '/a/setting/domain' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Domain") ?></div>
+			</div>
+		</a>
+	</div>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::this(). '/sitemap' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Sitemap") ?></div>
+			</div>
+		</a>
+	</div>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::this(). '/staticfile' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Static file") ?></div>
+			</div>
+		</a>
+	</div>
+
+
+
+</section>
