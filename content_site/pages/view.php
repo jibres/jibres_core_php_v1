d03a856b88d72b557fef4252d6d263295b9ba760
Reza <rm.biqarar@gmail.com>
2022-05-13 12:54:42 +0430

    Move content site to pages module

---
+++ content_site/pages/view.php
@@ -0,0 +1,61 @@
+<?php
+namespace content_site\pages;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\face::title(T_('Site Builder pages'));
+
+		\dash\data::action_text(T_('Add New Page'));
+		\dash\data::action_link(\dash\url::here(). '/page/new');
+
+		\dash\data::back_text(T_('Dashboard'));
+		// \dash\data::back_direct(true);
+		\dash\data::back_link(\dash\url::here());
+
+		\dash\data::include_adminPanelBuilder(true);
+
+		\dash\data::listEngine_start(true);
+		\dash\data::listEngine_search(\dash\url::that());
+		\dash\data::listEngine_filter(\dash\app\posts\filter::list('pagebuilder'));
+		\dash\data::listEngine_sort(true);
+		\dash\data::sortList(\dash\app\posts\filter::sort_list('pagebuilder'));
+
+		$args =
+		[
+			// 'order'       => \dash\request::get('order'),
+			// 'sort'        => \dash\request::get('sort'),
+			// 'status'      => \dash\request::get('status'),
+			'type'                     => 'pagebuilder',
+			'load_pagebuilder_changes' => true,
+
+			// 'pd'      => \dash\request::get('pd'),
+			// 'g'       => \dash\request::get('g'),
+			// 'fi'      => \dash\request::get('fi'),
+			// 'co'      => \dash\request::get('co'),
+			// 'seo'     => \dash\request::get('seo'),
+			// 'sa'      => \dash\request::get('sa'),
+			// 'com'     => \dash\request::get('com'),
+			// 't'       => \dash\request::get('t'),
+			// 'r'       => \dash\request::get('r'),
+		];
+
+		$search_string = \dash\validate::search_string();
+		$postList      = \dash\app\posts\search::list($search_string, $args, true);
+
+		\dash\data::dataTable($postList);
+
+		$isFiltered = \dash\app\posts\search::is_filtered();
+		\dash\data::isFiltered($isFiltered);
+
+		if($isFiltered)
+		{
+			\dash\face::title(\dash\face::title() . '  '. T_('Filtered'));
+		}
+
+	}
+
+}
+?>
