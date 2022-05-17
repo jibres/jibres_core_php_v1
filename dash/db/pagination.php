47a235f064eb4f7c17e05d7fa391c5131ccf4216
Reza <rm.biqarar@gmail.com>
2022-05-17 18:49:46 +0430

    Get pagination by total rows

--- dash/db/pagination.php
+++ dash/db/pagination.php
@@ -17,7 +17,21 @@ class pagination
 	{
 		$total_rows = \dash\pdo::get($_query, $_param, 'count', true, $_fuel);
 		$total_rows = floatval($total_rows);
-		$result     = \dash\utility\pagination::init($total_rows, $_length);
+		return self::get_limit($total_rows, $_length);
+	}
+
+
+	/**
+	 * Gets the limit.
+	 *
+	 * @param      <type>  $_total_rows  The total rows
+	 * @param      int     $_length      The length
+	 *
+	 * @return     string  The limit.
+	 */
+	public static function get_limit($_total_rows, $_length = 10)
+	{
+		$result     = \dash\utility\pagination::init(floatval($_total_rows), $_length);
 
 		if($result)
 		{
