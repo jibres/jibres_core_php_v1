4f06ad74040cd41716baa05b1a446c1119e5b628
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-05-26 17:53:15 +0430

    update link to new version

--- content/investment/controller.php
+++ content/investment/controller.php
@@ -11,11 +11,11 @@ class controller
 		switch (\dash\url::child())
 		{
 			case 'pitchdeck':
-				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pdf?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pdf?v=2');
 				break;
 
 			case 'pitchdeck-ppt':
-				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pptx?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pptx?v=2');
 				break;
 
 			case 'financial':
@@ -23,15 +23,15 @@ class controller
 				break;
 
 			case 'businessplan':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.pdf?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1.pdf?v=1');
 				break;
 
 			case 'businessplan-onepage':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1-OnePage.jpg?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1-OnePage.jpg?v=1');
 				break;
 
 			case 'swot':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1-SWOT.jpg?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.0-SWOT.jpg?v=1');
 				break;
 
 			case 'canvas':
