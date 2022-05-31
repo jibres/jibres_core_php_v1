b7e5ebbbdcffd7afd98ce6952acdd03488d094ce
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-05-31 17:13:24 +0430

    update version of files

--- content/investment/controller.php
+++ content/investment/controller.php
@@ -11,31 +11,31 @@ class controller
 		switch (\dash\url::child())
 		{
 			case 'pitchdeck':
-				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pdf?v=2');
+				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pdf?v=3');
 				break;
 
 			case 'pitchdeck-ppt':
-				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pptx?v=2');
+				\dash\redirect::to(\dash\url::cdn(). '/business/pitch-deck/v1/Jibres-PitchDeck-v'. $pitchdeckVersion. '.pptx?v=3');
 				break;
 
 			case 'financial':
-				\dash\redirect::to(\dash\url::cdn(). '/business/financial/Jibres-Financial-Projection-v'. $FinancialVersion. '.xlsx?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/financial/Jibres-Financial-Projection-v'. $FinancialVersion. '.xlsx?v=3');
 				break;
 
 			case 'businessplan':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1.pdf?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1.pdf?v=2');
 				break;
 
 			case 'businessplan-onepage':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1-OnePage.jpg?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.1-OnePage.jpg?v=2');
 				break;
 
 			case 'swot':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.0-SWOT.jpg?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-plan/Jibres-BusinessPlan-v1.0-SWOT.jpg?v=2');
 				break;
 
 			case 'canvas':
-				\dash\redirect::to(\dash\url::cdn(). '/business/business-model-canvas/jibres-business-model-canvas-v1.jpg?v=1');
+				\dash\redirect::to(\dash\url::cdn(). '/business/business-model-canvas/jibres-business-model-canvas-v1.jpg?v=2');
 				break;
 
 		}
