1117d6976802ee74bf8158be6a9a05e4e05f536c
Reza <rm.biqarar@gmail.com>
2022-06-12 14:24:36 +0430

    Add upload read file function

--- dash/upload/quick.php
+++ dash/upload/quick.php
@@ -65,5 +65,36 @@ class quick
 
 		return $data;
 	}
+
+
+
+	public static function read_file($_upload_name)
+	{
+		if(!$_upload_name)
+		{
+			return false;
+		}
+
+		$meta =
+		[
+			'allow_size' => \dash\upload\size::MB(1),
+			'return_real_addr' => true,
+			'ext' =>
+			[
+				'csv', 'txt',
+			],
+		];
+
+		$file = \dash\upload\file::upload($_upload_name, $meta);
+
+		if(!$file)
+		{
+			return false;
+		}
+
+		$data = \dash\file::read($file);
+
+		return $data;
+	}
 }
 ?>
