2be924f2d76f3cc403dbfef5376086edfd1bdc0b
Reza <rm.biqarar@gmail.com>
2022-06-14 11:40:05 +0430

    JD-17 add form comment update method

---
+++ lib/db/form_comment/update.php
@@ -0,0 +1,30 @@
+<?php
+namespace lib\db\form_comment;
+
+
+class update
+{
+	public static function set_whole_dateview(string $_form_comment_ids)
+	{
+		$date = date("Y-m-d H:i:s");
+		$query  =
+		"
+			UPDATE
+				form_comment
+			SET
+				form_comment.view = 1,
+				form_comment.dateview = :date
+			WHERE
+				form_comment.id IN ($_form_comment_ids)
+		";
+
+		$param = [':date' => $date];
+
+		$result = \dash\pdo::query($query, $param);
+
+		return $result;
+
+	}
+
+}
+?>
