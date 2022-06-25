5529a73bff0d2e373fb774e274cd3b3e8365c038
Reza <rm.biqarar@gmail.com>
2022-06-25 16:42:55 +0430

    Bug fix php 8.1

--- dash/utility/convert.php
+++ dash/utility/convert.php
@@ -11,7 +11,7 @@ class convert
 
 	public static function to_en_number($_string)
     {
-        $result = str_replace(self::$fa, self::$en, $_string);
+        $result = str_replace(self::$fa, self::$en, strval($_string));
         $result = str_replace(self::$ar, self::$en, $result);
         return $result;
     }
@@ -19,21 +19,21 @@ class convert
 
     public static function to_fa_number($_string)
     {
-        $result = str_replace(self::$en, self::$fa, $_string);
+        $result = str_replace(self::$en, self::$fa, strval($_string));
         $result = str_replace(self::$ar, self::$fa, $result);
         return $result;
     }
 
     public static function ar_to_fa_number($_string)
     {
-        $result = str_replace(self::$ar, self::$fa, $_string);
+        $result = str_replace(self::$ar, self::$fa, strval($_string));
         return $result;
     }
 
 
     public static function to_ar_number($_string)
     {
-        $result = str_replace(self::$en, self::$ar, $_string);
+        $result = str_replace(self::$en, self::$ar, strval($_string));
         $result = str_replace(self::$fa, self::$ar, $result);
         return $result;
     }
