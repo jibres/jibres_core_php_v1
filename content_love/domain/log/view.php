d129a77cdf5cc3474f7b33306ef15faa29fd7e00
Reza <rm.biqarar@gmail.com>
2022-06-28 16:02:18 +0430

    Save static count in domain log page

--- content_love/domain/log/view.php
+++ content_love/domain/log/view.php
@@ -48,17 +48,52 @@ class view
 		}
 
 
-		$group_by = \lib\app\nic_log\get::group_by_type();
-		if(!is_array($group_by))
+		// $group_by = \lib\app\nic_log\get::group_by_type();
+		// if(!is_array($group_by))
 		{
-			$group_by = [];
+			$group_by =
+			[
+				['count' =>  5806,      'type' =>  'contact_check' ,],
+				['count' =>  72,      'type' =>  'contact_info' ,],
+				['count' =>  26,      'type' =>  'contact_create' ,],
+				['count' =>  2153,      'type' =>  'contact_credit' ,],
+				['count' =>  49682,      'type' =>  'domain_check' ,],
+				['count' =>  21,      'type' =>  'domain_lock' ,],
+				['count' =>  24,      'type' =>  'domain_unlock' ,],
+				['count' =>  4669,      'type' =>  'domain_info' ,],
+				['count' =>  1130,      'type' =>  'domain_create' ,],
+				['count' =>  293,      'type' =>  'domain_update' ,],
+				['count' =>  361,      'type' =>  'domain_renew' ,],
+				['count' =>  20,      'type' =>  'domain_transfer' ,],
+				['count' =>  148478,      'type' =>  'poll_request' ,],
+				['count' =>  1225,      'type' =>  'poll_acknowledge' ,],
+			];
 		}
 		\dash\data::groupByType($group_by);
 
-		$group_by_code = \lib\app\nic_log\get::group_by_code();
-		if(!is_array($group_by_code))
+		// $group_by_code = \lib\app\nic_log\get::group_by_code();
+		// if(!is_array($group_by_code))
 		{
-			$group_by_code = [];
+			$group_by_code =
+			[
+				['count' => 51094,      'result_code' => '0'],
+				['count' => 61190,      'result_code' =>  '1000',],
+				['count' => 334,      'result_code' =>  '1001',],
+				['count' => 97926,      'result_code' =>  '1300',],
+				['count' => 1241,      'result_code' =>  '1301',],
+				['count' => 208,      'result_code' =>  '2005',],
+				['count' => 59,      'result_code' =>  '2100',],
+				['count' => 1518,      'result_code' =>  '2102',],
+				['count' => 1,      'result_code' =>  '2104',],
+				['count' => 4,      'result_code' =>  '2105',],
+				['count' => 34,      'result_code' =>  '2200',],
+				['count' => 34,      'result_code' =>  '2201',],
+				['count' => 2,      'result_code' =>  '2302',],
+				['count' => 219,      'result_code' =>  '2303',],
+				['count' => 18,      'result_code' =>  '2304',],
+				['count' => 29,      'result_code' =>  '2306',],
+				['count' => 49,      'result_code' =>  '2308',],
+			];
 		}
 		\dash\data::groupByCode($group_by_code);
 	}
