0c60a58b62c5dffc6ec3d0b5b3a0318f2e5aff27
Reza <rm.biqarar@gmail.com>
2022-06-29 18:09:47 +0430

    Show message

--- content_a/form/answer/add/display.php
+++ content_a/form/answer/add/display.php
@@ -1,3 +1,17 @@
 <?php
+$url = \lib\store::url(). '/f/'. \dash\data::formId();
+$html = '';
+
+$html .= '<div class="alert-danger text-center font-bold">';
+{
+	$html .= T_("Your are in admin mode");
+	$html .= '<br>';
+	$html .= T_("Public address to access this form");
+	$html .= ' <a target="_blank" class="block" data-copy="'. $url.'" href="'.$url. '">'. $url. '</a>';
+
+}
+$html .= '</div>';
+
+echo $html;
 echo \lib\app\form\generator::full_html(\dash\request::get('id'));
 ?>
