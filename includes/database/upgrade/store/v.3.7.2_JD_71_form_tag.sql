b2ca5e3ab9cb71379d32b4fbdd64d5acb91c600e
Reza <rm.biqarar@gmail.com>
2022-06-29 12:21:00 +0430

    JD-71 add form tag fields

---
+++ includes/database/upgrade/store/v.3.7.2_JD_71_form_tag.sql
@@ -0,0 +1,5 @@
+ALTER TABLE jibres_XXXXXXX.form_tag ADD `autocomment` bit  NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.form_tag ADD `comment` text  NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.form_tag ADD `sendsms` bit  NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.form_tag ADD `smstext` text  NULL DEFAULT NULL;
+ALTER TABLE jibres_XXXXXXX.form_tagusage ADD `datecreated` timestamp  NULL DEFAULT NULL;
