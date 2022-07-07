52b5a6809d0758df735dc90eb6a2bf2589a667b2
Reza <rm.biqarar@gmail.com>
2022-07-07 15:55:08 +0430

    JD-93 fix back link from business dashboard

--- content_a/home/view.php
+++ content_a/home/view.php
@@ -26,7 +26,7 @@ class view
 		{
 			// back
 			\dash\data::back_text(T_('Control Center'));
-			\dash\data::back_link(\dash\url::sitelang(). '/my');
+			\dash\data::back_link(\dash\url::set_subdomain('admin'). '/my');
 			// \dash\data::back_direct(true);
 		}
 	}
