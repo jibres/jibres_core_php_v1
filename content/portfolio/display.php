2f125a82a0fe7184f087cab9468d0f7f51c86245
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-07-11 21:06:04 +0430

    add fancy link and improve header

--- content/portfolio/display.php
+++ content/portfolio/display.php
@@ -1,32 +1,41 @@
-<div class="jibresBanner" data-text-style="t1">
+<div class="jibresBanner">
 
 
- <div class="avand impact text-center">
- 	<div class="grid grid-cols-1 md:grid-cols-3 items-center gap-2">
+ <div class="avand impact text-center" data-text-style="t1">
+ 	<div class="grid grid-cols-1 md:grid-cols-2 items-center gap-2 md:gap-4">
  		<img class="block" src="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg" alt="<?php echo T_('Jibres Portfolio'); ?>">
- 		<div class="col-span-2">
-			<?php echo T_("Jibres has provided infrastructure for many businesses. The trust of our dear customers in Jibres is very valuable to us. You can have your own website quickly and for free. Just try Jibres."); ?>
- 		</div>
+		<div class="text-xl leading-relaxed">
+			<p>
+				<span><?php echo T_("Jibres has provided infrastructure for many businesses."); ?></span>
+				<span><?php echo T_("The trust of our dear customers in Jibres is very valuable to us."); ?></span>
+			</p>
+			<p>
+				<span><?php echo T_("You can have your own website quickly and for free."); ?></span>
+			</p>
+			<a class="btn-outline-primary" href=""><?php echo T_("SIGN UP FREE"); ?></a>
+		</div>
  	</div>
  </div>
 
  <div class="m-auto max-w-screen-lg w-full px-2 sm:px-4 lg:px-5 relative py-5">
 	<div class="grid grid-cols-1 md:grid-cols-2 items-center gap-2">
 		
-			<figure>
+		<figure class="my-4">
+			<a target="_blank" data-fancybox="portfolioPreview" href="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg">
 				<img class="aspect-video bg-white rounded-lg" loading="lazy" src="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg" alt="w1">
-				<figcaption class="items long mt-2">
-					<ul class="">
-						<li>
-							<a class="item f" href="https://Bitty.ir" target="_blank" rel="nofollow noopener">
-							<div class="key">Bitty Store</div>
-							<div class="value">Bitty.ir</div>
-							<div class="go external"></div>
-							</a>
-						</li>
-					</ul>
-				</figcaption>
-			</figure>
+			</a>
+			<figcaption class="items long mt-2">
+				<ul class="">
+					<li>
+						<a class="item f" href="https://Bitty.ir" target="_blank" rel="nofollow noopener">
+						<div class="key">Bitty Store</div>
+						<div class="value">Bitty.ir</div>
+						<div class="go external"></div>
+						</a>
+					</li>
+				</ul>
+			</figcaption>
+		</figure>
 
 	</div>
  </div>
