922c28e76be32286c0ac9d8df16440079cb7cd71
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-07-11 21:11:26 +0430

    improve more

--- content/portfolio/display.php
+++ content/portfolio/display.php
@@ -9,19 +9,19 @@
 				<span><?php echo T_("Jibres has provided infrastructure for many businesses."); ?></span>
 				<span><?php echo T_("The trust of our dear customers in Jibres is very valuable to us."); ?></span>
 			</p>
-			<p>
-				<span><?php echo T_("You can have your own website quickly and for free."); ?></span>
-			</p>
+			<h2><?php echo T_("You can have your own website quickly and for free."); ?></h2>
 			<a class="btn-outline-primary" href=""><?php echo T_("SIGN UP FREE"); ?></a>
 		</div>
  	</div>
  </div>
+</div>
+
 
  <div class="m-auto max-w-screen-lg w-full px-2 sm:px-4 lg:px-5 relative py-5">
 	<div class="grid grid-cols-1 md:grid-cols-2 items-center gap-2">
 		
 		<figure class="my-4">
-			<a target="_blank" data-fancybox="portfolioPreview" href="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg">
+			<a class="hover:shadow-lg shadow transition block rounded-lg" target="_blank" data-fancybox="portfolioPreview" href="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg">
 				<img class="aspect-video bg-white rounded-lg" loading="lazy" src="<?php echo \dash\url::cdn(); ?>/img/cover/Jibres-cover-portfolio-1.jpg" alt="w1">
 			</a>
 			<figcaption class="items long mt-2">
@@ -39,4 +39,3 @@
 
 	</div>
  </div>
-</div>
