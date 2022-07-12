0ede641e65c3883f23db5c012944604a78a62604
Reza <rm.biqarar@gmail.com>
2022-07-12 16:52:22 +0430

    JD-105 check blog p4

--- content_site/body/blog/b1.php
+++ content_site/body/blog/b1.php
@@ -235,6 +235,8 @@ class b1
 			'version'        => 1,
 			'options' =>
 			[
+				'radius_normal'            => 'none',
+				'radius'                   => 'lg',
 				'post_show_readingtime'    => null,
 				'post_show_image'          => null,
 				'post_show_excerpt'        => true,
@@ -246,7 +248,7 @@ class b1
 				'coverratio'               => '16:9',
 				'count'                    => 1,
 				'color_text'               => '#333333',
-				'radius'                   => 'lg',
+				'btn_viewall_mode'         => 'dark',
 				'btn_viewall_check'        => null,
 				'btn_viewall'              => T_('View all'),
 				'background_pack'          => 'gradient',
