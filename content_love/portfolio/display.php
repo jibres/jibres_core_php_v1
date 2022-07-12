ad2b5ee98395c3a270a7ee4741d7a002b84dfd62
Reza <rm.biqarar@gmail.com>
2022-07-12 14:23:28 +0430

    Fix show language in portfolio

--- content_love/portfolio/display.php
+++ content_love/portfolio/display.php
@@ -4,9 +4,10 @@
     <?php foreach (\dash\data::dataTable() as $key => $value) {?>
      <li>
       <a class="item f align-center" href="<?php echo \dash\url::that(). '/edit?id='. a($value, 'id'); ?>">
-        <div class="key"><?php echo a($value, 'language'). ' - '. (a($value, 'title')); ?></div>
+        <div class="key"><?php echo a($value, 'title'); ?></div>
         <div class="value"><?php echo htmlGenerateTag($value) ?></div>
         <time class="value"><?php echo T_(ucfirst(strval(a($value, 'industry')))) ?></time>
+        <time class="value"><?php echo a($value, 'language') ?></time>
         <div class="go <?php if(a($value, 'status') === 'accept') {echo 'ok';} ?>"></div>
       </a>
      </li>
