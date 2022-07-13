034825c9d9e7bf87cf4209ab58d83607f71421db
Reza <rm.biqarar@gmail.com>
2022-07-13 19:05:56 +0430

    JD-105 visitcart1

--- content_site/body/visitcard/visitcard1.php
+++ content_site/body/visitcard/visitcard1.php
@@ -66,20 +66,39 @@ class visitcard1
 	/**
 	 * Preview 1
 	 */
+
+
+
+	/**
+	 * Auto Generate Function
+	 * @date 2022-07-13 19:05:08
+	 * @author rm.biqarar@gmail.com
+	 *
+	 * @path content_site/body/visitcard/visitcard1.php
+	 * body / visitcard / visitcard1 / p1
+	 *
+	*/
 	public static function p1()
 	{
 		return
 		[
-			'version'        => 1,
+			'version' => 1,
 			'options' =>
 			[
+				'use_as_socialnetwork' => 'business_socialnetwork',
+				'use_as_logo'          => 'business_logo',
+				'use_as_heading'       => 'business_heading',
+				'use_as_description'   => 'business_description',
 				'radius'               => 'full',
-
+				'height'               => 'fullscreen',
+				'heading'              => null,
+				'background_pack'      => 'solid',
+				'background_color'     => '#eeeeee',
 			],
 		];
 	}
-
-
+	// path content_site/body/visitcard/visitcard1.php
+	// body / visitcard / visitcard1 / p1
 
 }
 ?>
