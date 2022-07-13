fff6e39ceed4e19f5567c6944e04a670da5b277b
Reza <rm.biqarar@gmail.com>
2022-07-13 14:10:52 +0430

    Add some link in content site

--- content_site/home/display.php
+++ content_site/home/display.php
@@ -6,6 +6,16 @@ $html = '';
 echo $html;
 ?>
 <section class="row">
+	<?php if(\content_site\homepage::id()) {?>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::this(). '/page?id='. \content_site\homepage::code() ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Edit homepage") ?></div>
+			</div>
+		</a>
+	</div>
+	<?php } //endif ?>
+
 	<div class="c-xs-12 c-4 mb-1">
 		<a href="<?php echo \dash\url::this(). '/pages' ?>">
 			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
@@ -38,6 +48,13 @@ echo $html;
 		<a href="<?php echo \dash\url::this(). '/staticfile' ?>">
 			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
 				<div class="text-center "><?php echo T_("Static file") ?></div>
+			</div>
+		</a>
+	</div>
+	<div class="c-xs-12 c-4 mb-1">
+		<a href="<?php echo \dash\url::this(). '/autosave' ?>">
+			<div class="bg-white rounded-lg flex justify-center items-center align-middle h-16 w-full block">
+				<div class="text-center "><?php echo T_("Setting auto-save and publish") ?></div>
 			</div>
 		</a>
 	</div>
