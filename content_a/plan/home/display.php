064063a806ee824a1608a052ad18b93fdeac0cbc
Reza <rm.biqarar@gmail.com>
2022-08-14 16:02:53 +0430

    show plan button

--- content_a/plan/home/display.php
+++ content_a/plan/home/display.php
@@ -4,7 +4,7 @@ $planList = \dash\data::planList();
 
 foreach ($planList as $plan)
 {
-    $html .= "<div data-ajaxify data-data='". json_encode(['plan' => $plan['name']]). "'>". $plan['title']. '</div>';
+    $html .= "<div class='btn-primary mx-2' data-ajaxify data-data='". json_encode(['plan' => $plan['name']]). "'>". $plan['title']. '</div>';
 }
 echo $html;
 ?>
