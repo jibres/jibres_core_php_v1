cb0ffda0418f672767601523ae63ddfcbc0020cf
Reza <rm.biqarar@gmail.com>
2022-08-20 15:56:33 +0430

    JD-172 Add sample private plan

---
+++ lib/app/plan/plans/rafiei.php
@@ -0,0 +1,54 @@
+<?php
+namespace lib\app\plan\plans;
+
+
+use lib\app\plan\planPrepare;
+
+
+class rafiei extends planPrepare
+{
+
+    public function name(): string
+    {
+        return 'rafiei';
+    }
+
+
+    public function title(): string
+    {
+        return T_("Rafiei");
+    }
+
+
+    public function priceRial(): int
+    {
+        return 2000000; // IRT
+    }
+
+
+    public function priceDollar(): int
+    {
+        return 90; // USD
+    }
+
+
+    public function featureList() : array
+    {
+        return
+        [
+
+            T_("Enterprice plan"),
+        ];
+    }
+
+    public function type(): string
+    {
+       return 'enterprise';
+    }
+
+
+    public static function enterprise() : string
+    {
+        return 'rafiei';
+    }
+}
