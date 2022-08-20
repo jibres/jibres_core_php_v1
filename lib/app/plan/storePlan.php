8c6306c7e963ee3608c8f7705529b834de745487
Reza <rm.biqarar@gmail.com>
2022-08-20 11:31:46 +0430

    JD-172 save

--- lib/app/plan/storePlan.php
+++ lib/app/plan/storePlan.php
@@ -8,7 +8,10 @@ class storePlan
     public static function currentPlan($_business_id)
     {
         $loadBusinessData = \lib\db\store\get::data($_business_id);
-        print_r($loadBusinessData);exit;
+        $dateNow = date("Y-m-d H:i:s");
+        $planHistoryList = \lib\db\store_plan_history\get::activePlanList($_business_id, $dateNow);
+
+        var_dump($planHistoryList);exit();
     }
 
 }
