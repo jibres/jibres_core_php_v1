5d62ff2491e2964de7a62327a494d51899fbcb0c
Reza <rm.biqarar@gmail.com>
2022-08-20 15:56:08 +0430

    JD-172 add plan prepare

---
+++ lib/app/plan/planPrepare.php
@@ -0,0 +1,86 @@
+<?php
+namespace lib\app\plan;
+
+abstract class planPrepare implements plan
+{
+    private $period;
+    private planPrice $planPrice;
+    private $days = 0;
+
+    public function prepare()
+    {
+        $this->planPrice = new planPrice($this);
+    }
+
+    public function period()
+    {
+        return $this->period;
+    }
+
+    public function setPeriod($_period)
+    {
+        $this->period = $_period;
+
+    }
+
+
+    public function setDays($_days)
+    {
+        $this->days = $_days;
+    }
+
+    public function price()
+    {
+        return $this->planPrice->calculatePrice($this->period);
+    }
+
+    public function currency()
+    {
+        return $this->planPrice->getCurrency();
+    }
+
+
+    public function setBy()
+    {
+        if(in_array(\dash\url::conent(), ['love']))
+        {
+            $setBy = 'admin';
+        }
+        else
+        {
+            $setBy = 'customer';
+        }
+        // if is cronjob
+        // return 'system'
+
+        return $setBy;
+    }
+
+
+    public function calculateDays() : int
+    {
+        $days = 0;
+        if($this->period === 'monthly')
+        {
+            $days = 31;
+        }
+        elseif($this->period === 'yearly')
+        {
+            $days = 365;
+        }
+        elseif($this->period === 'custom')
+        {
+            $days = $this->days;
+        }
+        else
+        {
+            $days = 0;
+        }
+
+        return $days;
+    }
+
+
+
+
+}
