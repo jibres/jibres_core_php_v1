4e4d04dad460e66c1ff481978a92c598cd06fa13
Reza <rm.biqarar@gmail.com>
2022-08-25 18:48:19 +0430

    JD-172 Admin can edit plan

--- content_love/plan/detail/display.php
+++ content_love/plan/detail/display.php
@@ -1,44 +1,86 @@
 <?php
 $data = \dash\data::dataRow();
 $html = '';
-$html .= '<nav class="items ltr">';
+$html .= '<div class="avand">';
 {
-    $html .= '<ul>';
+    $html .= '<form method="post" autocomplete="off">';
     {
-        foreach ($data as $field => $value) {
-            $html .= '<li>';
+
+        $html .= "<div class='box'>";
+        {
+            $html .= "<div class='pad'>";
             {
-                $html .= '<a class="f item">';
-                {
-                    $html .= '<div class="key">';
-                    {
-                        $html .= $field;
-                    }
-                    $html .='</div>';
-                    $html .= '<div class="value font-bold">';
-                    {
+                $html .= '<label for="status">' . T_("Status") . '</label>';
 
-                        $html .= $value;
-                    }
-                    $html .= '</div>';
-                }
-                $html .= '</a>';
-
-                $html .= $field;
-                $html .= ' <b>';
+                $html .= '<select name="status" class="select22" id="status">';
                 {
-                    $html .= $value;
+                    $html .= '<option value="active" ';
+                    if(\dash\data::dataRow_status() === 'active')
+                    {
+                        $html .= 'selected';
+                    }
+                    $html .= '>' . T_("Active") . '</option>';
+                    $html .= '<option value="deactive" ';
+                    if(\dash\data::dataRow_status() === 'deactive')
+                    {
+                        $html .= 'selected';
+                    }
+                    $html .= '>' . T_("Deactive") . '</option>';
                 }
-                $html .= '</b>';
+                $html .= '</select>';
+
+                $html .= '<label for="reason">' . T_("Reason") . '</label>';
+                $html .= '<div class="input">';
+                {
+                    $html .= '<input type="text" name="reason" placeholder="' . T_("Reason") . '" value="'.a($data, 'reason').'">';
+                }
+                $html .= '</div>';
+
+                $html .= '<div class="txtRa mt-2">';
+                {
+                    $html .= '<button type="submit" class="btn-success">' . T_("Save") . '</button>';
+                }
+                $html .= '</div>';
+
             }
-            $html .= '</li>';
+            $html .= "</div>";
         }
+        $html .= "</div>";
+
     }
-    $html .= '</ul>';
+    $html .= '</form>';
+
+
+    $html .= '<nav class="items ltr">';
+    {
+        $html .= '<ul>';
+        {
+            foreach ($data as $field => $value) {
+                $html .= '<li>';
+                {
+                    $html .= '<a class="f item">';
+                    {
+                        $html .= '<div class="key">';
+                        {
+                            $html .= $field;
+                        }
+                        $html .= '</div>';
+                        $html .= '<div class="value font-bold">';
+                        {
+                            $html .= $value;
+                        }
+                        $html .= '</div>';
+                    }
+                    $html .= '</a>';
+                }
+                $html .= '</li>';
+            }
+        }
+        $html .= '</ul>';
+    }
+    $html .= '</nav>';
 }
-$html .= '</nav>';
-
-
+$html .= '</div>';
 echo $html;
 ?>
 
