c97bad4658e825acedd28f5cc5aa952bf660cddc
Reza <rm.biqarar@gmail.com>
2022-08-28 10:12:32 +0430

    JD-172 Load plan and check plugin key

--- lib/app/plan/planCheck.php
+++ lib/app/plan/planCheck.php
@@ -7,11 +7,20 @@ class planCheck
 
     public static function access($_plugin_key) : bool
     {
-        // TODO load current plan
-        // if this plugin key exist in contain plan
-        return true;
-        // else
-        // return false;
+        $loadCurrentPlan = businessPlanDetail::getMyPlanDetail();
+        if($loadCurrentPlan)
+        {
+            $contain = $loadCurrentPlan->contain();
+        }
+
+        if(in_array($_plugin_key, $contain))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
     }
 
