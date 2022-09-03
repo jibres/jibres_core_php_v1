b9a57030b02a3d749a655c837cca4844b40d765f
Reza <rm.biqarar@gmail.com>
2022-09-03 12:58:49 +0430

    JD-212 Bug fix show real data in user store list

--- content_love/business/userstore/display.php
+++ content_love/business/userstore/display.php
@@ -52,11 +52,7 @@
                 </td>
 
             </tr>
-            <tr class="">
-                <td colspan="7">
-                    <pre class="hide"><?php print_r($value) ?></pre>
-                </td>
-            </tr>
+
 
 		<?php } //endfor ?>
         </tbody>
