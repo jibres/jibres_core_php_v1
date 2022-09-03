358cf6534657f5d26d527a40f290cf31dd9cadac
Reza <rm.biqarar@gmail.com>
2022-09-03 09:59:26 +0430

    Try to find wrong user store record

--- content_sudo/fix/user_store/controller.php
+++ content_sudo/fix/user_store/controller.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace content_sudo\fix\user_store;
 
 class controller
@@ -9,77 +10,100 @@ class controller
 
 		\dash\code::time_limit(0);
 
-		$get_all_staff = \dash\pdo::get("SELECT * FROM store_user WHERE 1 ");
+		$get_all_user_store = \dash\pdo::get("SELECT * FROM store_user WHERE 1 ");
 
-		$result                                           = [];
-		$result['ok']                                     = 0;
-		$result['in_store_admin_but_in_jibres_not_staff'] = 0;
-		$result['in_store_customer_but_in_jibres_staff']  = 0;
-		$result['ok2']                                    = 0;
-		$result['user_not_found_in_store']                = 0;
-		$result['list1']                                  = [];
-		$result['list2']                                  = [];
-		$result['fix1']                                   = [];
+		$result                           = [];
+		$result['ok']                     = 0;
+		$result['ok2']                     = 0;
 
-		foreach ($get_all_staff as $key => $staff_detail)
+		// in business user not found
+		$result['a1']  = 0;
+		$result['a1l'] = [];
+
+
+		$result['a2']  = 0;
+		$result['a2l'] = [];
+
+		$result['a3']  = 0;
+		$result['a3l'] = [];
+
+
+		foreach ($get_all_user_store as $user_store)
 		{
-			$store = \dash\pdo::get("SELECT * FROM store WHERE store.id = :id LIMIT 1", [':id' => a($staff_detail, 'store_id')], null, true, 'master');
+			$store =
+				\dash\pdo::get(
+					"SELECT * FROM store WHERE store.id = :id LIMIT 1",
+					[':id' => a($user_store, 'store_id')],
+					null,
+					true,
+					'master');
 
 			\dash\engine\store::force_lock($store);
 
-			$store_user_detail = \dash\pdo::get("SELECT * FROM users WHERE users.jibres_user_id = :jibres_user_id  LIMIT 1", [':jibres_user_id' => a($staff_detail, 'user_id')], null, true);
+			$in_business_user_detail =
+				\dash\pdo::get("SELECT * FROM users WHERE users.jibres_user_id = :jibres_user_id  LIMIT 1", [':jibres_user_id' => a($user_store, 'user_id')], null, true);
 
-			if(isset($store_user_detail['permission']) && $store_user_detail['permission'])
+			$in_business_user_found = a($in_business_user_detail, 'id') ? true : false;
+			$in_business_user_admin = a($in_business_user_detail, 'permission') ? true : false;
+			$in_jibres_user_staff   = (a($user_store, 'staff') === 'yes') ? true : false;
+
+			if(!$in_business_user_found)
 			{
-				if(isset($staff_detail['staff']) && $staff_detail['staff'] === 'yes')
-				{
-					$result['ok']++;
-					// ok
-				}
-				else
-				{
-					$result['in_store_admin_but_in_jibres_not_staff']++;
-					$result['list1'][]     = $store_user_detail['mobile'];
-					$result['set_staff'][] = ['user_id' => $staff_detail['user_id'], 'store_id' => $staff_detail['store_id']];
-				}
-			}
-			elseif(isset($store_user_detail['id']))
-			{
-				if(isset($staff_detail['staff']) && $staff_detail['staff'] === 'yes')
-				{
-					$result['in_store_customer_but_in_jibres_staff']++;
-					$result['list2'][] = $store_user_detail['mobile'];
-					// ok
-				}
-				else
-				{
-					$result['ok2']++;
-				}
-			}
-			else
-			{
-				$result['user_not_found_in_store']++;
+				$result['a1']++;
+				$result['a1l'][] = [$user_store['id'], self::get_mobile($user_store)];
+				continue;
 			}
 
+			if($in_business_user_admin && $in_jibres_user_staff)
+			{
+				$result['ok']++;
+				continue;
+			}
+
+			if($in_business_user_admin && !$in_jibres_user_staff)
+			{
+				$result['a2']++;
+				$result['a2l'][] = [$user_store['id'], self::get_mobile($user_store)];
+				continue;
+			}
+
+			if(!$in_business_user_admin && $in_jibres_user_staff)
+			{
+				$result['a3']++;
+				$result['a3l'][] = [$user_store['id'], self::get_mobile($user_store)];
+				continue;
+			}
+
+			if(!$in_business_user_admin && !$in_jibres_user_staff)
+			{
+				$result['ok2']++;
+				continue;
+			}
 
 
 
+
+
+
+			\dash\engine\store::unlock();
 			\dash\pdo::close();
 		}
 
 
-		\dash\log::to_supervisor('user_store_result:'. json_encode($result));
-		var_dump($result);exit;
-
+		\dash\log::to_supervisor('user_store_result:' . json_encode($result));
+		var_dump($result);
+		exit;
 
 
 	}
 
 
-	private static function get_mobile($staff_detail)
+	private static function get_mobile($user_store)
 	{
-		return \dash\pdo::get("SELECT users.mobile FROM users WHERE users.id = :id LIMIT 1", [':id' => a($staff_detail, 'user_id')], 'mobile', true, 'master');
+		return \dash\pdo::get("SELECT users.mobile FROM users WHERE users.id = :id LIMIT 1", [':id' => a($user_store, 'user_id')], 'mobile', true, 'master');
 
 	}
+
 }
+
 ?>
