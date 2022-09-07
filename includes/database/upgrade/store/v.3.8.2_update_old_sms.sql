ee1226121fb0131cfff4be71e723f776058fc1c4
Reza <rm.biqarar@gmail.com>
2022-09-07 15:51:04 +0430

    Try to send sms

--- includes/database/upgrade/store/v.3.8.2_update_old_sms.sql
+++
@@ -1,1 +0,0 @@
-UPDATE jibres_XXXXXXX.sms_log SET sms_log.status = 'expired' WHERE sms_log.status IN('pending', 'register') AND sms_log.jibres_sms_id IS NULL AND sms_log.datecreated < '2022-09-07 12:00:00' ;
