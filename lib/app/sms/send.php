2ef915d54bf81ccb3dc8eba769c63e086f68b180
Reza <rm.biqarar@gmail.com>
2022-09-07 16:01:22 +0430

    Try to send sms

--- lib/app/sms/send.php
+++ lib/app/sms/send.php
@@ -352,7 +352,7 @@ class send
 
 	public static function notSended()
 	{
-		$notSend = \lib\db\sms_log\get::not_sended(1);
+		$notSend = \lib\db\sms_log\get::not_sended(2);
 
 		if(!is_array($notSend))
 		{
