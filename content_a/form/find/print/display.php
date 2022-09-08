3c3ed731ffc58c78feb6600516dfbf03f106b449
Reza <rm.biqarar@gmail.com>
2022-09-08 12:21:08 +0430

    JD-240 Try to add special print page

--- content_a/form/find/print/display.php
+++ content_a/form/find/print/display.php
@@ -1,79 +1,62 @@
 <?php
 $html = '';
 
-if(\dash\data::specailPage())
+
+$html .= '<div class="printArea" data-size="A4">';
 {
-	$html .= \dash\data::specailPage();
-}
-else
-{
-	$html .= '<div class="printArea" data-size="A4">';
+	$html .= '<div class="row">';
 	{
-
-		$html .= '<div class="alert-info text-left ltr font-bold text-sm">';
+		$html .= '<div class="c-auto mb-1">';
 		{
-
-			$html .= '<div class="f">';
-			{
-
-				$html .= '<div class="cauto">';
-				{
-
-					$html .= '<span>' . T_("Answer ID") . '</span>';
-					$html .= '<span><code class="inline-block font-bold">' . \dash\request::get('id') . '_' . \dash\request::get('aid') . '</code></span>';
-				}
-				$html .= '</div>';
-
-				$html .= '<div class="c"></div>';
-
-				$html .= '<div class="cauto">';
-				{
-					$html .= '<a class="font-14 print:hidden" href="' . \dash\url::current() . \dash\request::full_get(['print' => null]) . '">' . T_("Back") . '</a>';
-				}
-				$html .= '</div>';
-			}
-			$html .= '</div>';
+			$html .= '<img class="inline-block w-16 h-16 rounded" src="'.\lib\store::logo().'">';
 		}
 		$html .= '</div>';
-
-		$html .= '<table class="tbl1 v6">';
+		$html .= '<div class="c">';
 		{
-
-			$html .= '<tbody class="text-sm">';
-			{
-
-				$i = 0;
-				foreach (\dash\data::dataTable() as $key => $value)
-				{
-					$i++;
-					if($i % 2)
-					{
-						$html .= '<tr>';
-					}
-
-					$html .= '<th class="">' . a($value, 'item_title') . '</th>';
-					$html .= '<td class="">';
-					{
-						$html .= \lib\app\form\answer\get::HTMLshowDetaiRecrod($value);
-					}
-					$html .= '</td>';
-					if(!($i % 2))
-					{
-						$html .= '</tr>';
-					}
-				}
-			}
-			$html .= '</tbody>';
+			$html .= '<div class="inline-block px-2 text-2xl font-bold mt-4">'. \lib\store::title(). '</div>';
 		}
-		$html .= '</table>';
-
+		$html .= '</div>';
 	}
 	$html .= '</div>';
 
+
+
+	$html .= '<table class="tbl1 v6 responsive">';
+	{
+
+		$html .= '<tbody class="text-sm">';
+		{
+			foreach (\dash\data::dataTable() as $key => $value)
+			{
+				$html .= '<tr>';
+				{
+					$html .= '<th class="">';
+					{
+						$html .= a($value, 'item_title');
+					}
+					$html .= '</th>';
+
+					$html .= '<td class="">';
+					{
+						$html .=  \lib\app\form\answer\get::HTMLshowDetaiRecrod($value);
+					}
+					$html .= '</td>';
+				}
+				$html .= '</tr>';
+			}
+		}
+		$html .= '</tbody>';
+	}
+	$html .= '</table>';
 	$html .= \dash\utility\pagination::html(true);
 
 
+
 }
+$html .= '</div>';
+
+$html .= \dash\utility\pagination::html(true);
+
 
 echo $html;
 ?>
