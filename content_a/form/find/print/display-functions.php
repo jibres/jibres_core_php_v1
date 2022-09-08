f6a7d7584007b9398740cf8cebd748657237ef75
Reza <rm.biqarar@gmail.com>
2022-09-08 12:57:11 +0430

    JD-240 Try to add special print page

--- content_a/form/find/print/display-functions.php
+++ content_a/form/find/print/display-functions.php
@@ -1,5 +1,57 @@
 <?php
 
+function HTMLDetectData()
+{
+
+	$tagWinner      = false;
+	$tagRemain      = false;
+	$eastProvince   = false;
+	$westProvince   = false;
+	$tagPrintBefore = false;
+
+	$tagWinner      = true;
+	$tagRemain      = true;
+	$eastProvince   = true;
+	$westProvince   = true;
+	$tagPrintBefore = true;
+
+
+	if($tagWinner)
+	{
+		$payablePrice = 2000000;
+
+		if($eastProvince)
+		{
+			$payablePrice = 2500000;
+		}
+
+	}
+	elseif($tagRemain)
+	{
+		$payablePrice = 500000;
+	}
+	else
+	{
+		$payablePrice = 0;
+	}
+
+	$currency = T_("Toman");
+
+	$data = (object)
+	[
+		'tagPrintBefore' => $tagPrintBefore,
+		'westProvince'   => $westProvince,
+		'tagWinner'      => $tagWinner,
+		'eastProvince'   => $eastProvince,
+		'tagRemain'      => $tagRemain,
+		'payablePrice'   => $payablePrice,
+		'currency'       => $currency,
+	];
+
+	return $data;
+}
+
+
 
 function HTMLWinnerMessage(object $data)
 {
