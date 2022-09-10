ab743479b8d28f510711d232f2800b5f77092c80
Reza <rm.biqarar@gmail.com>
2022-09-10 12:52:46 +0430

    Bug fix check domain

--- lib/app/business_domain/add.php
+++ lib/app/business_domain/add.php
@@ -192,33 +192,33 @@ class add
 		$ir_domain = \dash\validate::ir_domain($domain, false);
 		if($ir_domain)
 		{
-			$domainRaw = $parse_url['root']. '.'. $parse_url['tld'];
-			$fetch_domain = \lib\app\nic_domain\get::only_info($domainRaw);
-			if(isset($fetch_domain[$domain]['status']) && is_array($fetch_domain[$domain]['status']))
-			{
-				if(in_array('ok', $fetch_domain[$domain]['status']))
-				{
-					// ok
-				}
-				else
-				{
-					\dash\notif::error(T_("This domain is not activate yet in nic provider"));
-					return false;
-				}
-			}
-			else
-			{
-				if(isset($fetch_domain['available']) && $fetch_domain['available'] === true)
-				{
-					\dash\notif::error(T_("Domain not exist!"));
-					return false;
-				}
-				else
-				{
-					\dash\notif::error(T_("Can not fetch domain detail from nic provider"));
-					return false;
-				}
-			}
+			// $domainRaw = $parse_url['root']. '.'. $parse_url['tld'];
+			// $fetch_domain = \lib\app\nic_domain\get::only_info($domainRaw);
+			// if(isset($fetch_domain[$domain]['status']) && is_array($fetch_domain[$domain]['status']))
+			// {
+			// 	if(in_array('ok', $fetch_domain[$domain]['status']))
+			// 	{
+			// 		// ok
+			// 	}
+			// 	else
+			// 	{
+			// 		\dash\notif::error(T_("This domain is not activate yet in nic provider"));
+			// 		return false;
+			// 	}
+			// }
+			// else
+			// {
+			// 	if(isset($fetch_domain['available']) && $fetch_domain['available'] === true)
+			// 	{
+			// 		\dash\notif::error(T_("Domain not exist!"));
+			// 		return false;
+			// 	}
+			// 	else
+			// 	{
+			// 		\dash\notif::error(T_("Can not fetch domain detail from nic provider"));
+			// 		return false;
+			// 	}
+			// }
 		}
 		else
 		{
