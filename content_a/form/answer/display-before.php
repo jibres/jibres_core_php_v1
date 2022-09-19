b46ba61c2a445cb174aca7397f1815fe0eec3661
Reza <rm.biqarar@gmail.com>
2022-09-19 17:29:51 +0430

    Some fix on design

--- content_a/form/answer/display-before.php
+++ content_a/form/answer/display-before.php
@@ -1,6 +1,6 @@
 <?php if(\dash\data::countNotReviewed()) {?>
-   <div class="alert-info text-sm row align-center mb-2">
+   <div class="alert-secondary text-sm row align-center mb-2">
      <div class="c c-xs-12 font-bold"><?php echo T_("You have :val not reviewed answer", ['val' => \dash\fit::number(\dash\data::countNotReviewed())]) ?></div>
-     <div class="c-auto c-xs-12"><div class="btn-dark btn-sm" data-confirm data-data='{"mark": "all"}'><?php echo T_("Mark all as review") ?></div></div>
+     <div class="c-auto c-xs-12"><div class="btn-outline-secondary btn-sm" data-confirm data-data='{"mark": "all"}'><?php echo T_("Mark all as review") ?></div></div>
    </div>
 <?php } ?>
--- content_a/form/answer/display.php
+++ content_a/form/answer/display.php
@@ -4,7 +4,7 @@
     <thead>
       <tr>
         <th class="collapsing"><?php echo T_("ID") ?></th>
-        <th><?php echo T_("Start date") ?></th>
+        <th><?php echo T_("Date") ?></th>
         <th><?php echo T_("Count answer") ?></th>
         <th><?php echo T_("Status") ?></th>
         <th class="collapsing"></th>
@@ -20,13 +20,13 @@
           <td><?php echo \dash\fit::number(a($value, 'count_answer')); ?></td>
           <td><?php echo T_(strval(a($value, 'status'))); ?></td>
           <td class="collapsing">
-            <?php if(a($value, 'user_id')) {?><a class="btn-link  leading-none  text-sm" href="<?php echo \dash\url::kingdom(). '/crm/member/glance?id='. \dash\coding::encode(a($value, 'user_id')); ?>"><?php echo T_("View User profile") ?></a><?php } //endif ?>
-            <?php if(a($value, 'factor_id')) {?><a class="btn-link  leading-none  text-sm" href="<?php echo \dash\url::kingdom(). '/a/order/comment?id='. a($value, 'factor_id'); ?>"><?php echo T_("View Order") ?></a><?php } //endif ?>
-            <?php if(a($value, 'transaction_id')) {?><a class="btn-link  leading-none text-sm" href="<?php echo \dash\url::kingdom(). '/crm/transactions/detail?id='. a($value, 'transaction_id'); ?>"><?php echo T_("View Transaction") ?></a><?php } //endif ?>
-            <?php if(a($value, 'ticket_id')) {?><a class="btn-link  leading-none  text-sm" href="<?php echo \dash\url::kingdom(). '/crm/ticket/view?id='. a($value, 'ticket_id'); ?>"><?php echo T_("View Ticket") ?></a><?php } //endif ?>
+            <?php if(a($value, 'user_id')) {?><a class="btn-link  leading-none  btn-sm" href="<?php echo \dash\url::kingdom(). '/crm/member/glance?id='. \dash\coding::encode(a($value, 'user_id')); ?>"><?php echo T_("View User profile") ?></a><?php } //endif ?>
+            <?php if(a($value, 'factor_id')) {?><a class="btn-link  leading-none  btn-sm" href="<?php echo \dash\url::kingdom(). '/a/order/comment?id='. a($value, 'factor_id'); ?>"><?php echo T_("View Order") ?></a><?php } //endif ?>
+            <?php if(a($value, 'transaction_id')) {?><a class="btn-link  leading-none btn-sm" href="<?php echo \dash\url::kingdom(). '/crm/transactions/detail?id='. a($value, 'transaction_id'); ?>"><?php echo T_("View Transaction") ?></a><?php } //endif ?>
+            <?php if(a($value, 'ticket_id')) {?><a class="btn-link  leading-none  btn-sm" href="<?php echo \dash\url::kingdom(). '/crm/ticket/view?id='. a($value, 'ticket_id'); ?>"><?php echo T_("View Ticket") ?></a><?php } //endif ?>
           </td>
           <td class="collapsing"><?php if(a($value, 'review')){echo \dash\utility\icon::svg('check-circle', 'bootstrap', 'green', 'w-4') ;}else{echo \dash\utility\icon::svg('question-circle', 'bootstrap', '#D3D3D3', 'w-4');} ?></td>
-          <td class="collapsing"><a class="btn-secondary btn-sm" href="<?php echo \dash\url::that(). '/detail?id='. \dash\request::get('id'). '&aid='. a($value, 'id'); ?>"><?php echo T_("Detail") ?></a></td>
+          <td class="collapsing"><a class="btn-outline-secondary btn-sm" href="<?php echo \dash\url::that(). '/detail?id='. \dash\request::get('id'). '&aid='. a($value, 'id'); ?>"><?php echo T_("Detail") ?></a></td>
         </tr>
       <?php } //endif ?>
     </tbody>
