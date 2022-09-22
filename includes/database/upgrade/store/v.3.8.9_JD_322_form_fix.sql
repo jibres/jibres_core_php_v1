38ca8d3cdd256bf2dc958fcea83101a9164a27bb
Reza <rm.biqarar@gmail.com>
2022-09-22 16:57:59 +0330

    JD-322 Add form view table

---
+++ includes/database/upgrade/store/v.3.8.9_JD_322_form_fix.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS jibres_XXXXXXX.form_view
+(
+	`id`         bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+	`form_id`    int UNSIGNED         DEFAULT NULL,
+	`token`      char(32)        NULL DEFAULT NULL,
+	`ip_id`      int UNSIGNED         DEFAULT NULL,
+	`agent_id`   int UNSIGNED         DEFAULT NULL,
+	`user_id`    int UNSIGNED         DEFAULT NULL,
+	`viewtime`   timestamp       NULL DEFAULT NULL,
+	`starttime`  timestamp       NULL DEFAULT NULL,
+	`url_id`     int UNSIGNED         DEFAULT NULL,
+	`referer_id` int UNSIGNED         DEFAULT NULL,
+	`questions`  text                 DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `form_view_index_token` (`form_id`, `token`),
+	INDEX `form_view_index_ip_agent` (`form_id`, `ip_id`, `agent_id`),
+	CONSTRAINT `form_view_form_id` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+
+
+ALTER TABLE jibres_XXXXXXX.form_answer
+	ADD `form_view_id` bigint UNSIGNED NULL DEFAULT NULL;
+
+ALTER TABLE jibres_XXXXXXX.form_answer
+	ADD CONSTRAINT `form_answer_form_view_id` FOREIGN KEY (`form_view_id`) REFERENCES `form_view` (`id`) ON UPDATE CASCADE;
+
+
