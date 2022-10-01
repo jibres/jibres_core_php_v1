78de7d71683573d607c23eaa4a3d536d1ab75fd4
Reza <rm.biqarar@gmail.com>
2022-10-02 02:38:11 +0330

    Remove slug pattern

--- content_a/products/edit/block/seo.php
+++ content_a/products/edit/block/seo.php
@@ -21,7 +21,7 @@
         <div>
           <label for="seoSlug"><?php echo T_("Slug"); ?> <small><?php echo T_("End part of your product url."); ?></small></label>
           <div class="input ltr mb-2">
-            <input type="text" name="slug" id="seoSlug" placeholder='<?php echo T_("Slug"); ?>' value="<?php echo a($productDataRow,'slug'); ?>" maxlength='100' minlength="1" pattern=".{1,100}">
+            <input type="text" name="slug" id="seoSlug" placeholder='<?php echo T_("Slug"); ?>' value="<?php echo a($productDataRow,'slug'); ?>" maxlength='100' minlength="1" ">
           </div>
         </div>
         <div>
